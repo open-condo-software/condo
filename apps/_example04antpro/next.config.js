f3779ff2ffd7b70ed98ad0e998923971372b7907
Pahaz <pahaz.white@gmail.com>
2020-04-24 17:26:27 +0300

    _example04antpro: remove babel hacks!

--- apps/_example04antpro/next.config.js
+++ apps/_example04antpro/next.config.js
@@ -4,27 +4,4 @@ module.exports = withLess({
     lessLoaderOptions: {
         javascriptEnabled: true,
     },
-    webpack: (config, { isServer }) => {
-        if (isServer) {
-            const antStyles = /antd\/.*?\/style.*?/
-            const origExternals = [...config.externals]
-            config.externals = [
-                (context, request, callback) => {
-                    if (request.match(antStyles)) return callback()
-                    if (typeof origExternals[0] === 'function') {
-                        origExternals[0](context, request, callback)
-                    } else {
-                        callback()
-                    }
-                },
-                ...(typeof origExternals[0] === 'function' ? [] : origExternals),
-            ]
-
-            config.module.rules.unshift({
-                test: antStyles,
-                use: 'null-loader',
-            })
-        }
-        return config
-    },
 });
