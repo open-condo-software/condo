19d023764db56979ebbf6c310045a9096f0559ff
Pahaz <pahaz.white@gmail.com>
2020-04-30 20:30:13 +0300

    config: use mongodb by default

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ require('dotenv').config();
 
 module.exports = {
     PROJECT_NAME: process.env.PROJECT_NAME || 'my-keystone-app',
-    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres:postgres@127.0.0.1/main',
-    // DATABASE_URL: process.env.DATABASE_URL || 'mongodb://mongo:mongo@127.0.0.1/main?authSource=admin',
+    // DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres:postgres@127.0.0.1/main',
+    DATABASE_URL: process.env.DATABASE_URL || 'mongodb://mongo:mongo@127.0.0.1/main?authSource=admin',
     MEDIA_ROOT: process.env.MEDIA_ROOT || '__media',
     MEDIA_URL: process.env.MEDIA_URL || '/media',
     DIST_DIR: process.env.DIST_DIR || 'dist',
