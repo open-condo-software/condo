fd20f7d00abeff052743207159cb2829294421b3
Pahaz <pahaz.white@gmail.com>
2020-04-30 20:16:14 +0300

    core: fix node 10.x compatibility (v3)

--- core/lists.js
+++ core/lists.js
@@ -1,27 +1,30 @@
-const { ArgumentError } = require("ow");
-const ow = require('ow');
+const { ArgumentError } = require('ow')
+const ow = require('ow')
 
 const registerLists = (keystone, modulesList) => {
     modulesList.forEach(
         (module) => Object.values(module).forEach(
             (ksList) => ksList._type === 'KeystoneList' && ksList.register(keystone)))
-};
-
-class KeystoneList {
-    constructor(name, schema) {
-        ow(schema, ow.object.partialShape({fields: ow.object.valuesOfType(ow.object.hasKeys('type')), access: ow.object.nonEmpty}));
-        this.schema = schema;
-        this.name = name;
-    }
-
-    register(keystone) {
-        keystone.createList(this.name, this.schema);
-    }
 }
 
-KeystoneList._type = 'KeystoneList';
+class KeystoneList {
+
+    constructor (name, schema) {
+        ow(schema, ow.object.partialShape({
+            fields: ow.object.valuesOfType(ow.object.hasKeys('type')),
+            access: ow.object.nonEmpty,
+        }))
+        this.schema = schema
+        this.name = name
+        this._type = 'KeystoneList'
+    }
+
+    register (keystone) {
+        keystone.createList(this.name, this.schema)
+    }
+}
 
 module.exports = {
     KeystoneList,
     registerLists,
-};
+}
