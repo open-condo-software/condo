3984eb622807f5747f65e306fb64c77df40f3acf
Pahaz <pahaz.white@gmail.com>
2020-04-30 20:07:47 +0300

    core: fix node 10.x compatibility

--- core/lists.js
+++ core/lists.js
@@ -4,11 +4,11 @@ const ow = require('ow');
 const registerLists = (keystone, modulesList) => {
     modulesList.forEach(
         (module) => Object.values(module).forEach(
-            (ksList) => ksList.type === 'KeystoneList' && ksList.register(keystone)))
+            (ksList) => ksList._type === 'KeystoneList' && ksList.register(keystone)))
 };
 
 class KeystoneList {
-    type = 'KeystoneList';
+    _type = 'KeystoneList';
 
     constructor(name, schema) {
         ow(schema, ow.object.partialShape({fields: ow.object.valuesOfType(ow.object.hasKeys('type')), access: ow.object.nonEmpty}));
