be74c62a79c176ecf969c449ad4409295e18e312
Pahaz <pahaz.white@gmail.com>
2020-05-02 16:42:25 +0300

    access: style

--- core/access.js
+++ core/access.js
@@ -1,46 +1,61 @@
-const userIsAdmin = ({ authentication: { item: user } }) => Boolean(user && user.isAdmin);
+const userIsAdmin = ({ authentication: { item: user } }) => Boolean(user && user.isAdmin)
+
 const userIsThisItem = ({ existingItem, authentication: { item: user } }) => {
     if (!user || !existingItem) {
-        return false;
+        return false
     }
-    return existingItem.id === user.id;
-};
+    return existingItem.id === user.id
+}
+
+const userIsOwner = ({ existingItem, authentication: { item: user } }) => {
+    if (!user || !existingItem || !existingItem.user) {
+        return false
+    }
+    return existingItem.user.id === user.id
+}
+
+const userIsAdminOrOwner = auth => {
+    const isAdmin = userIsAdmin(auth)
+    const isOwner = userIsOwner(auth)
+    return Boolean(isAdmin || isOwner)
+}
 
 const userIsAdminOrIsThisItem = auth => {
-    const isAdmin = userIsAdmin(auth);
-    const isOwner = userIsThisItem(auth);
-    return Boolean(isAdmin || isOwner);
-};
+    const isAdmin = userIsAdmin(auth)
+    const isThisItem = userIsThisItem(auth)
+    return Boolean(isAdmin || isThisItem)
+}
 
 const canReadOnlyActive = ({ authentication: { item: user } }) => {
-    if (!user) return false;
-    if (user.isAdmin) return {};
+    if (!user) return false
+    if (user.isAdmin) return {}
 
     // Approximately; users.filter(user => user.isActive === true);
     return {
         isActive: true,
-    };
-};
+    }
+}
 
 const canReadOnlyIfInUsers = ({ authentication: { item: user } }) => {
-    if (!user) return false;
-    if (user.isAdmin) return {};
+    if (!user) return false
+    if (user.isAdmin) return {}
     return {
         users_some: { id: user.id },
     }
-};
+}
 
 const readOnlyField = {
     read: true,
     create: false,
     update: false,
-};
+}
 
 module.exports = {
     userIsAdmin,
     userIsThisItem,
     userIsAdminOrIsThisItem,
+    userIsAdminOrOwner,
     canReadOnlyActive,
     canReadOnlyIfInUsers,
     readOnlyField,
-};
+}
