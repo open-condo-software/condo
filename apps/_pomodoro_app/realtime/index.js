d12986bd22e8c3149afde198642246b83990cb9e
toplenboren <toplenboren@yandex.ru>
2020-05-24 14:45:18 +0500

    pomodoro: refactor index.js to use timer.js instead of hardcoded logic

--- apps/_pomodoro_app/realtime/index.js
+++ apps/_pomodoro_app/realtime/index.js
@@ -1,35 +1,45 @@
+/**
+ * One socket is created for one team
+ * Sockets are stored inside an array
+ * Sockets die if there are no active connections
+ * On connection we auth the socket
+ * TODO When socket dies we fire out an http request to create statistics
+ * Pipeline: get the team -> get the team timer data -> start session -> run session -> send session to statistics
+ * @param io
+ */
 function init (io) {
     let timerValue = 0;
     let timerIsAlive = false;
     let timer;
 
+    /**
+     * Active timers storage
+     * @type {{Timer}}
+     */
+    const timers = {}
+
     io.on('connection', (socket) => {
+        console.log("check auth")
+        if (!timers.hasOwnProperty(socket.id)) {
+            timers[socket.id] = Timer()
+        }
+
+        const timer = timers[id]
         console.log(socket.id, 'a user connected')
 
         socket.on('start', (msg) => {
-            if (!timerIsAlive) {
-                timer = setInterval(() => {
-                    timerValue += 1
-                    io.emit('timer', timerValue)
-                } , 1000)
-                timerIsAlive = true;
-            }
+            timer.start()
             console.log('started timer', socket.id)
         })
 
         socket.on('pause', () => {
-            if (timerIsAlive) {
-                clearInterval(timer)
-                timer = 0
-                timerIsAlive = false;
-            }
+            timer.pause()
             console.log(socket.id, 'paused timer')
         })
 
         socket.on('clear', () => {
-            io.emit('pause')
-            timerValue = 0;
-            io.emit('timer', timerValue)
+            timer.pause()
+            timer.clear()
             console.log('timer was cleared')
         })
     })
