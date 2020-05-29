6ac87084139cdda4c812851e7ce1e87f6b700b0d
Pahaz <pahaz.white@gmail.com>
2020-05-30 00:26:59 +0500

    _back02keystone: test migrations

---
+++ apps/_back02keystone/schema/Test.js
@@ -0,0 +1,79 @@
+const {
+    Checkbox,
+    Color,
+    Decimal,
+    Float,
+    Integer,
+    Location,
+    Select,
+    Slug,
+    Url,
+    Uuid,
+    Relationship,
+} = require('@keystonejs/fields')
+const { Content } = require('@keystonejs/field-content');
+const {GQLListSchema} = require('@core/keystone/schema')
+
+const Test = new GQLListSchema('Test', {
+    fields: {
+        heroColor: {type: Color},
+        isEnabled: {type: Checkbox, isRequired: true},
+
+        body: {
+            type: Content,
+            blocks: [
+                Content.blocks.blockquote,
+                Content.blocks.image,
+                Content.blocks.link,
+                Content.blocks.orderedList,
+                Content.blocks.unorderedList,
+                Content.blocks.heading,
+            ],
+        },
+
+        rating1: {type: Integer},
+        rating2: {type: Float},
+        rating3: {type: Decimal},
+        venue: {
+            type: Location,
+            googleMapsKey: 'no',
+        },
+
+        status: {type: Select, options: 'pending, processed', isRequired: true},
+        uuid: {
+            type: Uuid,
+            isRequired: true,
+        },
+        url: {
+            type: Url,
+            isRequired: true,
+
+        },
+        slug: {
+            type: Slug,
+            isRequired: true,
+
+        },
+        users: {
+            type: Relationship,
+            ref: 'User',
+            many: true,
+        },
+        self: {
+            type: Relationship,
+            ref: 'Test',
+            many: false,
+        },
+    },
+    access: {
+        read: true,
+        create: true,
+        update: true,
+        delete: true,
+        auth: true,
+    },
+})
+
+module.exports = {
+    Test,
+}
