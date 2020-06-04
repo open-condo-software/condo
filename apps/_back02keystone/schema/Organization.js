16dccfe15206e097117bb48a7074cc39a26b1073
Pahaz <pahaz.white@gmail.com>
2020-06-04 21:18:32 +0500

    _back02keystone: more real example

--- apps/_back02keystone/schema/Organization.js
+++ apps/_back02keystone/schema/Organization.js
@@ -1,4 +1,21 @@
-const { Organization, OrganizationToUserLink } = require('@core/keystone/schemas/Organization')
+const { Wysiwyg } = require('@keystonejs/fields-wysiwyg-tinymce')
+const { LocalFileAdapter } = require('@keystonejs/file-adapters')
+const { Text, Checkbox, Password, CalendarDay, File, Relationship, DateTime } = require('@keystonejs/fields')
+const { Organization: BaseOrganization, OrganizationToUserLink } = require('@core/keystone/schemas/Organization')
+const conf = require('@core/config')
+
+const AVATAR_FILE_ADAPTER = new LocalFileAdapter({
+    src: `${conf.MEDIA_ROOT}/orgavatars`,
+    path: `${conf.MEDIA_URL}/orgavatars`,
+})
+
+const Organization = BaseOrganization._override({
+    fields: {
+        // settings: { type: MultiCheck, options: ['Feature1', 'Feature2'] },
+        avatar: { type: File, adapter: AVATAR_FILE_ADAPTER },
+        description: { type: Wysiwyg },
+    },
+})
 
 module.exports = {
     Organization,
--- apps/_back02keystone/schema/User.js
+++ apps/_back02keystone/schema/User.js
@@ -1,10 +1,19 @@
 const { Wysiwyg } = require('@keystonejs/fields-wysiwyg-tinymce')
+const { LocalFileAdapter } = require('@keystonejs/file-adapters')
+const { Text, Checkbox, Password, CalendarDay, File, Relationship, DateTime } = require('@keystonejs/fields')
 const { User: BaseUser, ForgotPasswordAction, ForgotPasswordService, RegisterService } = require('@core/keystone/schemas/User')
+const conf = require('@core/config')
 
 const { Stars, MultiCheck } = require('../custom-fields')
 
+const AVATAR_FILE_ADAPTER = new LocalFileAdapter({
+    src: `${conf.MEDIA_ROOT}/avatars`,
+    path: `${conf.MEDIA_URL}/avatars`,
+})
+
 const User = BaseUser._override({
     fields: {
+        avatar: { type: File, adapter: AVATAR_FILE_ADAPTER },
         rating: { type: Stars, starCount: 5 },
         settings: { type: MultiCheck, options: ['Feature1', 'Feature2'] },
         aboutMyself: { type: Wysiwyg },
@@ -12,11 +21,11 @@ const User = BaseUser._override({
 })
 
 ForgotPasswordService.on('afterStartPasswordRecovery', (ctx) => {
-    console.log('Fake send mail!', JSON.stringify(ctx))
+    console.log('Fake send email!', JSON.stringify(ctx))
 })
 
 ForgotPasswordService.on('afterChangePasswordWithToken', (ctx) => {
-    console.log('Fake send mail!', JSON.stringify(ctx))
+    console.log('Fake send email!', JSON.stringify(ctx))
 })
 
 module.exports = {
