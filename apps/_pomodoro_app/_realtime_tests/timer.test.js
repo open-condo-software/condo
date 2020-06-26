7535ee335cf6d0e4745d8e70f9e2a9533671720d
toplenboren <toplenboren@yandex.ru>
2020-06-26 23:38:16 +0500

    pomodoro: add intervals processing to the timer

--- apps/_pomodoro_app/_realtime_tests/timer.test.js
+++ apps/_pomodoro_app/_realtime_tests/timer.test.js
@@ -6,36 +6,36 @@ function sleep(ms) {
 }
 
 test('Timer start is working', () => {
-    const x = new Timer()
+    const x = new Timer(10,10,10)
     x.start()
     sleep(1000).then(() => {
-        expect(x.getTime()).toBe(1);
+        expect(x.getTime()).toBe(9);
     })
 });
 
 test('Timer pause is working', () => {
-    const x = new Timer()
+    const x = new Timer(10,10,10)
     x.start()
     sleep(1000).then(() => {
         x.pause()
         sleep(1000).then(() => {
-            expect(x.getTime()).toBe(1);
+            expect(x.getTime()).toBe(9);
         })
     })
 });
 
 test('Timer reset is working', () => {
-    const x = new Timer()
+    const x = new Timer(10,10,10)
     x.start()
     sleep(1000).then(() => {
-        expect(x.getTime()).toBe(1)
+        expect(x.getTime()).toBe(9)
         x.reset()
-        expect(x.getTime()).toBe(0)
+        expect(x.getTime()).toBe(10)
     })
 });
 
 test('Timers are async', async () => {
-    const x = new Timer()
+    const x = new Timer(10,10,10)
     const y = x
     const z = x
     // first client pushes start:
@@ -49,5 +49,28 @@ test('Timers are async', async () => {
     // somebody pauses the timers
     y.pause()
     await sleep(1000)
+    expect(x.getTime()).toBe(8)
+});
+
+// we should wait more then 10 secs in order to pass the last test
+jest.setTimeout(15000)
+
+test('Timers give correct period', async () => {
+    const x = new Timer(2,2,2)
     expect(x.getTime()).toBe(2)
+    expect(x.getInterval()).toBe('WORK')
+    expect(x.getNextInterval()).toBe('BREAK')
+    x.start()
+    await sleep(2500)
+    expect(x.getTime()).toBe(2)
+    expect(x.getInterval()).toBe('BREAK')
+    expect(x.getNextInterval()).toBe('WORK')
+    await sleep(2000) // work 2
+    await sleep(2000) // break 2
+    await sleep(2000) // work 3
+    await sleep(2000) // break 3
+    await sleep(2500) // work 4
+    console.log(x.getTime())
+    expect(x.getInterval()).toBe('BIG_BREAK')
+    expect(x.getNextInterval()).toBe('WORK')
 });
--- apps/_pomodoro_app/realtime/application/Timer.js
+++ apps/_pomodoro_app/realtime/application/Timer.js
@@ -1,10 +1,33 @@
-const intervals = {
-    'every_second': 1000
-}
+const CyclicQueue = require('./CyclicQueue');
+const Interval = require('./Interval');
 
 const status = {
     'paused':'paused',
     'working':'working',
+}
+
+const intervals = {
+    'work': 'WORK',
+    'break': 'BREAK',
+    'bigBreak':'BIG_BREAK'
+}
+
+function getIntervalQueue(breakTime, bigBreakTime, worktimeTime) {
+
+    const workInterval = new Interval(intervals.work, worktimeTime)
+    const breakInterval = new Interval(intervals.break, breakTime)
+    const bigBreakInterval = new Interval(intervals.bigBreak, bigBreakTime)
+
+    return new CyclicQueue([
+        workInterval,
+        breakInterval,
+        workInterval,
+        breakInterval,
+        workInterval,
+        breakInterval,
+        workInterval,
+        bigBreakInterval
+    ])
 }
 
 /**
@@ -12,27 +35,49 @@ const status = {
  */
 class Timer {
 
-    constructor() {
+    constructor(breakTime, bigBreakTime, worktimeTime) {
         this.timer = 0
-        this.counter = 0
+        this.counterFunction = 0
         this.status = status.paused
+
+        this.intervalQueue = getIntervalQueue(breakTime, bigBreakTime, worktimeTime)
     }
 
-    getTime() {
-        return this.timer
+    _incrementTimer() {
+        this.timer++
+        if(this.getTime() === 0) {
+            this._changeInterval()
+        }
+    }
+
+    _changeInterval() {
+        this.timer = 0
+        return this.intervalQueue.pop()
+    }
+
+    getInterval() {
+        return this.intervalQueue.current().name
+    }
+
+    getNextInterval() {
+        return this.intervalQueue.peekNext().name
+    }
+
+    getNextIntervalLength() {
+        return this.intervalQueue.peekNext().time
     }
 
     start() {
         if (this.status === status.paused) {
-            this.counter = setInterval(() => this.timer++, intervals.every_second)
+            this.counterFunction = setInterval(() => { this._incrementTimer() }, 1000)
             this.status = status.working
         }
     }
 
     pause() {
         if (this.status === status.working) {
-            clearInterval(this.counter)
-            this.counter = 0
+            clearInterval(this.counterFunction)
+            this.counterFunction = 0
             this.status = status.paused
         }
     }
@@ -43,6 +88,10 @@ class Timer {
 
     isPaused() {
         return this.status === status.paused
+    }
+
+    getTime() {
+        return this.intervalQueue.current().time - this.timer
     }
 }
 
