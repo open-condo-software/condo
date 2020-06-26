666457f6c30583478913dc310e91b9467a52845f
toplenboren <toplenboren@yandex.ru>
2020-06-26 20:24:50 +0500

    pomodoro: add cyclic query data structure and test it out

---
+++ apps/_pomodoro_app/_realtime_tests/cyclicQueue.test.js
@@ -0,0 +1,24 @@
+const CyclicQueue = require('../realtime/application/CyclicQueue');
+
+test('Cyclic queue works as a queue', () => {
+    const x = new CyclicQueue([1,2,3])
+    expect(x.current()).toBe(1)
+    expect(x.pop()).toBe(1)
+    expect(x.current()).toBe(2)
+});
+
+test('Cyclic queue cycling is working', () => {
+    const x = new CyclicQueue([1,2,3])
+    x.pop()
+    x.pop()
+    x.pop()
+    expect(x.current()).toBe(1)
+});
+
+test('Cyclic queue cycling with peeking is working', () => {
+    const x = new CyclicQueue([1,2,3])
+    x.pop()
+    x.pop()
+    expect(x.current()).toBe(3)
+    expect(x.peekNext()).toBe(1)
+});
