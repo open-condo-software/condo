d6160ffda681a86346bd1bf1ac43d4185183eb15
Pahaz <pahaz.white@gmail.com>
2020-08-20 16:40:21 +0500

    _compability_app: keystonejs 13.0.0 changes

---
+++ apps/_compability_app/initial-data.js
@@ -0,0 +1,29 @@
+module.exports = [
+    {
+        listKey: 'User',
+        items: [
+            {
+                data: {
+                    name: 'Admin',
+                    email: 'admin@example.com',
+                    isEmailVerified: false,
+                    isAdmin: true,
+                    isActive: true,
+                    dob: '1990-01-02',
+                    password: '3a74b3f07978',
+                },
+            },
+            {
+                data: {
+                    name: 'JustUser',
+                    email: 'user@example.com',
+                    isEmailVerified: false,
+                    isAdmin: false,
+                    isActive: true,
+                    dob: '1995-06-09',
+                    password: '1a92b3a07c78',
+                },
+            },
+        ],
+    },
+]
