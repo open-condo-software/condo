aa2d0f4fe0f29015ec06f675932f3f180631902e
Pahaz <pahaz.white@gmail.com>
2020-08-20 16:40:50 +0500

    _pomodoro_app: keystonejs 13.0.0 changes

--- apps/_pomodoro_app/initial-data.js
+++ apps/_pomodoro_app/initial-data.js
@@ -1,20 +1,29 @@
-module.exports = {
-    User: [
-        {
-            name: 'Admin',
-            email: 'admin@example.com',
-            isAdmin: true,
-            isActive: true,
-            dob: '1990-01-02',
-            password: '3a74b3f07978',
-        },
-        {
-            name: 'JustUser',
-            email: 'user@example.com',
-            isAdmin: false,
-            isActive: true,
-            dob: '1995-06-09',
-            password: '1a92b3a07c78',
-        },
-    ],
-};
+module.exports = [
+    {
+        listKey: 'User',
+        items: [
+            {
+                data: {
+                    name: 'Admin',
+                    email: 'admin@example.com',
+                    isEmailVerified: false,
+                    isAdmin: true,
+                    isActive: true,
+                    dob: '1990-01-02',
+                    password: '3a74b3f07978',
+                },
+            },
+            {
+                data: {
+                    name: 'JustUser',
+                    email: 'user@example.com',
+                    isEmailVerified: false,
+                    isAdmin: false,
+                    isActive: true,
+                    dob: '1995-06-09',
+                    password: '1a92b3a07c78',
+                },
+            },
+        ],
+    },
+]
