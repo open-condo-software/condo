0bd93765c08f2b8d212b6216d084726d401091ac
Дмитрий Данев <Danewdima@gmail.com>
2020-09-06 20:51:17 +0300

    code_workaround: redirect unauth users to auth form

--- apps/_compability_app/common/containers/AuthLayout.jsx
+++ apps/_compability_app/common/containers/AuthLayout.jsx
@@ -1,10 +1,18 @@
-import { useAuth } from '@core/next/auth'
+import {useEffect} from "react";
+import {useAuth} from '@core/next/auth'
+import Router from "next/router";
 
 import Translate from '../components/Translate'
 import PageLoader from '../components/PageLoader'
 
-export function AuthLayout({ children }) {
+export function AuthLayout({ children, redirect }) {
     const {isAuthenticated, isLoading} = useAuth();
+
+    useEffect(() => {
+        if (!isAuthenticated && redirect) {
+            Router.push("/auth/signin")
+        }
+    }, [isAuthenticated]);
 
     if (isLoading) {
         return (<PageLoader/>)
--- apps/_compability_app/pages/index.jsx
+++ apps/_compability_app/pages/index.jsx
@@ -3,16 +3,18 @@ import { Typography } from 'antd'
 import CustomLink from '../common/components/CustomLink'
 import Translate from '../common/components/Translate'
 
+import { AuthLayout } from "../common/containers";
+
 function HomePage () {
     return (
-        <div>
+        <AuthLayout redirect>
             <Typography.Title>
                 <Translate id={"pages.index.title"}/>
             </Typography.Title>
             <CustomLink path="/tests/disc">
                 <Translate id={"pages.index.apply_test"}/>
             </CustomLink>
-        </div>
+        </AuthLayout>
     )
 }
 
