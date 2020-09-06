fbc886f7d1804339e5329fefc85f8d0aa100ff4c
Дмитрий Данев <Danewdima@gmail.com>
2020-09-06 20:29:21 +0300

    code_workaround: fix MenuHeader.jsx dropdown

--- apps/_compability_app/common/components/CustomAvatar.jsx
+++ apps/_compability_app/common/components/CustomAvatar.jsx
@@ -1,29 +1,35 @@
 /** @jsx jsx */
-import { jsx } from '@emotion/core'
-import {useIntl} from "react-intl";
-import { Avatar } from 'antd'
+import { jsx } from "@emotion/core";
+import styled from "@emotion/styled";
+import { Avatar } from "antd"
+import React from "react";
+import { useIntl } from "react-intl";
 
-export const CustomAvatar = ({auth, styles}) => {
+const UserName = styled.span`
+    padding-right: 12px;
+`;
+
+export const CustomAvatar = ({auth}) => {
     const intl = useIntl();
     const avatarUrl = (auth.user && auth.user.avatar && auth.user.avatar.publicUrl)
         ? auth.user.avatar.publicUrl
         : 'https://gw.alipayobjects.com/zos/antfincdn/XAosXuNZyF/BiazfanxmamNRoxxVxka.png';
 
     return (
-        <div css={styles}>
+        <div>
+            <UserName>
+                {
+                    auth.user
+                        ? auth.user.name
+                        : intl.formatMessage({id: 'baselayout.menuheader.GuestUsername'})
+                }
+            </UserName>
             <Avatar
                 size="small"
                 src={avatarUrl}
                 alt={intl.formatMessage({id: 'Avatar'})}
                 className="avatar"
             />
-            <span className="name">
-                {
-                    auth.user
-                        ? auth.user.name
-                        : intl.formatMessage({id: 'baselayout.menuheader.GuestUsername'})
-                }
-            </span>
         </div>
     )
 }
