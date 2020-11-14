964b42ab9b1421745cbbc873de3084d3e3db76b1
Pahaz <pahaz.white@gmail.com>
2020-11-14 16:00:02 +0300

    tests: add genTestGQLUtils function

--- apps/_back02keystone/_api_tests/_example.test.js
+++ apps/_back02keystone/_api_tests/_example.test.js
@@ -45,69 +45,64 @@ function genDeleteGQL (MODEL, MODELs, MODEL_FIELDS) {
     `
 }
 
+function genTestGQLUtils (MODEL, MODELs, MODEL_FIELDS) {
+    const GET_ALL_OBJS_QUERY = genGetAllGQL(MODEL, MODELs, MODEL_FIELDS)
+    const CREATE_OBJ_MUTATION = genCreateGQL(MODEL, MODELs, MODEL_FIELDS)
+    const UPDATE_OBJ_MUTATION = genUpdateGQL(MODEL, MODELs, MODEL_FIELDS)
+    const DELETE_OBJ_MUTATION = genDeleteGQL(MODEL, MODELs, MODEL_FIELDS)
+
+    async function getAll (client, where, { raw = false } = {}) {
+        const { data, errors } = await client.query(GET_ALL_OBJS_QUERY, { where: where })
+        if (raw) return { data, errors }
+        expect(errors).toEqual(undefined)
+        return data.objs
+    }
+
+    async function create (client, attrs = {}, { raw = false } = {}) {
+        const { data, errors } = await client.mutate(CREATE_OBJ_MUTATION, {
+            data: { ...attrs },
+        })
+        if (raw) return { data, errors }
+        expect(errors).toEqual(undefined)
+        return data.obj
+    }
+
+    async function update (client, id, attrs = {}, { raw = false } = {}) {
+        const { data, errors } = await client.mutate(UPDATE_OBJ_MUTATION, {
+            id, data: { ...attrs },
+        })
+        if (raw) return { data, errors }
+        expect(errors).toEqual(undefined)
+        return data.obj
+    }
+
+    async function delete_ (client, id, { raw = false } = {}) {
+        const { data, errors } = await client.mutate(DELETE_OBJ_MUTATION, { id })
+        if (raw) return { data, errors }
+        expect(errors).toEqual(undefined)
+        return data.obj
+    }
+
+    return {
+        MODEL_FIELDS,
+        GET_ALL_OBJS_QUERY,
+        CREATE_OBJ_MUTATION,
+        UPDATE_OBJ_MUTATION,
+        DELETE_OBJ_MUTATION,
+        getAll, create, update,
+        delete: delete_,
+    }
+}
+
 const FIELDS = '{ id v text meta createdAt updatedAt createdBy { id } updatedBy { id } }'
 const ITEM_FIELDS = `{ id v meta test ${FIELDS} }`
+const Test = genTestGQLUtils('Test', 'Tests', FIELDS)
+const TestItem = genTestGQLUtils('TestItem', 'TestItems', ITEM_FIELDS)
+
 const HISTORY_FIELDS = '{ id v text history_id history_action history_date }'
 const ITEM_HISTORY_FIELDS = '{ id v meta test history_id history_action history_date }'
-const GET_ALL_TEST_OBJS_QUERY = genGetAllGQL('Test', 'Tests', FIELDS)
 const GET_ALL_TEST_HISTORY_OBJS_QUERY = genGetAllGQL('TestHistoryRecord', 'TestHistoryRecords', HISTORY_FIELDS)
 const GET_ALL_TEST_ITEM_HISTORY_OBJS_QUERY = genGetAllGQL('TestItemHistoryRecord', 'TestItemHistoryRecords', ITEM_HISTORY_FIELDS)
-const CREATE_TEST_ITEM_OBJ_MUTATION = genCreateGQL('TestItem', 'TestItems', ITEM_FIELDS)
-const UPDATE_TEST_ITEM_OBJ_MUTATION = genUpdateGQL('TestItem', 'TestItems', ITEM_FIELDS)
-const DELETE_TEST_ITEM_OBJ_MUTATION = genDeleteGQL('TestItem', 'TestItems', ITEM_FIELDS)
-const CREATE_TEST_OBJ_MUTATION = genCreateGQL('Test', 'Tests', FIELDS)
-const UPDATE_TEST_OBJ_MUTATION = genUpdateGQL('Test', 'Tests', FIELDS)
-const DELETE_TEST_OBJ_MUTATION = genDeleteGQL('Test', 'Tests', FIELDS)
-
-async function createTestObj (client, attrs = {}) {
-    const { data, errors } = await client.mutate(CREATE_TEST_OBJ_MUTATION, {
-        data: { ...attrs },
-    })
-    expect(errors).toEqual(undefined)
-    return data.obj
-}
-
-async function updateTestObj (client, id, attrs = {}) {
-    const { data, errors } = await client.mutate(UPDATE_TEST_OBJ_MUTATION, {
-        id, data: { ...attrs },
-    })
-    expect(errors).toEqual(undefined)
-    return data.obj
-}
-
-async function deleteTestObj (client, id) {
-    const { data, errors } = await client.mutate(DELETE_TEST_OBJ_MUTATION, { id })
-    expect(errors).toEqual(undefined)
-    return data.obj
-}
-
-async function createTestItemObj (client, attrs = {}) {
-    const { data, errors } = await client.mutate(CREATE_TEST_ITEM_OBJ_MUTATION, {
-        data: { ...attrs },
-    })
-    expect(errors).toEqual(undefined)
-    return data.obj
-}
-
-async function updateTestItemObj (client, id, attrs = {}) {
-    const { data, errors } = await client.mutate(UPDATE_TEST_ITEM_OBJ_MUTATION, {
-        id, data: { ...attrs },
-    })
-    expect(errors).toEqual(undefined)
-    return data.obj
-}
-
-async function deleteTestItemObj (client, id) {
-    const { data, errors } = await client.mutate(DELETE_TEST_ITEM_OBJ_MUTATION, { id })
-    expect(errors).toEqual(undefined)
-    return data.obj
-}
-
-async function getTestObjs (client, where) {
-    const { data, errors } = await client.query(GET_ALL_TEST_OBJS_QUERY, { where: where })
-    expect(errors).toEqual(undefined)
-    return data.objs
-}
 
 async function getTestHistoryObjs (client, history_id) {
     const { data, errors } = await client.query(GET_ALL_TEST_HISTORY_OBJS_QUERY, { where: { history_id } })
@@ -124,7 +119,7 @@ async function getTestItemHistoryObjs (client, history_id) {
 describe('Json field', () => {
     async function testJsonValue (value) {
         const client = await makeClient()
-        let obj = await createTestObj(client, { meta: value })
+        let obj = await Test.create(client, { meta: value })
         expect(obj.meta).toStrictEqual(value)
     }
 
@@ -192,8 +187,8 @@ describe('Json field', () => {
 describe('Json field exact match filter', () => {
     async function testFilterByValue (value, metaSuffix = '') {
         const client = await makeClient()
-        const obj = await createTestObj(client, { meta: value })
-        const objs = await getTestObjs(client, { ['meta' + metaSuffix]: value })
+        const obj = await Test.create(client, { meta: value })
+        const objs = await Test.getAll(client, { ['meta' + metaSuffix]: value })
         const objsIds = objs.map(x => x.id)
         const objsMetas = [...(new Set(objs.map(x => JSON.stringify(x.meta))))].map(JSON.parse)
         expect(objsIds).toContain(obj.id)
@@ -287,10 +282,10 @@ describe('Json field exact match filter', () => {
 describe('historical()', () => {
     test('create/update/delete history', async () => {
         const client = await makeClient()
-        let obj = await createTestObj(client, {})
-        await updateTestObj(client, obj.id, { text: 'hello' })
-        await updateTestObj(client, obj.id, { text: 'no' })
-        await deleteTestObj(client, obj.id)
+        let obj = await Test.create(client, {})
+        await Test.update(client, obj.id, { text: 'hello' })
+        await Test.update(client, obj.id, { text: 'no' })
+        await Test.delete(client, obj.id)
 
         let histObjs = await getTestHistoryObjs(client, obj.id)
         expect(histObjs).toEqual([
@@ -304,11 +299,11 @@ describe('historical()', () => {
     test('delete related object and set FK null without history update', async () => {
         if (isMongo()) return console.error('SKIP() Mongo: doesn\'t support UUID fk!')
         const client = await makeClient()
-        let obj = await createTestItemObj(client, { test: { create: { text: 'new1' } }, meta: { foo: 1 } })
-        await updateTestItemObj(client, obj.id, { meta: { foo: 2 } })
-        await updateTestObj(client, obj.test.id, { text: 'new2' })
-        await deleteTestObj(client, obj.test.id)
-        await deleteTestItemObj(client, obj.id)
+        let obj = await TestItem.create(client, { test: { create: { text: 'new1' } }, meta: { foo: 1 } })
+        await TestItem.update(client, obj.id, { meta: { foo: 2 } })
+        await Test.update(client, obj.test.id, { text: 'new2' })
+        await Test.delete(client, obj.test.id)
+        await TestItem.delete(client, obj.id)
 
         let histObjs = await getTestHistoryObjs(client, obj.test.id)
         expect(histObjs).toEqual([
@@ -348,17 +343,15 @@ describe('versioned()', () => {
     test('check v field autoincrement', async () => {
         const client = await makeClient()
 
-        let obj = await createTestObj(client, {})
+        let obj = await Test.create(client, {})
         expect(obj.v).toBe(1)
 
-        obj = await updateTestObj(client, obj.id, { text: 'hello' })
+        obj = await Test.update(client, obj.id, { text: 'hello' })
         expect(obj.v).toBe(2)
     })
     test('try to set v', async () => {
         const admin = await makeLoggedInAdminClient()
-        const { data, errors } = await admin.mutate(CREATE_TEST_OBJ_MUTATION, {
-            data: { v: 5 },
-        })
+        const { data, errors } = await Test.create(admin, { v: 5 }, { raw: true })
         expect(data).toEqual(undefined)
         expect(JSON.stringify(errors)).toMatch(/Field [\\"']+v[\\"']+ is not defined by type [\\"']+TestCreateInput[\\"']+/i)
     })
@@ -367,7 +360,7 @@ describe('versioned()', () => {
 describe('uuided()', () => {
     test('chek id field is uuid', async () => {
         const client = await makeClient()
-        let obj = await createTestItemObj(client, { test: { create: { text: 'autoGen' } } })
+        let obj = await TestItem.create(client, { test: { create: { text: 'autoGen' } } })
         expect(obj.id).toMatch(UUID_RE)
     })
 })
@@ -375,41 +368,44 @@ describe('uuided()', () => {
 describe('tracked()', () => {
     test('check createAt/updateAt for anonymous', async () => {
         const client = await makeClient()
-        let { id, createdAt, updatedAt } = await createTestObj(client, {})
+        let { id, createdAt, updatedAt } = await Test.create(client, {})
         expect(createdAt).toMatch(DATETIME_RE)
         expect(updatedAt).toMatch(DATETIME_RE)
         expect(updatedAt).toEqual(createdAt)
-        let obj = await updateTestObj(client, id, { text: 'new' })
+        let obj = await Test.update(client, id, { text: 'new' })
         expect(obj.createdAt).toEqual(createdAt)
         expect(obj.updatedAt).toMatch(DATETIME_RE)
         expect(obj.updatedAt).not.toEqual(createdAt)
     })
     test('check createBy/updateBy for anonymous', async () => {
         const client = await makeClient()
-        let { id, createdBy, updatedBy } = await createTestObj(client, {})
+        let { id, createdBy, updatedBy } = await Test.create(client, {})
         expect(createdBy).toBe(null)
         expect(updatedBy).toBe(null)
-        let obj = await updateTestObj(client, id, { text: 'new' })
+        let obj = await Test.update(client, id, { text: 'new' })
         expect(obj.createdBy).toBe(null)
         expect(obj.updatedBy).toBe(null)
     })
     test('check createBy/updateBy for user1/user2', async () => {
         const client = await makeLoggedInClient()
         const admin = await makeLoggedInAdminClient()
-        let { id, createdBy, updatedBy } = await createTestObj(client, {})
+        let { id, createdBy, updatedBy } = await Test.create(client, {})
         expect(createdBy).toEqual({ id: client.user.id })
         expect(updatedBy).toEqual({ id: client.user.id })
-        let obj = await updateTestObj(admin, id, { text: 'new' })
+        let obj = await Test.update(admin, id, { text: 'new' })
         expect(obj.createdBy).toEqual({ id: client.user.id })
         expect(obj.updatedBy).toEqual({ id: admin.user.id })
     })
     test('try to set createBy', async () => {
         const client = await makeLoggedInClient()
         const admin = await makeLoggedInAdminClient()
-        const { data, errors } = await client.mutate(CREATE_TEST_OBJ_MUTATION, {
-            data: { createdBy: { connect: { id: admin.user.id } } },
-        })
+        const { data, errors } = await Test.create(client, { createdBy: { connect: { id: admin.user.id } } }, { raw: true })
         expect(data).toEqual(undefined)
         expect(JSON.stringify(errors)).toMatch(/Field [\\"']+createdBy[\\"']+ is not defined by type [\\"']+TestCreateInput[\\"']+/i)
     })
 })
+
+// describe('softDeleted()', () => {
+//     test('check createAt/updateAt for anonymous', async () => {
+//     })
+// })
