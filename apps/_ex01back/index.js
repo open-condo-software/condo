8cbd3b9ed8246de5681d8351a93fa6a297b52c02
Pahaz <pahaz.white@gmail.com>
2021-02-03 21:17:24 +0300

    _ex01back: test it! and simplify

--- apps/_ex01back/index.js
+++ apps/_ex01back/index.js
@@ -1,25 +1,15 @@
 const { Keystone } = require('@keystonejs/keystone')
 const { GraphQLApp } = require('@keystonejs/app-graphql')
 const { AdminUIApp } = require('@keystonejs/app-admin-ui')
-const { StaticApp } = require('@keystonejs/app-static')
 const { PasswordAuthStrategy } = require('@keystonejs/auth-password')
-const { getAdapter } = require('@core/keystone/adapter.utils')
-const { getCookieSecret } = require('@core/keystone/keystone.utils')
-const { registerSchemas } = require('@core/keystone/schema')
-const conf = require('@core/config')
 const { createItems } = require('@keystonejs/server-side-graphql-client')
 
+const conf = require('@core/config')
+const { registerSchemas } = require('@core/keystone/schema')
+const { prepareDefaultKeystoneConfig } = require('@core/keystone/setup.utils')
+
 const keystone = new Keystone({
-    cookieSecret: getCookieSecret(conf.COOKIE_SECRET),
-    cookie: {
-        sameSite: false,
-        secure: false,
-        maxAge: 1000 * 60 * 60 * 24 * 130, // 130 days
-    },
-    name: conf.PROJECT_NAME,
-    adapter: getAdapter(conf.DATABASE_URL),
-    defaultAccess: { list: false, field: true, custom: false },
-    queryLimits: { maxTotalResults: 1000 },
+    ...prepareDefaultKeystoneConfig(conf),
     onConnect: async () => {
         if (conf.NODE_ENV !== 'development') return // Just for dev purposes!
 
@@ -27,7 +17,7 @@ const keystone = new Keystone({
         try {
             const users = await keystone.lists.User.adapter.findAll()
             if (!users.length) {
-                const initialData = require('./initial-data')
+                const initialData = require('./initialData')
                 for (let { listKey, items } of initialData) {
                     await createItems({
                         keystone,
@@ -55,6 +45,9 @@ module.exports = {
     keystone,
     apps: [
         new GraphQLApp(),
-        new StaticApp({ path: conf.MEDIA_URL, src: conf.MEDIA_ROOT }),
-        new AdminUIApp({ authStrategy, enableDefaultRoute: true })],
+        new AdminUIApp({
+            authStrategy,
+            enableDefaultRoute: true
+        }),
+    ],
 }
