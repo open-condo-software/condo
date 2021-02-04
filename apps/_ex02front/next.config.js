4cee5dca6de7b729acb81c6579fceddc930ea734
Pahaz <pahaz.white@gmail.com>
2021-02-04 10:58:34 +0300

    _ex02front: remove firebase

--- apps/_ex02front/next.config.js
+++ apps/_ex02front/next.config.js
@@ -7,14 +7,12 @@ const withTM = require('next-transpile-modules')(['@core/next'])
 
 const serverUrl = process.env.SERVER_URL || 'http://localhost:3000'
 const apolloGraphQLUrl = `${serverUrl}/admin/api`
-const firebaseConfig = conf['FIREBASE_CONFIG'] && JSON.parse(conf['FIREBASE_CONFIG'])
 
 module.exports = withTM(withLess(withCSS({
     publicRuntimeConfig: {
         // Will be available on both server and client
         serverUrl,
         apolloGraphQLUrl,
-        firebaseConfig,
     },
     lessLoaderOptions: {
         javascriptEnabled: true,
