7168aa380ee0aa59b8b10ef4b02cba6f55ac269c
Pahaz <pahaz.white@gmail.com>
2021-02-04 10:57:30 +0300

    demo: index with examples

--- apps/__demo/index.js
+++ apps/__demo/index.js
@@ -1,28 +1,21 @@
+const express = require('express')
 const { Keystone } = require('@keystonejs/keystone')
 const { PasswordAuthStrategy } = require('@keystonejs/auth-password')
 const { GraphQLApp } = require('@keystonejs/app-graphql')
 const { AdminUIApp } = require('@keystonejs/app-admin-ui')
-const { NextApp } = require('@keystonejs/app-next')
 const { StaticApp } = require('@keystonejs/app-static')
-const access = require('@core/keystone/access')
-const { getAdapter } = require('@core/keystone/adapter.utils')
-const { getCookieSecret } = require('@core/keystone/keystone.utils')
-const { registerSchemas } = require('@core/keystone/schema')
-const conf = require('@core/config')
-const { areWeRunningTests, EmptyApp } = require('@core/keystone/test.utils')
+const { NextApp } = require('@keystonejs/app-next')
 const { createItems } = require('@keystonejs/server-side-graphql-client')
 
+const conf = require('@core/config')
+const access = require('@core/keystone/access')
+const { areWeRunningTests } = require('@core/keystone/test.utils')
+const { EmptyApp } = require('@core/keystone/test.utils')
+const { prepareDefaultKeystoneConfig } = require('@core/keystone/setup.utils')
+const { registerSchemas } = require('@core/keystone/schema')
+
 const keystone = new Keystone({
-    name: conf.PROJECT_NAME,
-    cookieSecret: getCookieSecret(conf.COOKIE_SECRET),
-    cookie: {
-        sameSite: false,
-        secure: false,
-        maxAge: 1000 * 60 * 60 * 24 * 130, // 130 days
-    },
-    adapter: getAdapter(conf.DATABASE_URL),
-    defaultAccess: { list: false, field: true, custom: false },
-    queryLimits: { maxTotalResults: 1000 },
+    ...prepareDefaultKeystoneConfig(conf),
     onConnect: async () => {
         // Initialise some data
         if (conf.NODE_ENV !== 'development') return // Just for dev env purposes!
@@ -30,8 +23,9 @@ const keystone = new Keystone({
         try {
             const users = await keystone.lists.User.adapter.findAll()
             if (!users.length) {
-                const initialData = require('./initial-data')
+                const initialData = require('./initialData')
                 for (let { listKey, items } of initialData) {
+                    console.log(`🗿 createItems(${listKey}) -> ${items.length}`)
                     await createItems({
                         keystone,
                         listKey,
@@ -48,7 +42,55 @@ const keystone = new Keystone({
 registerSchemas(keystone, [
     require('./schema/User'),
     require('./schema/Organization'),
+    require('./schema/Auth'),
+    require('./schema/Test'),
 ])
+
+class CustomAppExample {
+    prepareMiddleware ({ keystone, dev, distDir }) {
+        const app = express()
+        app.use(function (req, res, next) {
+            req._CustomApp_requestLevelData = { foo: 22 }
+            // console.log('Time:', Date.now())
+            next()
+        })
+        return app
+    }
+}
+
+class CustomGQLPluginExample {
+    serverWillStart () {
+        console.log('Server starting!')
+    }
+
+    requestDidStart (requestContext) {
+        return undefined
+
+        console.log('GQL!o :', requestContext.operation)
+        console.log('GQL!op:', requestContext.request.operationName)
+        console.log('GQL!q :', requestContext.request.query.replace(/\n/g, '').replace(/\s+/g, ' '))
+        console.log('GQL!v :', requestContext.request.variables)
+        console.log('GQL!u :', requestContext.context.authedItem && requestContext.context.authedItem.id)
+        console.log('GQL!l :', requestContext.context.authedListKey)
+        // console.log('req!req:', requestContext.context.req)
+        const { req } = requestContext.context
+        // console.log('REQ.body:', req.body)
+        // console.log('REQ.query:', req.query)
+        // console.log('REQ.cookies:', req.cookies)
+        // console.log('REQ.hostname:', req.hostname)
+        console.log('REQ.headers:', req.headers)
+        // console.log('REQ.rawHeaders:', req.rawHeaders)
+        console.log('REQ.ip:', req.ip)
+        // console.log('REQ.ips:', req.ips)
+        console.log('REQ.protocol:', req.protocol)
+        console.log('REQ.method:', req.method)
+        console.log('REQ.sessionID:', req.sessionID)
+        console.log('REQ.id:', req.id)
+        // console.log('REQ.originalUrl:', req.originalUrl)
+        // console.log('REQ.url:', req.url)
+        // console.log('Request starting!', requestContext)
+    }
+}
 
 const authStrategy = keystone.createAuthStrategy({
     type: PasswordAuthStrategy,
@@ -56,12 +98,21 @@ const authStrategy = keystone.createAuthStrategy({
 })
 
 module.exports = {
+    configureExpress: (app) => {
+    },
     keystone,
     apps: [
-        new GraphQLApp({ apollo: { debug: conf.NODE_ENV === 'development' } }),
+        new CustomAppExample(),
+        new GraphQLApp({
+            apollo: {
+                debug: conf.NODE_ENV === 'development' || conf.NODE_ENV === 'test',
+                plugins: [new CustomGQLPluginExample()],
+            },
+        }),
         new StaticApp({ path: conf.MEDIA_URL, src: conf.MEDIA_ROOT }),
         new AdminUIApp({
             adminPath: '/admin',
+            // enableDefaultRoute: true,
             isAccessAllowed: access.userIsAdmin,
             authStrategy,
         }),
