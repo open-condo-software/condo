8c54af4f5c979e2708451d8a05abf9f583fb24cb
Pahaz <pahaz.white@gmail.com>
2021-02-12 11:46:45 +0300

    Ticket: add default classificator status source

--- apps/condo/schema/_common.js
+++ apps/condo/schema/_common.js
@@ -60,8 +60,23 @@ const ORGANIZATION_OWNED_FIELD = {
     },
 }
 
+const COMMON_AND_ORGANIZATION_OWNED_FIELD = {
+    schemaDoc: 'Ref to the organization. If this ref is null the object is common for all organizations',
+    type: Relationship,
+    ref: 'Organization',
+    isRequired: true,
+    kmigratorOptions: { null: true, on_delete: 'models.CASCADE' },
+    // TODO(pahaz): check access to organization (can't create without organization)!
+    access: {
+        read: true,
+        create: true,
+        update: true,
+    },
+}
+
 module.exports = {
     DV_FIELD,
     SENDER_FIELD,
     ORGANIZATION_OWNED_FIELD,
+    COMMON_AND_ORGANIZATION_OWNED_FIELD,
 }
