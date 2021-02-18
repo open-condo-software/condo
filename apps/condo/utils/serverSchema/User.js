0695a59b47f4600bf7bd8d95d5133f255d9552c2
Pahaz <pahaz.white@gmail.com>
2021-02-19 00:02:47 +0300

    SBERDOMA-16 refactor Organization.js logic

---
+++ apps/condo/utils/serverSchema/User.js
@@ -0,0 +1,19 @@
+const { execGqlWithoutAccess } = require('./utils')
+const { User } = require('../../schema/User.gql')
+
+async function findUser (context, query) {
+    if (!context) throw new Error('no context')
+    if (!query) throw new Error('no query')
+    return await execGqlWithoutAccess(context, {
+        query: User.GET_ALL_OBJS_QUERY,
+        variables: {
+            where: query,
+        },
+        errorMessage: '[error] Unable to query users',
+        dataPath: 'objs',
+    })
+}
+
+module.exports = {
+    findUser,
+}
