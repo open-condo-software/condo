ddf74de0459bb5f729f607d0f6b31d1ed8be3e55
Pahaz <pahaz.white@gmail.com>
2021-02-19 00:04:33 +0300

    SBERDOMA-16 fix Property.test.js

--- apps/condo/schema/Property.test.js
+++ apps/condo/schema/Property.test.js
@@ -6,16 +6,17 @@
 const { setFakeClientMode } = require('@core/keystone/test.utils')
 const conf = require('@core/config')
 if (conf.TESTS_FAKE_CLIENT_MODE) setFakeClientMode(require.resolve('../index'))
+
 const faker = require('faker')
-const { makeLoggedInClient } = require('@core/keystone/test.utils')
 const { UUID_RE, DATETIME_RE } = require('@core/keystone/test.utils')
 
+const { makeClientWithRegisteredOrganization } = require('./Organization.test')
 const { buildingMapJson } = require('../constants/property.example')
 const { Property } = require('./Property.gql')
 
-Property.DEFAULT_ORGANIZATION_ID = '640eee89-a45d-4d3f-8b27-e63156c0c156'
-
-async function createProperty (client, extraAttrs = {}) {
+async function createProperty (client, organization, extraAttrs = {}) {
+    if (!client) throw new Error('no client')
+    if (!organization) throw new Error('no organization')
     const sender = { dv: 1, fingerprint: faker.random.alphaNumeric(8) }
     const name = faker.address.streetAddress(true)
     const address = faker.address.streetAddress(true)
@@ -28,7 +29,7 @@ async function createProperty (client, extraAttrs = {}) {
     const attrs = {
         dv: 1,
         sender,
-        organization: { connect: { id: Property.DEFAULT_ORGANIZATION_ID } },
+        organization: { connect: { id: organization.id } },
         type: 'building',
         name, address, addressMeta,
         ...extraAttrs,
@@ -37,45 +38,59 @@ async function createProperty (client, extraAttrs = {}) {
     return [obj, attrs]
 }
 
-test('create property by minimal fields (dv, organization, sender, type, name, address, addressMeta)', async () => {
-    const client = await makeLoggedInClient()
-    const [obj, attrs] = await createProperty(client)
-    expect(obj.id).toMatch(UUID_RE)
-    expect(obj.dv).toEqual(1)
-    expect(obj.sender).toStrictEqual(attrs.sender)
-    expect(obj.organization).toEqual(expect.objectContaining({ id: Property.DEFAULT_ORGANIZATION_ID }))
-    expect(obj.type).toEqual('building')
-    expect(obj.name).toEqual(attrs.name)
-    expect(obj.address).toEqual(attrs.address)
-    expect(obj.addressMeta).toStrictEqual(attrs.addressMeta)
-    expect(obj.map).toEqual(null)
-    expect(obj.v).toEqual(1)
-    expect(obj.newId).toEqual(null)
-    expect(obj.deletedAt).toEqual(null)
-    expect(obj.createdBy).toEqual(expect.objectContaining({ id: client.user.id }))
-    expect(obj.updatedBy).toEqual(expect.objectContaining({ id: client.user.id }))
-    expect(obj.createdAt).toMatch(DATETIME_RE)
-    expect(obj.updatedAt).toMatch(DATETIME_RE)
+async function makeClientWithProperty () {
+    const client = await makeClientWithRegisteredOrganization()
+    const [property] = await createProperty(client, client.organization, { map: buildingMapJson })
+    client.property = property
+    return client
+}
+
+describe('Property', () => {
+    test('user: createProperty()', async () => {
+        const client = await makeClientWithRegisteredOrganization()
+        const [obj, attrs] = await createProperty(client, client.organization)
+        expect(obj.id).toMatch(UUID_RE)
+        expect(obj.dv).toEqual(1)
+        expect(obj.sender).toStrictEqual(attrs.sender)
+        expect(obj.organization).toEqual(expect.objectContaining({ id: client.organization.id }))
+        expect(obj.type).toEqual('building')
+        expect(obj.name).toEqual(attrs.name)
+        expect(obj.address).toEqual(attrs.address)
+        expect(obj.addressMeta).toStrictEqual(attrs.addressMeta)
+        expect(obj.map).toEqual(null)
+        expect(obj.v).toEqual(1)
+        expect(obj.newId).toEqual(null)
+        expect(obj.deletedAt).toEqual(null)
+        expect(obj.createdBy).toEqual(expect.objectContaining({ id: client.user.id }))
+        expect(obj.updatedBy).toEqual(expect.objectContaining({ id: client.user.id }))
+        expect(obj.createdAt).toMatch(DATETIME_RE)
+        expect(obj.updatedAt).toMatch(DATETIME_RE)
+    })
+
+    test('user: update Property.map field for created property', async () => {
+        const client = await makeClientWithRegisteredOrganization()
+        const [obj1, attrs] = await createProperty(client, client.organization)
+        const obj = await Property.update(client, obj1.id, { dv: 1, sender: attrs.sender, map: buildingMapJson })
+        expect(obj.dv).toEqual(1)
+        expect(obj.sender).toStrictEqual(attrs.sender)
+        expect(obj.organization).toEqual(expect.objectContaining({ id: client.organization.id }))
+        expect(obj.type).toEqual('building')
+        expect(obj.name).toEqual(attrs.name)
+        expect(obj.address).toEqual(attrs.address)
+        expect(obj.addressMeta).toStrictEqual(attrs.addressMeta)
+        expect(obj.map).toStrictEqual(buildingMapJson)
+        expect(obj.v).toEqual(2)
+        expect(obj.newId).toEqual(null)
+        expect(obj.deletedAt).toEqual(null)
+        expect(obj.createdBy).toEqual(expect.objectContaining({ id: client.user.id }))
+        expect(obj.updatedBy).toEqual(expect.objectContaining({ id: client.user.id }))
+        expect(obj.createdAt).toMatch(DATETIME_RE)
+        expect(obj.updatedAt).toMatch(DATETIME_RE)
+        expect(obj.updatedAt).not.toEqual(obj.createdAt)
+    })
 })
 
-test('update map for existing property (map)', async () => {
-    const client = await makeLoggedInClient()
-    const [obj1, attrs] = await createProperty(client)
-    const obj = await Property.update(client, obj1.id, { dv: 1, sender: attrs.sender, map: buildingMapJson })
-    expect(obj.dv).toEqual(1)
-    expect(obj.sender).toStrictEqual(attrs.sender)
-    expect(obj.organization).toEqual(expect.objectContaining({ id: Property.DEFAULT_ORGANIZATION_ID }))
-    expect(obj.type).toEqual('building')
-    expect(obj.name).toEqual(attrs.name)
-    expect(obj.address).toEqual(attrs.address)
-    expect(obj.addressMeta).toStrictEqual(attrs.addressMeta)
-    expect(obj.map).toStrictEqual(buildingMapJson)
-    expect(obj.v).toEqual(2)
-    expect(obj.newId).toEqual(null)
-    expect(obj.deletedAt).toEqual(null)
-    expect(obj.createdBy).toEqual(expect.objectContaining({ id: client.user.id }))
-    expect(obj.updatedBy).toEqual(expect.objectContaining({ id: client.user.id }))
-    expect(obj.createdAt).toMatch(DATETIME_RE)
-    expect(obj.updatedAt).toMatch(DATETIME_RE)
-    expect(obj.updatedAt).not.toEqual(obj.createdAt)
-})
+module.exports = {
+    createProperty,
+    makeClientWithProperty,
+}
