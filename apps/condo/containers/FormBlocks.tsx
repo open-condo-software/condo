2551728d2dd2b8e1a46314c7d058e94f1b1ea775
Dmitry Danev <danewdima@gmail.com>
2021-02-18 15:42:23 +0300

    SBERDOMA-19 fix FormBlock Autocomplete Select search result view

--- apps/condo/containers/FormBlocks.tsx
+++ apps/condo/containers/FormBlocks.tsx
@@ -5,45 +5,51 @@ import { Select } from 'antd'
 function SearchInput ({ search, ...props }) {
     const client = useApolloClient()
     const [selected, setSelected] = useState('')
+    const [isLoading, setLoading] = useState(false)
     const [data, setData] = useState([])
     const [value, setValue] = useState('')
     const options = useMemo(
-        () => data.map(d => <Select.Option key={d.value} value={d.value}>{d.text}</Select.Option>),
+        () => data.map(d => <Select.Option key={d.value} value={d.value} title={d.text}>{d.text}</Select.Option>),
         [data, value],
     )
 
+    useEffect(() => {
+        handleSearch('')
+    }, [])
+
     async function handleSearch (value) {
+        setLoading(true)
         const data = await search(client, (selected) ? selected + ' ' + value : value)
+        setLoading(false)
         if (data.length) setData(data)
         setValue(value)
     }
 
-    function handleSelect (value, options) {
-        setSelected(options.children)
+    function handleSelect (value, option) {
+        setSelected(option.children)
     }
 
     function handleClear () {
         setSelected('')
     }
 
-    useEffect(() => {
-        handleSearch('')
-    }, [])
-
-    return <Select
-        showSearch
-        autoClearSearchValue={false}
-        allowClear={true}
-        defaultActiveFirstOption={false}
-        defaultValue={value}
-        onSearch={handleSearch}
-        onSelect={handleSelect}
-        onClear={handleClear}
-        searchValue={value}
-        {...props}
-    >
-        {options}
-    </Select>
+    return (
+        <Select
+            showSearch
+            autoClearSearchValue={false}
+            allowClear={true}
+            optionFilterProp={'title'}
+            defaultActiveFirstOption={false}
+            onSearch={handleSearch}
+            onSelect={handleSelect}
+            onClear={handleClear}
+            searchValue={value}
+            loading={isLoading}
+            {...props}
+        >
+            {options}
+        </Select>
+    )
 }
 
 export {
