9b6913ac2b89751f36d99555b46ffa744e92804b
Pahaz <pahaz.white@gmail.com>
2021-02-19 00:04:41 +0300

    SBERDOMA-16 fix Ticket.test.js

--- apps/condo/schema/Ticket.test.js
+++ apps/condo/schema/Ticket.test.js
@@ -1,60 +1,69 @@
 /**
  * @jest-environment node
- * @test-style 3
  */
 
 const { setFakeClientMode } = require('@core/keystone/test.utils')
 const conf = require('@core/config')
 if (conf.TESTS_FAKE_CLIENT_MODE) setFakeClientMode(require.resolve('../index'))
+
 const faker = require('faker')
+const { makeClientWithProperty } = require('./Property.test')
 const { NUMBER_RE, UUID_RE, DATETIME_RE } = require('@core/keystone/test.utils')
-const { makeLoggedInClient } = require('@core/keystone/test.utils')
 
 const { Ticket } = require('./Ticket.gql')
 
-Ticket.OPEN_STATUS_ID = '6ef3abc4-022f-481b-90fb-8430345ebfc2'
-Ticket.UNKNOWN_CLASSIFIER_ID = '4f4b43d5-0951-425c-9428-945dc6193361'
-Ticket.OTHER_SOURCE_ID = '7da1e3be-06ba-4c9e-bba6-f97f278ac6e4'
-Ticket.DEFAULT_ORGANIZATION_ID = '1'
-Ticket.DEFAULT_PROPERTY_ID = '5c7df4a7-6064-454e-bde2-eac3006cc09b'
+const OPEN_STATUS_ID = '6ef3abc4-022f-481b-90fb-8430345ebfc2'
+const UNKNOWN_CLASSIFIER_ID = '4f4b43d5-0951-425c-9428-945dc6193361'
+const OTHER_SOURCE_ID = '7da1e3be-06ba-4c9e-bba6-f97f278ac6e4'
 
-test('create ticket by minimal fields (dv, organization, sender, source, classifier, status, details)', async () => {
-    const client = await makeLoggedInClient()
+async function createTicket (client, organization, property, extraAttrs = {}) {
+    if (!client) throw new Error('no client')
+    if (!organization || !organization.id) throw new Error('no organization.id')
+    if (!property || !property.id) throw new Error('no property.id')
     const sender = { dv: 1, fingerprint: faker.random.alphaNumeric(8) }
     const details = faker.random.alphaNumeric(10)
-    const obj = await Ticket.create(client, {
+
+    const attrs = {
         dv: 1,
         sender,
         details,
-        organization: { connect: { id: Ticket.DEFAULT_ORGANIZATION_ID } },
-        status: { connect: { id: Ticket.OPEN_STATUS_ID } },
-        classifier: { connect: { id: Ticket.UNKNOWN_CLASSIFIER_ID } },
-        source: { connect: { id: Ticket.OTHER_SOURCE_ID } },
-        property: { connect: { id: Ticket.DEFAULT_PROPERTY_ID } },
-    })
-    expect(obj.id).toMatch(UUID_RE)
-    expect(String(obj.number)).toMatch(NUMBER_RE)
-    expect(obj.dv).toEqual(1)
-    expect(obj.sender).toStrictEqual(sender)
-    expect(obj.source).toEqual(expect.objectContaining({ id: Ticket.OTHER_SOURCE_ID }))
-    expect(obj.sourceMeta).toEqual(null)
-    expect(obj.classifier).toEqual(expect.objectContaining({ id: Ticket.UNKNOWN_CLASSIFIER_ID }))
-    expect(obj.property).toEqual(expect.objectContaining({ id: Ticket.DEFAULT_PROPERTY_ID }))
-    expect(obj.status).toEqual(expect.objectContaining({ id: Ticket.OPEN_STATUS_ID }))
-    expect(obj.statusReopenedCounter).toEqual(0)
-    expect(obj.statusReason).toEqual(null)
-    expect(obj.details).toEqual(details)
-    expect(obj.meta).toEqual(null)
-    expect(obj.organization).toEqual(expect.objectContaining({ id: Ticket.DEFAULT_ORGANIZATION_ID }))
-    expect(obj.client).toEqual(null)
-    expect(obj.operator).toEqual(null)
-    expect(obj.assignees).toEqual([])
-    expect(obj.executors).toEqual([])
-    expect(obj.v).toEqual(1)
-    expect(obj.newId).toEqual(null)
-    expect(obj.deletedAt).toEqual(null)
-    expect(obj.createdBy).toEqual(expect.objectContaining({ id: client.user.id }))
-    expect(obj.updatedBy).toEqual(expect.objectContaining({ id: client.user.id }))
-    expect(obj.createdAt).toMatch(DATETIME_RE)
-    expect(obj.updatedAt).toMatch(DATETIME_RE)
+        organization: { connect: { id: organization.id } },
+        property: { connect: { id: property.id } },
+        status: { connect: { id: OPEN_STATUS_ID } },
+        classifier: { connect: { id: UNKNOWN_CLASSIFIER_ID } },
+        source: { connect: { id: OTHER_SOURCE_ID } },
+        ...extraAttrs,
+    }
+    const obj = await Ticket.create(client, attrs)
+    return [obj, attrs]
+}
+
+test('user: createTicket()', async () => {
+    const client = await makeClientWithProperty()
+    const [ticket, attrs] = await createTicket(client, client.organization, client.property)
+    expect(ticket.id).toMatch(UUID_RE)
+    expect(String(ticket.number)).toMatch(NUMBER_RE)
+    expect(ticket.dv).toEqual(1)
+    expect(ticket.sender).toEqual(attrs.sender)
+    expect(ticket.source).toEqual(expect.objectContaining({ id: OTHER_SOURCE_ID }))
+    expect(ticket.sourceMeta).toEqual(null)
+    expect(ticket.classifier).toEqual(expect.objectContaining({ id: UNKNOWN_CLASSIFIER_ID }))
+    expect(ticket.property).toEqual(expect.objectContaining({ id: client.property.id }))
+    expect(ticket.status).toEqual(expect.objectContaining({ id: OPEN_STATUS_ID }))
+    expect(ticket.statusReopenedCounter).toEqual(0)
+    expect(ticket.statusReason).toEqual(null)
+    expect(ticket.details).toEqual(attrs.details)
+    expect(ticket.meta).toEqual(null)
+    expect(ticket.organization).toEqual(expect.objectContaining({ id: client.organization.id }))
+    expect(ticket.client).toEqual(null)
+    expect(ticket.operator).toEqual(null)
+    expect(ticket.assignees).toEqual([])
+    expect(ticket.executors).toEqual([])
+    expect(ticket.v).toEqual(1)
+    expect(ticket.newId).toEqual(null)
+    expect(ticket.deletedAt).toEqual(null)
+    expect(ticket.createdBy).toEqual(expect.objectContaining({ id: client.user.id }))
+    expect(ticket.updatedBy).toEqual(expect.objectContaining({ id: client.user.id }))
+    expect(ticket.createdAt).toMatch(DATETIME_RE)
+    expect(ticket.updatedAt).toMatch(DATETIME_RE)
 })
