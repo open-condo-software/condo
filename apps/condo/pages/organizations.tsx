14d432a99ce26f08d48db92d3dc33f7cf43b1fb8
Dmitry Danev <danewdima@gmail.com>
2021-02-20 08:16:16 +0300

    SBERDOMA-19 delete migrations, delete unused utils

--- apps/condo/pages/organizations.tsx
+++ apps/condo/pages/organizations.tsx
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
 import { Avatar, Button, Form, Input, notification, Radio, Tag } from 'antd'
 import { QuestionCircleOutlined } from '@ant-design/icons'
