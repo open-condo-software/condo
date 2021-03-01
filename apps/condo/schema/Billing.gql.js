dfee21df4b4416e1ae45ab115ed3c157a52f1526
Pahaz <pahaz.white@gmail.com>
2021-03-01 14:09:21 +0500

    SBERDOMA-15 repair ticket tests

--- apps/condo/schema/Billing.gql.js
+++ apps/condo/schema/Billing.gql.js
@@ -3,7 +3,7 @@ const { genTestGQLUtils } = require('@core/keystone/gen.gql.utils')
 const BILLING_INTEGRATION_FIELDS = '{ id dv v name deletedAt newId createdBy { id name } updatedBy { id name } createdAt updatedAt }'
 const BillingIntegration = genTestGQLUtils('BillingIntegration', BILLING_INTEGRATION_FIELDS)
 
-const BillingIntegrationAccessRight = genTestGQLUtils('BillingIntegrationAccessRight', `{ id dv sender v integration ${BILLING_INTEGRATION_FIELDS} user { id } createdBy { id name } updatedBy { id name } createdAt updatedAt }`)
+const BillingIntegrationAccessRight = genTestGQLUtils('BillingIntegrationAccessRight', `{ id dv integration ${BILLING_INTEGRATION_FIELDS} user { id } createdBy { id name } updatedBy { id name } createdAt updatedAt }`)
 const BillingIntegrationOrganizationContext = genTestGQLUtils('BillingIntegrationOrganizationContext', `{ id dv sender v integration ${BILLING_INTEGRATION_FIELDS} organization { id } settings state createdBy { id name } updatedBy { id name } createdAt updatedAt }`)
 const BillingIntegrationLog = genTestGQLUtils('BillingIntegrationLog', `{ id dv sender v context { dv integration ${BILLING_INTEGRATION_FIELDS} organization { id } } type message meta createdBy { id name } updatedBy { id name } createdAt updatedAt }`)
 
