03bc465dc1b18238f3f344eb3f0c75721bb79235
Dmitry Danev <danewdima@gmail.com>
2021-03-01 18:49:49 +0300

    SBERDOMA-21 add AddressSearchInput.tsx to propertyCreate Page

--- apps/condo/pages/property/index.tsx
+++ apps/condo/pages/property/index.tsx
@@ -1,4 +1,3 @@
-// @ts-nocheck
 import Head from 'next/head'
 import Link from 'next/link'
 import React, { useEffect } from 'react'
@@ -17,35 +16,11 @@ import FormList, {
     useCreateAndEditModalForm,
 } from '../../containers/FormList'
 import { useTable } from '../../containers/FormTableBlocks'
-import { SearchInput } from '../../components/SearchInput'
+import { AddressSearchInput } from '../../components/AddressSearchInput'
 
 import { useCreate, useObjects } from '../../schema/Property.uistate'
 
-async function searchDadataAddress (client, value) {
-    // https://dadata.ru/api/suggest/address/
-    const token = '257f4bd2c057e727f4e48438d121ffa7a665fce7'
-    const loadSuggestionsUrl = 'https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/address'
-    const response = await fetch(loadSuggestionsUrl, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            Accept: 'application/json',
-            Authorization: `Token ${token}`,
-        },
-        body: JSON.stringify({ query: value }),
-    })
-    const data = await response.json()
-    if (!data.suggestions) throw new Error('no suggestions')
-    const suggestions = data.suggestions.map(x => {
-        const notNull = Object.fromEntries(Object.keys(x.data).filter((k) => !!x.data[k]).map((k) => [k, x.data[k]]))
-        return {
-            text: x.value,
-            value: JSON.stringify({ ...notNull, address: x.value }),
-        }
-    })
-    console.log(suggestions)
-    return suggestions
-}
+
 
 function CreateAndEditPropertyModalForm ({ action, visible, editableItem, cancelModal }) {
     const intl = useIntl()
@@ -65,45 +40,47 @@ function CreateAndEditPropertyModalForm ({ action, visible, editableItem, cancel
         },
     }
 
-    return <BaseModalForm
-        /* NOTE: we need to recreate form if editableItem changed because the form initialValues are cached */
-        key={editableItem}
-        action={action}
-        formValuesToMutationDataPreprocessor={(x) => {
-            const addressMeta = JSON.parse(x['address'])
-            return { ...x, addressMeta, address: addressMeta['address'] }
-        }}
-        visible={visible}
-        cancelModal={cancelModal}
-        ModalTitleMsg={(editableItem) ? EditPropertyModalTitleMsg : CreatePropertyModalTitleMsg}
-        ErrorToFormFieldMsgMapping={ErrorToFormFieldMsgMapping}
-    >
-        <Form.Item
-            name="address"
-            label={AddressMsg}
-            rules={[{ required: true, message: FieldIsRequiredMsg }]}
+    return (
+        <BaseModalForm
+            /* NOTE: we need to recreate form if editableItem changed because the form initialValues are cached */
+            key={editableItem}
+            action={action}
+            formValuesToMutationDataPreprocessor={(x) => {
+                const addressMeta = JSON.parse(x['address'])
+                return { ...x, addressMeta, address: addressMeta['address'] }
+            }}
+            visible={visible}
+            cancelModal={cancelModal}
+            ModalTitleMsg={(editableItem) ? EditPropertyModalTitleMsg : CreatePropertyModalTitleMsg}
+            ErrorToFormFieldMsgMapping={ErrorToFormFieldMsgMapping}
         >
-            <SearchInput search={searchDadataAddress}/>
-        </Form.Item>
-        <Form.Item
-            name="name"
-            label={NameMsg}
-            rules={[{ required: true, message: FieldIsRequiredMsg }]}
-        >
-            <Input/>
-        </Form.Item>
-        <Form.Item
-            name="type"
-            label={TypeMsg}
-            rules={[{ required: true, message: FieldIsRequiredMsg }]}
-            initialValue={'building'}
-        >
-            <Select>
-                <Select.Option value="building">{BuildingMsg}</Select.Option>
-                <Select.Option value="village">{VillageMsg}</Select.Option>
-            </Select>
-        </Form.Item>
-    </BaseModalForm>
+            <Form.Item
+                name="address"
+                label={AddressMsg}
+                rules={[{ required: true, message: FieldIsRequiredMsg }]}
+            >
+                <AddressSearchInput/>
+            </Form.Item>
+            <Form.Item
+                name="name"
+                label={NameMsg}
+                rules={[{ required: true, message: FieldIsRequiredMsg }]}
+            >
+                <Input/>
+            </Form.Item>
+            <Form.Item
+                name="type"
+                label={TypeMsg}
+                rules={[{ required: true, message: FieldIsRequiredMsg }]}
+                initialValue={'building'}
+            >
+                <Select>
+                    <Select.Option value="building">{BuildingMsg}</Select.Option>
+                    <Select.Option value="village">{VillageMsg}</Select.Option>
+                </Select>
+            </Form.Item>
+        </BaseModalForm>
+    )
 }
 
 function CreatePropertyModalBlock ({ modal, create }) {
@@ -133,47 +110,24 @@ function ViewOrEditPropertyListBlock ({ loading, objs }) {
     const DeleteMsg = intl.formatMessage({ id: 'Delete' })
     const AreYouSureMsg = intl.formatMessage({ id: 'AreYouSure' })
 
-    // function handleSelect (item) {
-    //     selectLink(item).then(() => {
-    //         const query = getQueryParams()
-    //         if (query.next) Router.push(query.next)
-    //         else Router.push('/')
-    //     })
-    // }
-
-    return <FormList
-        loading={loading}
-        // pagination={paginationProps}
-        // dataSource={objs || LOADING_LIST}
-        dataSource={objs}
-        renderItem={(item) => {
-            const { name, address, avatar, href } = (item)
-            return {
-                extraBlockMeta: { style: { width: '150px' } },
-                avatar: <Avatar src={avatar} shape="square" size="large"/>,
-                title: <Link href={href}><a>{name}</a></Link>,
-                description: <ExpandableDescription>{address}</ExpandableDescription>,
-                actions: [
-                    [<Link href={href}><Button size="small" type={'primary'}>{SelectMsg}</Button></Link>],
-                    // [<ExtraDropdownActionsMenu actions={[
-                    //     {
-                    //         label: EditMsg,
-                    //         action: () => openEditModal(item),
-                    //     },
-                    //     {
-                    //         label: DeleteMsg,
-                    //         confirm: {
-                    //             title: AreYouSureMsg,
-                    //             icon: <QuestionCircleOutlined style={{ color: 'red' }}/>,
-                    //         },
-                    //         action: () => handleDelete(item),
-                    //     },
-                    // ]}/>],
-                ],
-            }
-        }}
-    />
-
+    return (
+        <FormList
+            loading={loading}
+            dataSource={objs}
+            renderItem={(item) => {
+                const { name, address, avatar, href } = (item)
+                return {
+                    extraBlockMeta: { style: { width: '150px' } },
+                    avatar: <Avatar src={avatar} shape="square" size="large"/>,
+                    title: <Link href={href}><a>{name}</a></Link>,
+                    description: <ExpandableDescription>{address}</ExpandableDescription>,
+                    actions: [
+                        [<Link href={href}><Button size="small" type={'primary'}>{SelectMsg}</Button></Link>],
+                    ],
+                }
+            }}
+        />
+    )
 }
 
 function PropertyCRUDListBlock () {
@@ -189,37 +143,36 @@ function PropertyCRUDListBlock () {
         if (objs) {
             table.setData(objs)
             table.updateFilterPaginationSort({ total: count })
-            const actions = {
-                // CreateOrUpdate: handleCreateOrUpdate,
-                // Delete: handleDelete,
-            }
-            // table.updateActions(actions)
         }
     }, [objs])
 
-    return <>
-        <CreatePropertyModalBlock modal={modal} create={create}/>
-        <ViewOrEditPropertyListBlock objs={objs} loading={loading}/>
-    </>
+    return (
+        <>
+            <CreatePropertyModalBlock modal={modal} create={create}/>
+            <ViewOrEditPropertyListBlock objs={objs} loading={loading}/>
+        </>
+    )
 }
 
 const PropertyIndexPage = () => {
     const intl = useIntl()
     const PageTitleMsg = intl.formatMessage({ id: 'pages.condo.property.index.PageTitle' })
 
-    return <>
-        <Head>
-            <title>{PageTitleMsg}</title>
-        </Head>
-        <PageWrapper>
-            <PageHeader title={PageTitleMsg}/>
-            <PageContent>
-                <OrganizationRequired>
-                    <PropertyCRUDListBlock/>
-                </OrganizationRequired>
-            </PageContent>
-        </PageWrapper>
-    </>
+    return (
+        <>
+            <Head>
+                <title>{PageTitleMsg}</title>
+            </Head>
+            <PageWrapper>
+                <PageHeader title={PageTitleMsg}/>
+                <PageContent>
+                    <OrganizationRequired>
+                        <PropertyCRUDListBlock/>
+                    </OrganizationRequired>
+                </PageContent>
+            </PageWrapper>
+        </>
+    )
 }
 
 export default PropertyIndexPage
