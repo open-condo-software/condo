8f0f7601e2fc8c886cdc61f7436030f712195d02
Pahaz <pahaz.white@gmail.com>
2021-03-01 12:54:28 +0500

    SBERDOMA-15 repair access tests

--- apps/condo/schema/Billing.gql.js
+++ apps/condo/schema/Billing.gql.js
@@ -1,11 +1,11 @@
 const { genTestGQLUtils } = require('@core/keystone/gen.gql.utils')
 
-const BILLING_INTEGRATION_FIELDS = '{ id dv name v deletedAt newId createdBy { id name } updatedBy { id name } createdAt updatedAt }'
+const BILLING_INTEGRATION_FIELDS = '{ id dv sender v name deletedAt newId createdBy { id name } updatedBy { id name } createdAt updatedAt }'
 const BillingIntegration = genTestGQLUtils('BillingIntegration', BILLING_INTEGRATION_FIELDS)
 
-const BillingIntegrationAccessRight = genTestGQLUtils('BillingIntegrationAccessRight', `{ id dv integration ${BILLING_INTEGRATION_FIELDS} user { id } createdBy { id name } updatedBy { id name } createdAt updatedAt }`)
-const BillingIntegrationOrganizationContext = genTestGQLUtils('BillingIntegrationOrganizationContext', `{ id dv v integration ${BILLING_INTEGRATION_FIELDS} organization { id } settings state createdBy { id name } updatedBy { id name } createdAt updatedAt }`)
-const BillingIntegrationLog = genTestGQLUtils('BillingIntegrationLog', `{ id dv v context { dv integration ${BILLING_INTEGRATION_FIELDS} organization { id } } type message meta createdBy { id name } updatedBy { id name } createdAt updatedAt }`)
+const BillingIntegrationAccessRight = genTestGQLUtils('BillingIntegrationAccessRight', `{ id dv sender v integration ${BILLING_INTEGRATION_FIELDS} user { id } createdBy { id name } updatedBy { id name } createdAt updatedAt }`)
+const BillingIntegrationOrganizationContext = genTestGQLUtils('BillingIntegrationOrganizationContext', `{ id dv sender v integration ${BILLING_INTEGRATION_FIELDS} organization { id } settings state createdBy { id name } updatedBy { id name } createdAt updatedAt }`)
+const BillingIntegrationLog = genTestGQLUtils('BillingIntegrationLog', `{ id dv sender v context { dv integration ${BILLING_INTEGRATION_FIELDS} organization { id } } type message meta createdBy { id name } updatedBy { id name } createdAt updatedAt }`)
 
 module.exports = {
     BillingIntegration,
