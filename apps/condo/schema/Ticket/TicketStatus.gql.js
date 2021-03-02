b3e29353335db5b2344bff98f0b692bf10413845
Dmitry Danev <danewdima@gmail.com>
2021-03-02 15:37:24 +0300

    SBERDOMA-45 naming changes

--- apps/condo/schema/Ticket/TicketStatus.gql.js
+++ apps/condo/schema/Ticket/TicketStatus.gql.js
@@ -1,9 +1,8 @@
 const { genTestGQLUtils } = require('@core/keystone/gen.gql.utils')
 
-const TICKET_FIELDS = '{ id dv name }'
-const TicketStatus = genTestGQLUtils('TicketStatus', TICKET_FIELDS)
+const TICKET_STATUS_FIELDS = '{ id dv name }'
+const TicketStatus = genTestGQLUtils('TicketStatus', TICKET_STATUS_FIELDS)
 
 module.exports = {
     TicketStatus,
-    TICKET_FIELDS,
 }
