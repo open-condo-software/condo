72f35861755242e31759161e848ceec19cd0940d
Dmitry Danev <danewdima@gmail.com>
2021-03-02 18:09:03 +0300

    SBERDOMA-45 add utils/testSchema/Billing

--- apps/condo/schema/Billing.test.js
+++ apps/condo/schema/Billing.test.js
@@ -4,82 +4,18 @@
 
 const { setFakeClientMode, makeLoggedInAdminClient } = require('@core/keystone/test.utils')
 const conf = require('@core/config')
-if (conf.TESTS_FAKE_CLIENT_MODE) setFakeClientMode(require.resolve('../index'))
-
-const faker = require('faker')
+const { BillingIntegrationLog, BillingIntegrationOrganizationContext } = require('../gql/Billing')
+const {
+    createBillingIntegrationLog,
+    createBillingIntegrationOrganizationContext,
+    createBillingIntegrationAccessRight,
+    createBillingIntegration,
+} = require('../utils/testSchema/Billing')
 const { getRandomString } = require('@core/keystone/test.utils')
 const { makeClientWithNewRegisteredAndLoggedInUser } = require('../utils/testSchema/User')
-const { BillingIntegration, BillingIntegrationAccessRight, BillingIntegrationOrganizationContext, BillingIntegrationLog } = require('./Billing.gql')
 const { makeClientWithProperty } = require('./Property/Property.test')
 
-async function createBillingIntegration (client, extraAttrs = {}) {
-    if (!client) throw new Error('no client')
-    const name = faker.company.companyName()
-
-    const attrs = {
-        dv: 1,
-        name,
-        ...extraAttrs,
-    }
-    const obj = await BillingIntegration.create(client, attrs)
-    return [obj, attrs]
-}
-
-async function createBillingIntegrationAccessRight (client, user, integration, extraAttrs = {}) {
-    if (!client) throw new Error('no client')
-    if (!user.id) throw new Error('no user.id')
-    if (!integration.id) throw new Error('no integration.id')
-
-    const attrs = {
-        dv: 1,
-        user: { connect: { id: user.id } },
-        integration: { connect: { id: integration.id } },
-        ...extraAttrs,
-    }
-    const obj = await BillingIntegrationAccessRight.create(client, attrs)
-    return [obj, attrs]
-}
-
-async function createBillingIntegrationOrganizationContext (client, organization, integration, extraAttrs = {}) {
-    if (!client) throw new Error('no client')
-    if (!organization.id) throw new Error('no organization.id')
-    if (!integration.id) throw new Error('no integration.id')
-    const sender = { dv: 1, fingerprint: faker.random.alphaNumeric(8) }
-
-    const attrs = {
-        dv: 1, sender,
-        organization: { connect: { id: organization.id } },
-        integration: { connect: { id: integration.id } },
-        settings: {
-            dv: 1,
-            'billing data source': 'https://api.dom.gosuslugi.ru/',
-        },
-        state: {
-            dv: 1,
-        },
-        ...extraAttrs,
-    }
-    const obj = await BillingIntegrationOrganizationContext.create(client, attrs)
-    return [obj, attrs]
-}
-
-async function createBillingIntegrationLog (client, context, extraAttrs = {}) {
-    if (!client) throw new Error('no client')
-    if (!context.id) throw new Error('no context.id')
-    const sender = { dv: 1, fingerprint: faker.random.alphaNumeric(8) }
-    const type = faker.lorem.words().replace(/[ ]/g, '_').toUpperCase()
-    const message = faker.lorem.sentences()
-    const meta = { username: faker.lorem.word(), server: faker.internet.url(), ip: faker.internet.ipv6() }
-
-    const attrs = {
-        dv: 1, sender,
-        context: { connect: { id: context.id } },
-        type, message, meta,
-        ...extraAttrs,
-    }
-    const obj = await BillingIntegrationLog.create(client, attrs)
-    return [obj, attrs]
-}
+if (conf.TESTS_FAKE_CLIENT_MODE) setFakeClientMode(require.resolve('../index'))
 
 describe('BillingIntegration', () => {
     test('admin: createBillingIntegration', async () => {
