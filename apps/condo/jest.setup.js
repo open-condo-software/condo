0d2380eb14ac14a84a541bbd472dd82dde8d0e16
Pahaz <pahaz.white@gmail.com>
2021-03-06 23:55:36 +0500

    SBERDOMA-62 rewrite jest configs (speedUp)

---
+++ apps/condo/jest.setup.js
@@ -0,0 +1,4 @@
+// setup TESTS_FAKE_CLIENT_MODE
+const { setFakeClientMode } = require('@core/keystone/test.utils')
+const conf = require('@core/config')
+if (conf.TESTS_FAKE_CLIENT_MODE) setFakeClientMode(require.resolve('./index'))
