973327edf8299cb8e1fab7ee073e4fdd8547d8dd
Dmitry Danev <danewdima@gmail.com>
2021-03-14 18:17:24 +0300

    SBERDOMA-59 add buttonSecondaryCss styles

--- apps/condo/components/Button.tsx
+++ apps/condo/components/Button.tsx
@@ -31,16 +31,40 @@ const buttonCss = (color) => css`
   }
 `
 
+const buttonSecondaryCss = (color) => css`
+  background-color: ${colors.white};
+  box-shadow: 0 3px 6px ${colors.lightGrey[4]}, 0 6px 16px ${colors.lightGrey[3]}, 0 9px 28px ${colors.lightGrey[2]};
+  border: 1px solid ${color[5]};
+  color: ${color[6]};
+  font-weight: 400;
+
+  &:hover, &:focus {
+    box-shadow: none;
+    color: ${color[6]};
+    border-color: ${color[5]};
+  }
+
+  &:active {
+    border-color: ${color[4]};
+  }
+
+  &:disabled, &:hover:disabled {
+    box-shadow: none;
+    border-color: ${color[3]};
+    color: ${color[3]}
+  }
+`
+
 interface CustomButtonProps extends Omit<ButtonProps, 'type'>{
     type: 'sberDefault' | 'sberPrimary' | ButtonProps['type'],
-    noShadow?: boolean,
+    secondary?: boolean
 }
 
-export const Button:React.FunctionComponent<CustomButtonProps> = ({ type, ...restProps }) => {
+export const Button:React.FunctionComponent<CustomButtonProps> = ({ type, secondary, ...restProps }) => {
     if (type !== 'sberDefault' && type !== 'sberPrimary') {
         return <DefaultButton {...{ ...restProps, type }}/>
     } else {
-        const buttonStyles = buttonCss(colors[type])
+        const buttonStyles = secondary ? buttonSecondaryCss(colors[type]) : buttonCss(colors[type])
 
         return <DefaultButton css={buttonStyles} {...restProps}/>
     }
