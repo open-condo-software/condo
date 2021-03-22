3c73e36e1c16940b36a842dba6626534e6d3aa1a
Pahaz <pahaz.white@gmail.com>
2021-03-22 16:38:37 +0500

    SBERDOMA-63 refactor Ticket schema

--- apps/condo/domains/ticket/schema/Ticket.js
+++ apps/condo/domains/ticket/schema/Ticket.js
@@ -7,8 +7,10 @@ const { GQLListSchema } = require('@core/keystone/schema')
 const { Json, AutoIncrementInteger } = require('@core/keystone/fields')
 const { historical, versioned, uuided, tracked, softDeleted } = require('@core/keystone/plugins')
 
-const { SENDER_FIELD, DV_FIELD, ORGANIZATION_OWNED_FIELD } = require('@condo/domains/common/schema/fields')
+const { SENDER_FIELD, DV_FIELD } = require('@condo/domains/common/schema/fields')
 const access = require('@condo/domains/ticket/access/Ticket')
+
+const { ORGANIZATION_OWNED_FIELD } = require('../../../schema/_common')
 const { hasRequestAndDbFields } = require('../../../utils/validation.utils')
 const { JSON_EXPECT_OBJECT_ERROR, DV_UNKNOWN_VERSION_ERROR, STATUS_UPDATED_AT_ERROR, JSON_UNKNOWN_VERSION_ERROR } = require('../../../constants/errors')
 
