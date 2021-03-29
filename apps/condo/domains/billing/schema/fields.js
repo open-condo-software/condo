7033df6ed67edfb75962ae732cb60aebfe6b6bd6
Pahaz <pahaz.white@gmail.com>
2021-03-30 01:05:16 +0300

    SBERDOMA-62 extract IMPORT_ID_FIELD

--- apps/condo/domains/billing/schema/fields.js
+++ apps/condo/domains/billing/schema/fields.js
@@ -7,6 +7,14 @@ const INTEGRATION_CONTEXT_FIELD = {
     kmigratorOptions: { null: false, on_delete: 'models.CASCADE' },
 }
 
+// TODO(pahaz): add constrains with this field! + context
+const IMPORT_ID_FIELD = {
+    schemaDoc: '`billing data source` local object ID. Used only for the internal needs of the `integration component`',
+    type: Text,
+    isRequired: false,
+}
+
 module.exports = {
     INTEGRATION_CONTEXT_FIELD,
+    IMPORT_ID_FIELD,
 }
