951bfe4f35146b97ee36488a2d24ab82ced416f7
Dmitry Koviazin <dkoviazin@gmail.com>
2021-03-30 16:59:04 +0500

    Remove comments

--- packages/@core.keystone/fields/AutoIncrementInteger/Implementation.js
+++ packages/@core.keystone/fields/AutoIncrementInteger/Implementation.js
@@ -33,24 +33,6 @@ class AutoIncrementIntegerKnexFieldAdapter extends Integer.adapters.knex {
         max = max || 0
         return ++max
     }    
-    /*
-    setupHooks ({ addPreSaveHook, addPostReadHook }) {
-        addPreSaveHook(item => {
-            // TODO(Dimtreee|Pahaz): fix autoincrement on status update, or add new field
-            if (this.path in item) {
-                return item
-            }
-
-            const tableName = this.listAdapter.tableName
-            const fieldName = this.dbPath
-            const knex = this.listAdapter.parentAdapter.knex
-            item[this.path] = knex.raw(
-                'coalesce( (select max(:fieldName:) as max from :tableName:), 0) + 1', { tableName, fieldName })
-                .wrap('(', ')')
-            return item
-        })
-    }
-    */
 }
 
 // TODO(pahaz): add mongo support for AutoIncrementInteger
@@ -58,17 +40,6 @@ class AutoIncrementIntegerMongooseFieldAdapter extends Integer.adapters.mongoose
     async nextValue () {
         throw new Error('Is not supported yet!')
     }
-
-    /*
-    setupHooks ({ addPreSaveHook, addPostReadHook }) {
-        addPreSaveHook(item => {
-            throw new Error('Is not supported yet!')
-        })
-        addPostReadHook(item => {
-            throw new Error('Is not supported yet!')
-        })
-    }
-    */
 }
 
 module.exports = {
