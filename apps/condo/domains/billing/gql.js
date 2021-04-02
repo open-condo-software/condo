41aaf343c41e5fd7d24a4d444f1dcc3adcd78483
Pahaz <pahaz.white@gmail.com>
2021-04-02 13:17:49 +0300

    SBERDOMA-62 remove Billing.test.js

--- apps/condo/domains/billing/gql.js
+++ apps/condo/domains/billing/gql.js
@@ -11,7 +11,7 @@ const COMMON_FIELDS = 'id dv sender v deletedAt newId createdBy { id name } upda
 const BILLING_INTEGRATION_FIELDS = `{ name ${COMMON_FIELDS} }`
 const BillingIntegration = generateGqlQueries('BillingIntegration', BILLING_INTEGRATION_FIELDS)
 
-const BILLING_INTEGRATION_ACCESS_RIGHT_FIELDS = `{ integration { id name } user { id name } id dv createdBy { id name } updatedBy { id name } createdAt updatedAt }`
+const BILLING_INTEGRATION_ACCESS_RIGHT_FIELDS = '{ integration { id name } user { id name } id dv createdBy { id name } updatedBy { id name } createdAt updatedAt }'
 const BillingIntegrationAccessRight = generateGqlQueries('BillingIntegrationAccessRight', BILLING_INTEGRATION_ACCESS_RIGHT_FIELDS)
 
 const BILLING_INTEGRATION_ORGANIZATION_CONTEXT_FIELDS = `{ integration { id name } organization { id name } settings state ${COMMON_FIELDS} }`
