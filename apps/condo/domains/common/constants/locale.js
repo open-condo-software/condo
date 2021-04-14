f40a2cb98e5a0884fb77cb898e1427f3a6bd3316
Dmitry Koviazin <dkoviazin@gmail.com>
2021-04-14 16:36:41 +0500

    SBERDOMA-112 change Reflex usage for lodash/has

--- apps/condo/domains/common/constants/locale.js
+++ apps/condo/domains/common/constants/locale.js
@@ -1,11 +1,12 @@
 const RU = require('date-fns/locale/ru')
 const EN = require('date-fns/locale/en-US')
+const { has } = require('lodash')
 
 const getLocalize = (locale) => {
-    if (Reflect.has(locale, 'localize')) {
+    if (has(locale, 'localize')) {
         return locale
     }
-    if (Reflect.has(locale, 'default') && Reflect.has(locale.default, 'localize')) {
+    if (has(locale, ['default', 'localize'])) {
         return locale.default
     }
     throw new Error('Something wrong with locales in date-fns again')
