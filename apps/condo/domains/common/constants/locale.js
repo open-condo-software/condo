c64a440c53537a279fc47124d82acf04a07fd41c
Dmitry Koviazin <dkoviazin@gmail.com>
2021-04-14 16:03:06 +0500

    SBERDOMA-112 Fix type to object and add test

--- apps/condo/domains/common/constants/locale.js
+++ apps/condo/domains/common/constants/locale.js
@@ -1,9 +1,20 @@
 const RU = require('date-fns/locale/ru')
 const EN = require('date-fns/locale/en-US')
 
+const getLocalize = (locale) => {
+    if (Reflect.has(locale, 'localize')) {
+        return locale
+    }
+    if (Reflect.has(locale, 'default') && Reflect.has(locale.default, 'localize')) {
+        return locale.default
+    }
+    throw new Error('Something wrong with locales in date-fns again')
+}
+
+
 const LOCALES = {
-    ru: typeof RU.localize === 'function' ? RU : RU.default,
-    en: typeof EN.localize === 'function' ? EN : EN.default,
+    ru: getLocalize(RU),
+    en: getLocalize(EN),
 }
 
 module.exports = {
