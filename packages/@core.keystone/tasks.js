625521beb647cfd235f5e37af36bad929958a6d2
Pahaz <pahaz.white@gmail.com>
2021-04-17 16:02:36 +0300

    SBERDOMA-123 init worker logic

---
+++ packages/@core.keystone/tasks.js
@@ -0,0 +1,137 @@
+const Queue = require('bull')
+
+const conf = require('@core/config')
+
+const TASK_TYPE = 'TASK'
+const WORKER_CONCURRENCY = parseInt(conf.WORKER_CONCURRENCY || '2')
+// NOTE: If this is True, all tasks will be executed locally by blocking until the task returns.
+// Tasks will be executed locally instead of being sent to the queue.
+const FAKE_WORKER_MODE = conf.FAKE_WORKER_MODE
+const WORKER_REDIS_URL = conf.WORKER_REDIS_URL
+if (!WORKER_REDIS_URL) throw new Error('No WORKER_REDIS_URL environment')
+const taskQueue = new Queue('tasks', WORKER_REDIS_URL)
+const globalTaskOptions = { attempts: 3, backoff: true }
+const TASKS = new Map()
+
+function createTask (name, fn, opts = {}) {
+    if (typeof fn !== 'function') throw new Error('unsupported fn argument type. Function expected')
+    if (!name) throw new Error('no name')
+
+    if (TASKS.has(name)) throw new Error(`Task with name ${name} is already registered`)
+    TASKS.set(name, fn)
+    return createTaskWrapper(fn, opts)
+}
+
+async function awaitResult (jobId) {
+    const job = await Queue.Job.fromId(taskQueue, jobId)
+    return await job.finished()
+}
+
+function createTaskWrapper (fn, defaultTaskOptions = {}) {
+    async function applyAsync (args, taskOptions) {
+        if (!isSerializable(args)) throw new Error('arguments is not serializable')
+        if (FAKE_WORKER_MODE) {
+            // NOTE: it's just for test purposes
+            // similar to https://docs.celeryproject.org/en/3.1/configuration.html#celery-always-eager
+            // TODO(pahaz): think about test errors!?
+            const result = await executeTask(fn.name, createSerializableCopy([...arguments]))
+            return {
+                getState: async () => { return Promise.resolve('completed') },
+                awaitResult: async () => { return Promise.resolve(result) },
+            }
+        }
+        const job = await taskQueue.add(fn.name, { args: createSerializableCopy([...args]) }, {
+            ...globalTaskOptions,
+            ...defaultTaskOptions,
+            ...taskOptions,
+        })
+        console.log('delayed', fn.name, args, job.id)
+        return {
+            getState: async () => {
+                return await job.getState()
+            },
+            awaitResult: async () => {
+                return await awaitResult(job.id)
+            },
+        }
+    }
+
+    async function delay () {
+        return await applyAsync([...arguments])
+    }
+
+    function errorTaskCallPrevent () {
+        throw new Error('This function is converted to a task, and you need to use fn.delay(...) to call the tasks')
+    }
+
+    fn.delay = delay
+    fn.applyAsync = applyAsync
+    errorTaskCallPrevent.delay = delay
+    errorTaskCallPrevent.applyAsync = applyAsync
+    errorTaskCallPrevent._type = TASK_TYPE
+    delay.fn = fn
+    applyAsync.fn = fn
+    return errorTaskCallPrevent
+}
+
+function registerTasks (modulesList) {
+    modulesList.forEach(
+        (module) => {
+            Object.values(module).forEach(
+                (task) => {
+                    if (task._type !== TASK_TYPE) {
+                        console.warn(task)
+                        throw new Error('Wrong task module export format! What\'s this? You should wrap everything by createTask()')
+                    }
+                })
+        })
+}
+
+function createSerializableCopy (data) {
+    return JSON.parse(JSON.stringify(data))
+}
+
+function isSerializable (data) {
+    try {
+        JSON.stringify(data)
+        return true
+    } catch (e) {
+        return false
+    }
+}
+
+function executeTask (name, args, job = null) {
+    if (!TASKS.has(name)) throw new Error(`executeTask: Unknown task name ${name}`)
+    if (!isSerializable(args)) throw new Error('executeTask: args is not serializable')
+
+    const fn = TASKS.get(name)
+
+    return fn.apply(job, args)
+}
+
+function createWorker () {
+    taskQueue.process('*', WORKER_CONCURRENCY, async function (job) {
+        console.log(`Job:${job.id} status=processing (${JSON.stringify(job.toJSON())})`)
+        try {
+            return await executeTask(job.name, job.data.args, job)
+        } catch (e) {
+            console.error(e)
+            throw e
+        }
+    })
+
+    taskQueue.on('failed', function (job) {
+        console.log(`Job:${job.id} status=error (${JSON.stringify(job.toJSON())})`)
+    })
+
+    taskQueue.on('completed', function (job) {
+        console.log(`Job:${job.id} status=completed t0=${job.finishedOn - job.timestamp}ms t1=${job.processedOn - job.timestamp}ms t2=${job.finishedOn - job.processedOn} (${JSON.stringify(job.toJSON())})`)
+    })
+}
+
+module.exports = {
+    taskQueue,
+    createTask,
+    registerTasks,
+    createWorker,
+}
