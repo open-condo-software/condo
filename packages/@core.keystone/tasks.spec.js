9d5d7fb9862124f6625031729bb812efb1af5ba1
Pahaz <pahaz.white@gmail.com>
2021-04-20 01:10:21 +0300

    SBERDOMA-123 init some spec

---
+++ packages/@core.keystone/tasks.spec.js
@@ -0,0 +1,46 @@
+const { createTask, createWorker, taskQueue } = require('./tasks')
+
+async function asyncAddTask (a, b) {
+    return Promise.resolve(a + b)
+}
+
+afterAll(() => {
+    taskQueue.close()
+})
+
+describe('tasks', () => {
+    test('createTask result', () => {
+        const task = createTask('asyncAddTask1', asyncAddTask)
+        expect(task).toHaveProperty('delay')
+        expect(task).toHaveProperty('applyAsync')
+        expect(task._type).toEqual('TASK')
+        expect(task).toThrowError(/This function is converted to a task/)
+    })
+
+    test('createTask().delay result', async () => {
+        const task = createTask('asyncAddTask2', asyncAddTask)
+        createWorker()
+        const delayed = await task.delay(33, 44)
+        expect(delayed).toHaveProperty('getState')
+        expect(delayed).toHaveProperty('awaitResult')
+    })
+
+    test('awaitResult', async () => {
+        const task = createTask('asyncAddTask3', asyncAddTask)
+        createWorker()
+        const delayed = await task.delay(44, 45)
+        const result = await delayed.awaitResult()
+        expect(result).toEqual(44 + 45)
+    })
+
+    test('getState', async () => {
+        const task = createTask('asyncAddTask4', asyncAddTask)
+        createWorker()
+        const delayed = await task.delay(44, 45)
+        const state1 = await delayed.getState()
+        expect(state1).toEqual('waiting')
+        await delayed.awaitResult()
+        const state2 = await delayed.getState()
+        expect(state2).toEqual('completed')
+    })
+})
