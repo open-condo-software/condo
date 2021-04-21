e00bf8f3d13431018cd80258d81fd6491bfe4083
Pahaz <pahaz.white@gmail.com>
2021-04-21 03:27:21 +0300

    SBERDOMA-266 repair tests

--- apps/condo/domains/notification/access/Message.js
+++ apps/condo/domains/notification/access/Message.js
@@ -5,7 +5,9 @@
 async function canReadMessages ({ authentication: { item: user } }) {
     if (!user) return false
     if (user.isAdmin) return true
-    return false
+    return {
+        user: { id: user.id },
+    }
 }
 
 async function canManageMessages ({ authentication: { item: user }, operation }) {
