dbc6ff6b2e572eff9b2159d802453fa4701c9075
Антон Алёшин <Anton_Aleschin@mail.ru>
2021-04-21 19:22:55 +0300

    SBERDOMA-151 Editing of selected text in AddressSearchInput
    `Select` component from Ant displays either `searchText` or selected `Option`,
    which are two DOM elements on the same position. It's not supposed by Ant to display both of them.
    Once `Option` is selected, `searchText` (typed by hands) will be wiped out.
    Out of the box, it's impossible to continue editing what user has selected,
    because `Option` is not editable, it's selectable.
    To make it possible from visual point of view, we need to:
    1. Set `searchText` to text of selected `Option` to let user to reuse selected Option's text and edit it
    2. Clear `value`, that causes selected Option to disappear

--- apps/condo/domains/common/components/AddressSearchInput.tsx
+++ apps/condo/domains/common/components/AddressSearchInput.tsx
@@ -43,41 +43,69 @@ async function searchAddress (query) {
 }
 
 export const AddressSearchInput: React.FC = (props) => {
-    const [selected, setSelected] = useState('')
+    const [value, setValue] = useState()
+    const [searchValue, setSearchValue] = useState('')
     const [fetching, setFetching] = useState(false)
     const [data, setData] = useState([])
     const options = useMemo(() => {
         return data.map(d => <Select.Option key={d.value} value={d.value} title={d.text}>{d.text}</Select.Option>)
     }, [data])
 
-    const searchSuggestions = useCallback(async (value) => {
+    const searchSuggestions = useCallback(async (term) => {
         setFetching(true)
-        const data = await searchAddress((selected) ? selected + ' ' + value : value)
+        const data = await searchAddress(term)
 
         setFetching(false)
         setData(data)
     }, [searchAddress])
 
-    const debouncedSearch = useMemo(() => {
+    const debouncedSearchSuggestions = useMemo(() => {
         return debounce(searchSuggestions, DEBOUNCE_TIMEOUT)
     }, [searchSuggestions])
 
-    const handleSelect = useCallback((value, option) => {
-        setSelected(option.children)
+    const handleOnSearch = (term) => {
+        setSearchValue(term)
+        debouncedSearchSuggestions(term)
+    }
+
+    const handleSelect = useCallback((val, option) => {
+        setValue(option.children)
     }, [])
 
     const handleClear = useCallback(() => {
-        setSelected('')
+        clearSelection()
     }, [])
+
+    const handleKeyDown = (e: KeyboardEvent) => {
+        // `Select` component from Ant displays either `searchText` or selected `Option`,
+        // which are two DOM elements on the same position. It's not supposed by Ant to display both of them.
+        // Once `Option` is selected, `searchText` (typed by hands) will be wiped out.
+        // Out of the box, it's impossible to continue editing what user has selected,
+        // because `Option` is not editable, it's selectable.
+        // To make it possible from visual point of view, we need to:
+        // 1. Set `searchText` to text of selected `Option` to let user to reuse selected Option's text and edit it
+        // 2. Clear `value`, that causes selected Option to disappear
+        if (e.key === 'Backspace' && value) {
+            setSearchValue(value)
+            clearSelection()
+        }
+    }
+
+    const clearSelection = () => {
+        setValue(null)
+    }
 
     return (
         <Select
             showSearch
+            value={value}
+            searchValue={searchValue}
             autoClearSearchValue={false}
             allowClear={true}
             defaultActiveFirstOption={false}
-            onSearch={debouncedSearch}
+            onSearch={handleOnSearch}
             onSelect={handleSelect}
+            onKeyDown={handleKeyDown}
             onClear={handleClear}
             loading={fetching}
             notFoundContent={fetching ? <Spin size="small" /> : null}
