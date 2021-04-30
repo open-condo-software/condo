adf2a0cf248b30a5fcd64f20cc57022743670186
Антон Алёшин <Anton_Aleschin@mail.ru>
2021-04-30 21:24:01 +0300

    SBERDOMA-102 fix wrong return type in canMangeRoles
    `GraphQLWhere` type cannot be used in case of `create` operation, because we will get an error:
    > Expected a Boolean for OrganizationEmployeeRole.access.create(), but got Object
    
    In https://www.keystonejs.com/api/access-control#list-level-access-control it states:
    > For `create` operations, an `AccessDeniedError` is returned if the operation is set to / returns `false`
    
    Actually, in case for create we are repeating the same logic, as declared for another operations, but using queries to data storage.

--- apps/condo/access.js
+++ apps/condo/access.js
@@ -75,12 +75,31 @@ const rules = {
         if (user.isAdmin) return {}
         return {}
     },
-    canManageRoles: ({ authentication: { item: user } }) => {
+    canManageRoles: async ({ authentication: { item: user }, operation, originalInput }) => {
         if (!user) return false
-        if (user.isAdmin) return {}
-        return {
-            // user is inside employee list
-            organization: { employees_some: { user: { id: user.id }, role: { canManageRoles: true } } },
+        if (operation === 'create') {
+            // `GraphQLWhere` type cannot be used in case of `create` operation,
+            // because we will get an error:
+            // > Expected a Boolean for OrganizationEmployeeRole.access.create(), but got Object
+            // In https://www.keystonejs.com/api/access-control#list-level-access-control it states:
+            // > For `create` operations, an `AccessDeniedError` is returned if the operation is set to / returns `false`
+            // Actually, here we repeating the same logic, as declared for another operations
+            if (user.isAdmin) return true
+            const employeeForUser = await getByCondition('OrganizationEmployee', {
+                organization: { id: originalInput.organization },
+                user: { id: user.id },
+            })
+            const employeeRole = await getByCondition('OrganizationEmployeeRole', {
+                id: employeeForUser.role,
+            })
+            if (!employeeRole) return false
+            return employeeRole.canManageRoles
+        } else {
+            if (user.isAdmin) return {}
+            return {
+                // user is inside employee list
+                organization: { employees_some: { user: { id: user.id }, role: { canManageRoles: true } } },
+            }
         }
     },
     canRegisterNewOrganization: isSignedIn,
