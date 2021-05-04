ab9fa5db4634e439acdd3e038b45e0290a1978ca
Pahaz <pahaz.white@gmail.com>
2021-05-04 14:33:01 +0300

    SBERDOMA-266 run skipped tests

--- packages/@core.keystone/tasks.spec.js
+++ packages/@core.keystone/tasks.spec.js
@@ -4,8 +4,12 @@ async function asyncAddTask (a, b) {
     return Promise.resolve(a + b)
 }
 
+beforeAll(async () => {
+    return createWorker()
+})
+
 afterAll(() => {
-    taskQueue.close()
+    return taskQueue.close()
 })
 
 describe('tasks', () => {
@@ -19,7 +23,6 @@ describe('tasks', () => {
 
     test('createTask().delay result', async () => {
         const task = createTask('asyncAddTask2', asyncAddTask)
-        createWorker()
         const delayed = await task.delay(33, 44)
         expect(delayed).toHaveProperty('getState')
         expect(delayed).toHaveProperty('awaitResult')
@@ -27,7 +30,6 @@ describe('tasks', () => {
 
     test('awaitResult', async () => {
         const task = createTask('asyncAddTask3', asyncAddTask)
-        createWorker()
         const delayed = await task.delay(44, 45)
         const result = await delayed.awaitResult()
         expect(result).toEqual(44 + 45)
@@ -35,7 +37,6 @@ describe('tasks', () => {
 
     test('getState', async () => {
         const task = createTask('asyncAddTask4', asyncAddTask)
-        createWorker()
         const delayed = await task.delay(44, 45)
         const state1 = await delayed.getState()
         expect(state1).toEqual('waiting')
@@ -46,7 +47,6 @@ describe('tasks', () => {
 
     test('createTask().applyAsync result', async () => {
         const task = createTask('asyncAddTask5', asyncAddTask)
-        createWorker()
         const delayed = await task.applyAsync([33, 44], { attempts: 2, backoff: true })
         expect(delayed).toHaveProperty('getState')
         expect(delayed).toHaveProperty('awaitResult')
