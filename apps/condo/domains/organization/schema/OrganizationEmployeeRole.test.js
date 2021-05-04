d88e75506db0726b733e46829de01a311b0d6ad3
Антон Алёшин <Anton_Aleschin@mail.ru>
2021-05-04 20:00:06 +0300

    SBERDOMA-102 tests for `canManageRoles`
    user: no access to create role without granted `canMangeRoles`
    user: can create role when granted `canMangeRoles`

--- apps/condo/domains/organization/schema/OrganizationEmployeeRole.test.js
+++ apps/condo/domains/organization/schema/OrganizationEmployeeRole.test.js
@@ -1,10 +1,10 @@
 const faker = require('faker')
+const _ = require('lodash')
 const {makeClientWithNewRegisteredAndLoggedInUser} = require('@condo/domains/user/utils/testSchema');
 const { getRandomString } = require('@core/keystone/test.utils')
-const { createTestOrganizationEmployeeRole } = require('../utils/testSchema')
+const { createTestOrganizationEmployeeRole, OrganizationEmployee, OrganizationEmployeeRole } = require('../utils/testSchema')
 const { makeLoggedInAdminClient } = require('@core/keystone/test.utils')
 const { makeClientWithRegisteredOrganization } = require('../../../utils/testSchema/Organization')
-const { OrganizationEmployee } = require('../gql')
 
 describe('OrganizationEmployeeRole', () => {
     test('user: no access to update employee without granted `canManageEmployees`', async () => {
@@ -72,4 +72,74 @@ describe('OrganizationEmployeeRole', () => {
         expect(result.errors).toBeUndefined()
         expect(result.data.obj).toMatchObject(attrs)
     })
+
+    test('user: no access to create role without granted `canMangeRoles`', async () => {
+        const organizationAdminClient = await makeClientWithRegisteredOrganization()
+        const userClient = await makeClientWithNewRegisteredAndLoggedInUser()
+        const adminClient = await makeLoggedInAdminClient()
+
+        const [role] = await createTestOrganizationEmployeeRole(adminClient, {
+            name: 'Role1',
+            canManageRoles: false,
+            organization: { connect: { id: organizationAdminClient.organization.id } },
+        })
+
+        const { data: { obj: employee } } = await OrganizationEmployee.create(adminClient, {
+            dv: 1,
+            sender: { dv: 1, fingerprint: getRandomString() },
+            name: faker.name.firstName(),
+            role: { connect: { id: role.id } },
+            user: { connect: { id: userClient.user.id } },
+            organization: { connect: { id: organizationAdminClient.organization.id } },
+        }, { raw: true })
+
+        const attrs = {
+            dv: 1,
+            sender: { dv: 1, fingerprint: getRandomString() },
+            name: 'Role2',
+            organization: { connect: { id: organizationAdminClient.organization.id } },
+        }
+
+        const { errors } = await OrganizationEmployeeRole.create(userClient, attrs, { raw: true })
+        expect(errors[0]).toMatchObject({
+            'data': { 'target': 'createOrganizationEmployeeRole', 'type': 'mutation' },
+            'message': 'You do not have access to this resource',
+            'name': 'AccessDeniedError',
+            'path': ['obj'],
+        })
+    })
+
+    test('user: can create role when granted `canMangeRoles`', async () => {
+        const organizationAdminClient = await makeClientWithRegisteredOrganization()
+        const userClient = await makeClientWithNewRegisteredAndLoggedInUser()
+        const adminClient = await makeLoggedInAdminClient()
+
+        const [role] = await createTestOrganizationEmployeeRole(adminClient, {
+            name: 'Role1',
+            canManageRoles: true,
+            organization: { connect: { id: organizationAdminClient.organization.id } },
+        })
+
+        const { data: { obj: employee } } = await OrganizationEmployee.create(adminClient, {
+            dv: 1,
+            sender: { dv: 1, fingerprint: getRandomString() },
+            name: faker.name.firstName(),
+            role: { connect: { id: role.id } },
+            user: { connect: { id: userClient.user.id } },
+            organization: { connect: { id: organizationAdminClient.organization.id } },
+        }, { raw: true })
+
+        expect(employee).toBeDefined()
+
+        const attrs = {
+            dv: 1,
+            sender: { dv: 1, fingerprint: getRandomString() },
+            name: 'Role2',
+            organization: { connect: { id: organizationAdminClient.organization.id } },
+        }
+
+        const { data, errors } = await OrganizationEmployeeRole.create(userClient, attrs, { raw: true })
+        expect(data.obj).toMatchObject(_.omit(attrs, 'organization'))
+        expect(errors).toBeUndefined()
+    });
 })
