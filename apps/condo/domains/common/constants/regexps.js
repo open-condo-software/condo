668eb7e39a20ad878944906a5c60dce315ca3c74
Pahaz <pahaz.white@gmail.com>
2021-05-04 03:33:03 +0300

    SBERDOMA-266 add ALPHANUMERIC_REGEXP

--- apps/condo/domains/common/constants/regexps.js
+++ apps/condo/domains/common/constants/regexps.js
@@ -1,8 +1,10 @@
+const ALPHANUMERIC_REGEXP = /^[a-zA-Z0-9_]+$/
 const UPPER_CASE_ALPHANUMERIC_REGEXP = /^[A-Z0-9_]+$/
 const LETTERS_AND_NUMBERS = /[\p{L}\p{N}]/gu
 const PHONE = /^\+?\d*(\.\d*)?$/
 
 module.exports = {
+    ALPHANUMERIC_REGEXP,
     UPPER_CASE_ALPHANUMERIC_REGEXP,
     LETTERS_AND_NUMBERS,
     PHONE,
