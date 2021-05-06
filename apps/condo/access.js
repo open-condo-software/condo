aab9005e459dc61a7da0dfeac324bdb946068c48
Антон Алёшин <Anton_Aleschin@mail.ru>
2021-05-06 16:42:11 +0300

    SBERDOMA-102 refactor moved `can…Employees` to domains/organization/access/Employee

--- apps/condo/access.js
+++ apps/condo/access.js
@@ -31,36 +31,6 @@ const rules = {
       Organization
 
     */
-    canReadEmployees: ({ authentication: { item: user } }) => {
-        if (!user) return false
-        if (user.isAdmin) return {}
-        return {
-            // user is inside employee list
-            organization: { employees_some: { user: { id: user.id } } },
-        }
-    },
-    canManageEmployees: async (args) => {
-        const { authentication: { item: user }, operation, itemId } = args
-        if (!user) return false
-        if (user.isAdmin) return true
-        if (operation === 'update' || operation === 'delete') {
-            if (!itemId) return false
-            const employeeToEdit = await getById('OrganizationEmployee', itemId)
-            if (!employeeToEdit || !employeeToEdit.organization) return false
-            const employeeForUser = await getByCondition('OrganizationEmployee', {
-                organization: { id: employeeToEdit.organization },
-                user: { id: user.id },
-            })
-            if (!employeeForUser || !employeeForUser.role) return false
-            const employeeRole = await getByCondition('OrganizationEmployeeRole', {
-                id: employeeForUser.role,
-                organization: { id: employeeToEdit.organization },
-            })
-            if (!employeeRole) return false
-            return employeeRole.canManageEmployees
-        }
-        return false
-    },
     canReadRoles: ({ authentication: { item: user } }) => {
         if (!user) return false
         if (user.isAdmin) return {}
