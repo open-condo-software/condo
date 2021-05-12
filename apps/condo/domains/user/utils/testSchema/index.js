0b699ae0fe378aabf0c3253330ba43664b9007fa
Pahaz <pahaz.white@gmail.com>
2021-05-12 22:18:21 +0300

    SBERDOMA-294 hotfix: CI tests

--- apps/condo/domains/user/utils/testSchema/index.js
+++ apps/condo/domains/user/utils/testSchema/index.js
@@ -15,7 +15,7 @@ const User = generateGQLTestUtils(UserGQL)
 const UserAdmin = generateGQLTestUtils(UserAdminGQL)
 
 const createTestEmail = () => ('test.' + getRandomString() + '@example.com').toLowerCase()
-const createTestPhone = () => '+1' + String(Math.random()).slice(2).slice(-9)
+const createTestPhone = () => '+1' + String(Math.random()).slice(2).slice(-10)
 
 async function createTestUser (client, extraAttrs = {}) {
     if (!client) throw new Error('no client')
