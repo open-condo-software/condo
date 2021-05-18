a5c5ea32e768149b2e8b3dc78e4b949c4269dd99
Dmitry Danev <danewdima@gmail.com>
2021-05-18 16:15:27 +0300

    SBERDOMA-311 add user password reset from profile

---
+++ apps/condo/domains/common/utils/timer.ts
@@ -0,0 +1,39 @@
+// refs to: https://www.w3schools.com/howto/howto_js_countdown.asp
+interface ITimerArguments {
+    onUpdate: (progress: number) => void
+    onStart?: (startDate: Date) => void
+    onFinish?: () => void
+    duration: number // s
+    step?: number // ms
+}
+
+export const timer = (args: ITimerArguments) => {
+    const {
+        onStart,
+        onUpdate,
+        onFinish,
+        duration,
+        step = 1000,
+    } = args
+
+    const nextDate = new Date()
+    nextDate.setSeconds(nextDate.getSeconds() + duration)
+
+    if (onStart) {
+        onStart(nextDate)
+    }
+
+    const countDownDate = new Date(nextDate).getTime()
+
+    const update = setInterval(() => {
+        const now = new Date().getTime()
+        const distance = countDownDate - now
+
+        onUpdate(Math.ceil((distance % (1000 * 60)) / 1000))
+
+        if (distance < 0) {
+            clearInterval(update)
+            onFinish()
+        }
+    }, step)
+}
