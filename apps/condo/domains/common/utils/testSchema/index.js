ab64156f1cad294f3d942080ec175190bbba79b3
Антон Алёшин <Anton_Aleschin@mail.ru>
2021-05-20 08:46:52 +0300

    SBERDOMA-335 test utils for catching errors in a right way 👌
    catchErrorFrom – Implements correct expecting of GraphQLError, thrown by Keystone.
    expectToThrowAccessDeniedErrorToObj – Expects a GraphQLError of type 'AccessDeniedError', thrown by Keystone on access to a single schema object
    expectToThrowAccessDeniedErrorToObjects – Expects a GraphQLError of type 'AccessDeniedError', thrown by Keystone on access to a collection of schema objects.

---
+++ apps/condo/domains/common/utils/testSchema/index.js
@@ -0,0 +1,91 @@
+/**
+ * Callback function to examine GraphQLError, thrown by Keystone
+ * @callback ErrorInspectionCallback
+ * @param {GraphQLError} error
+ */
+
+/**
+ * Function, expected to throw an error
+ * @callback TestFunc
+ */
+
+/**
+ * Implements correct expecting of GraphQLError, thrown by Keystone.
+ * Expectation checks inside of `catch` are not covering a case,
+ * when no exception is thrown, — test will pass, but should fail.
+ * https://stackoverflow.com/questions/48707111/asserting-against-thrown-error-objects-in-jest
+ *
+ * @example
+ * catchErrorFrom(async () => {
+ *     await doSomethingThatShouldThrowAnError()
+ * }, (e) => {
+ *     // any `expect` checks for catched error
+ * })
+ *
+ *
+ * @param {TestFunc} testFunc - Function, expected to throw an error
+ * @param {ErrorInspectionCallback} inspect - Function, that should inspect the error in details
+ * @return {Promise<*>}
+ */
+export const catchErrorFrom = async (testFunc, inspect) => {
+    let thrownError
+    try {
+        await testFunc()
+    } catch (e) {
+        thrownError = e
+    }
+    expect(thrownError).toBeDefined()
+    return inspect(thrownError)
+}
+
+/**
+ * Expects a GraphQLError of type 'AccessDeniedError', thrown by Keystone on access to a single schema object.
+ * Should be used to examine access to operation of GraphQL utility wrapper, that returns `obj`.
+ * @example
+ *
+ * test('something, that should throw an error', async () => {
+ *     const userClient = await makeClientWithNewRegisteredAndLoggedInUser()
+ *     await expectToThrowAccessDeniedErrorToObj(async () => {
+ *         await createTestOrganization(userClient)
+ *     })
+ * })
+ *
+ * @param {TestFunc} testFunc - Function, expected to throw an error
+ * @return {Promise<void>}
+ */
+export const expectToThrowAccessDeniedErrorToObj = async (testFunc) => {
+    catchErrorFrom(testFunc, ({errors, data}) => {
+        expect(errors[0]).toMatchObject({
+            'message': 'You do not have access to this resource',
+            'name': 'AccessDeniedError',
+            'path': ['obj'],
+        })
+        expect(data).toEqual({ 'obj': null })
+    })
+}
+
+/**
+ * Expects a GraphQLError of type 'AccessDeniedError', thrown by Keystone on access to a collection of schema objects.
+ * Should be used to examine access to `getAll` GraphQL utility wrapper, that returns `objs`.
+ * @example
+ *
+ * test('something, that should throw an error', async () => {
+ *     const client = await makeClient()
+ *     await expectToThrowAccessDeniedErrorToObj(async () => {
+ *         await Organization.getAll(client)
+ *     })
+ * })
+ *
+ * @param {TestFunc} testFunc - Function, expected to throw an error
+ * @return {Promise<void>}
+ */
+export const expectToThrowAccessDeniedErrorToObjects = async (testFunc) => {
+    catchErrorFrom(testFunc, ({errors, data}) => {
+        expect(errors[0]).toMatchObject({
+            'message': 'You do not have access to this resource',
+            'name': 'AccessDeniedError',
+            'path': ['objs'],
+        })
+        expect(data).toEqual({ 'objs': null })
+    })
+}
