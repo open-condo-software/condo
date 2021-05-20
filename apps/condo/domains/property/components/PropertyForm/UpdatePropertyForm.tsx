306ddd2630e32a4d2d44b5801b61d63ad040aa00
Dmitry Koviazin <dkoviazin@gmail.com>
2021-05-20 16:44:40 +0500

    SBERDOMA-52 confirm on property delete

--- apps/condo/domains/property/components/PropertyForm/UpdatePropertyForm.tsx
+++ apps/condo/domains/property/components/PropertyForm/UpdatePropertyForm.tsx
@@ -1,5 +1,5 @@
 import { Col, Form, Row, Space, Typography } from 'antd'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
 import { useIntl } from '@core/next/intl'
 import BasePropertyForm from '../BasePropertyForm'
@@ -9,6 +9,7 @@ import { Property } from '@condo/domains/property/utils/clientSchema'
 import { useOrganization } from '@core/next/organization'
 import { FormResetButton } from '@condo/domains/common/components/FormResetButton'
 import { runMutation } from '@condo/domains/common/utils/mutations.utils'
+import Modal from 'antd/lib/modal/Modal'
 
 interface IUpdatePropertyForm {
     id: string
@@ -16,20 +17,27 @@ interface IUpdatePropertyForm {
 
 
 export const UpdatePropertyForm: React.FC<IUpdatePropertyForm> = ({ id }) => {
-
     const intl = useIntl()
     const ApplyChangesLabel = intl.formatMessage({ id: 'ApplyChanges' })
     const LoadingMessage = intl.formatMessage({ id: 'Loading' })
     const DeletePropertyLabel = intl.formatMessage({ id: 'pages.condo.property.form.DeleteLabel' })
-    
+    const ConfirmDeleteTitle = intl.formatMessage({ id: 'pages.condo.property.form.ConfirmDeleteTitle' })
+    const ConfirmDeleteMessage = intl.formatMessage({ id: 'pages.condo.property.form.ConfirmDeleteMessage' })
     const { push } = useRouter()
     const { organization } = useOrganization()
     const { refetch, obj: property, loading, error } = Property.useObject({ where: { id } })
-
     const initialValues = Property.convertToUIFormState(property)
     const action = Property.useUpdate({}, (property) => push(`/property/${property.id}`))
     const updateAction = (value) => action(value, property)
     const softDeleteAction = Property.useSoftDelete({}, () => push('/property/'))
+  
+    const [isConfirmVisible, setIsConfirmVisible] = useState(false)
+    const showConfirm = () => setIsConfirmVisible(true)
+    const handleOk = () => {
+        setIsConfirmVisible(false)
+        handleDelete({ item: property })
+    }
+    const handleCancel = () => setIsConfirmVisible(false)
 
     
     function handleDelete ({ item }) {
@@ -98,9 +106,32 @@ export const UpdatePropertyForm: React.FC<IUpdatePropertyForm> = ({ id }) => {
                                                 </Space>
                                             </Col>
                                             <Col flex={0}>
+                                                <Modal 
+                                                    title={
+                                                        <Typography.Title style={{ fontSize: '24px', lineHeight: '32px' }}>
+                                                            {ConfirmDeleteTitle}
+                                                        </Typography.Title>
+                                                    }
+                                                    visible={isConfirmVisible} 
+                                                    onCancel={handleCancel}
+                                                    footer={[
+                                                        <Button 
+                                                            key="submit" 
+                                                            type='sberDanger' 
+                                                            onClick={handleOk}
+                                                            style={{ margin: '15px' }}
+                                                        >
+                                                            {DeletePropertyLabel}
+                                                        </Button>,
+                                                    ]}                                                    
+                                                >
+                                                    <Typography.Text>
+                                                        {ConfirmDeleteMessage}
+                                                    </Typography.Text>
+                                                </Modal>
                                                 <Button
                                                     key='submit'
-                                                    onClick={() => handleDelete({ item: property })}
+                                                    onClick={showConfirm}
                                                     type='sberDanger'
                                                     loading={isLoading}
                                                     secondary
