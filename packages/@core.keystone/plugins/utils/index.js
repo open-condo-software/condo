f52fab16bd08dd610fe116cf91fad1d03b710c80
Pahaz <pahaz.white@gmail.com>
2021-05-25 23:01:09 +0500

    SBERDOMA-121 fix plugin composition access check

--- packages/@core.keystone/plugins/utils/index.js
+++ packages/@core.keystone/plugins/utils/index.js
@@ -1,4 +1,6 @@
 const { HiddenRelationship } = require('./HiddenRelationship')
+const { getType } = require('@keystonejs/utils')
+const { get } = require('lodash')
 
 const composeHook = (originalHook, newHook) => async params => {
     let { resolvedData } = params
@@ -12,8 +14,29 @@ function isValidDate (date) {
     return date && Object.prototype.toString.call(date) === '[object Date]' && !isNaN(date)
 }
 
+async function evaluateKeystoneAccessResult (access, operation, args, keystone = null) {
+    const type = getType(access)
+    if (type === 'Boolean') {
+        return access || false
+    } else if (type === 'Function') {
+        return access(args) || false
+    } else if (type === 'AsyncFunction') {
+        return await access(args) || false
+    } else if (type === 'Object') {
+        const innerAccess = access[operation]
+        if (innerAccess) return await evaluateKeystoneAccessResult(innerAccess, operation, args, keystone)
+        return get(keystone, ['defaultAccess', 'list'], false)
+    } else if (type === 'Undefined') {
+        return get(keystone, ['defaultAccess', 'list'], false)
+    }
+    throw new Error(
+        `evaluateKeystoneAccessResult(), received ${type}.`,
+    )
+}
+
 module.exports = {
     HiddenRelationship,
     composeHook,
     isValidDate,
+    evaluateKeystoneAccessResult,
 }
