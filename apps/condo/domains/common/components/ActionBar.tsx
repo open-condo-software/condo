87fa9b51efd21b2ae6aa5f23b06eec11af9c3bf7
Dmitry Koviazin <dkoviazin@gmail.com>
2021-06-02 20:51:12 +0500

    SBERDOMA-340 remove styles from global

--- apps/condo/domains/common/components/ActionBar.tsx
+++ apps/condo/domains/common/components/ActionBar.tsx
@@ -1,10 +1,25 @@
+/** @jsx jsx */
+
 import { Affix } from 'antd'
 import React from 'react'
+import { css, jsx } from '@emotion/core'
 
+
+const actionBar = css`
+    padding: 24px;
+    margin: 0 -24px;
+    transition: box-shadow 0.3s ease-out;
+
+    .ant-affix & {
+        background: rgba(255,255,255,0.9);
+        box-shadow: 0px 9px 28px 8px rgba(0, 0, 0, 0.05), 0px 6px 16px rgba(0, 0, 0, 0.08), 0px 3px 6px -4px rgba(0, 0, 0, 0.12);
+        border-radius: 8px;
+    }  
+`
 const ActionBar: React.FC = ({ children }) => {
     return (
         <Affix offsetBottom={24}>
-            <div className={'floatingActionBar'}>
+            <div css={actionBar}>
                 { children }
             </div>
         </Affix>
