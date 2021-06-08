e44c983b283500ba6ec5d83e73acce090cf91c50
Pahaz <pahaz.white@gmail.com>
2021-06-08 10:20:33 +0300

    SBERDOMA-427 access error if GRAPHQL_PLAYGROUND is disabled

--- apps/condo/next.config.js
+++ apps/condo/next.config.js
@@ -10,9 +10,10 @@ const withTM = require('next-transpile-modules')(['@core/next', '@core/keystone'
 const serverUrl = process.env.SERVER_URL || 'http://localhost:3000'
 const apolloGraphQLUrl = `${serverUrl}/admin/api`
 const firebaseConfig = conf['FIREBASE_CONFIG'] && JSON.parse(conf['FIREBASE_CONFIG'])
-const plerdy = conf['BEHAVIOR_RECORDER_PLERDY_CONFIG']
 const addressSuggestionsConfig = conf['ADDRESS_SUGGESTIONS_CONFIG'] && JSON.parse(conf['ADDRESS_SUGGESTIONS_CONFIG'])
 const mapApiKey = conf['MAP_API_KEY']
+const behaviorRecorder = { 'plerdy': conf['BEHAVIOR_RECORDER_PLERDY_CONFIG'] }
+const docsConfig = { 'isGraphqlPlaygroundEnabled': conf['ENABLE_DANGEROUS_GRAPHQL_PLAYGROUND'] === 'true' }
 
 module.exports = withTM(withLess(withCSS({
     publicRuntimeConfig: {
@@ -22,9 +23,8 @@ module.exports = withTM(withLess(withCSS({
         firebaseConfig,
         addressSuggestionsConfig,
         mapApiKey,
-        behaviorRecorder: {
-            plerdy,
-        },
+        behaviorRecorder,
+        docsConfig,
     },
     lessLoaderOptions: {
         javascriptEnabled: true,
