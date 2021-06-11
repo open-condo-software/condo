fadc88e4199d886f71bb6c6ff9221776034e12ae
Dmitry Koviazin <dkoviazin@gmail.com>
2021-06-12 03:29:39 +0500

    SBERDOMA-436 files protection

--- apps/condo/domains/common/utils/sberCloudFileAdapter.js
+++ apps/condo/domains/common/utils/sberCloudFileAdapter.js
@@ -1,9 +1,52 @@
 const ObsClient = require('esdk-obs-nodejs')
 const path = require('path')
 const { SERVER_URL } = require('@core/config')
-const { runCustomQuery } = require('@keystonejs/server-side-graphql-client')
-const has = require('lodash/has')
-const { gql } = require('graphql-tag')
+const { getItem } = require('@keystonejs/server-side-graphql-client')
+const { isEmpty } = require('lodash')
+
+const conf = require('@core/config')
+class SberCloudObsAcl {
+
+    constructor (config) {
+        this.bucket = config.bucket
+        this.s3 = new ObsClient(config.s3Options)
+        this.server = config.s3Options.server
+    }
+
+    async getMeta (filename) {
+        const result  = await this.s3.getObjectMetadata({
+            Bucket: this.bucket,
+            Key: filename,
+        })
+        if (result.CommonMsg.Status < 300) {
+            return result.InterfaceResult.Metadata
+        } else {
+            return {}
+        }
+    }
+
+    async setMeta (filename, newMeta = {} ) {
+        const result = await this.s3.setObjectMetadata({
+            Bucket: this.bucket,
+            Key: filename,
+            Metadata: newMeta,
+            MetadataDirective: 'REPLACE_NEW',
+        })
+        const  { CommonMsg: { Status } } = result 
+        return Status < 300
+    }
+    // createSignedUrlSync is executed without request to obs, so there is no need to cache result
+    generateUrl (filename, ttl = 300) { // obs default
+        const { SignedUrl } = this.s3.createSignedUrlSync({
+            Method: 'GET',
+            Bucket : this.bucket, 
+            Key : filename,
+            Expires: ttl,
+        })
+        return SignedUrl
+    }
+
+}
 
 class SberCloudFileAdapter {
 
@@ -12,6 +55,7 @@ class SberCloudFileAdapter {
         this.s3 = new ObsClient(config.s3Options)
         this.server = config.s3Options.server
         this.folder = config.folder
+        this.acl = new SberCloudObsAcl(config)
     }
 
     save ({ stream, filename, id, mimetype, encoding }) {
@@ -65,45 +109,48 @@ class SberCloudFileAdapter {
         return `${SERVER_URL}/api/files/${this.folder}/${filename}`
     }
 
-    uploadParams ({ id }) {
+    uploadParams () {
         return { 
-            Metadata: { 
-                keystone_id: `${id}`,
-            },
+            Metadata: {},
         }
     }
 }
 
-const obsRouterHandler = (keystone) => async function (req, res, next) {
-    const file = req.params.file
-    // TODO(zuch) - move this to file meta or make constants
-    const translate = {
-        'ticket': 'TicketFiles',
-    }
-    if (!req.user) {
-        return res.status(403).render()
-    }
-    const [ folder, name, _ ] = file.split(/[/.]/g)
-    if (!has(translate, folder)) {
-        return res.end('public file')
-    }
-    const { id: userId } = req.user
-    const context = await keystone.createContext({ authentication: { item: { id: userId }, listKey: 'User' } })
-    
-    console.log(context.gqlNames('TicketFile'))
+const obsRouterHandler = ({ keystone }) => {
 
-    const { itemQueryName, listQueryName } = context.gqlNames('TicketFile')
+    const obsConfig = conf.SBERCLOUD_OBS_CONFIG ? JSON.parse(conf.SBERCLOUD_OBS_CONFIG) : {}
+    const Acl = new SberCloudObsAcl(obsConfig)
 
-    try {
-        const query = gql`query ($file: String!) { ${listQueryName}(where: { file: $file }) { id organization }  }`
-        const result = await runCustomQuery({ keystone, query, variables: { file: file }, context })
-        console.log('result 1 ', result)
-    } catch (error) {
-        console.log('1 ', error)
+    return async function (req, res, next) {
+        if (!req.user) {
+            res.sendStatus(403)
+            return res.end()
+        }
+        const meta = await Acl.getMeta(req.params.file)
+        if (isEmpty(meta)) {
+            res.sendStatus(404)
+            return next()
+        }
+        const { id: itemId, listkey: listKey } = meta
+        if (isEmpty(itemId) || isEmpty(listKey)) {
+            res.sendStatus(404)
+            return next()
+        }
+        const { id: userId } = req.user
+        const context = await keystone.createContext({ authentication: { item: { id: userId }, listKey: 'User' } })
+        const fileAfterAccessCheck = await getItem({
+            keystone,
+            listKey,
+            itemId,
+            context,
+        })
+        if (!fileAfterAccessCheck) {
+            res.sendStatus(403)
+            return res.end()
+        }
+        const url = Acl.generateUrl(req.params.file)
+        return res.redirect(url)        
     }
-
-    
-    res.end(file)
 }
 
 
