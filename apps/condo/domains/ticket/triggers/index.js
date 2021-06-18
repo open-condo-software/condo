a0980cc1efeedd904b3bb19b60e890936091008a
Dmitry Danev <danewdima@gmail.com>
2021-06-18 08:19:43 +0300

    SBERDOMA-419 fix triggers export

--- apps/condo/domains/ticket/triggers/index.js
+++ apps/condo/domains/ticket/triggers/index.js
@@ -1,3 +1,4 @@
 const { setInitialStatusTrigger } = require('./setInitialStatusTrigger')
+const { responsibleStatusTrigger } = require('./responsibleStatusTrigger')
 
-module.exports = { setInitialStatusTrigger }
+module.exports = { setInitialStatusTrigger, responsibleStatusTrigger }
