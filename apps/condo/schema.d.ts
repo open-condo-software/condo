458ea81e43d0c891a015821d416df3b260984ca5
Dmitry Koviazin <dkoviazin@gmail.com>
2021-06-20 17:13:30 +0500

    SBERDOMA-441 Fix for organization field

--- apps/condo/schema.d.ts
+++ apps/condo/schema.d.ts
@@ -14677,7 +14677,7 @@ export type TicketClassifier = {
   dv?: Maybe<Scalars['Int']>;
   /**  Client-side devise identification used for the anti-fraud detection. Example `{ dv: '1', fingerprint: 'VaxSw2aXZa'}`. Where the `fingerprint` should be the same for the same devices and it's not linked to the user ID. It's the device ID like browser / mobile application / remote system  */
   sender?: Maybe<Scalars['JSON']>;
-  /**  Multi level classification support  */
+  /**  Ref to the organization. If this ref is null the object is common for all organizations  */
   organization?: Maybe<Organization>;
   /**  Multi level name  */
   fullName?: Maybe<Scalars['String']>;
