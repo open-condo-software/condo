3001b636c9cb62b0bcef3907778b1180a9158db2
Pahaz <pahaz.white@gmail.com>
2021-06-24 03:11:16 +0500

    SBERDOMA-478 add sender validation hook

--- apps/condo/domains/common/schema/fields.js
+++ apps/condo/domains/common/schema/fields.js
@@ -1,3 +1,4 @@
+const { hasValidJsonStructure } = require('@condo/domains/common/utils/validation.utils')
 const { Integer } = require('@keystonejs/fields')
 const { Json } = require('@core/keystone/fields')
 
@@ -11,11 +12,22 @@ const DV_FIELD = {
 const SENDER_FIELD = {
     type: Json,
     schemaDoc: 'Client-side devise identification used for the anti-fraud detection. ' +
-        'Example `{ dv: \'1\', fingerprint: \'VaxSw2aXZa\'}`. ' +
+        'Example `{ dv: 1, fingerprint: \'VaxSw2aXZa\'}`. ' +
         'Where the `fingerprint` should be the same for the same devices and it\'s not linked to the user ID. ' +
         'It\'s the device ID like browser / mobile application / remote system',
     isRequired: true,
     kmigratorOptions: { null: false },
+    hooks: {
+        validateInput: (args) => {
+            if (!hasValidJsonStructure(args, true, 1, {
+                fingerprint: {
+                    type: 'string',
+                    presence: true,
+                    length: { minimum: 5, maximum: 20 },
+                },
+            })) return
+        },
+    },
 }
 
 module.exports = {
