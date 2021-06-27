3f37ddb1083b28d08e845a19a5e4bb49da2bdb65
toplenboren <toplenboren@yandex.ru>
2021-06-27 15:08:04 +0500

    SBERDOMA-484 finalize api and define types

--- apps/condo/domains/common/components/Loader.tsx
+++ apps/condo/domains/common/components/Loader.tsx
@@ -1,8 +1,27 @@
 import React from 'react'
-import { Spin } from 'antd'
+import { Spin, SpinProps } from 'antd'
 
-export const Loader: React.FC = (props) => {
+const DEFAULT_DELAY = 200 // milliseconds
+
+interface ILoaderProps extends SpinProps{
+    fill: boolean
+}
+
+export const Loader: React.FC<ILoaderProps> = (props) => {
+    if (props.fill) {
+        return (
+            <section>
+                { <Spin {...props}/> }
+            </section>
+        )
+    }
+
     return (
-        <Spin/>
+        <Spin {...props}/>
     )
+}
+
+Loader.defaultProps = {
+    fill: false,
+    delay: DEFAULT_DELAY,
 }
