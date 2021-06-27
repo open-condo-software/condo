8227370cede6085237b4dbe6207c6ee48bedc7f3
toplenboren <toplenboren@yandex.ru>
2021-06-27 21:38:39 +0500

    SBERDOMA-484 add color prop

--- apps/condo/domains/common/components/Loader.tsx
+++ apps/condo/domains/common/components/Loader.tsx
@@ -1,27 +1,49 @@
+/** @jsx jsx */
 import React from 'react'
 import { Spin, SpinProps } from 'antd'
+import { css, jsx } from '@emotion/core'
+import { colors } from '../constants/style'
 
 const DEFAULT_DELAY = 200 // milliseconds
 
-interface ILoaderProps extends SpinProps{
-    fill: boolean
+interface ILoaderProps extends SpinProps {
+    fill?: boolean
+    color?: string
 }
 
 export const Loader: React.FC<ILoaderProps> = (props) => {
+
+    const filledContainerStyles = css`
+            display:'flex';
+            justify-content: 'center';
+            align-items: 'center';
+            height: '100%';
+            width: '100%';
+        `
+
+    const coloredSpinnerStyles = css`
+        .ant-spin-dot-item {
+            background-color: ${props.color};
+        }
+    `
+
+    console.log(colors)
+
+    const loaderStyles = [coloredSpinnerStyles]
+
     if (props.fill) {
-        return (
-            <section>
-                { <Spin {...props}/> }
-            </section>
-        )
+        loaderStyles.push(filledContainerStyles)
     }
 
     return (
-        <Spin {...props}/>
+        <div css={loaderStyles}>
+            <Spin {...props}/>
+        </div>
     )
 }
 
 Loader.defaultProps = {
     fill: false,
     delay: DEFAULT_DELAY,
+    color: colors?.sberDefault[5],
 }
