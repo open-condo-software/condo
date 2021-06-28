025e975940d694741ba24b7e5271a4a5f365ca88
Dmitriy Nikiforov <tuningiposadka@gmail.com>
2021-06-28 15:07:33 +0500

    SBERDOMA-421 initial commit

--- apps/condo/domains/common/utils/addressApi/AddressMeta.ts
+++ apps/condo/domains/common/utils/addressApi/AddressMeta.ts
@@ -1,83 +1,8 @@
-import getConfig from 'next/config'
-import get from 'lodash/get'
-
-type SuggestionsResponse = Promise<{ suggestions: Array<AddressMeta> }>
-
-export interface IAddressApi {
-    getAddressMeta(address: string): AddressMeta
-    getSuggestions(query: string): SuggestionsResponse
-    cacheAddressMeta(address: string, addressMeta: AddressMeta): void
-}
-
-export class AddressApi implements IAddressApi {
-    constructor () {
-        this.setAddressSuggestionsConfig()
-    }
-
-    public getSuggestions (query: string): SuggestionsResponse {
-        return fetch(this.suggestionsUrl, this.getAddressSuggestionRequestParams(query))
-            .then(response => response.text())
-            .then((res) => JSON.parse(res))
-    }
-
-    public getAddressMeta (address: string): AddressMeta | undefined {
-        const addressMeta = this.addressMetaCache.get(address)
-
-        if (!addressMeta) {
-            throw new Error('addressMetaError')
-        }
-
-        return addressMeta
-    }
-
-    public cacheAddressMeta (address: string, addressMeta: AddressMeta): void {
-        this.addressMetaCache.set(address, addressMeta)
-    }
-
-    private setAddressSuggestionsConfig () {
-        const {
-            publicRuntimeConfig: { addressSuggestionsConfig },
-        } = getConfig()
-        const apiUrl = get(addressSuggestionsConfig, 'apiUrl', '')
-        const apiToken = get(addressSuggestionsConfig, 'apiToken', '')
-        if (!apiToken || !apiUrl) console.error('Wrong AddressSuggestionsConfig! no apiUrl/apiToken')
-
-        this.apiToken = apiToken
-        this.suggestionsUrl = apiUrl
-    }
-
-    private getAddressSuggestionRequestParams (query: string) {
-        return {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json',
-                'Authorization': `Token ${this.apiToken}`,
-            },
-            body: JSON.stringify(
-                {
-                    query,
-                    ...this.defaultSearchApiParams,
-                }
-            ),
-        }
-    }
-
-    private suggestionsUrl: string
-    private apiToken: string
-    private defaultSearchApiParams = {
-        from_bound: { value: 'country' },
-        to_bound: { value: 'house' },
-        restrict_value: true,
-    }
-    private addressMetaCache: Map<AddressValue, AddressMeta> = new Map()
-}
-
 /*
 * AddressMetaCache includes normalized addressMeta, used for getting addressMeta for address value.
 * */
 // Generated from API response using http://json2ts.com
-type AddressMeta = {
+export type AddressMeta = {
     value: string;
     unrestricted_value: string;
     data: {
@@ -125,7 +50,7 @@ type AddressMeta = {
         street: string;
         house_fias_id: string;
         house_kladr_id: string;
-        house_type: string;
+        house_type: 'д' | 'влд' | 'к';
         house_type_full: string;
         house: string;
         block_type?: string;
@@ -168,4 +93,4 @@ type AddressMeta = {
     };
 }
 
-type AddressValue = string
+export type AddressValue = string
