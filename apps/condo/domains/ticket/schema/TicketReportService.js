c30645b805a551347078f1314f8e0878b3853ef7
Sitnikov Ivan <sit7602@gmail.com>
2021-07-01 15:45:50 +0500

    SBERDOMA-503 review fixes - report generation algorithm

--- apps/condo/domains/ticket/schema/TicketReportService.js
+++ apps/condo/domains/ticket/schema/TicketReportService.js
@@ -3,18 +3,19 @@ const { Ticket, TicketStatus } = require('@condo/domains/ticket/utils/serverSche
 const moment = require('moment')
 const { checkOrganizationPermission } = require('@condo/domains/organization/utils/accessSchema')
 const access = require('@condo/domains/ticket/access/Ticket')
+const { TICKET_STATUS_TYPES: ticketTypes } = require('@condo/domains/ticket/constants')
 
 const PERIOD_TYPES = ['week', 'month', 'quarter']
 
-const countTicketsByStatuses = async (context, statuses, dateStart, dateEnd, organizationId) => {
+const countTicketsByStatuses = async (context, dateStart, dateEnd, organizationId) => {
     const answer = {}
-    for (const status of statuses) {
+    for (const type of ticketTypes) {
         const queryByStatus = [
             { createdAt_lte: dateEnd }, { createdAt_gte: dateStart },
-            { status: { name: status } }, { organization: { id: organizationId } },
+            { status: { type } }, { organization: { id: organizationId } },
         ]
         const count = await Ticket.count(context, { AND: queryByStatus })
-        answer[status] = count || 0
+        answer[type] = count || 0
     }
     return answer
 }
@@ -40,11 +41,19 @@ const TicketReportService = new GQLCustomSchema('TicketReportService', {
                 if (!hasAccess) {
                     throw new Error('[error] you do not have access to this organization')
                 }
-                const statuses = await TicketStatus.getAll(context, { OR: [
+                let statuses = await TicketStatus.getAll(context, { OR: [
                     { organization: { id: userOrganizationId } },
                     { organization_is_null: true },
                 ] })
-                const statusNames = statuses.map(status => status.name)
+
+                statuses = statuses.filter(status =>
+                    !(!status.organization && statuses
+                        .find(organizationStatus => organizationStatus.organization !== null
+                            && organizationStatus.type === status.type))
+                )
+
+                const statusesMap = Object.fromEntries(statuses.map(({ type, name }) => ([type, name])))
+
                 if (!PERIOD_TYPES.includes(periodType)) {
                     throw new Error(`[error] possible period types are: ${PERIOD_TYPES.join(', ')}`)
                 }
@@ -54,20 +63,20 @@ const TicketReportService = new GQLCustomSchema('TicketReportService', {
                 const previousEndDate =  moment().endOf(periodType).add(offset - 1, periodType).toISOString()
 
 
-                const currentData = await countTicketsByStatuses(context, statusNames, startDate, endDate, userOrganizationId)
-                const previousData = await countTicketsByStatuses(context, statusNames, previousStartDate, previousEndDate, userOrganizationId)
+                const currentData = await countTicketsByStatuses(context,  startDate, endDate, userOrganizationId)
+                const previousData = await countTicketsByStatuses(context, previousStartDate, previousEndDate, userOrganizationId)
 
                 const data = []
                 Object.entries(currentData).forEach((e) => {
-                    const [statusName, currentValue] = e
-                    const previousValue = previousData[statusName]
+                    const [statusType, currentValue] = e
+                    const previousValue = previousData[statusType]
                     let growth = 0
-                    if (previousValue !== 0 && currentValue !== 0) {
+                    if (previousValue !== 0) {
                         growth = Number((currentValue * 100 / previousValue - 100).toFixed(2))
                     }
 
                     data.push({
-                        statusName,
+                        statusName: statusesMap[statusType],
                         currentValue,
                         growth,
                     })
