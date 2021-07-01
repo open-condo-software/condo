2a614602a679b0b64241f5a5e4850f1557bff4b0
Антон Алёшин <Anton_Aleschin@mail.ru>
2021-07-01 14:45:17 +0300

    SBERDOMA-508 added field `canManageTicketContacts` to GraphQL schema

--- apps/condo/domains/organization/utils/clientSchema/OrganizationEmployeeRole.ts
+++ apps/condo/domains/organization/utils/clientSchema/OrganizationEmployeeRole.ts
@@ -10,7 +10,7 @@ import { generateReactHooks } from '@condo/domains/common/utils/codegeneration/g
 import { OrganizationEmployeeRole as OrganizationEmployeeRoleGQL } from '@condo/domains/organization/gql'
 import { OrganizationEmployeeRole, OrganizationEmployeeRoleUpdateInput, QueryAllOrganizationEmployeeRolesArgs } from '../../../../schema'
 
-const FIELDS = ['id', 'deletedAt', 'createdAt', 'updatedAt', 'createdBy', 'updatedBy', 'organization', 'name', 'statusTransitions', 'canManageOrganization', 'canManageEmployees', 'canManageRoles', 'canManageIntegrations', 'canManageProperties', 'canManageTickets']
+const FIELDS = ['id', 'deletedAt', 'createdAt', 'updatedAt', 'createdBy', 'updatedBy', 'organization', 'name', 'statusTransitions', 'canManageOrganization', 'canManageEmployees', 'canManageRoles', 'canManageIntegrations', 'canManageProperties', 'canManageTickets', 'canManageTicketComments']
 const RELATIONS = ['organization']
 
 export interface IOrganizationEmployeeRoleUIState extends OrganizationEmployeeRole {
