16cbe401c6db98b25d14046af34678e782a3a81f
Pavel <nomerdvadcatpyat@gmail.com>
2021-07-05 17:43:47 +0500

    SBERDOMA-539 remove function 'removeSpacesInsideQuotes' replaced it with regex

--- apps/condo/domains/common/utils/text.js
+++ apps/condo/domains/common/utils/text.js
@@ -1,28 +1,10 @@
-function removeSpacesInsideQuotes (s) {
-    const nestedQuotes = []
-    let res = [...s]
-    for (let i = 0; i < res.length; i++) {
-        const char = res[i]
-        if (char === '"') {
-            if (nestedQuotes[nestedQuotes.length - 1] === char) {
-                nestedQuotes.pop()
-                if (res[i - 1] === ' ') res[i - 1] = ''
-            } else {
-                nestedQuotes.push(char)
-                if (res[i + 1] === ' ') res[i + 1] = ''
-            }
-        }
-    }
-    return res.join('')
-}
-
 function normalizeText (text) {
     if (!text) return
     String(text).normalize()
     const EOL = text.match(/\r\n/gm) ? '\r\n' : '\n'
     const threeOrMoreEOL = new RegExp(`(${EOL}){3,}`, 'gm')
     const punctuations = '.,:'
-    const formatted = text
+    return text
         // replace three or more EOL to two EOL
         .replace(threeOrMoreEOL, EOL + EOL)
         // replace two or more spaces to one space
@@ -35,12 +17,12 @@ function normalizeText (text) {
         .replace(new RegExp(`[^${punctuations}] *[${punctuations}]+ *`, 'gm'), wordWithPunctuation => (
             `${wordWithPunctuation.replace(new RegExp(` *[${punctuations}] *`, 'gm'), punctuation => punctuation.trim())} `
         ))
+        // normalize spaces in double quotes, e.g: "  a b c   " => "a b c"
+        .replace(/"[^"]*"/gm, m => `"${m.split('"')[1].trim()}"`)
         // trim each row
         .split(EOL)
         .map(str => str.trim())
         .join(EOL)
-
-    return removeSpacesInsideQuotes(formatted)
 }
 
 module.exports = {
