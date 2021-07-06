c354d3049b19738913e75b1698bec1aa223ae4f0
sitozzz-home <sit7602@gmail.com>
2021-07-06 13:50:38 +0500

    SBERDOMA-503 review fixes - ticketreport access for deleted users

--- apps/condo/domains/ticket/access/TicketReportService.js
+++ apps/condo/domains/ticket/access/TicketReportService.js
@@ -2,7 +2,7 @@ async function canReadTicketReportWidgetData ({ authentication: { item: user } }
     if (!user) return false
     if (user.isAdmin) return true
 
-    return { organization: { employees_some: { user: { id: user.id }, isBlocked: false }, deletedAt: null } }
+    return { organization: { employees_some: { user: { id: user.id }, isBlocked: false, deletedAt: null } } }
 }
 
 module.exports = {
