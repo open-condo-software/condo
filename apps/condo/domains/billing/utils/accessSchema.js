c3387ff77576f8b56d12f243d5cce8f1e11006e8
toplenboren <toplenboren@yandex.ru>
2021-07-06 16:55:27 +0500

    SBERDOMA-513 remove access check on employees_some

--- apps/condo/domains/billing/utils/accessSchema.js
+++ apps/condo/domains/billing/utils/accessSchema.js
@@ -17,7 +17,7 @@ async function canReadBillingEntity (user) {
     if (user.isAdmin) return true
     return {
         OR: [
-            { context: { organization: { employees_some: { user: { id: user.id }, role: { canManageIntegrations: true, deletedAt: null }, deletedAt: null, isBlocked: false } } } },
+            { context: { organization: { employees_some: { user: { id: user.id }, role: { canManageIntegrations: true }, deletedAt: null, isBlocked: false } } } },
             { context: { integration: { accessRights_some: { user: { id: user.id } } } } },
         ],
     }
