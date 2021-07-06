943c9ac0a6748b8bbfb49e370aa4c4b03ac012ec
Pavel <nomerdvadcatpyat@gmail.com>
2021-07-06 13:39:07 +0500

    SBERDOMA-539 remove \r\n replaces

--- apps/condo/domains/common/utils/text.js
+++ apps/condo/domains/common/utils/text.js
@@ -1,12 +1,10 @@
 function normalizeText (text) {
     if (!text) return
     String(text).normalize()
-    const EOL = text.match(/\r\n/gm) ? '\r\n' : '\n'
-    const threeOrMoreEOL = new RegExp(`(${EOL}){3,}`, 'gm')
     const punctuations = '.,:'
     return text
         // replace three or more EOL to two EOL
-        .replace(threeOrMoreEOL, EOL + EOL)
+        .replace(/\n{3,}/gm, '\n\n')
         // replace two or more spaces to one space
         .replace(/[^\S\r\n]+/g, ' ')
         // normalize punctuation between words, e.g: 'test  ,test' -> 'test, test'
@@ -16,9 +14,9 @@ function normalizeText (text) {
         // normalize spaces in double quotes, e.g: "  a b c   " => "a b c"
         .replace(/"[^"]*"/gm, m => `"${m.split('"')[1].trim()}"`)
         // trim each row
-        .split(EOL)
+        .split('\n')
         .map(str => str.trim())
-        .join(EOL)
+        .join('\n')
 }
 
 module.exports = {
