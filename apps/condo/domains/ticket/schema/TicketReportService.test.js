4d1cc0ba4c8c56a203ffc9010b040d3f2c8419ba
sitozzz-home <sit7602@gmail.com>
2021-07-09 19:58:23 +0500

    SBERDOMA-687 add test case for user with role in organization

--- apps/condo/domains/ticket/schema/TicketReportService.test.js
+++ apps/condo/domains/ticket/schema/TicketReportService.test.js
@@ -1,3 +1,7 @@
+const { createTestOrganizationEmployee } = require('@condo/domains/organization/utils/testSchema')
+const { makeClientWithNewRegisteredAndLoggedInUser } = require('@condo/domains/user/utils/testSchema')
+const { createTestOrganizationEmployeeRole } = require('@condo/domains/organization/utils/testSchema')
+const { makeAdminClientWithRegisteredOrganizationWithRoleWithEmployee } = require('@condo/domains/organization/utils/testSchema')
 const { NEW_OR_REOPENED_STATUS_TYPE } = require('@condo/domains/ticket/constants')
 const { createTestTicket } = require('@condo/domains/ticket/utils/testSchema')
 const { GET_TICKET_WIDGET_REPORT_DATA } = require('@condo/domains/ticket/gql')
@@ -6,7 +10,7 @@ const { makeClientWithProperty } = require('@condo/domains/property/utils/testSc
 
 describe('TicketReportService', () => {
     describe('User', () => {
-        it('can get ticket report with selected organization', async () => {
+        it('can get ticket report without role in organization', async () => {
             const client = await makeClientWithProperty()
             await createTestTicket(client, client.organization, client.property)
             const { data: { result: { data } } } = await client.query(GET_TICKET_WIDGET_REPORT_DATA, {
@@ -17,6 +21,24 @@ describe('TicketReportService', () => {
             const clientTicket = data.find(e => e.statusType === NEW_OR_REOPENED_STATUS_TYPE)
             expect(clientTicket).toBeDefined()
             expect(clientTicket.currentValue).toEqual(1)
+            expect(clientTicket.growth).toEqual(0)
+        })
+
+        it('can get ticket report with role in organization', async () => {
+            const { admin, organization } = await makeAdminClientWithRegisteredOrganizationWithRoleWithEmployee()
+            const [role] = await createTestOrganizationEmployeeRole(admin, organization, {
+                canManageEmployees: true,
+            })
+            const managerClient = await makeClientWithNewRegisteredAndLoggedInUser()
+            const [employee] = await createTestOrganizationEmployee(admin, organization, managerClient.user, role, { isBlocked: false })
+            const { data: { result: { data } } } = await managerClient.query(GET_TICKET_WIDGET_REPORT_DATA, {
+                data: { userOrganizationId: employee.organization.id, periodType: 'week' } }
+            )
+            expect(data).toBeInstanceOf(Array)
+            expect(data.length).toBeGreaterThanOrEqual(1)
+            const clientTicket = data.find(e => e.statusType === NEW_OR_REOPENED_STATUS_TYPE)
+            expect(clientTicket).toBeDefined()
+            expect(clientTicket.currentValue).toEqual(0)
             expect(clientTicket.growth).toEqual(0)
         })
 
