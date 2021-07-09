6de4ee3e5f75ef752e2fb5e01e9ac2087b5bb3aa
matthewsavelev <savelevmatthew@gmail.com>
2021-07-09 20:11:44 +0500

    SBERDOMA-677 Conflicts resolved pt2

--- apps/condo/domains/common/constants/regexps.js
+++ apps/condo/domains/common/constants/regexps.js
@@ -4,10 +4,17 @@ const LETTERS_AND_NUMBERS = /[\p{L}\p{N}]/gu
 const PHONE = /^\+?\d*(\.\d*)?$/
 const PHONE_CLEAR_REGEXP = /[^+0-9]/g
 
+// ESCAPES SPECIAL REGEX CHARACTERS OF STRING TO ENABLE ITS USAGE INSIDE REGEX
+const ESCAPE_REGEX = (text) => {
+    return text.replace(/[-[\]{}()*+?.,\\/^$|#\s]/g, '\\$&')
+}
+
+
 module.exports = {
     ALPHANUMERIC_REGEXP,
     UPPER_CASE_ALPHANUMERIC_REGEXP,
     LETTERS_AND_NUMBERS,
     PHONE,
     PHONE_CLEAR_REGEXP,
+    ESCAPE_REGEX,
 }
