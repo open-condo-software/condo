06ac7eb389e2e985224ae8d563752bc26b631c2e
matthewsavelev <savelevmatthew@gmail.com>
2021-07-14 18:41:38 +0500

    SBERDOMA-592 Permissions hotfix

--- apps/condo/domains/organization/permissions.ts
+++ apps/condo/domains/organization/permissions.ts
@@ -40,10 +40,7 @@ export const canManageContacts = (organizationLink?: OrganizationToUserLink, con
     if (!organizationLink || !contact) {
         return false
     }
-
-    const isOrganizationEqual = get(organizationLink, ['organization', 'id']) === get(contact, ['organization', 'id'])
-    if (isOrganizationEqual) {
-        return get(organizationLink, ['role', 'canManageContacts'])
-    }
-    return false
+    // TODO (SavelevMatthew): this method called only if org. matches, so can skip redundant check?
+    // const isOrganizationEqual = get(organizationLink, ['organization', 'id']) === get(contact, ['organization', 'id'])
+    return get(organizationLink, ['role', 'canManageContacts'])
 }
