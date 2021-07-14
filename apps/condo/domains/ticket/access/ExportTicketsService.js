665e7ce0d50b3190966a24fa233e3ca75db59bd5
Dmitry Koviazin <dkoviazin@gmail.com>
2021-07-14 12:10:08 +0500

    SBERDOMA-704 Auth error in access check

--- apps/condo/domains/ticket/access/ExportTicketsService.js
+++ apps/condo/domains/ticket/access/ExportTicketsService.js
@@ -1,8 +1,9 @@
+const { throwAuthenticationError } = require('@condo/domains/common/utils/apolloErrorFormatter')
 const { checkOrganizationPermission } = require('@condo/domains/organization/utils/accessSchema')
 const get = require('lodash/get')
 
 async function canExportTicketsToExcel ({ args: { data: { where } }, authentication: { item: user } }) {
-    if (!user) return false
+    if (!user) return throwAuthenticationError()
     if (user.isAdmin) return true
     const organizationId = get(where, 'organization.id')
     if (!organizationId) {
