f38d7fadb767071f05dae8ba1bb4e2c5c6eba15c
Dmitry Koviazin <dkoviazin@gmail.com>
2021-07-20 19:27:43 +0500

    SBERDOMA-506 tickets counters with groups

--- apps/condo/schema.d.ts
+++ apps/condo/schema.d.ts
@@ -6557,6 +6557,7 @@ export type Mutation = {
   reInviteOrganizationEmployee?: Maybe<OrganizationEmployee>;
   acceptOrRejectOrganizationInviteById?: Maybe<OrganizationEmployee>;
   acceptOrRejectOrganizationInviteByCode?: Maybe<OrganizationEmployee>;
+  ticketAnalyticsReport?: Maybe<TicketAnalyticsReportOutput>;
   sendMessage?: Maybe<SendMessageOutput>;
   resendMessage?: Maybe<ResendMessageOutput>;
   registerResident?: Maybe<Resident>;
@@ -8248,6 +8249,11 @@ export type MutationAcceptOrRejectOrganizationInviteByIdArgs = {
 export type MutationAcceptOrRejectOrganizationInviteByCodeArgs = {
   inviteCode: Scalars['String'];
   data: AcceptOrRejectOrganizationInviteInput;
+};
+
+
+export type MutationTicketAnalyticsReportArgs = {
+  data?: Maybe<TicketAnalyticsReportInput>;
 };
 
 
@@ -14181,6 +14187,16 @@ export type Ticket_WatchersMetaArgs = {
   orderBy?: Maybe<Scalars['String']>;
   first?: Maybe<Scalars['Int']>;
   skip?: Maybe<Scalars['Int']>;
+};
+
+export type TicketAnalyticsReportInput = {
+  where: TicketWhereInput;
+  groupBy: Array<Maybe<Scalars['String']>>;
+};
+
+export type TicketAnalyticsReportOutput = {
+  __typename?: 'TicketAnalyticsReportOutput';
+  result: Scalars['JSON'];
 };
 
 /**  Incremental changes of Ticket  */
