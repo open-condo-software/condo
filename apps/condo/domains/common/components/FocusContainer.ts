456a5fcf28c55fad07634cd1bcd45b1bb1d6fa41
Dimitreee <danewdima@gmail.com>
2021-07-20 16:23:07 +0300

    SBERDOMA-762 Add billing trial page

--- apps/condo/domains/common/components/FocusContainer.ts
+++ apps/condo/domains/common/components/FocusContainer.ts
@@ -1,9 +1,14 @@
 import styled from '@emotion/styled'
 import { colors } from '../constants/style'
 
-export const FocusContainer = styled.div`
+interface IFocusContainerProps {
+    color?: string,
+    margin?: string
+}
+
+export const FocusContainer = styled.div<IFocusContainerProps>`
   max-width: calc(100% + 48px);
-  margin: 0 -24px;
+  margin: ${({ margin }) => margin || '0 -24px'};
   border: 1px solid ${({ color }) => color || colors.lightGrey[5]};
   border-radius: 8px;
   padding: 24px;
