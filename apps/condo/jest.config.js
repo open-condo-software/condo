b644413dcf5649b133e1e1eb5711e936c0748337
Dmitriy Nikiforov <tuningiposadka@gmail.com>
2021-07-27 19:40:18 +0500

    SBERDOMA-872 remove useless options

--- apps/condo/jest.config.js
+++ apps/condo/jest.config.js
@@ -22,9 +22,6 @@ module.exports = {
             testEnvironment: 'node',
             testMatch: [`${__dirname}/schema/**/*.test.js`, `${__dirname}/domains/**/schema/*.test.js`],
             setupFilesAfterEnv: [`${__dirname}/jest.setup.js`],
-            testEnvironmentOptions: {
-                testTimeout: 1000000,
-            },
         },
         {
             displayName: 'main',
