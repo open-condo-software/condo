d1c40bbb48ffab8c6f6a5942d64a4c8c33f33b4d
toplenboren <toplenboren@yandex.ru>
2021-07-29 22:56:38 +0500

    SBERDOMA-586 Add ServiceConsumer access

---
+++ apps/condo/domains/resident/access/ServiceConsumer.js
@@ -0,0 +1,27 @@
+// @ts-nocheck
+const { RESIDENT } = require('@condo/domains/user/constants/common')
+async function canReadServiceConsumers ({ authentication: { item: user } }) {
+    if (!user) return false
+    if (user.isAdmin) return {}
+    if (user.type === RESIDENT) {
+        return {
+            user: { id: user.id },
+        }
+    }
+    return false
+}
+
+async function canManageServiceConsumers ({ authentication: { item: user }, originalInput, operation, itemId }) {
+    if (!user) return false
+    if (user.isAdmin) return true
+    return operation === 'create' && user.type === RESIDENT
+}
+
+/*
+  Rules are logical functions that used for list access, and may return a boolean (meaning
+  all or no items are available) or a set of filters that limit the available items.
+*/
+module.exports = {
+    canReadServiceConsumers,
+    canManageServiceConsumers,
+}
