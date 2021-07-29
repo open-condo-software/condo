c45712ea0285bc036142a9f41fe37dd6fd4dbfaf
Dimitreee <danewdima@gmail.com>
2021-07-29 17:05:14 +0300

    Revert "SBERDOMA-627 unbind search input value"
    This reverts commit 98d72843

--- apps/condo/domains/common/components/BaseSearchInput/index.tsx
+++ apps/condo/domains/common/components/BaseSearchInput/index.tsx
@@ -98,11 +98,7 @@ export const BaseSearchInput = <S extends string>(props: ISearchInput<S>) => {
 
     useEffect(
         () => {
-            // This input is usually controlled by form that is directly setting value, so we don't need to 
-            // bind in two way, as it causes input to force place careete to the end of text.
-            if (!onChange){
-                setSearchValue(restSelectProps.value)
-            }
+            setSearchValue(restSelectProps.value)
         },
         [restSelectProps.value]
     )
