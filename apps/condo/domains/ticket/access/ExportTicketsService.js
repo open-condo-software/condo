73f26bfc400d3efbe0d55f99c366ec1477c26ce3
Pavel <nomerdvadcatpyat@gmail.com>
2021-08-02 12:15:17 +0500

    SBERDOMA-794 add acessess for related organization employee for Contact, OrganizationEmployee and ExportTicketsService

--- apps/condo/domains/ticket/access/ExportTicketsService.js
+++ apps/condo/domains/ticket/access/ExportTicketsService.js
@@ -1,12 +1,23 @@
 const { throwAuthenticationError } = require('@condo/domains/common/utils/apolloErrorFormatter')
 const { checkOrganizationPermission } = require('@condo/domains/organization/utils/accessSchema')
 const get = require('lodash/get')
+const { checkRelatedOrganizationPermission } = require('@condo/domains/organization/utils/accessSchema')
+const { Organization } = require('@condo/domains/organization/utils/serverSchema')
 
 async function canExportTicketsToExcel ({ args: { data: { where } }, authentication: { item: user }, context }) {
     if (!user) return throwAuthenticationError()
     if (user.isAdmin) return true
     const organizationId = get(where, 'organization.id')
     if (!organizationId) {
+        const organizationFromWhere = get(where, 'organization')
+        const [relatedFromOrganization] = await Organization.getAll(context, organizationFromWhere)
+        if (!relatedFromOrganization) {
+            return false
+        }
+        const canManageRelatedOrganizationTickets = await checkRelatedOrganizationPermission(context, user.id, relatedFromOrganization.id, 'canManageTickets')
+        if (canManageRelatedOrganizationTickets) {
+            return true
+        }
         return false
     }
     const hasAccess = await checkOrganizationPermission(context, user.id, organizationId, 'canManageTickets')
