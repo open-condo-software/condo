390a4cf802f5158c20a9c43c49a1c3b4f6a4bcbd
Dmitriy Nikiforov <tuningiposadka@gmail.com>
2021-08-03 18:26:22 +0500

    SBERDOMA-476 LocalizedText type refactoring

--- packages/@core.keystone/fields/LocalizedText/Implementation.js
+++ packages/@core.keystone/fields/LocalizedText/Implementation.js
@@ -1,6 +1,7 @@
-const { extractReqLocale, defaultLocale } = require('@condo/domains/common/utils/locales')
+const { extractReqLocale } = require('@condo/domains/common/utils/locales')
 const { getTranslations } = require('@condo/domains/common/utils/localesLoader')
 const { Text } = require('@keystonejs/fields')
+const conf = require('@core/config')
 const minimatch = require('minimatch')
 
 // docs: https://github.com/keystonejs/keystone-5/blob/a7b19759a007fd2d6dcfcf6a998c9706d5dfdaa6/packages/fields/src/Implementation.js
@@ -14,14 +15,15 @@ class LocalizedText extends Text.implementation {
 
     gqlOutputFieldResolvers () {
         return {
-            [`${this.path}`]: (item, args, context, info) => {
-                const locale = context.req ? extractReqLocale(context.req) : defaultLocale
+            [this.path]: (item, args, context, info) => {
+                const locale = context.req ? extractReqLocale(context.req) : conf.DEFAULT_LOCALE
                 const translations = getTranslations(locale)
+                const fieldValue = item[this.path]
                 // collision detection
-                if (translations[item[this.path]] && minimatch(item[this.path], this.config.template)) {
-                    return translations[item[this.path]]
+                if (translations[fieldValue] && minimatch(fieldValue, this.config.template)) {
+                    return translations[fieldValue]
                 }
-                return item[this.path]
+                return fieldValue
             },
         }
     }
