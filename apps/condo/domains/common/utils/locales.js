130e422299fd8e8f41589dba1cb8bd118837205d
Dmitriy Nikiforov <tuningiposadka@gmail.com>
2021-08-04 15:19:15 +0500

    SBERDOMA-476 restore client locale extraction

--- apps/condo/domains/common/utils/locales.js
+++
@@ -1,8 +0,0 @@
-const extractReqLocale = (req, defaultLocale) => {
-    const headersLocale = req.headers['accept-language'] && req.headers['accept-language'].slice(0, 2)
-    return headersLocale
-}
-
-module.exports = {
-    extractReqLocale,
-}
