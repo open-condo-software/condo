123e59f93d4d70ff93834c060deebdf1142e2325
Dmitriy Nikiforov <tuningiposadka@gmail.com>
2021-08-04 15:19:15 +0500

    SBERDOMA-476 restore client locale extraction

--- packages/@core.keystone/fields/LocalizedText/Implementation.js
+++ packages/@core.keystone/fields/LocalizedText/Implementation.js
@@ -1,4 +1,3 @@
-const { extractReqLocale } = require('@condo/domains/common/utils/locales')
 const { getTranslations } = require('@condo/domains/common/utils/localesLoader')
 const { Text } = require('@keystonejs/fields')
 const conf = require('@core/config')
@@ -12,11 +11,14 @@ class LocalizedText extends Text.implementation {
             throw new Error(`Provide template id from language dictionary for localized text field: ${this.listKey}.${this.path}. Example: "employee.role.title.*'`)
         }
     }
-
+    extractReqLocale (req, defaultLocale) {
+        const headersLocale = req.headers['accept-language'] && req.headers['accept-language'].slice(0, 2)
+        return headersLocale
+    }
     gqlOutputFieldResolvers () {
         return {
             [this.path]: (item, args, context, info) => {
-                const locale = context.req ? extractReqLocale(context.req) : conf.DEFAULT_LOCALE
+                const locale = context.req ? this.extractReqLocale(context.req) : conf.DEFAULT_LOCALE
                 const translations = getTranslations(locale)
                 const fieldValue = item[this.path]
                 // collision detection
