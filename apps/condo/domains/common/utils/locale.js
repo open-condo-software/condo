e1a1dca6e4ce45cf243051d972a121bfc45ec617
Dmitriy Nikiforov <tuningiposadka@gmail.com>
2021-08-04 15:32:11 +0500

    SBERDOMA-476 share reqeust extract locale logic

---
+++ apps/condo/domains/common/utils/locale.js
@@ -0,0 +1,15 @@
+const nextCookie = require('next-cookies')
+
+const extractReqLocale = (req) => {
+    try {
+        const cookieLocale = nextCookie({ req }).locale
+        const headersLocale = req.headers['accept-language'] && req.headers['accept-language'].slice(0, 2)
+        return cookieLocale || headersLocale
+    }
+    catch {
+        return null
+    }
+}
+module.exports = {
+    extractReqLocale,
+}
--- packages/@core.keystone/fields/LocalizedText/Implementation.js
+++ packages/@core.keystone/fields/LocalizedText/Implementation.js
@@ -1,7 +1,8 @@
-const { getTranslations } = require('@condo/domains/common/utils/localesLoader')
+const minimatch = require('minimatch')
 const { Text } = require('@keystonejs/fields')
 const conf = require('@core/config')
-const minimatch = require('minimatch')
+const { getTranslations } = require('@condo/domains/common/utils/localesLoader')
+const { extractReqLocale } = require('@condo/domains/common/utils/locale')
 
 // docs: https://github.com/keystonejs/keystone-5/blob/a7b19759a007fd2d6dcfcf6a998c9706d5dfdaa6/packages/fields/src/Implementation.js
 class LocalizedText extends Text.implementation {
@@ -11,14 +12,10 @@ class LocalizedText extends Text.implementation {
             throw new Error(`Provide template id from language dictionary for localized text field: ${this.listKey}.${this.path}. Example: "employee.role.title.*'`)
         }
     }
-    extractReqLocale (req, defaultLocale) {
-        const headersLocale = req.headers['accept-language'] && req.headers['accept-language'].slice(0, 2)
-        return headersLocale
-    }
     gqlOutputFieldResolvers () {
         return {
             [this.path]: (item, args, context, info) => {
-                const locale = context.req ? this.extractReqLocale(context.req) : conf.DEFAULT_LOCALE
+                const locale = context.req ? extractReqLocale(context.req) : conf.DEFAULT_LOCALE
                 const translations = getTranslations(locale)
                 const fieldValue = item[this.path]
                 // collision detection
