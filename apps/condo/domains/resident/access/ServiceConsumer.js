36575ba8f213f3d7a68edadb57396939a29bf4b1
toplenboren <toplenboren@gmail.com>
2021-08-05 19:31:54 +0500

    SBERDOMA-586 Add logic to only allow update if user is going to delete the service consumer

--- apps/condo/domains/resident/access/ServiceConsumer.js
+++ apps/condo/domains/resident/access/ServiceConsumer.js
@@ -1,6 +1,7 @@
 
 const { ServiceConsumer, Resident } = require('@condo/domains/resident/utils/serverSchema')
 const { RESIDENT } = require('@condo/domains/user/constants/common')
+
 async function canReadServiceConsumers ({ authentication: { item: user } }) {
     if (!user) return false
     if (user.isAdmin) return {}
@@ -20,7 +21,19 @@ async function canManageServiceConsumers ({ authentication: { item: user }, cont
             const [serviceConsumer] = await ServiceConsumer.getAll(context, { id: itemId })
             if (!serviceConsumer) return false
             const [resident] = await Resident.getAll(context, { id: serviceConsumer.resident.id })
-            return resident.user.id === user.id
+            const isOwnBillingAccount = resident.user.id === user.id
+
+            let isSoftDeleteOperation = true
+
+            const SAFE_FIELDS = ['dv', 'sender', 'deletedAt']
+            for (let prop in originalInput) {
+                if (!originalInput.hasOwnProperty(prop)) { continue }
+                if ( !SAFE_FIELDS.includes(prop) && !prop.startsWith('_') ) {
+                    isSoftDeleteOperation = false
+                }
+            }
+
+            return isOwnBillingAccount && isSoftDeleteOperation
         }
     }
 }
