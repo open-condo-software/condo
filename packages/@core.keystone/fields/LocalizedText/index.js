0d6c1dabebc7680881c4f40e484d8185b607589a
Дмитрий <tuningiposadka@gmail.com>
2021-08-06 19:24:01 +0500

    SBERDOMA-476 fix admin view for new field type

--- packages/@core.keystone/fields/LocalizedText/index.js
+++ packages/@core.keystone/fields/LocalizedText/index.js
@@ -4,6 +4,11 @@ const { Text } = require('@keystonejs/fields')
 module.exports = {
     type: 'LocalizedText',
     implementation: LocalizedText,
+    views: {
+        Controller: '@keystonejs/fields/types/Text/views/Controller',
+        Field: '@keystonejs/fields/types/Text/views/Field',
+        Filter: '@keystonejs/fields/types/Text/views/Filter',
+    },
     adapters: {
         mongoose: Text.adapters.mongoose,
         knex: Text.adapters.knex,
