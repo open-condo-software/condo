62a406fc569fabf78583fd30d6b83b8293c6597b
matthewsavelev <savelevmatthew@gmail.com>
2021-08-06 12:56:17 +0500

    SBERDOMA-651 Removed redundant local changes

--- apps/condo/initialData.js
+++ apps/condo/initialData.js
@@ -26,30 +26,4 @@ module.exports = [
             },
         ],
     },
-    {
-        listKey: 'BillingIntegration',
-        items: [
-            {
-                data: {
-                    dv: 1,
-                    sender: { dv: 1, fingerprint: 'initial' },
-                    name: 'очень длинное имя интеграции аываываыва',
-                    shortDescription: 'Short integration description, that would be shown on settings card',
-                    detailsTitle: 'подключение картошки',
-                    detailsText: 'asdasdasdasdkasldkals;kdlaskdl;asd;lals;d;kasl;dkalsdk;asd;as',
-                    detailsInstructionButtonText: 'Инструкция',
-                    detailsInstructionButtonLink: 'https://www.ivi.ru/collections/movies-taxi',
-                },
-            },
-            {
-                data: {
-                    dv: 1,
-                    sender: { dv: 1, fingerprint: 'initial' },
-                    name: 'Такси',
-                    shortDescription: 'Short integration description, that would be shown on settings card',
-                    detailsTitle: 'подключение картошки',
-                },
-            },
-        ],
-    },
 ]
