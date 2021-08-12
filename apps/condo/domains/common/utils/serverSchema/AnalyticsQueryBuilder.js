ace890a78616dd396dd49d5424db666aa30ee5b2
sitozzz-home <sit7602@gmail.com>
2021-08-12 14:10:44 +0500

    SBERDOMA-969 - add AnalyticsQueryBuilder implementation for ticket domain

--- apps/condo/domains/common/utils/serverSchema/AnalyticsQueryBuilder.js
+++ apps/condo/domains/common/utils/serverSchema/AnalyticsQueryBuilder.js
@@ -1,18 +1,20 @@
+const has = require('lodash/has')
+
 class AnalyticsQueryBuilder {
-    #domainName = null
-    #where = []
-    #dayGroups = ['day', 'month', 'week', 'quarter', 'year']
-    #dayGroup = 'day'
-    #dateRange = { from: null, to: null }
-    #groups = []
-    #result = null
+    domainName = null
+    where = []
+    dayGroups = ['day', 'month', 'week', 'quarter', 'year']
+    dayGroup = 'day'
+    dateRange = { from: null, to: null }
+    groups = []
+    result = null
 
     constructor (domainName, where = {}, groupBy) {
-        this.#domainName = domainName
+        this.domainName = domainName
         let whereConditions = []
         for (const field in where) {
             if (field === 'AND') {
-                whereConditions = [...whereConditions, where[field]]
+                whereConditions = [...whereConditions, ...where[field]]
             } else {
                 whereConditions.push({ [field]: where[field] })
             }
@@ -20,25 +22,30 @@ class AnalyticsQueryBuilder {
         whereConditions.forEach(condition => {
             Object.entries(condition).forEach(([field, query]) => {
                 if (field === 'createdAt_gte') {
-                    this.#dateRange.from = query
+                    this.dateRange.from = query
                 } else if (field === 'createdAt_lte') {
-                    this.#dateRange.to = query
+                    this.dateRange.to = query
                 } else {
-                    this.#where.push({ [field]: query })
+                    this.where.push({ [field]: query })
                 }
             })
         })
-        this.#dayGroup = 'day'
-        this.#dayGroups.forEach(possibleDateGroup => {
+        this.dayGroup = 'day'
+        this.dayGroups.forEach(possibleDateGroup => {
             if (groupBy.includes(possibleDateGroup)) {
-                this.#dayGroup = possibleDateGroup
+                this.dayGroup = possibleDateGroup
             }
         })
-        this.#groups = groupBy.filter(type => !this.#dayGroups.includes(type))
+        this.groups = groupBy.filter(type => !this.dayGroups.includes(type))
     }
 
     getResult () {
-        return this.#result
+        return this.result
+    }
+
+    isWhereInCondition (condition) {
+        const [, query] = Object.entries(condition)[0]
+        return has(query, 'id_in')
     }
 }
 
