7b46b1e86a08516588694aaa4cb2246003f841d6
sitozzz-home <sit7602@gmail.com>
2021-08-12 12:54:33 +0500

    SBERDOMA-969 - init base sql query builder class

---
+++ apps/condo/domains/common/utils/serverSchema/AnalyticsQueryBuilder.js
@@ -0,0 +1,47 @@
+class AnalyticsQueryBuilder {
+    #domainName = null
+    #where = []
+    #dayGroups = ['day', 'month', 'week', 'quarter', 'year']
+    #dayGroup = 'day'
+    #dateRange = { from: null, to: null }
+    #groups = []
+    #result = null
+
+    constructor (domainName, where = {}, groupBy) {
+        this.#domainName = domainName
+        let whereConditions = []
+        for (const field in where) {
+            if (field === 'AND') {
+                whereConditions = [...whereConditions, where[field]]
+            } else {
+                whereConditions.push({ [field]: where[field] })
+            }
+        }
+        whereConditions.forEach(condition => {
+            Object.entries(condition).forEach(([field, query]) => {
+                if (field === 'createdAt_gte') {
+                    this.#dateRange.from = query
+                } else if (field === 'createdAt_lte') {
+                    this.#dateRange.to = query
+                } else {
+                    this.#where.push({ [field]: query })
+                }
+            })
+        })
+        this.#dayGroup = 'day'
+        this.#dayGroups.forEach(possibleDateGroup => {
+            if (groupBy.includes(possibleDateGroup)) {
+                this.#dayGroup = possibleDateGroup
+            }
+        })
+        this.#groups = groupBy.filter(type => !this.#dayGroups.includes(type))
+    }
+
+    getResult () {
+        return this.#result
+    }
+}
+
+module.exports = {
+    AnalyticsQueryBuilder,
+}
