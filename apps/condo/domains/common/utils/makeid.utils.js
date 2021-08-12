a26b51c232ca761affd24048c73a538dde981a99
Дмитрий <tuningiposadka@gmail.com>
2021-08-12 12:54:39 +0500

    SBERDOMA-962 replace custom logic with lodash.sample

--- apps/condo/domains/common/utils/makeid.utils.js
+++ apps/condo/domains/common/utils/makeid.utils.js
@@ -1,9 +1,10 @@
+const { sample } = require('lodash')
+
 function makeId (length) {
     let result = ''
     const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
-    const charactersLength = characters.length
     for (let i = 0; i < length; i++) {
-        result += characters.charAt(Math.floor(Math.random() * charactersLength))
+        result += sample(characters)
     }
     return result
 }
