29242e9140ffc12b74af6558789b492f0e4f090d
toplenboren <toplenboren@gmail.com>
2021-08-17 15:48:19 +0500

    SBERDOMA-1005 Add ability to set feature flags configuration from .env

--- apps/condo/domains/common/components/containers/FeatureFlag.tsx
+++ apps/condo/domains/common/components/containers/FeatureFlag.tsx
@@ -1,8 +1,24 @@
-// features should be added at local storage as 'featureName1,featureName2'
+/**
+ * Basic component for feature flags
+ *
+ * enabledFlags are stored in localstorage like this: feat1,feat2,feat3
+ * disabledFlags are not present in localstorage
+ *
+ * Feature flags are configured in .env file. Look at .env.example
+ *
+ * To use feature flags you need two components:
+ * - controller (UI for client-side configuration of feature flags)
+ * - container (Feature wrapper)
+ *
+ * You also have access to the function hasFeature(name) to handle other cases
+ *
+ */
 
-import React, { ReactNode, useEffect, useState } from 'react'
-import { Modal, Switch } from 'antd'
+import React, { ReactNode, useState } from 'react'
+import { Modal, Switch, Alert } from 'antd'
 import useKeyboardShortcut from 'use-keyboard-shortcut'
+import getConfig from 'next/config'
+import { useIntl } from '@core/next/intl'
 
 
 const getEnabledFeatures = (): Array<string> => {
@@ -12,6 +28,20 @@ const getEnabledFeatures = (): Array<string> => {
         if (featuresFromStorage) {
             return featuresFromStorage.split(',')
         }
+    }
+
+    return []
+}
+
+const getAllFeatures = (): Array<string> => {
+    const { publicRuntimeConfig } = getConfig()
+    let { featureFlagsConfig } = publicRuntimeConfig
+
+    try {
+        featureFlagsConfig = JSON.parse(featureFlagsConfig)
+        return featureFlagsConfig
+    } catch (err) {
+        console.error(`Feature flags are not configured properly, found: ${featureFlagsConfig}, error ${err}`)
     }
 
     return []
@@ -55,26 +85,12 @@ export const FeatureFlagRequired: React.FC<IFeature> = (props) => {
         fallback,
     } = props
 
-    const [features, setFeatures] = useState([])
-
-    useEffect(() => {
-        const enabledFeatures = _getEnabledFeatures()
-
-        if (enabledFeatures.length) {
-            setFeatures(enabledFeatures)
-        } 
-    }, [])
-
-    const hasFeature = features.includes(name)
-
-    if (hasFeature) {
+    if (hasFeature(name)) {
         return <>{ children }</>
     }
 
     return (
-        <>
-            {fallback}
-        </>
+        <>{fallback}</>
     )
 }
 
@@ -87,21 +103,26 @@ export const FeatureFlagsController: React.FC = () => {
 
     const [isModalVisible, setIsModalVisible] = useState(false)
 
-    const featureFlagsConfig = ['billing', 'settings', 'se']
+    const allFeatures = getAllFeatures()
 
     const showModal = () => {
         setIsModalVisible(true)
+    }
+
+    const closeModal = () => {
+        setIsModalVisible(false)
     }
 
     const enabledFlags = getEnabledFeatures()
 
     return (
         <>
-            <Modal title="Flags" visible={isModalVisible} onOk={() => setIsModalVisible(false)} >
+            <Modal title={ featureFlagsTitle } visible={isModalVisible} onOk={closeModal} >
+                <Alert message={ featureFlagsDescription } type="success" />
                 {
-                    featureFlagsConfig.map((name) => (
+                    allFeatures.map((name) => (
                         <>
-                            <div>
+                            <div style={{ paddingTop: '30px' }}>
                                 <h2><b>{name}</b></h2>
                                 <Switch defaultChecked={enabledFlags.includes(name)} checkedChildren="1" unCheckedChildren="0" onChange={() => toggleFeature(name)} />
                             </div>
