67dfdd4ec8dc67e69be345a1e83bcfc85565e114
Антон Алёшин <Anton_Aleschin@mail.ru>
2021-08-18 14:56:58 +0500

    SBERDOMA-951 new component FormSubheader
    Displays a subtitle and an optional hint in a tooltip

---
+++ apps/condo/domains/common/components/FormSubheader.tsx
@@ -0,0 +1,30 @@
+import { Col, Row, Tooltip, Typography } from 'antd'
+import React from 'react'
+import { QuestionCircleFilled } from '@ant-design/icons'
+import { grey } from '@ant-design/colors'
+
+interface IFormSubheaderProps {
+    title: string,
+    hint?: string
+}
+
+/**
+ * Displays a subtitle and an optional hint in a tooltip
+ */
+const FormSubheader: React.FC<IFormSubheaderProps> = ({ title, hint }) => {
+    return (
+        <Typography.Title level={2} style={{ fontSize: '16px', marginBottom: '24px' }}>
+            {title}
+            {hint && (
+                <>
+                    &nbsp;
+                    <Tooltip title={hint}>
+                        <QuestionCircleFilled style={{ color: grey[0] }}/>
+                    </Tooltip>
+                </>
+            )}
+        </Typography.Title>
+    )
+}
+
+export default FormSubheader
