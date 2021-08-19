40a5e02f342967cc287e44a6c8ee60f52b648f0f
Dimitreee <danewdima@gmail.com>
2021-08-19 10:23:35 +0300

    SBERDOMA-763 fix sidenav markup

--- apps/condo/domains/onboarding/components/OnBoardingProgressIconContainer.tsx
+++ apps/condo/domains/onboarding/components/OnBoardingProgressIconContainer.tsx
@@ -1,7 +1,7 @@
 import { useOnBoardingContext } from './OnBoardingContext'
 
 export const OnBoardingProgressIconContainer = ({ children }) => {
-    const { progress } = useOnBoardingContext()
+    const { progress, isLoading } = useOnBoardingContext()
 
-    return progress < 100 ? children : null
+    return isLoading || progress < 100 ? children : null
 }
