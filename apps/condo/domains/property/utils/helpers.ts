4f63b944458ab5e686b8cfd7a4ef1a9b1d327a95
Антон Алёшин <Anton_Aleschin@mail.ru>
2021-08-19 19:29:43 +0500

    SBERDOMA-1057 extracted removing flat from addressString into `getAddressUpToBuildingFrom` util
    It will be used in Resident validations

--- apps/condo/domains/property/utils/helpers.ts
+++ apps/condo/domains/property/utils/helpers.ts
@@ -98,3 +98,25 @@ export const filtersToQuery = (filters: IFilters): PropertyWhereInput => {
         }
     }
 }
+
+export const FLAT_WITHOUT_FLAT_TYPE_MESSAGE = 'Flat is specified, but flat type is not!'
+
+/**
+ * Sometimes address can contain flat with prefix, for example, in case of scanning receipt with QR-code.
+ * Input data is out of control ;)
+ * @return {String} address without flat
+ */
+export const getAddressUpToBuildingFrom = (addressMeta) => {
+    const data = get(addressMeta, 'data')
+    const value = get(addressMeta, 'value')
+
+    const flat = get(data, 'flat')
+    let result = value
+    if (flat) {
+        const flatType = get(data, 'flat_type')
+        if (!flatType) throw new Error(FLAT_WITHOUT_FLAT_TYPE_MESSAGE)
+        const suffix = `, ${flatType} ${flat}`
+        result = value.substring(0, value.length - suffix.length)
+    }
+    return result
+}
