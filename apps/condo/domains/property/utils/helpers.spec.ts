e4add3ea106b64eee18cc2ba6490b7876f3e6fbd
Антон Алёшин <Anton_Aleschin@mail.ru>
2021-08-19 22:15:08 +0500

    SBERDOMA-1057 moved `getAddressUpToBuildingFrom` to common domain
    It is being used by several domains now.
    It should not be in TypeScript, since it is being used on server side.

--- apps/condo/domains/property/utils/helpers.spec.ts
+++ apps/condo/domains/property/utils/helpers.spec.ts
@@ -5,12 +5,7 @@ import {
     getSortStringFromQuery,
     getPageIndexFromQuery,
     searchToQuery,
-    getAddressUpToBuildingFrom,
-    FLAT_WITHOUT_FLAT_TYPE_MESSAGE,
 } from './helpers'
-
-import { buildFakeAddressMeta } from '@condo/domains/common/utils/testSchema/factories'
-const { catchErrorFrom } = require('@condo/domains/common/utils/testSchema')
 
 describe('Helpers property', () => {
     describe('queryUtils property', () => {
@@ -160,45 +155,6 @@ describe('Helpers property', () => {
                     expect(searchToQuery()).toBeUndefined()
                 })
             })
-        })
-
-        describe('getAddressUpToBuildingFrom', () => {
-            it('wipes out flat with prefix', () => {
-                const withFlat = true
-                const addressMeta = buildFakeAddressMeta(withFlat)
-                const { address } = addressMeta
-
-                // Try with automatic preparation
-                const index = address.lastIndexOf(',')
-                const addressWithoutFlat = address.substring(0, index)
-                expect(getAddressUpToBuildingFrom(addressMeta)).toEqual(addressWithoutFlat)
-
-                // Try with manual preparation
-                addressMeta.value = 'г. Москва, ул. Тверская, д 1, кв. 123'
-                addressMeta.data.flat = '123'
-                addressMeta.data.flat_type = 'кв.'
-                expect(getAddressUpToBuildingFrom(addressMeta)).toEqual('г. Москва, ул. Тверская, д 1')
-            })
-
-            it('returns address without flat as is', () => {
-                const addressMeta = buildFakeAddressMeta(false)
-                const { address } = addressMeta
-                expect(getAddressUpToBuildingFrom(addressMeta)).toEqual(address)
-            })
-
-            it('throws an error, if flat is presented in addressMeta without flat_type', () => {
-                const withFlat = true
-                const addressMeta = buildFakeAddressMeta(withFlat)
-                addressMeta.data.flat_type = null
-                const { address } = addressMeta
-
-                catchErrorFrom(async () => {
-                    getAddressUpToBuildingFrom(address, addressMeta)
-                }, (error) => {
-                    expect(error.message).toEqual(FLAT_WITHOUT_FLAT_TYPE_MESSAGE)
-                })
-            })
-
         })
     })
 })
