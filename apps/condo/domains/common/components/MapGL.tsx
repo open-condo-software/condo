2235803a9ff1ef7e92e6299629f68a94b9fc3a2c
MrFoxPro <tuningiposadka@gmail.com>
2021-08-20 10:08:34 +0500

    SBERDOMA-952 division list page

--- apps/condo/domains/common/components/MapGL.tsx
+++ apps/condo/domains/common/components/MapGL.tsx
@@ -1,3 +1,4 @@
+/** @jsx jsx */
 import React, { useEffect, useState } from 'react'
 
 import { load } from '@2gis/mapgl'
@@ -8,6 +9,8 @@ import { Card, Tag } from 'antd'
 const GUEST_API_KEY = 'bfd8bbca-8abf-11ea-b033-5fa57aae2de7'
 
 import getConfig from 'next/config'
+
+import { jsx } from '@emotion/core'
 
 const {
     publicRuntimeConfig: { mapApiKey },
@@ -22,12 +25,13 @@ export interface IPointsOfInterest {
 
 export interface IMapProps {
     points: [IPointsOfInterest],
+    containerCss?: any
 }
 
 
 const _toArrCoordinates = ({ lng, lat }) => [Number(lng), Number(lat)] 
 
-export const MapGL: React.FC<IMapProps> = ({ points }) => {
+export const MapGL: React.FC<IMapProps> = ({ points, containerCss }) => {
     
     const [map, setMap] = useState(null)
     const [api, setApi] = useState(null)
@@ -49,6 +53,7 @@ export const MapGL: React.FC<IMapProps> = ({ points }) => {
                 map.invalidateSize()
             })
             window.addEventListener('resize', handleResize)
+            // console.log(container)
         })
         return () => {
             window.removeEventListener('resize', handleResize)
@@ -76,7 +81,7 @@ export const MapGL: React.FC<IMapProps> = ({ points }) => {
 
     return (
         <>  
-            <div id="map-container" style={{ position: 'absolute', top: 140, bottom: 0, right: 0, left: 0 }} />
+            <div id="map-container" css={containerCss}/>
             { selected ? 
                 <Card style={{ width: 300, bottom: 20, position: 'absolute' }} >
                     <p>
---
+++ apps/condo/domains/division/hooks/useTableColumns.tsx
@@ -0,0 +1,93 @@
+import { colors } from '@condo/domains/common/constants/style'
+import { FilterValue } from 'antd/es/table/interface'
+
+import React, { useMemo } from 'react'
+import { useIntl } from '@core/next/intl'
+
+// import { createSorterMap, IFilters } from '../utils/helpers'
+import get from 'lodash/get'
+
+import { Typography } from 'antd'
+import { isEmpty } from 'lodash'
+import { Highliter } from '@condo/domains/common/components/Highliter'
+import { getTextFilterDropdown, getFilterIcon } from '@condo/domains/common/components/TableFilter'
+import { EmptyTableCell } from '@condo/domains/common/components/EmptyTableCell'
+
+export interface ITableColumn {
+    title: string,
+    ellipsis?: boolean,
+    sortOrder?: string,
+    filteredValue?: FilterValue,
+    dataIndex?: string,
+    key?: string,
+    sorter?: boolean,
+    width?: string,
+    filterDropdown?: unknown,
+    filterIcon?: unknown
+}
+
+const getFilteredValue = (filters: any, key: string | Array<string>): FilterValue => get(filters, key, null)
+
+export const useTableColumns = (sort: Array<string>, filters: any,
+    setFiltersApplied: React.Dispatch<React.SetStateAction<boolean>>): Array<ITableColumn> => {
+    const intl = useIntl()
+    const columns = useMemo(() => {
+        const DivisionTitleMessage = intl.formatMessage({ id: 'pages.condo.property.index.TableField.Division' })
+        const BuildingsTitleMessage = intl.formatMessage({ id: 'pages.condo.property.index.TableField.Buildings' })
+        const ForemanTitleMessage = intl.formatMessage({ id: 'pages.condo.property.index.TableField.Foreman' })
+        const TechiesTitleMessage = intl.formatMessage({ id: 'pages.condo.property.index.TableField.Techies' })
+        // const sorterMap = createSorterMap(sort)
+        // const search = getFilteredValue(filters, 'search')
+        // const render = (text) => {
+        //     let result = text
+        //     if (!isEmpty(search) && text) {
+        //         result = (
+        //             <Highliter
+        //                 text={String(text)}
+        //                 search={String(search)}
+        //                 renderPart={(part) => (
+        //                     <Typography.Text style={{ backgroundColor: colors.markColor }}>
+        //                         {part}
+        //                     </Typography.Text>
+        //                 )}
+        //             />
+        //         )
+        //     }
+        //     return (<EmptyTableCell>{result}</EmptyTableCell>)
+        // }
+        return [
+            {
+                title: DivisionTitleMessage,
+                ellipsis: true,
+                // sortOrder: get(sorterMap, 'address'),
+                filteredValue: getFilteredValue(filters, 'address'),
+                dataIndex: 'unitName',
+                key: 'name',
+                width: '25%',
+            },
+            {
+                title: BuildingsTitleMessage,
+                ellipsis: true,
+                dataIndex: 'address',
+                key: 'address',
+                width: '25%',
+            },
+            {
+                title: ForemanTitleMessage,
+                ellipsis: true,
+                dataIndex: 'unitName',
+                key: 'unitName',
+                width: '25%',
+            },
+            {
+                title: TechiesTitleMessage,
+                ellipsis: true,
+                dataIndex: 'unitName',
+                key: 'unitName',
+                width: '25%',
+            },
+        ]
+    }, [sort, filters, intl])
+
+    return columns
+}
