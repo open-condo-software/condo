148c3075a318d38b5d172f2b934c9479f3e3788a
Dimitreee <danewdima@gmail.com>
2021-08-24 18:16:04 +0300

    SBERDOMA-763 remove common interface

--- apps/condo/next-env.d.ts
+++ apps/condo/next-env.d.ts
@@ -1,9 +1,2 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
-
-import React from 'react'
-
-interface IPageInterface extends React.FC{
-    headerAction?: React.ReactElement
-    container?: React.FC
-}
