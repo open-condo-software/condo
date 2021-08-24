b70e8dcfbf7b0291d71eeed6bf75522dbee9039c
Pavel <nomerdvadcatpyat@gmail.com>
2021-08-24 16:32:56 +0500

    SBERDOMA-1012 MeterStatus without default values

---
+++ apps/condo/domains/meter/constants/statusTypes.js
@@ -0,0 +1,15 @@
+const NEW_OR_REOPEND = 'new_or_reopened'
+const PROCESSING = 'processing'
+const CANCELED = 'canceled'
+const COMPLETED = 'completed'
+const DEFERRED = 'deferred'
+const CLOSED = 'closed'
+
+module.exports = {
+    NEW_OR_REOPEND,
+    PROCESSING,
+    CANCELED,
+    COMPLETED,
+    DEFERRED,
+    CLOSED,
+}
