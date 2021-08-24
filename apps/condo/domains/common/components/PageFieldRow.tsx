a1fef54824e86f6e4f1dd924d662d621e4c3e3b3
Антон Алёшин <Anton_Aleschin@mail.ru>
2021-08-24 16:33:05 +0300

    SBERDOMA-1028 extracted name-value display into PageFieldRow
    It defines common text formatting and layout rules and implements automatic span calculation

---
+++ apps/condo/domains/common/components/PageFieldRow.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Col, Typography } from 'antd'
+
+interface IPageFieldRowProps {
+    title: string
+    style?: React.CSSProperties
+    highlight?: boolean
+    children: React.ReactNode
+    labelSpan: number
+}
+
+const PageFieldRow: React.FC<IPageFieldRowProps> = ({ labelSpan, title, children, highlight, style }) => {
+    return (
+        <>
+            <Col span={labelSpan} style={{ fontSize: '16px' }}>
+                {title}
+            </Col>
+            <Col span={24 - labelSpan - 1} push={1} style={{ fontSize: '16px' }}>
+                <Typography.Text
+                    type={highlight ? 'success' : null}
+                    style={{ wordWrap: 'break-word' }}
+                >
+                    {children}
+                </Typography.Text>
+            </Col>
+        </>
+    )
+}
+
+PageFieldRow.defaultProps = {
+    labelSpan: 8,
+}
+
+export { PageFieldRow }
