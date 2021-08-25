07ea1ca4904f41f15f510753755850b98eca65ae
Pavel <nomerdvadcatpyat@gmail.com>
2021-08-25 13:20:30 +0500

    SBERDOMA-1012 MeterReadingTicketSource schema

--- apps/condo/domains/meter/utils/serverSchema/index.js
+++ apps/condo/domains/meter/utils/serverSchema/index.js
@@ -8,29 +8,29 @@ const { generateServerUtils, execGqlWithoutAccess } = require('@condo/domains/co
 
 const { MeterResource: MeterResourceGQL } = require('@condo/domains/meter/gql')
 const { Meter: MeterGQL } = require('@condo/domains/meter/gql')
-const { MeterSource: MeterSourceGQL } = require('@condo/domains/meter/gql')
 const { MeterReading: MeterReadingGQL } = require('@condo/domains/meter/gql')
 const { MeterReadingTicket: MeterReadingTicketGQL } = require('@condo/domains/meter/gql')
 const { MeterReadingTicketChange: MeterReadingTicketChangeGQL } = require('@condo/domains/meter/gql')
 const { MeterReadingTicketComment: MeterReadingTicketCommentGQL } = require('@condo/domains/meter/gql')
+const { MeterReadingTicketSource: MeterReadingTicketSourceGQL } = require('@condo/domains/meter/gql')
 /* AUTOGENERATE MARKER <IMPORT> */
 
 const MeterResource = generateServerUtils(MeterResourceGQL)
 const Meter = generateServerUtils(MeterGQL)
-const MeterSource = generateServerUtils(MeterSourceGQL)
 const MeterReading = generateServerUtils(MeterReadingGQL)
 const MeterReadingTicket = generateServerUtils(MeterReadingTicketGQL)
 const MeterReadingTicketChange = generateServerUtils(MeterReadingTicketChangeGQL)
 const MeterReadingTicketComment = generateServerUtils(MeterReadingTicketCommentGQL)
+const MeterReadingTicketSource = generateServerUtils(MeterReadingTicketSourceGQL)
 /* AUTOGENERATE MARKER <CONST> */
 
 module.exports = {
     MeterResource,
     Meter,
-    MeterSource,
     MeterReading,
     MeterReadingTicket,
     MeterReadingTicketChange,
     MeterReadingTicketComment,
+    MeterReadingTicketSource,
 /* AUTOGENERATE MARKER <EXPORTS> */
 }
