ba01211af124f84bb330118932574d0a331364f7
matthewsavelev <savelevmatthew@gmail.com>
2021-08-26 20:34:14 +0500

    SBERDOMA-1069 Updated generators + space separation for money render

--- apps/condo/domains/common/components/Table/Renders.tsx
+++ apps/condo/domains/common/components/Table/Renders.tsx
@@ -8,6 +8,7 @@ import { EmptyTableCell } from './EmptyTableCell'
 type RenderReturnType = string | React.ReactNode
 
 const DEFAULT_CURRENCY_SEPARATOR = '.'
+const MONEY_PARTS_SEPARATOR = ' '
 
 export const getTextRender = (search: string) => {
     return function render (text: string): RenderReturnType {
@@ -29,18 +30,33 @@ export const getTextRender = (search: string) => {
     }
 }
 
-const recolorMoney = (text: string, startIndex: number, separatorIndex: number, extraStyles: React.CSSProperties) => {
+const fillMoneyWithSpaces = (substring: string, startIndex: number, spaces: Array<number>) => {
+    const chars = []
+    for (let i = 0; i < substring.length; i++) {
+        const absoluteIndex = startIndex + i
+        const char = substring.charAt(i)
+        if (spaces.includes(absoluteIndex)) {
+            chars.push(MONEY_PARTS_SEPARATOR)
+        }
+        chars.push(char)
+    }
+    return chars.join('')
+}
+
+const recolorMoney = (
+    text: string, startIndex: number, separatorIndex: number, spaces: Array<number>, extraStyles: React.CSSProperties
+) => {
     if (separatorIndex === - 1 || startIndex + text.length <= separatorIndex) {
         return (
             <Typography.Text style={extraStyles}>
-                {text}
+                {fillMoneyWithSpaces(text, startIndex, spaces)}
             </Typography.Text>
         )
     }
     if (startIndex >= separatorIndex) {
         return (
             <Typography.Text style={{ color: colors.sberGrey[3], ...extraStyles }}>
-                {text}
+                {fillMoneyWithSpaces(text, startIndex, spaces)}
             </Typography.Text>
         )
     }
@@ -48,7 +64,7 @@ const recolorMoney = (text: string, startIndex: number, separatorIndex: number, 
     return (
         <>
             <Typography.Text style={extraStyles}>
-                {text.substring(0, blackLength)}
+                {fillMoneyWithSpaces(text.substring(0, blackLength), startIndex, spaces)}
             </Typography.Text>
             <Typography.Text style={{ color: colors.sberGrey[3], ...extraStyles }}>
                 {text.substring(blackLength)}
@@ -61,11 +77,16 @@ export const getMoneyRender = (search: string, currencyMark = '₽', partSeparat
     return function render (text: string): RenderReturnType {
         if (!text) return <EmptyTableCell/>
         const separatorIndex = text.indexOf(partSeparator)
+        const lastSymbol = text.startsWith('-') ? 2 : 1
+        const spaces = []
+        for (let i = separatorIndex - 3; i >= lastSymbol; i -= 3) {
+            spaces.push(i)
+        }
         const markStyles = { backgroundColor: colors.markColor }
         if (isEmpty(search)) {
             return (
                 <>
-                    {recolorMoney(text, 0, separatorIndex, {})}
+                    {recolorMoney(text, 0, separatorIndex, spaces, {})}
                     &nbsp;
                     {currencyMark}
                 </>
@@ -77,7 +98,7 @@ export const getMoneyRender = (search: string, currencyMark = '₽', partSeparat
                     text={text}
                     search={search}
                     renderPart={(part, startIndex, marked) =>
-                        recolorMoney(part, startIndex, separatorIndex, marked ? markStyles : {})}
+                        recolorMoney(part, startIndex, separatorIndex, spaces, marked ? markStyles : {})}
                 />
                 &nbsp;
                 {currencyMark}
