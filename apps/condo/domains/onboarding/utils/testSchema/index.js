9755b7c30e01976a916a0d94c021b92b10ac2ea9
Dimitreee <danewdima@gmail.com>
2021-08-26 06:59:53 +0300

    SBERDOMA-763 update onboarding tests

--- apps/condo/domains/onboarding/utils/testSchema/index.js
+++ apps/condo/domains/onboarding/utils/testSchema/index.js
@@ -36,8 +36,6 @@ async function updateTestOnBoarding (client, id, extraAttrs = {}) {
     if (!id) throw new Error('no id')
     const sender = { dv: 1, fingerprint: faker.random.alphaNumeric(8) }
 
-    // TODO(codegen): check the updateTestOnBoarding logic for generate fields
-
     const attrs = {
         dv: 1,
         sender,
