597f47e8fe2c96ef748c58f9130ac54533a8410f
Pavel <nomerdvadcatpyat@gmail.com>
2021-08-27 13:05:38 +0500

    SBERDOMA-1012 refactor canManageMeterEntity, add source and resource id to constants

--- apps/condo/domains/meter/schema/Meter.js
+++ apps/condo/domains/meter/schema/Meter.js
@@ -3,7 +3,6 @@
  */
 
 const { Text, Relationship } = require('@keystonejs/fields')
-const { Json } = require('@core/keystone/fields')
 const { GQLListSchema } = require('@core/keystone/schema')
 const { historical, versioned, uuided, tracked, softDeleted } = require('@core/keystone/plugins')
 const { SENDER_FIELD, DV_FIELD } = require('@condo/domains/common/schema/fields')
