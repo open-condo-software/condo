1303a9b988eb85e1ac1e6d03b37c1f5031edc5ea
Dmitry Koviazin <dkoviazin@gmail.com>
2021-08-31 13:31:05 +0500

    SBERDOMA-992 Add mixed loader knex+gql

--- apps/condo/domains/common/utils/serverSchema/index.js
+++ apps/condo/domains/common/utils/serverSchema/index.js
@@ -1,18 +1,107 @@
 
-const loadModelsByChunks = async ({
+const { getItems } = require('@keystonejs/server-side-graphql-client')
+const { getSchemaCtx } = require('@core/keystone/schema')
+const GLOBAL_QUERY_LIMIT = 1000
+
+// When we load models with Apollo graphql - every relation on a field for every object makes sql request
+// For example, loading 50 tickets will cause a result of ~1000 sql gueries which is near server limit
+// But we want to keep sortBy and where functionality from gql
+// What we do:
+// 1. We use gql to load fields from main table with sort and where
+// 2. We use knex to load all relations in one sql request for ids in 1.
+// 3. We can use aggregate functions from knex on multiple relation fields
+// 4. We merge 1 and 2
+// 5. We load all data by chunks (size of chunk is equal to global guery sql limit)
+// It's about ~4.5 times faster then using only gql queries
+// Tested on tickets export for 24755 tickets: without knex  71220.902ms, with knex: 16094.841ms )
+// TODO(zuch): find out how to make 1 request for 1. and 2.
+
+class GqlWithKnexLoadList {
+
+    constructor ({ listKey, fields, singleRelations = [], multipleRelations = [], where = {}, sortBy = [] }) {
+        this.listKey = listKey
+        this.fields = fields
+        this.where = where
+        this.sortBy = sortBy
+        this.singleRelations = singleRelations
+        this.multipleRelations = multipleRelations
+    }
+
+    async load () {
+        const { keystone: modelAdapter } = await getSchemaCtx(this.listKey)
+        this.keystone = modelAdapter
+        this.knex = modelAdapter.adapter.knex
+        let skip = 0
+        let newchunk = []
+        let all = []
+        let maxiterationsCount = 100 // we need some limits - 100K records is more then enough
+        do {
+            newchunk = await this.loadChunk(skip)
+            all = all.concat(newchunk)
+            skip += newchunk.length
+            if (newchunk.length < GLOBAL_QUERY_LIMIT) {
+                break
+            }
+        } while (--maxiterationsCount > 0 && newchunk.length)
+        return all
+    }
+
+    async loadChunk (skip = 0) {
+        const mainTableObjects = await getItems({
+            keystone: this.keystone,
+            listKey: this.listKey,
+            where: this.where,
+            sortBy: this.sortBy,
+            skip: skip,
+            first: GLOBAL_QUERY_LIMIT,
+            returnFields: this.fields,
+        })
+        if (mainTableObjects.length === 0) {
+            return []
+        }
+        const ids = mainTableObjects.map(object => object.id)
+        const knexQuery = this.knex(`${this.listKey} as mainModel`)
+        knexQuery.select('mainModel.id')
+        if (this.multipleRelations.length !== 0) {
+            knexQuery.groupBy('mainModel.id')
+        }
+        this.singleRelations.forEach(([ Model, fieldName, value ], idx) => {
+            knexQuery.select(`sr${idx}.${value} as ${fieldName}`)
+            if (this.multipleRelations.length !== 0) {
+                knexQuery.groupBy(`sr${idx}.${value}`)
+            }
+            knexQuery.leftJoin(`${Model} as sr${idx}`, `sr${idx}.id`, `mainModel.${fieldName}`)
+        })
+        this.multipleRelations.forEach(([select, join], idx) => {
+            knexQuery.select(select(idx, this.knex))
+            knexQuery.leftJoin(...join(idx))
+        })
+        knexQuery.whereIn('mainModel.id', ids)
+        const joinTablesObjects = await knexQuery
+        const main = Object.fromEntries(mainTableObjects.map(object => ([object.id, object])))
+        const joins = Object.fromEntries(joinTablesObjects.map(object => ([object.id, object])))
+        const merged = {}
+        for (const id in main) {
+            merged[id] = { ...main[id], ...joins[id] }
+        }
+        return Object.values(merged)
+    }
+}
+// Simple way to load all models
+const loadListByChunks = async ({
     context,
-    model,
+    list,
     where = {},
     sortBy = ['createdAt_ASC'],
     chunkSize = 100,
     limit = 100000,
 }) => {
     let skip = 0
-    let maxiterationsCount = Math.floor(limit / chunkSize)
+    let maxiterationsCount = Math.ceil(limit / chunkSize)
     let newchunk = []
     let all = []
     do {
-        newchunk = await model.getAll(context, where, { sortBy, first: chunkSize, skip: skip })
+        newchunk = await list.getAll(context, where, { sortBy, first: chunkSize, skip: skip })
         all = all.concat(newchunk)
         skip += newchunk.length
     } while (--maxiterationsCount > 0 && newchunk.length)
@@ -21,5 +110,6 @@ const loadModelsByChunks = async ({
 
 
 module.exports = {
-    loadModelsByChunks,
+    GqlWithKnexLoadList,
+    loadListByChunks,
 }
--- apps/condo/domains/ticket/schema/ExportTicketsService.js
+++ apps/condo/domains/ticket/schema/ExportTicketsService.js
@@ -1,14 +1,13 @@
 const { GQLCustomSchema } = require('@core/keystone/schema')
 const access = require('@condo/domains/ticket/access/ExportTicketsService')
-const { Ticket, TicketComment, TicketChange, TicketStatus } = require('@condo/domains/ticket/utils/serverSchema')
+const { TicketStatus, loadTicketsForExcelExport } = require('@condo/domains/ticket/utils/serverSchema')
 const moment = require('moment')
 const { createExportFile } = require('@condo/domains/common/utils/createExportFile')
-const { has, get } = require('lodash')
 const { DEFAULT_ORGANIZATION_TIMEZONE } = require('@condo/domains/organization/constants/common')
 const { normalizeTimeZone } = require('@condo/domains/common/utils/timezone')
-const { loadModelsByChunks } = require('@condo/domains/common/utils/serverSchema')
 const { EMPTY_DATA_EXPORT_ERROR } = require('@condo/domains/common/constants/errors')
 const DATE_FORMAT = 'DD.MM.YYYY HH:mm'
+
 
 
 // TODO(zuch): if we add timeZone and locale to organization settings use organization timeZone instead of client's timezone
@@ -31,62 +30,17 @@ const ExportTicketsService = new GQLCustomSchema('ExportTicketsService', {
                 const { where, sortBy, timeZone: timeZoneFromUser } = args.data
                 const timeZone = normalizeTimeZone(timeZoneFromUser) || DEFAULT_ORGANIZATION_TIMEZONE
                 const formatDate = (date) => moment(date).tz(timeZone).format(DATE_FORMAT)
-
-                const allTickets = await loadModelsByChunks({
-                    context,
-                    model: Ticket,
-                    where,
-                    sortBy,
-                    chunkSize: 20,
-                    limit: 10000,
-                })
-
+                const statuses = await TicketStatus.getAll(context, {})
+                const indexedStatuses = Object.fromEntries(statuses.map(status => ([status.type, status.name])))
+                const allTickets = await loadTicketsForExcelExport({ where, sortBy })
                 if (allTickets.length === 0) {
                     throw new Error(`${EMPTY_DATA_EXPORT_ERROR}] empty export file`)
                 }
-                const ticketIds = allTickets.map(ticket => ticket.id)
-
-                const comments = await loadModelsByChunks({
-                    context,
-                    model: TicketComment,
-                    where: { ticket: { id_in: ticketIds } },
-                })
-
-                const indexedComments = {}
-                comments.forEach(comment => {
-                    if (!has(indexedComments, comment.ticket.id)) {
-                        indexedComments[comment.ticket.id] = []
-                    }
-                    indexedComments[comment.ticket.id].push(comment.content)
-                })
-
-                const statuses = await TicketStatus.getAll(context, { type_in: ['processing', 'canceled', 'completed'] })
-                const indexedStatuses = Object.fromEntries(statuses.map(({ id, type }) => ([id, type])))
-
-                const statusChanges = await loadModelsByChunks({
-                    context,
-                    model: TicketChange,
-                    where: { ticket: { id_in: ticketIds }, statusIdTo_in: Object.keys(indexedStatuses) },
-                })
-
-                const statusDateByTickets = {}
-                statusChanges.forEach(statusChange => {
-                    if (!has(statusDateByTickets, statusChange.ticket.id)){
-                        statusDateByTickets[statusChange.ticket.id] = {}
-                    }
-                    statusDateByTickets[statusChange.ticket.id][indexedStatuses[statusChange.statusIdTo]] = statusChange.createdAt
-                })
                 const excelRows = allTickets.map(ticket => {
-                    let inWork = get(statusDateByTickets, `${ticket.id}.processing`, '')
-                    // When ticket created with assigner it will aoutomatically get processing status on creation
-                    if (ticket.status.type !== 'new_or_reopened' && !inWork) {
-                        inWork = ticket.createdAt
-                    }
-                    const completed = get(statusDateByTickets, `${ticket.id}.completed`) ||  get(statusDateByTickets, `${ticket.id}.canceled`, '')
                     return {
                         number: ticket.number,
-                        organization: ticket.organization.name,
-                        property: ticket.property.address,
+                        organization: ticket.organization,
+                        property: ticket.property,
                         unitName: ticket.unitName,
                         entranceName: ticket.sectionName,
                         floorName: ticket.floorName,
@@ -95,19 +49,18 @@ const ExportTicketsService = new GQLCustomSchema('ExportTicketsService', {
                         details: ticket.details,
                         isEmergency: ticket.isEmergency ? 'X' : '',
                         isPaid: ticket.isPaid ? 'X' : '',
-                        classifier: get(ticket.classifier, 'name', ''),
-                        place: get(ticket.placeClassifier, 'name', ''),
-                        category: get(ticket.categoryClassifier, 'name', ''),
-                        description: get(ticket.problemClassifier, 'name', ''),
+                        place: ticket.placeClassifier || '',
+                        category: ticket.categoryClassifier || '',
+                        description: ticket.problemClassifier || '',
                         createdAt: formatDate(ticket.createdAt),
                         updatedAt: formatDate(ticket.updatedAt),
-                        inworkAt: inWork ? formatDate(inWork) : '',
-                        completedAt: completed ? formatDate(completed) : '',
-                        status: ticket.status.name,
-                        operator: get(ticket, 'operator.name', '') || get(ticket, 'createdBy.name', ''),
-                        executor: get(ticket, 'executor.name', ''),
-                        assignee: get(ticket, 'assignee.name', ''),
-                        comments: indexedComments[ticket.id] ? indexedComments[ticket.id].join('\n') : '',
+                        inworkAt: ticket.startedAt ? formatDate(ticket.startedAt) : '',
+                        completedAt: ticket.completedAt ? formatDate(ticket.completedAt) : '',
+                        status: indexedStatuses[ticket.status],
+                        operator: ticket.operator || '',
+                        executor: ticket.executor || '',
+                        assignee: ticket.assignee || '',
+                        comments: ticket.TicketComment.join('\n'),
                     }
                 })
                 const linkToFile = await createExportFile({
