890d2bc53c1373107b3a3ce75295674b7e6d9d9b
Sitnikov Ivan <sit7602@gmail.com>
2021-09-01 17:17:23 +0500

    SBERDOMA-505 - labels alphabetic sort

--- apps/condo/pages/reports/detail/report_by_tickets/index.tsx
+++ apps/condo/pages/reports/detail/report_by_tickets/index.tsx
@@ -46,7 +46,7 @@ import { ReturnBackHeaderAction } from '@condo/domains/common/components/HeaderA
 import TicketChartView from '@condo/domains/ticket/components/analytics/TicketChartView'
 import TicketListView from '@condo/domains/ticket/components/analytics/TicketListView'
 import { DATE_DISPLAY_FORMAT, TICKET_REPORT_DAY_GROUP_STEPS } from '@condo/domains/ticket/constants/common'
-import { TicketAnalyticsGroupBy, TicketAnalyticsReportOutput, TicketGroupedCounter } from '../../../../schema'
+import { TicketGroupedCounter } from '../../../../schema'
 
 dayjs.extend(quarterOfYear)
 
@@ -429,10 +429,11 @@ const TicketAnalyticsPage: ITicketAnalyticsPage = () => {
                         const series = []
 
                         const legend = [...new Set(Object.values(data).flatMap(e => Object.keys(e)))]
+                            .sort((a, b) => a.localeCompare(b))
                         Object.entries(data).forEach(([label, groupObject]) => {
                             const chartData = Object.entries(groupObject)
                                 .map(([name, value]) => ({ name, value }))
-                                .sort((a, b) => b.value - a.value)
+                                .sort((a, b) => a.name.localeCompare(b.name))
                             series.push({
                                 name: label,
                                 data: chartData,
