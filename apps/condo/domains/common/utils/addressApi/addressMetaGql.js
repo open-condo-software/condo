c73793dc7891dae19569a0e5c44fb15afe7ba39e
Антон Алёшин <Anton_Aleschin@mail.ru>
2021-09-01 20:15:24 +0300

    SBERDOMA-1122 strong GraphQL type for `addressMeta` JSON field

---
+++ apps/condo/domains/common/utils/addressApi/addressMetaGql.js
@@ -0,0 +1,190 @@
+const ADDRESS_META_GRAPHQL_TYPES = `
+    type AddressMetaData {
+        postal_code: String!
+        country: String!
+        country_iso_code: String!
+        federal_district: String
+        region_fias_id: String!
+        region_kladr_id: String!
+        region_iso_code: String!
+        region_with_type: String!
+        region_type: String!
+        region_type_full: String!
+        region: String!
+        area_fias_id: String
+        area_kladr_id: String
+        area_with_type: String
+        area_type: String
+        area_type_full: String
+        area: String
+        city_fias_id: String!
+        city_kladr_id: String!
+        city_with_type: String!
+        city_type: String!
+        city_type_full: String!
+        city: String!
+        city_area: String
+        city_district_fias_id: String
+        city_district_kladr_id: String
+        city_district_with_type: String
+        city_district_type: String
+        city_district_type_full: String
+        city_district: String
+        settlement_fias_id: String
+        settlement_kladr_id: String
+        settlement_with_type: String
+        settlement_type: String
+        settlement_type_full: String
+        settlement: String
+        street_fias_id: String!
+        street_kladr_id: String!
+        street_with_type: String!
+        street_type: String!
+        street_type_full: String!
+        street: String!
+        house_fias_id: String!
+        house_kladr_id: String!
+        house_type: String!
+        house_type_full: String!
+        house: String!
+        block_type: String
+        block_type_full: String
+        block: String
+        entrance: String
+        floor: String
+        flat_fias_id: String
+        flat_type: String
+        flat_type_full: String
+        flat: String
+        flat_area: String
+        square_meter_price: String
+        flat_price: String
+        postal_box: String
+        fias_id: String!
+        fias_code: String!
+        fias_level: String!
+        fias_actuality_state: String!
+        kladr_id: String!
+        geoname_id: String!
+        capital_marker: String!
+        okato: String!
+        oktmo: String!
+        tax_office: String!
+        tax_office_legal: String!
+        timezone: String
+        geo_lat: String!
+        geo_lon: String!
+        beltway_hit: String
+        beltway_distance: String
+        metro: String
+        qc_geo: String!
+        qc_complete: String
+        qc_house: String
+        history_values: [String]
+        unparsed_parts: String
+        source: String
+        qc: String
+    }
+    
+    type AddressMeta {
+        dv: Int!
+        value: String!
+        unrestricted_value: String!
+        data: AddressMetaData!
+    }
+`
+
+const ADDRESS_META_DATA_FIELDS = [
+    'postal_code',
+    'country',
+    'country_iso_code',
+    'federal_district',
+    'region_fias_id',
+    'region_kladr_id',
+    'region_iso_code',
+    'region_with_type',
+    'region_type',
+    'region_type_full',
+    'region',
+    'area_fias_id',
+    'area_kladr_id',
+    'area_with_type',
+    'area_type',
+    'area_type_full',
+    'area',
+    'city_fias_id',
+    'city_kladr_id',
+    'city_with_type',
+    'city_type',
+    'city_type_full',
+    'city',
+    'city_area',
+    'city_district_fias_id',
+    'city_district_kladr_id',
+    'city_district_with_type',
+    'city_district_type',
+    'city_district_type_full',
+    'city_district',
+    'settlement_fias_id',
+    'settlement_kladr_id',
+    'settlement_with_type',
+    'settlement_type',
+    'settlement_type_full',
+    'settlement',
+    'street_fias_id',
+    'street_kladr_id',
+    'street_with_type',
+    'street_type',
+    'street_type_full',
+    'street',
+    'house_fias_id',
+    'house_kladr_id',
+    'house_type',
+    'house_type_full',
+    'house',
+    'block_type',
+    'block_type_full',
+    'block',
+    'entrance',
+    'floor',
+    'flat_fias_id',
+    'flat_type',
+    'flat_type_full',
+    'flat',
+    'flat_area',
+    'square_meter_price',
+    'flat_price',
+    'postal_box',
+    'fias_id',
+    'fias_code',
+    'fias_level',
+    'fias_actuality_state',
+    'kladr_id',
+    'geoname_id',
+    'capital_marker',
+    'okato',
+    'oktmo',
+    'tax_office',
+    'tax_office_legal',
+    'timezone',
+    'geo_lat',
+    'geo_lon',
+    'beltway_hit',
+    'beltway_distance',
+    'metro',
+    'qc_geo',
+    'qc_complete',
+    'qc_house',
+    'history_values',
+    'unparsed_parts',
+    'source',
+    'qc',
+]
+
+const ADDRESS_META_SUBFIELDS_QUERY_LIST = `dv value unrestricted_value data { ${ADDRESS_META_DATA_FIELDS.join(' ')} }`
+
+module.exports = {
+    ADDRESS_META_GRAPHQL_TYPES,
+    ADDRESS_META_SUBFIELDS_QUERY_LIST,
+    ADDRESS_META_DATA_FIELDS,
+}
--- apps/condo/domains/common/utils/testSchema/factories.js
+++ apps/condo/domains/common/utils/testSchema/factories.js
@@ -1,44 +1,65 @@
 const faker = require('faker')
+const { sample } = require('lodash')
 const { AddressMetaDataFields } = require('../addressApi/AddressMetaSchema')
+
+const FIAS_LEVELS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 35, 65, 75, 90, 91]
+const FIAS_ACTUALITY_STATE = [0, 1]
 
 const buildFakeAddressMeta = (withFlat = false) => {
     const emptyData = Object.assign({}, ...AddressMetaDataFields.map((field) => ({[field]: null})))
     emptyData.postal_code = faker.address.zipCode()
     emptyData.country = faker.address.country()
     emptyData.country_iso_code = faker.address.countryCode()
+    emptyData.capital_marker = String(faker.datatype.number())
+
+    emptyData.fias_id = faker.datatype.uuid()
+    emptyData.fias_level = String(sample(FIAS_LEVELS))
+    emptyData.fias_code = String(faker.datatype.number())
+    emptyData.fias_actuality_state = String(sample(FIAS_ACTUALITY_STATE))
+    emptyData.kladr_id = String(faker.datatype.number())
+    emptyData.geoname_id = String(faker.datatype.number())
+    emptyData.okato = String(faker.datatype.number())
+    emptyData.oktmo = String(faker.datatype.number())
+    emptyData.tax_office = String(faker.datatype.number())
+    emptyData.tax_office_legal = String(faker.datatype.number())
+    emptyData.qc_geo = String(faker.datatype.number())
 
     emptyData.city = faker.address.city()
-    emptyData.city_type = 'c'
-    emptyData.city_type_full = 'city'
+    emptyData.city_type = 'г'
+    emptyData.city_type_full = 'город'
     emptyData.city_with_type = `${emptyData.city_type} ${emptyData.city}`
+    emptyData.city_fias_id = faker.datatype.uuid()
+    emptyData.city_kladr_id = String(faker.datatype.number())
 
     emptyData.street = faker.address.streetName()
-    emptyData.street_type = 'st'
-    emptyData.street_type_full = 'street'
+    emptyData.street_type = 'ул'
+    emptyData.street_type_full = 'улица'
     emptyData.street_with_type = `${emptyData.street_type} ${emptyData.street}`
+    emptyData.street_fias_id = faker.datatype.uuid()
+    emptyData.street_kladr_id = String(faker.datatype.number())
 
     emptyData.house = String(faker.datatype.number())
-    emptyData.house_type = 'h'
-    emptyData.house_type_full = 'house'
+    emptyData.house_type = 'д'
+    emptyData.house_type_full = 'дом'
+    emptyData.house_fias_id = faker.datatype.uuid()
+    emptyData.house_kladr_id = String(faker.datatype.number())
 
-    if (faker.datatype.boolean()) {
-        emptyData.block = String(faker.datatype.number())
-        emptyData.block_type = 'b'
-        emptyData.block_type_full= 'block'
-    }
+    emptyData.block = String(faker.datatype.number())
+    emptyData.block_type = 'б'
+    emptyData.block_type_full= 'блок'
 
-    if (faker.datatype.boolean()) {
-        emptyData.region = faker.address.state()
-        emptyData.region_iso_code = faker.address.stateAbbr()
-        emptyData.region_type_full = 'region'
-        emptyData.region_type = 'r'
-        emptyData.region_with_type = `${emptyData.region_type} ${emptyData.region}`
-    }
+    emptyData.region = faker.address.state()
+    emptyData.region_iso_code = faker.address.stateAbbr()
+    emptyData.region_type_full = 'регион'
+    emptyData.region_type = 'р'
+    emptyData.region_with_type = `${emptyData.region_type} ${emptyData.region}`
+    emptyData.region_fias_id = faker.datatype.uuid()
+    emptyData.region_kladr_id = String(faker.datatype.number())
 
     if (withFlat) {
         emptyData.flat = String(faker.datatype.number())
-        emptyData.flat_type = 'fl'
-        emptyData.flat_type_full = 'flat'
+        emptyData.flat_type = 'кв'
+        emptyData.flat_type_full = 'квартира'
     }
 
     emptyData.geo_lat = faker.address.latitude()
@@ -57,13 +78,12 @@ const buildFakeAddressMeta = (withFlat = false) => {
         data: emptyData,
         value,
         unrestricted_value: unrestrictedValue,
-        address: value
     }
 }
 
 const buildFakeAddressAndMeta = (withFlat) => {
     const addressMeta = buildFakeAddressMeta(withFlat)
-    let address = addressMeta.address
+    let address = addressMeta.value
     if (withFlat) {
         const index = address.lastIndexOf(',')
         address = address.substring(0, index)
