48f57c2ea0b1964dad8044c1edc8afa374d02767
Антон Алёшин <Anton_Aleschin@mail.ru>
2021-09-02 20:20:36 +0300

    SBERDOMA-1122 cleaned `__typename` field in formData of update property form
    Requested fields of Property of JSON-type, mapped to GraphQL, containing `__typename` field in each typed node.
    It seems, like we cannot control it.
    So, these fields should be cleaned, because it will result to incorrect input into update-mutation.

---
+++ apps/condo/domains/common/utils/omitRecursively.js
@@ -0,0 +1,18 @@
+import { isArray, isObject, omit, transform } from 'lodash'
+
+/**
+ * Omits property with given name from object
+ * @param obj will be transformed to another object without property with name `propToOmit` in any nested object and arrays
+ * @param propToOmit name of property to recursively omit from `obj`
+ * @return {*}
+ */
+export const omitRecursively = (obj, propToOmit) => {
+    const cleanedObj = omit(obj, propToOmit)
+    return transform(cleanedObj, (result, value, key) => (
+        result[key] = isObject(value) && propToOmit in value
+            ? omitRecursively(value, propToOmit)
+            : isArray(value)
+                ? value.map(item => omitRecursively(item, propToOmit))
+                : value
+    ))
+}
