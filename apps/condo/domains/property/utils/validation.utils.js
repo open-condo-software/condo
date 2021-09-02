41e0346db59bc84b9f52ba32337a6d4c03a981b7
Антон Алёшин <Anton_Aleschin@mail.ru>
2021-09-02 15:09:01 +0300

    SBERDOMA-1122 moved `addressMeta` validation utils to "property" domain

--- apps/condo/domains/property/utils/validation.utils.js
+++
@@ -1,9 +0,0 @@
-const { AddressMetaJSONSchema } = require('../../common/utils/addressApi/AddressMetaSchema')
-const Ajv = require('ajv')
-const ajv = new Ajv()
-
-const jsonAddressMetaValidator = ajv.compile(AddressMetaJSONSchema)
-
-module.exports = {
-    jsonAddressMetaValidator,
-}
