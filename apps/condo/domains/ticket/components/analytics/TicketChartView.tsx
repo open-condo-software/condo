ae3bc7b8f01bb67c561dc78e84d4e625d0f3ee4b
Sitnikov Ivan <sit7602@gmail.com>
2021-09-04 18:22:03 +0500

    SBERDOMA-505 - replace pdf full list of pie chart with requestAnimationFrame API

--- apps/condo/domains/ticket/components/analytics/TicketChartView.tsx
+++ apps/condo/domains/ticket/components/analytics/TicketChartView.tsx
@@ -1,4 +1,4 @@
-import React, { useRef,  useEffect, useState, useCallback } from 'react'
+import React, { useRef, useEffect, useState, useCallback, useLayoutEffect } from 'react'
 import { useIntl } from '@core/next/intl'
 import { Skeleton, Typography, List } from 'antd'
 import isEmpty from 'lodash/isEmpty'
@@ -46,8 +46,7 @@ const TicketChartView: React.FC<ITicketAnalyticsPageChartProps> = ({
 }) => {
     const intl = useIntl()
     const NoData = intl.formatMessage({ id: 'NoData' })
-    let series = [], legend = []
-    let axisData = {}, tooltip = {}
+    let legend = [], axisData = {}, tooltip = {}
     const [chartReadyCounter, setChartReadyCounter] = useState<number>(0)
     // Start from 1 because used as multiplier with TICKET_CHART_PAGE_SIZE
     const [pieChartPage, setPieChartPage] = useState(1)
@@ -55,15 +54,15 @@ const TicketChartView: React.FC<ITicketAnalyticsPageChartProps> = ({
     const chartRefs = useRef([])
     // Cache series result for client side paging
     const seriesRef = useRef([])
+    const seriesCacheRef = useRef([])
 
     if (data !== null) {
         const mapperResult = mapperInstance.getChartConfig(viewMode, data)
-        seriesRef.current = mapperResult.series
+        seriesCacheRef.current = mapperResult.series
+        const pageSize = TICKET_CHART_PAGE_SIZE * pieChartPage
+        seriesRef.current = mapperResult.series.slice(0, pageSize)
         // If component used as report widget, we should use page separation. Otherwise we load all data (for pdf page)
-        const pageSize = onChartReady === undefined
-            ? TICKET_CHART_PAGE_SIZE * pieChartPage
-            : seriesRef.current.length
-        series = seriesRef.current.slice(0, pageSize)
+        // series = seriesRef.current.slice(0, pageSize)
         legend = mapperResult.legend
         axisData = mapperResult.axisData
         tooltip = mapperResult.tooltip
@@ -72,21 +71,33 @@ const TicketChartView: React.FC<ITicketAnalyticsPageChartProps> = ({
     useEffect(() => {
         // Clean chart refs if input data was changed
         if (viewMode === 'pie') {
-            // TODO(sitozzz): find way to optimize render speed
-            // (with requestAnimationFrame API or something else that help us draw chart components not in one time)
-            if (series.length !== chartRefs.current.length) {
+            if (seriesRef.current.length !== chartRefs.current.length) {
                 // It means that filters were changed & total chart count may be changed too, so just reset refs & page
                 setChartReadyCounter(0)
                 setPieChartPage(1)
             }
-            chartRefs.current = chartRefs.current.slice(0, series.length)
+            chartRefs.current = chartRefs.current.slice(0, seriesRef.current.length)
         }
     }, [data])
 
     // Way to await moment when all pie chart instance were rendered (needed for client side pdf generation)
-    useEffect(() => {
+    useLayoutEffect(() => {
         if (viewMode === 'pie' && onChartReady !== undefined) {
-            chartReadyCounter === series.length && onChartReady()
+            if (seriesRef.current.length !== 0 && seriesCacheRef.current.length !== 0 && seriesRef.current.length === seriesCacheRef.current.length) {
+                onChartReady()
+                return
+            }
+
+            if (chartReadyCounter < seriesRef.current.length) {
+                let animationFrameId
+                const nextAnimationFrame = () => {
+                    setPieChartPage(x => x + 1)
+                    animationFrameId = requestAnimationFrame(nextAnimationFrame)
+                }
+
+                animationFrameId = requestAnimationFrame(nextAnimationFrame)
+                return () => cancelAnimationFrame(animationFrameId)
+            }
         }
     }, [chartReadyCounter])
 
@@ -124,7 +135,7 @@ const TicketChartView: React.FC<ITicketAnalyticsPageChartProps> = ({
                 borderWidth: 1,
             },
             ...axisData,
-            series,
+            series: seriesRef.current,
         }
 
         const isEmptyDataSet = Object.values(data).every(ticketStatus => {
@@ -181,7 +192,7 @@ const TicketChartView: React.FC<ITicketAnalyticsPageChartProps> = ({
     const chartHeight = get(chartOptions, 'height', 'auto')
     const chartStyle = {}
     const hasMore = pieChartPage * TICKET_CHART_PAGE_SIZE <= seriesRef.current.length
-    let infiniteScrollContainerHeight = series.length > 2 ? '640px' : '340px'
+    let infiniteScrollContainerHeight = seriesRef.current.length > 2 ? '640px' : '340px'
     // onChartReadyCallback is used only for pdf generation page to make sure that chart component was rendered at DOM
     if (onChartReady !== undefined) {
         infiniteScrollContainerHeight = '100%'
@@ -227,7 +238,7 @@ const TicketChartView: React.FC<ITicketAnalyticsPageChartProps> = ({
                             width: '100%',
                             backgroundColor: 'white',
                         },
-                        series: [{ ...series[0], top: -1000, left: -1000, labelLayout: {}, label: { show: false } }],
+                        series: [{ ...seriesRef.current[0], top: -1000, left: -1000, labelLayout: {}, label: { show: false } }],
                         title: {
                             display: false,
                         },
@@ -242,7 +253,7 @@ const TicketChartView: React.FC<ITicketAnalyticsPageChartProps> = ({
                         useWindow={false}>
                         <List
                             grid={{ gutter: 24, xs: 1, sm: 1, md: 1, lg: 1, xl: 1, xxl: 2 }}
-                            dataSource={series}
+                            dataSource={seriesRef.current}
                             renderItem={(chartSeries, index) => (
                                 <List.Item key={`pie-${index}`} style={{ width: 620 }} >
                                     <ReactECharts
