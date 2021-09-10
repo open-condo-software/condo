57b09f3416b1eec9b1594fcdc8f9ad03419de8d2
Pavel <nomerdvadcatpyat@gmail.com>
2021-09-10 13:20:55 +0500

    DOMA-1138 accountNumber as form field

--- apps/condo/domains/meter/components/AccountNumberInput.tsx
+++ apps/condo/domains/meter/components/AccountNumberInput.tsx
@@ -1,4 +1,4 @@
-import { Alert, Form, Input, Row, Typography } from 'antd'
+import { Alert, Col, Form, Input, Row, Typography } from 'antd'
 import React, { useEffect, useRef, useState } from 'react'
 import { useIntl } from '@core/next/intl'
 import { BasicEmptyListView } from '../../common/components/EmptyListView'
@@ -7,7 +7,7 @@ import { Button } from '../../common/components/Button'
 import { useCreateAccountModal } from './hooks/useCreateAccountModal'
 
 
-export const AccountNumberInput = ({ form, existingMeters }) => {
+export const AccountNumberInput = ({ accountNumber, setAccountNumber, existingMeters }) => {
     const intl = useIntl()
     const AccountNumberMessage = intl.formatMessage({ id: 'pages.condo.meter.AccountNumber' })
     const NewAccountAlertMessage = intl.formatMessage({ id: 'pages.condo.meter.NewAccountAlert' })
@@ -15,50 +15,53 @@ export const AccountNumberInput = ({ form, existingMeters }) => {
     const AddAccountDescription = intl.formatMessage({ id: 'pages.condo.meter.AddAccountDescription' })
     const AddAccountMessage = intl.formatMessage({ id: 'pages.condo.meter.AddAccount' })
 
-    useEffect(() => {
-        if (existingMeters.length > 0)
-            form.setFieldsValue({ accountNumber: existingMeters[0].accountNumber })
-        else
-            form.setFieldsValue({ accountNumber: null })
-    }, [existingMeters])
-
     const { CreateAccountModal, setIsCreateAccountModalVisible } = useCreateAccountModal()
 
-    return form.getFieldValue('accountNumber') ? (
-        <Row gutter={[0, 10]}>
-            <Form.Item
-                name={'accountNumber'}
-                label={AccountNumberMessage}
-            >
-                <Input disabled={existingMeters.length > 0} />
-            </Form.Item>
-            {
-                existingMeters.length === 0 ? (
-                    <Alert showIcon type='warning' message={NewAccountAlertMessage}/>
-                ) : null
-            }
-        </Row>
+    return accountNumber ? (
+        <Col lg={14} md={24}>
+            <Row gutter={[0, 10]}>
+                {/*<Form.Item*/}
+                {/*    name={'accountNumber'}*/}
+                {/*    label={AccountNumberMessage}*/}
+                {/*>*/}
+                <Input
+                    disabled={existingMeters.length > 0}
+                    onChange={e => setAccountNumber(e.target.value)}
+                    value={accountNumber}
+                />
+                {/*</Form.Item>*/}
+                {
+                    existingMeters.length === 0 ? (
+                        <Alert showIcon type='warning' message={NewAccountAlertMessage}/>
+                    ) : null
+                }
+            </Row>
+        </Col>
     ) : (
-        <>
-            <BasicEmptyListView>
-                <Typography.Title level={3}>
-                    {NoAccountNumber}
-                </Typography.Title>
-                <Typography.Text style={{ fontSize: fontSizes.content }}>
-                    {AddAccountDescription}
-                </Typography.Text>
-                <Button
-                    type='sberPrimary'
-                    style={{ marginTop: '16px' }}
-                    onClick={() => setIsCreateAccountModalVisible(true)}
-                >
-                    {AddAccountMessage}
-                </Button>
-            </BasicEmptyListView>
+        <Col span={24}>
+            <Row justify={'center'}>
+                <Col span={8} style={{ padding: '100px 0' }}>
+                    <BasicEmptyListView>
+                        <Typography.Title level={3}>
+                            {NoAccountNumber}
+                        </Typography.Title>
+                        <Typography.Text style={{ fontSize: fontSizes.content }}>
+                            {AddAccountDescription}
+                        </Typography.Text>
+                        <Button
+                            type='sberPrimary'
+                            style={{ marginTop: '16px' }}
+                            onClick={() => setIsCreateAccountModalVisible(true)}
+                        >
+                            {AddAccountMessage}
+                        </Button>
+                    </BasicEmptyListView>
 
-            <CreateAccountModal
-                handleSubmit={({ accountNumber }) => { form.setFieldsValue({ accountNumber }) }}
-            />
-        </>
+                    <CreateAccountModal
+                        handleSubmit={({ accountNumber }) => { setAccountNumber(accountNumber) }}
+                    />
+                </Col>
+            </Row>
+        </Col>
     )
 }
