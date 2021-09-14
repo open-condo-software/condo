6a2c0d4738facac7afff72b6931fcddf2a7ac63e
Pavel <nomerdvadcatpyat@gmail.com>
2021-09-14 14:22:02 +0500

    DOMA-1155 rebase with create page branch

---
+++ apps/condo/domains/meter/hooks/useTableColumns.tsx
@@ -0,0 +1,158 @@
+import { FilterValue } from 'antd/es/table/interface'
+import { format } from 'date-fns'
+import { get } from 'lodash'
+import { useIntl } from '@core/next/intl'
+import React, { useMemo } from 'react'
+import { LOCALES } from '@condo/domains/common/constants/locale'
+import {
+    getFilterIcon,
+    getOptionFilterDropdown,
+    getTextFilterDropdown,
+} from '@condo/domains/common/components/Table/Filters'
+import { getDateFilterDropdown } from '../../common/components/Table/Filters'
+import { useRouter } from 'next/router'
+import { getSorterMap, parseQuery } from '../../common/utils/tables.utils'
+import { getTextRender } from '../../common/components/Table/Renders'
+import { MeterReadingSource, MeterResource } from '../utils/clientSchema'
+
+
+const getFilteredValue = (filters, key: string | Array<string>): FilterValue => get(filters, key, null)
+
+export const useTableColumns = () => {
+    const intl = useIntl()
+    const ClientNameMessage = intl.formatMessage({ id: 'Contact' })
+    const AddressMessage = intl.formatMessage({ id: 'field.Address' })
+    const UserNameMessage = intl.formatMessage({ id: 'filters.UserName' })
+    const ShortFlatNumber = intl.formatMessage({ id: 'field.FlatNumber' })
+    const MeterReadingDateMessage = intl.formatMessage({ id: 'pages.condo.meter.MeterReadingDate' })
+    const ServiceMessage = intl.formatMessage({ id: 'pages.condo.meter.Service' })
+    const MeterNumberMessage = intl.formatMessage({ id: 'pages.condo.meter.MeterNumber' })
+    const PlaceMessage = intl.formatMessage({ id: 'pages.condo.meter.Place' })
+    const MeterReadingMessage = intl.formatMessage({ id: 'pages.condo.meter.MeterReading' })
+    const SourceMessage = intl.formatMessage({ id: 'pages.condo.ticket.field.Source' })
+
+    const router = useRouter()
+    const { filters, sorters } = parseQuery(router.query)
+    const sorterMap = getSorterMap(sorters)
+
+    const { objs: sources, loading: sourcesLoading } = MeterReadingSource.useObjects({})
+    const sourcesOptions = sources.map(source => ({ label: source.name, value: source.id }))
+
+    const { objs: resources, loading: resourcesLoading } = MeterResource.useObjects({})
+    const resourcesOptions = resources.map(resource => ({ label: resource.name, value: resource.id }))
+
+    return useMemo(() => {
+        return [
+            {
+                title: MeterReadingDateMessage,
+                sortOrder: get(sorterMap, 'date'),
+                filteredValue: getFilteredValue(filters, 'date'),
+                dataIndex: 'date',
+                key: 'date',
+                sorter: true,
+                width: '10%',
+                render: (createdAt) => (
+                    format(
+                        new Date(createdAt),
+                        'dd MMMM yyyy',
+                        { locale: LOCALES[intl.locale] }
+                    )
+                ),
+                filterDropdown: getDateFilterDropdown(),
+            },
+            {
+                title: AddressMessage,
+                ellipsis: false,
+                sortOrder: get(sorterMap, 'address'),
+                filteredValue: getFilteredValue(filters, 'address'),
+                key: 'address',
+                sorter: true,
+                width: '12%',
+                render: (record) => {
+                    const unitName = get(record, 'unitName')
+                    const property = get(record, 'property')
+                    const text = get(property, 'address')
+                    const unitPrefix = unitName ? `${ShortFlatNumber} ${unitName}` : ''
+
+                    return `${text} ${unitPrefix}`
+                },
+                filterDropdown: getTextFilterDropdown(AddressMessage),
+                filterIcon: getFilterIcon,
+            },
+            {
+                title: ServiceMessage,
+                ellipsis: true,
+                sortOrder: get(sorterMap, 'resource'),
+                filteredValue: getFilteredValue(filters, 'resource'),
+                dataIndex: ['meter', 'resource', 'name'],
+                key: 'resource',
+                sorter: true,
+                width: '12%',
+                filterDropdown: getOptionFilterDropdown(resourcesOptions, resourcesLoading),
+                render: getTextRender(),
+                filterIcon: getFilterIcon,
+            },
+            {
+                title: MeterNumberMessage,
+                sortOrder: get(sorterMap, 'number'),
+                filteredValue: getFilteredValue(filters, 'number'),
+                dataIndex: ['meter', 'number'],
+                key: 'number',
+                sorter: true,
+                width: '10%',
+                filterDropdown: getTextFilterDropdown(MeterNumberMessage),
+                render: getTextRender(),
+                filterIcon: getFilterIcon,
+            },
+            {
+                title: PlaceMessage,
+                ellipsis: true,
+                sortOrder: get(sorterMap, 'place'),
+                filteredValue: getFilteredValue(filters, 'place'),
+                dataIndex: ['meter', 'place'],
+                key: 'place',
+                sorter: true,
+                width: '9%',
+                filterDropdown: getTextFilterDropdown(PlaceMessage),
+                render: getTextRender(),
+                filterIcon: getFilterIcon,
+            },
+            {
+                title: MeterReadingMessage,
+                ellipsis: true,
+                sortOrder: get(sorterMap, 'value1'),
+                filteredValue: getFilteredValue(filters, 'value1'),
+                dataIndex: 'value1',
+                key: 'value1',
+                sorter: true,
+                width: '10%',
+                render: getTextRender(),
+            },
+            {
+                title: ClientNameMessage,
+                ellipsis: true,
+                sortOrder: get(sorterMap, 'clientName'),
+                filteredValue: getFilteredValue(filters, 'clientName'),
+                dataIndex: 'clientName',
+                key: 'clientName',
+                sorter: true,
+                width: '10%',
+                filterDropdown: getTextFilterDropdown(UserNameMessage),
+                render: getTextRender(),
+                filterIcon: getFilterIcon,
+            },
+            {
+                title: SourceMessage,
+                sortOrder: get(sorterMap, 'source'),
+                filteredValue: getFilteredValue(filters, 'source'),
+                dataIndex: ['source', 'name'],
+                key: 'source',
+                sorter: true,
+                width: '10%',
+                filterDropdown: getOptionFilterDropdown(sourcesOptions, sourcesLoading),
+                render: getTextRender(),
+                filterIcon: getFilterIcon,
+            },
+        ]
+    }, [filters])
+}
---
+++ apps/condo/pages/meter/index.tsx
@@ -0,0 +1,242 @@
+/** @jsx jsx */
+import { jsx } from '@emotion/core'
+import { PageContent, PageHeader, PageWrapper } from '@condo/domains/common/components/containers/BaseLayout'
+import { OrganizationRequired } from '@condo/domains/organization/components/OrganizationRequired'
+import { MeterReading } from '@condo/domains/meter/utils/clientSchema'
+import { EXPORT_TICKETS_TO_EXCEL } from '@condo/domains/ticket/gql'
+import { DatabaseFilled } from '@ant-design/icons'
+import { useIntl } from '@core/next/intl'
+import { useLazyQuery } from '@core/next/apollo'
+import { notification, Col, Row, Typography } from 'antd'
+import Head from 'next/head'
+import { useRouter } from 'next/router'
+import { get } from 'lodash'
+import React, { useCallback, useEffect, useState } from 'react'
+import { EmptyListView } from '@condo/domains/common/components/EmptyListView'
+import { Button } from '@condo/domains/common/components/Button'
+import { useOrganization } from '@core/next/organization'
+import { MeterReadingWhereInput, SortMeterReadingsBy } from '../../schema'
+import {
+    getDayGteFilter, getDayLteFilter,
+    getFilter,
+    getPageIndexFromOffset,
+    getStringContainsFilter,
+    parseQuery,
+    QueryMeta,
+} from '../../domains/common/utils/tables.utils'
+import { useTableColumns } from '../../domains/meter/hooks/useTableColumns'
+import { DEFAULT_PAGE_SIZE, Table } from '@condo/domains/common/components/Table/Index'
+import { useQueryMappers } from '../../domains/common/hooks/useQueryMappers'
+import qs from 'qs'
+import DateRangePicker from '@condo/domains/common/components/Pickers/DateRangePicker'
+import { Dayjs } from 'dayjs'
+
+interface ITicketIndexPage extends React.FC {
+    headerAction?: JSX.Element
+    requiredAccess?: React.FC
+}
+
+export const MetersPageContent = ({
+    searchMeterReadingsQuery,
+    tableColumns,
+    sortBy,
+}) => {
+    const intl = useIntl()
+    const PageTitleMessage = intl.formatMessage({ id: 'pages.condo.meter.index.PageTitle' })
+    const ExportAsExcel = intl.formatMessage({ id: 'ExportAsExcel' })
+    const EmptyListLabel = intl.formatMessage({ id: 'ticket.EmptyList.header' })
+    const EmptyListMessage = intl.formatMessage({ id: 'ticket.EmptyList.title' })
+    const CreateTicket = intl.formatMessage({ id: 'CreateTicket' })
+    const DownloadExcelLabel = intl.formatMessage({ id: 'pages.condo.ticket.id.DownloadExcelLabel' })
+    const timeZone = intl.formatters.getDateTimeFormat().resolvedOptions().timeZone
+
+    const router = useRouter()
+    const { filters, offset } = parseQuery(router.query)
+    const currentPageIndex = getPageIndexFromOffset(offset, DEFAULT_PAGE_SIZE)
+
+    const {
+        loading,
+        count: total,
+        objs: meterReadings,
+    } = MeterReading.useObjects({
+        sortBy,
+        where: searchMeterReadingsQuery,
+        first: DEFAULT_PAGE_SIZE,
+        skip: (currentPageIndex - 1) * DEFAULT_PAGE_SIZE,
+    }, {
+        fetchPolicy: 'network-only',
+    })
+
+    const [downloadLink, setDownloadLink] = useState(null)
+
+    const [
+        exportToExcel,
+        { loading: isXlsLoading },
+    ] = useLazyQuery(
+        EXPORT_TICKETS_TO_EXCEL,
+        {
+            onError: error => {
+                notification.error(error)
+            },
+            onCompleted: data => {
+                setDownloadLink(data.result.linkToFile)
+            },
+        },
+    )
+
+    const handleRowAction = useCallback((record) => {
+        return {
+            onClick: () => {
+                router.push(`/meter/${record.id}/`)
+            },
+        }
+    }, [])
+
+    const [dateRange, setDateRange] = useState<[Dayjs, Dayjs]>()
+
+    const updateUrlDateRangeFilters = useCallback(() => {
+        if (!dateRange) return
+
+        const [startDate, endDate] = dateRange
+        const newFilters = {
+            ...filters,
+            createdAt_gte: startDate.toISOString(),
+            createdAt_lte: endDate.toISOString(),
+        }
+        const query = {
+            ...router.query,
+            filters: JSON.stringify(newFilters),
+        }
+
+        const newQuery = qs.stringify({ ...query }, { arrayFormat: 'comma', skipNulls: true, addQueryPrefix: true })
+        router.push(router.route + newQuery)
+    }, [dateRange])
+
+    useEffect(() => {
+        updateUrlDateRangeFilters()
+    }, [dateRange])
+
+    return (
+        <>
+            <Head>
+                <title>{PageTitleMessage}</title>
+            </Head>
+            <PageWrapper>
+                <PageHeader title={<Typography.Title style={{ margin: 0 }}>{PageTitleMessage}</Typography.Title>}/>
+                <PageContent>
+                    {
+                        !meterReadings.length && !filters
+                            ? <EmptyListView
+                                label={EmptyListLabel}
+                                message={EmptyListMessage}
+                                createRoute='/ticket/create'
+                                createLabel={CreateTicket} />
+                            : <Row gutter={[0, 40]} align={'middle'}>
+                                <Col span={6}>
+                                    <DateRangePicker
+                                        value={dateRange}
+                                        onChange={(range) => {
+                                            setDateRange(range)
+                                        }}
+                                    />
+                                </Col>
+                                <Col span={6} push={1}>
+                                    {
+                                        downloadLink
+                                            ?
+                                            <Button
+                                                type={'inlineLink'}
+                                                icon={<DatabaseFilled />}
+                                                loading={isXlsLoading}
+                                                target='_blank'
+                                                href={downloadLink}
+                                                rel='noreferrer'>{DownloadExcelLabel}
+                                            </Button>
+                                            :
+                                            <Button
+                                                type={'inlineLink'}
+                                                icon={<DatabaseFilled />}
+                                                loading={isXlsLoading}
+                                                onClick={
+                                                    () => exportToExcel({ variables: { data: { where: searchMeterReadingsQuery, sortBy: sortBy, timeZone } } })
+                                                }>{ExportAsExcel}
+                                            </Button>
+                                    }
+                                </Col>
+                                {/*<Col span={24}>*/}
+                                {/*    <Row>*/}
+                                {/*        <Col span={4}>*/}
+                                {/*            <Checkbox*/}
+                                {/*                onChange={handleEmergencyChange}*/}
+                                {/*                checked={emergency}*/}
+                                {/*                style={{ paddingLeft: '0px', fontSize: '16px' }}*/}
+                                {/*            >Звонок жителя</Checkbox>*/}
+                                {/*        </Col>*/}
+                                {/*        <Col span={6}>*/}
+                                {/*            <Checkbox*/}
+                                {/*                onChange={handleEmergencyChange}*/}
+                                {/*                checked={emergency}*/}
+                                {/*                style={{ paddingLeft: '0px', fontSize: '16px' }}*/}
+                                {/*            >Приложение жителя</Checkbox>*/}
+                                {/*        </Col>*/}
+                                {/*    </Row>*/}
+                                {/*</Col>*/}
+                                <Col span={24}>
+                                    <Table
+                                        totalRows={total}
+                                        loading={loading}
+                                        dataSource={meterReadings}
+                                        columns={tableColumns}
+                                        onRow={handleRowAction}
+                                    />
+                                </Col>
+                            </Row>
+                    }
+                </PageContent>
+            </PageWrapper>
+        </>
+    )
+}
+
+const MetersPage: ITicketIndexPage = () => {
+    const userOrganization = useOrganization()
+    const userOrganizationId = get(userOrganization, ['organization', 'id'])
+
+    const addressFilter = getStringContainsFilter(['property', 'address'])
+    const readingDateGteFilter = getDayGteFilter('date')
+    const readingDateLteFilter = getDayLteFilter('date')
+    const sourceFilter = getFilter(['source', 'id'], 'array', 'string', 'in')
+    const resourceFilter = getFilter(['meter', 'resource', 'id'], 'array', 'string', 'in')
+
+    const queryMetas: Array<QueryMeta<MeterReadingWhereInput>> = [
+        { keyword: 'address', filters: [addressFilter] },
+        { keyword: 'date', filters: [readingDateGteFilter, readingDateLteFilter] },
+        { keyword: 'createdAt_gte', filters: [readingDateGteFilter] },
+        { keyword: 'createdAt_lte', filters: [readingDateLteFilter] },
+        { keyword: 'source', filters: [sourceFilter] },
+        { keyword: 'resource', filters: [resourceFilter] },
+    ]
+
+    const sortableProperties = ['date', 'address', 'resource', 'number', 'place', 'value1', 'clientName', 'source']
+
+    const { filtersToWhere, sortersToSortBy } = useQueryMappers(queryMetas, sortableProperties)
+
+    const router = useRouter()
+    const { filters, sorters } = parseQuery(router.query)
+
+    const tableColumns = useTableColumns()
+    const searchMeterReadingsQuery = { ...filtersToWhere(filters), organization: { id: userOrganizationId } }
+
+    return (
+        <MetersPageContent
+            tableColumns={tableColumns}
+            searchMeterReadingsQuery={searchMeterReadingsQuery}
+            sortBy={sortersToSortBy(sorters) as SortMeterReadingsBy[]}
+        />
+    )
+}
+
+// MetersPage.headerAction = <TitleHeaderAction descriptor={{ id: 'menu.ControlRoom' }}/>
+MetersPage.requiredAccess = OrganizationRequired
+
+export default MetersPage
