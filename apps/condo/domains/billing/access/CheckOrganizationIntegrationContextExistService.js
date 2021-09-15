84064bd4541dbc154cfc034e77a5ac681c8626f6
toplenboren <toplenboren@gmail.com>
2021-09-16 01:14:29 +0500

    DOMA-1227 write the access checks logic

--- apps/condo/domains/billing/access/CheckOrganizationIntegrationContextExistService.js
+++ apps/condo/domains/billing/access/CheckOrganizationIntegrationContextExistService.js
@@ -4,8 +4,7 @@
 const { throwAuthenticationError } = require('@condo/domains/common/utils/apolloErrorFormatter')
 async function canCheckOrganizationIntegrationContextExist ({ authentication: { item: user } }) {
     if (!user) return throwAuthenticationError()
-    if (user.isAdmin) return true
-    return false
+    return true
 }
 
 /*
