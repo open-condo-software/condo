bc4cda8f6b911a528565c7ffc9757e45b94a8fa6
Dmitry Koviazin <dkoviazin@gmail.com>
2021-09-20 13:40:42 +0500

    DOMA-742 debug broken tests on github

--- packages/@core.keystone/schema.js
+++ packages/@core.keystone/schema.js
@@ -181,8 +181,10 @@ async function getSchemaCtx (schemaObjOrName) {
     } else {
         throw new Error('unexpected type')
     }
-    console.log('Debug broken tests: ', SCHEMAS, schemaObjOrName)
-    if (!SCHEMAS.has(name)) throw new Error(`Schema ${name} is not registered yet`)
+    if (!SCHEMAS.has(name)) {
+        console.log('Debug broken tests: ', SCHEMAS, schemaObjOrName)
+        throw new Error(`Schema ${name} is not registered yet`)
+    }
     const schema = SCHEMAS.get(name)
     return {
         name: schema.name,
