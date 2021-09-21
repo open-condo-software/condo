b762c004bf85ea6c84f1ed63e28b89d0d98bc8dd
Pavel <nomerdvadcatpyat@gmail.com>
2021-09-21 11:51:24 +0500

    DOMA-1155 modify excel data mapper

--- apps/condo/domains/meter/utils/serverSchema/meterReadingDataMapper.js
+++ apps/condo/domains/meter/utils/serverSchema/meterReadingDataMapper.js
@@ -1,15 +1,21 @@
 const meterReadingDataMapper = ({ row = {}, constants = {} }) => ({
     header: {
-        ru: ['Дата снятия', 'Адрес', 'Услуга', 'Номер прибора', 'Место', 'Показание', 'Житель', 'Источник'],
-        en: ['Reading date', 'Address', 'Service', 'Meter number', 'Place', 'Reading', 'Contact', 'Source'],
+        ru: ['Дата снятия', 'Адрес', 'Квартира', 'Услуга', 'Номер прибора', 'Место', 'Показание по тарифу №1', 'Показание по тарифу №2',
+            'Показание по тарифу №3', 'Показание по тарифу №4', 'Житель', 'Источник'],
+        en: ['Reading date', 'Address', 'Unit', 'Service', 'Meter number', 'Place', 'Reading from tariff №1', 'Reading from tariff №2',
+            'Reading from tariff №3', 'Reading from tariff №4', 'Contact', 'Source'],
     },
     rows: {
         date: () => row.date,
         address: () => row.property.address,
+        unitName: () => row.meter.unitName,
         resource: () => row.meter.resource.name,
         number: () => row.meter.number,
         place: () => row.meter.place,
         value1: () => row.value1,
+        value2: () => row.value2,
+        value3: () => row.value3,
+        value4: () => row.value4,
         clientName: () => row.clientName,
         source: () => row.source.name,
     },
