5e8123107ba028a9f93ecbcc4535ce422d466cbd
Sitnikov Ivan <sit7602@gmail.com>
2021-09-22 12:58:45 +0500

    DOMA-746 add TicketWarningModal hook & restrict pdf generation dataset length

---
+++ apps/condo/domains/ticket/hooks/useTicketWarningModal.tsx
@@ -0,0 +1,60 @@
+import React, { Dispatch, SetStateAction, useCallback, useState } from 'react'
+import { Col, Modal, Row, Typography } from 'antd'
+import { useIntl } from '@core/next/intl'
+import { Button } from '@condo/domains/common/components/Button'
+import { MAX_FILTERED_ELEMENTS } from '@condo/domains/ticket/constants/restrictions'
+
+interface IUseTicketWarningModal {
+    (selectedEntity: string): { TicketWarningModal: React.FC, setIsVisible: Dispatch<SetStateAction<boolean>> }
+}
+
+export const useTicketWarningModal: IUseTicketWarningModal = (selectedEntity) => {
+    const intl = useIntl()
+    const ConfirmTitle = intl.formatMessage({ id: 'component.TicketWarningModal.ConfirmTitle' })
+    const EntityTitle = intl.formatMessage({ id: 'component.TicketWarningModal.Entity.' + selectedEntity })
+    const ModalTitle = intl.formatMessage({ id: 'component.TicketWarningModal.Title' }, {
+        ticketFilterRestriction: MAX_FILTERED_ELEMENTS,
+        selectedEntity: EntityTitle,
+    })
+    const ModalDescription = intl.formatMessage({ id: 'component.TicketWarningModal.Description' }, {
+        ticketFilterRestriction: MAX_FILTERED_ELEMENTS,
+        selectedEntity: EntityTitle,
+    })
+
+    const [isVisible, setIsVisible] = useState<boolean>(false)
+    const closeModal = useCallback(() => {
+        setIsVisible(false)
+    }, [])
+
+    const TicketWarningModal: React.FC = () => (
+        <Modal
+            closable={false}
+            visible={isVisible}
+            onCancel={closeModal}
+            footer={[
+                <Button
+                    key='submit'
+                    size='large'
+                    type='sberPrimary'
+                    onClick={closeModal}
+                >{ConfirmTitle}</Button>,
+            ]}
+        >
+            <Row gutter={[0, 40]}>
+                <Col span={24}>
+                    <Typography.Title level={3}>{ModalTitle}</Typography.Title>
+                </Col>
+                <Col span={24}>
+                    <Typography.Text>
+                        {ModalDescription}
+                    </Typography.Text>
+                </Col>
+            </Row>
+        </Modal>
+    )
+
+    return {
+        setIsVisible,
+        TicketWarningModal,
+    }
+}
