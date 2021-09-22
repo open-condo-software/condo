4ebb0540588a12ed49641a30b45e8bd54d59b67d
Pavel <nomerdvadcatpyat@gmail.com>
2021-09-22 11:20:56 +0500

    DOMA-1224 add tests to getQueryToValueProcessorByType

---
+++ apps/condo/domains/common/utils/filters.utils.spec.ts
@@ -0,0 +1,27 @@
+import { ComponentType, getQueryToValueProcessorByType } from './filters.utils'
+import dayjs from 'dayjs'
+
+describe('Filter utils', () => {
+    describe('getQueryToValueProcessorByType', () => {
+        it('should return right queryToDateProcessor', () => {
+            const queryToDateProcessor = getQueryToValueProcessorByType(ComponentType.Date)
+            const date = queryToDateProcessor(dayjs().toISOString())
+
+            expect(date instanceof dayjs).toBeTruthy()
+        })
+
+        it('should return right queryToDateRangeProcessor', () => {
+            const queryToDateRangeProcessor = getQueryToValueProcessorByType(ComponentType.DateRange)
+            const [startDate, endDate] = queryToDateRangeProcessor([dayjs().toISOString(), dayjs().toISOString()])
+
+            expect(startDate instanceof dayjs).toBeTruthy()
+            expect(endDate instanceof dayjs).toBeTruthy()
+        })
+
+        it('should return no processor on Input type component', () => {
+            const processor = getQueryToValueProcessorByType(ComponentType.Input)
+
+            expect(processor).toBeUndefined()
+        })
+    })
+})
