eef23f98ef51cf92758baeff07a73a6f0f2d1fd4
Антон Алёшин <Anton_Aleschin@mail.ru>
2021-09-28 20:33:09 +0300

    DOMA-742 try to fetch data from SBBOL API endpoints
    By sending requests on any endpoint, following response will be received:
    
    ```
    --> GET https://edupirfintech.sberbank.ru:9443/v1/partner-info/advance-acceptances?clientId=111286&date=2021-10-28
    --> HEADERS {
      'user-agent': 'openid-client/4.7.4 (https://github.com/panva/node-openid-client)',
      authorization: 'Bearer fbcbbb68-ca3a-4e64-a1a8-cce376a5e0f9-1',
      'accept-encoding': 'gzip, deflate, br'
    }
    <-- 404 FROM GET https://edupirfintech.sberbank.ru:9443/v1/partner-info/advance-acceptances?clientId=111286&date=2021-10-28
    <-- HEADERS {
      'x-powered-by': 'Servlet/3.1',
      'content-type': 'text/html',
      'transfer-encoding': 'chunked',
      connection: 'Close',
      date: 'Tue, 28 Sep 2021 17:11:55 GMT'
    }
    <-- BODY <H1>SRVE0255E: A WebGroup/Virtual Host to handle /v1/partner-info/advance-acceptances has not been defined.</H1><BR><H3>SRVE0255E: A WebGroup/Virtual Host to handle edupirfintech.sberbank.ru:9443 has not been defined.</H3><BR>
    <-- 404 FROM GET https://edupirfintech.sberbank.ru:9443/v1/partner-info/advance-acceptances?clientId=111286&date=2021-10-28
    ```

--- apps/condo/domains/organization/sbbol/routes.js
+++ apps/condo/domains/organization/sbbol/routes.js
@@ -4,6 +4,7 @@ const { SbbolOauth2Api } = require('@condo/domains/organization/sbbol/oauth2')
 const { SbbolOrganization } = require('@condo/domains/organization/sbbol/sync')
 const { JSON_SCHEMA_VALIDATION_ERROR } = require('@condo/domains/common/constants/errors')
 const { getSchemaCtx } = require('@core/keystone/schema')
+const { getItems } = require('@keystonejs/server-side-graphql-client')
 
 class SbbolRoutes {
     constructor () {
@@ -44,6 +45,74 @@ class SbbolRoutes {
             res.cookie('organizationLinkId', organizationLinkId)
             delete req.session[SBBOL_SESSION_KEY]
             await req.session.save()
+
+            const context = await keystone.createContext({ skipAccessControl: true })
+            console.log('Loading subscriptions in doma.ai')
+            const subscriptions = await getItems({
+                context,
+                listKey: 'ServiceSubscription',
+                where: {
+                    organization: {
+                        id: Sync.organization.id,
+                    },
+                },
+                returnFields: 'id',
+            })
+            if (subscriptions.length > 0) {
+                console.log('User already have subscriptions')
+            } else {
+                console.log('No subscriptions found')
+                console.log('Fetching subscribers and offers from SBBOL')
+                const sbbolSubscribersInfo = await this.helper.fetchSubscribers(access_token);
+                sbbolSubscribersInfo.offers.on('data', data => {
+                    console.log('Reading `offers` response')
+                    console.debug(data)
+                })
+
+                /*
+                    By sending requests on any endpoint, following response will be received:
+
+                    ```
+                    --> GET https://edupirfintech.sberbank.ru:9443/v1/partner-info/advance-acceptances?clientId=111286&date=2021-10-28
+                    --> HEADERS {
+                      'user-agent': 'openid-client/4.7.4 (https://github.com/panva/node-openid-client)',
+                      authorization: 'Bearer fbcbbb68-ca3a-4e64-a1a8-cce376a5e0f9-1',
+                      'accept-encoding': 'gzip, deflate, br'
+                    }
+                    <-- 404 FROM GET https://edupirfintech.sberbank.ru:9443/v1/partner-info/advance-acceptances?clientId=111286&date=2021-10-28
+                    <-- HEADERS {
+                      'x-powered-by': 'Servlet/3.1',
+                      'content-type': 'text/html',
+                      'transfer-encoding': 'chunked',
+                      connection: 'Close',
+                      date: 'Tue, 28 Sep 2021 17:11:55 GMT'
+                    }
+                    <-- BODY <H1>SRVE0255E: A WebGroup/Virtual Host to handle /v1/partner-info/advance-acceptances has not been defined.</H1><BR><H3>SRVE0255E: A WebGroup/Virtual Host to handle edupirfintech.sberbank.ru:9443 has not been defined.</H3><BR>
+                    <-- 404 FROM GET https://edupirfintech.sberbank.ru:9443/v1/partner-info/advance-acceptances?clientId=111286&date=2021-10-28
+                    ```
+
+                    To see it, add `SBBOL_DEBUG` environment variable
+
+                    Example response from SBBOL API at Swagger:
+                    At this stage response can be following:
+                    ```json
+                    {
+                      "cause": "DATA_NOT_FOUND_EXCEPTION",
+                      "referenceId": "91c03a40-637e-4589-b4fe-9b77463aeca3",
+                      "message": "Не найдено ни одного заранее данного акцепта за указанную дату"
+                    }
+                    ```
+                 */
+                sbbolSubscribersInfo.advanceAcceptances.on('data', data => {
+                    console.log('Reading `advanceAcceptances` response')
+                    console.debug(data)
+                })
+                sbbolSubscribersInfo.packageOfServices.on('data', data => {
+                    console.log('Reading `packageOfServices` response')
+                    console.debug(data)
+                })
+            }
+
             return res.redirect('/')
         } catch (error) {
             return next(error)
