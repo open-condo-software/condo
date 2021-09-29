6ff4a7d22645d8700caea2068da02831928e5f39
Sitnikov Ivan <sit7602@gmail.com>
2021-09-29 12:33:43 +0500

    DOMA-1304 fix division create validations

--- apps/condo/domains/division/components/BaseDivisionForm/ErrorsContainer.tsx
+++ apps/condo/domains/division/components/BaseDivisionForm/ErrorsContainer.tsx
@@ -3,22 +3,36 @@ import React from 'react'
 import { ErrorsWrapper } from '@condo/domains/common/components/ErrorsWrapper'
 
 interface IErrorsContainerProps {
+    name: string,
     properties: string,
     responsible: string,
+    executors: string
 }
 
-export const ErrorsContainer: React.FC<IErrorsContainerProps> = ({ properties, responsible }) => {
+export const ErrorsContainer: React.FC<IErrorsContainerProps> = ({ name, properties, responsible, executors }) => {
     const intl = useIntl()
     const ErrorsContainerTitle = intl.formatMessage({ id: 'errorsContainer.requiredErrors' })
+
+    const NameLabel = intl.formatMessage({ id: 'division.field.name' })
     const PropertiesLabel = intl.formatMessage({ id: 'division.field.properties' })
     const ResponsibleLabel = intl.formatMessage({ id: 'division.field.responsible' })
-    const disableUserInteraction = !properties || properties.length === 0 || !responsible
+    const ExecutorLabel = intl.formatMessage({ id: 'division.field.executors' })
+    const disableUserInteraction = name.length === 0
+        || !properties || properties.length === 0
+        || !responsible
+        || !executors || executors.length === 0
 
     const errors = []
+    if (name.length === 0) {
+        errors.push(NameLabel)
+    }
     if (properties && properties.length === 0)
         errors.push(PropertiesLabel)
     if (!responsible)
         errors.push(ResponsibleLabel)
+    if (executors && executors.length === 0) {
+        errors.push(ExecutorLabel)
+    }
 
     return (
         disableUserInteraction && (
