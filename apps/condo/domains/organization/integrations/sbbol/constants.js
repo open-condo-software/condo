b71e6237e6339a79c0378150c0574028a3d1ce6e
Антон Алёшин <Anton_Aleschin@mail.ru>
2021-10-07 22:43:03 +0300

    DOMA-1215 changed export syntax in constants of `sbbol`

--- apps/condo/domains/organization/integrations/sbbol/constants.js
+++ apps/condo/domains/organization/integrations/sbbol/constants.js
@@ -1,6 +1,10 @@
 const { SBBOL_IMPORT_NAME } = require('./common')
 
-export const dvSenderFields = {
+const dvSenderFields = {
     dv: 1,
     sender: { dv: 1, fingerprint: `import-${SBBOL_IMPORT_NAME}` },
+}
+
+module.exports = {
+    dvSenderFields,
 }
