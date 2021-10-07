30526290e50b43edcf49532bf6296ccf229b27d2
Dimitreee <danewdima@gmail.com>
2021-10-07 12:29:31 +0300

    DOMA-1365 remove extra fields

--- apps/condo/domains/user/components/UserMenu/index.tsx
+++ apps/condo/domains/user/components/UserMenu/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useLayoutContext } from '../../../common/components/LayoutContext'
+import { useLayoutContext } from '@condo/domains/common/components/LayoutContext'
 import { DesktopUserMenu } from './DesktopUserMenu'
 import { MobileUserMenu } from './MobileUserMenu'
 
