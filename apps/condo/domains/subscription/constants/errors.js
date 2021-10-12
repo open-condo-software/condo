596317e9a26f55cfe379268687d2667caf975107
Антон Алёшин <Anton_Aleschin@mail.ru>
2021-10-12 12:36:58 +0300

    DOMA-1341 status transitions for `PaymentSubscriptionService`

--- apps/condo/domains/subscription/constants/errors.js
+++ apps/condo/domains/subscription/constants/errors.js
@@ -1,5 +1,8 @@
 const OVERLAPPING_ERROR = '[overlapping]'
 
+const WRONG_PAYMENT_STATUS_TRANSITION_ERROR = '[status:wrongTransition]'
+
 module.exports = {
     OVERLAPPING_ERROR,
+    WRONG_PAYMENT_STATUS_TRANSITION_ERROR,
 }
