b156909f64420377297c2df396e94c9c089a1c44
Антон Алёшин <Anton_Aleschin@mail.ru>
2021-10-14 20:23:24 +0300

    DOMA-1400 post unsigned payment request to SBBOL without `crucialFieldsHash`

--- apps/condo/domains/organization/tasks/index.js
+++ apps/condo/domains/organization/tasks/index.js
@@ -1,5 +1,7 @@
 const syncSbbolSubscriptions = require('./syncSbbolSubscriptions')
+const syncSbbolPaymentRequestsForSubscriptions = require('./syncSbbolPaymentRequestsForSubscriptions')
 
 module.exports = {
     syncSbbolSubscriptions,
+    syncSbbolPaymentRequestsForSubscriptions,
 }
