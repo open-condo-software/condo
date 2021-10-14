ee84404f98819e13517083a2e453e5d75b8d4f62
matthewsavelev <savelevmatthew@gmail.com>
2021-10-14 18:03:17 +0500

    DOMA-1307 Tooltip colors fix

--- apps/condo/domains/common/components/Tooltip.tsx
+++ apps/condo/domains/common/components/Tooltip.tsx
@@ -1,18 +1,22 @@
-import { Tooltip as DefaultTooltip, TooltipProps, Typography } from 'antd'
+import { Tooltip as DefaultTooltip, TooltipProps as DefaultTooltipProps } from 'antd'
 import React from 'react'
 import { colors } from '../constants/style'
+import get from 'lodash/get'
+
+type TooltipProps = DefaultTooltipProps & { textColor?: string }
 
 export const Tooltip: React.FC<TooltipProps> = (props) => {
+    const overlayInnerStyle: React.CSSProperties = get(props, 'overlayInnerStyle', {})
+    overlayInnerStyle.color = get(props, 'textColor', colors.black)
     const tooltipProps = {
-        title: <Typography.Text>{props.title}</Typography.Text>,
         arrowPointAtCenter: true,
         mouseEnterDelay: 0.3,
         color: colors.white,
         ...props,
+        overlayInnerStyle,
     }
 
     return (
         <DefaultTooltip {...tooltipProps}/>
     )
-
 }
