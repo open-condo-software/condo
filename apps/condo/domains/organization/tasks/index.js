238e88c7f25ae21a08c718ab40dfe3c244a0cdce
Антон Алёшин <Anton_Aleschin@mail.ru>
2021-10-16 20:57:57 +0300

    DOMA-1400 get status of payment request from SBBOL with `syncSbbolSubscriptionPaymentRequestsState` and create subscription if payment is done

--- apps/condo/domains/organization/tasks/index.js
+++ apps/condo/domains/organization/tasks/index.js
@@ -1,7 +1,9 @@
 const syncSbbolSubscriptions = require('./syncSbbolSubscriptions')
 const syncSbbolPaymentRequestsForSubscriptions = require('./syncSbbolPaymentRequestsForSubscriptions')
+const syncSbbolSubscriptionPaymentRequestsState = require('./syncSbbolSubscriptionPaymentRequestsState')
 
 module.exports = {
     syncSbbolSubscriptions,
     syncSbbolPaymentRequestsForSubscriptions,
+    syncSbbolSubscriptionPaymentRequestsState,
 }
