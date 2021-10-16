c710ad101f1a0a43b88732195184a34fdddb84fd
Антон Алёшин <Anton_Aleschin@mail.ru>
2021-10-16 20:57:57 +0300

    DOMA-1400 get status of payment request from SBBOL with `syncSbbolSubscriptionPaymentRequestsState` and create subscription if payment is done

---
+++ apps/condo/domains/organization/tasks/syncSbbolSubscriptionPaymentRequestsState.js
@@ -0,0 +1,15 @@
+const dayjs = require('dayjs')
+const { createCronTask } = require('@core/keystone/tasks')
+const { syncSbbolSubscriptionPaymentRequestsState } = require('@condo/domains/organization/integrations/sbbol/sync/syncSbbolSubscriptionPaymentRequestsState')
+const { getSchemaCtx } = require('@core/keystone/schema')
+
+/**
+ * Syncs new and cancelled subscriptions
+ */
+const task = createCronTask('syncSbbolSubscriptionPaymentRequestsState', '* * * * *', async () => {
+    const today = dayjs().format('YYYY-MM-DD')
+    const { keystone } = await getSchemaCtx('User')
+    await syncSbbolSubscriptionPaymentRequestsState({ date: today, context: keystone })
+})
+
+module.exports = task
