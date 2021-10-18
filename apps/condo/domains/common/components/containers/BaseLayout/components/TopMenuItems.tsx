ed4ed77b7f08ee022099ab685105cc80d95c1daf
toplenboren <toplenboren@gmail.com>
2021-10-18 19:49:32 +0500

    DOMA-1453 remove loader for top menu

--- apps/condo/domains/common/components/containers/BaseLayout/components/TopMenuItems.tsx
+++ apps/condo/domains/common/components/containers/BaseLayout/components/TopMenuItems.tsx
@@ -5,7 +5,6 @@ import { useOrganization } from '@core/next/organization'
 import { OrganizationSelect } from '@condo/domains/organization/components/OrganizationSelect'
 import { useOrganizationInvites } from '@condo/domains/organization/hooks/useOrganizationInvites'
 import { UserMenu } from '@condo/domains/user/components/UserMenu'
-import { Loader } from '@condo/domains/common/components/Loader'
 
 export interface ITopMenuItemsProps {
     headerAction?: React.ElementType
@@ -16,19 +15,16 @@ export const TopMenuItems: React.FC<ITopMenuItemsProps> = (props) => {
     const { isLoading } = useOrganization()
     const { loading: isInvitesLoading } = useOrganizationInvites()
 
-    if (isLoading || auth.isLoading || isInvitesLoading) {
+    if (!isLoading && !auth.isLoading && !isInvitesLoading) {
         return (
-            <Loader fill={false} />
+            <>
+                {props.headerAction && props.headerAction}
+                <Space direction={'horizontal'} size={40} style={{ marginLeft: 'auto' }}>
+                    <OrganizationSelect/>
+                    <UserMenu/>
+                </Space>
+            </>
         )
     }
-
-    return (
-        <>
-            {props.headerAction && props.headerAction}
-            <Space direction={'horizontal'} size={40} style={{ marginLeft: 'auto' }}>
-                <OrganizationSelect/>
-                <UserMenu/>
-            </Space>
-        </>
-    )
+    return null
 }
