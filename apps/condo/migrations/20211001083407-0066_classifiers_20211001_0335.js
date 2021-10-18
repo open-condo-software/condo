1c2b538ddf58094bbf88bea4189fd224bd4e3776
Dmitry Koviazin <dkoviazin@gmail.com>
2021-10-18 10:37:22 +0500

    DOMA-1436 add script to generate migration for new classifiers

---
+++ apps/condo/migrations/20211001083407-0066_classifiers_20211001_0335.js
@@ -0,0 +1,45 @@
+exports.up = async (knex) => {
+    await knex.raw(`
+    BEGIN;
+    
+    INSERT INTO public."TicketCategoryClassifier" (dv, sender, "name", id, v, "createdAt", "updatedAt", "deletedAt", "newId", "createdBy", organization,  "updatedBy") VALUES (1, '{"dv": 1, "fingerprint": "initial_import"}', 'Доводчик', '1975f713-4f6e-4b38-97c2-0af88712e7e6', 1, '2021-07-22 00:00:00.000000', '2021-07-22 00:00:00.000000', null, null, null, null, null);
+    INSERT INTO public."TicketProblemClassifier" (dv, sender, "name", id, v, "createdAt", "updatedAt", "deletedAt", "newId", "createdBy", organization,  "updatedBy") VALUES (1, '{"dv": 1, "fingerprint": "initial_import"}', 'Сломался доводчик', '2f4c64a1-aad8-4b1a-80d3-05921e60382f', 1, '2021-07-22 00:00:00.000000', '2021-07-22 00:00:00.000000', null, null, null, null, null);
+    INSERT INTO public."TicketProblemClassifier" (dv, sender, "name", id, v, "createdAt", "updatedAt", "deletedAt", "newId", "createdBy", organization,  "updatedBy") VALUES (1, '{"dv": 1, "fingerprint": "initial_import"}', 'Установить доводчик', '86b82a68-c29d-4da4-b980-384f1c84826c', 1, '2021-07-22 00:00:00.000000', '2021-07-22 00:00:00.000000', null, null, null, null, null);
+    INSERT INTO public."TicketClassifierRule" ("dv", "sender", "id", "v", "createdAt", "updatedAt", "deletedAt", "newId", "category", "createdBy", "place", "problem", "updatedBy") VALUES (1, '{"dv": 1, "fingerprint": "initial_import"}', '1373142b-c94d-4eef-8aeb-1f44972959d0', 1, '2021-07-22 00:00:00.000000', '2021-07-22 00:00:00.000000', null, null, '1975f713-4f6e-4b38-97c2-0af88712e7e6', null, 'a56153a2-d797-4822-ba95-392b69ee0fa6', '2f4c64a1-aad8-4b1a-80d3-05921e60382f', null);
+    INSERT INTO public."TicketClassifierRule" ("dv", "sender", "id", "v", "createdAt", "updatedAt", "deletedAt", "newId", "category", "createdBy", "place", "problem", "updatedBy") VALUES (1, '{"dv": 1, "fingerprint": "initial_import"}', '76066a74-fda5-41c3-8b8c-7b07eec09fec', 1, '2021-07-22 00:00:00.000000', '2021-07-22 00:00:00.000000', null, null, '1975f713-4f6e-4b38-97c2-0af88712e7e6', null, 'a56153a2-d797-4822-ba95-392b69ee0fa6', '86b82a68-c29d-4da4-b980-384f1c84826c', null);
+    INSERT INTO public."TicketClassifierRule" ("dv", "sender", "id", "v", "createdAt", "updatedAt", "deletedAt", "newId", "category", "createdBy", "place", "problem", "updatedBy") VALUES (1, '{"dv": 1, "fingerprint": "initial_import"}', '01c52e55-61c0-478e-9adb-c86c8402b7f7', 1, '2021-07-22 00:00:00.000000', '2021-07-22 00:00:00.000000', null, null, '1975f713-4f6e-4b38-97c2-0af88712e7e6', null, 'c08725fc-3714-4d9c-a274-2d297f8e1163', '2f4c64a1-aad8-4b1a-80d3-05921e60382f', null);
+    INSERT INTO public."TicketClassifierRule" ("dv", "sender", "id", "v", "createdAt", "updatedAt", "deletedAt", "newId", "category", "createdBy", "place", "problem", "updatedBy") VALUES (1, '{"dv": 1, "fingerprint": "initial_import"}', '70cbd63a-c40a-4c22-aa0b-23a6cdf5d499', 1, '2021-07-22 00:00:00.000000', '2021-07-22 00:00:00.000000', null, null, '1975f713-4f6e-4b38-97c2-0af88712e7e6', null, 'c08725fc-3714-4d9c-a274-2d297f8e1163', '86b82a68-c29d-4da4-b980-384f1c84826c', null);
+    INSERT INTO public."TicketClassifierRule" ("dv", "sender", "id", "v", "createdAt", "updatedAt", "deletedAt", "newId", "category", "createdBy", "place", "problem", "updatedBy") VALUES (1, '{"dv": 1, "fingerprint": "initial_import"}', '4ff27c3f-9d28-4618-a978-10c36b362a11', 1, '2021-07-22 00:00:00.000000', '2021-07-22 00:00:00.000000', null, null, '1975f713-4f6e-4b38-97c2-0af88712e7e6', null, '067883f5-baa3-48c1-aca7-47213405fb28', '2f4c64a1-aad8-4b1a-80d3-05921e60382f', null);
+    INSERT INTO public."TicketClassifierRule" ("dv", "sender", "id", "v", "createdAt", "updatedAt", "deletedAt", "newId", "category", "createdBy", "place", "problem", "updatedBy") VALUES (1, '{"dv": 1, "fingerprint": "initial_import"}', '071dcd59-460d-4c7d-9463-5fdb940d5df3', 1, '2021-07-22 00:00:00.000000', '2021-07-22 00:00:00.000000', null, null, '1975f713-4f6e-4b38-97c2-0af88712e7e6', null, '067883f5-baa3-48c1-aca7-47213405fb28', '86b82a68-c29d-4da4-b980-384f1c84826c', null);
+    INSERT INTO public."TicketClassifierRule" ("dv", "sender", "id", "v", "createdAt", "updatedAt", "deletedAt", "newId", "category", "createdBy", "place", "problem", "updatedBy") VALUES (1, '{"dv": 1, "fingerprint": "initial_import"}', '43de5ac5-6909-40d1-92c0-82a457dd28a8', 1, '2021-07-22 00:00:00.000000', '2021-07-22 00:00:00.000000', null, null, '1975f713-4f6e-4b38-97c2-0af88712e7e6', null, 'd1ab29e5-3193-49be-9a91-20a37df477f8', '2f4c64a1-aad8-4b1a-80d3-05921e60382f', null);
+    INSERT INTO public."TicketClassifierRule" ("dv", "sender", "id", "v", "createdAt", "updatedAt", "deletedAt", "newId", "category", "createdBy", "place", "problem", "updatedBy") VALUES (1, '{"dv": 1, "fingerprint": "initial_import"}', '52670064-a290-4c68-b195-5fb56698e356', 1, '2021-07-22 00:00:00.000000', '2021-07-22 00:00:00.000000', null, null, '1975f713-4f6e-4b38-97c2-0af88712e7e6', null, 'd1ab29e5-3193-49be-9a91-20a37df477f8', '86b82a68-c29d-4da4-b980-384f1c84826c', null);
+    INSERT INTO public."TicketClassifierRule" ("dv", "sender", "id", "v", "createdAt", "updatedAt", "deletedAt", "newId", "category", "createdBy", "place", "problem", "updatedBy") VALUES (1, '{"dv": 1, "fingerprint": "initial_import"}', 'beef687e-401c-46ed-8738-d58f645801c8', 1, '2021-07-22 00:00:00.000000', '2021-07-22 00:00:00.000000', null, null, '1975f713-4f6e-4b38-97c2-0af88712e7e6', null, 'a56153a2-d797-4822-ba95-392b69ee0fa6', null, null);
+    INSERT INTO public."TicketClassifierRule" ("dv", "sender", "id", "v", "createdAt", "updatedAt", "deletedAt", "newId", "category", "createdBy", "place", "problem", "updatedBy") VALUES (1, '{"dv": 1, "fingerprint": "initial_import"}', 'd689a2e1-8153-447a-a13a-8caabc78663f', 1, '2021-07-22 00:00:00.000000', '2021-07-22 00:00:00.000000', null, null, '1975f713-4f6e-4b38-97c2-0af88712e7e6', null, 'c08725fc-3714-4d9c-a274-2d297f8e1163', null, null);
+    INSERT INTO public."TicketClassifierRule" ("dv", "sender", "id", "v", "createdAt", "updatedAt", "deletedAt", "newId", "category", "createdBy", "place", "problem", "updatedBy") VALUES (1, '{"dv": 1, "fingerprint": "initial_import"}', 'cdb6afb9-76d2-4b57-96ed-45bc9037ecb2', 1, '2021-07-22 00:00:00.000000', '2021-07-22 00:00:00.000000', null, null, '1975f713-4f6e-4b38-97c2-0af88712e7e6', null, '067883f5-baa3-48c1-aca7-47213405fb28', null, null);
+    INSERT INTO public."TicketClassifierRule" ("dv", "sender", "id", "v", "createdAt", "updatedAt", "deletedAt", "newId", "category", "createdBy", "place", "problem", "updatedBy") VALUES (1, '{"dv": 1, "fingerprint": "initial_import"}', 'a6016fbb-a38d-4070-84bf-e739d2077363', 1, '2021-07-22 00:00:00.000000', '2021-07-22 00:00:00.000000', null, null, '1975f713-4f6e-4b38-97c2-0af88712e7e6', null, 'd1ab29e5-3193-49be-9a91-20a37df477f8', null, null);
+
+    `)
+}
+
+exports.down = async (knex) => {
+    await knex.raw(`
+    BEGIN;
+    
+    DELETE FROM public."TicketCategoryClassifier" WHERE "id" = '1975f713-4f6e-4b38-97c2-0af88712e7e6'; 
+    DELETE FROM public."TicketProblemClassifier" WHERE "id" = '2f4c64a1-aad8-4b1a-80d3-05921e60382f';
+    DELETE FROM public."TicketProblemClassifier" WHERE "id" = '86b82a68-c29d-4da4-b980-384f1c84826c';
+    DELETE FROM public."TicketClassifierRule" WHERE "id" = '1373142b-c94d-4eef-8aeb-1f44972959d0';
+    DELETE FROM public."TicketClassifierRule" WHERE "id" = '76066a74-fda5-41c3-8b8c-7b07eec09fec';
+    DELETE FROM public."TicketClassifierRule" WHERE "id" = '01c52e55-61c0-478e-9adb-c86c8402b7f7';
+    DELETE FROM public."TicketClassifierRule" WHERE "id" = '70cbd63a-c40a-4c22-aa0b-23a6cdf5d499';
+    DELETE FROM public."TicketClassifierRule" WHERE "id" = '4ff27c3f-9d28-4618-a978-10c36b362a11';
+    DELETE FROM public."TicketClassifierRule" WHERE "id" = '071dcd59-460d-4c7d-9463-5fdb940d5df3';
+    DELETE FROM public."TicketClassifierRule" WHERE "id" = '43de5ac5-6909-40d1-92c0-82a457dd28a8';
+    DELETE FROM public."TicketClassifierRule" WHERE "id" = '52670064-a290-4c68-b195-5fb56698e356';
+    DELETE FROM public."TicketClassifierRule" WHERE "id" = 'beef687e-401c-46ed-8738-d58f645801c8';
+    DELETE FROM public."TicketClassifierRule" WHERE "id" = 'd689a2e1-8153-447a-a13a-8caabc78663f';
+    DELETE FROM public."TicketClassifierRule" WHERE "id" = 'cdb6afb9-76d2-4b57-96ed-45bc9037ecb2';
+    DELETE FROM public."TicketClassifierRule" WHERE "id" = 'a6016fbb-a38d-4070-84bf-e739d2077363';
+
+    `)
+}
---
+++ bin/add-classifiers.js
@@ -0,0 +1,223 @@
+const has = require('lodash/has')
+const { v4: uuid } = require('uuid')
+const path = require('path')
+const { GraphQLApp } = require('@keystonejs/app-graphql')
+const { GqlWithKnexLoadList } = require('@condo/domains/common/utils/serverSchema')
+
+const CLASSIFIER_PLACE_TEMPLATE = 'INSERT INTO public."TicketPlaceClassifier" (dv, sender, "name", id, v, "createdAt", "updatedAt", "deletedAt", "newId", "createdBy", organization,  "updatedBy") VALUES (1, \'{"dv": 1, "fingerprint": "initial_import"}\', \'{name}\', \'{uuid}\', 1, \'2021-07-22 00:00:00.000000\', \'2021-07-22 00:00:00.000000\', null, null, null, null, null);'
+const CLASSIFIER_CATEGORY_TEMPLATE = 'INSERT INTO public."TicketCategoryClassifier" (dv, sender, "name", id, v, "createdAt", "updatedAt", "deletedAt", "newId", "createdBy", organization,  "updatedBy") VALUES (1, \'{"dv": 1, "fingerprint": "initial_import"}\', \'{name}\', \'{uuid}\', 1, \'2021-07-22 00:00:00.000000\', \'2021-07-22 00:00:00.000000\', null, null, null, null, null);'
+const CLASSIFIER_PROBLEM_TEMPLATE = 'INSERT INTO public."TicketProblemClassifier" (dv, sender, "name", id, v, "createdAt", "updatedAt", "deletedAt", "newId", "createdBy", organization,  "updatedBy") VALUES (1, \'{"dv": 1, "fingerprint": "initial_import"}\', \'{name}\', \'{uuid}\', 1, \'2021-07-22 00:00:00.000000\', \'2021-07-22 00:00:00.000000\', null, null, null, null, null);'
+const CLASSIFIER_RULE_TEMPLATE = 'INSERT INTO public."TicketClassifierRule" ("dv", "sender", "id", "v", "createdAt", "updatedAt", "deletedAt", "newId", "category", "createdBy", "place", "problem", "updatedBy") VALUES (1, \'{"dv": 1, "fingerprint": "initial_import"}\', \'{uuid}\', 1, \'2021-07-22 00:00:00.000000\', \'2021-07-22 00:00:00.000000\', null, null, \'{category}\', null, \'{place}\', {problem}, null);'
+
+
+const CLASSIFIER_PLACE_REMOVE_TEMPLATE = 'DELETE FROM public."TicketPlaceClassifier" WHERE "id" = \'{uuid}\'; '
+const CLASSIFIER_CATEGORY_REMOVE_TEMPLATE = 'DELETE FROM public."TicketCategoryClassifier" WHERE "id" = \'{uuid}\'; '
+const CLASSIFIER_PROBLEM_REMOVE_TEMPLATE = 'DELETE FROM public."TicketProblemClassifier" WHERE "id" = \'{uuid}\'; '
+const CLASSIFIER_RULE_REMOVE_TEMPLATE = 'DELETE FROM public."TicketClassifierRule" WHERE "id" = \'{uuid}\';'
+
+const MIGRATION_TEMPLATE = `
+exports.up = async (knex) => {
+    await knex.raw(\`
+    BEGIN;
+    
+    [UP]
+
+    \`)
+}
+
+exports.down = async (knex) => {
+    await knex.raw(\`
+    BEGIN;
+    
+    [DOWN]
+
+    \`)
+}
+`
+// Example:
+// Чердаки, подвалы;Доступ;Ограничение доступа
+// type=place =  Чердаки, подвалы
+// type=category = Доступ
+// type=problem = Ограничение доступа
+// relations (2): place + category + problem (null)
+// Чердаки, подвалы => Доступ => Ограничение доступа
+// Чердаки, подвалы => Доступ => null
+
+
+const importCsvData = `
+Подъезд;Доводчик;Сломался доводчик
+Подъезд;Доводчик;Установить доводчик
+Нежилое помещение;Доводчик;Сломался доводчик
+Нежилое помещение;Доводчик;Установить доводчик
+Кладовка;Доводчик;Сломался доводчик
+Кладовка;Доводчик;Установить доводчик
+Чердаки подвалы;Доводчик;Сломался доводчик
+Чердаки подвалы;Доводчик;Установить доводчик
+`
+
+class NewClassifiersToSql {
+
+    places = {}
+    categories = {}
+    problems = {}
+    relations = []
+    nullRelations = {}
+
+    existingClassifiers = {
+        places: {},
+        categories: {},
+        problems: {},
+    }
+
+    uuids = {
+        places: {},
+        categories: {},
+        problems: {},
+    }
+
+    async init () {
+        const resolved = path.resolve('./index.js')
+        const { distDir, keystone, apps } = require(resolved)
+        const graphqlIndex = apps.findIndex(app => app instanceof GraphQLApp)
+        // we need only apollo
+        await keystone.prepare({ apps: [apps[graphqlIndex]], distDir, dev: true })
+        await keystone.connect()
+        this.context = await keystone.createContext({ skipAccessControl: true })
+        await this.loadExistingClassifiers()
+    }
+
+    async loadExistingClassifiers () {
+        const modelsToLoad = {
+            places: 'TicketPlaceClassifier',
+            categories: 'TicketCategoryClassifier',
+            problems: 'TicketPlaceClassifier',
+        }
+        await Promise.all(Object.entries(modelsToLoad).map( async ([key, Model]) => {
+            const knexLoader = new GqlWithKnexLoadList({
+                listKey: Model,
+                fields: 'id name',
+            })
+            const records = await knexLoader.load()
+            records.forEach( record => {
+                this.existingClassifiers[key][this.toKey(record.name)] = record.id
+            })
+        }))
+    }
+
+    toKey (name) {
+        return name.replace(/[^A-Za-zА-Яа-яЁё]/g, '').toLowerCase()
+    }
+
+    setUUID (type, key) {
+        if (!has(this.uuids[type], key)) {
+            this.uuids[type][key] = this.existingClassifiers[type][key] ? this.existingClassifiers[type][key] : uuid()
+        }
+    }
+
+    setUUIDS (place, category, problem){
+        this.setUUID('places', place)
+        this.setUUID('categories', category)
+        this.setUUID('problems', problem)
+    }
+
+    setRelation (place, category, problem) {
+        this.setUUIDS(place, category, problem)
+        this.relations.push({
+            place: this.uuids.places[place],
+            category: this.uuids.categories[category],
+            problem: this.uuids.problems[problem],
+        })
+        this.nullRelations[[place, category].join('_')] = {
+            place: this.uuids.places[place],
+            category: this.uuids.categories[category],
+            problem: null,
+        }
+    }
+
+    prepareData () {
+        const data = importCsvData.split('\n')
+        data.forEach(line => {
+            line = line.trim()
+            const semiColonsCount =  (line.match(/;/g) || []).length
+            if (line.length && semiColonsCount === 2) {
+                const [place, category, problem] = line.trim().split(';')
+                const [placeKey, categoryKey, problemKey] = [place, category, problem].map( key => this.toKey(key) )
+                this.places[placeKey] = place
+                this.categories[categoryKey] = category
+                this.problems[problemKey] = problem
+                this.setRelation(placeKey, categoryKey, problemKey)
+            }
+        })
+    }
+
+    getClassifierSql ({ name, uuid }, template) {
+        return template
+            .split('{name}').join(name)
+            .split('{uuid}').join(uuid)
+    }
+
+    getClassifiersSql () {
+        const classifiersUp = []
+        const classifiersDown = []
+        for (const placeKey in this.places) {
+            if (!this.existingClassifiers.places[placeKey]) {
+                classifiersUp.push(this.getClassifierSql({ name: this.places[placeKey], uuid: this.uuids.places[placeKey] }, CLASSIFIER_PLACE_TEMPLATE))
+                classifiersDown.push(CLASSIFIER_PLACE_REMOVE_TEMPLATE.split('{uuid}').join(this.uuids.places[placeKey]))
+            }
+        }
+        for (const categoryKey in this.categories) {
+            if (!this.existingClassifiers.categories[categoryKey]) {
+                classifiersUp.push(this.getClassifierSql({ name: this.categories[categoryKey], uuid: this.uuids.categories[categoryKey] }, CLASSIFIER_CATEGORY_TEMPLATE))
+                classifiersDown.push(CLASSIFIER_CATEGORY_REMOVE_TEMPLATE.split('{uuid}').join(this.uuids.categories[categoryKey]))
+            }
+        }
+        for (const problemKey in this.problems) {
+            if (!this.existingClassifiers.problems[problemKey]) {
+                classifiersUp.push(this.getClassifierSql({ name: this.problems[problemKey], uuid: this.uuids.problems[problemKey] }, CLASSIFIER_PROBLEM_TEMPLATE))
+                classifiersDown.push(CLASSIFIER_PROBLEM_REMOVE_TEMPLATE.split('{uuid}').join(this.uuids.problems[problemKey]))
+            }
+        }
+        return [ classifiersUp.join('\n'), classifiersDown.join('\n') ]
+    }
+
+    getRelationsSql () {
+        const relationsUp = []
+        const relationsDown = []
+        const allRelations = [...this.relations, ...Object.values(this.nullRelations)]
+        allRelations.forEach(({ place, category, problem }) => {
+            const newId = uuid()
+            relationsUp.push(
+                CLASSIFIER_RULE_TEMPLATE
+                    .split('{uuid}').join(newId)
+                    .split('{place}').join(place)
+                    .split('{category}').join(category)
+                    .split('{problem}').join(problem ? `'${problem}'` : null)
+            )
+            relationsDown.push(CLASSIFIER_RULE_REMOVE_TEMPLATE.split('{uuid}').join(newId))
+        })
+        return [ relationsUp.join('\n'), relationsDown.join('\n') ]
+    }
+
+    async printResult () {
+        await this.init()
+        this.prepareData()
+        const [ classifiersUp, classifiersDown ] = this.getClassifiersSql()
+        const [ relationsUp, relationsDown ] = this.getRelationsSql()
+        process.stdout.write(
+            MIGRATION_TEMPLATE
+                .replace('[UP]', classifiersUp + '\n' + relationsUp)
+                .replace('[DOWN]',  classifiersDown + '\n' + relationsDown)
+        )
+        process.exit(0)
+    }
+
+}
+
+
+const Migration = new NewClassifiersToSql()
+Migration.printResult().then(result => {
+    process.exit(0)
+}).catch(error => {
+    console.error('Error', error)
+    process.exit(1)
+})
