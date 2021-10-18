a63454dba22b24b268d0535845a7d93e465a67e9
Dimitreee <danewdima@gmail.com>
2021-10-18 16:44:17 +0300

    DOMA-1400 add comments to shell

--- bin/sbbol_subscription.js
+++ bin/sbbol_subscription.js
@@ -1,15 +1,12 @@
 const yargs = require('yargs/yargs')
 const dayjs = require('dayjs')
+const { createCronTask } = require('@core/keystone/tasks')
 const { syncSubscriptions } = require('@condo/domains/organization/integrations/sbbol/sync/syncSubscriptions')
 
-const date = yargs(process.argv).date
-const today = dayjs(date).format('YYYY-MM-DD')
+const syncSbbolSubscriptions = createCronTask('syncSbbolSubscriptions', '20 9 * * *', async () => {
+    const date = yargs(process.argv).date
+    const today = dayjs(date).format('YYYY-MM-DD')
+    await syncSubscriptions(today)
+})
 
-syncSubscriptions(today).then(
-    () => {
-        console.log('sucessfully synced')
-    },
-    () => {
-        console.log('error')
-    }
-)
+syncSbbolSubscriptions.delay()
