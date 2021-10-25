2f42c9b67fca0a548447d7c242ef2d458c6c8843
Dimitreee <danewdima@gmail.com>
2021-10-25 08:47:11 +0300

    DOMA-1400 add sbbol register message notify, add subscription indicator

--- apps/condo/domains/common/components/containers/BaseLayout/components/TopMenuItems.tsx
+++ apps/condo/domains/common/components/containers/BaseLayout/components/TopMenuItems.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Space } from 'antd'
 import { useAuth } from '@core/next/auth'
+import { UserMenu } from '@condo/domains/user/components/UserMenu'
 import { useOrganization } from '@core/next/organization'
 import { OrganizationSelect } from '@condo/domains/organization/components/OrganizationSelect'
 import { useOrganizationInvites } from '@condo/domains/organization/hooks/useOrganizationInvites'
-import { UserMenu } from '@condo/domains/user/components/UserMenu'
+import { ServiceSubscriptionIndicator } from '@condo/domains/subscription/components/ServiceSubscriptionIndicator'
 
 export interface ITopMenuItemsProps {
     headerAction?: React.ElementType
@@ -20,6 +21,7 @@ export const TopMenuItems: React.FC<ITopMenuItemsProps> = (props) => {
             <>
                 {props.headerAction && props.headerAction}
                 <Space direction={'horizontal'} size={40} style={{ marginLeft: 'auto' }}>
+                    <ServiceSubscriptionIndicator/>
                     <OrganizationSelect/>
                     <UserMenu/>
                 </Space>
