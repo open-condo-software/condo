a1a9c79a38f53d11d3945da1ecb405fa92ab563d
Pavel <nomerdvadcatpyat@gmail.com>
2021-10-28 20:37:38 +0500

    DOMA-297 move ExportToExcelActionBar to common

---
+++ apps/condo/domains/common/components/ExportToExcelActionBar.tsx
@@ -0,0 +1,66 @@
+import { useIntl } from '@core/next/intl'
+import { useState } from 'react'
+import { useLazyQuery } from '@core/next/apollo'
+import { Form, notification } from 'antd'
+import ActionBar from './ActionBar'
+import { Button } from './Button'
+import { DatabaseFilled } from '@ant-design/icons'
+
+export const ExportToExcelActionBar = (
+    { searchObjectsQuery, sortBy, exportToExcelQuery }
+) => {
+    const intl = useIntl()
+    const DownloadExcelLabel = intl.formatMessage({ id: 'pages.condo.ticket.id.DownloadExcelLabel' })
+    const ExportAsExcelLabel = intl.formatMessage({ id: 'ExportAsExcel' })
+
+    const timeZone = intl.formatters.getDateTimeFormat().resolvedOptions().timeZone
+
+    const [downloadLink, setDownloadLink] = useState(null)
+
+    const [
+        exportToExcel,
+        { loading: isXlsLoading },
+    ] = useLazyQuery(
+        exportToExcelQuery,
+        {
+            onError: error => {
+                notification.error(error)
+            },
+            onCompleted: data => {
+                setDownloadLink(data.result.linkToFile)
+            },
+        },
+    )
+
+    return (
+        <Form.Item noStyle>
+            <ActionBar>
+                {
+                    downloadLink
+                        ?
+                        <Button
+                            type={'inlineLink'}
+                            icon={<DatabaseFilled />}
+                            loading={isXlsLoading}
+                            target='_blank'
+                            href={downloadLink}
+                            rel='noreferrer'
+                        >
+                            {DownloadExcelLabel}
+                        </Button>
+                        :
+                        <Button
+                            type={'sberPrimary'}
+                            secondary
+                            icon={<DatabaseFilled />}
+                            loading={isXlsLoading}
+                            onClick={
+                                () => exportToExcel({ variables: { data: { where: searchObjectsQuery, sortBy: sortBy, timeZone } } })
+                            }>
+                            {ExportAsExcelLabel}
+                        </Button>
+                }
+            </ActionBar>
+        </Form.Item>
+    )
+}
