9aa56113de1ac18653f1547e4299bdcc78a54bab
matthewsavelev <savelevmatthew@gmail.com>
2021-11-01 17:39:06 +0500

    DOMA-1443 Added dv validation on AcquiringIntegrationAccessRight + test

--- apps/condo/domains/acquiring/schema/AcquiringIntegrationAccessRight.js
+++ apps/condo/domains/acquiring/schema/AcquiringIntegrationAccessRight.js
@@ -7,6 +7,8 @@ const { GQLListSchema } = require('@core/keystone/schema')
 const { historical, versioned, uuided, tracked, softDeleted } = require('@core/keystone/plugins')
 const { SENDER_FIELD, DV_FIELD } = require('@condo/domains/common/schema/fields')
 const access = require('@condo/domains/acquiring/access/AcquiringIntegrationAccessRight')
+const { DV_UNKNOWN_VERSION_ERROR } = require('@condo/domains/common/constants/errors')
+const { hasDvAndSenderFields } = require('@condo/domains/common/utils/validation.utils')
 
 
 const AcquiringIntegrationAccessRight = new GQLListSchema('AcquiringIntegrationAccessRight', {
@@ -41,6 +43,17 @@ const AcquiringIntegrationAccessRight = new GQLListSchema('AcquiringIntegrationA
         update: access.canManageAcquiringIntegrationAccessRights,
         delete: false,
         auth: true,
+    },
+    hooks: {
+        validateInput: ({ resolvedData, context, addValidationError }) => {
+            if (!hasDvAndSenderFields(resolvedData, context, addValidationError)) return
+            const { dv } = resolvedData
+            if (dv === 1) {
+                // NOTE: version 1 specific translations. Don't optimize this logic
+            } else {
+                return addValidationError(`${DV_UNKNOWN_VERSION_ERROR}dv] Unknown \`dv\``)
+            }
+        },
     },
 })
 
