988db3bbed4a005c5f39174c4af9137213d7e179
toplenboren <toplenboren@gmail.com>
2021-11-03 02:39:51 +0500

    DOMA-1554 add support rights to read and manage service consumers

--- apps/condo/domains/resident/access/ServiceConsumer.js
+++ apps/condo/domains/resident/access/ServiceConsumer.js
@@ -4,7 +4,7 @@ const { RESIDENT } = require('@condo/domains/user/constants/common')
 
 async function canReadServiceConsumers ({ authentication: { item: user } }) {
     if (!user) return false
-    if (user.isAdmin) return {}
+    if (user.isAdmin || user.isSupport) return {}
     if (user.type === RESIDENT) {
         return {
             resident: { user: { id: user.id } },
@@ -15,7 +15,7 @@ async function canReadServiceConsumers ({ authentication: { item: user } }) {
 
 async function canManageServiceConsumers ({ authentication: { item: user }, context, originalInput, operation, itemId }) {
     if (!user) return false
-    if (user.isAdmin) return true
+    if (user.isAdmin || user.isSupport) return true
     if (user.type === RESIDENT) {
         if (operation === 'update') {
             const [serviceConsumer] = await ServiceConsumer.getAll(context, { id: itemId })
