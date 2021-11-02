6456c976bcd4a506bf9de057aa0125f5531d25c9
Антон Алёшин <Anton_Aleschin@mail.ru>
2021-11-02 16:41:03 +0300

    DOMA-1501 used child-loggers for each sbbol sync module

--- apps/condo/domains/organization/integrations/sbbol/sync/syncPaymentRequestsForSubscriptions.js
+++ apps/condo/domains/organization/integrations/sbbol/sync/syncPaymentRequestsForSubscriptions.js
@@ -6,12 +6,11 @@ const { SbbolFintechApi } = require('../SbbolFintechApi')
 const { SBBOL_YEARLY_SUBSCRIPTION_PRICE, SUBSCRIPTION_TYPE, SUBSCRIPTION_PAYMENT_STATUS, SUBSCRIPTION_PAYMENT_CURRENCY } = require('@condo/domains/subscription/constants')
 const conf = process.env
 const SBBOL_CONFIG = conf.SBBOL_CONFIG ? JSON.parse(conf.SBBOL_CONFIG) : {}
-const { logger } = require('../common')
+const { logger: baseLogger } = require('../common')
 const { dvSenderFields, BANK_OPERATION_CODE } = require('../constants')
 const { SBBOL_OFFER_ACCEPT_IS_NULL_ERROR, SBBOL_OFFER_ACCEPT_HAS_INCORRECT_PAYER_REQUISITES_ERROR } = require('@condo/domains/subscription/constants/errors')
 
-// For logging
-const module = 'syncPaymentRequestsForSubscriptions'
+const logger = baseLogger.child({ module: 'syncPaymentRequestsForSubscriptions' })
 
 // TODO(antonal): Add 3 days for trial subscription when payment is emitted to compensate payment processing lag and subsequent in service unavailability
 
@@ -24,7 +23,6 @@ const postPaymentRequestFor = async (subscription, fintechApi) => {
             message: 'Latest ServiceSubscription does not have information about offer accept',
             error: SBBOL_OFFER_ACCEPT_IS_NULL_ERROR,
             subscription,
-            module,
         })
         return
     }
@@ -36,7 +34,6 @@ const postPaymentRequestFor = async (subscription, fintechApi) => {
             message: 'Latest ServiceSubscription has insufficient data in offer accept',
             error: SBBOL_OFFER_ACCEPT_HAS_INCORRECT_PAYER_REQUISITES_ERROR,
             sbbolOfferAccept: subscription.sbbolOfferAccept,
-            module,
         })
         logger.info({ message: 'Abort postPaymentRequestsFor', subscription })
         return
@@ -57,7 +54,6 @@ const postPaymentRequestFor = async (subscription, fintechApi) => {
         logger.error({
             message: 'Error by creating ServiceSubscriptionPayment',
             data,
-            module,
         })
         return
     }
@@ -65,7 +61,6 @@ const postPaymentRequestFor = async (subscription, fintechApi) => {
     logger.info({
         message: 'Created ServiceSubscriptionPayment',
         record: newPayment,
-        module,
     })
 
     const today = dayjs()
@@ -123,7 +118,7 @@ const postPaymentRequestFor = async (subscription, fintechApi) => {
  * For each expired SBBOL-subscription post payment request to SBBOL
  */
 const syncPaymentRequestsForSubscriptions = async () => {
-    logger.info({ message: 'Start', function: 'syncPaymentRequestsForSubscriptions', module })
+    logger.info({ message: 'Start', function: 'syncPaymentRequestsForSubscriptions' })
 
     let ourOrganizationAccessToken
     try {
@@ -135,7 +130,6 @@ const syncPaymentRequestsForSubscriptions = async () => {
             message: 'Failed to obtain organization access token from SBBOL',
             error: e.message,
             hashOrgId: SBBOL_CONFIG.service_organization_hashOrgId,
-            module,
         })
         return
     }
@@ -155,14 +149,12 @@ const syncPaymentRequestsForSubscriptions = async () => {
         logger.info({
             message: 'No expired subscriptions found for today',
             where,
-            module,
         })
     } else {
         logger.info({
             message: `Found expired ServiceSubscription records`,
             count: expiredSubscriptions.length,
             where,
-            module,
         })
         expiredSubscriptions.map(async (expiredSubscription) => {
             // Payments without external id are not created at SBBOL side
@@ -178,12 +170,11 @@ const syncPaymentRequestsForSubscriptions = async () => {
                     message: 'Subscription already has payments. Do nothing',
                     subscription: expiredSubscription,
                     paymentsCount: paymentsForSubscription.length,
-                    module,
                 })
             }
         })
     }
-    logger.info({ message: 'End', function: 'syncPaymentRequestsForSubscriptions', module })
+    logger.info({ message: 'End', function: 'syncPaymentRequestsForSubscriptions' })
 }
 
 module.exports = {
