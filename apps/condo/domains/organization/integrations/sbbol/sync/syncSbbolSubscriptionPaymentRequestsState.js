1b6e6b3673403a43b5ebb6ceacb452ea814c9c79
Антон Алёшин <Anton_Aleschin@mail.ru>
2021-11-02 14:26:17 +0300

    DOMA-1501 more detailed logs for `syncSbbolSubscriptionPaymentRequestsState` module

--- apps/condo/domains/organization/integrations/sbbol/sync/syncSbbolSubscriptionPaymentRequestsState.js
+++ apps/condo/domains/organization/integrations/sbbol/sync/syncSbbolSubscriptionPaymentRequestsState.js
@@ -12,20 +12,29 @@ const dayjs = require('dayjs')
 const conf = process.env
 const SBBOL_CONFIG = conf.SBBOL_CONFIG ? JSON.parse(conf.SBBOL_CONFIG) : {}
 
+// For logging
+const module = 'syncSbbolSubscriptionPaymentRequestsState'
+
 const syncSbbolSubscriptionPaymentRequestStateFor = async (payment, fintechApi) => {
-    const paymentLogString = `ServiceSubscriptionPayment(id=${payment.id}, externalId=${payment.externalId})`
     const { data, error } = await fintechApi.getPaymentRequestState(payment.id)
     if (error) {
-        logger.error({ message: `Error fetching status for ${paymentLogString}: `, error })
+        logger.error({ message: 'Error fetching status for payment', error, payment, module })
     } else {
         const { keystone: context } = await getSchemaCtx('ServiceSubscriptionPayment')
         if (!data.bankStatus) {
-            logger.error({ message: 'Error: Status response does not contains "bankStatus" field. Something was changed in SBBOL Fintech API.' })
+            logger.error({
+                message: 'Status response does not contains "bankStatus" field. Maybe, something was changed in setup of SBBOL Fintech API or in API itself.',
+                module
+            })
         } else {
             if (get(payment.statusMeta, 'bankStatus') !== data.bankStatus) {
                 const status = SBBOL_PAYMENT_STATUS_MAP[data.bankStatus]
                 if (!status) {
-                    logger.error({ message: `Error: status "${data.bankStatus}" cannot be mapped to status values of ServiceSubscriptionPayment, because it does not belongs to the list of known statuses. Consider to add it.` })
+                    logger.error({
+                        message: 'Value of bankStatus cannot be mapped to status values of ServiceSubscriptionPayment, because it does not belongs to the list of known statuses. Consider to add it.',
+                        bankStatus: data.bankStatus,
+                        module,
+                    })
                 } else {
                     await ServiceSubscriptionPayment.update(context, payment.id, {
                         status,
@@ -49,7 +58,7 @@ const syncSbbolSubscriptionPaymentRequestStateFor = async (payment, fintechApi) 
 }
 
 const syncSbbolSubscriptionPaymentRequestsState = async () => {
-    logger.info({ message: 'Start syncSbbolSubscriptionPaymentRequestsState' })
+    logger.info({ message: 'Start', function: 'syncSbbolSubscriptionPaymentRequestsState', module })
     const { keystone: context } = await getSchemaCtx('ServiceSubscriptionPayment')
 
     let ourOrganizationAccessToken
@@ -58,7 +67,12 @@ const syncSbbolSubscriptionPaymentRequestsState = async () => {
         // for organization, that will be queried in SBBOL using `SbbolFintechApi`.
         ourOrganizationAccessToken = await SbbolRequestApi.getOrganizationAccessToken(SBBOL_CONFIG.service_organization_hashOrgId)
     } catch (e) {
-        console.error(e.message)
+        logger.error({
+            message: 'Failed to obtain organization access token from SBBOL',
+            error: e.message,
+            hashOrgId: SBBOL_CONFIG.service_organization_hashOrgId,
+            module,
+        })
         return
     }
 
@@ -72,13 +86,16 @@ const syncSbbolSubscriptionPaymentRequestsState = async () => {
     }, { sortBy: ['updatedAt_DESC'] })
 
     if (paymentsToSync.length === 0) {
-        logger.info({ message: 'No ServiceSubscriptionPayment found with state CREATED, PROCESSING or STOPPED. Do nothing.' })
+        logger.info({
+            message: 'No ServiceSubscriptionPayment found with state CREATED, PROCESSING or STOPPED. Do nothing.',
+            module,
+        })
     } else {
         const syncTasks = paymentsToSync.map(payment => () => syncSbbolSubscriptionPaymentRequestStateFor(payment, fintechApi))
         await executeInSequence(syncTasks)
     }
 
-    logger.info({ message: 'End syncSbbolSubscriptionPaymentRequestsState' })
+    logger.info({ message: 'End', function: 'syncSbbolSubscriptionPaymentRequestsState', module })
 }
 
 
