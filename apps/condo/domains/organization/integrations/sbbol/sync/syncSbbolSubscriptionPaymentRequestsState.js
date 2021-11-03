b30a5db6b5f98e44cdb3f7f987cbf17f68a4cbf5
Антон Алёшин <Anton_Aleschin@mail.ru>
2021-11-03 16:34:40 +0300

    DOMA-1500 splitted `SBBOL_CONFIG` into `SBBOL_AUTH_CONFIG` and `SBBOL_FINTECH_CONFIG`
    There is a small duplication of `host` and `protected_host` in `SbbolOauth2Api` left unresolved.

--- apps/condo/domains/organization/integrations/sbbol/sync/syncSbbolSubscriptionPaymentRequestsState.js
+++ apps/condo/domains/organization/integrations/sbbol/sync/syncSbbolSubscriptionPaymentRequestsState.js
@@ -10,7 +10,8 @@ const { get } = require('lodash')
 const dayjs = require('dayjs')
 
 const conf = process.env
-const SBBOL_CONFIG = conf.SBBOL_CONFIG ? JSON.parse(conf.SBBOL_CONFIG) : {}
+const SBBOL_FINTECH_CONFIG = conf.SBBOL_FINTECH_CONFIG ? JSON.parse(conf.SBBOL_FINTECH_CONFIG) : {}
+const SBBOL_PFX = conf.SBBOL_PFX ? JSON.parse(conf.SBBOL_PFX) : {}
 
 const logger = baseLogger.child({ module: 'syncSbbolSubscriptionPaymentRequestsState' })
 
@@ -57,21 +58,27 @@ const syncSbbolSubscriptionPaymentRequestStateFor = async (payment, fintechApi) 
 const syncSbbolSubscriptionPaymentRequestsState = async () => {
     const { keystone: context } = await getSchemaCtx('ServiceSubscriptionPayment')
 
-    let ourOrganizationAccessToken
+    let accessToken
     try {
         // `service_organization_hashOrgId` is a `userInfo.HashOrgId` from SBBOL, that used to obtain accessToken
         // for organization, that will be queried in SBBOL using `SbbolFintechApi`.
-        ourOrganizationAccessToken = await SbbolRequestApi.getOrganizationAccessToken(SBBOL_CONFIG.service_organization_hashOrgId)
+        accessToken = await SbbolRequestApi.getOrganizationAccessToken(SBBOL_FINTECH_CONFIG.service_organization_hashOrgId)
     } catch (e) {
         logger.error({
             message: 'Failed to obtain organization access token from SBBOL',
             error: e.message,
-            hashOrgId: SBBOL_CONFIG.service_organization_hashOrgId,
+            hashOrgId: SBBOL_FINTECH_CONFIG.service_organization_hashOrgId,
         })
         return
     }
 
-    const fintechApi = new SbbolFintechApi(ourOrganizationAccessToken)
+    const fintechApi = new SbbolFintechApi({
+        accessToken,
+        host: SBBOL_FINTECH_CONFIG.host,
+        port: SBBOL_FINTECH_CONFIG.port,
+        certificate: SBBOL_PFX.certificate,
+        passphrase: SBBOL_PFX.passphrase,
+    })
 
     const { CREATED, PROCESSING, STOPPED } = SUBSCRIPTION_PAYMENT_STATUS
 
