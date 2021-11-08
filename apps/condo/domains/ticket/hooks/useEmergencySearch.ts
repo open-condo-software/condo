03389849e648608d3af26fff62419c36e14ba00e
Pavel <nomerdvadcatpyat@gmail.com>
2021-11-08 15:42:31 +0500

    DOMA-297 remove state from useEmergencySearch and usePaidSearch

--- apps/condo/domains/ticket/hooks/useEmergencySearch.ts
+++ apps/condo/domains/ticket/hooks/useEmergencySearch.ts
@@ -1,32 +1,25 @@
-import { useCallback, useEffect, useState } from 'react'
-import { pickBy, get, debounce } from 'lodash'
+import { useCallback } from 'react'
+import { get, debounce } from 'lodash'
 import { useRouter } from 'next/router'
 import { CheckboxChangeEvent } from 'antd/lib/checkbox/Checkbox'
 
 import { getFiltersFromQuery } from '@condo/domains/common/utils/helpers'
 import { setFiltersToQuery } from '@condo/domains/common/utils/filters.utils'
 
-export const useEmergencySearch = <F>(loading): [boolean, (e: CheckboxChangeEvent) => void] => {
+export const useEmergencySearch = <F>(loading: boolean): [boolean, (e: CheckboxChangeEvent) => void] => {
     const router = useRouter()
     const filtersFromQuery = getFiltersFromQuery<F>(router.query)
 
     const attributes = get(filtersFromQuery, 'attributes', [])
-    const hasEmergency = attributes.includes('isEmergency')
-
-    const [isEmergency, setIsEmergency] = useState(hasEmergency)
-
-    useEffect(() => {
-        setIsEmergency(hasEmergency)
-    }, [hasEmergency])
+    const isEmergency = attributes.includes('isEmergency')
 
     const searchChange = useCallback(debounce(async (isEmergency) => {
         const queryAttributes = isEmergency ? [...attributes, 'isEmergency'] : attributes.filter(attr => attr !== 'isEmergency')
 
         await setFiltersToQuery(router, { ...filtersFromQuery, attributes: queryAttributes }, true)
-    }, 400), [loading, attributes, hasEmergency])
+    }, 400), [loading, attributes, isEmergency])
 
     const handleEmergencyChange = (e: CheckboxChangeEvent): void => {
-        setIsEmergency(e.target.checked)
         searchChange(e.target.checked)
     }
 
