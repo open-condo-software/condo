da51893613f12e14351c6f78e5682fb5ddc06e2e
matthewsavelev <savelevmatthew@gmail.com>
2021-11-10 14:02:52 +0500

    DOMA-1440 Changed acquiring integration fee url

--- apps/condo/domains/acquiring/constants/links.js
+++ apps/condo/domains/acquiring/constants/links.js
@@ -1,7 +1,7 @@
 // Used to navigate users to web page, where shown amount + explicit fee + confirm button
 const WEB_VIEW_PATH = '/pay/[id]'
 // Used to get explicit fee + available payment methods such as Apple Pay, web-view and etc
-const FEE_CALCULATION_PATH = '/api/get_fee/[id]'
+const FEE_CALCULATION_PATH = '/api/multi_payment/[id]'
 
 module.exports = {
     WEB_VIEW_PATH,
