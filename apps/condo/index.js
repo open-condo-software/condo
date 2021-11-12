e71ab3d75f07173a76176763b2ef6da86103796f
Dmitry Koviazin <dkoviazin@gmail.com>
2021-11-12 15:12:07 +0500

    DOMA-1614 restore admin ui

--- apps/condo/index.js
+++ apps/condo/index.js
@@ -22,7 +22,12 @@ const FileAdapter = require('@condo/domains/common/utils/fileAdapter')
 
 const IS_ENABLE_DD_TRACE = conf.NODE_ENV === 'production'
 const IS_ENABLE_APOLLO_DEBUG = conf.NODE_ENV === 'development' || conf.NODE_ENV === 'test'
-const IS_BUILD_PHASE = conf.PHASE === 'build'
+
+let IS_BUILD_PHASE = conf.PHASE === 'build'
+
+// TODO(DOMA-1614): if set to true adminUi will failed to load after build + start is launched
+IS_BUILD_PHASE = false
+
 // NOTE: should be disabled in production: https://www.apollographql.com/docs/apollo-server/testing/graphql-playground/
 // WARN: https://github.com/graphql/graphql-playground/tree/main/packages/graphql-playground-html/examples/xss-attack
 const IS_ENABLE_DANGEROUS_GRAPHQL_PLAYGROUND = conf.ENABLE_DANGEROUS_GRAPHQL_PLAYGROUND === 'true'
