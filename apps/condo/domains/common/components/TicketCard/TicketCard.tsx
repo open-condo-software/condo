c216c47eef5604591ae20e43c217ae5afc43c3f2
Sitnikov Ivan <sit7602@gmail.com>
2021-11-17 18:07:13 +0500

    DOMA-1259 group ticket card info by contact address

--- apps/condo/domains/common/components/TicketCard/TicketCard.tsx
+++ apps/condo/domains/common/components/TicketCard/TicketCard.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Row, Col, Typography } from 'antd'
+import React, { useCallback } from 'react'
+import { Row, Col, Typography, RowProps } from 'antd'
 import styled from '@emotion/styled'
 import { colors } from '@condo/domains/common/constants/style'
 import { Ticket } from '@condo/domains/ticket/utils/clientSchema'
@@ -8,13 +8,19 @@ import { SortTicketsBy } from '@app/condo/schema'
 import { TicketOverview } from './TicketOverview'
 import Link from 'next/link'
 import qs from 'qs'
-import { pickBy } from 'lodash'
+import pickBy from 'lodash/pickBy'
+import groupBy from 'lodash/groupBy'
 import { green } from '@ant-design/colors'
 
-const Container = styled.aside`
+const Container = styled.div`
+  &:last-child {
+    margin-bottom: 24px;
+  }
+
   border: 1px solid ${colors.inputBorderGrey};
   border-radius: 8px;
-  
+  width: 100%;
+  max-width: 310px;
   display: flex;
   flex-flow: column nowrap;
   align-content: center;
@@ -32,20 +38,17 @@ const TicketsPartContainer = styled.div`
 
 interface ITicketCardProps {
     organizationId: string
-    contactId: string
+    contactPhone: string
     contactName: string
-    address: string
-    unitName: string
 }
 
 const TICKETS_ON_CARD = 2
+const TICKET_CARD_GUTTER: RowProps['gutter'] = [0, 24]
 
 const TicketCard: React.FC<ITicketCardProps> = ({
     organizationId,
-    contactId,
+    contactPhone,
     contactName,
-    address,
-    unitName,
 }) => {
     const intl = useIntl()
     const AddressLabel = intl.formatMessage({ id: 'field.Address' })
@@ -55,108 +58,115 @@ const TicketCard: React.FC<ITicketCardProps> = ({
     const LoadingMessage = intl.formatMessage({ id: 'Loading' })
     const NoTicketsOnAddressMessage = intl.formatMessage({ id: 'Contact.NoTicketOnAddress' })
 
-    const unitSuffix = unitName ? `, ${UnitShortMessage} ${unitName}` : ''
-    const addressPrefix = address ? address : DeletedMessage
-    const contactAddress = `${addressPrefix}${unitSuffix}`
+    // const contactAddress = `${addressPrefix}${unitSuffix}`
 
     const {
         loading,
-        count: total,
-        objs: tickets, 
+        objs: tickets,
     } = Ticket.useObjects({
         sortBy: ['createdAt_DESC'] as SortTicketsBy[],
         where: {
             organization: { id: organizationId },
-            contact: { id: contactId },
-            property: { address: address },
-            unitName: unitName,
+            contact: { phone: contactPhone },
         },
-        first: TICKETS_ON_CARD,
     }, {
         fetchPolicy: 'network-only',
     })
 
-    const moreTickets = (loading || total <= TICKETS_ON_CARD) ? 0 : total - TICKETS_ON_CARD
-    const MoreTicketsLeftMessage = intl.formatMessage({ id: 'MoreTicketsLeft' }, {
-        ticketsLeft: moreTickets,
-    })
+    const hasMoreTickets = useCallback((tickets) => {
+        return !loading || tickets >= TICKETS_ON_CARD ? tickets.length - TICKETS_ON_CARD : 0
+    }, [loading])
 
-    const filters = {
-        clientName: contactName,
-        property: address,
-    }
-    const query = qs.stringify(
-        {
-            filters: JSON.stringify(pickBy(filters)),
-        },
-        { arrayFormat: 'comma', skipNulls: true, addQueryPrefix: true },
-    )
+    const addresses = Object.entries(groupBy(tickets, (ticket) => ticket.property.address))
 
     return (
-        <Container>
-            <AddressPartContainter>
-                <Row gutter={[0, 8]}>
-                    <Col span={24}>
-                        <Typography.Text type='secondary'>
-                            {AddressLabel}
-                        </Typography.Text>
-                    </Col>
-                    <Col span={24}>
-                        <Typography.Title
-                            level={4}
-                            style={{ marginBottom: 0 }}>
-                            {contactAddress}
-                        </Typography.Title>
-                    </Col>
-                </Row>
-            </AddressPartContainter>
-            <TicketsPartContainer>
-                <Row gutter={[0, 12]}>
-                    <Col span={24}>
-                        {loading
-                            ?
-                            <Typography.Text type='secondary'>
-                                {LoadingMessage}
-                            </Typography.Text>
-                            :
-                            <>
-                                {tickets.length > 0
-                                    ?
-                                    <Typography.Title
-                                        level={5}
-                                    >
-                                        {TicketsByContactMessage}
-                                    </Typography.Title>
-                                    :
-                                    <Typography.Text style={{ fontSize: 16 }}>
-                                        {NoTicketsOnAddressMessage}
-                                    </Typography.Text>
-                                }
-                                {
-                                    tickets.map((ticket) => {
-                                        return <TicketOverview
-                                            key={ticket.id}
-                                            id={ticket.id}
-                                            details={ticket.details}
-                                            createdAt={ticket.createdAt}
-                                            number={ticket.number}
-                                            status={ticket.status.name}/>
-                                    })
-                                }
-                                {moreTickets > 0 &&
-                                <Col span={24}>
-                                    <Link href={`/ticket/${query}`}>
-                                        <Typography.Link style={{ fontSize: 12, marginTop: 16, color: `${green[6]}` }}>
-                                            {MoreTicketsLeftMessage}
-                                        </Typography.Link>
-                                    </Link>
-                                </Col> }
-                            </>
-                        }
-                    </Col>
-                </Row>
-            </TicketsPartContainer>
-        </Container>
+        <Row gutter={TICKET_CARD_GUTTER} justify={'end'}>
+            {
+                addresses.map(([address,  tickets], key) => {
+                    const addressPrefix = address ? address : DeletedMessage
+                    // const unitSuffix = unitName ? `, ${UnitShortMessage} ${unitName}` : ''
+
+                    const hasMore = hasMoreTickets(tickets)
+                    const filters = { clientName: contactName, property: address }
+                    const query = qs.stringify(
+                        {
+                            filters: JSON.stringify(pickBy(filters)),
+                        },
+                        { arrayFormat: 'comma', skipNulls: true, addQueryPrefix: true },
+                    )
+
+                    return (
+                        <Container key={key}>
+                            <AddressPartContainter>
+                                <Row gutter={[0, 8]}>
+                                    <Col span={24}>
+                                        <Typography.Text type='secondary'>
+                                            {AddressLabel}
+                                        </Typography.Text>
+                                    </Col>
+                                    <Col span={24}>
+                                        <Typography.Title
+                                            level={4}
+                                            style={{ marginBottom: 0 }}>
+                                            {addressPrefix}
+                                        </Typography.Title>
+                                    </Col>
+                                </Row>
+                            </AddressPartContainter>
+                            <TicketsPartContainer>
+                                <Row gutter={[0, 12]}>
+                                    <Col span={24}>
+                                        {loading
+                                            ?
+                                            <Typography.Text type='secondary'>
+                                                {LoadingMessage}
+                                            </Typography.Text>
+                                            :
+                                            <>
+                                                {tickets.length > 0
+                                                    ?
+                                                    <Typography.Title
+                                                        level={5}
+                                                    >
+                                                        {TicketsByContactMessage}
+                                                    </Typography.Title>
+                                                    :
+                                                    <Typography.Text>
+                                                        {NoTicketsOnAddressMessage}
+                                                    </Typography.Text>
+                                                }
+                                                {
+                                                    tickets.slice(0, TICKETS_ON_CARD).map((ticket) => {
+                                                        return <TicketOverview
+                                                            key={ticket.id}
+                                                            id={ticket.id}
+                                                            details={ticket.details}
+                                                            createdAt={ticket.createdAt}
+                                                            number={ticket.number}
+                                                            status={ticket.status.name}/>
+                                                    })
+                                                }
+                                                {hasMore > 0 && (
+                                                    <Col span={24}>
+                                                        <Link href={`/ticket/${query}`}>
+                                                            <Typography.Link style={{ fontSize: 12, marginTop: 16, color: `${green[6]}` }}>
+                                                                {intl.formatMessage({ id: 'MoreTicketsLeft' }, {
+                                                                    ticketsLeft: hasMore,
+                                                                })}
+                                                            </Typography.Link>
+                                                        </Link>
+                                                    </Col>
+                                                )}
+                                            </>
+                                        }
+                                    </Col>
+                                </Row>
+                            </TicketsPartContainer>
+                        </Container>
+                    )
+                })
+            }
+        </Row>
     )
 }
 
