057915d2d5d800b4004118cba583732a316a0af3
Pavel <nomerdvadcatpyat@gmail.com>
2021-11-18 13:36:08 +0500

    DOMA-1469 update contact table in condo and callcenter

---
+++ apps/condo/domains/contact/hooks/useTableFilters.tsx
@@ -0,0 +1,93 @@
+import React, { useMemo } from 'react'
+import { useIntl } from '@core/next/intl'
+
+import {
+    ComponentType,
+    FiltersMeta,
+} from '@condo/domains/common/utils/filters.utils'
+import { MeterReadingWhereInput } from '@app/condo/schema'
+import {
+    getFilter,
+    getStringContainsFilter,
+} from '@condo/domains/common/utils/tables.utils'
+
+const filterName = getStringContainsFilter('name')
+const filterPhone = getStringContainsFilter('phone')
+const filterEmail = getStringContainsFilter('email')
+const filterAddress = getStringContainsFilter(['property', 'address'])
+const filterUnit = getFilter('unitName', 'array', 'string', 'in')
+
+export function useContactsTableFilters (): Array<FiltersMeta<MeterReadingWhereInput>>  {
+    const intl = useIntl()
+    const UserNameMessage = intl.formatMessage({ id: 'field.FullName.short' })
+    const AddressMessage = intl.formatMessage({ id: 'field.Address' })
+    const PhoneMessage = intl.formatMessage({ id: 'Phone' })
+    const EmailMessage = intl.formatMessage({ id: 'field.EMail' })
+    const EnterUnitNameLabel = intl.formatMessage({ id: 'pages.condo.ticket.filters.EnterUnitName' })
+
+    return useMemo(() => {
+        return [
+            {
+                keyword: 'search',
+                filters: [
+                    filterName,
+                    filterPhone,
+                    filterAddress,
+                    filterEmail,
+                ],
+                combineType: 'OR',
+            },
+            {
+                keyword: 'name',
+                filters: [filterName],
+                component: {
+                    type: ComponentType.Input,
+                    props: {
+                        placeholder: UserNameMessage,
+                    },
+                },
+            },
+            {
+                keyword: 'address',
+                filters: [filterAddress],
+                component: {
+                    type: ComponentType.Input,
+                    props: {
+                        placeholder: AddressMessage,
+                    },
+                },
+            },
+            {
+                keyword: 'unitName',
+                filters: [filterUnit],
+                component: {
+                    type: ComponentType.TagsSelect,
+                    props: {
+                        tokenSeparators: [' '],
+                        placeholder: EnterUnitNameLabel,
+                    },
+                },
+            },
+            {
+                keyword: 'phone',
+                filters: [filterPhone],
+                component: {
+                    type: ComponentType.Input,
+                    props: {
+                        placeholder: PhoneMessage,
+                    },
+                },
+            },
+            {
+                keyword: 'email',
+                filters: [filterEmail],
+                component: {
+                    type: ComponentType.Input,
+                    props: {
+                        placeholder: EmailMessage,
+                    },
+                },
+            },
+        ]
+    }, [AddressMessage, EmailMessage, PhoneMessage, UserNameMessage])
+}
