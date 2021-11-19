9cb52386c1aa0bdf30d1104e185afeb769d1ecd0
matthewsavelev <savelevmatthew@gmail.com>
2021-11-19 16:27:20 +0500

    DOMA-1443 Changed core/keystone/fields views + adapters

--- packages/@core.keystone/fields/LocalizedText/index.js
+++ packages/@core.keystone/fields/LocalizedText/index.js
@@ -4,14 +4,6 @@ const { Text } = require('@keystonejs/fields')
 module.exports = {
     type: 'LocalizedText',
     implementation: LocalizedText,
-    views: {
-        Controller: '@keystonejs/fields/types/Text/views/Controller',
-        Field: '@keystonejs/fields/types/Text/views/Field',
-        Filter: '@keystonejs/fields/types/Text/views/Filter',
-    },
-    adapters: {
-        mongoose: Text.adapters.mongoose,
-        knex: Text.adapters.knex,
-        prisma: Text.adapters.prisma,
-    },
+    views: Text.views,
+    adapters: Text.adapters,
 }
--- packages/@core.keystone/fields/SignedDecimal/index.js
+++ packages/@core.keystone/fields/SignedDecimal/index.js
@@ -4,14 +4,6 @@ const { SignedDecimal } = require('./Implementation')
 module.exports = {
     type: 'SignedDecimal',
     implementation: SignedDecimal,
-    views: {
-        Controller: '@keystonejs/fields/types/Decimal/views/Controller',
-        Field: '@keystonejs/fields/types/Decimal/views/Field',
-        Filter: '@keystonejs/fields/types/Decimal/views/Filter',
-    },
-    adapters: {
-        mongoose: Decimal.adapters.mongoose,
-        knex: Decimal.adapters.knex,
-        prisma: Decimal.adapters.prisma,
-    },
+    views: Decimal.views,
+    adapters: Decimal.adapters,
 }
