97914919b39a1bc2ce8f1b0670ab350f27018a50
akarjakin <akarjakin@doma.ai>
2021-11-30 12:35:29 +0500

    DOMA-1519 handle residents connections on property creation, deletion and restoration

---
+++ apps/condo/domains/property/schema/helpers.js
@@ -0,0 +1,69 @@
+const get = require('lodash/get')
+const dayjs = require('dayjs')
+const isSameOrBefore = require('dayjs/plugin/isSameOrBefore')
+const { NOT_FOUND_ERROR } = require('@condo/domains/common/constants/errors')
+// const { Resident: ResidentAPI } = require('@condo/domains/resident/utils/serverSchema')
+
+dayjs.extend(isSameOrBefore)
+
+/**
+ * Connects or disconnects residents to/from organization & property.
+ * Property should be provided to connect.
+ * In other case residents will be disconnected.
+ * @param ResidentAPI (API) either Server or Test API
+ * @param context (context)
+ * @param residents (Resident[])
+ * @param property (Property)
+ * @param reconnectToOlder (boolean) forces reconnecting residents with non-deleted property to restored older one
+ * @returns {Promise<void>}
+ */
+const connectResidents = async (ResidentAPI, context, residents, property, reconnectToOlder = false) => {
+    const propertyId = get(property, 'id')
+    const shouldConnect = Boolean(property) && propertyId
+    const organizationId = shouldConnect ? property.organization.id : null
+    const organizationConnect = shouldConnect ? { connect: { id: organizationId } } : { disconnectAll: true }
+    const propertyConnect = shouldConnect ? { connect: { id: propertyId } } : { disconnectAll: true }
+
+    // Nothing to connect
+    if (!Array.isArray(residents)) return
+
+    for (const resident of residents) {
+        const residentId = get(resident, 'id')
+
+        if (shouldConnect) {
+            // NOTE: for optimization sake, we decide here if property should be switched, by calculating which property is older, current or next one
+            const residentProperty = get(resident, 'property')
+            const currCreatedAt = get(residentProperty, 'createdAt')
+            const currDeletedAt = get(residentProperty, 'deletedAt')
+
+            // NOTE: do not reconnect residents with non-deleted property at all, because of reconnectToOlder === false
+            if (residentProperty && !reconnectToOlder && !currDeletedAt) break
+
+            // NOTE: we need createdAt to make decision about connecting or not resident to other property
+            if (residentProperty && !currCreatedAt) console.error(`${NOT_FOUND_ERROR}createdAt] 'resident.property.createdAt' is missing`)
+
+            // NOTE: resident already has non-deleted property and it's older than property.createdAt
+            if (residentProperty && !currDeletedAt && dayjs(currCreatedAt).isSameOrBefore(dayjs(property.createdAt))) break
+        }
+
+        const attrs = {
+            property: propertyConnect,
+            organization: organizationConnect,
+        }
+
+        await ResidentAPI.update(context, residentId, attrs)
+    }
+}
+
+/**
+ * Disconnects residents from their organization and property each
+ * @param context (context)
+ * @param residents (Resident[])
+ * @returns {Promise<void>}
+ */
+const disconnectResidents = async (ResidentAPI, context, residents) => await connectResidents(ResidentAPI, context, residents, null)
+
+module.exports = {
+    connectResidents,
+    disconnectResidents,
+}
---
+++ apps/condo/domains/property/schema/helpers.test.js
@@ -0,0 +1,60 @@
+const faker = require('faker')
+const get = require('lodash/get')
+
+const { buildFakeAddressAndMeta } = require('@condo/domains/property/utils/testSchema/factories')
+const { makeClientWithResidentUser } = require('@condo/domains/user/utils/testSchema')
+const { registerResidentByTestClient, Resident } = require('@condo/domains/resident/utils/testSchema')
+const { makeClientWithRegisteredOrganization } = require('@condo/domains/organization/utils/testSchema/Organization')
+const { buildingMapJson } = require('@condo/domains/property/constants/property')
+const { createTestProperty, Property } = require('@condo/domains/property/utils/testSchema')
+const { connectResidents } = require('./helpers')
+const {makeLoggedInAdminClient} = require("@core/keystone/test.utils");
+
+// NOTE: here we test only 1 case: for forced re-connection of already connected to non-deleted property residents
+// to oldest non-deleted property. All other cases are tested by the way in apps/condo/domains/resident/schema/RegisterResidentService.test.js
+describe('connectResidents', () => {
+    it('reconnects residents from younger property to restored older one', async () => {
+        const { address, addressMeta } = buildFakeAddressAndMeta(true)
+        const adminClient = await makeLoggedInAdminClient()
+        const userClient = await makeClientWithResidentUser()
+        const organizationClient = await makeClientWithRegisteredOrganization()
+        const organizationClient1 = await makeClientWithRegisteredOrganization()
+
+        // remove flat number from address for organization
+        const orgAddressMeta = { ...addressMeta, value: address }
+        const propertyPayload = { address, addressMeta: orgAddressMeta, map: buildingMapJson }
+        const [property] = await createTestProperty(organizationClient, organizationClient.organization, propertyPayload)
+        const [property1] = await createTestProperty(organizationClient1, organizationClient1.organization, propertyPayload)
+
+        const [deletedProperty] = await Property.softDelete(organizationClient, property.id)
+
+        expect(deletedProperty.deletedAt).not.toBeNull()
+
+        await registerResidentByTestClient(userClient, { address: addressMeta.value, addressMeta })
+
+        const [resident] = await Resident.getAll(userClient, { id: userClient.id })
+
+        // Resident should be connected to oldest non-deleted property1
+        expect(get(resident, 'organization.id')).toEqual(organizationClient1.organization.id)
+        expect(get(resident, 'property.id')).toEqual(property1.id)
+
+        const sender = { dv: 1, fingerprint: faker.random.alphaNumeric(8) }
+        const restoredProperty = await Property.update(organizationClient, property.id, { deletedAt: null, dv: 1, sender })
+
+        expect(restoredProperty.deletedAt).toBeNull()
+
+        const residents = await Resident.getAll(userClient, { id: userClient.id })
+
+        // Resident should be still connected to oldest non-deleted property1
+        expect(get(residents[0], 'organization.id')).toEqual(organizationClient1.organization.id)
+        expect(get(residents[0], 'property.id')).toEqual(property1.id)
+
+        await connectResidents(Resident, adminClient, residents, restoredProperty, true)
+
+        const [resident1] = await Resident.getAll(userClient, { id: userClient.id })
+
+        // Resident should be connected to restoredProperty
+        expect(get(resident1, 'organization.id')).toEqual(organizationClient.organization.id)
+        expect(get(resident1, 'property.id')).toEqual(restoredProperty.id)
+    })
+})
