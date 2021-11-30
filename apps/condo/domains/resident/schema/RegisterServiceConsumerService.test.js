4e259f9b1a6eb374ee2676998ff2b01b7c3617f7
toplenboren <toplenboren@gmail.com>
2021-11-30 14:26:52 +0500

    DOMA-1615 refactor tests (change lines)

--- apps/condo/domains/resident/schema/RegisterServiceConsumerService.test.js
+++ apps/condo/domains/resident/schema/RegisterServiceConsumerService.test.js
@@ -137,10 +137,10 @@ describe('RegisterServiceConsumerService', () => {
 
         const [ out ] = await registerServiceConsumerByTestClient(userClient, payload)
         expect(out).toBeDefined()
-        expect(out.residentBillingAccount.id).toEqual(billingAccountAttrs.id)
         expect(out.residentOrganization.id).toEqual(organization.id)
         expect(out.residentAcquiringIntegrationContext.id).toEqual(acquiringIntegrationContext.id)
         expect(out.residentAcquiringIntegrationContext.integration).toEqual(acquiringIntegration.id)
+        expect(out.residentBillingAccount.id).toEqual(billingAccountAttrs.id)
     })
 
     it('creates serviceConsumer with billingAccount and Meters', async () => {
@@ -189,6 +189,7 @@ describe('RegisterServiceConsumerService', () => {
         expect(out.residentOrganization.id).toEqual(userClient.organization.id)
         expect(out.residentAcquiringIntegrationContext.id).toEqual(acquiringIntegrationContext.id)
         expect(out.residentAcquiringIntegrationContext.integration).toEqual(acquiringIntegration.id)
+        expect(out.residentBillingAccount.id).toEqual(billingAccountAttrs.id)
         expect(meter).toBeDefined()
         expect(meter.number).toBeDefined()
     })
