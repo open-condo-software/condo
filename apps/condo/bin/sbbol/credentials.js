7ed4cd4d946bb375940cd3ef4a6568a4b2de6566
Антон Алёшин <Anton_Aleschin@mail.ru>
2021-12-01 11:16:58 +0300

    DOMA-1722 refactored old usage of `getOrganizationAccessToken` via class

--- apps/condo/bin/sbbol/credentials.js
+++ apps/condo/bin/sbbol/credentials.js
@@ -1,10 +1,26 @@
+/**
+ * Manages credentials through SBBOL API
+ *
+ * Example:
+ *      yarn workspace @app/condo sbbol:credentials refresh-all-tokens
+ *      yarn workspace @app/condo sbbol:credentials refresh-client-secret
+ */
 const path = require('path')
+const { values } = require('lodash')
 const { GraphQLApp } = require('@keystonejs/app-graphql')
 const { SBBOL_IMPORT_NAME } = require('@condo/domains/organization/integrations/sbbol/common')
 const { SbbolRequestApi } = require('@condo/domains/organization/integrations/sbbol/SbbolRequestApi')
 const { TokenSet: TokenSetApi, Organization: OrganizationApi } = require('@condo/domains/organization/utils/serverSchema')
 const conf = require('@core/config')
+const { getOrganizationAccessToken } = require('@condo/domains/organization/integrations/sbbol/accessToken')
+
 const SBBOL_FINTECH_CONFIG = conf.SBBOL_FINTECH_CONFIG ? JSON.parse(conf.SBBOL_FINTECH_CONFIG) : {}
+const SBBOL_PFX = conf.SBBOL_PFX ? JSON.parse(conf.SBBOL_PFX) : {}
+
+const COMMAND = {
+    REFRESH_ALL_TOKENS: 'refresh-all-tokens',
+    REFRESH_CLIENT_SECRET: 'refresh-client-secret',
+}
 
 
 class SbbolCredentials {
@@ -22,12 +38,40 @@ class SbbolCredentials {
     }
 
     async getAccessToken () {
-        const serviceToken = await SbbolRequestApi.getOrganizationAccessToken(SBBOL_FINTECH_CONFIG.service_organization_hashOrgId)
-        return serviceToken
+        // `service_organization_hashOrgId` is a `userInfo.HashOrgId` from SBBOL, that used to obtain accessToken
+        // for organization, that will be queried in SBBOL using `SbbolFintechApi`.
+        const accessToken = await getOrganizationAccessToken(SBBOL_FINTECH_CONFIG.service_organization_hashOrgId)
+        return accessToken
     }
 
-    async updateSecret () {
-        // we need to update secret of application once in a 2 weeks
+    async refreshClientSecret ({ clientId, clientSecret }) {
+        const accessToken = this.getAccessToken()
+        const requestApi = new SbbolRequestApi({
+            accessToken,
+            host: SBBOL_FINTECH_CONFIG.host,
+            port: SBBOL_FINTECH_CONFIG.port,
+            certificate: SBBOL_PFX.certificate,
+            passphrase: SBBOL_PFX.passphrase,
+        })
+
+        const { data, statusCode } = await requestApi.request({
+            method: 'GET',
+            path: 'ic/sso/api/v1/change-client-secret',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: {
+                client_id: clientId,
+                client_secret: clientSecret,
+            },
+        })
+        if (statusCode !== 200) {
+            throw new Error('Something went wrong')
+        }
+        if (data) {
+            const jsonData = JSON.parse(data)
+            console.log(jsonData)
+        }
     }
 
     async refreshAllTokens () {
@@ -36,7 +80,7 @@ class SbbolCredentials {
         await Promise.all(organizations.map(async organization => {
             console.log('Updating tokens for ', organization.name)
             try {
-                await SbbolRequestApi.getOrganizationAccessToken(organization.importId)
+                await getOrganizationAccessToken(organization.importId)
             } catch (error) {
                 console.log(error)
             }
@@ -47,14 +91,35 @@ class SbbolCredentials {
 }
 
 const workerJob = async () => {
-    const tokensJob = new SbbolCredentials()
-    await tokensJob.connect()
-    await tokensJob.refreshAllTokens()
+    const [command] = process.argv.slice(2)
+    if (!values(COMMAND).includes(command)) {
+        throw new Error('Wrong command.')
+    }
+
+    const credentialsManager = new SbbolCredentials()
+    await credentialsManager.connect()
+
+    if (command === COMMAND.REFRESH_ALL_TOKENS) {
+        await credentialsManager.refreshAllTokens()
+    }
+
+    if (command === COMMAND.REFRESH_CLIENT_SECRET) {
+        const [clientId, clientSecret] = process.argv.slice(3)
+        if (!clientId) {
+            throw new Error('cliendId should be specified as a first argument of the command')
+        }
+        if (!clientSecret) {
+            throw new Error('Old clientSecret is not specified as a second argument of the command')
+        }
+
+        await credentialsManager.refreshClientSecret({ clientId, clientSecret })
+    }
 }
 
 workerJob().then(() => {
     console.log('All done')
     process.exit(0)
 }).catch(err => {
-    console.error('Failed to done', err)
+    console.error(err)
+    process.exit(1)
 })
