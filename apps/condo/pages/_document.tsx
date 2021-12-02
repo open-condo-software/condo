17ce381af86046a157f88f361208eb6ace7dad72
Dmitriy Nikiforov <tuningiposadka@gmail.com>
2021-12-02 19:37:40 +0500

    DOMA-1735 fix head meta tag warning

--- apps/condo/pages/_document.tsx
+++ apps/condo/pages/_document.tsx
@@ -16,7 +16,6 @@ export default class MyDocument extends Document {
             <Html>
                 <Head>
                     <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon"/>
-                    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
                 </Head>
                 <body>
                     <Main/>
