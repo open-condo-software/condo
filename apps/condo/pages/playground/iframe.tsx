b7d107baea9a9bf0f73aec3b4785f83d091072ad
matthewsavelev <savelevmatthew@gmail.com>
2021-12-20 19:13:55 +0500

    DOMA-1831 Minor fixes to make it work

---
+++ apps/condo/pages/playground/iframe.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Head from 'next/head'
+import { PageWrapper } from '@condo/domains/common/components/containers/BaseLayout'
+import { useIFrame } from '@condo/domains/common/hooks/useIFrame'
+import { useOrganization } from '@core/next/organization'
+import get from 'lodash/get'
+
+const ContactsPage = () => {
+    const iframe = useIFrame('http://localhost:3002/import')
+    const { organization } = useOrganization()
+
+    return (
+        <>
+            <Head>
+                <title>Страница с iframe</title>
+            </Head>
+            <PageWrapper>
+                {get(organization, 'id')x}
+                {iframe}
+            </PageWrapper>
+        </>
+    )
+}
+
+export default ContactsPage
