0c5f59917a3667e3e4edb1923ce457de1be1094f
matthewsavelev <savelevmatthew@gmail.com>
2021-12-21 16:47:04 +0500

    DOMA-1831 Added IFrameWrapper to automate authorization needs

--- apps/condo/domains/common/components/IFrame/IFrameWrapper.tsx
+++ apps/condo/domains/common/components/IFrame/IFrameWrapper.tsx
@@ -1,11 +1,32 @@
 import React, { useEffect } from 'react'
-import { sendLoadedStatus } from './utils'
+import { sendLoadedStatus, sendRequirementRequest } from './utils'
+import { useAuth } from '@core/next/auth'
+import { useOrganization } from '@core/next/organization'
 
-export const IFrameWrapper: React.FC = (props) => {
+interface IFrameWrapperProps {
+    withOrganization?: boolean
+    withUser?: boolean
+}
+
+export const IFrameWrapper: React.FC<IFrameWrapperProps> = (props) => {
+    const { withUser, withOrganization } = props
+    const parentType = typeof parent
+    const { isAuthenticated } = useAuth()
+    const { organization } = useOrganization()
+
     useEffect(() => {
         // NOTE: Sending load info manually for Safari support, in which iframe.onload sometimes doesn't trigger
         sendLoadedStatus()
-    }, [])
+    }, [parentType])
+
+    useEffect(() => {
+        if (withOrganization && !organization) sendRequirementRequest('organization')
+    }, [parentType, organization, withOrganization])
+
+    useEffect(() => {
+        if (withUser && !isAuthenticated) sendRequirementRequest('auth')
+    }, [parentType, isAuthenticated, withUser])
+
 
     return (
         <>
