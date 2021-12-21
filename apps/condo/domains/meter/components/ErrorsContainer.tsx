9b4df5b28f32657ff8904114d672b1708fddb3af
Pavel <nomerdvadcatpyat@gmail.com>
2021-12-21 13:05:07 +0500

    DOMA-1803 fix typo in translation

--- apps/condo/domains/meter/components/ErrorsContainer.tsx
+++ apps/condo/domains/meter/components/ErrorsContainer.tsx
@@ -13,31 +13,21 @@ export const ErrorsContainer: React.FC<IErrorsContainerProps> = ({ property, uni
     const intl = useIntl()
     const ErrorsContainerTitle = intl.formatMessage({ id: 'errorsContainer.requiredErrors' })
     const AddressLabel = intl.formatMessage({ id: 'field.Address' })
+    const UnitMessage = intl.formatMessage({ id: 'field.UnitName' })
 
     const disableUserInteraction = !property || !unitName
     const propertyErrorMessage = !property && AddressLabel
-    const unitErrorMessage = !unitName && 'Квартира'
-    const newMeterReadingsErrorMessage = isEmpty(newMeterReadings) && 'Для отправки показаний введите хотя бы одно показание'
+    const unitErrorMessage = !unitName && UnitMessage
     const fieldsErrorMessages = [propertyErrorMessage, unitErrorMessage]
         .filter(Boolean)
         .map(errorField => errorField.toLowerCase())
         .join(', ')
 
     return (
-        (disableUserInteraction || newMeterReadingsErrorMessage) && (
+        disableUserInteraction && fieldsErrorMessages && (
             <ErrorsWrapper>
-                {
-                    fieldsErrorMessages ? (
-                        <>
-                            {ErrorsContainerTitle}&nbsp;
-                            {fieldsErrorMessages}
-                        </>
-                    ) : (
-                        <>
-                            { newMeterReadingsErrorMessage }
-                        </>
-                    )
-                }
+                {ErrorsContainerTitle}&nbsp;
+                {fieldsErrorMessages}
             </ErrorsWrapper>
         )
     )
