028609fc0fd0925eabe3e4d889b7651dfa2b80a1
toplenboren <toplenboren@gmail.com>
2021-12-22 20:52:35 +0500

    DOMA-1876 add tests for getting hostUrl from serviceConsumer

--- apps/condo/domains/resident/schema/ServiceConsumer.js
+++ apps/condo/domains/resident/schema/ServiceConsumer.js
@@ -91,7 +91,7 @@ const ServiceConsumer = new GQLListSchema('ServiceConsumer', {
             resolver: async (item) => {
                 if (!item.acquiringIntegrationContext) { return null }
                 const acquiringIntegrationContext = await getById('AcquiringIntegrationContext', item.acquiringIntegrationContext)
-                const acquiringIntegration = await getById('AcquiringIntegration', get(item.acquiringIntegrationContext, 'integration'))
+                const acquiringIntegration = await getById('AcquiringIntegration', get(acquiringIntegrationContext, 'integration'))
 
                 const result = pick(acquiringIntegrationContext, ['id', 'integration'])
                 result.integration = acquiringIntegration
