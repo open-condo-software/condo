cb015fbde2ef244c17c1acfd6922718e5102a5a7
matthewsavelev <savelevmatthew@gmail.com>
2021-12-22 15:28:29 +0500

    DOMA-1831 Unite utils and made messages bidirectional

--- apps/condo/domains/common/components/IFrame/IFrameWrapper.tsx
+++ apps/condo/domains/common/components/IFrame/IFrameWrapper.tsx
@@ -1,31 +1,42 @@
 import React, { useEffect } from 'react'
-import { sendLoadedStatus, sendRequirementRequest } from './utils'
+import { sendLoadedStatus, sendRequirementRequest } from '@condo/domains/common/utils/iframe.utils'
 import { useAuth } from '@core/next/auth'
 import { useOrganization } from '@core/next/organization'
+import getConfig from 'next/config'
 
 interface IFrameWrapperProps {
     withOrganization?: boolean
     withUser?: boolean
+    parentOrigin?: string
 }
 
+const { publicRuntimeConfig: { serverUrl } } = getConfig()
+
 export const IFrameWrapper: React.FC<IFrameWrapperProps> = (props) => {
-    const { withUser, withOrganization } = props
+    const { withUser, withOrganization, parentOrigin: propParentOrigin } = props
+    const parentOrigin = propParentOrigin ? propParentOrigin : serverUrl
     const parentType = typeof parent
     const { isAuthenticated } = useAuth()
     const { organization } = useOrganization()
 
     useEffect(() => {
         // NOTE: Sending load info manually for Safari support, in which iframe.onload sometimes doesn't trigger
-        sendLoadedStatus()
-    }, [parentType])
+        if (parentType) {
+            sendLoadedStatus(parent, parentOrigin)
+        }
+    }, [parentType, parentOrigin])
 
     useEffect(() => {
-        if (withOrganization && !organization) sendRequirementRequest('organization')
-    }, [parentType, organization, withOrganization])
+        if (withOrganization && !organization && parentType) {
+            sendRequirementRequest('organization', parent, parentOrigin)
+        }
+    }, [parentType, organization, withOrganization, parentOrigin])
 
     useEffect(() => {
-        if (withUser && !isAuthenticated) sendRequirementRequest('auth')
-    }, [parentType, isAuthenticated, withUser])
+        if (withUser && !isAuthenticated) {
+            sendRequirementRequest('auth', parent, parentOrigin)
+        }
+    }, [parentType, isAuthenticated, withUser, parentOrigin])
 
 
     return (
