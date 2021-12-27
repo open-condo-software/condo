37832010f3cb00c4567c1a813bc2f11279e5726a
akarjakin <akarjakin@doma.ai>
2021-12-27 13:28:05 +0500

    DOMA-1892 added personal tin support, fixed tests

--- apps/condo/domains/organization/utils/tin.utils.js
+++ apps/condo/domains/organization/utils/tin.utils.js
@@ -6,6 +6,7 @@ const { RUSSIA_COUNTRY } = require('@condo/domains/common/constants/countries')
 // Digits for Ru tin checksum calculation
 const RU_TIN_DIGITS = [3, 7, 2, 4, 10, 3, 5, 9, 4, 6, 8, 0]
 const RU_ORGANIZATION_TIN_REGEXP = /^\d{10}$/
+const RU_PERSONAL_TIN_REGEXP = /^\d{12}$/
 
 const getTinChecksumRU = num => {
     const n = RU_TIN_DIGITS.slice(-num.length)
@@ -21,12 +22,9 @@ const validateTinRU = tinValue => {
 
     const inn = tinValue.toString().trim()
 
-    if (!RU_ORGANIZATION_TIN_REGEXP.test(inn)) return false
+    if (!RU_ORGANIZATION_TIN_REGEXP.test(inn) && !RU_PERSONAL_TIN_REGEXP.test(inn)) return false
     if (inn.length == 10) return getTinChecksumRU(inn) == inn.slice(-1)
-
-    // NOTE: we need INNs only for organizations, that is of 10 chars length.
-    // So valid 12 char length person INN doesn`t suit
-    // if (inn.length == 12) return getTinChecksumRU(inn.slice(0, 11)) == inn.slice(10, -1) && getTinChecksumRU(inn) == inn.slice(-1)
+    if (inn.length == 12) return getTinChecksumRU(inn.slice(0, 11)) == inn.slice(10, -1) && getTinChecksumRU(inn) == inn.slice(-1)
 
     return false
 }
