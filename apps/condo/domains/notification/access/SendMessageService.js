ec51e2c6d51db94d8310e3a5df772690fc3a7e2e
matthewsavelev <savelevmatthew@gmail.com>
2021-12-29 15:32:27 +0500

    DOMA-1903 Rebasing conflicts with 1898 task

--- apps/condo/domains/notification/access/SendMessageService.js
+++ apps/condo/domains/notification/access/SendMessageService.js
@@ -1,8 +1,12 @@
 const { throwAuthenticationError } = require('@condo/domains/common/utils/apolloErrorFormatter')
+const { USER_SCHEMA_NAME } = require('@condo/domains/common/constants/utils')
 
-async function canSendMessage ({ authentication: { item: user } }) {
-    if (!user) return throwAuthenticationError()
-    if (user.isAdmin) return true
+async function canSendMessage ({ authentication: { item, listKey } }) {
+    if (!listKey || !item) return throwAuthenticationError()
+    if (item.deletedAt) return false
+    if (listKey === USER_SCHEMA_NAME) {
+        return !!item.isAdmin
+    }
     return false
 }
 
