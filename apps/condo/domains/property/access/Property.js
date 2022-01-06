129c5f095398428760cb778b577850785d4c4398
matthewsavelev <savelevmatthew@gmail.com>
2022-01-06 14:13:57 +0500

    DOMA-1903 Domain Property processed

--- apps/condo/domains/property/access/Property.js
+++ apps/condo/domains/property/access/Property.js
@@ -4,67 +4,57 @@
 const get = require('lodash/get')
 const { queryOrganizationEmployeeFromRelatedOrganizationFor } = require('@condo/domains/organization/utils/accessSchema')
 const { queryOrganizationEmployeeFor } = require('@condo/domains/organization/utils/accessSchema')
-const { getById } = require('@core/keystone/schema')
+const { getById, find } = require('@core/keystone/schema')
 const { checkOrganizationPermission } = require('@condo/domains/organization/utils/accessSchema')
 const { throwAuthenticationError } = require('@condo/domains/common/utils/apolloErrorFormatter')
-const { Resident } = require('@condo/domains/resident/utils/serverSchema')
 const { RESIDENT } = require('@condo/domains/user/constants/common')
 const { uniq, compact } = require('lodash')
+const { USER_SCHEMA_NAME } = require('@condo/domains/common/constants/utils')
 
 
-async function canReadProperties ({ authentication: { item: user }, context }) {
-    if (!user) return throwAuthenticationError()
-    if (user.isAdmin || user.isSupport) return {}
-    const userId = user.id
-    if (user.type === RESIDENT) {
-        const residents = await Resident.getAll(context, { user: { id: userId } })
-        if (residents.length === 0) {
-            return false
-        }
-        const propertyIds = compact(residents.map(resident => get(resident, ['property', 'id'])))
-        if (propertyIds.length > 0) {
+async function canReadProperties ({ authentication: { item, listKey } }) {
+    if (!listKey || !item) return throwAuthenticationError()
+    if (item.deletedAt) return false
+    if (listKey === USER_SCHEMA_NAME) {
+        if (item.isSupport || item.isAdmin) return {}
+        const userId = item.id
+        if (item.type === RESIDENT) {
+            const residents = await find('Resident', { user: { id: userId }, deletedAt: null })
+            if (!residents.length) return false
+            const residentProperties = compact(residents.map(resident => get(resident, 'property')))
             return {
-                // We can have multiple residents in different units of the same property for current user,
-                // and ids and be duplicated
-                id_in: uniq(propertyIds),
+                id_in: uniq(residentProperties),
             }
+        }
+        return {
+            organization: {
+                OR: [
+                    queryOrganizationEmployeeFor(userId),
+                    queryOrganizationEmployeeFromRelatedOrganizationFor(userId),
+                ],
+            },
+        }
+    }
+    return false
+}
+
+async function canManageProperties ({ authentication: { item, listKey }, originalInput, operation, itemId }) {
+    if (!listKey || !item) return throwAuthenticationError()
+    if (item.deletedAt) return false
+    if (listKey === USER_SCHEMA_NAME) {
+        if (item.isAdmin) return true
+        if (operation === 'create') {
+            const organizationId = get(originalInput, ['organization', 'connect', 'id'])
+            if (!organizationId) return false
+            return await checkOrganizationPermission(item.id, organizationId, 'canManageProperties')
+        } else if (operation === 'update' && itemId) {
+            const property = await getById('Property', itemId)
+            if (!property) return false
+            const { organization: organizationId } = property
+            return await checkOrganizationPermission(item.id, organizationId, 'canManageProperties')
         }
         return false
     }
-    return {
-        organization: {
-            OR: [
-                queryOrganizationEmployeeFor(userId),
-                queryOrganizationEmployeeFromRelatedOrganizationFor(userId),
-            ],
-        },
-    }
-}
-
-async function canManageProperties ({ authentication: { item: user }, originalInput, operation, itemId, context }) {
-    if (!user) return throwAuthenticationError()
-    if (user.isAdmin) return true
-    if (operation === 'create') {
-        const organizationId = get(originalInput, ['organization', 'connect', 'id'])
-        if (!organizationId) {
-            return false
-        }
-
-        return await checkOrganizationPermission(user.id, organizationId, 'canManageProperties')
-    } else if (operation === 'update') {
-        if (!itemId) {
-            return false
-        }
-
-        const property = await getById('Property', itemId)
-        if (!property) {
-            return false
-        }
-
-        const { organization: organizationId } = property
-
-        return await checkOrganizationPermission(user.id, organizationId, 'canManageProperties')
-    } 
     return false
 }
 
