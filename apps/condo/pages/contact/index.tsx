59305edfe35de0f63d707bb4cd6eab58281226a3
Pavel <nomerdvadcatpyat@gmail.com>
2022-01-12 17:12:41 +0500

    DOMA-1770 added spase after import button on contact and property pages

--- apps/condo/pages/contact/index.tsx
+++ apps/condo/pages/contact/index.tsx
@@ -1,3 +1,13 @@
+import { Col, Input, Row, Typography } from 'antd'
+import Head from 'next/head'
+import { useRouter } from 'next/router'
+import { get } from 'lodash'
+import React, { useCallback } from 'react'
+import { Gutter } from 'antd/es/grid/row'
+import { useIntl } from '@core/next/intl'
+import { useOrganization } from '@core/next/organization'
+import { DiffOutlined } from '@ant-design/icons'
+
 import {
     PageHeader,
     PageWrapper,
@@ -5,30 +15,18 @@ import {
 } from '@condo/domains/common/components/containers/BaseLayout'
 import { OrganizationRequired } from '@condo/domains/organization/components/OrganizationRequired'
 import {
-    filtersToQuery,
     getPageIndexFromQuery,
-    getSortStringFromQuery,
     CONTACT_PAGE_SIZE,
-    sorterToQuery, queryToSorter,
 } from '@condo/domains/contact/utils/helpers'
 import { getFiltersFromQuery } from '@condo/domains/common/utils/helpers'
 import { IFilters } from '@condo/domains/contact/utils/helpers'
-import { useIntl } from '@core/next/intl'
-
-import { Col, Input, Row, Typography } from 'antd'
-import Head from 'next/head'
-import { useRouter } from 'next/router'
-import { get, debounce } from 'lodash'
-import React, { useCallback, useState } from 'react'
 import { EmptyListView } from '@condo/domains/common/components/EmptyListView'
 import { useTableColumns } from '@condo/domains/contact/hooks/useTableColumns'
 import { useSearch } from '@condo/domains/common/hooks/useSearch'
-import { useOrganization } from '@core/next/organization'
 import { Contact } from '@condo/domains/contact/utils/clientSchema'
 import { Button } from '@condo/domains/common/components/Button'
 import { TitleHeaderAction } from '@condo/domains/common/components/HeaderActions'
 import { ImportWrapper } from '@condo/domains/common/components/Import/Index'
-import { DiffOutlined } from '@ant-design/icons'
 import { useImporterFunctions } from '@condo/domains/contact/hooks/useImporterFunctions'
 import { getTableScrollConfig, parseQuery } from '@condo/domains/common/utils/tables.utils'
 import { TablePageContent } from '@condo/domains/common/components/containers/BaseLayout/BaseLayout'
@@ -37,6 +35,8 @@ import { Table } from '@condo/domains/common/components/Table/Index'
 import { useContactsTableFilters } from '@condo/domains/contact/hooks/useTableFilters'
 
 const ADD_CONTACT_ROUTE = '/contact/create/'
+const ROW_VERTICAL_GUTTERS: [Gutter, Gutter] = [0, 40]
+const ROW_HORIZONTAL_GUTTERS: [Gutter, Gutter] = [10, 0]
 
 export const ContactsPageContent = ({
     tableColumns,
@@ -104,9 +104,9 @@ export const ContactsPageContent = ({
                                 />
                             )
                             : (
-                                <Row gutter={[0, 40]} align={'middle'} justify={'start'}>
+                                <Row gutter={ROW_VERTICAL_GUTTERS} align={'middle'} justify={'start'}>
                                     <Col span={24}>
-                                        <Row justify={'start'} gutter={[0, 40]}>
+                                        <Row justify={'space-between'} gutter={ROW_VERTICAL_GUTTERS}>
                                             <Col xs={24} lg={6}>
                                                 <Input
                                                     placeholder={SearchPlaceholder}
@@ -114,43 +114,47 @@ export const ContactsPageContent = ({
                                                     value={search}
                                                 />
                                             </Col>
-                                            <Col lg={1} offset={13} hidden={isSmall}>
-                                                {
-                                                    canManageContacts && (
-                                                        <ImportWrapper
-                                                            objectsName={ContactsMessage}
-                                                            accessCheck={canManageContacts}
-                                                            onFinish={refetch}
-                                                            columns={columns}
-                                                            rowNormalizer={contactNormalizer}
-                                                            rowValidator={contactValidator}
-                                                            objectCreator={contactCreator}
-                                                            domainTranslate={ContactTitle}
-                                                            exampleTemplateLink={'/contact-import-example.xlsx'}
-                                                        >
-                                                            <Button
-                                                                type={'sberPrimary'}
-                                                                icon={<DiffOutlined />}
-                                                                block
-                                                                secondary
-                                                            />
-                                                        </ImportWrapper>
-                                                    )
-                                                }
-                                            </Col>
-                                            <Col xs={24} lg={4}>
-                                                {
-                                                    canManageContacts && (
-                                                        <Button
-                                                            block={!isSmall}
-                                                            key='left'
-                                                            type={'sberPrimary'}
-                                                            onClick={() => router.push(ADD_CONTACT_ROUTE)}
-                                                        >
-                                                            {CreateContact}
-                                                        </Button>
-                                                    )
-                                                }
+                                            <Col>
+                                                <Row gutter={ROW_HORIZONTAL_GUTTERS} align={'middle'} justify={'center'}>
+                                                    <Col hidden={isSmall}>
+                                                        {
+                                                            canManageContacts && (
+                                                                <ImportWrapper
+                                                                    objectsName={ContactsMessage}
+                                                                    accessCheck={canManageContacts}
+                                                                    onFinish={refetch}
+                                                                    columns={columns}
+                                                                    rowNormalizer={contactNormalizer}
+                                                                    rowValidator={contactValidator}
+                                                                    objectCreator={contactCreator}
+                                                                    domainTranslate={ContactTitle}
+                                                                    exampleTemplateLink={'/contact-import-example.xlsx'}
+                                                                >
+                                                                    <Button
+                                                                        type={'sberPrimary'}
+                                                                        icon={<DiffOutlined />}
+                                                                        block
+                                                                        secondary
+                                                                    />
+                                                                </ImportWrapper>
+                                                            )
+                                                        }
+                                                    </Col>
+                                                    <Col>
+                                                        {
+                                                            canManageContacts && (
+                                                                <Button
+                                                                    block={!isSmall}
+                                                                    key='left'
+                                                                    type={'sberPrimary'}
+                                                                    onClick={() => router.push(ADD_CONTACT_ROUTE)}
+                                                                >
+                                                                    {CreateContact}
+                                                                </Button>
+                                                            )
+                                                        }
+                                                    </Col>
+                                                </Row>
                                             </Col>
                                         </Row>
                                     </Col>
