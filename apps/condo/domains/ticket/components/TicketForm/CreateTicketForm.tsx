9661ee21f24c3cf809b89c30ddbce018d50209a3
AleX83Xpert <AleX83Xpert@mail.ru>
2022-01-17 07:35:32 +0500

    DOMA-1752 New hook to working with cache was added. Using this hook to update cache after new ticket created.

--- apps/condo/domains/ticket/components/TicketForm/CreateTicketForm.tsx
+++ apps/condo/domains/ticket/components/TicketForm/CreateTicketForm.tsx
@@ -1,15 +1,17 @@
-import { Form, Space } from 'antd'
-import React, { useCallback, useEffect, useRef } from 'react'
-import { useOrganization } from '@core/next/organization'
-import { useRouter } from 'next/router'
+import ActionBar from '@condo/domains/common/components/ActionBar'
+import { Button } from '@condo/domains/common/components/Button'
+import { BaseTicketForm } from '@condo/domains/ticket/components/BaseTicketForm'
+import { ErrorsContainer } from '@condo/domains/ticket/components/BaseTicketForm/ErrorsContainer'
+import { REQUIRED_TICKET_FIELDS } from '@condo/domains/ticket/constants/common'
+import { useCacheUtils } from '@condo/domains/ticket/hooks/useCacheUtils'
+import { Ticket } from '@condo/domains/ticket/utils/clientSchema'
 import { useAuth } from '@core/next/auth'
 import { useIntl } from '@core/next/intl'
-import { BaseTicketForm } from '../BaseTicketForm'
-import { REQUIRED_TICKET_FIELDS } from '@condo/domains/ticket/constants/common'
-import { Button } from '@condo/domains/common/components/Button'
-import { ErrorsContainer } from '../BaseTicketForm/ErrorsContainer'
-import { Ticket } from '@condo/domains/ticket/utils/clientSchema'
-import ActionBar from '@condo/domains/common/components/ActionBar'
+import { useOrganization } from '@core/next/organization'
+import { Form, Space } from 'antd'
+import { useRouter } from 'next/router'
+import React, { useCallback } from 'react'
+import { useApolloClient } from '@core/next/apollo'
 
 const OPEN_STATUS = '6ef3abc4-022f-481b-90fb-8430345ebfc2'
 const DEFAULT_TICKET_SOURCE_CALL_ID = '779d7bb6-b194-4d2c-a967-1f7321b2787f'
@@ -57,13 +59,16 @@ export const CreateTicketForm: React.FC = () => {
     const { organization, link } = useOrganization()
     const router = useRouter()
     const auth = useAuth() as { user: { id: string } }
+    const client = useApolloClient()
+    const { addTicketToQueryCacheForTicketCardList } = useCacheUtils(client.cache)
 
     const action = Ticket.useCreate(
         {
             status: OPEN_STATUS,
             source: DEFAULT_TICKET_SOURCE_CALL_ID,
         },
-        () => {
+        (ticket) => {
+            addTicketToQueryCacheForTicketCardList(ticket)
             router.push('/ticket')
         })
 
