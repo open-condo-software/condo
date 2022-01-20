64c8bbdda3fb7dc035e14c06b34b7b48e6d6372d
Pavel <nomerdvadcatpyat@gmail.com>
2022-01-21 02:54:10 +0500

    DOMA-1833 renamed filters to fields in client schema

--- apps/condo/domains/ticket/schema/TicketFilterTemplate.js
+++ apps/condo/domains/ticket/schema/TicketFilterTemplate.js
@@ -9,7 +9,7 @@ const { historical, versioned, uuided, tracked, softDeleted } = require('@core/k
 const { SENDER_FIELD, DV_FIELD } = require('@condo/domains/common/schema/fields')
 const access = require('@condo/domains/ticket/access/TicketFilterTemplate')
 
-const { TICKET_FILTER_FIELD } = require('./fields/TicketFilters')
+const { TICKET_FILTER_FIELD } = require('./fields/TicketFilter')
 
 
 const TicketFilterTemplate = new GQLListSchema('TicketFilterTemplate', {
