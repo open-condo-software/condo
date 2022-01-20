f70ff37c592d4137a33d06ab3fa5c1dd2808d606
Антон Алёшин <Anton_Aleschin@mail.ru>
2022-01-20 14:58:03 +0300

    DOMA-1722 fallback to environment variable when obtaining current value of client secret
    (cherry picked from commit a4810f336eccf9ec31f36fca05bacbd8266b2941)

--- apps/condo/domains/organization/tasks/refreshSbbolClientSecret.js
+++ apps/condo/domains/organization/tasks/refreshSbbolClientSecret.js
@@ -20,13 +20,18 @@ const refreshSbbolClientSecret = createCronTask('refreshSbbolClientSecret', '0 9
     // Assume, that we have once logged in using SBBOl and got a `TokenSet` record for our organization
     const { tokenSet } = await credentialsManager.getAccessToken()
 
+    // Actually, a value of client secret is set for `TokenSet` record in `syncTokens` after first logging in,
+    // but, this auto-refresh task will be deployed, having existing record in `TokenSet` table, that missing
+    // the value. To be sure that we have a current value of client secret, fallback to environment variable
+    const currentClientSecret = tokenSet.clientSecret || SBBOL_AUTH_CONFIG.client_secret
+
     const now = dayjs()
     if (now.isBefore(dayjs(tokenSet.clientSecretExpiresAt))) {
         logger.info('No need to change client secret', { clientSecretExpiresAt: tokenSet.clientSecretExpiresAt } )
     } else {
         await credentialsManager.changeClientSecret({
             clientId: SBBOL_AUTH_CONFIG.client_id,
-            currentClientSecret: tokenSet.clientSecret,
+            currentClientSecret,
             newClientSecret: faker.random.alphaNumeric(8),
         })
     }
