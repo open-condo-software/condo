a800a26f6f8a58437ac2bec2919fb3e67c0b13b9
AleX83Xpert <AleX83Xpert@mail.ru>
2022-01-24 16:32:40 +0500

    DOMA-2027 DOMA-2030 service for exporting contacts

---
+++ apps/condo/domains/contact/access/ExportContactsService.js
@@ -0,0 +1,27 @@
+const { throwAuthenticationError } = require('@condo/domains/common/utils/apolloErrorFormatter')
+const { checkOrganizationPermission, checkRelatedOrganizationPermission } = require('@condo/domains/organization/utils/accessSchema')
+const { Organization } = require('@condo/domains/organization/utils/serverSchema')
+const get = require('lodash/get')
+
+async function canExportContactsToExcel ({ args: { data: { where } }, authentication: { item: user }, context }) {
+    if (!user) return throwAuthenticationError()
+    if (user.isAdmin) return true
+    const organizationId = get(where, 'organization.id')
+    if (!organizationId) {
+        const organizationFromWhere = get(where, 'organization')
+        const [relatedFromOrganization] = await Organization.getAll(context, organizationFromWhere)
+        if (!relatedFromOrganization) {
+            return false
+        }
+        const canManageRelatedOrganizationContacts = await checkRelatedOrganizationPermission(context, user.id, relatedFromOrganization.id, 'canManageContacts')
+        if (canManageRelatedOrganizationContacts) {
+            return true
+        }
+        return false
+    }
+    return await checkOrganizationPermission(context, user.id, organizationId, 'canManageContacts')
+}
+
+module.exports = {
+    canExportContactsToExcel,
+}
---
+++ apps/condo/domains/contact/schema/ExportContactsService.js
@@ -0,0 +1,63 @@
+const { EMPTY_DATA_EXPORT_ERROR } = require('@condo/domains/common/constants/errors')
+const { createExportFile } = require('@condo/domains/common/utils/createExportFile')
+const access = require('@condo/domains/contact/access/ExportContactsService')
+const { loadContactsForExcelExport } = require('@condo/domains/contact/utils/serverSchema')
+const { GQLCustomSchema } = require('@core/keystone/schema')
+const dayjs = require('dayjs')
+const timezone = require('dayjs/plugin/timezone')
+const utc = require('dayjs/plugin/utc')
+
+dayjs.extend(utc)
+dayjs.extend(timezone)
+
+const ExportContactsService = new GQLCustomSchema('ExportContactsService', {
+    types: [
+        {
+            access: true,
+            type: 'input ExportContactsToExcelInput { where: ContactWhereInput!, sortBy: [SortContactsBy!], timeZone: String! }',
+        },
+        {
+            access: true,
+            type: 'type ExportContactsToExcelOutput { status: String!, linkToFile: String! }',
+        },
+    ],
+    queries: [
+        {
+            access: access.canExportContactsToExcel,
+            schema: 'exportContactsToExcel(data: ExportContactsToExcelInput!): ExportContactsToExcelOutput',
+            resolver: async (parent, args, context, info, extra = {}) => {
+                const { where, sortBy } = args.data
+                const contacts = await loadContactsForExcelExport({ where, sortBy })
+                if (contacts.length === 0) {
+                    throw new Error(`${EMPTY_DATA_EXPORT_ERROR}] empty export file`)
+                }
+                const excelRows = contacts.map(contact => {
+                    return {
+                        name: contact.name,
+                        address: contact.property,
+                        unitName: contact.unitName,
+                        phone: contact.phone,
+                        email: contact.email,
+                    }
+                })
+                const linkToFile = await createExportFile({
+                    fileName: `contacts_${dayjs().format('DD_MM')}.xlsx`,
+                    templatePath: './domains/contact/templates/ContactsExportTemplate.xlsx',
+                    replaces: { contacts: excelRows },
+                    meta: {
+                        listkey: 'Contact',
+                        id: contacts[0].id,
+                    },
+                })
+                return { status: 'ok', linkToFile }
+            },
+        },
+    ],
+    mutations: [
+
+    ],
+})
+
+module.exports = {
+    ExportContactsService,
+}
