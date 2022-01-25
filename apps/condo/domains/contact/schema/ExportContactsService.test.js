dbcf4e81e2fbd40f92b7bde300bc2c50d2aeac76
AleX83Xpert <AleX83Xpert@mail.ru>
2022-01-25 10:08:24 +0500

    DOMA-2027 DOMA-2030 stop using `isObsConfigured` in tests

--- apps/condo/domains/contact/schema/ExportContactsService.test.js
+++ apps/condo/domains/contact/schema/ExportContactsService.test.js
@@ -6,20 +6,21 @@ const { EXPORT_CONTACTS_TO_EXCEL } = require('@condo/domains/contact/gql')
 const { makeClient } = require('@core/keystone/test.utils')
 const { makeClientWithProperty } = require('@condo/domains/property/utils/testSchema')
 const { DEFAULT_ORGANIZATION_TIMEZONE } = require('@condo/domains/organization/constants/common')
-const isObsConfigured = require('@condo/domains/common/utils/testSchema/isObsConfigured')
 
 describe('ExportContactsService', () => {
     describe('User', () => {
         it('can get contacts export from selected organization', async () => {
-            if (isObsConfigured()) {
-                const client = await makeClientWithProperty()
-                await createTestContact(client, client.organization, client.property)
-                const { data: { result: { status, linkToFile } } }  = await client.query(EXPORT_CONTACTS_TO_EXCEL, {
-                    data: { where: { organization: { id: client.organization.id } }, sortBy: 'id_ASC', timeZone: DEFAULT_ORGANIZATION_TIMEZONE },
-                })
-                expect(status).toBe('ok')
-                expect(linkToFile).not.toHaveLength(0)
-            }
+            const client = await makeClientWithProperty()
+            await createTestContact(client, client.organization, client.property)
+            const { data: { result: { status, linkToFile } } } = await client.query(EXPORT_CONTACTS_TO_EXCEL, {
+                data: {
+                    where: { organization: { id: client.organization.id } },
+                    sortBy: 'id_ASC',
+                    timeZone: DEFAULT_ORGANIZATION_TIMEZONE,
+                },
+            })
+            expect(status).toBe('ok')
+            expect(linkToFile).not.toHaveLength(0)
         })
 
         it('can not get contacts export from another organization', async () => {
@@ -29,7 +30,11 @@ describe('ExportContactsService', () => {
             const client2 = await makeClientWithProperty()
             await createTestContact(client2, client2.organization, client2.property)
             const { data: { result }, errors } = await client.query(EXPORT_CONTACTS_TO_EXCEL, {
-                data: { where: { organization: { id: client2.organization.id } }, sortBy: 'id_ASC', timeZone: DEFAULT_ORGANIZATION_TIMEZONE },
+                data: {
+                    where: { organization: { id: client2.organization.id } },
+                    sortBy: 'id_ASC',
+                    timeZone: DEFAULT_ORGANIZATION_TIMEZONE,
+                },
             })
             expect(result).toBeNull()
             expect(errors).toHaveLength(1)
@@ -43,7 +48,11 @@ describe('ExportContactsService', () => {
             const client2 = await makeClientWithProperty()
             await createTestContact(client2, client2.organization, client2.property)
             const { data: { result }, errors } = await client.query(EXPORT_CONTACTS_TO_EXCEL, {
-                data: { where: { organization: { id: client2.organization.id } }, sortBy: 'id_ASC', timeZone: DEFAULT_ORGANIZATION_TIMEZONE },
+                data: {
+                    where: { organization: { id: client2.organization.id } },
+                    sortBy: 'id_ASC',
+                    timeZone: DEFAULT_ORGANIZATION_TIMEZONE,
+                },
             })
             expect(result).toBeNull()
             expect(errors).toHaveLength(1)
