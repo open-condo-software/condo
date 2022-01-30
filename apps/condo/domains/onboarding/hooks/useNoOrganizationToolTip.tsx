4b96c4804d0eb0dec96408a4defa983dea990f1e
AleX83Xpert <AleX83Xpert@mail.ru>
2022-01-30 14:51:21 +0500

    DOMA-1967 The hook for wrapping menu item within tooltip.

---
+++ apps/condo/domains/onboarding/hooks/useNoOrganizationToolTip.tsx
@@ -0,0 +1,33 @@
+import { Tooltip } from 'antd'
+import { TooltipPlacement } from 'antd/es/tooltip'
+import React from 'react'
+import { useIntl } from 'react-intl'
+
+export interface INoOrganizationToolTipWrapper {
+    element: JSX.Element,
+    placement: TooltipPlacement,
+}
+
+interface INoOrganizationToolTipHook {
+    wrapElementIntoNoOrganizationToolTip: (params: INoOrganizationToolTipWrapper) => JSX.Element
+}
+
+export const useNoOrganizationToolTip = (): INoOrganizationToolTipHook => {
+    const intl = useIntl()
+    const CreateOrgWarning = intl.formatMessage({ id: 'onboarding.step.warn.create.Organization' })
+
+    const wrapElementIntoNoOrganizationToolTip = (params: INoOrganizationToolTipWrapper): JSX.Element => {
+        return (
+            <Tooltip
+                title={CreateOrgWarning}
+                placement={params.placement}
+                trigger="click"
+                zIndex={1250}
+            >
+                {params.element}
+            </Tooltip>
+        )
+    }
+
+    return { wrapElementIntoNoOrganizationToolTip }
+}
