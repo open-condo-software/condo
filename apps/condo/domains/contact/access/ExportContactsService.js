31b862cb0f56b71f3bbf0e83a23d4c9306871f6f
matthewsavelev <savelevmatthew@gmail.com>
2022-01-31 16:46:32 +0500

    DOMA-1903 Fixed export contact accesses

--- apps/condo/domains/contact/access/ExportContactsService.js
+++ apps/condo/domains/contact/access/ExportContactsService.js
@@ -3,33 +3,25 @@ const {
     checkOrganizationPermission,
     checkRelatedOrganizationPermission,
 } = require('@condo/domains/organization/utils/accessSchema')
-const { Organization } = require('@condo/domains/organization/utils/serverSchema')
+const { find } = require('@core/keystone/schema')
 const get = require('lodash/get')
 
-async function canExportContactsToExcel ({ args: { data: { where } }, authentication: { item: user }, context }) {
-    if (!user) {
-        return throwAuthenticationError()
-    }
-
-    if (user.isAdmin) {
-        return true
-    }
+async function canExportContactsToExcel ({ args: { data: { where } }, authentication: { item: user } }) {
+    if (!user) return throwAuthenticationError()
+    if (user.deletedAt) return false
+    if (user.isAdmin) return true
 
     const organizationId = get(where, 'organization.id')
 
-    if (!organizationId) {
-        const organizationFromWhere = get(where, 'organization')
-        const [relatedFromOrganization] = await Organization.getAll(context, organizationFromWhere)
-        if (!relatedFromOrganization) {
-            return false
-        }
+    if (organizationId) {
+        return await checkOrganizationPermission(user.id, organizationId, 'canManageContacts')
+    } else {
+        const organizationWhere = get(where, 'organization')
+        const [relatedFromOrganization] = await find('Organization', organizationWhere)
+        if (!relatedFromOrganization) return false
 
-        const canManageRelatedOrganizationContacts = await checkRelatedOrganizationPermission(context, user.id, relatedFromOrganization.id, 'canManageContacts')
-
-        return !!canManageRelatedOrganizationContacts
+        return  await checkRelatedOrganizationPermission(user.id, relatedFromOrganization.id, 'canManageContacts')
     }
-
-    return await checkOrganizationPermission(context, user.id, organizationId, 'canManageContacts')
 }
 
 module.exports = {
