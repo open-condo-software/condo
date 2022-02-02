d72798c2f574208db6a788c34c8226485f42b37e
Kanol <andrey.karmanov.2011@yandex.ru>
2022-02-02 12:41:34 +0500

    DOMA-1904 Set access only for admin or support

--- apps/condo/domains/user/access/RegisterNewServiceUserService.js
+++ apps/condo/domains/user/access/RegisterNewServiceUserService.js
@@ -4,8 +4,8 @@
 const { throwAuthenticationError } = require('@condo/domains/common/utils/apolloErrorFormatter')
 async function canRegisterNewServiceUser ({ authentication: { item: user } }) {
     if (!user) return throwAuthenticationError()
-    if (user.isAdmin) return true
-    return false
+    return !!(user.isAdmin || user.isSupport);
+
 }
 
 /*
