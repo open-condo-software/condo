4f6a5adc96b60ea9d6f9fb6e2fb261d4ca62238a
Sitnikov Ivan <sit7602@gmail.com>
2022-02-10 14:34:38 +0500

    HOTFIX add unitType constants to property

---
+++ apps/condo/domains/property/constants/common.js
@@ -0,0 +1,22 @@
+const FLAT_UNIT_TYPE = 'flat'
+const PARKING_UNIT_TYPE = 'parking'
+const APARTMENT_UNIT_TYPE = 'apartment'
+const COMMERCIAL_UNIT_TYPE = 'commercial'
+const WAREHOUSE_UNIT_TYPE = 'warehouse'
+
+const UNIT_TYPES = [
+    PARKING_UNIT_TYPE,
+    FLAT_UNIT_TYPE,
+    APARTMENT_UNIT_TYPE,
+    COMMERCIAL_UNIT_TYPE,
+    WAREHOUSE_UNIT_TYPE,
+]
+
+module.exports = {
+    PARKING_UNIT_TYPE,
+    FLAT_UNIT_TYPE,
+    APARTMENT_UNIT_TYPE,
+    COMMERCIAL_UNIT_TYPE,
+    WAREHOUSE_UNIT_TYPE,
+    UNIT_TYPES,
+}
