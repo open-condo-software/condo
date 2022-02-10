66fda437919c659c69b5adf0282e55d0e425c62a
toplenboren <toplenboren@gmail.com>
2022-02-10 21:43:34 +0500

    DOMA-2136 Refactor: Make AcquiringChooser and BillingChooser simply call IntegrationChooser

--- apps/condo/domains/acquiring/components/Settings/AcquiringChooser.tsx
+++ apps/condo/domains/acquiring/components/Settings/AcquiringChooser.tsx
@@ -9,99 +9,120 @@ import { AcquiringIntegration, AcquiringIntegrationContext } from '@condo/domain
 import { Loader } from '@condo/domains/common/components/Loader'
 
 
+
+import { BillingIntegration, BillingIntegrationOrganizationContext } from '@condo/domains/billing/utils/clientSchema'
+
+import { IntegrationChooser } from '@condo/domains/billing/components/Settings/IntegrationChooser'
+
 export const AcquiringChooser: React.FC = () => {
     const intl = useIntl()
 
-    const NoPermissionMessage = intl.formatMessage({ id: 'NoPermissionToSettings' })
+    const OneAcquiringWarningMessage = intl.formatMessage({ id: 'OneAcquiringWarning' })
 
-    const userOrganization = useOrganization()
-    const organizationId = get(userOrganization, ['organization', 'id'])
-    const canReadPayments = get(userOrganization, ['link', 'role', 'canReadPayments'], false)
-    const {
-        objs: integrations,
-        loading: integrationsLoading,
-        error: integrationsError,
-    } = AcquiringIntegration.useObjects({
-        where: {
-            isHidden: false,
-        },
-    })
-
-    const {
-        obj: currentContext,
-        error: contextError,
-        loading: contextLoading,
-    } = AcquiringIntegrationContext.useObject({
-        where: {
-            organization: {
-                id: organizationId,
-            },
-        },
-    }, {
-        fetchPolicy: 'network-only',
-    })
-
-    if (!canReadPayments) {
-        return (
-            <BasicEmptyListView>
-                <Typography.Title level={3}>
-                    {NoPermissionMessage}
-                </Typography.Title>
-            </BasicEmptyListView>
-        )
-    }
-
-    if (integrationsLoading || contextLoading) {
-        return (
-            <Loader fill size={'large'}/>
-        )
-    }
-
-    if (integrationsError || contextError) {
-        return (
-            <BasicEmptyListView>
-                <Typography.Title level={3}>
-                    {integrationsError ? integrationsError : contextError}
-                </Typography.Title>
-            </BasicEmptyListView>
-        )
-    }
-    return (
-        <>
-            <Space direction={'vertical'} size={40} style={{ width: '100%' }}>
-                <Col span={24}>
-                    <Row gutter={[44, 44]}>
-                        {
-                            integrations.map((integration) => {
-                                const isActiveIntegration = !!currentContext && integration.id === currentContext.integration.id
-                                let status: CardStatuses = 'disabled'
-                                if (isActiveIntegration) {
-                                    status = 'done'
-                                } else if (!currentContext) {
-                                    status = 'available'
-                                }
-
-                                return (
-                                    <Col
-                                        span={12}
-                                        key={integration.id}
-                                        style={{
-                                            order: isActiveIntegration ? -1 : 'unset',
-                                        }}
-                                    >
-                                        <IntegrationPanel
-                                            integrationId={integration.id}
-                                            title={integration.name}
-                                            shortDescription={get(integration, 'shortDescription')}
-                                            status={status}
-                                        />
-                                    </Col>
-                                )
-                            })
-                        }
-                    </Row>
-                </Col>
-            </Space>
-        </>
-    )
+    return <IntegrationChooser
+        integrationModel={AcquiringIntegration}
+        integrationContextModel={AcquiringIntegrationContext}
+        integrationMessages={{
+            'oneIntegrationWarningMessage': OneAcquiringWarningMessage,
+        }}
+    />
 }
+
+
+//
+// export const AcquiringChooser: React.FC = () => {
+//     const intl = useIntl()
+//
+//     const NoPermissionMessage = intl.formatMessage({ id: 'NoPermissionToSettings' })
+//
+//     const userOrganization = useOrganization()
+//     const organizationId = get(userOrganization, ['organization', 'id'])
+//     const canReadPayments = get(userOrganization, ['link', 'role', 'canReadPayments'], false)
+//     const {
+//         objs: integrations,
+//         loading: integrationsLoading,
+//         error: integrationsError,
+//     } = AcquiringIntegration.useObjects({
+//         where: {
+//             isHidden: false,
+//         },
+//     })
+//
+//     const {
+//         obj: currentContext,
+//         error: contextError,
+//         loading: contextLoading,
+//     } = AcquiringIntegrationContext.useObject({
+//         where: {
+//             organization: {
+//                 id: organizationId,
+//             },
+//         },
+//     }, {
+//         fetchPolicy: 'network-only',
+//     })
+//
+//     if (!canReadPayments) {
+//         return (
+//             <BasicEmptyListView>
+//                 <Typography.Title level={3}>
+//                     {NoPermissionMessage}
+//                 </Typography.Title>
+//             </BasicEmptyListView>
+//         )
+//     }
+//
+//     if (integrationsLoading || contextLoading) {
+//         return (
+//             <Loader fill size={'large'}/>
+//         )
+//     }
+//
+//     if (integrationsError || contextError) {
+//         return (
+//             <BasicEmptyListView>
+//                 <Typography.Title level={3}>
+//                     {integrationsError ? integrationsError : contextError}
+//                 </Typography.Title>
+//             </BasicEmptyListView>
+//         )
+//     }
+//     return (
+//         <>
+//             <Space direction={'vertical'} size={40} style={{ width: '100%' }}>
+//                 <Col span={24}>
+//                     <Row gutter={[44, 44]}>
+//                         {
+//                             integrations.map((integration) => {
+//                                 const isActiveIntegration = !!currentContext && integration.id === currentContext.integration.id
+//                                 let status: CardStatuses = 'disabled'
+//                                 if (isActiveIntegration) {
+//                                     status = 'done'
+//                                 } else if (!currentContext) {
+//                                     status = 'available'
+//                                 }
+//
+//                                 return (
+//                                     <Col
+//                                         span={12}
+//                                         key={integration.id}
+//                                         style={{
+//                                             order: isActiveIntegration ? -1 : 'unset',
+//                                         }}
+//                                     >
+//                                         <IntegrationPanel
+//                                             integrationId={integration.id}
+//                                             title={integration.name}
+//                                             shortDescription={get(integration, 'shortDescription')}
+//                                             status={status}
+//                                         />
+//                                     </Col>
+//                                 )
+//                             })
+//                         }
+//                     </Row>
+//                 </Col>
+//             </Space>
+//         </>
+//     )
+// }
