f94870c363322018b5a272d9cb1895b0c825d10f
Pavel <nomerdvadcatpyat@gmail.com>
2022-02-10 13:22:12 +0500

    DOMA-1564 updated upload icon and size

--- apps/condo/domains/common/components/MultipleFileUpload.tsx
+++ apps/condo/domains/common/components/MultipleFileUpload.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useReducer, useMemo, useRef } from 'react'
 import { Upload } from 'antd'
 import { Button } from '@condo/domains/common/components/Button'
 import { useIntl } from '@core/next/intl'
-import { UploadOutlined, DeleteFilled } from '@ant-design/icons'
+import { EditOutlined, DeleteFilled } from '@ant-design/icons'
 
 import { MAX_UPLOAD_FILE_SIZE } from '@condo/domains/common/constants/uploads'
 
@@ -205,10 +205,10 @@ const MultipleFileUpload: React.FC<IMultipleFileUploadProps> = (props) => {
         <div className={'upload-control-wrapper'}>
             <Upload { ...options }  >
                 <Button
-                    type={'sberGrey'}
+                    type={'sberDefaultGradient'}
                     secondary
-                    icon={<UploadOutlined />}
-                    size={'middle'}
+                    icon={<EditOutlined />}
+                    size={'large'}
                 >{AddFileLabel}</Button>
             </Upload>
         </div>
