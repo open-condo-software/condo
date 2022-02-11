681d9171fc07d368d5fca0a6fc5da456d1826078
Pavel <nomerdvadcatpyat@gmail.com>
2022-02-11 12:56:34 +0500

    DOMA-1564 reverted UnitName input

--- apps/condo/domains/user/components/UnitNameInput.tsx
+++ apps/condo/domains/user/components/UnitNameInput.tsx
@@ -1,10 +1,7 @@
 import { Select, Input, SelectProps } from 'antd'
-import React from 'react'
+import React, { useMemo } from 'react'
 import get from 'lodash/get'
 import flattenDeep from 'lodash/flattenDeep'
-import isEmpty from 'lodash/isEmpty'
-import { useIntl } from '@core/next/intl'
-
 import { IPropertyUIState } from '@condo/domains/property/utils/clientSchema/Property'
 
 export interface IUnitNameInputProps extends Pick<SelectProps<string>, 'onChange' | 'onSelect'> {
@@ -14,39 +11,28 @@ export interface IUnitNameInputProps extends Pick<SelectProps<string>, 'onChange
     loading: boolean
 }
 
-// TODO(Dimitreee): move search to serverside
-const getOptionGroupBySectionType = (groupLabel, sections) => {
-    if (!sections) return
-
-    const unflattenUnits = sections.map((section) => {
-        const floors = get(section, ['floors'], [])
-
-        return floors.map((floor) => floor.units).reverse()
-    })
-
-    const flattenUnits: Array<{ id: string, label }> = flattenDeep(unflattenUnits)
-
-    const options = flattenUnits.map(
-        (unit) => (
-            <Select.Option key={unit.label} value={unit.label} title={String(unit.label)}>{unit.label}</Select.Option>
-        )
-    )
-
-    return !isEmpty(options) && (
-        <Select.OptGroup label={groupLabel}>
-            {options}
-        </Select.OptGroup>
-    )
-}
-
 export const BaseUnitNameInput: React.FC<IUnitNameInputProps> = (props) => {
-    const intl = useIntl()
-    const UnitsLabel = intl.formatMessage({ id: 'pages.condo.ticket.select.group.Units' })
-    const ParkingLabel = intl.formatMessage({ id: 'pages.condo.ticket.select.group.Parking' })
-
     const { placeholder, property, loading, ...restInputProps } = props
-    const sections = get(property, ['map', 'sections'], [])
-    const parking = get(property, ['map', 'parking'], [])
+
+    // TODO(Dimitreee): move search to serverside
+    const options = useMemo(() => {
+        const sections = get(property, ['map', 'sections'], [])
+
+        const unflattenUnits = sections.map((section) => {
+            const floors = get(section, ['floors'], [])
+
+            return floors.map((floor) => floor.units).reverse()
+        })
+
+        const flattenUnits: Array<{ id: string, label }> = flattenDeep(unflattenUnits)
+
+        return flattenUnits.map(
+            (unit) => (
+                <Select.Option key={unit.label} value={unit.label} title={String(unit.label)}>{unit.label}</Select.Option>
+            )
+        )
+
+    }, [property])
 
     return (
         <Select
@@ -58,8 +44,7 @@ export const BaseUnitNameInput: React.FC<IUnitNameInputProps> = (props) => {
             disabled={loading}
             {...restInputProps}
         >
-            {getOptionGroupBySectionType(UnitsLabel, sections)}
-            {getOptionGroupBySectionType(ParkingLabel, parking)}
+            {options}
         </Select>
     )
 }
