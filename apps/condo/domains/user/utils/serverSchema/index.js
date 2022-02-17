905fc3c265bb4ef1794964fa4a91d67574cc78d1
toplenboren <toplenboren@gmail.com>
2022-02-17 21:44:38 +0500

    DOMA-2135 Remove // TODO(codegen): write resetUser serverSchema guards

--- apps/condo/domains/user/utils/serverSchema/index.js
+++ apps/condo/domains/user/utils/serverSchema/index.js
@@ -55,7 +55,6 @@ async function resetUser (context, data) {
     if (!context) throw new Error('no context')
     if (!data) throw new Error('no data')
     if (!data.sender) throw new Error('no data.sender')
-    // TODO(codegen): write resetUser serverSchema guards
 
     return await execGqlWithoutAccess(context, {
         query: RESET_USER_MUTATION,
