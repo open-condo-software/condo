ce4969334542516953a790030df93534bff59f47
toplenboren <toplenboren@gmail.com>
2022-02-25 06:11:17 +0500

    DOMA-2137 Add CRUD.update tests
    (cherry picked from commit 96c001fb7535b8ff3166e3254dbbbbca830a4299)

--- apps/condo/domains/billing/schema/BillingRecipient.test.js
+++ apps/condo/domains/billing/schema/BillingRecipient.test.js
@@ -5,11 +5,13 @@
 const { makeClient, makeLoggedInAdminClient } = require('@core/keystone/test.utils')
 
 const { createTestOrganization } = require('@condo/domains/organization/utils/testSchema')
-const { makeClientWithSupportUser } = require('@condo/domains/user/utils/testSchema')
+const {
+    makeClientWithSupportUser,
+    makeClientWithNewRegisteredAndLoggedInUser,
+} = require('@condo/domains/user/utils/testSchema')
 const {
     expectToThrowAuthenticationErrorToObj,
     expectToThrowAccessDeniedErrorToObj,
-    makeClientWithNewRegisteredAndLoggedInUser,
 } = require('@condo/domains/common/utils/testSchema')
 
 const {
@@ -79,6 +81,15 @@ describe('BillingRecipient', () => {
             expect(obj.meta).toBeDefined()
         })
 
+        test('organization integration manager can\'t create BillingRecipient', async () => {
+            const { managerUserClient, integration, organization } = await makeOrganizationIntegrationManager()
+            const [context] = await createTestBillingIntegrationOrganizationContext(managerUserClient, organization, integration)
+
+            await expectToThrowAccessDeniedErrorToObj(async () => {
+                await createTestBillingRecipient(managerUserClient, context)
+            })
+        })
+
         test('user can\'t create BillingRecipient', async () => {
             const user = await makeClientWithNewRegisteredAndLoggedInUser()
 
@@ -103,7 +114,7 @@ describe('BillingRecipient', () => {
     describe('Read', () => {
         test('admin can read BillingRecipient', async () => {
             const admin = await makeLoggedInAdminClient()
-            const { organization, context } = await makeContextWithOrganizationAndIntegrationAsAdmin()
+            const { context } = await makeContextWithOrganizationAndIntegrationAsAdmin()
 
             const [ createdObj ] = await createTestBillingRecipient(admin, context)
             const [ readObj ] = await BillingRecipient.getAll(admin, { id: createdObj.id })
@@ -119,7 +130,7 @@ describe('BillingRecipient', () => {
 
         test('support can read BillingRecipient', async () => {
             const support = await makeClientWithSupportUser()
-            const { organization, context } = await makeContextWithOrganizationAndIntegrationAsAdmin()
+            const { context } = await makeContextWithOrganizationAndIntegrationAsAdmin()
 
             const [ createdObj ] = await createTestBillingRecipient(support, context)
             const [ readObj ] = await BillingRecipient.getAll(support, { id: createdObj.id })
@@ -193,6 +204,97 @@ describe('BillingRecipient', () => {
             const readObjects = await BillingRecipient.getAll(user)
 
             expect(readObjects).toHaveLength(0)
+        })
+    })
+
+    describe('Update', () => {
+        test('admin can update BillingRecipient', async () => {
+            const admin = await makeLoggedInAdminClient()
+            const { context } = await makeContextWithOrganizationAndIntegrationAsAdmin()
+
+            const [ createdObj ] = await createTestBillingRecipient(admin, context)
+            const [ updatedObj ] = await updateTestBillingRecipient(admin, createdObj.id, { isApproved: true })
+
+            expect(createdObj.id).toEqual(updatedObj.id)
+            expect(createdObj.isApproved).toEqual(false)
+            expect(updatedObj.isApproved).toEqual(true)
+        })
+
+        test('support can update BillingRecipient', async () => {
+            const support = await makeClientWithSupportUser()
+            const { context } = await makeContextWithOrganizationAndIntegrationAsAdmin()
+
+            const [ createdObj ] = await createTestBillingRecipient(support, context)
+            const [ updatedObj ] = await updateTestBillingRecipient(support, createdObj.id, { isApproved: true })
+
+            expect(createdObj.id).toEqual(updatedObj.id)
+            expect(createdObj.isApproved).toEqual(false)
+            expect(updatedObj.isApproved).toEqual(true)
+        })
+
+        test('integration service account can update BillingRecipient', async () => {
+            const admin = await makeLoggedInAdminClient()
+            const integrationClient = await makeClientWithIntegrationAccess()
+
+            const [ organization ] = await createTestOrganization(admin)
+            const [ context ] = await createTestBillingIntegrationOrganizationContext(admin, organization, integrationClient.integration)
+
+            const [ createdObj ] = await createTestBillingRecipient(integrationClient, context)
+            const [ updatedObj ] = await updateTestBillingRecipient(integrationClient, createdObj.id, { isApproved: true })
+
+            expect(createdObj.id).toEqual(updatedObj.id)
+            expect(createdObj.isApproved).toEqual(false)
+            expect(updatedObj.isApproved).toEqual(true)
+        })
+
+        test('organization integration manager can\'t update BillingRecipient', async () => {
+
+            const admin = await makeLoggedInAdminClient()
+            const { managerUserClient, integration, organization } = await makeOrganizationIntegrationManager()
+            const [context] = await createTestBillingIntegrationOrganizationContext(managerUserClient, organization, integration)
+
+            const [ createdObj ] = await createTestBillingRecipient(admin, context)
+
+            await expectToThrowAccessDeniedErrorToObj(async () => {await updateTestBillingRecipient(managerUserClient,
+                createdObj.id, { isApproved: true })
+            })
+        })
+
+        test('user can\'t update BillingRecipients', async () => {
+            const adminClient = await makeLoggedInAdminClient()
+            const user = await makeClientWithNewRegisteredAndLoggedInUser()
+            const { context } = await makeContextWithOrganizationAndIntegrationAsAdmin()
+
+            const [ createdObj ] = await createTestBillingRecipient(adminClient, context)
+
+            await expectToThrowAccessDeniedErrorToObj(async () => {await updateTestBillingRecipient(user,
+                createdObj.id, { isApproved: true })
+            })
+        })
+
+        test('anonymous can\'t update BillingRecipients', async () => {
+            const adminClient = await makeLoggedInAdminClient()
+            const user = await makeClientWithNewRegisteredAndLoggedInUser()
+            const { context } = await makeContextWithOrganizationAndIntegrationAsAdmin()
+
+            const [ createdObj ] = await createTestBillingRecipient(adminClient, context)
+
+            await expectToThrowAccessDeniedErrorToObj(async () => {await updateTestBillingRecipient(user,
+                createdObj.id, { isApproved: true })
+            })
+        })
+    })
+
+    describe('Delete', () => {
+        test('admin can\'t delete BillingRecipients', async () => {
+            const adminClient = await makeLoggedInAdminClient()
+            const { context } = await makeContextWithOrganizationAndIntegrationAsAdmin()
+
+            const [ createdObj ] = await createTestBillingRecipient(adminClient, context)
+
+            await expectToThrowAccessDeniedErrorToObj(async () => {
+                await BillingRecipient.delete(adminClient, createdObj.id)
+            })
         })
     })
 })
