613b420cdb623f36176c36abf8334acb23d604b2
AleX83Xpert <AleX83Xpert@mail.ru>
2022-02-25 12:54:44 +0500

    DOMA-1210 NoBillingStub was added

---
+++ apps/condo/domains/billing/components/payments/NoBillingStub.tsx
@@ -0,0 +1,20 @@
+import EmptyListView from '@condo/domains/common/components/EmptyListView'
+import React from 'react'
+import { useIntl } from '@core/next/intl'
+
+export const NoBillingStub = (): JSX.Element => {
+    const intl = useIntl()
+
+    const noBillingLabel = intl.formatMessage({ id: 'pages.condo.payments.noBillingLabel' })
+    const noBillingMessage = intl.formatMessage({ id: 'pages.condo.payments.noBillingMessage' })
+    const createBillingLabel = intl.formatMessage({ id: 'pages.condo.payments.createBillingLabel' })
+
+    return (
+        <EmptyListView
+            label={noBillingLabel}
+            message={noBillingMessage}
+            createRoute="/settings"
+            createLabel={createBillingLabel}
+        />
+    )
+}
