4361041f81cd751f88d6a8e8ddd36cbe122907dd
Dmitry Koviazin <dkovyazin@doma.ai>
2022-03-14 22:41:28 +0500

    DOMA-1722 add error handler to SBBOL

---
+++ apps/condo/domains/common/utils/expressErrorHandler.js
@@ -0,0 +1,16 @@
+const cuid = require('cuid')
+const { get } = require('lodash')
+const { serializeError } = require('serialize-error')
+
+const { graphqlLogger } = require('@keystonejs/keystone/lib/Keystone/logger')
+
+const expressErrorHandler = (err, req, res, next) => {
+    const errId = cuid()
+    const reqId = get(req, ['id'], get(req, ['headers', 'X-Request-Id']))
+    graphqlLogger.error({ reason: 'expressErrorHandler', error: serializeError(err), errId, reqId })
+    return res.status(500).send(`Error! errId=${errId}; reqId=${reqId}`)
+}
+
+module.exports = {
+    expressErrorHandler,
+}
--- apps/condo/index.js
+++ apps/condo/index.js
@@ -20,6 +20,7 @@ const { hasValidJsonStructure } = require('@condo/domains/common/utils/validatio
 const { SbbolRoutes } = require('@condo/domains/organization/integrations/sbbol/routes')
 const FileAdapter = require('@condo/domains/common/utils/fileAdapter')
 const { OIDCMiddleware } = require('@condo/domains/user/oidc')
+const { expressErrorHandler } = require('@condo/domains/common/utils/expressErrorHandler')
 const { schemaDocPreprocessor } = require('@core/keystone/preprocessors/schemaDoc')
 
 const IS_ENABLE_DD_TRACE = conf.NODE_ENV === 'production'
@@ -111,6 +112,7 @@ class SberBuisnessOnlineMiddleware {
         // TODO(zuch): find a way to remove bind
         app.get('/api/sbbol/auth', Auth.startAuth.bind(Auth))
         app.get('/api/sbbol/auth/callback', Auth.completeAuth.bind(Auth))
+        app.use(expressErrorHandler)
         return app
     }
 }
