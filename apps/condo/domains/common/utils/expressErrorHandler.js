669457643da5df3bc93b50e26c44d4aeba8caa2e
Pahaz <pahaz.white@gmail.com>
2022-03-14 19:35:26 +0300

    HOTFIX fix infinity loop

--- apps/condo/domains/common/utils/expressErrorHandler.js
+++ apps/condo/domains/common/utils/expressErrorHandler.js
@@ -7,7 +7,7 @@ const { graphqlLogger } = require('@keystonejs/keystone/lib/Keystone/logger')
 const expressErrorHandler = (err, req, res, next) => {
     const errId = cuid()
     const reqId = get(req, ['id'], get(req, ['headers', 'X-Request-Id']))
-    graphqlLogger.error({ reason: 'expressErrorHandler', error: serializeError(err), errId, reqId, req })
+    graphqlLogger.error({ reason: 'expressErrorHandler', error: serializeError(err), errId, reqId })
     return res.status(500).send(`Error! errId=${errId}; reqId=${reqId}`)
 }
 
