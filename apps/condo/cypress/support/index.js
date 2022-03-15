d06cb6a8a8beccededfa60c85dc9d00b4d7d6b02
Sitnikov Ivan <sit7602@gmail.com>
2022-03-15 12:25:18 +0500

    DOMA-2244 move cypress dependency to app related package

--- apps/condo/cypress/support/index.js
+++ apps/condo/cypress/support/index.js
@@ -1,61 +1,20 @@
 import './commands'
 
-let commands = []
-let testAttributes
-
-Cypress.on('test:after:run', (attributes) => {
-    console.log('Test "%s" has finished in %dms', attributes.title, attributes.duration)
-    console.table(commands)
-    testAttributes = {
-        title: attributes.title,
-        duration: attributes.duration,
-        commands: Cypress._.cloneDeep(commands),
-    }
-    commands.length = 0
-
-})
-
-Cypress.on('command:start', (c) => {
-    commands.push({
-        name: c.attributes.name,
-        started: +new Date(),
-    })
-})
-
-Cypress.on('command:end', (c) => {
-    const lastCommand = commands[commands.length - 1]
-
-    if (lastCommand.name !== c.attributes.name) {
-        throw new Error('Last command is wrong')
-    }
-
-    lastCommand.endedAt = +new Date()
-    lastCommand.elapsed = lastCommand.endedAt - lastCommand.started
-})
-
 Cypress.on('uncaught:exception', (err) => {
     if (err.message.toLowerCase().includes('resizeobserver loop limit exceeded')) {
         return false
     }
 })
 
-const sendTestTimings = () => {
+const prepareTestEnvironment = () => {
     cy.viewport('macbook-13')
+
     cy.intercept('POST', '/admin/api', (req) =>
         new Promise((resolve, reject) => {
             req.alias = req.body.operationName
             resolve()
         })
     )
-    if (!testAttributes) {
-        return
-    }
-
-    const attr = testAttributes
-    testAttributes = null
-    cy.task('testTimings', attr)
 }
 
-beforeEach(sendTestTimings)
-
-after(sendTestTimings)
+beforeEach(prepareTestEnvironment)
