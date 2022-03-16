cf301a01a39fb0ef626c7f03981773c0a3d7503d
Pavel <nomerdvadcatpyat@gmail.com>
2022-03-16 13:01:18 +0500

    DOMA-2436 added cron task to close completed tickets

--- apps/condo/domains/ticket/tasks.js
+++ apps/condo/domains/ticket/tasks.js
@@ -1,6 +1,8 @@
 const { find, getSchemaCtx } = require('@core/keystone/schema')
-const { createTask } = require('@core/keystone/tasks')
+const { createTask, createCronTask } = require('@core/keystone/tasks')
 const { Ticket } = require('@condo/domains/ticket/utils/serverSchema')
+const dayjs = require('dayjs')
+const { STATUS_IDS } = require('@condo/domains/ticket/constants/statusTransitions')
 
 /**
  * Launched when address of some property was changed to sync propertyAddress field of all linked tickets
@@ -25,6 +27,27 @@ async function manageTicketPropertyAddressChange (propertyId, userInfo) {
     }
 }
 
+/**
+ * Closes tickets that are in the "completed" status for 7 days
+ */
+const closeCompletedTickets = createCronTask('closeCompletedTickets', '0 1 * * *', async () => {
+    const today = dayjs().format('YYYY-MM-DD').toISOString()
+    const {  keystone: context } = await getSchemaCtx('User')
+
+    const ticketsToChange = await find('Ticket', {
+        status: { id: STATUS_IDS.COMPLETED },
+        statusUpdatedAt_lte: today,
+        deletedAt: null,
+    })
+
+    for (const ticket of ticketsToChange) {
+        await Ticket.update(context, ticket.id, {
+            status: { connect: { id: STATUS_IDS.CLOSED } },
+        })
+    }
+})
+
 module.exports = {
     manageTicketPropertyAddressChange: createTask('manageTicketPropertyAddressChange', manageTicketPropertyAddressChange),
+    closeCompletedTickets,
 }
