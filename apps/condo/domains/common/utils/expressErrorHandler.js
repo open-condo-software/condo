68d6c3909a895f226f6d2ef5b05c4ed9ba77b660
Anton Aleshin <Anton_Aleschin@mail.ru>
2022-03-16 18:50:49 +0300

    DOMA-2425 added `originalError` to `expressErrorHandler`
    `serialize-error` will pick only following properties: `message`, `locations`, `path`, `name`, `stack`
    It's not enough, because critical information from GraphQL mutations is provided in `originalError` property

--- apps/condo/domains/common/utils/expressErrorHandler.js
+++ apps/condo/domains/common/utils/expressErrorHandler.js
@@ -1,13 +1,19 @@
 const cuid = require('cuid')
 const { get } = require('lodash')
 const { serializeError } = require('serialize-error')
-
 const { graphqlLogger } = require('@keystonejs/keystone/lib/Keystone/logger')
 
 const expressErrorHandler = (err, req, res, next) => {
+    if (!err) next()
     const errId = cuid()
     const reqId = get(req, ['id'], get(req, ['headers', 'X-Request-Id']))
-    graphqlLogger.error({ reason: 'expressErrorHandler', error: serializeError(err), errId, reqId })
+    // `serialize-error` will pick only following properties: `message`, `locations`, `path`, `name`, `stack`
+    const graphQLError = { reason: 'expressErrorHandler', error: serializeError(err), errId, reqId }
+    // It's not enough, because critical information from GraphQL mutations is provided in `originalError` property
+    if (err.originalError) {
+        graphQLError.originalError = serializeError(err.originalError)
+    }
+    graphqlLogger.error(graphQLError)
     return res.status(500).send(`Error! errId=${errId}; reqId=${reqId}`)
 }
 
