13c75b92fd1dcf63dc5511cafa3a88b82a060ec5
Pavel <nomerdvadcatpyat@gmail.com>
2022-03-18 15:14:25 +0500

    DOMA-2436 added sender info to closeCompletedTickets task

--- apps/condo/domains/ticket/utils/serverSchema/closeCompletedTickets.js
+++ apps/condo/domains/ticket/utils/serverSchema/closeCompletedTickets.js
@@ -4,17 +4,19 @@ const { STATUS_IDS } = require('@condo/domains/ticket/constants/statusTransition
 const { Ticket } = require('@condo/domains/ticket/utils/serverSchema')
 
 async function closeCompletedTickets (context) {
-    const today = dayjs().startOf('day').toISOString()
+    const weekAgo = dayjs().startOf('day').subtract('7', 'day').toISOString()
 
     const ticketsToChange = await find('Ticket', {
         status: { id: STATUS_IDS.COMPLETED },
-        statusUpdatedAt_lte: today,
+        statusUpdatedAt_lte: weekAgo,
         deletedAt: null,
     })
 
     for (const ticket of ticketsToChange) {
         await Ticket.update(context, ticket.id, {
             status: { connect: { id: STATUS_IDS.CLOSED } },
+            dv: 1,
+            sender: { fingerprint: 'auto-close', dv: 1 },
         })
     }
 }
