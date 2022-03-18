9fda6bb0ad334f377bc823cf4eff2133aeee86e8
Matthew Savelev <savelevmatthew@gmail.com>
2022-03-18 18:52:27 +0500

    DOMA-2419 Top card layout added

--- apps/condo/domains/miniapp/components/AppDescription/AppDescriptionPageContent.tsx
+++ apps/condo/domains/miniapp/components/AppDescription/AppDescriptionPageContent.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import { TopCard } from './TopCard'
+import dayjs from 'dayjs'
 
 export const AppDescriptionPageContent: React.FC = () => {
     return (
         <>
-            <TopCard/>
+            <TopCard
+                developer={'SBERlonglonglonglonglonglonglong'}
+                published={dayjs().toISOString()}
+                // description={'123'}
+                description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci autem blanditiis consectetur ea eaque ipsum, magnam minus, modi molestias nisi nobis numquam perferendis quos recusandae ut velit voluptas voluptatem! Quos.'}
+                // title={'asd'}
+                title={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci autem blanditiis consectetur ea eaque ipsum, magnam minus, modi molestias nisi nobis numquam perferendis quos recusandae ut velit voluptas voluptatem! Quos.'}
+                setupUrl={'https://spotify.com'}
+                partnerUrl={'https://spotify.com'}
+            />
         </>
     )
 }
