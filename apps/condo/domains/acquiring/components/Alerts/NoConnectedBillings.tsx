3ccabce47430ac2ed4e3798c5dfa56856685f855
Matthew Savelev <savelevmatthew@gmail.com>
2022-03-28 19:18:48 +0500

    DOMA-2420 Acquiring about page added

---
+++ apps/condo/domains/acquiring/components/Alerts/NoConnectedBillings.tsx
@@ -0,0 +1,42 @@
+import React, { CSSProperties } from 'react'
+import { Alert, Space, Typography } from 'antd'
+import { useIntl } from '@core/next/intl'
+import Link from 'next/link'
+
+const ALERT_BOX_STYLE: CSSProperties = {
+    width: 'fit-content',
+}
+
+const AlertBody: React.FC = () => {
+    const intl = useIntl()
+    const AlertMessage = intl.formatMessage({ id: 'services.acquiring.NoBillings.message' })
+    const AlertLinkMessage = intl.formatMessage({ id: 'services.acquiring.NoBillings.link' })
+
+    return (
+        <Space direction={'vertical'} size={12}>
+            <Typography.Text>
+                {AlertMessage}
+            </Typography.Text>
+            <Link href={'/services'}>
+                <Typography.Link>
+                    {AlertLinkMessage}
+                </Typography.Link>
+            </Link>
+        </Space>
+    )
+}
+
+export const NoConnectedBillings: React.FC = () => {
+    const intl = useIntl()
+    const AlertTitle = intl.formatMessage({ id: 'services.acquiring.NoBillings.title' })
+
+    return (
+        <Alert
+            message={<Typography.Text strong>{AlertTitle}</Typography.Text>}
+            description={<AlertBody/>}
+            type={'warning'}
+            showIcon
+            style={ALERT_BOX_STYLE}
+        />
+    )
+}
