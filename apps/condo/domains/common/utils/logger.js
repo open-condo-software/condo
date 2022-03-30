f0c49633354b85685d141e9592aef4cf0f407652
Dmitry Koviazin <dkoviazin@gmail.com>
2022-03-30 14:09:11 +0500

    DOMA-2599 remove beautiful colors from logger

--- apps/condo/domains/common/utils/logger.js
+++ apps/condo/domains/common/utils/logger.js
@@ -4,17 +4,13 @@ const appLogger = pino({ name: process.env.LOGGER_NAME || 'condo', enabled: fals
 
 class Logger {
 
-    colors = { debug: '\x1b[32m', info: '\x1b[0m', error: '\x1b[31m', reset: '\x1b[0m' }
-
     constructor (name, level = 'debug') {
         this.logger = appLogger.child({ module: name })
         this.logger.level = level
     }
 
     printLog (message, params = {}, type) {
-        process.stdout.write(this.colors[type])
         this.logger[type]({ message, ...params })
-        process.stdout.write(this.colors.reset)
     }
 
     debug (message, params = {}) {
