b9806820e69721887823bb6baf29c5dc9f31e26e
Matthew Savelev <savelevmatthew@gmail.com>
2022-03-31 15:03:14 +0500

    DOMA-2420 Added tabs with full logic and query support

--- apps/condo/domains/miniapp/components/AppSelector/AppSelectPageContent.tsx
+++ apps/condo/domains/miniapp/components/AppSelector/AppSelectPageContent.tsx
@@ -1,5 +1,5 @@
-import React, { CSSProperties } from 'react'
-import { Typography, Row, Col } from 'antd'
+import React, { CSSProperties, useMemo } from 'react'
+import { Typography, Row, Col, Tabs } from 'antd'
 import { useIntl } from '@core/next/intl'
 import { PageHeader } from '@condo/domains/common/components/containers/BaseLayout'
 import Carousel from '@condo/domains/common/components/Carousel'
@@ -13,6 +13,8 @@ import { ALL_ORGANIZATION_APPS_QUERY } from '@condo/domains/miniapp/gql.js'
 import { get } from 'lodash'
 import { useOrganization } from '@core/next/organization'
 import LoadingOrErrorPage from '@condo/domains/common/components/containers/LoadingOrErrorPage'
+import { BILLING_APP_TYPE, ACQUIRING_APP_TYPE } from '@condo/domains/miniapp/constants'
+import { useRouter } from 'next/router'
 
 
 const WINDOW_MEDIUM_SELECT_CARD_WIDTH = 870
@@ -29,12 +31,15 @@ export const AppSelectPageContent: React.FC = () => {
     const PageTitle = intl.formatMessage({ id: 'menu.Services' })
     const AlreadyConnectedSectionTitle = intl.formatMessage({ id: 'services.AlreadyConnected' })
     const AvailableSectionTitle = intl.formatMessage({ id: 'services.Available' })
+    const AllCategoryMessage = intl.formatMessage({ id: 'services.category.ALL' })
+    const BillingCategoryMessage = intl.formatMessage({ id: 'services.category.BILLING' })
+    const AcquiringCategoryMessage = intl.formatMessage({ id: 'services.category.ACQUIRING' })
 
     const { width } = useWindowSize()
 
-    let servicesPerRow: CardsPerRowType = 3
-    if (width && width < WINDOW_SMALL_SELECT_CARD_WIDTH) servicesPerRow = 1
-    else if (width && width < WINDOW_MEDIUM_SELECT_CARD_WIDTH) servicesPerRow = 2
+    let appsPerRow: CardsPerRowType = 3
+    if (width && width < WINDOW_SMALL_SELECT_CARD_WIDTH) appsPerRow = 1
+    else if (width && width < WINDOW_MEDIUM_SELECT_CARD_WIDTH) appsPerRow = 2
 
     let slidesToShow = 4
     if (width && width < WINDOW_SMALLEST_CAROUSEL_CARD_WIDTH) slidesToShow = 1
@@ -43,6 +48,7 @@ export const AppSelectPageContent: React.FC = () => {
 
     const userOrganization = useOrganization()
     const userOrganizationId = get(userOrganization, ['organization', 'id'])
+
     const { loading, error, data } = useQuery(ALL_ORGANIZATION_APPS_QUERY, {
         fetchPolicy: 'cache-and-network',
         variables: {
@@ -54,33 +60,57 @@ export const AppSelectPageContent: React.FC = () => {
         },
     })
 
+    const router = useRouter()
+
+    const apps = get(data, 'objs', [])
+    const connectedApps = apps.filter(app => app.connected)
+    const unconnectedApps = apps.filter(app => !app.connected)
+    const unconnectedBillingsApps = unconnectedApps.filter(app => app.type === BILLING_APP_TYPE)
+    const unconnectedAcquiringApps = unconnectedApps.filter(app => app.type === ACQUIRING_APP_TYPE)
+
+    const isAnyAppConnected = Boolean(connectedApps.length)
+    const isAnyAppAvailable = Boolean(unconnectedApps.length)
+    const isAnyBillingAvailable = Boolean(unconnectedBillingsApps.length)
+    const isAnyAcquiringAvailable = Boolean(unconnectedAcquiringApps.length)
+
+    const availableTabs = useMemo(() => {
+        const tabs = []
+        if (isAnyAppAvailable) tabs.push('all')
+        if (isAnyBillingAvailable) tabs.push('billing')
+        if (isAnyAcquiringAvailable) tabs.push('acquiring')
+        return tabs
+    }, [isAnyAppAvailable, isAnyAcquiringAvailable, isAnyBillingAvailable])
+
+    const { query: { tab } } = router
+    const defaultTab = (!Array.isArray(tab) && availableTabs.includes(tab)) ? tab : undefined
+    const handleTabChange = (newKey) => {
+        const newRoute = `${router.route}?tab=${newKey}`
+        return router.push(newRoute)
+    }
+
+    const pageHeaderStyles: CSSProperties = { paddingBottom: isAnyAppConnected ? 60 : 44 }
+
     if (loading || error) {
         return <LoadingOrErrorPage title={LoadingMessage} error={error} loading={loading}/>
     }
 
-    const services = get(data, 'objs', [])
-    const connectedServices = services.filter(service => service.connected)
-    const unconnectedServices = services.filter(service => !service.connected)
-
-    const isAnyServiceConnected = Boolean(connectedServices.length)
-
     return (
         <>
-            <PageHeader title={<Typography.Title>{PageTitle}</Typography.Title>} spaced/>
+            <PageHeader title={<Typography.Title>{PageTitle}</Typography.Title>} style={pageHeaderStyles}/>
             <Row gutter={[0, 60]} style={PAGE_CONTENT_STYLES}>
                 {
-                    isAnyServiceConnected &&
+                    isAnyAppConnected &&
                     <Col span={24}>
                         <Section title={AlreadyConnectedSectionTitle}>
                             <Carousel slidesToShow={slidesToShow}>
                                 {
-                                    connectedServices.map(service => {
-                                        const logo = service.logo || undefined
-                                        const url = `/miniapps/${service.id}?type=${service.type}`
+                                    connectedApps.map(app => {
+                                        const logo = app.logo || undefined
+                                        const url = `/miniapps/${app.id}?type=${app.type}`
                                         return (
                                             <AppCarouselCard
-                                                key={service.name}
-                                                title={service.name}
+                                                key={app.name}
+                                                title={app.name}
                                                 logoSrc={logo}
                                                 url={url}
                                             />
@@ -91,31 +121,97 @@ export const AppSelectPageContent: React.FC = () => {
                         </Section>
                     </Col>
                 }
-                <Col span={24}>
-                    <Section title={AvailableSectionTitle} hideTitle={!isAnyServiceConnected}>
-                        <CardsContainer cardsPerRow={servicesPerRow}>
-                            {
-                                unconnectedServices.map(service => {
-                                    const tag = service.category
-                                        ? intl.formatMessage({ id: `services.category.${service.category}` })
-                                        : undefined
-                                    const url = `/miniapps/${service.id}/about?type=${service.type}`
-                                    const logo = service.logo || undefined
-                                    return (
-                                        <AppSelectCard
-                                            key={service.name}
-                                            title={service.name}
-                                            description={service.shortDescription}
-                                            url={url}
-                                            tag={tag}
-                                            logoSrc={logo}
-                                        />
-                                    )
-                                })
-                            }
-                        </CardsContainer>
-                    </Section>
-                </Col>
+                {
+                    isAnyAppAvailable && (
+                        <Col span={24}>
+                            <Section title={AvailableSectionTitle} hideTitle={!isAnyAppConnected}>
+                                <Tabs
+                                    defaultActiveKey={defaultTab}
+                                    activeKey={defaultTab}
+                                    onChange={handleTabChange}
+                                >
+                                    <Tabs.TabPane tab={AllCategoryMessage} key={'all'}>
+                                        <CardsContainer cardsPerRow={appsPerRow}>
+                                            {
+                                                unconnectedApps.map(app => {
+                                                    const tag = app.category
+                                                        ? intl.formatMessage({ id: `services.category.${app.category}` })
+                                                        : undefined
+                                                    const url = `/miniapps/${app.id}/about?type=${app.type}`
+                                                    const logo = app.logo || undefined
+                                                    return (
+                                                        <AppSelectCard
+                                                            key={app.name}
+                                                            title={app.name}
+                                                            description={app.shortDescription}
+                                                            url={url}
+                                                            tag={tag}
+                                                            logoSrc={logo}
+                                                        />
+                                                    )
+                                                })
+                                            }
+                                        </CardsContainer>
+                                    </Tabs.TabPane>
+                                    {
+                                        isAnyBillingAvailable && (
+                                            <Tabs.TabPane tab={BillingCategoryMessage} key={'billing'}>
+                                                <CardsContainer cardsPerRow={appsPerRow}>
+                                                    {
+                                                        unconnectedBillingsApps.map(app => {
+                                                            const tag = app.category
+                                                                ? intl.formatMessage({ id: `services.category.${app.category}` })
+                                                                : undefined
+                                                            const url = `/miniapps/${app.id}/about?type=${app.type}`
+                                                            const logo = app.logo || undefined
+                                                            return (
+                                                                <AppSelectCard
+                                                                    key={app.name}
+                                                                    title={app.name}
+                                                                    description={app.shortDescription}
+                                                                    url={url}
+                                                                    tag={tag}
+                                                                    logoSrc={logo}
+                                                                />
+                                                            )
+                                                        })
+                                                    }
+                                                </CardsContainer>
+                                            </Tabs.TabPane>
+                                        )
+                                    }
+                                    {
+                                        isAnyAcquiringAvailable && (
+                                            <Tabs.TabPane tab={AcquiringCategoryMessage} key={'acquiring'}>
+                                                <CardsContainer cardsPerRow={appsPerRow}>
+                                                    {
+                                                        unconnectedAcquiringApps.map(app => {
+                                                            const tag = app.category
+                                                                ? intl.formatMessage({ id: `services.category.${app.category}` })
+                                                                : undefined
+                                                            const url = `/miniapps/${app.id}/about?type=${app.type}`
+                                                            const logo = app.logo || undefined
+                                                            return (
+                                                                <AppSelectCard
+                                                                    key={app.name}
+                                                                    title={app.name}
+                                                                    description={app.shortDescription}
+                                                                    url={url}
+                                                                    tag={tag}
+                                                                    logoSrc={logo}
+                                                                />
+                                                            )
+                                                        })
+                                                    }
+                                                </CardsContainer>
+                                            </Tabs.TabPane>
+                                        )
+                                    }
+                                </Tabs>
+                            </Section>
+                        </Col>
+                    )
+                }
             </Row>
         </>
     )
