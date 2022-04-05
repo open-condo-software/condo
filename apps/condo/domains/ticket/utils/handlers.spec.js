ee15431942e7541d0e51664bc0fc7929cd0b3fbc
maksimdanilov <maxim13danilov@gmail.com>
2022-04-05 11:06:42 +0500

    DOMA-2434 add test new test for detectEventTypes

--- apps/condo/domains/ticket/utils/handlers.spec.js
+++ apps/condo/domains/ticket/utils/handlers.spec.js
@@ -2,7 +2,12 @@ import {
     detectEventTypes,
     ASSIGNEE_CONNECTED_EVENT_TYPE,
     EXECUTOR_CONNECTED_EVENT_TYPE,
+    STATUS_CHANGED_EVENT_TYPE,
+    WARRANTY_CHANGED_EVENT_TYPE,
+    PAID_CHANGED_EVENT_TYPE,
+    EMERGENCY_CHANGED_EVENT_TYPE,
 } from './handlers'
+const { STATUS_IDS } = require('../constants/statusTransitions')
 
 describe('Ticket request event detection', () => {
     it('correctly detects assignee connection on ticket create', () => {
@@ -99,6 +104,161 @@ describe('Ticket request event detection', () => {
         }
 
         expect(detectEventTypes(requestData)).toMatchObject({ [EXECUTOR_CONNECTED_EVENT_TYPE]: true })
+    })
+
+    it('correctly detects event on ticket create', () => {
+        const requestData = {
+            operation: 'create',
+            existingItem: null,
+            updatedItem: {
+                client: '289d6f08-14fe-48ea-8c69-99890839bd6d',
+            },
+        }
+
+        expect(detectEventTypes(requestData)).toMatchObject({ [STATUS_CHANGED_EVENT_TYPE]: false } )
+    })
+
+    it('correctly detects event connection on ticket create', () => {
+        const requestData = {
+            operation: 'create',
+            existingItem: null,
+            updatedItem: {
+                status: { connect: { id: STATUS_IDS.IN_PROGRESS } },
+                client: '289d6f08-14fe-48ea-8c69-99890839bd6d',
+            },
+        }
+
+        expect(detectEventTypes(requestData)).toMatchObject({ [STATUS_CHANGED_EVENT_TYPE]: true } )
+    })
+
+    it('correctly detects event change on ticket update', () => {
+        const requestData = {
+            operation: 'update',
+            existingItem: { status: { connect: { id: STATUS_IDS.OPEN } } },
+            updatedItem: {
+                status: { connect: { id: STATUS_IDS.IN_PROGRESS } },
+                client: '289d6f08-14fe-48ea-8c69-99890839bd6d',
+            },
+        }
+
+        expect(detectEventTypes(requestData)).toMatchObject({ [STATUS_CHANGED_EVENT_TYPE]: true } )
+    })
+
+    it('correctly detects warranty indicator connection on ticket create', () => {
+        const requestData = {
+            operation: 'create',
+            existingItem: null,
+            updatedItem: {
+                client: '289d6f08-14fe-48ea-8c69-99890839bd6d',
+                isWarranty: true,
+            },
+        }
+
+        expect(detectEventTypes(requestData)).toMatchObject({ [WARRANTY_CHANGED_EVENT_TYPE]: true } )
+    })
+
+    it('correctly detects turning off warranty indicator on ticket update', () => {
+        const requestData = {
+            operation: 'update',
+            existingItem: { isWarranty: true },
+            updatedItem: {
+                client: '289d6f08-14fe-48ea-8c69-99890839bd6d',
+                isWarranty: false,
+            },
+        }
+
+        expect(detectEventTypes(requestData)).toMatchObject({ [WARRANTY_CHANGED_EVENT_TYPE]: true } )
+    })
+
+    it('correctly detects turning on warranty indicator on ticket update', () => {
+        const requestData = {
+            operation: 'update',
+            existingItem: { isWarranty: false },
+            updatedItem: {
+                client: '289d6f08-14fe-48ea-8c69-99890839bd6d',
+                isWarranty: true,
+            },
+        }
+
+        expect(detectEventTypes(requestData)).toMatchObject({ [WARRANTY_CHANGED_EVENT_TYPE]: true } )
+    })
+
+    it('correctly detects paid indicator connection on ticket create', () => {
+        const requestData = {
+            operation: 'create',
+            existingItem: null,
+            updatedItem: {
+                client: '289d6f08-14fe-48ea-8c69-99890839bd6d',
+                isPaid: true,
+            },
+        }
+
+        expect(detectEventTypes(requestData)).toMatchObject({ [PAID_CHANGED_EVENT_TYPE]: true } )
+    })
+
+    it('correctly detects turning off paid indicator on ticket update', () => {
+        const requestData = {
+            operation: 'update',
+            existingItem: { isPaid: true },
+            updatedItem: {
+                client: '289d6f08-14fe-48ea-8c69-99890839bd6d',
+                isPaid: false,
+            },
+        }
+
+        expect(detectEventTypes(requestData)).toMatchObject({ [PAID_CHANGED_EVENT_TYPE]: true } )
+    })
+
+    it('correctly detects turning on paid indicator on ticket update', () => {
+        const requestData = {
+            operation: 'update',
+            existingItem: { isPaid: false },
+            updatedItem: {
+                client: '289d6f08-14fe-48ea-8c69-99890839bd6d',
+                isPaid: true,
+            },
+        }
+
+        expect(detectEventTypes(requestData)).toMatchObject({ [PAID_CHANGED_EVENT_TYPE]: true } )
+    })
+
+    it('correctly detects emergency indicator connection on ticket create', () => {
+        const requestData = {
+            operation: 'create',
+            existingItem: null,
+            updatedItem: {
+                client: '289d6f08-14fe-48ea-8c69-99890839bd6d',
+                isEmergency: true,
+            },
+        }
+
+        expect(detectEventTypes(requestData)).toMatchObject({ [EMERGENCY_CHANGED_EVENT_TYPE]: true } )
+    })
+
+    it('correctly detects turning off emergency indicator on ticket update', () => {
+        const requestData = {
+            operation: 'update',
+            existingItem: { isEmergency: true },
+            updatedItem: {
+                client: '289d6f08-14fe-48ea-8c69-99890839bd6d',
+                isEmergency: false,
+            },
+        }
+
+        expect(detectEventTypes(requestData)).toMatchObject({ [EMERGENCY_CHANGED_EVENT_TYPE]: true } )
+    })
+
+    it('correctly detects turning on emergency indicator on ticket update', () => {
+        const requestData = {
+            operation: 'update',
+            existingItem: { isEmergency: false },
+            updatedItem: {
+                client: '289d6f08-14fe-48ea-8c69-99890839bd6d',
+                isEmergency: true,
+            },
+        }
+
+        expect(detectEventTypes(requestData)).toMatchObject({ [EMERGENCY_CHANGED_EVENT_TYPE]: true } )
     })
 
 })
