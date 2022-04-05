767483f49cfcef7e8a32d9421d24bb48a1e76620
Антон Алёшин <anton_aleschin@mail.ru>
2022-04-05 20:45:27 +0300

    DOMA-2150 port createOrUpdateUser from insurance app

---
+++ apps/miniapp/domains/condo/utils/serverSchema/createOrUpdateUser.js
@@ -0,0 +1,66 @@
+const { getById } = require('@core/keystone/schema')
+
+const { OIDC_SENDER_FIELD_VALUE } = require('@miniapp/domains/common/constants/oidc')
+
+const { User } = require('./index')
+
+/**
+ * @typedef OIDCCondoUserInfo
+ * @property {String} sub
+ * @property {Number} v
+ * @property {Number} dv
+ * @property {String} type
+ * @property {String} name
+ * @property {Boolean} isSupport
+ * @property {Boolean} isAdmin
+ */
+
+const hasChanges = (user, userInfo) => {
+    return user.name !== userInfo.name ||
+        user.v !== userInfo.v ||
+        user.dv !== userInfo.dv ||
+        user.type !== userInfo.type ||
+        user.isSupport !== userInfo.isSupport ||
+        user.isAdmin !== userInfo.isAdmin
+}
+
+/**
+ *
+ * @param keystone - instance of Keystone app
+ * @param {OIDCCondoUserInfo} userInfo
+ * @return {Promise<void>}
+ */
+const createOrUpdateUser = async (keystone, userInfo) => {
+    const context = await keystone.createContext({ skipAccessControl: true })
+    const existingUser = await getById('User', userInfo.sub)
+    if (existingUser) {
+        if (!hasChanges(existingUser, userInfo)) {
+            return existingUser
+        }
+        return await User.update(context, existingUser.id, {
+            sender: OIDC_SENDER_FIELD_VALUE,
+            name: userInfo.name,
+            v: userInfo.v,
+            dv: userInfo.dv,
+            type: userInfo.type,
+            isSupport: userInfo.isSupport,
+            isAdmin: userInfo.isAdmin,
+        })
+    } else {
+        const createdUser = await User.create(context, {
+            sender: OIDC_SENDER_FIELD_VALUE,
+            name: userInfo.name,
+            v: userInfo.v,
+            dv: userInfo.dv,
+            type: userInfo.type,
+            isSupport: userInfo.isSupport,
+            isAdmin: userInfo.isAdmin,
+        })
+        // UserCreateInput does not have `id` field, so, update it manually
+        return await keystone.lists.User.adapter.update(createdUser.id, { id: userInfo.sub })
+    }
+}
+
+module.exports = {
+    createOrUpdateUser,
+}
