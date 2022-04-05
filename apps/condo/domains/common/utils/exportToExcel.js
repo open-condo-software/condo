5c3faad129301be1d6ed113115bb5af42c00dff9
AleX83Xpert <AleX83Xpert@mail.ru>
2022-04-05 13:45:32 +0500

    DOMA-2542 Localization for Excel template (ticket_report_property_status)

--- apps/condo/domains/common/utils/exportToExcel.js
+++ apps/condo/domains/common/utils/exportToExcel.js
@@ -8,6 +8,14 @@ const EXPORT_TYPE_METERS = 'meters'
 const EXPORT_TYPE_BUILDINGS = 'buildings'
 const EXPORT_TYPE_TICKETS = 'tickets'
 
+const TICKETS_REPORTS_PREFIX = 'ticket_report_'
+const TICKET_REPORT_PROPERTY_STATUS = 'property_status'
+const TICKET_REPORT_STATUS_ASSIGNEE = 'status_assignee'
+const TICKET_REPORT_STATUS_CATEGORY_CLASSIFIER = 'status_categoryClassifier'
+const TICKET_REPORT_STATUS_DAY_GROUP = 'status_dayGroup'
+const TICKET_REPORT_STATUS_EXECUTOR = 'status_executor'
+const TICKET_REPORT_STATUS_PROPERTY = 'status_property'
+
 const EXCEL_TEMPLATES_HEADERS = {
     [EXPORT_TYPE_PAYMENTS]: ['date', 'account', 'address', 'unitName', 'type', 'transaction', 'amount'],
     [EXPORT_TYPE_CONTACTS]: ['name', 'address', 'unitName', 'phone', 'email'],
@@ -22,6 +30,15 @@ const EXCEL_TEMPLATES_HEADERS = {
         'createdAt', 'updatedAt', 'inworkAt', 'completedAt', 'status', 'operator', 'executor', 'assignee',
         'comments', 'source', 'deadline', 'reviewValue', 'reviewComment', 'statusReopenedCounter',
     ],
+    [`${TICKETS_REPORTS_PREFIX}${TICKET_REPORT_PROPERTY_STATUS}`]: [
+        'address', 'processing', 'completed', 'canceled', 'deferred', 'closed', 'new_or_reopened',
+    ],
+    //TODO
+    [`${TICKETS_REPORTS_PREFIX}${TICKET_REPORT_STATUS_ASSIGNEE}`]: [],
+    [`${TICKETS_REPORTS_PREFIX}${TICKET_REPORT_STATUS_CATEGORY_CLASSIFIER}`]: [],
+    [`${TICKETS_REPORTS_PREFIX}${TICKET_REPORT_STATUS_DAY_GROUP}`]: [],
+    [`${TICKETS_REPORTS_PREFIX}${TICKET_REPORT_STATUS_EXECUTOR}`]: [],
+    [`${TICKETS_REPORTS_PREFIX}${TICKET_REPORT_STATUS_PROPERTY}`]: [],
 }
 
 /**
@@ -64,6 +81,13 @@ module.exports = {
     EXPORT_TYPE_METERS,
     EXPORT_TYPE_BUILDINGS,
     EXPORT_TYPE_TICKETS,
+    TICKETS_REPORTS_PREFIX,
+    TICKET_REPORT_PROPERTY_STATUS,
+    TICKET_REPORT_STATUS_ASSIGNEE,
+    TICKET_REPORT_STATUS_CATEGORY_CLASSIFIER,
+    TICKET_REPORT_STATUS_DAY_GROUP,
+    TICKET_REPORT_STATUS_EXECUTOR,
+    TICKET_REPORT_STATUS_PROPERTY,
     EXCEL_TEMPLATES_HEADERS,
     getHeadersTranslations,
     translationStringKeyForExcelExportHeader,
