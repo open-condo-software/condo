ea8405b89f8a79e7fc5ec2631bdf941d581ba43b
Matthew Savelev <savelevmatthew@gmail.com>
2022-04-05 23:37:50 +0500

    Revert "DOMA-2542 Localization for Excel template (ticket_report_status_dayGroup)"
    This reverts commit 86769f38227985faf8f3c4d936d492ecc319ace2.

--- apps/condo/domains/common/utils/exportToExcel.js
+++ apps/condo/domains/common/utils/exportToExcel.js
@@ -39,9 +39,8 @@ const EXCEL_TEMPLATES_HEADERS = {
     [`${TICKETS_REPORTS_PREFIX}${TICKET_REPORT_STATUS_CATEGORY_CLASSIFIER}`]: [
         'categoryClassifier', 'address', 'processing', 'completed', 'canceled', 'deferred', 'closed', 'new_or_reopened',
     ],
-    [`${TICKETS_REPORTS_PREFIX}${TICKET_REPORT_STATUS_DAY_GROUP}`]: [
-        'address', 'date', 'processing', 'completed', 'canceled', 'deferred', 'closed', 'new_or_reopened',
-    ],
+    // TODO
+    [`${TICKETS_REPORTS_PREFIX}${TICKET_REPORT_STATUS_DAY_GROUP}`]: [],
     [`${TICKETS_REPORTS_PREFIX}${TICKET_REPORT_STATUS_EXECUTOR}`]: [],
     [`${TICKETS_REPORTS_PREFIX}${TICKET_REPORT_STATUS_PROPERTY}`]: [],
 }
