e5c358228d2da1d96a56a523e029922bb4482e89
Matthew Savelev <savelevmatthew@gmail.com>
2022-04-08 18:08:26 +0500

    HOTFIX Auth next XSS escape

--- apps/condo/domains/common/constants/regexps.js
+++ apps/condo/domains/common/constants/regexps.js
@@ -3,6 +3,7 @@ const UPPER_CASE_ALPHANUMERIC_REGEXP = /^[A-Z0-9_]+$/
 const LETTERS_AND_NUMBERS = /[\p{L}\p{N}]/gu
 const PHONE = /^\+?\d*(\.\d*)?$/
 const PHONE_CLEAR_REGEXP = /[^+0-9]/g
+const JAVASCRIPT_URL_XSS = /^[u00-u1F]*j[\r\n\t]*a[\r\n\t]*v[\r\n\t]*a[\r\n\t]*s[\r\n\t]*c[\r\n\t]*r[\r\n\t]*i[\r\n\t]*p[\r\n\t]*t[\r\n\t]*:/i
 
 module.exports = {
     ALPHANUMERIC_REGEXP,
@@ -10,4 +11,5 @@ module.exports = {
     LETTERS_AND_NUMBERS,
     PHONE,
     PHONE_CLEAR_REGEXP,
+    JAVASCRIPT_URL_XSS,
 }
