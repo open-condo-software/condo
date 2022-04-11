72c81835d23b568cbdd0eb639131efdd1479a2c6
toplenboren <toplenboren@gmail.com>
2022-04-11 23:35:54 +0300

    DOMA-2653 Add isApproved check to tasks.js to handle case with deletion of Properties

--- apps/condo/domains/resident/tasks.js
+++ apps/condo/domains/resident/tasks.js
@@ -26,7 +26,7 @@ async function manageResidentToPropertyAndOrganizationConnections (address) {
         address_i: address,
         deletedAt: null,
     }, {
-        sortBy: 'createdAt_ASC', // sorting order is essential here
+        sortBy: ['isApproved_ASC', 'createdAt_ASC'], // sorting order is essential here
         first: 1,
     })
 
