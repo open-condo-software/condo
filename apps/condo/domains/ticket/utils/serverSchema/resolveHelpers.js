b815c2c536d9cdcbfb35c70c80aef8562ab363d7
Pavel <nomerdvadcatpyat@gmail.com>
2022-04-13 15:01:45 +0500

    DOMA-2743 setted default ticket unitType in resolvedData for resident ticket

--- apps/condo/domains/ticket/utils/serverSchema/resolveHelpers.js
+++ apps/condo/domains/ticket/utils/serverSchema/resolveHelpers.js
@@ -5,6 +5,7 @@ const { Contact } = require('@condo/domains/contact/utils/serverSchema')
 const { TICKET_ORDER_BY_STATUS, STATUS_IDS } = require('@condo/domains/ticket/constants/statusTransitions')
 const { COMPLETED_STATUS_TYPE, NEW_OR_REOPENED_STATUS_TYPE } = require('@condo/domains/ticket/constants')
 const { TicketStatus } = require('@condo/domains/ticket/utils/serverSchema')
+const { FLAT_UNIT_TYPE } = require('@condo/domains/property/constants/common')
 
 function calculateTicketOrder (resolvedData, statusId) {
     if (statusId === STATUS_IDS.OPEN) {
@@ -30,23 +31,16 @@ async function calculateReopenedCounter (context, existingItem, resolvedData) {
     }
 }
 
-async function addClientInfoToResidentTicket (context, resolvedData) {
+async function createContactIfNotExists (context, resolvedData) {
     const user = get(context, ['req', 'user'])
     const organizationId = get(resolvedData, 'organization', null)
     const propertyId = get(resolvedData, 'property', null)
     const unitName = get(resolvedData, 'unitName', null)
 
-    const [property] = await Property.getAll(context, {
-        id: propertyId,
-    })
-
-    const { sectionName, floorName } = getSectionAndFloorByUnitName(property, unitName)
-    resolvedData.sectionName = sectionName
-    resolvedData.floorName = floorName
-
     const residentName = user.name
     const residentPhone = user.phone
     const residentEmail = user.email
+
     const [contact] = await Contact.getAll(context, {
         phone: residentPhone, organization: { id: organizationId }, property: { id: propertyId },
     })
@@ -66,15 +60,39 @@ async function addClientInfoToResidentTicket (context, resolvedData) {
             name: residentName,
         })
     }
+}
 
+async function setSectionAndFloorFieldsByDataFromPropertyMap (context, resolvedData) {
+    const unitName = get(resolvedData, 'unitName', null)
+    const propertyId = get(resolvedData, 'property', null)
+
+    const [property] = await Property.getAll(context, {
+        id: propertyId,
+    })
+
+    const { sectionName, floorName } = getSectionAndFloorByUnitName(property, unitName)
+    resolvedData.sectionName = sectionName
+    resolvedData.floorName = floorName
+}
+
+function setClientNamePhoneEmailFieldsByDataFromUser (user, resolvedData) {
     resolvedData.client = user.id
     resolvedData.clientName = user.name
     resolvedData.clientPhone = user.phone
     resolvedData.clientEmail = user.email
 }
 
+function overrideTicketFieldsForResidentUserType (context, resolvedData) {
+    resolvedData.canReadByResident = true
+    // set default unitType value to tickets, created from older versions of the resident's mobile app where no unitType is passed
+    resolvedData.unitType = resolvedData.unitType || FLAT_UNIT_TYPE
+}
+
 module.exports = {
     calculateReopenedCounter,
     calculateTicketOrder,
-    addClientInfoToResidentTicket,
+    createContactIfNotExists,
+    setSectionAndFloorFieldsByDataFromPropertyMap,
+    setClientNamePhoneEmailFieldsByDataFromUser,
+    overrideTicketFieldsForResidentUserType,
 }
