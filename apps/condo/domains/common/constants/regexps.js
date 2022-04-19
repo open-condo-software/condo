dce6278b93efb6c375e66b4b1cea7503406d4df8
Kanol <andrey.karmanov.2011@yandex.ru>
2022-04-19 12:52:14 +0500

    DOMA-2581 Move query split regex to const

--- apps/condo/domains/common/constants/regexps.js
+++ apps/condo/domains/common/constants/regexps.js
@@ -4,6 +4,7 @@ const LETTERS_AND_NUMBERS = /[\p{L}\p{N}]/gu
 const PHONE = /^\+?\d*(\.\d*)?$/
 const PHONE_CLEAR_REGEXP = /[^+0-9]/g
 const JAVASCRIPT_URL_XSS = /^[u00-u1F]*j[\r\n\t]*a[\r\n\t]*v[\r\n\t]*a[\r\n\t]*s[\r\n\t]*c[\r\n\t]*r[\r\n\t]*i[\r\n\t]*p[\r\n\t]*t[\r\n\t]*:/i
+const QUERY_SPLIT_REGEX = /[\s.,]+/gm
 
 module.exports = {
     ALPHANUMERIC_REGEXP,
@@ -12,4 +13,5 @@ module.exports = {
     PHONE,
     PHONE_CLEAR_REGEXP,
     JAVASCRIPT_URL_XSS,
+    QUERY_SPLIT_REGEX,
 }
