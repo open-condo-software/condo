9465b9b3c1b96d6e92b09c68a8369bb60645510e
AleX83Xpert <AleX83Xpert@mail.ru>
2022-04-21 16:12:23 +0500

    DOMA-2523 Temporary redirect from insurance page to 404

--- apps/condo/pages/insurance/index.tsx
+++ apps/condo/pages/insurance/index.tsx
@@ -4,6 +4,8 @@ import { useIntl } from '@core/next/intl'
 import { Typography } from 'antd'
 import getConfig from 'next/config'
 import Head from 'next/head'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 
 const {
     publicRuntimeConfig: { insuranceAppUrl },
@@ -11,7 +13,15 @@ const {
 
 const IndexInsurancePage = () => {
     const intl = useIntl()
+    const router = useRouter()
     const PageTitleMessage = intl.formatMessage({ id: 'pages.condo.insurance.IndexInsurancePage.PageTitle' })
+
+    useEffect(() => {
+        if (!insuranceAppUrl) {
+            router.replace('/404')
+        }
+    }, [])
+
     return (
         <>
             <Head>
