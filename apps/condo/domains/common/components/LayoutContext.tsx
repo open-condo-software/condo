b6890070ae28c6ae14bb2767d15fe6b081d4e6ae
Vasiliy Kislov <wkislov0@gmail.com>
2022-04-21 19:55:54 +0500

    DOMA-2503 Change variable name

--- apps/condo/domains/common/components/LayoutContext.tsx
+++ apps/condo/domains/common/components/LayoutContext.tsx
@@ -9,7 +9,7 @@ const { useBreakpoint } = Grid
 interface ILayoutContext {
     isMobile?: boolean
     isSmall?: boolean
-    tableScrollInLowResolution?: boolean
+    lowResScroll?: boolean
     breakpoints?: ScreenMap
     isCollapsed?: boolean
     toggleCollapsed?: () => void
@@ -35,7 +35,7 @@ export const LayoutContextProvider: React.FC = (props) => {
     }
 
     const isSmall = (breakpoints.md || breakpoints.xs || breakpoints.sm) && !breakpoints.lg
-    const tableScrollInLowResolution = (breakpoints.md || breakpoints.xs || breakpoints.sm) && !breakpoints.xl
+    const lowResScroll = (breakpoints.md || breakpoints.xs || breakpoints.sm) && !breakpoints.xl
 
     useEffect(() => {
         const isCollapsed = localStorage.getItem('isCollapsed') === 'true'
@@ -47,7 +47,7 @@ export const LayoutContextProvider: React.FC = (props) => {
         <LayoutContext.Provider value={{
             isMobile: detectMobileNavigator(),
             isSmall,
-            tableScrollInLowResolution,
+            lowResScroll,
             breakpoints,
             isCollapsed,
             toggleCollapsed,
