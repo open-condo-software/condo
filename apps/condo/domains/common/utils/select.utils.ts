d9a3088c7ffd951055d4d159d690c062cf356ada
Pavel <nomerdvadcatpyat@gmail.com>
2022-04-28 21:55:09 +0500

    DOMA-2885 calculate scrollTopMax if no scrollTopMax property in event target

--- apps/condo/domains/common/utils/select.utils.ts
+++ apps/condo/domains/common/utils/select.utils.ts
@@ -1,7 +1,12 @@
+import get from 'lodash/get'
 
 export function isNeedToLoadNewElements (scrollEvent, isLoading: boolean) {
-    const scrollTop = scrollEvent.target.scrollTop
-    const scrollTopMax = scrollEvent.target.scrollTopMax
+    const target = get(scrollEvent, 'target')
+    if (!target) return false
+
+    const scrollTop = target.scrollTop
+    let scrollTopMax = target.scrollTopMax
+    scrollTopMax = scrollTopMax || target.scrollHeight - target.clientHeight
 
     return scrollTopMax - 100 < scrollTop && !isLoading
 }
