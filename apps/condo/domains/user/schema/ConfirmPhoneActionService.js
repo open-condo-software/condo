17fa87613f2834eabe71b19340acf3556085ee71
Anton Aleshin <Anton_Aleschin@mail.ru>
2022-04-29 09:59:56 +0300

    DOMA-2243 utilized new errors standard in ConfirmPhoneActionService

--- apps/condo/domains/user/schema/ConfirmPhoneActionService.js
+++ apps/condo/domains/user/schema/ConfirmPhoneActionService.js
@@ -3,18 +3,11 @@ const isEmpty = require('lodash/isEmpty')
 const { GQLCustomSchema } = require('@core/keystone/schema')
 const { captchaCheck } = require('@condo/domains/user/utils/googleRecaptcha3')
 const {
-    CAPTCHA_CHECK_FAILED,
-    CONFIRM_PHONE_ACTION_EXPIRED,
-    CONFIRM_PHONE_SMS_CODE_EXPIRED,
-    CONFIRM_PHONE_SMS_CODE_MAX_RETRIES_REACHED,
-    CONFIRM_PHONE_SMS_CODE_VERIFICATION_FAILED,
-} = require('@condo/domains/user/constants/errors')
-const {
     ConfirmPhoneAction,
     generateSmsCode,
 } = require('@condo/domains/user/utils/serverSchema')
 const { normalizePhone } = require('@condo/domains/common/utils/phone')
-const { PHONE_WRONG_FORMAT_ERROR } = require('@condo/domains/common/constants/errors')
+const { WRONG_FORMAT } = require('@condo/domains/common/constants/errors')
 const {
     SMS_CODE_TTL,
     CONFIRM_PHONE_ACTION_EXPIRY,
@@ -25,9 +18,60 @@ const { COUNTRIES, RUSSIA_COUNTRY } = require('@condo/domains/common/constants/c
 const { sendMessage } = require('@condo/domains/notification/utils/serverSchema')
 const { SMS_VERIFY_CODE_MESSAGE_TYPE } = require('@condo/domains/notification/constants/constants')
 const { RedisGuard } = require('@condo/domains/user/utils/serverSchema/guards')
+const { GQLError, GQLErrorCode: { NOT_FOUND, BAD_USER_INPUT } } = require('@core/keystone/errors')
+const {
+    CAPTCHA_CHECK_FAILED,
+    UNABLE_TO_FIND_CONFIRM_PHONE_ACTION,
+    SMS_CODE_EXPIRED,
+    SMS_CODE_MAX_RETRIES_REACHED,
+    SMS_CODE_VERIFICATION_FAILED,
+} = require('../constants/errors')
 
 const redisGuard = new RedisGuard()
 
+const errors = {
+    CAPTCHA_CHECK_FAILED: {
+        variable: ['data', 'captcha'],
+        code: BAD_USER_INPUT,
+        type: CAPTCHA_CHECK_FAILED,
+        message: 'Failed to check CAPTCHA',
+    },
+    UNABLE_TO_FIND_CONFIRM_PHONE_ACTION: {
+        variable: ['data', 'token'],
+        code: BAD_USER_INPUT,
+        type: UNABLE_TO_FIND_CONFIRM_PHONE_ACTION,
+        message: 'Confirm phone action was expired or it could not be found. Try to initiate phone confirmation again',
+    },
+    WRONG_PHONE_FORMAT: {
+        mutation: 'startConfirmPhoneAction',
+        variable: ['data', 'phone'],
+        code: BAD_USER_INPUT,
+        type: WRONG_FORMAT,
+        message: 'Wrong format of provided phone number',
+        correctExample: '+79991234567',
+    },
+    SMS_CODE_EXPIRED: {
+        mutation: 'completeConfirmPhoneAction',
+        variable: ['data', 'smsCode'],
+        code: BAD_USER_INPUT,
+        type: SMS_CODE_EXPIRED,
+        message: 'SMS code expired. Try to initiate phone confirmation again',
+    },
+    SMS_CODE_MAX_RETRIES_REACHED: {
+        mutation: 'completeConfirmPhoneAction',
+        variable: ['data', 'smsCode'],
+        code: BAD_USER_INPUT,
+        type: SMS_CODE_MAX_RETRIES_REACHED,
+        message: 'Max retries reached for SMS code confirmation. Try to initiate phone confirmation again',
+    },
+    SMS_CODE_VERIFICATION_FAILED: {
+        mutation: 'completeConfirmPhoneAction',
+        variable: ['data', 'smsCode'],
+        code: BAD_USER_INPUT,
+        type: SMS_CODE_VERIFICATION_FAILED,
+        message: 'SMS code verification mismatch',
+    },
+}
 
 const ConfirmPhoneActionService = new GQLCustomSchema('ConfirmPhoneActionService', {
     types: [
@@ -72,7 +116,7 @@ const ConfirmPhoneActionService = new GQLCustomSchema('ConfirmPhoneActionService
                 const { token, captcha } = args.data
                 const { error } = await captchaCheck(captcha, 'get_confirm_phone_token_info')
                 if (error) {
-                    throw new Error(`${CAPTCHA_CHECK_FAILED}] ${error}`)
+                    throw new GQLError({ ...errors.CAPTCHA_CHECK_FAILED, mutation: 'getPhoneByConfirmPhoneActionToken', data: { error } })
                 }
                 const now = extra.extraNow || Date.now()
                 const actions = await ConfirmPhoneAction.getAll(context, {
@@ -81,7 +125,7 @@ const ConfirmPhoneActionService = new GQLCustomSchema('ConfirmPhoneActionService
                     completedAt: null,
                 })
                 if (isEmpty(actions)) {
-                    throw new Error(`${CONFIRM_PHONE_ACTION_EXPIRED}] unable to find confirm phone action by token`)
+                    throw new GQLError({ ...errors.UNABLE_TO_FIND_CONFIRM_PHONE_ACTION, mutation: 'getPhoneByConfirmPhoneActionToken' })
                 }
                 const { phone, isPhoneVerified } = actions[0]
                 return { phone, isPhoneVerified }
@@ -96,11 +140,11 @@ const ConfirmPhoneActionService = new GQLCustomSchema('ConfirmPhoneActionService
                 const { phone: inputPhone, sender, dv, captcha } = args.data
                 const { error } = await captchaCheck(captcha, 'start_confirm_phone')
                 if (error) {
-                    throw new Error(`${CAPTCHA_CHECK_FAILED}] ${error}`)
+                    throw new GQLError({ ...errors.CAPTCHA_CHECK_FAILED, mutation: 'startConfirmPhoneAction', data: { error } })
                 }
                 const phone = normalizePhone(inputPhone)
                 if (!phone) {
-                    throw new Error(`${PHONE_WRONG_FORMAT_ERROR}]: not valid phone number provided`)
+                    throw new GQLError(errors.WRONG_PHONE_FORMAT)
                 }
                 await redisGuard.checkSMSDayLimitCounters(phone, context.req.ip)
                 await redisGuard.checkLock(phone, 'sendsms')
@@ -145,7 +189,7 @@ const ConfirmPhoneActionService = new GQLCustomSchema('ConfirmPhoneActionService
                 const { token, sender, captcha } = args.data
                 const { error } = await captchaCheck(captcha, 'resend_sms')
                 if (error) {
-                    throw new Error(`${CAPTCHA_CHECK_FAILED}] ${error}`)
+                    throw new GQLError({ ...errors.CAPTCHA_CHECK_FAILED, mutation: 'resendConfirmPhoneActionSms', data: { error } })
                 }
                 const now = extra.extraNow || Date.now()
                 const actions = await ConfirmPhoneAction.getAll(context, {
@@ -154,7 +198,7 @@ const ConfirmPhoneActionService = new GQLCustomSchema('ConfirmPhoneActionService
                     completedAt: null,
                 })
                 if (isEmpty(actions)) {
-                    throw new Error(`${CONFIRM_PHONE_ACTION_EXPIRED}] unable to find confirm phone action by token`)
+                    throw new GQLError({ ...errors.UNABLE_TO_FIND_CONFIRM_PHONE_ACTION, mutation: 'resendConfirmPhoneActionSms' })
                 }
                 const { id, phone } = actions[0]
                 await redisGuard.checkSMSDayLimitCounters(phone, context.req.ip)
@@ -187,7 +231,7 @@ const ConfirmPhoneActionService = new GQLCustomSchema('ConfirmPhoneActionService
                 const { token, smsCode, captcha } = args.data
                 const { error } = await captchaCheck(captcha, 'complete_verify_phone')
                 if (error) {
-                    throw new Error(`${CAPTCHA_CHECK_FAILED}] ${error}`)
+                    throw new GQLError({ ...errors.CAPTCHA_CHECK_FAILED, mutation: 'completeConfirmPhoneAction', data: { error } })
                 }
                 const now = extra.extraNow || Date.now()
                 const actions = await ConfirmPhoneAction.getAll(context, {
@@ -196,26 +240,26 @@ const ConfirmPhoneActionService = new GQLCustomSchema('ConfirmPhoneActionService
                     completedAt: null,
                 })
                 if (isEmpty(actions)) {
-                    throw new Error(`${CONFIRM_PHONE_ACTION_EXPIRED}] unable to find confirm phone action`)
+                    throw new GQLError({ ...errors.UNABLE_TO_FIND_CONFIRM_PHONE_ACTION, mutation: 'completeConfirmPhoneAction' })
                 }
                 await redisGuard.checkLock(token, 'confirm')
                 await redisGuard.lock(token, 'confirm', LOCK_TIMEOUT)
                 const { id, smsCode: actionSmsCode, retries, smsCodeExpiresAt } = actions[0]
                 const isExpired = (new Date(smsCodeExpiresAt) < new Date(now))
                 if (isExpired) {
-                    throw new Error(`${CONFIRM_PHONE_SMS_CODE_EXPIRED}] SMS code expired `)
+                    throw new GQLError(errors.SMS_CODE_EXPIRED)
                 }
                 if (retries >= CONFIRM_PHONE_SMS_MAX_RETRIES) {
                     await ConfirmPhoneAction.update(context, id, {
                         completedAt: new Date(now).toISOString(),
                     })
-                    throw new Error(`${CONFIRM_PHONE_SMS_CODE_MAX_RETRIES_REACHED}] Retries limit is excided try to confirm from start`)
+                    throw new GQLError(errors.SMS_CODE_MAX_RETRIES_REACHED)
                 }
                 if (actionSmsCode !== smsCode) {
                     await ConfirmPhoneAction.update(context, id, {
                         retries: retries + 1,
                     })
-                    throw new Error(`${CONFIRM_PHONE_SMS_CODE_VERIFICATION_FAILED}]: SMSCode mismatch`)
+                    throw new GQLError(errors.SMS_CODE_VERIFICATION_FAILED)
                 }
                 await ConfirmPhoneAction.update(context, id, {
                     isPhoneVerified: true,
