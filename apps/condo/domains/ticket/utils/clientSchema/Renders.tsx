93dce3c5e93351df0c1ca9be91ea995c0161747a
Vasiliy Kislov <wkislov0@gmail.com>
2022-04-29 12:54:44 +0500

    DOMA-2503 change "?" on get and adding a comment to a function

--- apps/condo/domains/ticket/utils/clientSchema/Renders.tsx
+++ apps/condo/domains/ticket/utils/clientSchema/Renders.tsx
@@ -13,11 +13,12 @@ export const getTicketDetailsRender = (search: FilterValue) => {
     }
 }
 
-export const getTicketClientNameRender = (search: FilterValue) => {
+// This function is needed to shorten the ClientName so that the field in which it is located is not unnecessarily stretched
+export const getTicketClientNameRender = (search: FilterValue) => { 
     return function render (ClientName: string, ticket: ITicketUIState) {
         const address = get(ticket, ['property', 'address'])
         const maxClientNameLength = address ? address.length : ClientName.length
-        const trimmedClientName = ClientName?.length > maxClientNameLength ? `${ClientName.substring(0, maxClientNameLength)}…` : ClientName
+        const trimmedClientName = get(ticket, ['ClientName'], '').length > maxClientNameLength ? `${ClientName.substring(0, maxClientNameLength)}…` : ClientName
 
         return getTableCellRenderer(search, false, null, null, null, ClientName)(trimmedClientName)
     }
