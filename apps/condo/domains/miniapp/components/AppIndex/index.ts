0dfa66c3f0c722b6faf27683bd20002823d2900b
Matthew Savelev <savelevmatthew@gmail.com>
2022-05-05 20:58:33 +0500

    DOMA-2886 Added app index page

--- apps/condo/domains/miniapp/components/AppIndex/index.ts
+++ apps/condo/domains/miniapp/components/AppIndex/index.ts
@@ -1,9 +1,11 @@
 import { AppConnectedPageContent } from './AppConnectedPageContent'
 import { IndexBillingAppPage } from './IndexBillingAppPage'
 import { IndexAcquiringAppPage } from './IndexAcquiringAppPage'
+import {  IndexB2BAppPage } from './IndexB2BAppPage'
 
 export default AppConnectedPageContent
 export {
     IndexBillingAppPage,
     IndexAcquiringAppPage,
+    IndexB2BAppPage,
 }
