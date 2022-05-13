1aca6ff2fad7e4a2bf907ec62f01628c08193be1
Anton Aleshin <Anton_Aleschin@mail.ru>
2022-05-13 14:49:23 +0300

    DOMA-2920 replaced changing `finishAt` from setting absolute to increasing by relative value in `prolong-subscription` script

--- apps/condo/bin/prolong-subscription.js
+++ apps/condo/bin/prolong-subscription.js
@@ -4,30 +4,43 @@
  * and which satisfies filtering conditions
  *
  * Usage:
- *  yarn workspace @app/condo subscription:prolong <type> <isTrial> <date> <finishAt> <filePathWithOrganizationIds>
+ *  yarn workspace @app/condo subscription:prolong <type> <isTrial> <finishAt> <filePathWithOrganizationIds>
  *
  *  <type>
  *      Value for `ServiceSubscription.type` for filtering condition of records, that will be affected with this script
  *  <isTrial>
  *      Value for `ServiceSubscription.isTrial` for filtering condition of records, that will be affected with this script
- *  <date>
- *      New value of `ServiceSubscription.finishAt` for records, that will be affected with this script
+ *  <duration>
+ *      Relative amount in format of "N unit", like "3 days", on that the `ServiceSubscription.finishAt` for each selected record will be increased.
+ *      Where "unit" value should belong to UNIT_VALUES
  *  <filePathWithOrganizationIds>
  *      Path to file, that contains a list of organization ids, whose subscriptions should be altered. Each id should be on separate line
  *
  * Example:
- *      yarn workspace @app/condo subscription:prolong default true 2021-12-01T12:00:00.000Z ./ids.txt
+ *      cd apps/condo
+ *      yarn node ./bin/prolong-subscription.js default true '15 days' ./ids.txt
  */
 const fs = require('fs')
 const path = require('path')
 const readline = require('readline')
-const { values } = require('lodash')
+const { values, identity, filter } = require('lodash')
 const dayjs = require('dayjs')
 const { GraphQLApp } = require('@keystonejs/app-graphql')
 const { SUBSCRIPTION_TYPE } = require('@condo/domains/subscription/constants')
 const { ServiceSubscription } = require('@condo/domains/subscription/utils/serverSchema')
 
 const CORRECT_TYPE_VALUES = ['true', 'false']
+
+const UNIT_VALUES = [
+    'days',
+    'weeks',
+    'months',
+    'years',
+    'hours',
+    'minutes',
+    'seconds',
+    'milliseconds',
+]
 
 const parseArguments = () => {
     const args = process.argv.slice(2)
@@ -40,28 +53,38 @@ const parseArguments = () => {
         //
         isTrial,
         //
-        finishAt,
+        duration,
         //
         filePathWithOrganizationIds,
     ] = args
     if (!values(SUBSCRIPTION_TYPE).includes(type)) {
         throw new Error(`Wrong value of "type" argument. Correct values: ${values(SUBSCRIPTION_TYPE).join(', ')}`)
     }
-    const finishAtDate = dayjs(finishAt)
-    if (!finishAtDate.isValid()) {
-        throw new Error('Wrong value of "date" argument. Correct value is a date in format, supported by dayjs library')
-    }
-    const fileContentWithOrganizationIds = fs.readFileSync(filePathWithOrganizationIds, 'utf8')
     if (!CORRECT_TYPE_VALUES.includes(isTrial)) {
         throw new Error(`Wrong value of "isTrial" argument. Correct values: ${CORRECT_TYPE_VALUES.join(', ')}`)
     }
+    const fileContentWithOrganizationIds = fs.readFileSync(filePathWithOrganizationIds, 'utf8')
     if (!fileContentWithOrganizationIds) {
         throw new Error(`Loaded file "${filePathWithOrganizationIds}" seems to be empty or have incorrect data format`)
     }
-    const organizationIds = fileContentWithOrganizationIds.split('\n')
+    const organizationIds = filter(fileContentWithOrganizationIds.split('\n'), identity)
     if (!Array.isArray(organizationIds)) {
         throw new Error(`File ${filePathWithOrganizationIds}, specified in "filePathWithOrganizationIds" argument has wrong format. Each id should be on separate line`)
     }
+
+    let increaseFinishAtBy
+    const parsedDuration = duration.split(' ')
+    const [ amount, unit ] = parsedDuration
+    if (parsedDuration.length !== 2) {
+        throw new Error(`Wrong format of relative "finishAt" argument with provided value "${duration}". It should be provided in format "N units", where "N" is an integer representing amount and "units" is a duration unit, supported by https://day.js.org/docs/en/durations/durations, for example "days", "months" etc.`)
+    } else if (!UNIT_VALUES.includes(unit)) {
+        throw new Error(`Invalid unit type part "${unit}" for argument <duration> with value "${duration}". It should belong to UNIT_VALUES.`)
+    } else if (Number.isNaN(parseInt(amount))) {
+        throw new Error(`Invalid amount part "${amount}" for argument <duration> with value "${duration}". It should be a number.`)
+    } else {
+        increaseFinishAtBy = parsedDuration
+    }
+
     return {
         where: {
             type,
@@ -70,7 +93,7 @@ const parseArguments = () => {
                 id_in: organizationIds,
             },
         },
-        finishAt: dayjs.utc(finishAt),
+        increaseFinishAtBy,
     }
 }
 
@@ -82,12 +105,12 @@ async function main () {
     await keystone.prepare({ apps: [apps[graphqlIndex]], distDir, dev: true })
     await keystone.connect()
 
-    const { where, finishAt } = parseArguments()
+    const { where, increaseFinishAtBy } = parseArguments()
 
     const adminContext = await keystone.createContext({ skipAccessControl: true })
 
     console.debug('where', where)
-    console.debug('finishAt', finishAt.format())
+    console.debug('increaseFinishAtBy', increaseFinishAtBy)
 
     const serviceSubscriptions = await ServiceSubscription.getAll(adminContext, where)
     if (serviceSubscriptions.length === 0) {
@@ -95,13 +118,12 @@ async function main () {
         process.exit(0)
     }
     console.debug(serviceSubscriptions)
+    console.log(`Above are ${serviceSubscriptions.length} ServiceSubscription records, that will get "finishAt" increased to ${increaseFinishAtBy.join(' ')}\n`)
 
     const rl = readline.createInterface({
         input: process.stdin,
         output: process.stdout,
     })
-
-    console.log(`Above are ${serviceSubscriptions.length} ServiceSubscription records, that will get "finishAt" changed to ${finishAt.format()}.\n`)
 
     rl.question('Continue (y/n)?', async answer => {
         if (answer === 'n') {
@@ -109,12 +131,16 @@ async function main () {
         } else {
             let processedCount = 0
             await Promise.all(serviceSubscriptions.map(async serviceSubscription => {
+                const [ amount, unit ] = increaseFinishAtBy
+                const newFinishAt = dayjs(serviceSubscription.finishAt).add(amount, unit).format()
                 console.log(`Updating service subscription with organization=${serviceSubscription.id}`)
+                console.debug('old finishAt', serviceSubscription.finishAt)
+                console.debug('new finishAt', newFinishAt)
                 console.debug('before', serviceSubscription)
                 const result = await ServiceSubscription.update(adminContext, serviceSubscription.id, {
                     dv: 1,
                     sender: { dv: 1, fingerprint: 'prolong-subscriptions' },
-                    finishAt: finishAt.format(),
+                    finishAt: newFinishAt,
                 })
                 console.debug('after', result)
                 processedCount++
