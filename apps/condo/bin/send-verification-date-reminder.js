8f0d29e5feebd4741830b74121792b2dd3deba14
ekabardinsky <ekabardinsky@gmail.com>
2022-05-13 13:46:43 +0500

    DOMA-2970 prepare a draft implementation of extraction meters set

---
+++ apps/condo/bin/send-verification-date-reminder.js
@@ -0,0 +1,30 @@
+/**
+ * Load meters with 30/60 days reminders and sending notifications
+ *
+ * Usage:
+ *      yarn workspace @app/condo node bin/send-verification-date-reminder
+ */
+
+const dayjs = require('dayjs')
+
+const path = require('path')
+const { VERIFICATION_DATE_REMINDER_30_DAYS_TYPE } = require('@condo/domains/notification/constants/constants')
+const { GraphQLApp } = require('@keystonejs/app-graphql')
+const { sendVerificationDateReminder } = require('@condo/domains/meter/integrations/sendVerificationDateReminder')
+
+
+async function main () {
+    const resolved = path.resolve('./index.js')
+    const { distDir, keystone, apps } = require(resolved)
+    const graphqlIndex = apps.findIndex(app => app instanceof GraphQLApp)
+    // we need only apollo
+    await keystone.prepare({ apps: [apps[graphqlIndex]], distDir, dev: true })
+    await keystone.connect()
+
+    await sendVerificationDateReminder(dayjs(), VERIFICATION_DATE_REMINDER_30_DAYS_TYPE, 0, 30)
+}
+
+main().catch((e) => {
+    console.error(e)
+    process.exit(1)
+})
---
+++ apps/condo/domains/meter/tasks/index.js
@@ -0,0 +1,5 @@
+const sendVerificationDateReminder = require('./sendVerificationDateReminder')
+
+module.exports = {
+    sendVerificationDateReminder,
+}
