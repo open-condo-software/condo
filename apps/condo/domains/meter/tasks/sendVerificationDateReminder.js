0e8f46f765158cfeedebf8b706c6f86fe80ae2f5
ekabardinsky <ekabardinsky@gmail.com>
2022-05-16 12:49:28 +0500

    DOMA-2970 Add base functionality of meters reminder notification

--- apps/condo/domains/meter/tasks/sendVerificationDateReminder.js
+++ apps/condo/domains/meter/tasks/sendVerificationDateReminder.js
@@ -1,16 +1,14 @@
+const dayjs = require('dayjs')
 const { createCronTask } = require('@core/keystone/tasks')
 const { sendVerificationDateReminder } = require('../integrations/sendVerificationDateReminder')
-const {
-    VERIFICATION_DATE_REMINDER_30_DAYS_TYPE,
-    VERIFICATION_DATE_REMINDER_60_DAYS_TYPE,
-} = require('@condo/domains/notification/constants/constants')
+const { METER_VERIFICATION_DATE_REMINDER_TYPE } = require('@condo/domains/notification/constants/constants')
 
 /**
  * Syncs new and cancelled subscriptions
  */
 const sendVerificationDateReminderTask = createCronTask('sendVerificationDateReminder', '* * * * *', async (date) => {
-    await sendVerificationDateReminder(date, VERIFICATION_DATE_REMINDER_30_DAYS_TYPE, 0, 30)
-    await sendVerificationDateReminder(date, VERIFICATION_DATE_REMINDER_60_DAYS_TYPE, 30, 30)
+    await sendVerificationDateReminder(dayjs(), METER_VERIFICATION_DATE_REMINDER_TYPE, 0, 30)
+    await sendVerificationDateReminder(dayjs(), METER_VERIFICATION_DATE_REMINDER_TYPE, 30, 30)
 })
 
 module.exports = sendVerificationDateReminderTask
