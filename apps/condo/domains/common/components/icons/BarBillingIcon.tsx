33b4ba00871df838084c4a1f8700ce01a1613113
Vasiliy Kislov <wkislov0@gmail.com>
2022-05-23 14:02:31 +0500

    DOMA-1896 removing unnecessary empty lines

--- apps/condo/domains/common/components/icons/BarBillingIcon.tsx
+++ apps/condo/domains/common/components/icons/BarBillingIcon.tsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import Icon from '@ant-design/icons'
 
-
-
 const BarBillingIconSVG: React.FC = () => {
     return (
         <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
