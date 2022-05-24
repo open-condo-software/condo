11cd7b8a48d5b50ffec83e0d09ba392a8f3a6d36
Anton Aleshin <Anton_Aleschin@mail.ru>
2022-05-24 15:02:47 +0300

    DOMA-3039 create test comments in test of ExportTicketService

--- apps/condo/domains/ticket/schema/ExportTicketsService.test.js
+++ apps/condo/domains/ticket/schema/ExportTicketsService.test.js
@@ -1,36 +1,57 @@
-const { makeClientWithNewRegisteredAndLoggedInUser } = require('@condo/domains/user/utils/testSchema')
-const { createTestTicket } = require('@condo/domains/ticket/utils/testSchema')
+const { makeLoggedInClient, makeClientWithNewRegisteredAndLoggedInUser } = require('@condo/domains/user/utils/testSchema')
+const { createTestUser } = require('@condo/domains/user/utils/testSchema')
+const { makeLoggedInAdminClient } = require('@core/keystone/test.utils')
+const { createTestTicket, createTestTicketComment } = require('@condo/domains/ticket/utils/testSchema')
 const { EXPORT_TICKETS_TO_EXCEL } = require('@condo/domains/ticket/gql')
 const { makeClient } = require('@core/keystone/test.utils')
-const { makeClientWithProperty } = require('@condo/domains/property/utils/testSchema')
+const { makeClientWithProperty, createTestProperty } = require('@condo/domains/property/utils/testSchema')
 const { DEFAULT_ORGANIZATION_TIMEZONE } = require('@condo/domains/organization/constants/common')
 const isObsConfigured = require('@condo/domains/common/utils/testSchema/isObsConfigured')
 const faker = require('faker')
+const {
+    createTestOrganization,
+    createTestOrganizationEmployeeRole,
+    createTestOrganizationEmployee
+} = require('@condo/domains/organization/utils/testSchema')
 
 describe('ExportTicketService', () => {
     describe('User', () => {
         it('can get tickets export from selected organization', async () => {
             if (isObsConfigured()) {
-                const client = await makeClientWithProperty()
-                await createTestTicket(client, client.organization, client.property)
-                const { data: { result: { status, linkToFile } } } = await client.query(EXPORT_TICKETS_TO_EXCEL, {
+                const adminClient = await makeLoggedInAdminClient()
+                const userClient = await makeClientWithNewRegisteredAndLoggedInUser()
+                const [organization] = await createTestOrganization(adminClient)
+                const [property] = await createTestProperty(adminClient, organization)
+                const [role] = await createTestOrganizationEmployeeRole(adminClient, organization, {
+                    canManageTickets: true,
+                    canManageTicketComments: true,
+                })
+                await createTestOrganizationEmployee(adminClient, organization, userClient.user, role)
+
+                const [ticket] = await createTestTicket(userClient, organization, property)
+                // Generated file should contain created comments, examine it manually
+                for (let i = 0; i < 10; i++) {
+                    await createTestTicketComment(userClient, ticket, userClient.user)
+                }
+                const { data: { result: { status, linkToFile } } } = await userClient.query(EXPORT_TICKETS_TO_EXCEL, {
                     data: {
                         dv: 1,
                         sender: { dv: 1, fingerprint: 'test-' + faker.random.alphaNumeric(8) },
-                        where: { organization: { id: client.organization.id } },
-                        dv: 1,
-                        sender: { dv: 1, fingerprint: 'test-' + faker.random.alphaNumeric(8) },
+                        where: { organization: { id: organization.id } },
                         sortBy: 'id_ASC',
                         timeZone: DEFAULT_ORGANIZATION_TIMEZONE,
                     },
                 })
                 expect(status).toBe('ok')
                 expect(linkToFile).not.toHaveLength(0)
+                // TODO(antonal): automatically examine created export file
             }
         })
 
         it('can not get tickets export from another organization', async () => {
-            const client = await makeClientWithNewRegisteredAndLoggedInUser()
+            const admin = await makeLoggedInAdminClient()
+            const [, userAttrs] = await createTestUser(admin)
+            const client = await makeLoggedInClient(userAttrs)
             const client2 = await makeClientWithProperty()
             await createTestTicket(client2, client2.organization, client2.property)
             const { data: { result }, errors } = await client.query(EXPORT_TICKETS_TO_EXCEL, {
