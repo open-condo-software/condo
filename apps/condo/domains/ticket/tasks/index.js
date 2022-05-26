2102b6376ccd509476f04b6e7f39e728177ed951
Anton Aleshin <Anton_Aleschin@mail.ru>
2022-05-26 10:24:36 +0300

    DOMA-3039 splitted tasks in `ticket` domain into separate modules

---
+++ apps/condo/domains/ticket/tasks/index.js
@@ -0,0 +1,7 @@
+const manageTicketPropertyAddressChange = require('./manageTicketPropertyAddressChange')
+const closeCompletedTicketsTask = require('./closeCompletedTicketsTask')
+
+module.exports = {
+    manageTicketPropertyAddressChange,
+    closeCompletedTicketsTask,
+}
