6be73455896b7561f5561823f6f659e5b8aca21d
Pahaz <pahaz.white@gmail.com>
2022-05-28 21:22:11 +0300

    HOTFIX optimize jest.setup.js

--- apps/condo/jest.setup.js
+++ apps/condo/jest.setup.js
@@ -1,19 +1,10 @@
 // setup TESTS_FAKE_CLIENT_MODE
 const { setFakeClientMode } = require('@core/keystone/test.utils')
-const { createWorker, taskQueue } = require('@core/keystone/tasks')
+const { createWorker } = require('@core/keystone/tasks')
 const conf = require('@core/config')
 
-if (conf.TESTS_FAKE_CLIENT_MODE) setFakeClientMode(require.resolve('./index'))
+if (conf.TESTS_FAKE_CLIENT_MODE) setFakeClientMode(require.resolve('./index'), { excludeApps: ['NextApp'] })
 if (conf.TESTS_FAKE_WORKER_MODE) createWorker(require.resolve('./index')).catch((error) => {
     console.error(error)
     process.exit(2)
-})
-
-beforeAll(async () => {
-    jest.setTimeout(10 * 60 * 1000) // 10 min
-    return await taskQueue.isReady()
-})
-
-afterAll(async () => {
-    return await taskQueue.close()
 })
