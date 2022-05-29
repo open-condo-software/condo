b11a6d854e2d36d465018f8a6d7f3fea022266df
Pahaz <pahaz.white@gmail.com>
2022-05-29 21:43:37 +0300

    HOTFIX disable EXTRA test LOGGING by default

--- apps/condo/jest.setup.js
+++ apps/condo/jest.setup.js
@@ -1,8 +1,12 @@
 // setup TESTS_FAKE_CLIENT_MODE
+const falsey = require('falsey')
+
 const { setFakeClientMode } = require('@core/keystone/test.utils')
 const { createWorker } = require('@core/keystone/tasks')
 const conf = require('@core/config')
+
 const index = require('./index')
+const EXTRA_LOGGING = falsey(process.env.DISABLE_LOGGING)
 
 if (conf.TESTS_FAKE_CLIENT_MODE) setFakeClientMode(index, { excludeApps: ['NextApp'] })
 if (conf.TESTS_FAKE_WORKER_MODE) createWorker(index).catch((error) => {
@@ -10,11 +14,13 @@ if (conf.TESTS_FAKE_WORKER_MODE) createWorker(index).catch((error) => {
     process.exit(2)
 })
 
-// Patch tests to include their own name
-jasmine.getEnv().addReporter({ // eslint-disable-line
-    specStarted: result => jasmine.currentTest = result, // eslint-disable-line
-    specDone: result => jasmine.currentTest = result, // eslint-disable-line
-})
+if (EXTRA_LOGGING) {
+    // Patch tests to include their own name
+    jasmine.getEnv().addReporter({ // eslint-disable-line
+        specStarted: result => jasmine.currentTest = result, // eslint-disable-line
+        specDone: result => jasmine.currentTest = result, // eslint-disable-line
+    })
 
-beforeEach(() => console.log('[BEGIN] TEST:', jasmine['currentTest'])) // eslint-disable-line
-afterEach(() => console.log('[END] TEST:', jasmine['currentTest'])) // eslint-disable-line
+    beforeEach(() => console.log('[BEGIN] TEST:', jasmine['currentTest'])) // eslint-disable-line
+    afterEach(() => console.log('[END] TEST:', jasmine['currentTest'])) // eslint-disable-line
+}
