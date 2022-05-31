1f15d8c1828a61cd60433abc343a65c3c3f033ae
Pavel <nomerdvadcatpyat@gmail.com>
2022-05-31 16:21:06 +0500

    DOMA-3133 moved findAllByKey to ecmascript.utils.js

--- apps/condo/domains/common/utils/ecmascript.utils.js
+++ apps/condo/domains/common/utils/ecmascript.utils.js
@@ -10,8 +10,19 @@ function isGeneratorFunction (functionToCheck) {
     return functionToCheck && Object.prototype.toString.call(functionToCheck) === '[object GeneratorFunction]'
 }
 
+function findAllByKey (obj, keyToFind) {
+    return Object.entries(obj)
+        .reduce((acc, [key, value]) => (key === keyToFind)
+            ? acc.concat(value)
+            : (typeof value === 'object' && value)
+                ? acc.concat(findAllByKey(value, keyToFind))
+                : acc
+        , [])
+}
+
 module.exports = {
     isFunction,
     isAsyncFunction,
     isGeneratorFunction,
+    findAllByKey,
 }
