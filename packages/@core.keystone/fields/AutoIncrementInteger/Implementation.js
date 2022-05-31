db394d906f96a87d23a076c476f6ed5a3af60ab9
Pahaz <pahaz.white@gmail.com>
2022-05-31 20:16:26 +0300

    DOMA-3146 improve Ticket.number generation algorithm to avoid collision

--- packages/@core.keystone/fields/AutoIncrementInteger/Implementation.js
+++ packages/@core.keystone/fields/AutoIncrementInteger/Implementation.js
@@ -1,10 +1,14 @@
+const { default: Redlock } = require('redlock')
+
 const { Integer } = require('@keystonejs/fields')
+const { taskQueue } = require('@core/keystone/tasks')
 
 class AutoIncrementInteger extends Integer.implementation {
     
     async resolveInput ({ resolvedData, existingItem }) {
         if (!existingItem && !resolvedData[this.path]) {
             const next = await this.adapter.nextValue()
+            if (!next || next <= 0) throw new Error('unexpected AutoIncrementInteger:adapter.nextValue()')
             resolvedData[this.path] = next
         }
         return resolvedData[this.path]
@@ -31,10 +35,32 @@ class AutoIncrementIntegerKnexFieldAdapter extends Integer.adapters.knex {
         const tableName = this.listAdapter.tableName
         const fieldName = this.dbPath
         const knex = this.listAdapter.parentAdapter.knex
-        let [{ max }] = await knex(tableName).max(fieldName)
-        max = max || 0
-        return ++max
-    }    
+        // NOTE(pahaz): it's really hack! please don't copy this staff in a future! I'll find a better solution for that!
+        //  I didn't found a way to use knex subquery for that. Probably, we need to create some DB procedure or use another table with
+        //  sequence column. At the moment this code just help us to avoid `duplicate key value violates unique constraint`
+        const rlock = new Redlock([taskQueue.client])
+        const redisLockKey = `AutoIncrementInteger:${tableName}:${fieldName}`
+        const redisMaxValueKey = `AutoIncrementInteger:${tableName}:${fieldName}:value`
+        let lock = await rlock.acquire([redisLockKey], 500) // 0.5 sec
+        try {
+            let currentMaxNumber = await taskQueue.client.get(redisMaxValueKey)
+            if (!currentMaxNumber) {
+                let [{ max }] = await knex(tableName).max(fieldName)
+                currentMaxNumber = max
+            } else {
+                // NOTE(pahaz): if someone set new number value by hands we need to generate next number MAX + 1
+                let [{ max }] = await knex(tableName).max(fieldName)
+                if (max > currentMaxNumber) {
+                    currentMaxNumber = max
+                }
+            }
+            currentMaxNumber = parseInt(currentMaxNumber || 0)
+            await taskQueue.client.set(redisMaxValueKey, currentMaxNumber + 1)
+            return currentMaxNumber + 1
+        } finally {
+            await lock.release()
+        }
+    }
 }
 
 // TODO(pahaz): add mongo support for AutoIncrementInteger
