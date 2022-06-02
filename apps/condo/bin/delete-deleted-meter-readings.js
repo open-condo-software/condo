d7aa104cac7cf4e3ad6e6a393c05d086667678da
Pavel <nomerdvadcatpyat@gmail.com>
2022-06-02 11:31:34 +0500

    DOMA-3079 added delete reading of deleted meter script

---
+++ apps/condo/bin/delete-deleted-meter-readings.js
@@ -0,0 +1,57 @@
+const path = require('path')
+const { find } = require('@core/keystone/schema')
+const { GraphQLApp } = require('@keystonejs/app-graphql')
+const get = require('lodash/get')
+const { MeterReading } = require('@condo/domains/meter/utils/serverSchema')
+const { RESIDENT } = require('@condo/domains/user/constants/common')
+
+class FixMeterReadingsClients {
+    context = null
+    brokenMeterReadings = []
+
+    async connect () {
+        const resolved = path.resolve('./index.js')
+        const { distDir, keystone, apps } = require(resolved)
+        const graphqlIndex = apps.findIndex(app => app instanceof GraphQLApp)
+        await keystone.prepare({ apps: [apps[graphqlIndex]], distDir, dev: true })
+        await keystone.connect()
+        this.context = await keystone.createContext({ skipAccessControl: true })
+    }
+
+    async findBrokenMeterReadings () {
+        this.brokenMeterReadings = await find('MeterReading', {
+            meter_is_null: true,
+        })
+    }
+
+    async fixBrokenMeterReadings () {
+        if (!get(this.brokenMeterReadings, 'length')) return
+
+        for (const meterReading of this.brokenMeterReadings) {
+            await MeterReading.update(this.context, meterReading.id, {
+                deletedAt: new Date().toDateString(),
+                dv: 1,
+                sender: { dv: 1, fingerprint: 'fixTicketScript' },
+            })
+        }
+    }
+}
+
+const fixMeterReadings = async () => {
+    const fixer = new FixMeterReadingsClients()
+    console.info('[INFO] Connecting to database...')
+    await fixer.connect()
+    console.info('[INFO] Finding broken meter readings...')
+    await fixer.findBrokenMeterReadings()
+    console.info(`[INFO] Following meter readings will be fixed: [${fixer.brokenMeterReadings.map(reading => `"${reading.id}"`).join(', ')}]`)
+    await fixer.fixBrokenMeterReadings()
+    console.info('[INFO] Broken meter readings are fixed...')
+}
+
+fixMeterReadings().then(() => {
+    console.log('\r\n')
+    console.log('All done')
+    process.exit(0)
+}).catch((err) => {
+    console.error('Failed to done', err)
+})
