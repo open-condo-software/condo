5bb33ee3316f0bb97a7adb50f8f84759f57ba52c
Vasiliy Kislov <wkislov0@gmail.com>
2022-06-02 15:43:11 +0500

    DOMA-1896 update logo and ticket creation menu icon

--- apps/condo/domains/common/components/Logo.tsx
+++ apps/condo/domains/common/components/Logo.tsx
@@ -14,18 +14,18 @@ const LogoWrapper = styled.span`
 
 const SunKeyFrames = keyframes`
   from {
-    transform: translate(-30px, -12px);
+    transform: translate(-15px, -12px);
     opacity: 0
   }
   to {
-    transform: translate(-5px, -15px);
+    transform: translate(28px, -15px);
     opacity: 1
   }
 `
 
 const SunCSS = css`
-  animation: ${SunKeyFrames} 5s ease 1;
-  transform: translate(-5px, -15px);
+  animation: ${SunKeyFrames} 3s ease 1;
+  transform: translate(28px, -15px);
 `
 
 interface ILogoProps {
@@ -44,9 +44,15 @@ export const Logo: React.FC<ILogoProps> = (props) => {
     if (minified) {
         return (
             <LogoWrapper onClick={onClick}>
-                <svg width='20' height='22' fill='none' xmlns='http://www.w3.org/2000/svg'>
-                    <path fill={fillColor}
-                        d='M0 0v21.0183h7.399c7.2485 0 12.0608-3.453 12.0608-10.9295C19.4598 3.6332 15.6701 0 7.1282 0H0Zm4.8123 16.5144V4.5039h2.6769c5.083 0 6.9477 2.0418 6.9477 5.9452 0 3.6632-2.1053 6.0653-6.8876 6.0653h-2.737Z'/>
+                <svg width="29" height="31" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path fill="#fff" d="M0 0h29v31H0z"/>
+                    <path fillRule="evenodd" clipRule="evenodd" d="M12.5635 7.1615c-.7113-.572-1.7329-.5692-2.4409.0069L.7004 14.8348C.257 15.1957 0 15.7333 0 16.3006v12.7995c0 1.0492.8619 1.8998 1.925 1.8998h18.9622c1.0632 0 1.9251-.8506 1.9251-1.8998V16.3072c0-.571-.2603-1.1118-.7089-1.4726l-9.5399-7.673Zm6.3986 20.0389v-9.9907l-7.6078-6.1191-7.5041 6.1059v10.0039H18.962Z" fill="url(#a)"/>
+                    <path d="M29 4.0124c0 2.216-1.8177 4.0124-4.0598 4.0124-2.2422 0-4.0598-1.7964-4.0598-4.0124S22.698 0 24.9402 0C27.1823 0 29 1.7964 29 4.0124Z" fill="#FDCF5A"/>
+                    <defs>
+                        <linearGradient id="a" x1="0" y1="18.8671" x2="18.2733" y2="27.9744" gradientUnits="userSpaceOnUse">
+                            <stop stopColor="#4CD174"/><stop offset="1" stopColor="#6DB8F2"/>
+                        </linearGradient>
+                    </defs>
                 </svg>
             </LogoWrapper>
         )
@@ -54,7 +60,6 @@ export const Logo: React.FC<ILogoProps> = (props) => {
 
     return (
         <LogoWrapper onClick={onClick}>
-            <Image preview={false} src={'/logoHouse.svg'}/>
             <Image preview={false} css={SunCSS} src={'/logoSun.svg'}/>
             <Image preview={false} src={'/logoDoma.svg'}/>
         </LogoWrapper>
