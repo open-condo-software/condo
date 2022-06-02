e1c68810a7f692d0eabd2d399a8c044e49fcfb56
Pavel <nomerdvadcatpyat@gmail.com>
2022-06-02 11:21:47 +0500

    DOMA-3079 added delete reading logic, wrote test

--- apps/condo/domains/meter/schema/MeterReading.test.js
+++ apps/condo/domains/meter/schema/MeterReading.test.js
@@ -806,6 +806,27 @@ describe('MeterReading', () => {
             expect(updatedMeterReading.id).toMatch(UUID_RE)
             expect(updatedMeterReading.value1).toEqual(newValue)
         })
+
+        test('readings are deleted after the related meter is deleted', async () => {
+            const adminClient = await makeLoggedInAdminClient()
+            const [organization] = await createTestOrganization(adminClient)
+            const [property] = await createTestProperty(adminClient, organization)
+            const [source] = await MeterReadingSource.getAll(adminClient, { id: CALL_METER_READING_SOURCE_ID })
+            const [resource] = await MeterResource.getAll(adminClient, { id: COLD_WATER_METER_RESOURCE_ID })
+            const [meter] = await createTestMeter(adminClient, organization, property, resource, {})
+            const [meterReading1] = await createTestMeterReading(adminClient, meter, organization, source)
+            const [meterReading2] = await createTestMeterReading(adminClient, meter, organization, source)
+
+            expect(meterReading1.deletedAt).toBeNull()
+            expect(meterReading2.deletedAt).toBeNull()
+
+            await updateTestMeter(adminClient, meter.id, {
+                deletedAt: new Date(),
+            })
+
+            expect(meterReading1.deletedAt).toBeDefined()
+            expect(meterReading2.deletedAt).toBeDefined()
+        })
     })
     describe('Read', () => {
         test('employee: can read MeterReadings', async () => {
