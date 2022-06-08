e5c059b2b4e301ff5fdd697a7c21291ef6f1cd23
Pavel <nomerdvadcatpyat@gmail.com>
2022-06-08 18:30:17 +0500

    DOMA-2807 help.Help -> ticket.TicketHint

--- apps/condo/index.js
+++ apps/condo/index.js
@@ -99,7 +99,6 @@ registerSchemas(keystone, [
     require('@condo/domains/subscription/schema'),
     require('@condo/domains/acquiring/schema'),
     require('@condo/domains/miniapp/schema'),
-    require('@condo/domains/help/schema'),
 ], [schemaDocPreprocessor])
 
 if (!IS_BUILD_PHASE) {
