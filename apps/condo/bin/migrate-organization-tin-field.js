af0553fc5d8062bc40a92551beb06666df094a6a
ekabardinsky <ekabardinsky@gmail.com>
2022-06-08 13:21:59 +0500

    DOMA-3126 add migration script for migration valid tin values from organization.meta.inn to organization.tin

---
+++ apps/condo/bin/migrate-organization-tin-field.js
@@ -0,0 +1,29 @@
+/**
+ * Load valid inn values from organization.meta.inn to organization.tin
+ *
+ * Usage:
+ *      yarn workspace @app/condo node bin/migrate-organization-tin-field
+ */
+
+const dayjs = require('dayjs')
+
+const path = require('path')
+const { GraphQLApp } = require('@keystonejs/app-graphql')
+const { migrateOrganizationTinField } = require('@condo/domains/organization/tasks/migrateOrganizationTinField')
+
+
+async function main () {
+    const resolved = path.resolve('./index.js')
+    const { distDir, keystone, apps } = require(resolved)
+    const graphqlIndex = apps.findIndex(app => app instanceof GraphQLApp)
+    // we need only apollo
+    await keystone.prepare({ apps: [apps[graphqlIndex]], distDir, dev: true })
+    await keystone.connect()
+
+    await migrateOrganizationTinField()
+}
+
+main().catch((e) => {
+    console.error(e)
+    process.exit(1)
+})
