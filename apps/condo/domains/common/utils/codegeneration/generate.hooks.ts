bb2c529742e69a3a19bc4fc0cca935d490a96a65
akarjakin <akarjakin@doma.ai>
2022-06-10 15:55:54 +0500

    DOMA-2601 refactord code of components and hooks

--- apps/condo/domains/common/utils/codegeneration/generate.hooks.ts
+++ apps/condo/domains/common/utils/codegeneration/generate.hooks.ts
@@ -1,4 +1,5 @@
 import { useMemo } from 'react'
+import { isFunction } from 'lodash/isFunction'
 import { ApolloError, QueryHookOptions, OperationVariables } from '@apollo/client'
 
 import { useMutation, useQuery } from '@core/next/apollo'
@@ -35,33 +36,32 @@ interface IHookResult<UI, UIForm, Q, TData = any, TVariables = OperationVariable
 }
 
 export function generateReactHooks<GQL, GQLInput, UIForm, UI, Q> (gql, { convertToGQLInput, convertToUIState }: IHookConverters<GQL, GQLInput, UI, UIForm>): IHookResult<UI, UIForm, Q> {
-
     function useObject (variables: Q, options?: QueryHookOptions<{ objs?: GQL[], meta?: { count?: number } }, Q>) {
         const { loading, refetch, fetchMore, objs, count, error } = useObjects(variables, options)
+
         if (count && count > 1) throw new Error('Wrong query condition! return more then one result')
+
         const obj = (objs.length) ? objs[0] : null
+
         return { loading, refetch, fetchMore, obj, error }
     }
+
     function useObjects (variables: Q, options?: QueryHookOptions<{ objs?: GQL[], meta?: { count?: number } }, Q>) {
         const intl = useIntl()
         const ServerErrorPleaseTryAgainLaterMsg = intl.formatMessage({ id: 'ServerErrorPleaseTryAgainLater' })
         const AccessErrorMsg = intl.formatMessage({ id: 'AccessError' })
-
         const result = useQuery<{ objs?: GQL[], meta?: { count?: number } }, Q>(gql.GET_ALL_OBJS_WITH_COUNT_QUERY, {
             variables,
             notifyOnNetworkStatusChange: true,
             ...options,
         })
-
         let error: ApolloError | string
-        if (error && String(error).includes('not have access')) {
-            error = AccessErrorMsg
-        } else if (error) {
-            error = ServerErrorPleaseTryAgainLaterMsg
+
+        if (error) {
+            error = String(error).includes('not have access') ? AccessErrorMsg : ServerErrorPleaseTryAgainLaterMsg
         }
 
         const objs: UI[] = getObjects(result.data, convertToUIState)
-
         const count = (result.data && result.data.meta) ? result.data.meta.count : null
 
         return {
@@ -80,20 +80,25 @@ export function generateReactHooks<GQL, GQLInput, UIForm, UI, Q> (gql, { convert
      */
     function useCreate (attrs: UIForm | Record<string, unknown> = {}, onComplete) {
         if (typeof attrs !== 'object' || !attrs) throw new Error('useCreate(): invalid attrs argument')
+
         const [rowAction] = useMutation(gql.CREATE_OBJ_MUTATION)
+
         async function _action (state: UIForm) {
-            const { data, errors } = await rowAction({
-                variables: { data: convertToGQLInput({ ...state, ...attrs }) },
-            })
+            const { data, errors } = await rowAction({ variables: { data: convertToGQLInput({ ...state, ...attrs }) } })
+
             if (data && data.obj) {
                 const result = convertToUIState(data.obj)
-                if (onComplete) onComplete(result)
+
+                if (isFunction(onComplete)) onComplete(result)
+
                 return result
             }
+
             if (errors) {
                 console.warn(errors)
                 throw errors
             }
+
             throw new Error('unknown action result')
         }
 
@@ -102,25 +107,32 @@ export function generateReactHooks<GQL, GQLInput, UIForm, UI, Q> (gql, { convert
 
     function useUpdate (attrs = {}, onComplete) {
         if (typeof attrs !== 'object' || !attrs) throw new Error('useUpdate(): invalid attrs argument')
+
         const [rowAction] = useMutation(gql.UPDATE_OBJ_MUTATION)
 
         async function _action (state, obj) {
             if (!obj || !obj.id) throw new Error('No obj.id argument')
+
             const { data, errors } = await rowAction({
                 variables: {
                     id: obj.id,
                     data: convertToGQLInput({ ...state, ...attrs }, obj),
                 },
             })
+
             if (data && data.obj) {
                 const result = convertToUIState(data.obj)
-                if (onComplete) onComplete(result)
+
+                if (isFunction(onComplete)) onComplete(result)
+
                 return result
             }
             if (errors) {
                 console.warn(errors)
+
                 throw errors
             }
+
             throw new Error('unknown action result')
         }
 
@@ -129,24 +141,32 @@ export function generateReactHooks<GQL, GQLInput, UIForm, UI, Q> (gql, { convert
 
     function useDelete (attrs = {}, onComplete) {
         if (typeof attrs !== 'object' || !attrs) throw new Error('useDelete(): invalid attrs argument')
+
         const [rowAction] = useMutation(gql.DELETE_OBJ_MUTATION)
 
         async function _action (obj) {
             if (!obj || !obj.id) throw new Error('No obj.id argument')
+
             const { data, errors } = await rowAction({
                 variables: {
                     id: obj.id,
                 },
             })
+
             if (data && data.obj) {
                 const result = convertToUIState(data.obj)
-                if (onComplete) onComplete(result)
+
+                if (isFunction(onComplete)) onComplete(result)
+
                 return result
             }
+
             if (errors) {
                 console.warn(errors)
+
                 throw errors
             }
+
             throw new Error('unknown action result')
         }
 
@@ -155,30 +175,39 @@ export function generateReactHooks<GQL, GQLInput, UIForm, UI, Q> (gql, { convert
 
     function useSoftDelete (attrs = {}, onComplete) {
         if (typeof attrs !== 'object' || !attrs) throw new Error('useSoftDelete(): invalid attrs argument')
+
         const [rowAction] = useMutation(gql.UPDATE_OBJ_MUTATION)
 
         async function _action (state, obj) {
             if (!obj.id) throw new Error('No obj.id argument')
+
             const { data, errors } = await rowAction({
                 variables: {
                     id: obj.id,
                     data: convertToGQLInput({ ...state, deletedAt: 'true' }, obj),
                 },
             })
+
             if (data && data.obj) {
                 const result = convertToUIState(data.obj)
-                if (onComplete) onComplete(result)
+
+                if (isFunction(onComplete)) onComplete(result)
+
                 return result
             }
+
             if (errors) {
                 console.warn(errors)
+
                 throw errors
             }
+
             throw new Error('unknown action result')
         }
 
         return useMemo(() => _action, [rowAction])
     }
+
     return {
         gql,
         useObject,
