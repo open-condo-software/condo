7ac324d7014c4e6afd1bbc53d2364a769a85bd94
toplenboren <toplenboren@gmail.com>
2022-06-10 15:36:49 +0500

    refactor(condo): DOMA-2681 Proof of concept v2

--- packages/keystone/adapterCache.js
+++ packages/keystone/adapterCache.js
@@ -18,29 +18,33 @@ class AdapterCacheMiddleware {
     // table_name -> lastUpdate (of this table)
     state = {}
 
-    prepareMiddleware ({ keystone, dev, distDir }) {
-        initAdapterCache(keystone, this.cache, this.state)
+    async prepareMiddleware ({ keystone, dev, distDir }) {
+        await initAdapterCache(keystone, this.cache, this.state)
     }
 }
 
 const UPDATED_AT = 'updatedAt'
+const UPDATED_AT_SORT_BY = 'updatedAt_DESC'
 
-const initAdapterCache = (keystone, state, cache) => {
-    const adapter = keystone.adapter
+const initAdapterCache = async (keystone, state, cache) => {
+    const keystoneAdapter = keystone.adapter
 
-    const listAdapters = Object.values(adapter.listAdapters)
-    listAdapters.forEach(adapter => {
+    const listAdapters = Object.values(keystoneAdapter.listAdapters)
 
-        const listName = adapter.key
-        state[listName] = ''
+    for (const listAdapter of listAdapters) {
+
+        const listName = listAdapter.key
         cache[listName] = {}
 
-        const originalItemsQuery = adapter._itemsQuery
-        adapter._itemsQuery = async ( args, opts ) => {
+        const lastUpdatedItem = await listAdapter._itemsQuery.apply(listAdapter, [{ sortBy: UPDATED_AT_SORT_BY, first: 1 }])
+        state[listName] = lastUpdatedItem[UPDATED_AT]
+
+        const originalItemsQuery = listAdapter._itemsQuery
+        listAdapter._itemsQuery = async ( args, opts ) => {
             
             totalRequests++
             
-            const key = JSON.stringify(args)
+            const key = JSON.stringify(args) + '_' + JSON.stringify(opts)
 
             let response = undefined
             const cached = cache[listName][key]
@@ -55,7 +59,7 @@ const initAdapterCache = (keystone, state, cache) => {
                 }
             } 
 
-            response = await originalItemsQuery.apply(adapter, [args, opts] )
+            response = await originalItemsQuery.apply(listAdapter, [args, opts] )
             cache[listName][key] = {
                 lastUpdate: tableLastUpdate,
                 response: response,
@@ -65,26 +69,27 @@ const initAdapterCache = (keystone, state, cache) => {
             return response
         }
 
-        const originalUpdate = adapter._update
-        adapter._update = async ( id, data ) => {
-            const updateResult = await originalUpdate.apply(adapter, [id, data] )
+        const originalUpdate = listAdapter._update
+        listAdapter._update = async ( id, data ) => {
+            const updateResult = await originalUpdate.apply(listAdapter, [id, data] )
             state[listName] = updateResult[UPDATED_AT]
             return updateResult
         }
 
-        const originalCreate = adapter._create
-        adapter._create = async ( data ) => {
-            const createResult = await originalCreate.apply(adapter, [data] )
+        const originalCreate = listAdapter._create
+        listAdapter._create = async ( data ) => {
+            const createResult = await originalCreate.apply(listAdapter, [data] )
             state[listName] = createResult[UPDATED_AT]
             return createResult
         }
 
-        // const originalDelete = adapter._delete
-        // adapter._delete = async ( id ) => {
-        //     const x = 20
-        //     return await originalDelete.apply(adapter, [id])
-        // }
-    })
+        const originalDelete = listAdapter._delete
+        listAdapter._delete = async ( id ) => {
+            const deleteResult = await originalDelete.apply(listAdapter, [id])
+            state[listName] = deleteResult[UPDATED_AT]
+            return deleteResult
+        }
+    }
 }
 
 module.exports = {
