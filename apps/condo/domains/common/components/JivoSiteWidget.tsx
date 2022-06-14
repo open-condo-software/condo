d7444ebd25df3eefdb5fb04a20b1aac9f3fbab41
Vasiliy Kislov <wkislov0@gmail.com>
2022-06-14 16:30:47 +0500

    DOMA-3257 change after review

--- apps/condo/domains/common/components/JivoSiteWidget.tsx
+++ apps/condo/domains/common/components/JivoSiteWidget.tsx
@@ -1,11 +1,7 @@
 import React from 'react'
 
 const JivoSiteWidget: React.FC = () => {
-    return (
-        <>
-            <head><script src="//code.jivosite.com/widget/qv5Fzmhye4" async></script></head>
-        </>
-    )
+    return <head><script src="//code.jivosite.com/widget/qv5Fzmhye4" async></script></head>
 }
 
 export default JivoSiteWidget
