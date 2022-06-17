d5a491080fc05ff5148f3eb9f5743a01ed0f0716
Pavel <nomerdvadcatpyat@gmail.com>
2022-06-17 11:24:14 +0500

    DOMA-3185 fixed hint display on ticket form

--- apps/condo/domains/ticket/components/TicketHint/CreateTicketHint.tsx
+++ apps/condo/domains/ticket/components/TicketHint/CreateTicketHint.tsx
@@ -2,6 +2,7 @@ import { Col, Row, Typography } from 'antd'
 import { get } from 'lodash'
 import Link from 'next/link'
 import React from 'react'
+import { colors } from '../../../common/constants/style'
 import { TicketHint } from '../../utils/clientSchema'
 
 export const CreateTicketHint = ({ selectedPropertyId }) => {
@@ -11,8 +12,8 @@ export const CreateTicketHint = ({ selectedPropertyId }) => {
         },
     })
 
-    return selectedPropertyId && !ticketHintLoading && (
-        <Row gutter={[0, 20]} style={{ position: 'absolute', overflow: 'hidden', backgroundColor: '#F2F3F7', padding: '10px', borderRadius: '12px' }}>
+    return selectedPropertyId && ticketHint && (
+        <Row gutter={[0, 20]} style={{ position: 'absolute', overflow: 'hidden', backgroundColor: colors.backgroundLightGrey, padding: '20px', borderRadius: '12px' }}>
             <Col span={24}>
                 <Typography.Title level={5}>Справка</Typography.Title>
             </Col>
@@ -21,7 +22,7 @@ export const CreateTicketHint = ({ selectedPropertyId }) => {
                     dangerouslySetInnerHTML={{
                         __html: get(ticketHint, 'content'),
                     }}
-                    style={{ maxHeight: '280px', maxWidth: '200px', overflow: 'hidden',  wordBreak: 'break-word' }}
+                    style={{ maxHeight: '250px', maxWidth: '200px', overflow: 'hidden',  wordBreak: 'break-word' }}
                 />
                 <Link href={`/property/${selectedPropertyId}/hint`} passHref>
                     <a target={'_blank'}>
