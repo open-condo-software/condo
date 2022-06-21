bbbbc712ed437ac161a60e4da9806ea4a7cba998
Vasiliy Kislov <wkislov0@gmail.com>
2022-06-21 11:38:00 +0500

    DOMA-3319 fix getConfig definition location

--- apps/condo/domains/common/components/JivoSiteWidget.tsx
+++ apps/condo/domains/common/components/JivoSiteWidget.tsx
@@ -1,13 +1,10 @@
 import React from 'react'
 import getConfig from 'next/config'
 
-const {
-    publicRuntimeConfig,
-} = getConfig()
-
-const { JivoSiteWidgetId } = publicRuntimeConfig
-
 const JivoSiteWidget: React.FC = () => {
+
+    const { publicRuntimeConfig:{ JivoSiteWidgetId } } = getConfig()
+
     return JivoSiteWidgetId ? 
         <script src={`//code.jivosite.com/widget/${JivoSiteWidgetId}`} async></script> : null 
 }
