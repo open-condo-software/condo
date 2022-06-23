924faac4967301df062ebac3a8df4e69d49424f3
Ilya Konik <konick.ilya@yandex.ru>
2022-06-23 15:02:38 +0500

    DOMA-3222 field type and filter function changed

--- apps/condo/domains/ticket/hooks/useTicketTableFilters.tsx
+++ apps/condo/domains/ticket/hooks/useTicketTableFilters.tsx
@@ -44,7 +44,7 @@ const filterReviewValue = getFilter('reviewValue', 'array', 'string', 'in')
 const filterSource = getFilter(['source', 'id'], 'array', 'string', 'in')
 const filterSection = getFilter('sectionName', 'array', 'string', 'in')
 const filterFloor = getFilter('floorName', 'array', 'string', 'in')
-const filterUnit = getFilter('unitName', 'array', 'string', 'in')
+const filterUnit = getStringContainsFilter(['unitName'])
 const filterPlaceClassifier = getFilter(['placeClassifier', 'id'], 'array', 'string', 'in')
 const filterCategoryClassifier = getFilter(['categoryClassifier', 'id'], 'array', 'string', 'in')
 const filterCategoryClassifierSearch = getStringContainsFilter(['categoryClassifier', 'name'])
@@ -274,14 +274,9 @@ export function useTicketTableFilters (): Array<FiltersMeta<MeterReadingWhereInp
                 keyword: 'unitName',
                 filters: [filterUnit],
                 component: {
-                    type: ComponentType.TagsSelect,
+                    type: ComponentType.Input,
                     props: {
-                        tokenSeparators: [' '],
                         placeholder: EnterUnitNameLabel,
-                    },
-                    modalFilterComponentWrapper: {
-                        label: UnitMessage,
-                        size: FilterComponentSize.Medium,
                     },
                 },
             },
