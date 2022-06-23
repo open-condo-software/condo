7d6d604317a96ea72af68fc4eac9b146f83a7bc2
Matthew Savelev <savelevmatthew@gmail.com>
2022-06-23 14:57:46 +0500

    DOMA-3348 Translations was fixed

--- apps/condo/domains/miniapp/components/AppDescription/Alerts/AlreadyConnectedBilling.tsx
+++ apps/condo/domains/miniapp/components/AppDescription/Alerts/AlreadyConnectedBilling.tsx
@@ -3,6 +3,7 @@ import { Alert, Space, Typography } from 'antd'
 import { useIntl } from '@core/next/intl'
 import { Button } from '@condo/domains/common/components/Button'
 import { useRouter } from 'next/router'
+import { SUPPORT_EMAIL } from '@condo/domains/common/constants/requisites'
 
 const ALERT_BOX_STYLE: CSSProperties = {
     width: 'fit-content',
@@ -10,8 +11,15 @@ const ALERT_BOX_STYLE: CSSProperties = {
 
 const AlertBody: React.FC = () => {
     const intl = useIntl()
-    const AlertMessage = intl.formatMessage({ id: 'miniapps.billing.AlreadyConnected.message' })
-    const AlertButtonMessage = intl.formatMessage({ id: 'miniapps.billing.AlreadyConnected.button' })
+    const CompanyName = intl.formatMessage({ id: 'CompanyName' })
+    const BillingSectionMessage = intl.formatMessage({ id: 'menu.Billing' })
+    const AlertMessage = intl.formatMessage({ id: 'miniapps.billing.AlreadyConnected.message' }, {
+        company: CompanyName,
+        email: SUPPORT_EMAIL,
+    })
+    const AlertButtonMessage = intl.formatMessage({ id: 'miniapps.billing.AlreadyConnected.button' }, {
+        section: BillingSectionMessage,
+    })
 
     const router = useRouter()
     const handleClick = useCallback(() => {
