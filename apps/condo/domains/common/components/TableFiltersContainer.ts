ce0bd2528946e510969f2d6e0b2b144fc0741c13
Vasiliy Kislov <wkislov0@gmail.com>
2022-06-24 15:04:28 +0500

    DOMA-3263 Removed stroke on ticket page in TableFiltersContainer.

--- apps/condo/domains/common/components/TableFiltersContainer.ts
+++ apps/condo/domains/common/components/TableFiltersContainer.ts
@@ -3,7 +3,6 @@ import { colors, DEFAULT_BORDER_RADIUS } from '@condo/domains/common/constants/s
 
 export const TableFiltersContainer = styled.div`
   max-width: calc(100% + 48px);
-  border: 1px solid ${colors.lightGrey[5]};
   border-radius: ${DEFAULT_BORDER_RADIUS};
   padding: 16px;
   background-color: ${colors.backgroundLightGrey};
