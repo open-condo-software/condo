0fd73c0e5979035a6b7787f0a41f20127b5d015a
Pahaz <pahaz.white@gmail.com>
2022-06-25 16:08:35 +0300

    DOMA-3366 @core/keystone/tasks: use getRedisClient from redis.js

--- packages/@core.keystone/tasks.js
+++ packages/@core.keystone/tasks.js
@@ -2,25 +2,34 @@ const Queue = require('bull')
 const falsey = require('falsey')
 const pino = require('pino')
 const { serializeError } = require('serialize-error')
-const IORedis = require('ioredis')
 
 const conf = require('@core/config')
+
 const { prepareKeystoneExpressApp, getRandomString } = require('./test.utils')
+const { getRedisClient } = require('./redis')
 
 const TASK_TYPE = 'TASK'
 const WORKER_CONCURRENCY = parseInt(conf.WORKER_CONCURRENCY || '2')
 
 // NOTE: If this is True, all tasks will be executed in the node process with setTimeout.
 const FAKE_WORKER_MODE = conf.TESTS_FAKE_WORKER_MODE
-const WORKER_REDIS_URL = conf.WORKER_REDIS_URL || conf.REDIS_URL
-if (!WORKER_REDIS_URL) throw new Error('No WORKER_REDIS_URL environment')
-const taskQueue = new Queue('tasks', WORKER_REDIS_URL, {
-    createClient: (type, opts) => {
-        return new IORedis(opts)
+const taskQueue = new Queue('tasks', {
+    /**
+     * @param {'client' | 'subscriber'} type
+     * @return {import('ioredis')}
+     */
+    createClient: (type) => {
+        if (type === 'client') {
+            return getRedisClient('worker', 'regular')
+        } else if (type === 'subscriber') {
+            return getRedisClient('worker', 'subscriber')
+        } else {
+            throw new Error(`Queue.createClient() unexpected type = ${type}`)
+        }
     },
 })
-// NOTE: same as keystone logger
-const taskLogger = pino({ name: 'worker', enabled: falsey(process.env.DISABLE_LOGGING) })
+
+const taskLogger = pino({ name: 'worker', enabled: falsey(conf.DISABLE_LOGGING) })
 const globalTaskOptions = {}
 const TASKS = new Map()
 const CRON_TASKS = new Map()
