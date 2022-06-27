9543fce8dd4f24d1b13323de10cd0eaee1d97d52
Anton Aleshin <Anton_Aleschin@mail.ru>
2022-06-27 13:36:08 +0300

    DOMA-3099 abstract translation for tasks progress tracking
    Domain-agnostic translation for tasks progress tracking
    Rebase after @sitozzz has copypasted all created files in this commit.

--- apps/condo/domains/common/components/TaskLauncher.tsx
+++ apps/condo/domains/common/components/TaskLauncher.tsx
@@ -1,63 +1,60 @@
 import { DatabaseFilled } from '@ant-design/icons'
 import ActionBar from '@condo/domains/common/components/ActionBar'
 import { Button } from '@condo/domains/common/components/Button'
-import { useLazyQuery } from '@core/next/apollo'
-import { Form, notification } from 'antd'
-import { DocumentNode } from 'graphql'
-import { get } from 'lodash'
-import React, { useCallback, useContext } from 'react'
-import { TasksContext } from './TasksContextProvider'
+import { Form } from 'antd'
+import React, { useCallback, useContext, useState } from 'react'
+import { TasksContext, TaskProgressTranslations } from './TasksContextProvider'
 import { OnCompleteFunc } from './TasksContextProvider'
 
 interface ITaskLauncherProps {
     label: string
-    launchTaskMutation: DocumentNode
-    taskClientSchema: any,
-    buildMutationVariables: () => any
+    taskClientSchema: any
+    attrs: any
+    translations: TaskProgressTranslations
     onComplete: OnCompleteFunc
     disabled?: boolean
     hidden?: boolean
 }
 
+/**
+ * Launches specified task by creating it's record with `useCreate`
+ * and adds it's progress representation using ITasksContext interface
+ * @param props
+ * @constructor
+ */
 export const TaskLauncher: React.FC<ITaskLauncherProps> = (props) => {
     const {
         label,
         taskClientSchema,
-        buildMutationVariables,
+        attrs,
+        translations,
         onComplete,
-        launchTaskMutation,
         hidden = false,
         disabled = false,
     } = props
 
+    const [loading, setLoading] = useState(false)
+
+    // TODO(antonal): load in-progress tasks and set loading state
     // @ts-ignore
     const { addTask } = useContext(TasksContext.Consumer)
 
-    const [
-        launchTask,
-        { loading },
-    ] = useLazyQuery(
-        launchTaskMutation,
-        {
-            onError: error => {
-                const message = get(error, ['graphQLErrors', 0, 'extensions', 'messageForUser']) || error.message
-                notification.error({ message })
+    const launchTask = taskClientSchema.useCreate(attrs, record => {
+        setLoading(true)
+        addTask({
+            record,
+            clientSchema: taskClientSchema,
+            translations,
+            onComplete: (result) => {
+                setLoading(false)
+                onComplete(result)
             },
-            onCompleted: data => {
-                addTask({
-                    record: data.task,
-                    clientSchema: taskClientSchema,
-                    onComplete,
-                })
-            },
-        },
-    )
-
-    const data = buildMutationVariables()
-
+        })
+    })
+    
     const handleClick = useCallback(() => {
-        launchTask({ variables: { data } })
-    }, [launchTask, data])
+        launchTask()
+    }, [launchTask])
 
     return (
         <Form.Item noStyle>
