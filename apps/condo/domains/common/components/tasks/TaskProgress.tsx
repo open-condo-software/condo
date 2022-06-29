3a8be99c48267018332af85b9dacc45b23004e61
Anton Aleshin <Anton_Aleschin@mail.ru>
2022-06-29 15:21:30 +0300

    DOMA-3099 splitted TaskProgress to tracker and visual component with styling

--- apps/condo/domains/common/components/tasks/TaskProgress.tsx
+++ apps/condo/domains/common/components/tasks/TaskProgress.tsx
@@ -1,271 +1,95 @@
-/** @jsx jsx */
-import React, { useEffect, useState, useCallback, useContext, useMemo } from 'react'
-import { css, jsx } from '@emotion/react'
+import React, { useEffect } from 'react'
+import { notification, Progress, Typography, List } from 'antd'
+import { TASK_STATUS_REFRESH_POLL_INTERVAL, WORKER_TASK_COMPLETED } from '../../constants/worker'
+import { colors, fontSizes } from '../../constants/style'
+import { IClientSchema, OnCompleteFunc, TaskRecord, Task, TaskProgressTranslations } from './index'
 import styled from '@emotion/styled'
-import { MinusOutlined } from '@ant-design/icons'
-import { InfoCircleOutlined } from '@ant-design/icons'
-import { useIntl } from '@core/next/intl'
-import { Progress, Typography, List, Row, Col } from 'antd'
-import isFunction from 'lodash/isFunction'
-import { TASK_COMPLETED_STATUS } from '@condo/domains/common/constants/tasks'
-import { colors } from '@condo/domains/common/constants/style'
-import {
-    OnCompleteFunc,
-    TaskRecord,
-    ITaskTrackableItem,
-    TaskProgressTranslations,
-    CalculateProgressFunc,
-    TaskRecordProgress, TASK_PROGRESS_UNKNOWN, ITasksStorage, TasksContext,
-} from './index'
-import { CheckIcon } from '@condo/domains/common/components/icons/Check'
-import { ExpandIcon } from '@condo/domains/common/components/icons/ExpandIcon'
-import { CrossIcon } from '@condo/domains/common/components/icons/CrossIcon'
-
-const InfiniteSpinningStyle = css`
-  @keyframes rotate {
-    100% {
-        transform: rotate(1turn);
-    }
-  }
-  .ant-progress-circle {
-    animation: rotate 1.6s linear infinite;
-  }
-`
-const PROGRESS_STROKE_COLOR = {
-    '0%': '#4CD174',
-    '100%': '#6DB8F2',
-}
-
-type ICircularProgressProps = {
-    progress: TaskRecordProgress,
-}
-
-/**
- * Gradient-colored circle that can be displayed in two states depending on value of a `progress` prop:
- * 1. Known progress displaying as partially filled circle
- * 2. Unknown progress displaying as "fully" filled spinning circle
- * "Fully" filling is implemented as 99 percents to prevent displaying Ant `Progress` component in completed state
- */
-export const CircularProgress = ({ progress }: ICircularProgressProps) => (
-    <Progress
-        type="circle"
-        width={22}
-        strokeWidth={14}
-        strokeColor={PROGRESS_STROKE_COLOR}
-        strokeLinecap="square"
-        showInfo={false}
-        percent={progress === TASK_PROGRESS_UNKNOWN ? 99 : progress}
-        css={progress === TASK_PROGRESS_UNKNOWN ? InfiniteSpinningStyle : {}}
-    />
-)
 
 interface ITaskProgressProps {
     task: TaskRecord
     translations: TaskProgressTranslations
-    progress: TaskRecordProgress
-    removeTask: () => void
 }
 
-const TaskProgressDoneHolder = styled.div`
-    cursor: pointer;
-  
-    .anticon:first-child {
-      display: block;
-    }
-    .anticon:last-child {
-      display: none;
-    }
-  
-    &:hover {
-      .anticon:first-child {
-        display: none;
-      }
-      .anticon:last-child {
-        display: block;
-      }
-    }
-`
-
-const TaskProgressDoneIcon = ({ onClick }) => (
-    <TaskProgressDoneHolder onClick={onClick}>
-        <CheckIcon />
-        <CrossIcon />
-    </TaskProgressDoneHolder>
-)
-
-
-/**
- * Displays task as an item in tasks list on panel
- */
-export const TaskProgress = ({ task, translations, progress, removeTask }: ITaskProgressProps) => (
+export const TaskProgress = ({ task, translations }: ITaskProgressProps) => (
     <List.Item>
         <List.Item.Meta
-            title={
-                <Typography.Text strong>
-                    {translations.title(task)}
-                </Typography.Text>
-            }
+            title={translations.title}
             description={translations.description(task)}
         />
-        {task.status === TASK_COMPLETED_STATUS ? (
-            <TaskProgressDoneIcon onClick={removeTask}/>
-        ) : (
-            <CircularProgress progress={progress}/>
-        )}
+        <Progress
+            type="circle"
+            percent={33}
+            width={22}
+            strokeWidth={14}
+            strokeColor={{
+                '0%': '#4CD174',
+                '100%': '#6DB8F2',
+            }}
+            strokeLinecap="square"
+            showInfo={false}
+        />
     </List.Item>
 )
 
-
 interface ITaskProgressTrackerProps {
     task: TaskRecord
-    storage: ITasksStorage
-    calculateProgress: CalculateProgressFunc
-    onComplete?: OnCompleteFunc
+    clientSchema: IClientSchema
+    onComplete: OnCompleteFunc
     translations: TaskProgressTranslations
 }
 
-// Prevents calling handle function multiple times when tracking component will be remounted.
-// Component will be remounted because recommended technique of updating existing notification by Ant is used,
-// that replaces the current instance to new one.
-const handledCompletedStatesOfTasksIds = []
-
-/**
- * Polls tasks record for updates and handles its transition to completed status.
- */
-export const TaskProgressTracker: React.FC<ITaskProgressTrackerProps> = ({ task, storage, calculateProgress, onComplete, translations }) => {
-    const { id } = task
-
-    const { record, stopPolling } = storage.useTask(id)
-    const { deleteTask } = useContext(TasksContext)
-
-    const removeTaskCallback = useCallback(() => {
-        const removeTask = storage.useDeleteTask({}, () => {
-            deleteTask(record)
-        })
-        removeTask(record)
-    }, [record, storage, deleteTask])
+export const TaskProgressTracker = ({ task/*: { id }*/, clientSchema, onComplete, translations }: ITaskProgressTrackerProps) => {
+    // const { obj: task } = clientSchema.useObject({
+    //     where: { id },
+    // }, {
+    //     pollInterval: TASK_STATUS_REFRESH_POLL_INTERVAL,
+    // })
 
     useEffect(() => {
-        if (!record) {
-            console.error('Could not fetch status of task', task)
-            return
+        if (task.status === WORKER_TASK_COMPLETED) {
+            onComplete(task)
         }
-        if (record.status === TASK_COMPLETED_STATUS) {
-            stopPolling()
-            if (isFunction(onComplete) && !handledCompletedStatesOfTasksIds.includes(record.id)) {
-                handledCompletedStatesOfTasksIds.push(record.id)
-                onComplete(record)
-            }
-        }
-    }, [record])
-
-    if (!record) {
-        return null
-    }
+    }, [task.status, task.progress])
 
     return (
-        <TaskProgress
-            task={record}
-            progress={calculateProgress(record)}
-            translations={translations}
-            removeTask={removeTaskCallback}
-        />
-    )
-}
-
-const VisibilityControlButton = styled.div`
-  position: absolute;
-  right: 20px;
-  top: 20px;
-  z-index: 1;
-  cursor: pointer;
-`
-
-const infoIconStyles = css`
-  color: ${colors.infoIconColor};
-  font-size: 20px;
-`
-
-interface ITasksProgressProps {
-    tasks: ITaskTrackableItem[]
-}
-
-const PARAGRAPH_DESCRIPTION_STYLE: React.CSSProperties = { color: colors.textSecondary }
-
-/**
- * Summary for all current tasks that has the same layout as `notification` from Ant (with icon, title and description).
- * Can be collapsed or expanded
- */
-export const TasksProgress = ({ tasks }: ITasksProgressProps) => {
-    const intl = useIntl()
-    const TitleMsg = intl.formatMessage({ id: 'tasks.progressNotification.title' })
-    const DescriptionMsg = intl.formatMessage({ id: 'tasks.progressNotification.description' })
-
-    const [collapsed, setCollapsed] = useState(false)
-
-    const toggleCollapsed = useCallback(() => {
-        setCollapsed(!collapsed)
-    }, [collapsed])
-
-    return (
-        <div>
-            <VisibilityControlButton
-                onClick={toggleCollapsed}
-            >
-                {collapsed ? (
-                    <ExpandIcon/>
-                ) : (
-                    <MinusOutlined/>
-                )}
-            </VisibilityControlButton>
-            <Row>
-                <Col span={2}>
-                    <InfoCircleOutlined css={infoIconStyles}/>
-                </Col>
-                <Col span={22}>
-                    <Typography.Paragraph strong>
-                        {TitleMsg}
-                    </Typography.Paragraph>
-                    {!collapsed && (
-                        <Typography.Paragraph style={PARAGRAPH_DESCRIPTION_STYLE}>
-                            {DescriptionMsg}
-                        </Typography.Paragraph>
-                    )}
-                    <List
-                        style={{ display: collapsed ? 'none' : 'block' }}
-                        dataSource={tasks}
-                        renderItem={({ record, storage, translations, calculateProgress, onComplete }) => (
-                            <TaskProgressTracker
-                                key={record.id}
-                                task={record}
-                                storage={storage}
-                                translations={translations}
-                                calculateProgress={calculateProgress}
-                                onComplete={onComplete}
-                            />
-                        )}
-                    />
-                </Col>
-            </Row>
-        </div>
+        <TaskProgress task={task} translations={translations}/>
     )
 }
 
 interface IDisplayTasksProgressArgs {
-    notificationApi: any
-    tasks: ITaskTrackableItem[]
+    title: string
+    description: string
+    tasks: Task[]
 }
 
-/**
- * Displays progress for all tasks using one panel
- */
-export const displayTasksProgress = ({ notificationApi, tasks }: IDisplayTasksProgressArgs) => {
-    notificationApi.open({
-        key: 'tasks',
-        className: 'tasks',
+// Displays progress for all tasks in one panel
+export const displayTasksProgress = ({ title, description, tasks }: IDisplayTasksProgressArgs) => {
+    notification.open({
+        key: 'task-notification',
+        message: (
+            <Typography.Paragraph strong>
+                {title}
+            </Typography.Paragraph>
+        ),
         duration: 0,
         description: (
-            <TasksProgress tasks={tasks}/>
+            <>
+                <Typography.Paragraph style={{ color: colors.textSecondary }}>
+                    {description}
+                </Typography.Paragraph>
+                <List
+                    dataSource={tasks}
+                    renderItem={({ record, clientSchema, translations, onComplete }) => (
+                        <TaskProgressTracker
+                            key={record.id}
+                            task={record}
+                            clientSchema={clientSchema}
+                            translations={translations}
+                            onComplete={onComplete}
+                        />
+                    )}
+                />
+            </>
         ),
     })
 }
