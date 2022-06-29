0b2113f0bea0db05ca9add8cb458d8f24e18190e
AleX83Xpert <AleX83Xpert@mail.ru>
2022-06-29 10:30:53 +0500

    DOMA-3188 Rename adapter class (for oidc `Client` models)

--- apps/condo/domains/user/oidc/adapter/AdapterFactory.js
+++ apps/condo/domains/user/oidc/adapter/AdapterFactory.js
@@ -1,15 +1,15 @@
 const { RedisAdapter } = require('./RedisAdapter')
-const { GQLAdapter } = require('./GQLAdapter')
+const { OidcModelClientAdapter } = require('./OidcModelClientAdapter')
 
 function createAdapterClass (context = null) {
     return class AdapterFactory {
         constructor (name) {
-            //Store Client inside keystone gql and others models inside Redis!
+            // Store Client inside keystone gql and others models inside Redis!
             if (name === 'Client') {
                 if (!context) {
-                    throw new Error('The context is mandatory for GQLAdapter')
+                    throw new Error('The context is mandatory for OidcModelClientAdapter')
                 }
-                return new GQLAdapter(name, context)
+                return new OidcModelClientAdapter(name, context)
             }
             return new RedisAdapter(name)
         }
