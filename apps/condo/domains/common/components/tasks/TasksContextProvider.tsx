5544fe1736791a60439797b009b722183a5dc4f7
Anton Aleshin <Anton_Aleschin@mail.ru>
2022-07-04 17:15:52 +0300

    DOMA-3099 replaced custom notification component `TasksProgress` to using Ant `notificaiton`
    Now a notification from Ant can be redisplayed in normal and minimized mode using it's recommented technique by calling `notification.open` with the same `key` argument, but with different other arguments like `description`.
    A guard array `handledCompletedStatesOfTasksIds` is added to prevent calling handle function multiple times when tracking component will be remounted.

--- apps/condo/domains/common/components/tasks/TasksContextProvider.tsx
+++ apps/condo/domains/common/components/tasks/TasksContextProvider.tsx
@@ -3,6 +3,7 @@ import find from 'lodash/find'
 import { useIntl } from '@core/next/intl'
 import { Task } from './index'
 import { displayTasksProgress, TasksProgress } from './TaskProgress'
+import { notification } from 'antd'
 
 /**
  * Should be used to launch and track specific delayed task
@@ -21,9 +22,14 @@ const TasksContext = React.createContext({})
 const TasksContextProvider = ({ children }) => {
     const [tasks, setTasks] = useState<Task[]>([])
     const [tasksProgressVisible, setTasksProgressVisible] = useState(false)
+    const [notificationApi, contextHolder] = notification.useNotification()
+    const intl = useIntl()
+    const TitleMsg = intl.formatMessage({ id: 'tasks.progressNotification.title' })
+    const DescriptionMsg = intl.formatMessage({ id: 'tasks.progressNotification.description' })
 
     const tasksContextInterface: ITasksContext = {
         addTask: (newTask) => {
+
             if (find(tasks, { id: newTask.record.id })) {
                 console.error('Task record already added for tracking', newTask.record)
             } else {
@@ -34,11 +40,33 @@ const TasksContextProvider = ({ children }) => {
         tasks,
     }
 
+    useEffect(() => {
+        // TODO(antonal): remove this example notification, added for layout testing
+        notificationApi.open({
+            message: 'Lorem ipsum',
+            description: 'Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.',
+            duration: 0,
+        })
+        displayTasksProgress({
+            notificationApi,
+            title: TitleMsg,
+            description: DescriptionMsg,
+            tasks,
+        })
+        // TODO(antonal): remove this example notification, added for layout testing
+        notificationApi.open({
+            message: 'Nullam quis',
+            description: 'Cras justo odio, dapibus ac facilisis in, egestas eget quam.',
+            duration: 0,
+        })
+    }, [tasks])
+
     return (
         <TasksContext.Provider value={tasksContextInterface}>
-            {tasksProgressVisible && (
-                <TasksProgress tasks={tasks}/>
-            )}
+            {/*{tasksProgressVisible && (*/}
+            {/*    <TasksProgress tasks={tasks}/>*/}
+            {/*)}*/}
+            {contextHolder}
             {children}
         </TasksContext.Provider>
     )
