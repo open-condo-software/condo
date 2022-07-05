2e6a3b84f3e8b0a370b504279057e0c458f74703
akarjakin <akarjakin@doma.ai>
2022-07-05 16:53:37 +0500

    DOMA-3260 rolled back wydr params

--- apps/condo/domains/common/components/wdyr.ts
+++ apps/condo/domains/common/components/wdyr.ts
@@ -4,5 +4,5 @@ import React from 'react'
 if (typeof window !== 'undefined' && process.env.NODE_ENV === 'development') {
     const whyDidYouRender = require('@welldone-software/why-did-you-render')
 
-    whyDidYouRender(React, { trackAllPureComponents: true })
+    whyDidYouRender(React, { logOnDifferentValues: true })
 }
