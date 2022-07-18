a5ed2fb961b010fa1ef6343418950f17e05f4a10
AleX83Xpert <AleX83Xpert@mail.ru>
2022-07-18 12:44:14 +0500

    DOMA-3372 Hook for contact roles columns

---
+++ apps/condo/domains/contact/hooks/useContactRolesTableColumns.tsx
@@ -0,0 +1,38 @@
+import { getFilterIcon } from '@condo/domains/common/components/TableFilter'
+import { FiltersMeta, getFilterDropdownByKey } from '@condo/domains/common/utils/filters.utils'
+import { getFilteredValue } from '@condo/domains/common/utils/helpers'
+import { parseQuery } from '@condo/domains/common/utils/tables.utils'
+import { IFilters } from '@condo/domains/contact/utils/helpers'
+import { useIntl } from '@core/next/intl'
+import { useRouter } from 'next/router'
+import { useMemo } from 'react'
+
+export function useContactRolesTableColumns<T> (filterMetas: Array<FiltersMeta<T>>): Array<Record<string, unknown>> {
+    const intl = useIntl()
+    const idMessage = intl.formatMessage({ id: 'ContactRoles.id' })
+    const nameMessage = intl.formatMessage({ id: 'ContactRoles.name' })
+
+    const router = useRouter()
+    const { filters } = parseQuery(router.query)
+
+    return useMemo(() => {
+        return [
+            {
+                title: idMessage,
+                ellipsis: true,
+                dataIndex: 'id',
+                key: 'id',
+            },
+            {
+                title: nameMessage,
+                filteredValue: getFilteredValue<IFilters>(filters, 'name'),
+                dataIndex: 'name',
+                key: 'name',
+                sorter: true,
+                filterDropdown: getFilterDropdownByKey(filterMetas, 'name'),
+                filterIcon: getFilterIcon,
+                ellipsis: true,
+            },
+        ]
+    }, [idMessage, nameMessage, filters, filterMetas])
+}
