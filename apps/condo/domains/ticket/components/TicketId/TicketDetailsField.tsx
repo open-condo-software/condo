99f8a26297c0cbfba5adb936bbc849f0110a1a56
Vasiliy Kislov <wkislov0@gmail.com>
2022-07-20 13:07:17 +0500

    DOMA-3541 fix display of details text on the ticket page

--- apps/condo/domains/ticket/components/TicketId/TicketDetailsField.tsx
+++ apps/condo/domains/ticket/components/TicketId/TicketDetailsField.tsx
@@ -3,6 +3,8 @@ import { useIntl } from '@condo/next/intl'
 import { Ticket } from '@app/condo/schema'
 
 import { PageFieldRow } from '@condo/domains/common/components/PageFieldRow'
+import { colors } from '../../../common/constants/style'
+import { Typography } from 'antd'
 
 type TicketDetailsFieldProps = {
     ticket: Ticket
@@ -11,10 +13,18 @@ type TicketDetailsFieldProps = {
 export const TicketDetailsField: React.FC<TicketDetailsFieldProps> = ({ ticket }) => {
     const intl = useIntl()
     const TicketDetailsMessage = intl.formatMessage({ id: 'Problem' })
-
     return (
         <PageFieldRow title={TicketDetailsMessage} ellipsis>
-            {ticket.details}
+            <pre style={{ 
+                background: `${colors.white}`, 
+                border:'none', 
+                margin:'0px', 
+                padding:'0px', 
+                fontFamily:'inherit' }}>
+                <Typography.Text style={{ fontSize: '16px' }}>
+                    {ticket.details}
+                </Typography.Text>
+            </pre>
         </PageFieldRow>
     )
 }
