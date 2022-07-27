6a7bad1756503e490c2ee41dc45e3b1321263452
Vasiliy Kislov <wkislov0@gmail.com>
2022-07-27 13:43:34 +0500

    DOMA-2308 apply dvAndSender plugin to TicketSource

--- apps/condo/domains/ticket/schema/TicketSource.js
+++ apps/condo/domains/ticket/schema/TicketSource.js
@@ -4,10 +4,10 @@ const LocalizedText = require('@core/keystone/fields/LocalizedText')
 const { GQLListSchema } = require('@core/keystone/schema')
 const { historical, versioned, uuided, tracked, softDeleted } = require('@core/keystone/plugins')
 
-const { SENDER_FIELD, DV_FIELD } = require('@condo/domains/common/schema/fields')
 const { COMMON_AND_ORGANIZATION_OWNED_FIELD } = require('@condo/domains/organization/schema/fields')
 
 const { TICKET_SOURCE_TYPES } = require('@condo/domains/ticket/constants/common')
+const { dvAndSender } = require('../../common/schema/plugins/dvAndSender')
 
 const READ_ONLY_ACCESS = {
     read: true,
@@ -20,9 +20,6 @@ const READ_ONLY_ACCESS = {
 const TicketSource = new GQLListSchema('TicketSource', {
     schemaDoc: 'Ticket source. Income call, mobile app, external system, ...',
     fields: {
-        dv: DV_FIELD,
-        sender: SENDER_FIELD,
-
         organization: COMMON_AND_ORGANIZATION_OWNED_FIELD,
 
         type: {
@@ -39,7 +36,7 @@ const TicketSource = new GQLListSchema('TicketSource', {
         },
 
     },
-    plugins: [uuided(), versioned(), tracked(), softDeleted(), historical()],
+    plugins: [uuided(), versioned(), tracked(), softDeleted(), historical(), dvAndSender()],
     // TODO(Dimitreee):use access check from access.js
     access: READ_ONLY_ACCESS,
     escapeSearch: true,
