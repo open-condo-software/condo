71af52ba578c051a02c0dc71ab9b8506eab89aa8
Vasiliy Kislov <wkislov0@gmail.com>
2022-07-27 12:36:22 +0500

    DOMA-2308 DOMA-2308 apply dvAndSender plugin to Contact

--- apps/condo/domains/contact/schema/Contact.js
+++ apps/condo/domains/contact/schema/Contact.js
@@ -5,12 +5,13 @@
 const { Text, Relationship } = require('@keystonejs/fields')
 const { GQLListSchema, find } = require('@core/keystone/schema')
 const { historical, versioned, uuided, tracked, softDeleted } = require('@core/keystone/plugins')
-const { SENDER_FIELD, DV_FIELD, UNIT_TYPE_FIELD } = require('@condo/domains/common/schema/fields')
+const { UNIT_TYPE_FIELD } = require('@condo/domains/common/schema/fields')
 const { ORGANIZATION_OWNED_FIELD } = require('@condo/domains/organization/schema/fields')
 const access = require('@condo/domains/contact/access/Contact')
 const { PHONE_WRONG_FORMAT_ERROR, EMAIL_WRONG_FORMAT_ERROR, PROPERTY_REQUIRED_ERROR } = require('@condo/domains/common/constants/errors')
 const { normalizePhone } = require('@condo/domains/common/utils/phone')
 const { normalizeEmail } = require('@condo/domains/common/utils/mail')
+const { dvAndSender } = require('../../common/schema/plugins/dvAndSender')
 
 /**
  * Composite unique constraint with name `Contact_uniq` is declared in a database-level on following set of columns:
@@ -19,9 +20,6 @@ const { normalizeEmail } = require('@condo/domains/common/utils/mail')
 const Contact = new GQLListSchema('Contact', {
     schemaDoc: 'Contact information of a person. Currently it will be related to a ticket, but in the future, it will be associated with more things',
     fields: {
-        dv: DV_FIELD,
-        sender: SENDER_FIELD,
-
         organization: ORGANIZATION_OWNED_FIELD,
 
         property: {
@@ -137,7 +135,7 @@ const Contact = new GQLListSchema('Contact', {
             }
         },
     },
-    plugins: [uuided(), versioned(), tracked(), softDeleted(), historical()],
+    plugins: [uuided(), versioned(), tracked(), softDeleted(), historical(), dvAndSender()],
     access: {
         read: access.canReadContacts,
         create: access.canManageContacts,
