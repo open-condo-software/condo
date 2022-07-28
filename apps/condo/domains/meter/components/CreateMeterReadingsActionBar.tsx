7f5de9ef705874f4e223ad2c273a5bc5da3ad3bd
Alllex202 <alllex.202@gmail.com>
2022-07-28 13:59:41 +0500

    DOMA-2932 refactoring after review

--- apps/condo/domains/meter/components/CreateMeterReadingsActionBar.tsx
+++ apps/condo/domains/meter/components/CreateMeterReadingsActionBar.tsx
@@ -1,6 +1,6 @@
-import React, { useCallback } from 'react'
+import React, { CSSProperties } from 'react'
 import { useIntl } from '@core/next/intl'
-import { Col, Form, Row, Space } from 'antd'
+import { Col, Form, Row } from 'antd'
 import { isEmpty } from 'lodash'
 import { PlusCircleFilled } from '@ant-design/icons'
 
@@ -8,7 +8,10 @@ import ActionBar from '@condo/domains/common/components/ActionBar'
 import { Button } from '@condo/domains/common/components/Button'
 
 import { ErrorsContainer } from './ErrorsContainer'
+import { Gutter } from 'antd/es/grid/row'
 
+const BUTTON_STYLE: CSSProperties = { marginRight: '12px' }
+const VERTICAL_GUTTER: [Gutter, Gutter] = [0, 24]
 const PROPERTY_DEPENDENCY = ['property']
 const handleShouldUpdate = (prev, next) => prev.unitName !== next.unitName
 
@@ -37,14 +40,14 @@ export const CreateMeterReadingsActionBar = ({
                     return (
                         <ActionBar>
                             <Col>
-                                <Row gutter={[0, 24]}>
+                                <Row gutter={VERTICAL_GUTTER}>
                                     <Button
                                         key='submit'
                                         onClick={handleSave}
                                         type='sberDefaultGradient'
                                         loading={isLoading}
                                         disabled={isSubmitButtonDisabled}
-                                        style={{ marginRight: '12px' }}
+                                        style={BUTTON_STYLE}
                                     >
                                         {SendMetersReadingMessage}
                                     </Button>
@@ -54,7 +57,7 @@ export const CreateMeterReadingsActionBar = ({
                                         disabled={isCreateMeterButtonDisabled}
                                         icon={<PlusCircleFilled/>}
                                         secondary
-                                        style={{ marginRight: '12px' }}
+                                        style={BUTTON_STYLE}
                                     >
                                         {AddMeterMessage}
                                     </Button>
@@ -66,9 +69,6 @@ export const CreateMeterReadingsActionBar = ({
                                     />
                                 </Row>
                             </Col>
-                            <Space size={12}>
-
-                            </Space>
                         </ActionBar>
                     )
                 }
