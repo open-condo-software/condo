643611aa79e0850c36b24bf12e8e835d7b69e3f2
Matthew Savelev <savelevmatthew@gmail.com>
2022-08-01 13:42:00 +0500

    DOMA-3719 Updated docstrings and other refactoring

--- apps/condo/domains/common/hooks/usePrevious.tsx
+++ apps/condo/domains/common/hooks/usePrevious.tsx
@@ -1,9 +1,12 @@
 import { useEffect, useRef } from 'react'
 
 /**
- * Monitoring some variable value
- * After it's change useEffect will update ref
- * But before that will be returned previous value
+ * Monitor some `variable` value
+ * Until `variable` is changed its previous value would be returned
+ * Changing the value of a variable will trigger an async ref change via useEffect
+ * At this point, the return value will still be the same
+ * This will catch the update state where value !== return value
+ * After useEffect has finished, the input and output value will be the same again
  * @param value to monitor
  */
 export function usePrevious<T> (value: T): T {
