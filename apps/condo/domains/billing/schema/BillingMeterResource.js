23214fee594119adc62d688284f70232a6959c8e
Vasiliy Kislov <64303474+VKislov@users.noreply.github.com>
2022-08-01 20:54:34 +0500

    Update BillingMeterResource.js

--- apps/condo/domains/billing/schema/BillingMeterResource.js
+++ apps/condo/domains/billing/schema/BillingMeterResource.js
@@ -6,7 +6,7 @@ const { Text } = require('@keystonejs/fields')
 const { GQLListSchema } = require('@core/keystone/schema')
 const { historical, versioned, uuided, tracked, softDeleted } = require('@core/keystone/plugins')
 const access = require('@condo/domains/billing/access/BillingMeterResource')
-const { dvAndSender } = require('../../common/schema/plugins/dvAndSender')
+const { dvAndSender } = require('@condo/domains/common/schema/plugins/dvAndSender')
 
 
 const BillingMeterResource = new GQLListSchema('BillingMeterResource', {
