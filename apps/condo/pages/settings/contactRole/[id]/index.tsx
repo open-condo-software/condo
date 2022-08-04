85d5a7f8c31d7db3279119e1dbbd65c0d0e41eb0
AleX83Xpert <AleX83Xpert@mail.ru>
2022-08-04 11:34:03 +0500

    DOMA-3372 Message for read only roles was added

--- apps/condo/pages/settings/contactRole/[id]/index.tsx
+++ apps/condo/pages/settings/contactRole/[id]/index.tsx
@@ -1,3 +1,4 @@
+import { ExclamationCircleOutlined } from '@ant-design/icons'
 import ActionBar from '@condo/domains/common/components/ActionBar'
 import { Button } from '@condo/domains/common/components/Button'
 import { PageContent, PageWrapper } from '@condo/domains/common/components/containers/BaseLayout'
@@ -12,7 +13,7 @@ import { ContactRole } from '@condo/domains/contact/utils/clientSchema'
 import { OrganizationRequired } from '@condo/domains/organization/components/OrganizationRequired'
 import { useIntl } from '@core/next/intl'
 import { useOrganization } from '@core/next/organization'
-import { Col, Row, Typography } from 'antd'
+import { Alert, Col, Row, Typography } from 'antd'
 import { Gutter } from 'antd/es/grid/row'
 import get from 'lodash/get'
 import Head from 'next/head'
@@ -35,6 +36,8 @@ const TheContactRolePage = (): JSX.Element => {
     const DeleteMessage = intl.formatMessage({ id: 'Delete' })
     const ConfirmDeleteTitle = intl.formatMessage({ id: 'ContactRoles.confirmDeleteTitle' })
     const ConfirmDeleteMessage = intl.formatMessage({ id: 'ContactRoles.confirmDeleteMessage' })
+    const ReadOnlyRoleWarningTitle = intl.formatMessage({ id: 'ContactRoles.readOnlyRoleWarningTitle' })
+    const ReadOnlyRoleWarningMessage = intl.formatMessage({ id: 'ContactRoles.readOnlyRoleWarningMessage' })
 
     const router = useRouter()
     const { link } = useOrganization()
@@ -70,6 +73,20 @@ const TheContactRolePage = (): JSX.Element => {
                         <Col span={24}>
                             <Typography.Title>{contactRoleTitleMessage}</Typography.Title>
                         </Col>
+                        {canManageContactRoles && !get(contactRole, 'organization', null) && (<Col>
+                            <Row gutter={BIG_VERTICAL_GUTTER}>
+                                <Col span={24}>
+                                    <Alert
+                                        icon={<ExclamationCircleOutlined/>}
+                                        message={<strong>{ReadOnlyRoleWarningTitle}</strong>}
+                                        description={ReadOnlyRoleWarningMessage}
+                                        type="warning"
+                                        showIcon
+                                    />
+                                </Col>
+                            </Row>
+                        </Col>)
+                        }
                         <Col span={24}>
                             <Row gutter={MEDIUM_VERTICAL_GUTTER}>
                                 <Col span={24}>
