536304691e7c735286d0940bdfde4ea2333f27ff
Matthew Savelev <savelevmatthew@gmail.com>
2022-08-10 19:12:20 +0500

    DOMA-3753 Initial storybook added

---
+++ packages/ui/.storybook/main.js
@@ -0,0 +1,40 @@
+module.exports = {
+    'stories': [
+        '../stories/*.stories.mdx',
+        '../stories/*.stories.@(ts|tsx)',
+    ],
+    'addons': [
+        '@storybook/addon-links',
+        '@storybook/addon-essentials',
+        '@storybook/addon-interactions',
+    ],
+    'typescript': true,
+    'framework': '@storybook/react',
+    'core': {
+        'builder': '@storybook/builder-webpack5',
+    },
+    'webpackFinal': async (config) => {
+        const rules = config.module.rules.map(rule => {
+            switch (String(rule.test)) {
+                case String(/\.(mjs|tsx?|jsx?)$/):
+                    return {
+                        ...rule,
+                        use: [
+                            ...rule.use,
+                            '@linaria/webpack-loader',
+                        ],
+                    }
+                default:
+                    return rule
+            }
+        })
+
+        return {
+            ...config,
+            module: {
+                ...config.module,
+                rules,
+            },
+        }
+    },
+}
