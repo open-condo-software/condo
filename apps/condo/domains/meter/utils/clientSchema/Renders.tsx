f4a9945b44b43fc073f4637c933a81661cbadbbe
Alllex202 <alllex.202@gmail.com>
2022-08-15 17:58:18 +0500

    DOMA-3775 refactoring after review

--- apps/condo/domains/meter/utils/clientSchema/Renders.tsx
+++ apps/condo/domains/meter/utils/clientSchema/Renders.tsx
@@ -3,7 +3,7 @@ import { TextProps } from 'antd/es/typography/Text'
 import get from 'lodash/get'
 
 import { getTableCellRenderer, RenderReturnType } from '@condo/domains/common/components/Table/Renders'
-import { METER_READING_SOURCE_EXTERNAL_IMPORT_TYPE } from '../../constants/constants'
+import { METER_READING_SOURCE_EXTERNAL_IMPORT_TYPE } from '@condo/domains/meter/constants/constants'
 import { Typography } from 'antd'
 import React from 'react'
 
@@ -28,10 +28,10 @@ export const getUnitRender = (intl, search: FilterValue) => {
 
 export const getResourceRender = (intl, search?: FilterValue | string) => {
     return function render (text, meterReading): RenderReturnType {
+        const AutoMessage = intl.formatMessage({ id: 'pages.condo.meter.AutoPrefix' })
         const value = get(meterReading, ['meter', 'resource', 'name'])
         const isAutomatic = get(meterReading, ['meter', 'isAutomatic'], false)
         const isExternalSource = Boolean(get(meterReading, ['source', 'type']) === METER_READING_SOURCE_EXTERNAL_IMPORT_TYPE)
-        const AutoMessage = intl.formatMessage({ id: 'pages.condo.meter.AutoPrefix' })
 
         const postfix = isAutomatic && isExternalSource ? (
             <Typography.Text type={'warning'}>
