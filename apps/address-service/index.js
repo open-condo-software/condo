0665f59855071edf79cab06fd198c426e76fa88c
AleX83Xpert <AleX83Xpert@mail.ru>
2022-08-16 13:10:40 +0500

    DOMA-3899 Frontend app (nextjs) was removed

--- apps/address-service/index.js
+++ apps/address-service/index.js
@@ -2,12 +2,10 @@ const { Keystone } = require('@keystonejs/keystone')
 const { PasswordAuthStrategy } = require('@keystonejs/auth-password')
 const { GraphQLApp } = require('@keystonejs/app-graphql')
 const { AdminUIApp } = require('@keystonejs/app-admin-ui')
-const { NextApp } = require('@keystonejs/app-next')
 const { createItems } = require('@keystonejs/server-side-graphql-client')
 
 const conf = require('@condo/config')
 const access = require('@condo/keystone/access')
-const { EmptyApp } = require('@condo/keystone/test.utils')
 const { prepareDefaultKeystoneConfig } = require('@condo/keystone/setup.utils')
 const { registerSchemas } = require('@condo/keystone/KSv5v6/v5/registerSchema')
 const { SuggestionKeystoneApp } = require('@address-service/domains/common/utils/services/suggest/SuggestionKeystoneApp')
@@ -35,6 +33,5 @@ module.exports = {
             authStrategy,
         }),
         new SuggestionKeystoneApp(),
-        // conf.NODE_ENV === 'test' ? new EmptyApp() : new NextApp({ dir: '.' }),
     ],
 }
