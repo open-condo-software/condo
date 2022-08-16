fad5718ff698d9c3863733752341c62e61209062
Pahaz <pahaz.white@gmail.com>
2022-08-16 12:32:21 +0500

    DOMA-3916 refactor tasks.js logging

--- packages/keystone/redis.js
+++ packages/keystone/redis.js
@@ -1,11 +1,10 @@
 const IORedis = require('ioredis')
-const pino = require('pino')
-const falsey = require('falsey')
 
 const conf = require('@condo/config')
+const { getLogger } = require('./logging')
 
 const REDIS_CLIENTS = {}
-const redisLogger = pino({ name: 'redis', enabled: falsey(conf.DISABLE_LOGGING) })
+const logger = getLogger('redis')
 
 /**
  * If you really need to use Redis then you should use this function!
@@ -18,7 +17,7 @@ const redisLogger = pino({ name: 'redis', enabled: falsey(conf.DISABLE_LOGGING) 
 function getRedisClient (name = 'default', purpose = 'regular', opts = {}) {
     const clientKey = name + ':' + purpose
 
-    redisLogger.info({ message: 'getRedisClient', name, purpose, opts })
+    logger.info({ msg: 'getRedisClient', clientKey, opts })
 
     if (!name) throw new Error('getRedisClient() without client name')
     if (typeof name !== 'string') throw new Error('getRedisClient() name is not a string')
@@ -28,11 +27,11 @@ function getRedisClient (name = 'default', purpose = 'regular', opts = {}) {
         const redisUrl = conf[redisEnvName] || conf.REDIS_URL
         if (!redisUrl) throw new Error(`No REDIS_URL env! You need to set ${redisEnvName} / REDIS_URL env`)
         const client = new IORedis(redisUrl, { connectionName: clientKey, ...opts })
-        client.on('connect', () => redisLogger.info({ eventName: 'connect', name, purpose }))
-        client.on('close', () => redisLogger.info({ eventName: 'close', name, purpose }))
-        client.on('reconnecting', (waitTime) => redisLogger.info({ eventName: 'reconnecting', name, purpose, waitTime }))
-        client.on('error', (error) => redisLogger.error({ eventName: 'error', name, purpose, error }))
-        client.on('end', () => redisLogger.error({ eventName: 'end', name, purpose }))
+        client.on('connect', () => logger.info({ msg: 'connect', clientKey }))
+        client.on('close', () => logger.info({ msg: 'close', clientKey }))
+        client.on('reconnecting', (waitTime) => logger.info({ msg: 'reconnecting', clientKey, waitTime }))
+        client.on('error', (error) => logger.error({ msg: 'error', clientKey, error }))
+        client.on('end', () => logger.error({ msg: 'end', clientKey }))
         REDIS_CLIENTS[clientKey] = client
     }
 
