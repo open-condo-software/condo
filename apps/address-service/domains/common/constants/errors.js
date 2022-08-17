f01dc88ed3ea60c89b8af47c05f2eae6ee108214
AleX83Xpert <AleX83Xpert@mail.ru>
2022-08-17 09:26:56 +0500

    DOMA-3899 Last version of dvAndSender plugin

--- apps/address-service/domains/common/constants/errors.js
+++ apps/address-service/domains/common/constants/errors.js
@@ -1,9 +1,9 @@
-const WRONG_PASSWORD_ERROR = '[passwordAuth:secret:mismatch'
-const EMPTY_PASSWORD_ERROR = '[passwordAuth:secret:notSet'
-const WRONG_EMAIL_ERROR = '[passwordAuth:identity:notFound'
+const DV_VERSION_MISMATCH = 'DV_VERSION_MISMATCH'
+
+// Provided value does not match the specified format. For example, not matches regexp, string length requirement, etc.
+const WRONG_FORMAT = 'WRONG_FORMAT'
 
 module.exports = {
-    WRONG_PASSWORD_ERROR,
-    EMPTY_PASSWORD_ERROR,
-    WRONG_EMAIL_ERROR,
+    DV_VERSION_MISMATCH,
+    WRONG_FORMAT,
 }
