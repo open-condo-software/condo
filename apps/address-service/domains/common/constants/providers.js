7ddc5e4b6fce355e1b894afaf94ee648566a393a
AleX83Xpert <AleX83Xpert@mail.ru>
2022-08-18 12:13:34 +0500

    DOMA-3899 Constants for providers were added

---
+++ apps/address-service/domains/common/constants/providers.js
@@ -0,0 +1,9 @@
+const DADATA_PROVIDER = 'dadata'
+const GOOGLE_PROVIDER = 'google'
+const YANDEX_PROVIDER = 'yandex'
+
+module.exports = {
+    DADATA_PROVIDER,
+    GOOGLE_PROVIDER,
+    YANDEX_PROVIDER,
+}
