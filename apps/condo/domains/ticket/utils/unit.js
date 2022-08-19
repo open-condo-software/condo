d143120dbd962cac16d974f9ddef090ca69727e7
VKislov <wkislov0@gmail.com>
2022-08-19 20:10:16 +0500

    DOMA-3964 added parking `sectionType` and `sectionName` displaying on ticket page

--- apps/condo/domains/ticket/utils/unit.js
+++ apps/condo/domains/ticket/utils/unit.js
@@ -1,17 +1,19 @@
 const { get } = require('lodash')
 
-function getSectionAndFloorByUnitName (property, unitName) {
+function getSectionAndFloorByUnitName (property, unitName, unitType) {
     const res = {
         sectionName: null,
+        sectionType: null,
         floorName: null,
     }
-    if (unitName) {
-        const sections = get(property, ['map', 'sections'], [])
+    if (unitName && unitType) {
+        const sections = get(property, ['map', `${unitType !== 'parking' ? 'sections' : 'parking'}`], [])
         for (const section of sections) {
             for (const floor of section.floors) {
                 for (const unit of floor.units) {
                     if (unit.label === unitName) {
                         res.sectionName = section.name
+                        res.sectionType = section.type
                         res.floorName = floor.name
                         break
                     }
