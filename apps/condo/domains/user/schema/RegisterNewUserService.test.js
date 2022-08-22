3d8d423563b8e0d6b4c974d626c5007337565dae
Ilya Konik <konick.ilya@yandex.ru>
2022-08-22 16:26:13 +0500

    DOMA-3957 test on notifications added

--- apps/condo/domains/user/schema/RegisterNewUserService.test.js
+++ apps/condo/domains/user/schema/RegisterNewUserService.test.js
@@ -1,10 +1,16 @@
 const faker = require('faker')
 const { createTestUser, registerNewUser, createTestPhone, createTestEmail, createTestLandlineNumber } = require('@condo/domains/user/utils/testSchema')
 const { REGISTER_NEW_USER_MUTATION } = require('@condo/domains/user/gql')
-const { makeLoggedInAdminClient, makeClient } = require('@condo/keystone/test.utils')
+const { makeLoggedInAdminClient, makeClient, makeLoggedInClient, waitFor } = require('@condo/keystone/test.utils')
 const { expectToThrowGQLError } = require('@condo/domains/common/utils/testSchema')
 
 const { errors } = require('./RegisterNewUserService')
+const { Message } = require('@condo/domains/notification/utils/testSchema')
+const { REGISTER_NEW_USER_MESSAGE_TYPE, WELCOME_NEW_USER_MESSAGE_TYPE } = require('../../notification/constants/constants')
+const { i18n } = require('@condo/locales/loader')
+const { translationStringKeyForEmailSubject } = require('@condo/domains/notification/templates')
+const { prepareMessageToSend } = require('@condo/domains/notification/transports/email')
+const conf = require('@condo/config')
 
 describe('RegisterNewUserService', () => {
     test('register new user', async () => {
@@ -135,5 +141,46 @@ describe('RegisterNewUserService', () => {
             errors.UNABLE_TO_FIND_CONFIRM_PHONE_ACTION,
             'user',
         )
+    })
+})
+
+describe('Notifications', () => {
+    test('Create new user sent REGISTER_NEW_USER_MESSAGE_TYPE and WELCOME_NEW_USER_MESSAGE_TYPE messages', async () => {
+        const [user, attrs] = await registerNewUser(await makeClient())
+        const client = await makeLoggedInClient(attrs)
+
+        await waitFor(async () => {
+            const messages = await Message.getAll(client, { user: { id: user.id } })
+            expect(messages).toHaveLength(2)
+            expect(messages[0].processingMeta).not.toBeNull()
+            console.log(JSON.stringify(messages[0].processingMeta, null, 2))
+            // TODO(pahaz): it looks like we need to arise password
+            expect(messages[0]).toMatchObject({
+                type: REGISTER_NEW_USER_MESSAGE_TYPE,
+                meta: {
+                    dv: 1,
+                    userId: user.id,
+                    userName: attrs.name,
+                    userPhone: attrs.phone,
+                    userEmail: attrs.email,
+                    userPassword: attrs.password,
+                },
+            })
+            expect(messages[1]).toMatchObject({
+                type: WELCOME_NEW_USER_MESSAGE_TYPE,
+                meta: {
+                    dv: 1,
+                    userId: user.id,
+                    userName: user.name,
+                    userPhone: attrs.phone,
+                    userEmail: attrs.email,
+                },
+            })
+
+            const expectedSubject = i18n(translationStringKeyForEmailSubject(WELCOME_NEW_USER_MESSAGE_TYPE),
+                { locale: conf.DEFAULT_LOCALE, meta: { userName: attrs.name } })
+            const { subject } = await prepareMessageToSend(messages[1])
+            expect(subject).toMatch(expectedSubject)
+        })
     })
 })
