e23ffeaed62907ab54ceff71f361a2b6e6312681
Sitnikov Ivan <sit7602@gmail.com>
2022-08-24 09:39:01 +0500

    DOMA-3952 review fixes - get page id from query and use shadow string from constants

--- apps/condo/pages/reports/external/[id]/index.tsx
+++ apps/condo/pages/reports/external/[id]/index.tsx
@@ -1,22 +1,27 @@
 import React from 'react'
 import { useRouter } from 'next/router'
 import { Row, Col, Typography } from 'antd'
+import { useIntl } from '@condo/next/intl'
 import { PageContent, PageHeader, PageWrapper } from '@condo/domains/common/components/containers/BaseLayout'
 import { OrganizationRequired } from '@condo/domains/organization/components/OrganizationRequired'
 import { ExternalReport } from '@condo/domains/analytics/utils/clientSchema'
 import { Loader } from '@condo/domains/common/components/Loader'
+import { BasicEmptyListView } from '@condo/domains/common/components/EmptyListView'
 
 const LAYOUT_STYLE: React.CSSProperties = { height: '100%' }
 const IFRAME_STYLE: React.CSSProperties = { border: 'none', width: '100%', height: '100%' }
 
 const ExternalReportDetailPage = () => {
-    const { asPath } = useRouter()
+    const intl = useIntl()
+    const NoDataTitle = intl.formatMessage({ id: 'NoData' })
+
+    const { query: { id } } = useRouter()
 
     const {
         obj: externalReport, loading,
     } = ExternalReport.useObject({
         where: {
-            id: asPath.split('/').pop(),
+            id: id as string,
         },
     }, { fetchPolicy: 'network-only' })
 
@@ -31,10 +36,19 @@ const ExternalReportDetailPage = () => {
                             <PageContent>
                                 <Row style={LAYOUT_STYLE}>
                                     <Col span={24} style={LAYOUT_STYLE}>
-                                        <iframe
-                                            src={externalReport.iframeUrl}
-                                            style={IFRAME_STYLE}
-                                        />
+                                        {!externalReport.iframeUrl
+                                            ? (
+                                                <BasicEmptyListView image='/dino/searching@2x.png'>
+                                                    <Typography.Title level={4}>
+                                                        {NoDataTitle}
+                                                    </Typography.Title>
+                                                </BasicEmptyListView>
+                                            )
+                                            : <iframe
+                                                src={externalReport.iframeUrl}
+                                                style={IFRAME_STYLE}
+                                            />
+                                        }
                                     </Col>
                                 </Row>
                             </PageContent>
