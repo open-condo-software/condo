acf6489e72af0d975f190e20c92adf2ae1db2ec1
Anton Aleshin <Anton_Aleschin@mail.ru>
2022-08-24 19:16:51 +0300

    DOMA-3800 added testing of chunk-by-chunk execution for `exportTickets` worker function

--- apps/condo/domains/ticket/tasks/exportTicketsTask.spec.js
+++ apps/condo/domains/ticket/tasks/exportTicketsTask.spec.js
@@ -10,19 +10,26 @@ const {
 } = require('@condo/domains/organization/utils/testSchema')
 const { createTestProperty } = require('@condo/domains/property/utils/testSchema')
 const { createTestTicket } = require('@condo/domains/ticket/utils/testSchema')
-const { exportTickets } = require('./exportTicketsTask')
+const { exportTicketsWorker } = require('./exportTicketsTask')
 const { createTestTicketExportTask, TicketExportTask } = require('../utils/testSchema')
 const { PROCESSING, EXCEL } = require('@condo/domains/common/constants/export')
 
 const index = require('../../../index')
 const { COMPLETED } = require('@condo/domains/common/constants/export')
 const { catchErrorFrom } = require('@condo/domains/common/utils/testSchema')
+const { EXPORT_PROCESSING_BATCH_SIZE } = require('../../common/constants/export')
+
+const jobMock = {
+    progress: jest.fn().mockImplementation(),
+}
 
 describe('exportTicketsTask', () => {
     describe('exportTickets', () => {
         setFakeClientMode(index)
+        // We need more time to generate tickets to process them chunk-by-chunk
+        jest.setTimeout(60000)
 
-        it('exports tickets selected by filters specified in provided id of ExportTicketsTask record', async () => {
+        it('processes tickets selected by filters specified in provided id of TicketExportTask record chunk-by-chunk', async () => {
             const adminClient = await makeLoggedInAdminClient()
             const userClient = await makeClientWithNewRegisteredAndLoggedInUser()
             const [organization] = await createTestOrganization(adminClient)
@@ -33,7 +40,12 @@ describe('exportTicketsTask', () => {
             })
             await createTestOrganizationEmployee(adminClient, organization, userClient.user, role)
 
-            await createTestTicket(userClient, organization, property)
+            let ticketsCountFor2Chunks = Math.floor(EXPORT_PROCESSING_BATCH_SIZE * 1.5)
+            let i = ticketsCountFor2Chunks
+            do {
+                await createTestTicket(userClient, organization, property)
+            } while (--i > 0)
+
             const [task] = await createTestTicketExportTask(adminClient, userClient.user, {
                 format: EXCEL,
                 where: {
@@ -45,12 +57,15 @@ describe('exportTicketsTask', () => {
                 status: PROCESSING,
             })
 
-            await exportTickets(task.id)
+            // NOTE: when using `apply` we need to send params as an array to avoid an error "TypeError: CreateListFromArrayLike called on non-object"
+            await exportTicketsWorker.apply(jobMock, [task.id])
 
             const updatedTask = await TicketExportTask.getOne(userClient, { id: task.id })
             expect(updatedTask.file).toBeDefined()
             expect(updatedTask.file).not.toBeNull()
-            expect(updatedTask.exportedRecordsCount).toEqual(1)
+            expect(updatedTask.exportedRecordsCount).toEqual(ticketsCountFor2Chunks)
+            expect(updatedTask.totalRecordsCount).toEqual(ticketsCountFor2Chunks)
+            expect(jobMock.progress.mock.calls.length).toEqual(2)
             // TODO(antonal): automatically examine created export file
         })
 
@@ -76,7 +91,8 @@ describe('exportTicketsTask', () => {
             })
 
             await catchErrorFrom(async () => {
-                await exportTickets(task.id)
+                // NOTE: when using `apply` we need to send params as an array to avoid an error "TypeError: CreateListFromArrayLike called on non-object"
+                await exportTicketsWorker.apply(jobMock, [task.id])
             }, async ( e) => {
                 expect(e).toMatchObject({
                     message: `No records to export for TicketExportTask with id "${task.id}"`,
@@ -90,6 +106,7 @@ describe('exportTicketsTask', () => {
                 expect(updatedTask.status).toEqual(COMPLETED)
                 expect(updatedTask.file).toBeNull()
                 expect(updatedTask.exportedRecordsCount).toEqual(0)
+                expect(updatedTask.totalRecordsCount).toEqual(0)
             })
         })
     })
