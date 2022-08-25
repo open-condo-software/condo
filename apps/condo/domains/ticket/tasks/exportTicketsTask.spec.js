0ddd5c84a6c29fa2135599e283114b63a48866a2
Anton Aleshin <Anton_Aleschin@mail.ru>
2022-08-25 17:27:38 +0300

    DOMA-3800 implemented `InProcessFakeJob` to partially mimick Bull Job interface for `FAKE_WORKER_MODE`

--- apps/condo/domains/ticket/tasks/exportTicketsTask.spec.js
+++ apps/condo/domains/ticket/tasks/exportTicketsTask.spec.js
@@ -1,7 +1,7 @@
 /**
  * @jest-environment node
  */
-const { makeLoggedInAdminClient, setFakeClientMode } = require('@condo/keystone/test.utils')
+const { makeLoggedInAdminClient, setFakeClientMode, waitFor } = require('@condo/keystone/test.utils')
 const { makeClientWithNewRegisteredAndLoggedInUser } = require('@condo/domains/user/utils/testSchema')
 const {
     createTestOrganization,
@@ -10,18 +10,10 @@ const {
 } = require('@condo/domains/organization/utils/testSchema')
 const { createTestProperty } = require('@condo/domains/property/utils/testSchema')
 const { createTestTicket } = require('@condo/domains/ticket/utils/testSchema')
-const { exportTicketsWorker } = require('./exportTicketsTask')
 const { createTestTicketExportTask, TicketExportTask } = require('../utils/testSchema')
-const { PROCESSING, EXCEL } = require('@condo/domains/common/constants/export')
+const { PROCESSING, EXCEL, EXPORT_PROCESSING_BATCH_SIZE } = require('@condo/domains/common/constants/export')
 
 const index = require('../../../index')
-const { COMPLETED } = require('@condo/domains/common/constants/export')
-const { catchErrorFrom } = require('@condo/domains/common/utils/testSchema')
-const { EXPORT_PROCESSING_BATCH_SIZE } = require('../../common/constants/export')
-
-const jobMock = {
-    progress: jest.fn().mockImplementation(),
-}
 
 describe('exportTicketsTask', () => {
     describe('exportTickets', () => {
@@ -29,7 +21,7 @@ describe('exportTicketsTask', () => {
         // We need more time to generate tickets to process them chunk-by-chunk
         jest.setTimeout(60000)
 
-        it('processes tickets selected by filters specified in provided id of TicketExportTask record chunk-by-chunk', async () => {
+        it('launches in `TicketExportTask.afterChange` hook on "create" operation and processes tickets selected by filters specified in provided id of TicketExportTask record chunk-by-chunk', async () => {
             const adminClient = await makeLoggedInAdminClient()
             const userClient = await makeClientWithNewRegisteredAndLoggedInUser()
             const [organization] = await createTestOrganization(adminClient)
@@ -57,56 +49,17 @@ describe('exportTicketsTask', () => {
                 status: PROCESSING,
             })
 
-            // NOTE: when using `apply` we need to send params as an array to avoid an error "TypeError: CreateListFromArrayLike called on non-object"
-            await exportTicketsWorker.apply(jobMock, [task.id])
+            // NOTE: This function is launched in `TicketExportTask.afterChange` hook on "create" operation
+            // Don't need to call it explicitly
+            // await exportTicketsWorker.apply(jobMock, [task.id])
 
-            const updatedTask = await TicketExportTask.getOne(userClient, { id: task.id })
-            expect(updatedTask.file).toBeDefined()
-            expect(updatedTask.file).not.toBeNull()
-            expect(updatedTask.exportedRecordsCount).toEqual(ticketsCountFor2Chunks)
-            expect(updatedTask.totalRecordsCount).toEqual(ticketsCountFor2Chunks)
-            expect(jobMock.progress.mock.calls.length).toEqual(2)
-            // TODO(antonal): automatically examine created export file
-        })
-
-        it('throws error if nothing to export and changes task state to "completed" value', async () => {
-            const adminClient = await makeLoggedInAdminClient()
-            const userClient = await makeClientWithNewRegisteredAndLoggedInUser()
-            const [organization] = await createTestOrganization(adminClient)
-            const [role] = await createTestOrganizationEmployeeRole(adminClient, organization, {
-                canManageTickets: true,
-                canManageTicketComments: true,
-            })
-            await createTestOrganizationEmployee(adminClient, organization, userClient.user, role)
-
-            const [task] = await createTestTicketExportTask(adminClient, userClient.user, {
-                format: EXCEL,
-                where: {
-                    organization: { id: organization.id },
-                },
-                sortBy: 'createdAt_ASC',
-                locale: 'ru',
-                timeZone: 'Europe/Moscow',
-                status: PROCESSING,
-            })
-
-            await catchErrorFrom(async () => {
-                // NOTE: when using `apply` we need to send params as an array to avoid an error "TypeError: CreateListFromArrayLike called on non-object"
-                await exportTicketsWorker.apply(jobMock, [task.id])
-            }, async ( e) => {
-                expect(e).toMatchObject({
-                    message: `No records to export for TicketExportTask with id "${task.id}"`,
-                    extensions: {
-                        code: 'BAD_USER_INPUT',
-                        type: 'NOTHING_TO_EXPORT',
-                        messageForUser: 'tasks.export.error.NOTHING_TO_EXPORT',
-                    },
-                })
+            await waitFor(async () => {
                 const updatedTask = await TicketExportTask.getOne(userClient, { id: task.id })
-                expect(updatedTask.status).toEqual(COMPLETED)
-                expect(updatedTask.file).toBeNull()
-                expect(updatedTask.exportedRecordsCount).toEqual(0)
-                expect(updatedTask.totalRecordsCount).toEqual(0)
+                expect(updatedTask.file).toBeDefined()
+                expect(updatedTask.file).not.toBeNull()
+                expect(updatedTask.exportedRecordsCount).toEqual(ticketsCountFor2Chunks)
+                expect(updatedTask.totalRecordsCount).toEqual(ticketsCountFor2Chunks)
+                // TODO(antonal): automatically examine created export file
             })
         })
     })
