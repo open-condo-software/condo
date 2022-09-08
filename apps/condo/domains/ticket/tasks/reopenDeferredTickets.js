c1b4ff5a88dc0cc7cf5f4c9444b6194e534a0a82
Dmitry Kovyazin <dkoviazin@gmail.com>
2022-09-08 14:54:48 +0500

    DOMA-4156 added try catch to reopen deffered tickets

--- apps/condo/domains/ticket/tasks/reopenDeferredTickets.js
+++ apps/condo/domains/ticket/tasks/reopenDeferredTickets.js
@@ -4,8 +4,11 @@ const { STATUS_IDS } = require('@condo/domains/ticket/constants/statusTransition
 const dayjs = require('dayjs')
 const { Ticket } = require('@condo/domains/ticket/utils/serverSchema')
 const { isEmpty } = require('lodash')
-
+const { getLogger } = require('@condo/keystone/logging')
 const CHUNK_SIZE = 50
+
+const appLogger = getLogger('condo')
+const taskLogger = appLogger.child({ module: 'reopenDeferredTickets' })
 
 /**
  * Opens tickets that are in the "deferred" status and the date they are deferring has expired.
@@ -32,15 +35,22 @@ const reopenDeferredTickets = async () => {
         if (isEmpty(ticketsToChange)) break
 
         changedTicketCounter += ticketsToChange.length
-
+        // TODO(DOMA-4155): Update ticket can break on getOrCreateContactByClientData
         for (const ticket of ticketsToChange) {
-            await Ticket.update(adminContext, ticket.id, {
-                dv: 1,
-                sender: { fingerprint: 'auto-reopen', dv: 1 },
-                executor: { disconnectAll: true },
-                assignee: { disconnectAll: true },
-                status: { connect: { id: STATUS_IDS.OPEN } },
-            })
+            try {
+                await Ticket.update(adminContext, ticket.id, {
+                    dv: 1,
+                    sender: { fingerprint: 'auto-reopen', dv: 1 },
+                    executor: { disconnectAll: true },
+                    assignee: { disconnectAll: true },
+                    status: { connect: { id: STATUS_IDS.OPEN } },
+                })
+            } catch (error) {
+                taskLogger.error({
+                    msg: 'Failed to update Ticket',
+                    data: { id: ticket.id },
+                })
+            }
         }
     }
 }
