b4c6f574e1ab197c86c68c8bc8329cdefcb1443c
VKislov <wkislov0@gmail.com>
2022-09-09 15:06:52 +0500

    DOMA-3964 fix unitType&sectionType in resident ticket

--- apps/condo/domains/ticket/utils/unit.js
+++ apps/condo/domains/ticket/utils/unit.js
@@ -1,6 +1,6 @@
 const { get } = require('lodash')
-const { SECTION_SECTION_TYPE, PARKING_UNIT_TYPE } = require('@condo/domains/property/constants/common')
-
+const { SECTION_SECTION_TYPE, PARKING_SECTION_TYPE, PARKING_UNIT_TYPE } = require('@condo/domains/property/constants/common')
+const SECTIONS_TYPE = 'sections'
 function getSectionAndFloorByUnitName (property, unitName, unitType) {
     const res = {
         sectionName: null,
@@ -8,13 +8,13 @@ function getSectionAndFloorByUnitName (property, unitName, unitType) {
         floorName: null,
     }
     if (unitName && unitType) {
-        const sections = get(property, ['map', `${unitType !== PARKING_UNIT_TYPE ? SECTION_SECTION_TYPE : PARKING_UNIT_TYPE}`], [])
+        const sections = get(property, ['map', `${unitType !== PARKING_UNIT_TYPE ? SECTIONS_TYPE : PARKING_UNIT_TYPE}`], [])
         for (const section of sections) {
             for (const floor of section.floors) {
                 for (const unit of floor.units) {
                     if (unit.label === unitName) {
                         res.sectionName = section.name
-                        res.sectionType = unitType !== PARKING_UNIT_TYPE ? 'sections' : 'parking'
+                        res.sectionType = unitType !== PARKING_UNIT_TYPE ? SECTION_SECTION_TYPE : PARKING_SECTION_TYPE
                         res.floorName = floor.name
                         break
                     }
