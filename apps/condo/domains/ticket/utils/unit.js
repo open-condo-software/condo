2a47bf07f8eb18e9c20cad73c917b88ec09342c2
Dmitry Kovyazin <dkoviazin@gmail.com>
2022-09-09 15:17:57 +0500

    Revert "DOMA-3964 added parking `sectionType` and `sectionName` displaying on ticket page"
    This reverts commit d143120d

--- apps/condo/domains/ticket/utils/unit.js
+++ apps/condo/domains/ticket/utils/unit.js
@@ -1,19 +1,17 @@
 const { get } = require('lodash')
 
-function getSectionAndFloorByUnitName (property, unitName, unitType) {
+function getSectionAndFloorByUnitName (property, unitName) {
     const res = {
         sectionName: null,
-        sectionType: null,
         floorName: null,
     }
-    if (unitName && unitType) {
-        const sections = get(property, ['map', `${unitType !== 'parking' ? 'sections' : 'parking'}`], [])
+    if (unitName) {
+        const sections = get(property, ['map', 'sections'], [])
         for (const section of sections) {
             for (const floor of section.floors) {
                 for (const unit of floor.units) {
                     if (unit.label === unitName) {
                         res.sectionName = section.name
-                        res.sectionType = section.type
                         res.floorName = floor.name
                         break
                     }
