5b31e19905ce8a7dbe34c2fee5d6f28a708f6163
AleX83Xpert <AleX83Xpert@mail.ru>
2022-09-15 10:31:38 +0500

    feat(address-service): DOMA-4064 Detect search
    The first version of the search provider detector
    is the same functionality as the suggestions provider detector.

---
+++ apps/address-service/domains/common/utils/services/search/SearchProviderDetector.js
@@ -0,0 +1,32 @@
+const {
+    DadataSearchProvider,
+} = require('@address-service/domains/common/utils/services/search/providers')
+const { DADATA_PROVIDER, GOOGLE_PROVIDER } = require('@address-service/domains/common/constants/providers')
+
+class SearchProviderDetector {
+
+    /**
+     * @param {string} geo
+     * @returns {AbstractSearchProvider}
+     */
+    getProvider (geo) {
+        /** @type {AbstractSearchProvider} */
+        let searchProvider
+        switch (geo) {
+            // In some cases, we need to force choose one of the providers.
+            // To achieve this, we give an ability to pass a particular provider name
+            case DADATA_PROVIDER:
+            case 'ru':
+                searchProvider = new DadataSearchProvider()
+                break
+            case GOOGLE_PROVIDER:
+            default:
+                //TODO(nas) always return dadata provider till the google's one will be ready
+                searchProvider = new DadataSearchProvider()
+        }
+
+        return searchProvider
+    }
+}
+
+module.exports = { SearchProviderDetector }
