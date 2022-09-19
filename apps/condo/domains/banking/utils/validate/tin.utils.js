cb685f5885818d1e335611a917b56ba5366a0c70
maksimdanilov <maxim13danilov@gmail.com>
2022-09-19 12:34:40 +0500

    feat(condo): DOMA-3252 update tin validator for localization

--- apps/condo/domains/banking/utils/validate/tin.utils.js
+++ apps/condo/domains/banking/utils/validate/tin.utils.js
@@ -3,66 +3,56 @@
  *
  * The following checks are performed:
  * 1) Сhecking for emptiness
- * 2) Checking for length and format (Consists of 10 or 12 digits)
  *
  * For RU tin:
  * 1) Сhecking checksum verification for TIN
+ * 2) Checking for length and format (Consists of 10 or 12 digits)
  *
  * Example:
- * TIN with 10 characters - 7791815382
- * TIN with 12 characters - 397144170672
+ * RU TIN with 10 characters - 7791815382
+ * RU TIN with 12 characters - 397144170672
  */
 
 const EMPTY = 'Tin is empty'
 const NOT_NUMERIC = 'Tin can contain only numeric digits'
-const WRONG_LENGTH = 'Tin length was expected to be 10 or 12, but received '
+const WRONG_LENGTH_RU = 'Ru tin length was expected to be 10 or 12, but received '
 const CONTROL_SUM_FAILED = 'Control sum is not valid for tin'
 
-const RU_TIN_DIGITS = [3, 7, 2, 4, 10, 3, 5, 9, 4, 6, 8, 0]
+const RU_TIN_WEIGHTS = [3, 7, 2, 4, 10, 3, 5, 9, 4, 6, 8, 0]
 
 
 function getTinControlSumRU (num) {
-    const weights = RU_TIN_DIGITS.slice(-num.length)
-    let sum = 0
+    const weights = RU_TIN_WEIGHTS.slice(-num.length)
+
+    let controlSum = 0
 
     for (let i = 0; i < num.length; i++)
-        sum += num[i] * weights[i]
+        controlSum += num[i] * weights[i]
 
-    return sum % 11 % 10
+    return controlSum % 11 % 10
 }
 
-class RecipientTinValidation {
+
+class TinValidation {
 
     errors = []
 
-    validateForNumbersAndLength (tin) {
-        if (!tin.length) {
-            this.errors.push(EMPTY)
-        }
+    validateRuTin (tin) {
+        if (!tin.length) this.errors.push(EMPTY)
 
-        if (!/^[0-9]*$/.test(tin)) {
-            this.errors.push(NOT_NUMERIC)
-        }
+        if (!/^[0-9]*$/.test(tin)) this.errors.push(NOT_NUMERIC)
 
-        if (tin.length !== 10 && tin.length !== 12) {
-            this.errors.push(WRONG_LENGTH + tin.length)
-        }
-    }
+        if (tin.length !== 10 && tin.length !== 12) this.errors.push(WRONG_LENGTH_RU + tin.length)
 
 
-    validateTin (tin, country) {
-        const tinWithoutSpaces = tin.toString().trim()
-
-        this.validateForNumbersAndLength(tinWithoutSpaces)
-
-        if (tinWithoutSpaces.length === 10)
-            if (getTinControlSumRU(tinWithoutSpaces) !== Number(tinWithoutSpaces.slice(-1))) {
+        if (tin.length === 10)
+            if (getTinControlSumRU(tin) !== Number(tin.slice(-1))) {
                 this.errors.push(CONTROL_SUM_FAILED)
             }
 
-        if (tinWithoutSpaces.length === 12)
-            if (getTinControlSumRU(tinWithoutSpaces.slice(0, 11)) !== Number(tinWithoutSpaces.slice(10, -1)
-                || getTinControlSumRU(tinWithoutSpaces) !== Number(tinWithoutSpaces.slice(-1)))) {
+        if (tin.length === 12)
+            if (getTinControlSumRU(tin.slice(0, 11)) !== Number(tin.slice(10, -1)
+                || getTinControlSumRU(tin) !== Number(tin.slice(-1)))) {
                 this.errors.push(CONTROL_SUM_FAILED)
             }
 
@@ -71,10 +61,21 @@ class RecipientTinValidation {
             errors: this.errors,
         }
     }
+
+
+    validateTin (tin, country) {
+        const tinWithoutSpaces = tin.toString().trim()
+
+        if (country === 'RU')
+            return this.validateRuTin(tinWithoutSpaces)
+
+        return
+    }
 }
 
 const validateTin = (tin, country) => {
-    const validator = new RecipientTinValidation()
+    const validator = new TinValidation()
+
     const { result, errors } = validator.validateTin(tin, country)
     return { result, errors }
 }
