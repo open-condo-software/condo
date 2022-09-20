fde3ed8d484d053b332a2369f86308e0362cdcf4
AleX83Xpert <AleX83Xpert@mail.ru>
2022-09-20 16:02:25 +0500

    fix(condo): DOMA-4253 Tests were fixed

--- packages/keystone/setup.utils.js
+++ packages/keystone/setup.utils.js
@@ -6,6 +6,8 @@ const IORedis = require('ioredis')
 const session = require('express-session')
 const RedisStore = require('connect-redis')(session)
 
+const HTTPS_REGEXP = /^https:/
+
 function _makeid (length) {
     let result = ''
     const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
@@ -63,7 +65,7 @@ function prepareDefaultKeystoneConfig (conf) {
         cookieSecret: getCookieSecret(conf.COOKIE_SECRET),
         cookie: {
             sameSite: 'none',
-            secure: true,
+            secure: HTTPS_REGEXP.test(conf.SERVER_URL),
             // 1000 * (Math.pow(2, 31) - 1) IS APPROXIMATELY 68 YEARS IN MILLISECONDS :)
             maxAge: conf.COOKIE_MAX_AGE || 1000 * (Math.pow(2, 31) - 1),
         },
