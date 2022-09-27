96fc205e16bd8fcdd07b4e8b19b7d42cdf8b8002
Anton Aleshin <Anton_Aleschin@mail.ru>
2022-09-27 13:23:52 +0300

    fix(condo): DOMA-3529 fixed closure issue in useImporterFunctions
    It has caused using wrong organization in import after it has been changed

--- apps/condo/domains/meter/hooks/useImporterFunction.ts
+++ apps/condo/domains/meter/hooks/useImporterFunction.ts
@@ -1,3 +1,4 @@
+import { useEffect, useMemo, useRef } from 'react'
 import dayjs from 'dayjs'
 import { useOrganization } from '@condo/next/organization'
 import { useApolloClient } from '@condo/next/apollo'
@@ -6,7 +7,6 @@ import map from 'lodash/map'
 import isEmpty from 'lodash/isEmpty'
 import { SortMetersBy } from '@app/condo/schema'
 import { useIntl } from '@condo/next/intl'
-import { useMemo } from 'react'
 
 import {
     Columns,
@@ -111,6 +111,10 @@ export const useImporterFunctions = (): [Columns, RowNormalizer, RowValidator, O
     const { addressApi } = useAddressApi()
 
     const userOrganizationId = get(userOrganization, ['organization', 'id'])
+    const userOrganizationIdRef = useRef(userOrganization.id)
+    useEffect(() => {
+        userOrganizationIdRef.current = userOrganizationId
+    }, [userOrganizationId])
 
     const meterCreateAction = Meter.useCreate({})
 
@@ -179,7 +183,7 @@ export const useImporterFunctions = (): [Columns, RowNormalizer, RowValidator, O
         addons.address = suggestion.value
 
         const properties = await searchPropertyWithMap(client, {
-            organization: { id: userOrganizationId },
+            organization: { id: userOrganizationIdRef.current },
             address_i: suggestion.value,
         }, undefined)
 
@@ -194,7 +198,7 @@ export const useImporterFunctions = (): [Columns, RowNormalizer, RowValidator, O
         addons.unitType = UNIT_TYPE_TRANSLATION_TO_TYPE[String(unitType).toLowerCase()]
 
         const searchMeterWhereConditions = {
-            organization: { id: userOrganizationId },
+            organization: { id: userOrganizationIdRef.current },
             property: { id: propertyId },
             unitName,
             unitType: addons.unitType,
@@ -304,7 +308,7 @@ export const useImporterFunctions = (): [Columns, RowNormalizer, RowValidator, O
             meterId = addons.meterId
         } else {
             const newMeter = await meterCreateAction({
-                organization: { connect: { id: String(userOrganizationId) } },
+                organization: { connect: { id: String(userOrganizationIdRef.current) } },
                 property: { connect: { id: String(addons.propertyId) } },
                 resource: { connect: { id: addons.meterResourceId } },
                 unitName: String(unitName),
