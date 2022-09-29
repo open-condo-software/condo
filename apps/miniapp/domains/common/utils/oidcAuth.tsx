c3cb87bc2e6c1fb7d4a6a6754a6decbce8b965ab
AleX83Xpert <AleX83Xpert@mail.ru>
2022-09-29 11:25:48 +0500

    fix(miniapp): DOMA-4253 Show message in case of there is no access to storage with first-party cookies

--- apps/miniapp/domains/common/utils/oidcAuth.tsx
+++ apps/miniapp/domains/common/utils/oidcAuth.tsx
@@ -1,9 +1,10 @@
 import { useAuth } from '@condo/next/auth'
+import { useIntl } from '@condo/next/intl'
+import { createCondoBridge } from '@miniapp/domains/common/clients/CondoBridge'
 import { useOrganization } from '@miniapp/domains/common/utils/organization'
 import getConfig from 'next/config'
 import Router from 'next/router'
-import React, { useContext, useEffect } from 'react'
-import { createCondoBridge } from '@miniapp/domains/common/clients/CondoBridge'
+import React, { useCallback, useContext, useEffect, useState } from 'react'
 
 interface IUseOidcAuthHookValue {
     user?: any,
@@ -20,14 +21,24 @@ const OidcAuthContext = React.createContext<IUseOidcAuthHookValue>({})
 const useOidcAuth = () => useContext(OidcAuthContext)
 
 const OidcAuthProvider = ({ children }) => {
+    const intl = useIntl()
+    const NoAccessToStorageMessage = intl.formatMessage({ id: 'NoAccessToStorage' })
+    const AskForAccessButtonMessage = intl.formatMessage({ id: 'AskForAccessButton' })
+
     const { organization: condoOrganization, isLoading: isOrganizationLoading } = useOrganization()
     const { result: condoUser, isLoading: isCondoUserLoading } = condoBridge.useUser()
     const { user, isLoading: isUserLoading, isAuthenticated } = useAuth()
 
+    /**
+     * In the case of Safari we have to ask browser to grant access to condo's session cookie using Storage Access API
+     * @link https://webkit.org/blog/8124/introducing-storage-access-api/
+     */
+    const [hasStorageAccess, setHasStorageAccess] = useState<boolean>(!(!!document.hasStorageAccess && !!document.requestStorageAccess))
+
     const isAllLoaded = !isOrganizationLoading && !isCondoUserLoading && !isUserLoading
 
     useEffect(() => {
-        if (isAllLoaded) {
+        if (isAllLoaded && hasStorageAccess) {
             if (!isAuthenticated || user.id !== condoUser.id || user.organizationId !== condoOrganization.id) {
                 console.debug('OidcAuthProvider: redirect to oidc auth', {
                     OIDC_AUTH_URL,
@@ -40,7 +51,54 @@ const OidcAuthProvider = ({ children }) => {
             }
         }
 
-    }, [isAllLoaded, user, isAuthenticated, condoUser, condoOrganization])
+    }, [isAllLoaded, user, isAuthenticated, condoUser, condoOrganization, hasStorageAccess])
+
+    useEffect(() => {
+        if (!hasStorageAccess && document && document.hasStorageAccess) {
+            document.hasStorageAccess().then(
+                (hasAccess) => {
+                    // Boolean hasAccess says whether the document has access or not.
+                    console.debug(`document.hasStorageAccess() resolved: ${hasAccess}`)
+                    setHasStorageAccess(hasAccess)
+                },
+                (reason) => {
+                    // Promise was rejected for some reason.
+                    console.debug(`document.hasStorageAccess() rejected: ${reason}`)
+                    setHasStorageAccess(false)
+                },
+            )
+        }
+    }, [hasStorageAccess])
+
+    const requestAccessToStorage = useCallback(() => {
+        if (!hasStorageAccess && document && document.requestStorageAccess) {
+            document && document.requestStorageAccess().then(
+                () => {
+                    // Storage access was granted.
+                    console.debug('document.requestStorageAccess(): access granted')
+                    setHasStorageAccess(true)
+                },
+                () => {
+                    // Storage access was denied.
+                    console.debug('document.requestStorageAccess(): access denied')
+                    setHasStorageAccess(false)
+                },
+            )
+        }
+    }, [hasStorageAccess])
+
+    if (!hasStorageAccess) {
+        return (
+            <div>
+                <p>{NoAccessToStorageMessage}</p>
+                <button
+                    onClick={requestAccessToStorage}
+                >
+                    {AskForAccessButtonMessage}
+                </button>
+            </div>
+        )
+    }
 
     return (
         <OidcAuthContext.Provider value={{ user, isLoading: (isUserLoading || isOrganizationLoading) }}>
