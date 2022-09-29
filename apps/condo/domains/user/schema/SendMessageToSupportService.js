b0f6dac0f1c1c350e538d3190f26d68e214c7dd1
Kanol <andrey.karmanov.2011@yandex.ru>
2022-09-29 18:34:39 +0500

    Revert "feat(condo): DOMA-3094 remove deprecated paymentCategory"
    This reverts commit d4a1e08febc57496ec467882d104fc8c474ad262.

--- apps/condo/domains/user/schema/SendMessageToSupportService.js
+++ apps/condo/domains/user/schema/SendMessageToSupportService.js
@@ -99,11 +99,12 @@ const SendMessageToSupportService = new GQLCustomSchema('SendMessageToSupportSer
                         organizationsIds.push(resident.organization.id)
                     }
 
-                    const residentInfo = { address: resident.address, accountNumber: null }
+                    const residentInfo = { address: resident.address, accountNumber: null, paymentCategory: null }
 
                     const serviceConsumer = await ServiceConsumer.getOne(context, { resident: { id: resident.id } })
                     if (serviceConsumer) {
                         residentInfo.accountNumber = serviceConsumer.accountNumber
+                        residentInfo.paymentCategory = serviceConsumer.paymentCategory
                     }
 
                     residentsExtraInfo.push(residentInfo)
