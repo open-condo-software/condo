2bdf3b5bb8ef982aad7116d627df918a6e239412
VKislov <wkislov0@gmail.com>
2022-10-07 11:13:27 +0500

    fix(condo): DOMA-4288 send tin and user info with user message

--- apps/condo/domains/common/components/UseDeskWidget.tsx
+++ apps/condo/domains/common/components/UseDeskWidget.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import { useOrganization } from '@condo/next/organization'
+import React, { useEffect } from 'react'
 import getConfig from 'next/config'
+import get from 'lodash/get'
 
 const { publicRuntimeConfig:{ UseDeskWidgetId } } = getConfig()
+const fieldId = 20560
 
 const UseDeskWidget: React.FC = () => {
+    const { link } = useOrganization()
+
+    useEffect(() => {
+        // @ts-ignore
+        if (UseDeskWidgetId) window.usedeskMessenger.userIdentify(
+            {
+                name: get(link, 'name', null),
+                email: get(link, 'email', null),
+                phone: get(link, 'phone', '').slice(1),
+                additional_fields:
+                    [
+                        {
+                            id: fieldId, value: get(link, ['organization', 'tin'], null),
+                        },
+                    ],
+            })
+    }, [link])
+
     return UseDeskWidgetId ?
         <script async src={`//lib.usedesk.ru/secure.usedesk.ru/${UseDeskWidgetId}.js`}></script> : null
 }
