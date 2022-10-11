66a2c62a54f3bc0a235cf79634a9ee81c1bad58c
akarjakin <akarjakin@doma.ai>
2022-10-11 13:24:02 +0500

    fix(condo): DOMA-4258 fixed cron according to UTC
    fix(condo): DOMA-4258 added removeCronTask for previous task
    
    fix(condo): DOMA-4258 removed constacts from cron

--- apps/condo/domains/resident/tasks/sendBillingReceiptNotifications.task.js
+++ apps/condo/domains/resident/tasks/sendBillingReceiptNotifications.task.js
@@ -1,14 +1,14 @@
-const { createCronTask } = require('@condo/keystone/tasks')
+const { createCronTask, removeCronTask } = require('@condo/keystone/tasks')
 
 const { sendBillingReceiptNotifications } = require('./helpers')
 
-const WORK_DAYS_FROM = 10
-const WORK_DAYS_TO = 15
-const WEEKENDS_FROM = 12
-const WEEKENDS_TO = 15
+// https://crontab.guru/#17_10-15_*_*_1-5
+removeCronTask('sendBillingReceiptNotificationsTaskWorkDays', '17 10-15 * * 1-5')
+removeCronTask('sendBillingReceiptNotificationsTaskWeekends', '17 12-15 * * 0,6')
 
-const sendBillingReceiptNotificationsTaskWorkDays = createCronTask('sendBillingReceiptNotificationsTaskWorkDays', `17 ${WORK_DAYS_FROM}-${WORK_DAYS_TO} * * 1-5`, sendBillingReceiptNotifications)
-const sendBillingReceiptNotificationsTaskWeekends = createCronTask('sendBillingReceiptNotificationsTaskWeekends', `17 ${WEEKENDS_FROM}-${WEEKENDS_TO} * * 0,6`, sendBillingReceiptNotifications)
+/** PODS work according to UTC, so time in cron-tasks should also be according to UTC too. */
+const sendBillingReceiptNotificationsTaskWorkDays = createCronTask('sendBillingReceiptNotificationsTaskWorkDays', '17 7-12 * * 1-5', sendBillingReceiptNotifications)
+const sendBillingReceiptNotificationsTaskWeekends = createCronTask('sendBillingReceiptNotificationsTaskWeekends', '17 9-12 * * 0,6', sendBillingReceiptNotifications)
 
 module.exports = {
     sendBillingReceiptNotificationsTaskWorkDays,
