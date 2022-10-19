06a8656b3b76d61ecd02e4a5de821af0498f171f
Anton Aleshin <Anton_Aleschin@mail.ru>
2022-10-19 20:59:35 +0300

    refactor(condo): DOMA-4226 basic tests for SbbolSecretStorage

---
+++ apps/condo/domains/organization/integrations/sbbol/SbbolSecretStorage.spec.js
@@ -0,0 +1,14 @@
+const { SbbolSecretStorage } = require('./SbbolSecretStorage')
+describe('SbbolSecretStorage', () => {
+    it('sets and gets clientSecret', async () => {
+        const storage = new SbbolSecretStorage('auth', '12345')
+        storage.setClientSecret('abcdefg-1234567')
+        expect(await storage.getClientSecret()).toEqual('abcdefg-1234567')
+    })
+
+    it('sets and gets refreshToken', async () => {
+        const storage = new SbbolSecretStorage('auth', '12345')
+        storage.setRefreshToken('abcdefg-1234567')
+        expect(await storage.getRefreshToken()).toEqual('abcdefg-1234567')
+    })
+})
