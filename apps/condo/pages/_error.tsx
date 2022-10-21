fe05ca0a45157445f3ff086bd42b07f16c205784
Pahaz <pahaz.white@gmail.com>
2022-10-22 00:47:10 +0300

    fix(condo): HOTFIX sync app.tsx with callcenter

--- apps/condo/pages/_error.tsx
+++ apps/condo/pages/_error.tsx
@@ -1,28 +1,38 @@
+import get from 'lodash/get'
+import { NextPageContext } from 'next/dist/next-server/lib/utils'
+import NextErrorComponent from 'next/error'
 import React from 'react'
+
 import Custom500, { ErrorLayout } from './500'
 
 export default function ErrorPage (): React.ReactElement {
-    return <Custom500 />
+    return <Custom500/>
 }
 
 ErrorPage.container = ErrorLayout
 
-ErrorPage.getInitialProps = ({ res, err }) => {
-    const statusCode = res ? res.statusCode : err ? err.statusCode : 404
+ErrorPage.getInitialProps = async (props: NextPageContext) => {
+    const { res, err } = props
 
-    if (statusCode >= 500 && statusCode < 600) {
+    console.error('ErrorPage:', err)
+
+    const errorInitialProps = await NextErrorComponent.getInitialProps(props)
+
+    const statusCode = get(res, 'statusCode', 0)
+
+    if (statusCode >= 500) {
         res.writeHead(302, {
             Location: '/500',
         })
         res.end()
     }
 
-    if (statusCode === 404){
+    if (statusCode === 404) {
         res.writeHead(302, {
             Location: '/404',
         })
         res.end()
     }
 
-    return { }
+    return errorInitialProps
 }
