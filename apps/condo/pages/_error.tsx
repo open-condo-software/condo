b8468ad07fc5aa7ec902b5f5f611cf155341129a
Pavel <nomerdvadcatpyat@gmail.com>
2022-10-23 21:32:47 +0400

    fix(condo): DOMA-4242 added logging in _error

--- apps/condo/pages/_error.tsx
+++ apps/condo/pages/_error.tsx
@@ -1,9 +1,9 @@
 import get from 'lodash/get'
-import { NextPageContext } from 'next/dist/next-server/lib/utils'
+import { NextPageContext } from 'next'
 import NextErrorComponent from 'next/error'
 import React from 'react'
 
-import Custom500, { ErrorLayout } from './500'
+import Custom500, { ErrorLayout } from '@app/condo/pages/500'
 
 export default function ErrorPage (): React.ReactElement {
     return <Custom500/>
@@ -12,13 +12,21 @@ export default function ErrorPage (): React.ReactElement {
 ErrorPage.container = ErrorLayout
 
 ErrorPage.getInitialProps = async (props: NextPageContext) => {
-    const { res, err } = props
+    const { req, asPath, pathname, res, err } = props
+
+    console.error('Pathname, asPath:', pathname, asPath)
 
     console.error('ErrorPage:', err)
 
+    console.error('Request:', req)
+
     const errorInitialProps = await NextErrorComponent.getInitialProps(props)
 
+    console.error('ErrorInitialProps:', errorInitialProps)
+
     const statusCode = get(res, 'statusCode', 0)
+
+    console.error('StatusCode:', statusCode)
 
     if (statusCode >= 500) {
         res.writeHead(302, {
