1467d53d0274db8102329b924f84ceba1268a228
Pahaz <pahaz.white@gmail.com>
2022-10-27 16:03:09 +0300

    chore(condo): HOTFIX move FeaturesMiddleware to package

---
+++ packages/featureflags/FeaturesMiddleware.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const { featureToggleManager } = require('@condo/featureflags/featureToggleManager')
+
+class FeaturesMiddleware {
+    async prepareMiddleware () {
+        const app = express()
+        app.get('/api/features', async (req, res) => {
+            const features = await featureToggleManager.fetchFeatures()
+            res.status(200).json(features)
+        })
+        return app
+    }
+}
+
+module.exports = {
+    FeaturesMiddleware,
+}
