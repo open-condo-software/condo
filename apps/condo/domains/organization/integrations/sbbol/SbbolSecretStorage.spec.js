777f5c1eae78a14069ca4225d0293cfe38d87dda
Anton Aleshin <Anton_Aleschin@mail.ru>
2022-11-03 10:46:50 +0300

    chore(condo): DOMA-4226 added more tests for SbbolSecretStorage

--- apps/condo/domains/organization/integrations/sbbol/SbbolSecretStorage.spec.js
+++ apps/condo/domains/organization/integrations/sbbol/SbbolSecretStorage.spec.js
@@ -1,21 +1,39 @@
+const faker = require('faker')
 const { SbbolSecretStorage } = require('./SbbolSecretStorage')
 
 describe('SbbolSecretStorage', () => {
     it('sets and gets clientSecret', async () => {
         const storage = new SbbolSecretStorage('auth', '12345')
-        storage.setClientSecret('abcdefg-1234567')
-        expect(await storage.getClientSecret()).toEqual('abcdefg-1234567')
+        const value = faker.datatype.uuid()
+        storage.setClientSecret(value)
+        expect(await storage.getClientSecret()).toEqual(value)
     })
 
     it('sets and gets refreshToken', async () => {
         const storage = new SbbolSecretStorage('auth', '12345')
-        storage.setRefreshToken('abcdefg-1234567')
-        expect(await storage.getRefreshToken()).toEqual('abcdefg-1234567')
+        const value = faker.datatype.uuid()
+        storage.setRefreshToken(value)
+        expect(await storage.getRefreshToken()).toEqual(value)
     })
 
     it('returns false for isRefreshTokenExpired() for not expired refreshToken', async () => {
         const storage = new SbbolSecretStorage('auth', '12345')
-        storage.setRefreshToken('abcdefg-1234567', { ttl: 999 })
+        const value = faker.datatype.uuid()
+        storage.setRefreshToken(value)
         expect(await storage.isRefreshTokenExpired()).toBeFalsy()
+    })
+
+    it('sets and gets accessToken', async () => {
+        const storage = new SbbolSecretStorage('auth', '12345')
+        const value = faker.datatype.uuid()
+        storage.setAccessToken(value)
+        expect(await storage.getAccessToken()).toEqual(value)
+    })
+
+    it('returns false for isAccessTokenExpired() for not expired accessToken', async () => {
+        const storage = new SbbolSecretStorage('auth', '12345')
+        const value = faker.datatype.uuid()
+        storage.setAccessToken(value)
+        expect(await storage.isAccessTokenExpired()).toBeFalsy()
     })
 })
