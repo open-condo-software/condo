946b5c2ec7eb4f61edbaac7198b29509f2500958
Matthew Savelev <savelevmatthew@gmail.com>
2022-11-07 14:29:44 +0500

    refactor(ui): DOMA-3816 rearranged exports and added watch-mode draft

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,1 +1,2 @@
-export { Button, ButtonProps } from './components/Button'
+export { Button } from './components/Button'
+export type { ButtonProps } from './components/Button'
---
+++ packages/ui/webpack.prod.js
@@ -0,0 +1,17 @@
+const { merge } = require('webpack-merge')
+const common = require('./webpack.common')
+
+module.exports = merge(common, {
+    mode: 'production',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: [
+                    'babel-loader',
+                ],
+                exclude: /node_modules/,
+            },
+        ],
+    },
+})
