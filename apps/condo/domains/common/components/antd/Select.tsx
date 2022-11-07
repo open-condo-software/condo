850a0af916029107462b300294d031d4940c15a3
Alllex202 <alllex.202@gmail.com>
2022-11-07 22:05:22 +0500

    fix(condo): DOMA-4592 fixed tracking by Select

--- apps/condo/domains/common/components/antd/Select.tsx
+++ apps/condo/domains/common/components/antd/Select.tsx
@@ -1,12 +1,9 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import { Select as DefaultSelect, SelectProps } from 'antd'
 import { useTracking, TrackingEventPropertiesType, TrackingEventType } from '@condo/domains/common/components/TrackingContext'
 import { RefSelectProps } from 'antd/lib/select'
 import get from 'lodash/get'
 import isFunction from 'lodash/isFunction'
-import compact from 'lodash/compact'
-import isArray from 'lodash/isArray'
-import isEmpty from 'lodash/isEmpty'
 
 export interface CustomSelectProps<T> extends SelectProps<T> {
     eventName?: string
@@ -15,26 +12,20 @@ export interface CustomSelectProps<T> extends SelectProps<T> {
 }
 
 const Select = <T extends string | number | { value: any, label: any }> (props: CustomSelectProps<T>) => {
-    const { eventName: propEventName, eventProperties = {}, onChange, ...restProps } = props
+    const { eventName: propEventName, eventProperties = {}, onSelect, ...restProps } = props
     const { logEvent, getEventName } = useTracking()
 
     const eventName = propEventName ? propEventName : getEventName(TrackingEventType.Select)
     const componentProperties = { ...eventProperties }
 
-    const onChangeCallback: CustomSelectProps<T>['onChange'] = (value, option) => {
+    const onSelectCallback = useCallback((value, option) => {
         if (eventName) {
-            let selectedValue = null
+            const componentValue = get(option, 'title')
 
-            if (isArray(option)) {
-                selectedValue = compact(option.map(opt => get(opt, 'title', false) || get(opt, 'label')))
-            } else {
-                selectedValue = get(option, 'title')
-            }
-
-            if (!isEmpty(selectedValue)) {
-                componentProperties['component'] = { value: selectedValue }
-
+            if (componentValue) {
                 const componentId = get(restProps, 'id')
+                componentProperties['component'] = { value: componentValue }
+
                 if (componentId) {
                     componentProperties['component']['id'] = componentId
                 }
@@ -43,14 +34,13 @@ const Select = <T extends string | number | { value: any, label: any }> (props: 
             }
         }
 
-        // fire prop onChange callback if it was passed to component props
-        if (isFunction(onChange)) {
-            onChange(value, option)
+        if (isFunction(onSelect)) {
+            onSelect(value, option)
         }
-    }
+    }, [onSelect])
 
     return (
-        <DefaultSelect <T> {...restProps } onChange={onChangeCallback} />
+        <DefaultSelect <T> {...restProps } onSelect={onSelectCallback} />
     )
 }
 
