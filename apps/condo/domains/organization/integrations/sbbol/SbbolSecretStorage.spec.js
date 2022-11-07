ab96884137de9d7faf786ec6df9fcb1ba52e2bd0
Pavel <nomerdvadcatpyat@gmail.com>
2022-11-07 10:54:30 +0400

    hotfix(condo): HOTFIX added await to return value in getClientSecret

--- apps/condo/domains/organization/integrations/sbbol/SbbolSecretStorage.spec.js
+++ apps/condo/domains/organization/integrations/sbbol/SbbolSecretStorage.spec.js
@@ -5,35 +5,35 @@ describe('SbbolSecretStorage', () => {
     it('sets and gets clientSecret', async () => {
         const storage = new SbbolSecretStorage('auth', '12345')
         const value = faker.datatype.uuid()
-        storage.setClientSecret(value)
+        await storage.setClientSecret(value)
         expect(await storage.getClientSecret()).toEqual(value)
     })
 
     it('sets and gets refreshToken', async () => {
         const storage = new SbbolSecretStorage('auth', '12345')
         const value = faker.datatype.uuid()
-        storage.setRefreshToken(value)
+        await storage.setRefreshToken(value)
         expect(await storage.getRefreshToken()).toEqual(value)
     })
 
     it('returns false for isRefreshTokenExpired() for not expired refreshToken', async () => {
         const storage = new SbbolSecretStorage('auth', '12345')
         const value = faker.datatype.uuid()
-        storage.setRefreshToken(value)
+        await storage.setRefreshToken(value)
         expect(await storage.isRefreshTokenExpired()).toBeFalsy()
     })
 
     it('sets and gets accessToken', async () => {
         const storage = new SbbolSecretStorage('auth', '12345')
         const value = faker.datatype.uuid()
-        storage.setAccessToken(value)
+        await storage.setAccessToken(value)
         expect(await storage.getAccessToken()).toEqual(value)
     })
 
     it('returns false for isAccessTokenExpired() for not expired accessToken', async () => {
         const storage = new SbbolSecretStorage('auth', '12345')
         const value = faker.datatype.uuid()
-        storage.setAccessToken(value)
+        await storage.setAccessToken(value)
         expect(await storage.isAccessTokenExpired()).toBeFalsy()
     })
 })
