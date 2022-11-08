78dc913d7831d7a51abaedd1939c40fa995871fb
Matthew Savelev <savelevmatthew@gmail.com>
2022-11-08 16:51:33 +0500

    refactor(ui): DOMA-3816 temp commit
    refactor(ui): DOMA-3816 temp commit

--- packages/ui/src/components/Button/button.tsx
+++ packages/ui/src/components/Button/button.tsx
@@ -9,13 +9,14 @@ const BUTTON_CLASS_PREFIX = 'condo-btn'
 
 type CondoButtonProps = {
     type: 'primary' | 'secondary',
+    children?: string
 }
 
 export type ButtonProps = Omit<DefaultButtonProps, 'shape' | 'size' | 'style' | 'ghost' | 'type' | 'prefix' | 'prefixCls'>
 & CondoButtonProps
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>((props, ref) => {
-    const { type, className, icon, ...rest } = props
+    const { type, className, icon, children,  ...rest } = props
     const classes = classNames(
         {
             [`${BUTTON_CLASS_PREFIX}-${type}`]: type,
@@ -34,7 +35,11 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>((props, ref) => 
             prefixCls={BUTTON_CLASS_PREFIX}
             className={classes}
             ref={ref}
-        />
+        >
+            <span className={`${BUTTON_CLASS_PREFIX}-text`} data-before={children}>
+                {children}
+            </span>
+        </DefaultButton>
     )
 })
 
