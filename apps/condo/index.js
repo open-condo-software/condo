ef497000fe800a7a21351ea8d8a471f2453f5578
toplenboren <toplenboren@gmail.com>
2022-11-10 02:15:58 +0600

    refactor(condo): DOMA-2681 move connectedTables away and incapsulate state logic

--- apps/condo/index.js
+++ apps/condo/index.js
@@ -97,6 +97,9 @@ if (IS_ENABLE_CACHE) {
     keystoneCacheApp = new KeystoneCacheMiddleware(keystone)
 }
 
+const adapterCacheConfiig = conf.ADAPTER_CACHE_CONFIG
+let keystoneAdapterCacheApp = new AdapterCacheMiddleware(adapterCacheConfiig)
+
 // Because Babel is used only for frontend to transpile and optimise code,
 // backend files will bring unnecessary workload to building stage.
 // They can be safely ignored without impact on final executable code
@@ -173,6 +176,7 @@ module.exports = {
         new OIDCMiddleware(),
         new FeaturesMiddleware(),
         new PaymentLinkMiddleware(),
+        new AdapterCacheMiddleware(adapterCacheConfiig, ADAPTER_CACHE_CONNECTED_TABLES),
         new GraphQLApp({
             apollo: {
                 formatError,
