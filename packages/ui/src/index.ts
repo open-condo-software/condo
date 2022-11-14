05dbfb76cedf0ff9d6f40a64973d1c2284c88c32
Matthew Savelev <savelevmatthew@gmail.com>
2022-11-14 12:43:48 +0500

    feat(ui): DOMA-4634 Added carousel component

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -2,3 +2,6 @@ import '@open-condo/ui/src/components/style/core/global.less'
 
 export { Button } from './components/Button'
 export type { ButtonProps } from './components/Button'
+
+export { Carousel } from './components/Carousel'
+export type { CarouselProps } from './components/Carousel'
