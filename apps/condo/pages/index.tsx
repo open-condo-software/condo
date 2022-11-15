b42130451b37eab6c5d3f29a3ea26bc931bd3892
Pavel <nomerdvadcatpyat@gmail.com>
2022-11-15 11:59:14 +0500

    fix(condo): DOMA-4261 fixed redirect to ticket page for contractor role

--- apps/condo/pages/index.tsx
+++ apps/condo/pages/index.tsx
@@ -1,13 +1,20 @@
+import { ASSIGNED_TICKET_VISIBILITY } from '@condo/domains/organization/constants/common'
+import get from 'lodash/get'
 import React, { useEffect } from 'react'
 import { OrganizationRequired } from '@condo/domains/organization/components/OrganizationRequired'
 import { useRouter } from 'next/router'
+import { useOrganization } from '@open-condo/next/organization'
 
 
 const IndexPage = () => {
     const router = useRouter()
+    const organization = useOrganization()
+
     useEffect(() => {
-        router.push('/reports')
-    }, [])
+        if (get(organization, ['link', 'role', 'ticketVisibilityType']) !== ASSIGNED_TICKET_VISIBILITY) {
+            router.push('/reports')
+        }
+    }, [organization, router])
     return <></>
 }
 
