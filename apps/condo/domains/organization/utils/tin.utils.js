c8573218008013b18e26bd34d846cb9ab8d86f3a
Andrew Karyakin <91055869+akarjakin@users.noreply.github.com>
2022-11-16 20:37:39 +0600

    refactor(condo): DOMA-3252 (AUTHORED: @akarjakin) return tin utils for organization
    Signed-off-by: toplenboren <toplenboren@gmail.com>

---
+++ apps/condo/domains/organization/utils/tin.utils.js
@@ -0,0 +1,45 @@
+const isString = require('lodash/isString')
+const isNumber = require('lodash/isNumber')
+
+const { RUSSIA_COUNTRY } = require('@condo/domains/common/constants/countries')
+
+// Digits for Ru tin checksum calculation
+const RU_TIN_DIGITS = [3, 7, 2, 4, 10, 3, 5, 9, 4, 6, 8, 0]
+const RU_ORGANIZATION_TIN_REGEXP = /^\d{10}$/
+const RU_PERSONAL_TIN_REGEXP = /^\d{12}$/
+
+const getTinChecksumRU = num => {
+    const n = RU_TIN_DIGITS.slice(-num.length)
+    let sum = 0
+
+    for (let i = 0; i < num.length; i++) sum += num[i] * n[i]
+
+    return sum % 11 % 10
+}
+
+const validateTinRU = tinValue => {
+    if (!isString(tinValue) && !isNumber(tinValue)) return false
+
+    const inn = tinValue.toString().trim()
+
+    if (!RU_ORGANIZATION_TIN_REGEXP.test(inn) && !RU_PERSONAL_TIN_REGEXP.test(inn)) return false
+    if (inn.length == 10) return getTinChecksumRU(inn) == inn.slice(-1)
+    if (inn.length == 12) return getTinChecksumRU(inn.slice(0, 11)) == inn.slice(10, -1) && getTinChecksumRU(inn) == inn.slice(-1)
+
+    return false
+}
+
+const isValidTin = (tinValue = null, country = RUSSIA_COUNTRY) => {
+    if (country === RUSSIA_COUNTRY) return validateTinRU(tinValue)
+
+    // TODO: DOMA-663 add tin validations for countries other than Russian Federation
+    return false
+}
+
+const getIsValidTin = (country = RUSSIA_COUNTRY) => (tinValue = null) => isValidTin(tinValue, country)
+
+module.exports = {
+    validateTinRU,
+    isValidTin,
+    getIsValidTin,
+}
---
+++ apps/condo/domains/organization/utils/tin.utils.spec.js
@@ -0,0 +1,52 @@
+const { RUSSIA_COUNTRY } = require('@condo/domains/common/constants/countries')
+const { SPACE_SYMBOLS, SPACE_SYMBOL_LABLES } = require('@condo/domains/common/utils/string.utils')
+const { isValidTin } = require('@condo/domains/organization/utils/tin.utils')
+
+const SPACES = SPACE_SYMBOLS.split('')
+const VALID_RU_TIN_10 = '1654019570'
+const VALID_RU_TIN_10_1 = '6311095616'
+const VALID_RU_TIN_12 = '500110474504'
+const VALID_TINS = [VALID_RU_TIN_10, VALID_RU_TIN_10_1, VALID_RU_TIN_12]
+const INVALID_RU_TIN_10 = '01234556789'
+const INVALID_RU_TIN_12 = '0123455678901'
+const SOME_RANDOM_LETTERS = 'ABCDEFGHIJ'
+
+describe('isValidTin()', () => {
+    VALID_TINS.forEach(tin => {
+        test(`for valid 10 or 12 char RU INN (${tin})`, () => {
+            expect(isValidTin(tin, RUSSIA_COUNTRY)).toBe(true)
+        })
+        SPACES.forEach(spaceSymbol => {
+            test(`for valid 10 or 12 char RU INN (${tin}) with spaces symbol (${SPACE_SYMBOL_LABLES[spaceSymbol] || spaceSymbol})`, () => {
+                const tinValue = `${spaceSymbol}${tin}${spaceSymbol}`
+
+                expect(isValidTin(tinValue, RUSSIA_COUNTRY)).toBe(true)
+            })
+        })
+    })
+    test('for valid 12 char RU INN ', () => {
+        // NOTE: we need INNs only for organizations, that is of 10 chars length.
+        // So valid 12 char length person INN doesn`t suit
+        expect(isValidTin(VALID_RU_TIN_12, RUSSIA_COUNTRY)).toBe(true)
+    })
+    test('for invalid 10 digits as RU INN', () => {
+        expect(isValidTin(INVALID_RU_TIN_10, RUSSIA_COUNTRY)).toBe(false)
+    })
+    test('for invalid 12 digits as RU INN', () => {
+        expect(isValidTin(INVALID_RU_TIN_12, RUSSIA_COUNTRY)).toBe(false)
+    })
+    test('for invalid random letters as RU INN', () => {
+        expect(isValidTin(SOME_RANDOM_LETTERS, RUSSIA_COUNTRY)).toBe(false)
+    })
+    test('for invalid random wrong length number as RU INN', () => {
+        expect(isValidTin(Math.floor(999 + Math.random() * 1000000), RUSSIA_COUNTRY)).toBe(false)
+    })
+})
+
+module.exports = {
+    VALID_RU_TIN_10,
+    VALID_RU_TIN_12,
+    INVALID_RU_TIN_10,
+    INVALID_RU_TIN_12,
+    SOME_RANDOM_LETTERS,
+}
