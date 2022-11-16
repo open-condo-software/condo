30991ae5d3f0e3a5e869daa3a3ead4d0fa9d5333
toplenboren <toplenboren@gmail.com>
2022-11-16 22:57:33 +0600

    refactor(condo): DOMA-3252 move all country specific validators to one file
    Signed-off-by: toplenboren <toplenboren@gmail.com>

--- apps/condo/domains/banking/utils/validate/routingNumber.utils.js
+++ apps/condo/domains/banking/utils/validate/routingNumber.utils.js
@@ -1,4 +1,5 @@
 const { get } = require('lodash')
+const { getCountrySpecificValidator } = require('./countrySpecificValidators')
 
 /*
  * Routing number validator
@@ -15,26 +16,6 @@ const EMPTY = 'Routing number is empty'
 const NOT_NUMERIC = 'Routing number can contain only digits'
 const WRONG_LENGTH = 'Routing number length was expected to be 9, but received '
 
-const COUNTRY_SPECIFIC_VALIDATORS = {
-    /**
-     * For RU routing number:
-     * *** In Russia, number routing is equivalent to BIC***
-     * 1) For RU organizations country code (first two digits) is 04
-     *
-     * @param {string} routingNumber
-     * @param {[]} errors
-     */
-    'ru': (routingNumber, errors) => {
-        const WRONG_RU_COUNTRY_CODE = 'For RU organizations country code is 04, but routing number have '
-
-        const countryCode = routingNumber.substr(0, 2)
-
-        if (countryCode !== '04') {
-            errors.push(WRONG_RU_COUNTRY_CODE + countryCode)
-        }
-    },
-}
-
 const validateRoutingNumber = (routingNumber, country) => {
     const errors = []
 
@@ -49,8 +30,7 @@ const validateRoutingNumber = (routingNumber, country) => {
         errors.push(WRONG_LENGTH + routingNumberWithoutSpaces.length)
     }
 
-    const countrySpecificValidator = get(COUNTRY_SPECIFIC_VALIDATORS, country, null)
-    if (countrySpecificValidator) { countrySpecificValidator(routingNumberWithoutSpaces, errors) }
+    getCountrySpecificValidator('routingNumber', country)(routingNumberWithoutSpaces, errors)
 
     return {
         result: !errors.length,
