601d87b060f191e35ec0e9f27b3305bee3e98cc2
toplenboren <toplenboren@gmail.com>
2022-11-16 22:24:56 +0600

    refactor(condo): DOMA-3252 refactor tin and accountNumber validators
    Signed-off-by: toplenboren <toplenboren@gmail.com>

--- apps/condo/domains/banking/utils/validate/number.utils.js
+++ apps/condo/domains/banking/utils/validate/number.utils.js
@@ -4,34 +4,34 @@
  * You need a routing number for validation
  *
  * The following checks are performed:
- * 1) Сhecking for emptiness
- * 2) Checking for length and format (Consists of 20 digits)
+ * 1) Сheck for emptiness
+ * 2) Check for length and format (Consists of 20 digits)
  *
- * For RU number:
- * 1) Сhecking checksum verification for number
- *
- * Example:
- * number - 50286516400000008106
- * routing number - 823397286
  */
 
 const { validateRoutingNumber } = require('@condo/domains/banking/utils/validate/routingNumber.utils')
+const { RU_NUMBER_WEIGHTS } = require('@condo/domains/banking/utils/validate/constants')
 
-//GENERAL ERRORS
 const EMPTY = 'Number is empty'
 const NOT_NUMERIC = 'Number can contain only digits'
 const WRONG_LENGTH = 'Number length was expected to be 20, but received '
 
-//RU ERRORS
-const RU_CONTROL_SUM_FAILED = 'Control sum is not valid for number'
+const COUNTRY_SPECIFIC_VALIDATORS = {
+    /**
+     * For RU number:
+     * 1) Сhecking checksum verification for number
+     *
+     * Example:
+     * number - 50286516400000008106
+     * routing number - 823397286
+     *
+     * @param {string} number - BankAccount number
+     * @param {string} routingNumber - Bic
+     * @param {[]} errors
+     */
+    'ru': (number, routingNumber, errors) => {
+        const RU_CONTROL_SUM_FAILED = 'Control sum is not valid for number'
 
-const RU_NUMBER_WEIGHTS = [7, 1, 3, 7, 1, 3, 7, 1, 3, 7, 1, 3, 7, 1, 3, 7, 1, 3, 7, 1, 3, 7, 1]
-
-class NumberValidation {
-
-    errors = []
-
-    validateRuNumber (number, routingNumber) {
         const controlString = routingNumber.substr(-3) + number
 
         let controlSum = 0
@@ -40,44 +40,38 @@ class NumberValidation {
         }
 
         if (controlSum % 10 !== 0) {
-            this.errors.push(RU_CONTROL_SUM_FAILED)
+            errors.push(RU_CONTROL_SUM_FAILED)
         }
-    }
-
-    validateNumber (number, routingNumber, country) {
-        const numberWithoutSpaces = number.toString().trim()
-        const routingNumberWithoutSpaces = routingNumber.toString().trim()
-
-        this.errors.push( ...validateRoutingNumber(routingNumberWithoutSpaces, country).errors )
-
-        if (!numberWithoutSpaces.length) {
-            this.errors.push(EMPTY)
-        }
-        if (!/^[0-9]*$/.test(numberWithoutSpaces)) {
-            this.errors.push(NOT_NUMERIC)
-        }
-        if (numberWithoutSpaces.length !== 20) {
-            this.errors.push(WRONG_LENGTH + numberWithoutSpaces.length)
-        }
-
-        if (country === 'ru')
-            this.validateRuNumber(numberWithoutSpaces, routingNumberWithoutSpaces)
-
-        return {
-            result: !this.errors.length,
-            errors: this.errors,
-        }
-    }
+    },
 }
 
 const validateNumber = (number, routingNumber, country) => {
-    const validator = new NumberValidation()
+    const errors = []
 
-    const { result, errors } = validator.validateNumber(number, routingNumber, country)
-    return { result, errors }
+    const numberWithoutSpaces = number.toString().trim()
+    const routingNumberWithoutSpaces = routingNumber.toString().trim()
+
+    errors.push( ...validateRoutingNumber(routingNumberWithoutSpaces, country).errors )
+
+    if (!numberWithoutSpaces.length) {
+        errors.push(EMPTY)
+    }
+    if (!/^[0-9]*$/.test(numberWithoutSpaces)) {
+        errors.push(NOT_NUMERIC)
+    }
+    if (numberWithoutSpaces.length !== 20) {
+        errors.push(WRONG_LENGTH + numberWithoutSpaces.length)
+    }
+
+    const countrySpecificValidator = COUNTRY_SPECIFIC_VALIDATORS[country]
+    if (countrySpecificValidator) { countrySpecificValidator(numberWithoutSpaces, routingNumberWithoutSpaces, errors) }
+
+    return {
+        result: !errors.length,
+        errors: errors,
+    }
 }
 
 module.exports = {
-    RU_NUMBER_WEIGHTS,
     validateNumber,
 }
--- apps/condo/domains/banking/utils/validate/tin.utils.js
+++ apps/condo/domains/banking/utils/validate/tin.utils.js
@@ -2,27 +2,47 @@
  * TIN validator
  *
  * The following checks are performed:
- * 1) Сhecking for emptiness
+ * 1) Check for emptiness
  *
- * For RU tin:
- * 1) Сhecking checksum verification for TIN
- * 2) Checking for length and format (Consists of 10 or 12 digits)
- *
- * Example:
- * RU TIN with 10 characters - 7791815382
- * RU TIN with 12 characters - 397144170672
  */
 
-//GENERAL ERRORS
+const { RU_TIN_WEIGHTS } = require('@condo/domains/banking/utils/validate/constants')
+
 const EMPTY = 'Tin is empty'
 const NOT_NUMERIC = 'Tin can contain only digits'
 
-//RU ERRORS
-const WRONG_LENGTH_RU = 'Ru tin length was expected to be 10 or 12, but received '
-const RU_CONTROL_SUM_FAILED = 'Control sum is not valid for tin'
+const COUNTRY_SPECIFIC_VALIDATIONS = {
+    /**
+     * For RU tin:
+     * 1) Сhecking checksum verification for TIN
+     * 2) Checking for length and format (Consists of 10 or 12 digits)
+     *  *
+     * Example:
+     * RU TIN with 10 characters - 7791815382
+     * RU TIN with 12 characters - 397144170672
+     */
+    'ru': (tin, errors) => {
+        const WRONG_LENGTH_RU = 'Ru tin length was expected to be 10 or 12, but received '
+        const RU_CONTROL_SUM_FAILED = 'Control sum is not valid for tin'
 
-const RU_TIN_WEIGHTS = [3, 7, 2, 4, 10, 3, 5, 9, 4, 6, 8, 0]
+        if (!/^[0-9]*$/.test(tin)) errors.push(NOT_NUMERIC)
 
+        if (tin.length !== 10 && tin.length !== 12) errors.push(WRONG_LENGTH_RU + tin.length)
+
+        if (tin.length === 10)
+            if (getTinControlSumRU(tin) !== Number(tin.slice(-1))) {
+                errors.push(RU_CONTROL_SUM_FAILED)
+            }
+
+        if (tin.length === 12) {
+            if (getTinControlSumRU(tin.slice(0, 11)) !==
+                Number(tin.slice(10, -1)
+                    || getTinControlSumRU(tin) !== Number(tin.slice(-1)))) {
+                errors.push(RU_CONTROL_SUM_FAILED)
+            }
+        }
+    },
+}
 
 function getTinControlSumRU (num) {
     const weights = RU_TIN_WEIGHTS.slice(-num.length)
@@ -34,53 +54,25 @@ function getTinControlSumRU (num) {
     return controlSum % 11 % 10
 }
 
-
-class TinValidation {
-
-    errors = []
-
-    validateRuTin (tin) {
-        if (!/^[0-9]*$/.test(tin)) this.errors.push(NOT_NUMERIC)
-
-        if (tin.length !== 10 && tin.length !== 12) this.errors.push(WRONG_LENGTH_RU + tin.length)
-
-        if (tin.length === 10)
-            if (getTinControlSumRU(tin) !== Number(tin.slice(-1))) {
-                this.errors.push(RU_CONTROL_SUM_FAILED)
-            }
-
-        if (tin.length === 12)
-            if (getTinControlSumRU(tin.slice(0, 11)) !== Number(tin.slice(10, -1)
-                || getTinControlSumRU(tin) !== Number(tin.slice(-1)))) {
-                this.errors.push(RU_CONTROL_SUM_FAILED)
-            }
-    }
-
-    validateTin (tin, country) {
-        const tinWithoutSpaces = tin.toString().trim()
-
-        if (!tin.length) {
-            this.errors.push(EMPTY)
-        }
-
-        if (country === 'ru')
-            this.validateRuTin(tinWithoutSpaces)
-
-        return {
-            result: !this.errors.length,
-            errors: this.errors,
-        }
-    }
-}
-
 const validateTin = (tin, country) => {
-    const validator = new TinValidation()
+    const errors = []
 
-    const { result, errors } = validator.validateTin(tin, country)
-    return { result, errors }
+    const tinWithoutSpaces = tin.toString().trim()
+
+    if (!tin.length) {
+        errors.push(EMPTY)
+    }
+
+    const countrySpecificValidator = COUNTRY_SPECIFIC_VALIDATIONS[country]
+    if (countrySpecificValidator) { countrySpecificValidator( tinWithoutSpaces, errors ) }
+
+    return {
+        result: !errors.length,
+        errors: errors,
+    }
 }
 
 module.exports = {
-    getTinControlSumRU,
     validateTin,
+    getTinControlSumRU,
 }
