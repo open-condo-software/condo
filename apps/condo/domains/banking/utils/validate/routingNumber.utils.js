db133dbffbef503d7b081b8f160f0789e38ec695
toplenboren <toplenboren@gmail.com>
2022-11-16 21:59:50 +0600

    refactor(condo): DOMA-3252 refactor routing number validations
    Signed-off-by: toplenboren <toplenboren@gmail.com>

--- apps/condo/domains/banking/utils/validate/routingNumber.utils.js
+++ apps/condo/domains/banking/utils/validate/routingNumber.utils.js
@@ -1,3 +1,5 @@
+const { get } = require('lodash')
+
 /*
  * Routing number validator
  *
@@ -5,63 +7,55 @@
  * 1) Сhecking for emptiness
  * 2) Checking for length and format (Consists of 9 digits)
  *
- * For RU routing number:
- * *** In Russia, number routing is equivalent to BIC***
- * 1) For RU organizations country code (first two digits) is 04
- *
  * Example:
  * RU - 046000987
  */
 
-//GENERAL ERRORS
 const EMPTY = 'Routing number is empty'
 const NOT_NUMERIC = 'Routing number can contain only digits'
 const WRONG_LENGTH = 'Routing number length was expected to be 9, but received '
 
-//RU ERRORS
-const WRONG_RU_COUNTRY_CODE = 'For RU organizations country code is 04, but routing number have '
+const COUNTRY_SPECIFIC_VALIDATORS = {
+    /**
+     * For RU routing number:
+     * *** In Russia, number routing is equivalent to BIC***
+     * 1) For RU organizations country code (first two digits) is 04
+     *
+     * @param {string} routingNumber
+     * @param {[]} errors
+     */
+    'ru': (routingNumber, errors) => {
+        const WRONG_RU_COUNTRY_CODE = 'For RU organizations country code is 04, but routing number have '
 
-
-class RoutingNumberValidation {
-
-    errors = []
-
-    validateRuRoutingNumber (routingNumber) {
         const countryCode = routingNumber.substr(0, 2)
 
         if (countryCode !== '04') {
-            this.errors.push(WRONG_RU_COUNTRY_CODE + countryCode)
+            errors.push(WRONG_RU_COUNTRY_CODE + countryCode)
         }
-    }
-
-    validateRoutingNumber (routingNumber, country) {
-        const routingNumberWithoutSpaces = routingNumber.toString().trim()
-
-        if (!routingNumberWithoutSpaces.length) {
-            this.errors.push(EMPTY)
-        }
-        if (!/^[0-9]*$/.test(routingNumberWithoutSpaces)) {
-            this.errors.push(NOT_NUMERIC)
-        }
-        if (routingNumberWithoutSpaces.length !== 9) {
-            this.errors.push(WRONG_LENGTH + routingNumberWithoutSpaces.length)
-        }
-
-        if (country === 'ru')
-            this.validateRuRoutingNumber(routingNumberWithoutSpaces)
-
-        return {
-            result: !this.errors.length,
-            errors: this.errors,
-        }
-    }
+    },
 }
 
 const validateRoutingNumber = (routingNumber, country) => {
-    const validator = new RoutingNumberValidation()
+    const errors = []
 
-    const { result, errors } = validator.validateRoutingNumber(routingNumber, country)
-    return { result, errors }
+    const routingNumberWithoutSpaces = routingNumber.toString().trim()
+    if (!routingNumberWithoutSpaces.length) {
+        errors.push(EMPTY)
+    }
+    if (!/^[0-9]*$/.test(routingNumberWithoutSpaces)) {
+        errors.push(NOT_NUMERIC)
+    }
+    if (routingNumberWithoutSpaces.length !== 9) {
+        errors.push(WRONG_LENGTH + routingNumberWithoutSpaces.length)
+    }
+
+    const countrySpecificValidator = get(COUNTRY_SPECIFIC_VALIDATORS, country, null)
+    if (countrySpecificValidator) { countrySpecificValidator(routingNumberWithoutSpaces, errors) }
+
+    return {
+        result: !errors.length,
+        errors: errors,
+    }
 }
 
 module.exports = {
