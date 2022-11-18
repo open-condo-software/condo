65eb6a4458088eb692e0c2dbe3183b9a5a2f7b51
AleX83Xpert <AleX83Xpert@mail.ru>
2022-11-18 15:30:56 +0500

    feat(address-service): DOMA-4064 address plugin now adds new address to service instead of searching it

--- packages/keystone/plugins/addressService.js
+++ packages/keystone/plugins/addressService.js
@@ -49,40 +49,6 @@ const getFieldsToAdd = (mainField, fieldsHooks) => ({
 })
 
 /**
- * This function must return fields according to jsdoc. Returned fields are in dadata format.
- * @param addressMeta
- * @returns {{data: Object, unrestricted_value: String}}
- */
-function extractAndDadatifyAddressMeta (addressMeta) {
-    const providerName = get(addressMeta, ['provider', 'name'])
-    const rawData = get(addressMeta, ['provider', 'rawData'])
-
-    let data
-    let unrestricted_value
-
-    switch (providerName) {
-        case 'dadata':
-            data = get(rawData, 'data', {})
-            unrestricted_value = String(get(rawData, 'unrestricted_value'))
-            break
-        case 'injections':
-            data = get(rawData, 'data', {})
-            unrestricted_value = String(get(rawData, 'unrestricted_value'))
-            break
-        case 'google':
-            data = {
-                // todo(AleX83Xpert) convert google raw data to dadata format
-            }
-            unrestricted_value = ''
-            break
-        default:
-            throw new Error(`Can not convert address meta due to no converter for '${providerName}' provider`)
-    }
-
-    return { data, unrestricted_value }
-}
-
-/**
  * Plugin to extend address field with address service
  * @param {string?} addressFieldName
  * @param {Object} fieldsHooks
@@ -126,29 +92,16 @@ const addressService = (addressFieldName = 'address', fieldsHooks = {}) => plugi
             const client = conf.NODE_ENV === 'test' || get(conf, 'ADDRESS_SERVICE_CLIENT_MODE') === 'fake'
                 ? createTestAddressServiceClientInstance({ ...existingItem, ...resolvedData })
                 : createAddressServiceClientInstance(get(conf, 'ADDRESS_SERVICE_URL'))
-            const results = await client.search(resolvedData[addressFieldName])
 
-            const result = results[0]
+            const result = await client.add({
+                source: resolvedData[addressFieldName],
+                value: resolvedData[getMetaFieldName(addressFieldName)],
+                token: 'TODO(AleX83Xpert)', // todo(AleX83Xpert)
+            })
 
             resolvedData[getSourceFieldName(addressFieldName)] = resolvedData[addressFieldName]
             resolvedData[addressFieldName] = get(result, 'address')
             resolvedData[getKeyFieldName(addressFieldName)] = get(result, 'addressKey')
-
-            //
-            // Meta field processing.
-            //
-
-            // We use raw data of dadata (O_o) in many places of the code
-            // So, we need to keep addressMeta field in the same format as before the address service appeared
-            const addressMeta = get(result, 'addressMeta', {})
-            const normalizedMetaFields = extractAndDadatifyAddressMeta(addressMeta)
-            resolvedData[getMetaFieldName(addressFieldName)] = {
-                dv: 1,
-                address: get(result, 'address'),
-                value: get(result, ['addressMeta', 'provider', 'rawData', 'value']),
-                unrestricted_value: normalizedMetaFields.unrestricted_value,
-                data: normalizedMetaFields.data,
-            }
         }
 
         return resolvedData
