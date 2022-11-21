30b4c01109aa8bc4af181156c86cb0a6c1828824
Pavel <nomerdvadcatpyat@gmail.com>
2022-11-21 17:16:25 +0500

    fix(condo): DOMA-4517 update payment indicator display logic

--- apps/condo/domains/ticket/components/TicketId/TicketResidentFeatures.tsx
+++ apps/condo/domains/ticket/components/TicketId/TicketResidentFeatures.tsx
@@ -1,16 +1,17 @@
-import { BillingReceipt } from '@app/condo/domains/billing/utils/clientSchema'
-import { BankCardIcon } from '@app/condo/domains/common/components/icons/BankCardIcon'
-import { SortBillingReceiptsBy, Ticket } from '@app/condo/schema'
-import { MobileIcon } from '@condo/domains/common/components/icons/MobileIcon'
-import { Loader } from '@condo/domains/common/components/Loader'
-
-import { Tooltip } from '@condo/domains/common/components/Tooltip'
-import { useIntl } from '@open-condo/next/intl'
 import { Col, Row } from 'antd'
 import { Gutter } from 'antd/es/grid/row'
 import dayjs from 'dayjs'
 import get from 'lodash/get'
-import React, { useMemo } from 'react'
+import React from 'react'
+
+import { SortBillingReceiptsBy, Ticket } from '@app/condo/schema'
+import { useIntl } from '@open-condo/next/intl'
+
+import { BillingReceipt } from '@condo/domains/billing/utils/clientSchema'
+import { BankCardIcon } from '@condo/domains/common/components/icons/BankCardIcon'
+import { MobileIcon } from '@condo/domains/common/components/icons/MobileIcon'
+import { Tooltip } from '@condo/domains/common/components/Tooltip'
+
 
 interface MobileAppInstalledIndicatorProps {
     isContactHasMobileApp: boolean
@@ -33,76 +34,33 @@ interface PaymentsAvailableIndicatorProps {
     propertyAddress: string
 }
 
-/**
- Availability is determined by:
- 1. If receipts are found at this address for the last 2 months
- 2. Or:
-    At least 1 receipt for the current or previous period has been uploaded to the organization.
-    If the number of receipts for the previous period is not equal to 0,
-    then the number of receipts for the current period must be greater than or equal to the number for the previous one.
- */
-const getIsPaymentsInMobileAppAvailable = (receiptsByProperty, receiptsInCurrentPeriod, receiptsInPreviousPeriod) => {
-    if (receiptsByProperty) return true
-
-    if (receiptsInCurrentPeriod > 0 || receiptsInPreviousPeriod > 0) {
-        if (receiptsInPreviousPeriod === 0) return true
-
-        if (receiptsInCurrentPeriod >= receiptsInPreviousPeriod) {
-            return true
-        }
-    }
-
-    return false
-}
-
-const TWO_MONTHS_AGO = dayjs().startOf('day').subtract(2, 'months').toISOString()
+const LAST_MONTH_BEGINNING = dayjs().subtract(1, 'months').startOf('month').format('YYYY-MM-DD')
 
 const PaymentsAvailableIndicator: React.FC<PaymentsAvailableIndicatorProps> = ({ ticketOrganizationId, propertyAddress }) => {
     const intl = useIntl()
     const PaymentsAvailableMessage = intl.formatMessage({ id: 'pages.condo.ticket.PaymentsAvailable' })
     const PaymentsNotAvailableMessage = intl.formatMessage({ id: 'pages.condo.ticket.PaymentsNotAvailable' })
 
-    const currentPeriod = dayjs().startOf('month').subtract(1, 'month').format('YYYY-MM-DD')
-    const previousPeriod = dayjs(currentPeriod).subtract(1, 'month').format('YYYY-MM-DD')
-
-    const receiptsSearchQuery = { context: { organization: { id: ticketOrganizationId } } }
-
-    const { count: receiptsInCurrentPeriod, loading: currentPeriodLoading } = BillingReceipt.useCount({
-        where: {
-            ...receiptsSearchQuery,
-            period: currentPeriod,
-        },
-    })
-
-    const { count: receiptsInPreviousPeriod, loading: previousPeriodLoading } = BillingReceipt.useCount({
-        where: {
-            ...receiptsSearchQuery,
-            period: previousPeriod,
-        },
-    })
-
     const { count: receiptsByProperty, loading: receiptsByPropertyLoading } = BillingReceipt.useCount({
         where: {
+            context: { organization: { id: ticketOrganizationId } },
             property: {
                 OR: [
                     { address: propertyAddress },
                     { normalizedAddress: propertyAddress },
                 ],
             },
-            createdAt_gte: TWO_MONTHS_AGO,
+            period_gte: LAST_MONTH_BEGINNING,
         },
         sortBy: [SortBillingReceiptsBy.CreatedAtDesc],
     })
 
-    const isPaymentsAvailable = useMemo(
-        () => getIsPaymentsInMobileAppAvailable(receiptsByProperty, receiptsInCurrentPeriod, receiptsInPreviousPeriod),
-        [receiptsByProperty, receiptsInCurrentPeriod, receiptsInPreviousPeriod])
+    const isPaymentsAvailable = !!receiptsByProperty
     const title = receiptsByProperty || isPaymentsAvailable ? PaymentsAvailableMessage : PaymentsNotAvailableMessage
-    const loading = currentPeriodLoading || previousPeriodLoading || receiptsByPropertyLoading
 
     return (
         <Tooltip title={title}>
-            <BankCardIcon active={isPaymentsAvailable} loading={loading} />
+            <BankCardIcon active={isPaymentsAvailable} loading={receiptsByPropertyLoading} />
         </Tooltip>
     )
 }
