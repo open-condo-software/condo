1fdbb1c2f30708e66a7ea61f4da376bd4a4bf443
AleX83Xpert <AleX83Xpert@mail.ru>
2022-11-22 12:42:09 +0500

    feat(address-service): DOMA-4064 Address field name is constant

--- packages/keystone/plugins/addressService.js
+++ packages/keystone/plugins/addressService.js
@@ -17,56 +17,47 @@ const readOnlyAccess = {
     delete: false,
 }
 
-const getSourceFieldName = (mainFieldName) => `${mainFieldName}Sources`
-const getKeyFieldName = (mainFieldName) => `${mainFieldName}Key`
-const getMetaFieldName = (mainFieldName) => `${mainFieldName}Meta`
-
-const getFieldsToAdd = (mainField, fieldsHooks) => ({
-    [mainField]: {
+const getFieldsToAdd = (fieldsHooks) => ({
+    address: {
         schemaDoc: 'Normalized address',
         type: Text,
         isRequired: true,
         hooks: {
-            ...(fieldsHooks[mainField] || {}),
+            ...(fieldsHooks['address'] || {}),
         },
     },
-    [getSourceFieldName(mainField)]: {
+    addressSources: {
         type: Json,
         schemaDoc: 'The origins of the address (some strings which may looks like real address or some id)',
         access: readOnlyAccess,
         hooks: {
-            ...(fieldsHooks[getSourceFieldName(mainField)] || {}),
+            ...(fieldsHooks['addressSources'] || {}),
         },
     },
-    [getKeyFieldName(mainField)]: {
+    addressKey: {
         type: Text,
         schemaDoc: 'The unique key of the address',
         access: readOnlyAccess,
         hooks: {
-            ...(fieldsHooks[getKeyFieldName(mainField)] || {}),
+            ...(fieldsHooks['addressKey'] || {}),
         },
     },
-    [getMetaFieldName(mainField)]: ADDRESS_META_FIELD,
+    addressMeta: ADDRESS_META_FIELD,
 })
 
 /**
  * Plugin to extend address field with address service
- * @param {string?} addressFieldName
  * @param {Object} fieldsHooks
  */
-const addressService = (addressFieldName = 'address', fieldsHooks = {}) => plugin(({
+const addressService = (fieldsHooks = {}) => plugin(({
     fields = {},
     hooks = {},
     ...rest
 }, schemaObj) => {
-    if (!addressFieldName) {
-        throw new Error('The `addressFieldName` field is not set')
-    }
-
     //
     // Modify fields
     //
-    const fieldsToAdd = getFieldsToAdd(addressFieldName, fieldsHooks)
+    const fieldsToAdd = getFieldsToAdd(fieldsHooks)
 
     // Check fields before adding new ones
     Object.keys(fieldsToAdd).forEach((fieldNameToAdd) => {
@@ -82,25 +73,25 @@ const addressService = (addressFieldName = 'address', fieldsHooks = {}) => plugi
         // We will call to address service in the following cases:
         if (
             // 1. In the case of new property/resident creation
-            (operation === 'create' && !resolvedData[getKeyFieldName(addressFieldName)])
+            (operation === 'create' && !resolvedData['addressKey'])
 
             // 2. In the case of property/resident update and updating property/resident has no address key for some reason
-            || (operation === 'update' && !existingItem[getKeyFieldName(addressFieldName)])
+            || (operation === 'update' && !existingItem['addressKey'])
 
             // 3. In the case of update and if property/resident address has been changed
-            || (operation === 'update' && !!resolvedData[addressFieldName] && existingItem[addressFieldName] !== resolvedData[addressFieldName])
+            || (operation === 'update' && !!resolvedData['address'] && existingItem['address'] !== resolvedData['address'])
         ) {
             const client = conf.NODE_ENV === 'test' || get(conf, 'ADDRESS_SERVICE_CLIENT_MODE') === 'fake'
                 ? createTestAddressServiceClientInstance({ ...existingItem, ...resolvedData })
                 : createAddressServiceClientInstance(get(conf, 'ADDRESS_SERVICE_URL'))
 
-            const result = await client.search(get(resolvedData, ['addressMeta', 'rawValue'], resolvedData[addressFieldName]))
+            const result = await client.search(get(resolvedData, ['addressMeta', 'rawValue'], resolvedData['address']))
 
-            resolvedData[getSourceFieldName(addressFieldName)] = get(result, 'addressSources')
-            resolvedData[addressFieldName] = get(result, 'address')
-            resolvedData[getKeyFieldName(addressFieldName)] = get(result, 'addressKey')
+            resolvedData['addressSources'] = get(result, 'addressSources')
+            resolvedData['address'] = get(result, 'address')
+            resolvedData['addressKey'] = get(result, 'addressKey')
 
-            resolvedData[getMetaFieldName(addressFieldName)] = {
+            resolvedData['addressMeta'] = {
                 dv: 1,
                 value: get(result, ['addressMeta', 'value'], ''),
                 unrestricted_value: get(result, ['addressMeta', 'unrestricted_value'], ''),
