e62f1ea19654f479182184158402d0b7bccdd81e
Matthew Savelev <savelevmatthew@gmail.com>
2022-11-24 14:59:03 +0500

    fix(ui): DOMA-4762 Added path to analytics events

--- packages/ui/src/components/_utils/analytics.ts
+++ packages/ui/src/components/_utils/analytics.ts
@@ -4,23 +4,29 @@ type ComponentSpecificClickEventProps = {
     Button: { value: string }
 }
 
-type AnalyticsClickData<K extends keyof ComponentSpecificClickEventProps> = {
-    event: 'click'
-    component: K,
-} & ComponentSpecificClickEventProps[K]
+type CommonAnalyticsProps = {
+    event: 'click',
+    location: string,
+}
+
+type AnalyticsClickData<K extends keyof ComponentSpecificClickEventProps> = CommonAnalyticsProps & { component: K } & ComponentSpecificClickEventProps[K]
 
 const ANALYTICS_HANDLER_NAME = 'CondoWebSendAnalyticsEvent'
 
 export function sendAnalyticsClickEvent<K extends keyof ComponentSpecificClickEventProps> (component: K, data: ComponentSpecificClickEventProps[K]): void {
-    const params: AnalyticsClickData<K> = {
-        event: 'click',
-        component,
-        ...data,
+    if (typeof window !== 'undefined') {
+        const location = window.location.href
+        const params: AnalyticsClickData<K> = {
+            event: 'click',
+            location,
+            component,
+            ...data,
+        }
+        parent.postMessage({
+            handler: ANALYTICS_HANDLER_NAME,
+            params,
+            type: 'condo-ui',
+            version,
+        }, '*')
     }
-    parent.postMessage({
-        handler: ANALYTICS_HANDLER_NAME,
-        params,
-        type: 'condo-ui',
-        version,
-    }, '*')
 }
