a80f21f8d81dc8766b2ad9c312bc542282ab171c
Matthew Savelev <savelevmatthew@gmail.com>
2022-11-24 13:31:45 +0500

    fix(ui): DOMA-4762 Fixed markdown behaviour in SSR environment

--- packages/ui/src/components/Markdown/markdown.tsx
+++ packages/ui/src/components/Markdown/markdown.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactMarkdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
+import omit from 'lodash/omit'
 import { Typography } from '../Typography'
 
 const REMARK_PLUGINS = [
@@ -19,17 +20,16 @@ export const Markdown: React.FC<MarkdownProps> = ({ children }) => {
             className={MARKDOWN_CLASS_PREFIX}
             remarkPlugins={REMARK_PLUGINS}
             components={{
-                h1: (props) => <Typography.Title {...props} level={1}/>,
-                h2: (props) => <Typography.Title {...props} level={2}/>,
-                h3: (props) => <Typography.Title {...props} level={3}/>,
-                h4: (props) => <Typography.Title {...props} level={4}/>,
-                h5: (props) => <Typography.Title {...props} level={5}/>,
-                h6: (props) => <Typography.Title {...props} level={6}/>,
-                p: (props) => <Typography.Paragraph {...props} type='secondary' />,
-                a: (props) => <Typography.Link {...props} target='_blank'/>,
-                code: (props) => <Typography.Text {...props} code/>,
+                h1: (props) => <Typography.Title {...omit(props, 'ref')} level={1}/>,
+                h2: (props) => <Typography.Title {...omit(props, 'ref')} level={2}/>,
+                h3: (props) => <Typography.Title {...omit(props, 'ref')} level={3}/>,
+                h4: (props) => <Typography.Title {...omit(props, 'ref')} level={4}/>,
+                h5: (props) => <Typography.Title {...omit(props, 'ref')} level={5}/>,
+                h6: (props) => <Typography.Title {...omit(props, 'ref')} level={6}/>,
+                p: (props) => <Typography.Paragraph {...omit(props, 'ref')} type='secondary' />,
+                a: (props) => <Typography.Link {...omit(props, 'ref')} target='_blank'/>,
+                code: (props) => <Typography.Text {...omit(props, 'ref')} code/>,
                 li: ({ children, ...restProps }) => <li {...restProps}><Typography.Text type='secondary'>{children}</Typography.Text></li>,
-                em: 'i',
             }}
         >
             {children}
