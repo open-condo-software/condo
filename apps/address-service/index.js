bf940b3ed16a560b9c55c262830de39e3f76c6cb
AleX83Xpert <AleX83Xpert@mail.ru>
2022-11-26 18:05:13 +0500

    feat(address-service): DOMA-3580 Allow support users login to admin interface

--- apps/address-service/index.js
+++ apps/address-service/index.js
@@ -64,7 +64,7 @@ module.exports = {
         }),
         new AdminUIApp({
             adminPath: '/admin',
-            isAccessAllowed: access.userIsAdmin,
+            isAccessAllowed: access.userIsAdmin || access.userIsSupport,
             authStrategy,
             hooks: require.resolve('@app/address-service/admin-ui'),
         }),
