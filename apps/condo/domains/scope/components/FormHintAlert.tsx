9333c6a778d8282ee3f6a0784599f5c0a392b905
Pavel <nomerdvadcatpyat@gmail.com>
2022-12-01 13:37:57 +0500

    chore(condo): DOMA-4584 added tracking to alert link

--- apps/condo/domains/scope/components/FormHintAlert.tsx
+++ apps/condo/domains/scope/components/FormHintAlert.tsx
@@ -1,16 +1,23 @@
-import { useIntl } from '@open-condo/next/intl'
 import { Typography } from 'antd'
 import React, { CSSProperties, useMemo } from 'react'
 
+import { useIntl } from '@open-condo/next/intl'
+
+import { useTracking } from '@condo/domains/common/components/TrackingContext'
 import { Alert } from '@condo/domains/common/components/Alert'
 
 const ALERT_MESSAGE_STYLE: CSSProperties = { color: 'inherit', marginBottom: '4px' }
+const EMPLOYEES_LIST_LINK_CLICK_EVENT_NAME = 'PropertyScopeUpdateToEmployeeList'
 
 export const FormHintAlert = () => {
     const intl = useIntl()
     const AlertTitle = intl.formatMessage({ id: 'pages.condo.settings.propertyScope.form.alert.message' })
     const AlertDescriptionMessage = intl.formatMessage({ id: 'pages.condo.settings.propertyScope.form.alert.description' })
     const AlertDescriptionLink = intl.formatMessage({ id: 'pages.condo.settings.propertyScope.form.alert.descriptionLink' })
+
+    const { getTrackingWrappedCallback } = useTracking()
+    const handleLinkClick = useMemo(() => getTrackingWrappedCallback(EMPLOYEES_LIST_LINK_CLICK_EVENT_NAME),
+        [getTrackingWrappedCallback])
 
     const AlertMessage = useMemo(() => (
         <Typography.Paragraph strong style={ALERT_MESSAGE_STYLE}>
@@ -23,13 +30,13 @@ export const FormHintAlert = () => {
             <Typography.Paragraph>
                 {AlertDescriptionMessage}
             </Typography.Paragraph>
-            <Typography.Link underline href='/employee' target='_blank'>
+            <Typography.Link underline href='/employee' target='_blank' onClick={handleLinkClick}>
                 <Typography.Text>
                     {AlertDescriptionLink}
                 </Typography.Text>
             </Typography.Link>
         </>
-    ), [AlertDescriptionLink, AlertDescriptionMessage])
+    ), [AlertDescriptionLink, AlertDescriptionMessage, handleLinkClick])
 
     return (
         <Alert
