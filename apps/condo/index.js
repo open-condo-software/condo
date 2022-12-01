efbb214622671fad3a07354a7bfcf0007020652a
toplenboren <toplenboren@gmail.com>
2022-12-01 22:50:45 +0600

    refactor(condo): DOMA-2681 enable cache in index.js after rebase
    Signed-off-by: toplenboren <toplenboren@gmail.com>

--- apps/condo/index.js
+++ apps/condo/index.js
@@ -94,9 +94,6 @@ if (IS_ENABLE_CACHE) {
     keystoneCacheApp = new KeystoneCacheMiddleware(keystone)
 }
 
-const adapterCacheConfiig = conf.ADAPTER_CACHE_CONFIG
-let keystoneAdapterCacheApp = new AdapterCacheMiddleware(adapterCacheConfiig)
-
 // Because Babel is used only for frontend to transpile and optimise code,
 // backend files will bring unnecessary workload to building stage.
 // They can be safely ignored without impact on final executable code
@@ -165,7 +162,7 @@ module.exports = {
         new OIDCMiddleware(),
         new FeaturesMiddleware(),
         new PaymentLinkMiddleware(),
-        new AdapterCacheMiddleware(adapterCacheConfiig),
+        new AdapterCacheMiddleware(conf.ADAPTER_CACHE_CONFIG),
         new GraphQLApp({
             apollo: {
                 formatError,
