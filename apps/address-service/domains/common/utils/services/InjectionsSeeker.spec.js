b02b7a3c235e9fa7579b8e821cfb89791a6168e1
AleX83Xpert <AleX83Xpert@mail.ru>
2022-12-01 13:39:48 +0500

    fix(address-service): DOMA-4797 tests improved

--- apps/address-service/domains/common/utils/services/InjectionsSeeker.spec.js
+++ apps/address-service/domains/common/utils/services/InjectionsSeeker.spec.js
@@ -2,10 +2,10 @@ const { InjectionsSeeker } = require('@address-service/domains/common/utils/serv
 
 describe('Injections seeker', () => {
     test('Search parts extracted correctly', async () => {
-        const injectionsSeeker = new InjectionsSeeker('Some _- +== string with a comma: , ...  Hello world!')
+        const injectionsSeeker = new InjectionsSeeker('Some _- +== string with a comma: , ...  Hello world! 66')
 
         const searchParts = injectionsSeeker.extractSearchParts()
 
-        expect(searchParts).toEqual(['Some', 'string', 'with', 'comma', 'Hello', 'world'])
+        expect(searchParts).toEqual(['Some', 'string', 'with', 'comma', 'Hello', 'world', '66'])
     })
 })
