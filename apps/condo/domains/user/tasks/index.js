8f8314a5e74495633f399d04a3ad4361778721ec
ekabardinsky <ekabardinsky@gmail.com>
2022-12-02 15:41:57 +0500

    feat(condo): DOMA-4670 migrate User.importId data to UserExternalIdentity

---
+++ apps/condo/domains/user/tasks/index.js
@@ -0,0 +1,5 @@
+const migrateUserImportIdDataToUserExternalIdentity = require('./migrateUserImportIdDataToUserExternalIdentity')
+
+module.exports = {
+    migrateUserImportIdDataToUserExternalIdentity,
+}
