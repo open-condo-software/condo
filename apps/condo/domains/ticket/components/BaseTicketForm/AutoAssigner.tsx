f4389e82d9cb5900898df96806afbb6ddee9c5af
Pavel <nomerdvadcatpyat@gmail.com>
2022-12-06 12:23:47 +0500

    fix(condo): DOMA-4835 fix AutoAssigner
    (cherry picked from commit 71ef4d174d67417a4f039bf5744d921c74f7f67d)

--- apps/condo/domains/ticket/components/BaseTicketForm/AutoAssigner.tsx
+++ apps/condo/domains/ticket/components/BaseTicketForm/AutoAssigner.tsx
@@ -1,14 +1,14 @@
 import { Alert, Col } from 'antd'
-import { every, isEmpty } from 'lodash'
 import React, { useState } from 'react'
 
 import { useAuth } from '@open-condo/next/auth'
 import { useIntl } from '@open-condo/next/intl'
+import { useDeepCompareEffect } from '@open-condo/codegen/utils/useDeepCompareEffect'
 
 import {
-    getPropertyScopeNameByEmployee,
+    getEmployeesSortedByTicketVisibilityType,
+    getPropertyScopeNameByEmployee, isEmployeeSpecializationAndPropertyMatchesToScope,
 } from '@condo/domains/scope/utils/clientSchema/utils'
-import { useDeepCompareEffect } from '@open-condo/codegen/utils/useDeepCompareEffect'
 
 /**
  * Sets the employee user in the assignee and executor fields after selecting ticket category classifier.
@@ -20,10 +20,11 @@ import { useDeepCompareEffect } from '@open-condo/codegen/utils/useDeepCompareEf
 export const AutoAssigner = ({
     form,
     categoryClassifierId,
-    propertyId,
-    matchedEmployees,
+    employees,
+    allDataLoaded,
     propertyScopeEmployees,
     propertyScopes,
+    organizationEmployeeSpecializations,
 }) => {
     const intl = useIntl()
     const AutoAssignAlertTitle = intl.formatMessage({ id: 'pages.condo.ticket.autoAssignAlert.title' })
@@ -34,9 +35,25 @@ export const AutoAssigner = ({
     const [autoAssigneePropertyScopeName, setAutoAssigneePropertyScopeName] = useState<string>()
 
     useDeepCompareEffect(() => {
-        if (categoryClassifierId && propertyId) {
-            if (!isEmpty(matchedEmployees) && !every(matchedEmployees, employee => employee.isBlocked)) {
-                const firstEmployee = matchedEmployees.find(employee => !employee.isBlocked)
+        if (allDataLoaded) {
+            const employeesWithMatchesPropertyAndSpecializationScope = employees.filter(
+                isEmployeeSpecializationAndPropertyMatchesToScope(
+                    {
+                        categoryClassifierId,
+                        organizationEmployeeSpecializations,
+                        propertyScopes,
+                        propertyScopeEmployees,
+                    }
+                )
+            )
+
+            if (employeesWithMatchesPropertyAndSpecializationScope.length > 0) {
+                const sortedEmployees = getEmployeesSortedByTicketVisibilityType(
+                    employeesWithMatchesPropertyAndSpecializationScope,
+                    organizationEmployeeSpecializations,
+                    categoryClassifierId,
+                )
+                const firstEmployee = sortedEmployees.find(employee => !employee.isBlocked)
                 const firstEmployeeUserId = firstEmployee.user.id
 
                 form.setFieldsValue({
@@ -45,7 +62,6 @@ export const AutoAssigner = ({
                 })
 
                 const propertyScopeName = getPropertyScopeNameByEmployee(firstEmployee, propertyScopes, propertyScopeEmployees)
-
                 setAutoAssigneePropertyScopeName(propertyScopeName)
             } else {
                 const currentUserId = user.id
@@ -59,7 +75,8 @@ export const AutoAssigner = ({
             }
         }
     }, [
-        categoryClassifierId, propertyId, propertyScopes, propertyScopeEmployees, matchedEmployees,
+        allDataLoaded, categoryClassifierId, employees, form, organizationEmployeeSpecializations,
+        propertyScopeEmployees, propertyScopes, user.id,
     ])
 
     return autoAssigneePropertyScopeName ? (
