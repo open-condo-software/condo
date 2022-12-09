e942c3a5e85c43d1272c0a8f49e0e416a647c3d3
Kirik <lgstrwrs@gmail.com>
2022-12-09 07:56:37 +0500

    fix(condo): DOMA-4708 PR fixes

--- packages/ui/src/components/Alert/alert.tsx
+++ packages/ui/src/components/Alert/alert.tsx
@@ -10,12 +10,9 @@ const ALERT_CLASS_PREFIX = 'condo-alert'
 export type AlertProps = Pick<DefaultAlertProps, 'message' | 'showIcon' | 'description' | 'action'> & CondoAlertProps
 
 const Alert: React.FC<AlertProps> = (props) => {
-    const { type, ...rest } = props
-
     return (
         <DefaultAlert
-            {...rest}
-            type = {type}
+            {...props}
             prefixCls={ALERT_CLASS_PREFIX}
         />
     )
