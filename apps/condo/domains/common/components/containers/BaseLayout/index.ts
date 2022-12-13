f174646fef3db752ebb6f9f1c5941694991c6a47
Pavel <nomerdvadcatpyat@gmail.com>
2022-12-13 11:07:42 +0300

    feat(condo): DOMA-3744 used carousel from ui package

--- apps/condo/domains/common/components/containers/BaseLayout/index.ts
+++ apps/condo/domains/common/components/containers/BaseLayout/index.ts
@@ -1,4 +1,7 @@
 import BaseLayout, { PageContent, PageHeader, PageWrapper, useLayoutContext } from './BaseLayout'
+import classNames from 'classnames'
+import 'antd/dist/antd.less'
+import '@open-condo/ui/dist/styles.min.css'
 
 export default BaseLayout
 export {
