72f7ce5b110e25c9bd493fbf4f117c54a282873c
Matthew Savelev <savelevmatthew@gmail.com>
2022-12-13 12:15:25 +0500

    feat(condo): DOMA-4643 Added new index page

--- apps/condo/domains/miniapp/components/AppIndex/index.ts
+++ apps/condo/domains/miniapp/components/AppIndex/index.ts
@@ -1,11 +1,1 @@
-import { AppConnectedPageContent } from './AppConnectedPageContent'
-import { IndexBillingAppPage } from './IndexBillingAppPage'
-import { IndexAcquiringAppPage } from './IndexAcquiringAppPage'
-import {  IndexB2BAppPage } from './IndexB2BAppPage'
-
-export default AppConnectedPageContent
-export {
-    IndexBillingAppPage,
-    IndexAcquiringAppPage,
-    IndexB2BAppPage,
-}
+export { B2BAppPage } from './B2BAppPage'
