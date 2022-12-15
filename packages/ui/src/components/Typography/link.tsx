5332e0ed03a5f167eebee532950a986e8aa4bfb2
Matthew Savelev <savelevmatthew@gmail.com>
2022-12-15 12:20:23 +0500

    feat(ui): DOMA-4643 Added link analytics and some minor stories improvements

--- packages/ui/src/components/Typography/link.tsx
+++ packages/ui/src/components/Typography/link.tsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import Base from 'antd/lib/typography/Base'
 import classNames from 'classnames'
+import { sendAnalyticsClickEvent, extractChildrenContent } from '../_utils/analytics'
 import { TYPOGRAPHY_CLASS_PREFIX, TEXT_SIZES } from './constants'
 
 export type TypographyLinkProps = Omit<React.AnchorHTMLAttributes<HTMLAnchorElement>, 'type'> & {
@@ -14,12 +15,23 @@ export type TypographyLinkProps = Omit<React.AnchorHTMLAttributes<HTMLAnchorElem
 }
 
 const Link = React.forwardRef<HTMLElement, TypographyLinkProps>((props, ref) => {
-    const { size } = props
+    const { size, onClick, children, href } = props
     const className = classNames({
         [`${TYPOGRAPHY_CLASS_PREFIX}-${size}`]: size,
     })
     // NOTE: Used wrapper destructuring to explicitly pass component props, which is marked as internal in antd
     const componentProps = { component: 'a' }
+
+    const handleClick = useCallback((event) => {
+        const stringContent = extractChildrenContent(children)
+        if (stringContent) {
+            sendAnalyticsClickEvent('Typography.Link', { value: stringContent, href })
+        }
+
+        if (onClick) {
+            onClick(event)
+        }
+    }, [children, onClick, href])
 
     return (
         <Base
@@ -27,6 +39,7 @@ const Link = React.forwardRef<HTMLElement, TypographyLinkProps>((props, ref) => 
             className={className}
             prefixCls={TYPOGRAPHY_CLASS_PREFIX}
             ref={ref}
+            onClick={handleClick}
             {...componentProps}
         />
     )
