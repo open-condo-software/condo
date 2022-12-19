720238f70251344990e4a5fba7dceee9ed67ca67
Kirik <lgstrwrs@gmail.com>
2022-12-19 10:08:40 +0500

    feat(condo): DOMA-4711 added indeterminate prop and additional styles

--- packages/ui/src/components/Checkbox/checkbox.tsx
+++ packages/ui/src/components/Checkbox/checkbox.tsx
@@ -1,23 +1,31 @@
 import React from 'react'
 import { Checkbox as DefaultCheckbox, CheckboxProps as DefaultCheckboxProps } from 'antd'
+import { Typography } from '../Typography'
 
 const CHECKBOX_CLASS_PREFIX = 'condo-checkbox'
 
 type CondoCheckboxProps = {
     children?: string
+    boldLabel?: boolean
 }
 
-export type CheckboxProps = Pick<DefaultCheckboxProps, 'autoFocus' | 'defaultChecked' | 'disabled' | 'onChange'> & CondoCheckboxProps
+export type CheckboxProps = Pick<DefaultCheckboxProps, 'autoFocus' | 'defaultChecked' | 'disabled' | 'onChange' | 'indeterminate' | 'checked'> & CondoCheckboxProps
 
 const Checkbox: React.FC<CheckboxProps> = (props) => {
-    const { children, ...rest } = props
-
+    const { children, boldLabel, disabled, ...rest } = props
     return (
         <DefaultCheckbox
             {...rest}
             prefixCls={CHECKBOX_CLASS_PREFIX}
+            disabled={disabled}
         >
-            {children}
+            <Typography.Text
+                size='medium'
+                strong={boldLabel}
+                disabled={disabled}
+            >
+                {children}
+            </Typography.Text>
         </DefaultCheckbox>
     )
 }
