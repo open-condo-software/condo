453c3b13517a2f9a9c98a1707945627d5c6eebb9
Matthew Savelev <savelevmatthew@gmail.com>
2022-12-20 13:19:59 +0500

    hotfix(condo): DOMA-4993 Fixed billing redirects

--- apps/condo/domains/acquiring/components/payments/NoBillingStub.tsx
+++ apps/condo/domains/acquiring/components/payments/NoBillingStub.tsx
@@ -1,6 +1,7 @@
 import EmptyListView from '@condo/domains/common/components/EmptyListView'
 import React from 'react'
 import { useIntl } from '@open-condo/next/intl'
+import { ACCRUALS_AND_PAYMENTS_CATEGORY } from '@condo/domains/miniapp/constants'
 
 const NoBillingStub = (): JSX.Element => {
     const intl = useIntl()
@@ -8,12 +9,13 @@ const NoBillingStub = (): JSX.Element => {
     const noBillingLabel = intl.formatMessage({ id: 'pages.condo.payments.noBillingLabel' })
     const noBillingMessage = intl.formatMessage({ id: 'pages.condo.payments.noBillingMessage' })
     const createBillingLabel = intl.formatMessage({ id: 'pages.condo.payments.createBillingLabel' })
+    const createBillingRoute = `/miniapps?tab=${ACCRUALS_AND_PAYMENTS_CATEGORY}`
 
     return (
         <EmptyListView
             label={noBillingLabel}
             message={noBillingMessage}
-            createRoute='/miniapps?tab=billing'
+            createRoute={createBillingRoute}
             createLabel={createBillingLabel}
         />
     )
