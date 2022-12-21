df11d19c6fdaff391ca2f7f89b50b1f9508468cc
Matthew Savelev <savelevmatthew@gmail.com>
2022-12-21 12:23:51 +0500

    hotfix(condo): Returned support rights to visit billing miniapps temporally

--- apps/condo/domains/miniapp/components/AppIndex/BillingAppPage.tsx
+++ apps/condo/domains/miniapp/components/AppIndex/BillingAppPage.tsx
@@ -2,11 +2,9 @@ import React, { useEffect } from 'react'
 import get from 'lodash/get'
 import { useRouter } from 'next/router'
 import Head from 'next/head'
-import { useAuth } from '@open-condo/next/auth'
 import { useIntl } from '@open-condo/next/intl'
 import { useOrganization } from '@open-condo/next/organization'
 import { Typography } from '@open-condo/ui'
-import LoadingOrErrorPage from '@condo/domains/common/components/containers/LoadingOrErrorPage'
 import { PageContent, PageWrapper, PageHeader } from '@condo/domains/common/components/containers/BaseLayout'
 import IFrame from '@condo/domains/common/components/IFrame'
 import { BillingIntegrationOrganizationContext } from '@condo/domains/billing/utils/clientSchema'
@@ -20,12 +18,8 @@ type BillingAppPageProps = {
 export const BillingAppPage: React.FC<BillingAppPageProps> = ({ id }) => {
     const intl = useIntl()
     const FallbackPageTitle = intl.formatMessage({ id: 'global.section.miniapps' })
-    const NoPermissionMessage = intl.formatMessage({ id: 'global.noPageViewPermission' })
 
     const router = useRouter()
-    const auth = useAuth()
-    const isSupport = get(auth, ['user', 'isSupport'], false)
-    const isAdmin = get(auth, ['user', 'isAdmin'], false)
     const userOrganization = useOrganization()
     const organizationId = get(userOrganization, ['organization', 'id'], null)
 
@@ -46,9 +40,10 @@ export const BillingAppPage: React.FC<BillingAppPageProps> = ({ id }) => {
         }
     }, [id, loading, error, context, appUrl, router])
 
-    if (isSupport || isAdmin) {
-        return <LoadingOrErrorPage title={FallbackPageTitle} error={NoPermissionMessage}/>
-    }
+    // TODO(DOMA-5005): Return this restriction back till token scopes will be implemented
+    // if (isSupport || isAdmin) {
+    //     return <LoadingOrErrorPage title={FallbackPageTitle} error={NoPermissionMessage}/>
+    // }
 
 
     return (
