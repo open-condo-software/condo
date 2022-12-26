333bd23bc10622b51ff429d053c38c3ed4215883
Matthew Savelev <savelevmatthew@gmail.com>
2022-12-26 12:04:37 +0500

    build(global): DOMA-5044 changed publishing flow to multi-semantic-release

---
+++ bin/publish-packages.js
@@ -0,0 +1,12 @@
+const path = require('path')
+const multiRelease = require('multi-semantic-release')
+
+// NOTE: Edit this list to add / remove packages from auto-release cycle
+const RELEASE_LIST = [
+    'ui',
+]
+
+const releasePaths = RELEASE_LIST.map(pkgName => path.join(__dirname, '..', 'packages', pkgName, 'package.json'))
+
+multiRelease(releasePaths)
+    .then(() => console.log('ALL DONE! ✅'))
