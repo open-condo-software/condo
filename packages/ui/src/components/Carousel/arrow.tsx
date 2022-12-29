b4ad043a8bf8b73830f07f92d30443513866ec22
Kirik <lgstrwrs@gmail.com>
2022-12-29 14:49:24 +0500

    feat(ui): DOMA-5066 changed icon to the icon from the open-condo

--- packages/ui/src/components/Carousel/arrow.tsx
+++ packages/ui/src/components/Carousel/arrow.tsx
@@ -1,12 +1,11 @@
 import React from 'react'
+import { ChevronRight } from '@open-condo/icons'
 
 export const CarouselArrow: React.FC<React.HtmlHTMLAttributes<HTMLDivElement>> = (props) => {
     return (
         <div {...props}>
-            {/*TODO (DOMA-4666): Move to icons pack*/}
-            <svg width='9' height='14' fill='none' xmlns='http://www.w3.org/2000/svg'>
-                <path d='M1 2.374 5.414 7 1 11.626 2.293 13 8 7 2.293 1 1 2.374Z' fill='currentColor' stroke='currentColor' strokeWidth='.7'/>
-            </svg>
+            <ChevronRight
+            />
         </div>
     )
 }
