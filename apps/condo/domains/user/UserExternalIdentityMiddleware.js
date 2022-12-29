b349b5a22103b539207a902d90b1800289de275a
ekabardinsky <ekabardinsky@gmail.com>
2022-12-29 15:22:52 +0500

    feat(condo): DOMA-3995 implement generic UserExternalIdentity middleware and route

--- apps/condo/domains/user/UserExternalIdentityMiddleware.js
+++ apps/condo/domains/user/UserExternalIdentityMiddleware.js
@@ -1,15 +1,30 @@
 const express = require('express')
+const { expressErrorHandler } = require('@condo/domains/common/utils/expressErrorHandler')
 
+const { SbbolRoutes } = require('@condo/domains/organization/integrations/sbbol/routes')
 const { UserExternalIdentityRoute } = require('@condo/domains/user/routes/UserExternalIdentityRoute')
-const { USER_EXTERNAL_IDENTITY_CALLBACK_PATH } = require('@condo/domains/user/constants/links')
+
+const {
+    USER_EXTERNAL_IDENTITY_AUTH_PATH,
+    USER_EXTERNAL_IDENTITY_AUTH_CALLBACK_PATH,
+} = require('@condo/domains/user/constants/links')
 
 class UserExternalIdentityMiddleware {
     async prepareMiddleware ({ keystone }) {
         const app = express()
-        const router = new UserExternalIdentityRoute()
-        await router.init()
 
-        app.get(USER_EXTERNAL_IDENTITY_CALLBACK_PATH, router.handleRequest.bind(router))
+        // sbbol route
+        const Auth = new SbbolRoutes()
+        app.get('/api/sbbol/auth', Auth.startAuth.bind(Auth))
+        app.get('/api/sbbol/auth/callback', Auth.completeAuth.bind(Auth))
+
+        // generic router
+        const router = new UserExternalIdentityRoute()
+        app.get(USER_EXTERNAL_IDENTITY_AUTH_PATH, router.startAuth.bind(router))
+        app.get(USER_EXTERNAL_IDENTITY_AUTH_CALLBACK_PATH, router.completeAuth.bind(router))
+
+        // error handler
+        app.use(expressErrorHandler)
 
         return app
     }
