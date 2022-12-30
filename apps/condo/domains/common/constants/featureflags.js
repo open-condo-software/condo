0ef293b52bcfe69fc5e3a55bbe348bd75f6cd158
Sitnikov Ivan <sit7602@gmail.com>
2022-12-31 00:59:46 +0600

    feat(condo): DOMA-5050 add list and card to property

--- apps/condo/domains/common/constants/featureflags.js
+++ apps/condo/domains/common/constants/featureflags.js
@@ -7,6 +7,7 @@ const TICKET_IMPORT = 'ticket-import'
 const PAYMENT_LINK = 'payment-link'
 const MAX_COUNT_COMPLETED_TICKET_TO_CLOSE_FOR_ORGANIZATION_TASK = 'max-count-completed-ticket-to-close-for-organization-task'
 const HUAWEI_SILENT_DATA_PUSH_ENABLED = 'huawei-silent-data-push-enabled'
+const PROPERTY_BANK_ACCOUNT = 'property-bank-account'
 
 module.exports = {
     SMS_AFTER_TICKET_CREATION,
@@ -18,4 +19,5 @@ module.exports = {
     PAYMENT_LINK,
     MAX_COUNT_COMPLETED_TICKET_TO_CLOSE_FOR_ORGANIZATION_TASK,
     HUAWEI_SILENT_DATA_PUSH_ENABLED,
+    PROPERTY_BANK_ACCOUNT,
 }
