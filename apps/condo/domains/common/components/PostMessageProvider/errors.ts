8f2426d7609dd02c28169deeefc2b507192471d9
Matthew Savelev <savelevmatthew@gmail.com>
2023-01-13 08:16:55 +0500

    feat(condo): DOMA-5106 Added PostMessageProvider proof of work

---
+++ apps/condo/domains/common/components/PostMessageProvider/errors.ts
@@ -0,0 +1,18 @@
+// TODO(DOMA-5122): Provide documentation
+export const ACCESS_DENIED = 'ACCESS_DENIED'
+export const UNKNOWN_ERROR = 'UNKNOWN_ERROR'
+export const UNKNOWN_METHOD = 'UNKNOWN_METHOD'
+export const INVALID_PARAMETERS = 'INVALID_PARAMETERS'
+export const HANDLER_ERROR = 'HANDLER_ERROR'
+
+export const ERROR_REASONS = [ACCESS_DENIED, UNKNOWN_ERROR, UNKNOWN_METHOD, INVALID_PARAMETERS, HANDLER_ERROR] as const
+export type ErrorReason = typeof ERROR_REASONS[number]
+export const ERROR_CODES: { [reason in ErrorReason]: number } = {
+    ACCESS_DENIED:0,
+    UNKNOWN_ERROR: 1,
+    UNKNOWN_METHOD: 2,
+    INVALID_PARAMETERS: 3,
+    HANDLER_ERROR: 4,
+} as const
+
+export type ErrorCode<Reason extends ErrorReason> = typeof ERROR_CODES[Reason]
