0a41914437a7f575f140dc611cf90ca58ac94eae
Matthew Savelev <savelevmatthew@gmail.com>
2023-01-13 08:16:55 +0500

    feat(condo): DOMA-5106 Added PostMessageProvider proof of work

--- apps/condo/domains/common/components/PostMessageProvider/Provider.tsx
+++ apps/condo/domains/common/components/PostMessageProvider/Provider.tsx
@@ -2,9 +2,21 @@ import React, { useEffect, createContext, useState, useContext, useCallback } fr
 import Ajv from 'ajv'
 import type { JSONSchemaType } from 'ajv'
 import getConfig from 'next/config'
-import type { AllRequestMethods, RequestParams, RequestHandler } from './types'
-import { validators } from './validators'
+import omit from 'lodash/omit'
+import get from 'lodash/get'
+import type {
+    AllRequestMethods,
+    RequestParams,
+    RequestHandler,
+    ClientErrorResponse,
+    RequestId,
+    RequestIdType,
+} from './types'
 import type { ValidatorsType } from './validators'
+import type{ ErrorReason } from './errors'
+import { validators } from './validators'
+import { ERROR_CODES } from './errors'
+
 
 const {
     publicRuntimeConfig: {
@@ -21,23 +33,36 @@ type RegisterHandler = <Method extends AllRequestMethods>(
     handler: RequestHandler<Method>
 ) => void
 
+/**
+ * Context definitions
+ */
 type IPostMessageContext = {
     allowedOrigins: Readonly<Array<string>>
+    registerOrigin: (origin: string) => void
+    resetOrigin: (origin: string) => void
     handlers: Readonly<Record<HandlerOrigin, OriginHandlers>>
     registerHandler: RegisterHandler
     validators: Readonly<ValidatorsType>
 }
 
+/**
+ * Mocks in case Provider was not provided by DOM
+ */
 const PostMessageContext = createContext<IPostMessageContext>({
     allowedOrigins: [serverUrl],
+    registerOrigin: () => ({}),
+    resetOrigin: () => ({}),
     handlers: {},
     registerHandler: () => ({}),
     validators,
 })
+/**
+ * Common incoming message pattern and its schema
+ */
 
 type MessageType = {
     handler: string
-    params: Record<string, unknown>
+    params: RequestId & Record<string, unknown>
     type: 'condo-bridge' | 'condo-ui'
     version: string
 }
@@ -56,13 +81,35 @@ const messageSchema: JSONSchemaType<MessageType> = {
 }
 const validateMessage = ajv.compile(messageSchema)
 
+function getClientErrorMessage<Method extends AllRequestMethods> (
+    reason: ErrorReason,
+    message: string,
+    requestId?: RequestIdType,
+    method?: Method
+): ClientErrorResponse<AllRequestMethods, ErrorReason> {
+    return {
+        type: method ? `${method}Error` : 'CondoWebAppCommonError',
+        data: {
+            errorType: 'client',
+            errorCode: ERROR_CODES[reason],
+            errorReason: reason,
+            errorMessage: message,
+            ...(typeof requestId !== 'undefined' ? { requestId } : null),
+        },
+    }
+}
+
 /**
  * Single place to handle all incoming postMessages from condo and its miniapps.
- * Checks that message has correct type and sender, then emit
- * Possible sources: condo-bridge, condo-ui
+ * You can register global handler for all origins by specifying "*" as origin (Example: notification)
+ * Or pass "per origin" handler (Example: window resize)
+ * Provider takes care of 3 things:
+ * 1. Making sure messages are trusted and coming from allowed Windows
+ * 2. Making sure message can be handled and passing parameters are valid
+ * 3. Handling errors thrown by individual handlers and converting it to corresponding postMessage response
  */
 export const PostMessageProvider: React.FC = ({ children }) => {
-    const [allowedOrigins] = useState<Array<string>>([serverUrl])
+    const [allowedOrigins, setAllowedOrigins] = useState<Array<string>>([serverUrl])
     const [registeredHandlers, setRegisteredHandlers] = useState<Record<HandlerOrigin, OriginHandlers>>({})
     const isOnClient = typeof window !== 'undefined'
 
@@ -78,17 +125,79 @@ export const PostMessageProvider: React.FC = ({ children }) => {
         })
     }, [])
 
+    const registerOrigin = useCallback((origin: string) => {
+        setAllowedOrigins((prev) => prev.includes(origin) ? prev : [...prev, origin])
+    }, [])
+
+    const resetOrigin = useCallback((origin: string) => {
+        setAllowedOrigins((prev) => prev.filter(element => element !== origin))
+        setRegisteredHandlers((prev) => omit(prev, origin))
+    }, [])
+
     const handleMessage = useCallback((event: MessageEvent) => {
-        if (!event.isTrusted) {
+        if (!event.isTrusted || !event.source ||
+            event.source instanceof ServiceWorker || event.source instanceof MessagePort) {
             return
         }
 
         // If not valid message interface - skip. It can be system events, like webpack or some other legacy messages
         if (validateMessage(event.data)) {
             const message = event.data
-            console.log(message)
+            const { requestId, ...params } = message.params
+            const origin = event.origin
+            // Validator keys contains all handlers, so string cast can be performed safely
+            const method: AllRequestMethods | undefined = Object.keys(validators).includes(message.handler)
+                ? message.handler as AllRequestMethods
+                : undefined
+
+            if (!method) {
+                return event.source.postMessage(
+                    getClientErrorMessage('UNKNOWN_METHOD', 'Unknown method was provided. Make sure your runtime environment supports it.', requestId),
+                    event.origin,
+                )
+            }
+
+            if (!allowedOrigins.includes(origin)) {
+                return event.source.postMessage(
+                    getClientErrorMessage('ACCESS_DENIED', 'Message was received from unregistered origin', requestId),
+                    event.origin,
+                )
+            }
+
+            const localHandler = get(registeredHandlers, [origin, method])
+            const globalHandler = get(registeredHandlers, ['*', method])
+            const handler = localHandler || globalHandler
+            if (!handler) {
+                return event.source.postMessage(
+                    getClientErrorMessage('UNKNOWN_ERROR', 'There\'s no handler for this type of event. This is most likely a bug. We would be glad if you let us know about it', requestId),
+                    event.origin
+                )
+            }
+
+            const validator = validators[method]
+            if (validator(params)) {
+                try {
+                    const result = handler(params)
+                    return event.source.postMessage({
+                        type: `${method}Result`,
+                        data: {
+                            ...result,
+                            ...(typeof requestId !== 'undefined' ? { requestId } : null),
+                        },
+                    })
+                } catch (err) {
+                    return event.source.postMessage(
+                        getClientErrorMessage('HANDLER_ERROR', err.message, requestId, method)
+                    )
+                }
+            } else {
+                return event.source.postMessage(
+                    getClientErrorMessage('INVALID_PARAMETERS', JSON.stringify(validator.errors), requestId, method),
+                    event.origin
+                )
+            }
         }
-    }, [])
+    }, [allowedOrigins, registeredHandlers])
 
     useEffect(() => {
         if (isOnClient) {
@@ -99,7 +208,14 @@ export const PostMessageProvider: React.FC = ({ children }) => {
     }, [isOnClient, handleMessage])
 
     return (
-        <PostMessageContext.Provider value={{ allowedOrigins, handlers: registeredHandlers, registerHandler, validators }}>
+        <PostMessageContext.Provider value={{
+            allowedOrigins,
+            registerOrigin,
+            resetOrigin,
+            handlers: registeredHandlers,
+            registerHandler,
+            validators,
+        }}>
             {children}
         </PostMessageContext.Provider>
     )
--- apps/condo/domains/common/components/PostMessageProvider/types.ts
+++ apps/condo/domains/common/components/PostMessageProvider/types.ts
@@ -1,10 +1,37 @@
 import type { ValidateFunction } from 'ajv'
+import type { ErrorCode, ErrorReason } from './errors'
 
+export const COMMON_ERROR_PREFIX = 'CondoWebAppCommonError' as const
+
+// TODO(DOMA-5124): Inherit types from corresponding packages (bridge / ui) to reduce code duplication
+//  after migrating to some monorepo dep-graph build tool, since currently rebuilding everything takes time
 export type RequestParamsMap = {
+    CondoWebAppResizeWindow: { height: number }
+}
+
+export type HandlerResultsMap = {
     CondoWebAppResizeWindow: { height: number }
 }
 
 export type AllRequestMethods = keyof RequestParamsMap
 export type RequestParams<Method extends AllRequestMethods> = RequestParamsMap[Method]
-export type RequestHandler<Method extends AllRequestMethods> = (params: RequestParams<Method>) => Record<string, unknown>
+export type HandlerResult<Method extends AllRequestMethods> = HandlerResultsMap[Method]
+export type RequestHandler<Method extends AllRequestMethods> = (params: RequestParams<Method>) => HandlerResult<Method>
 export type RequestParamValidator<Method extends AllRequestMethods> = ValidateFunction<RequestParams<Method>>
+export type RequestIdType = string | number
+export type RequestId = { requestId?: RequestIdType }
+type ResponseEventNames<T extends AllRequestMethods, R extends string, E extends string> = Record<T, {
+    result: R,
+    error: E
+}>
+export type ResponseEventNamesMap = ResponseEventNames<'CondoWebAppResizeWindow', 'CondoWebAppResizeWindowResult', 'CondoWebAppResizeWindowError'>
+
+export type ClientErrorResponse<Method extends AllRequestMethods, Reason extends ErrorReason> = {
+    type: ResponseEventNamesMap[Method]['error'] | typeof COMMON_ERROR_PREFIX
+    data: {
+        errorType: 'client'
+        errorCode: ErrorCode<Reason>
+        errorReason: Reason
+        errorMessage: string
+    } & RequestId
+}
