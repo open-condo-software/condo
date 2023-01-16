e575a9a96b5ba2b8aca709b957462f2156660cb1
AleX83Xpert <AleX83Xpert@mail.ru>
2023-01-16 16:18:21 +0500

    feat(address-service): DOMA-4706 The plugin to searching by fiasId

--- apps/address-service/domains/common/utils/services/search/plugins/index.js
+++ apps/address-service/domains/common/utils/services/search/plugins/index.js
@@ -3,6 +3,7 @@ const { SearchByProvider } = require('./SearchByProvider')
 const { SearchByAddressKey } = require('./SearchByAddressKey')
 const { SearchByInjectionId } = require('./SearchByInjectionId')
 const { SearchByGooglePlaceId } = require('./SearchByGooglePlaceId')
+const { SearchByFiasId } = require('./SearchByFiasId')
 
 module.exports = {
     SearchBySource,
@@ -10,4 +11,5 @@ module.exports = {
     SearchByAddressKey,
     SearchByInjectionId,
     SearchByGooglePlaceId,
+    SearchByFiasId,
 }
