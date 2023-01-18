62c4a2c05cc7f28ffeae5a89e7e6bbca624f53f3
SavelevMatthew <savelevmatthew@gmail.com>
2023-01-18 09:47:02 +0500

    feat(bridge): DOMA-3534 Added GetCurrentUser method

--- packages/bridge/src/types/methods/index.ts
+++ packages/bridge/src/types/methods/index.ts
@@ -1,13 +1,14 @@
-export type ShowNotificationParams = {
-    type: 'info' | 'warning' | 'error' | 'success'
-    message: string
-    description?: string
-}
+// Imports for usage here, reexports for accessibility from outside
+import type { ShowNotificationParams } from './ShowNotification'
+export type { ShowNotificationParams } from './ShowNotification'
+import type { GetCurrentUserData } from './GetCurrentUser'
+export type { GetCurrentUserData } from './GetCurrentUser'
 
 /**
  * Mapping for event -> request payload
  */
 export type RequestMethodsParamsMap = {
+    CondoWebAppGetCurrentUser: Record<string, never>
     CondoWebAppResizeWindow: { height: number }
     CondoWebAppShowNotification: ShowNotificationParams
 }
@@ -16,6 +17,7 @@ export type RequestMethodsParamsMap = {
  * Mapping for event -> response data (what method returns unwrapped)
  */
 export type ResultResponseDataMap = {
+    CondoWebAppGetCurrentUser: GetCurrentUserData
     CondoWebAppResizeWindow: { height: number }
     CondoWebAppShowNotification: { success: boolean }
 }
@@ -29,4 +31,5 @@ type ResponseEventNames<T extends keyof RequestMethodsParamsMap, R extends strin
  * Mapping for event -> success / failed response names
  */
 export type ResponseEventNamesMap = ResponseEventNames<'CondoWebAppResizeWindow', 'CondoWebAppResizeWindowResult', 'CondoWebAppResizeWindowError'> &
-ResponseEventNames<'CondoWebAppShowNotification', 'CondoWebAppShowNotificationResult', 'CondoWebAppShowNotificationError'>
+ResponseEventNames<'CondoWebAppShowNotification', 'CondoWebAppShowNotificationResult', 'CondoWebAppShowNotificationError'> &
+ResponseEventNames<'CondoWebAppGetCurrentUser', 'CondoWebAppGetCurrentUserResult', 'CondoWebAppGetCurrentUserError'>
