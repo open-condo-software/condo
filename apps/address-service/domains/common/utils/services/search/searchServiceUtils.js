3e95ce5de2559b96a157ab57ed0ba0afcd1e03d0
AleX83Xpert <AleX83Xpert@mail.ru>
2023-01-20 12:37:03 +0500

    refactor(address-service): DOMA-5144 Function parameters were renamed
    (cherry picked from commit 946e454c229f5e08f9355ccbcc6aa9fdee6a3c0a)

--- apps/address-service/domains/common/utils/services/search/searchServiceUtils.js
+++ apps/address-service/domains/common/utils/services/search/searchServiceUtils.js
@@ -1,42 +1,42 @@
 /**
  * @param context Keystone context
- * @param addressGql
- * @param addressSourceGql
+ * @param addressServerUtils
+ * @param addressSourceServerUtils
  * @param {{ address: string, key: string, meta: NormalizedBuilding }} addressData
  * @param {string} addressSource
  * @param {{ dv: number, sender: { dv: number, fingerprint: string } }} dvSender
  */
-async function createOrUpdateAddressWithSource (context, addressGql, addressSourceGql, addressData, addressSource, dvSender) {
+async function createOrUpdateAddressWithSource (context, addressServerUtils, addressSourceServerUtils, addressData, addressSource, dvSender) {
     const { key } = addressData
 
     //
     // Address
     //
-    let addressItem = await addressGql.getOne(context, { key })
+    let addressItem = await addressServerUtils.getOne(context, { key })
 
     if (addressItem) {
-        addressItem = await addressGql.update(context, addressItem.id, {
+        addressItem = await addressServerUtils.update(context, addressItem.id, {
             ...dvSender,
             deletedAt: null, // Restore deleted address on demand
         })
     } else {
-        addressItem = await addressGql.create(context, { ...dvSender, ...addressData })
+        addressItem = await addressServerUtils.create(context, { ...dvSender, ...addressData })
     }
 
     //
     // Address source
     //
-    const addressSourceItem = await addressSourceGql.getOne(context, { source: addressSource })
+    const addressSourceItem = await addressSourceServerUtils.getOne(context, { source: addressSource })
 
     if (addressSourceItem) {
-        await addressSourceGql.update(context, addressSourceItem.id, {
+        await addressSourceServerUtils.update(context, addressSourceItem.id, {
             ...dvSender,
             source: addressSource,
             address: { connect: { id: addressItem.id } },
             deletedAt: null, // Restore deleted address source on demand
         })
     } else {
-        await addressSourceGql.create(
+        await addressSourceServerUtils.create(
             context,
             {
                 ...dvSender,
