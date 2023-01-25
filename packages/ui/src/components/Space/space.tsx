b18f5be6d8837b1b80fcc8706bea1f8a64d40afe
Sitnikov Ivan <sit7602@gmail.com>
2023-01-25 12:58:24 +0500

    feat(ui): DOMA-5179 add Space component

---
+++ packages/ui/src/components/Space/space.tsx
@@ -0,0 +1,19 @@
+import { Space as DefaultSpace } from 'antd'
+import React from 'react'
+
+import type { SpaceProps as DefaultSpaceProps } from 'antd'
+
+declare const SpaceSize: [8, 12, 16, 20, 24, 40, 52, 60]
+
+export type SpaceProps = {
+    size: typeof SpaceSize[number]
+} & Pick<DefaultSpaceProps, 'direction' | 'align' | 'wrap'>
+
+const SPACE_CLASS_PREFIX = 'condo-space'
+
+const Space: React.FC<SpaceProps> = (props) => {
+
+    return <DefaultSpace prefixCls={SPACE_CLASS_PREFIX} {...props} />
+}
+
+export { Space }
