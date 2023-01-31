e899d77ecbfc7c5194383c60b04f8e7964bdf860
SavelevMatthew <savelevmatthew@gmail.com>
2023-01-31 13:47:26 +0500

    feat(dev-portal): DOMA-5203 integrated full-ts intl

--- apps/dev-portal/next.config.js
+++ apps/dev-portal/next.config.js
@@ -1,9 +1,15 @@
+const path = require('path')
+
+const { requireTs } = require('./domains/common/utils/requireTs')
+const localesPath = path.resolve(__dirname, 'domains/common/constants/locales.ts')
+const { LOCALES, DEFAULT_LOCALE } = requireTs(localesPath)
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
     i18n: {
-        locales: ['en', 'ru'],
-        defaultLocale: 'ru',
+        locales: LOCALES,
+        defaultLocale: DEFAULT_LOCALE,
     },
 }
 
--- apps/dev-portal/pages/_app.tsx
+++ apps/dev-portal/pages/_app.tsx
@@ -1,8 +1,40 @@
+import { LOCALES, DEFAULT_LOCALE } from 'domains/common/constants/locales'
+import en from 'lang/en.json'
+import ru from 'lang/ru.json'
+import get from 'lodash/get'
+import { IntlProvider } from 'react-intl'
+
+
 import type { AppProps } from 'next/app'
 import type { ReactNode } from 'react'
 
-export default function App ({ Component, pageProps }: AppProps): ReactNode {
+type AvailableLocales = typeof LOCALES[number]
+// NOTE: Combine all keys together
+type MessagesKeysType = keyof typeof en | keyof typeof ru
+// NOTE: Require all message keys in all languages, so no lint translations needed
+type MessagesType = { [Locale in AvailableLocales]: { [Key in MessagesKeysType]: string } }
+
+const MESSAGES: MessagesType = {
+    ru,
+    en,
+}
+
+// NOTE: Override global interface allows us to use autocomplete in intl
+declare global {
+    // eslint-disable-next-line @typescript-eslint/no-namespace
+    namespace FormatjsIntl {
+        interface Message {
+            ids: MessagesKeysType
+        }
+    }
+}
+
+export default function App ({ Component, pageProps, router }: AppProps): ReactNode {
+    const { locale = DEFAULT_LOCALE } = router
+
     return (
-        <Component {...pageProps}/>
+        <IntlProvider locale={locale} messages={get(MESSAGES, locale, {})}>
+            <Component {...pageProps}/>
+        </IntlProvider>
     )
 }
