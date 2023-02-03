f70a27dac3ab5a7a52363b73ded8884898e4a752
akarjakin <akarjakin@doma.ai>
2023-02-03 15:32:16 +0500

    fix(condo): DOMA-5131 fix script payload

--- apps/condo/bin/notification/send-incoming-voip-call-notification.js
+++ apps/condo/bin/notification/send-incoming-voip-call-notification.js
@@ -4,7 +4,7 @@ const { isEmpty } = require('lodash')
  * Sends incoming VoIP call notification
  *
  * Usage:
- *      yarn workspace @app/condo node ./bin/notification/send-incoming-voip-call-notification <USER_ID> <MINI_APP_ID> <CALL_ID>
+ *      yarn workspace @app/condo node ./bin/notification/send-incoming-voip-call-notification <USER_ID> <B2C_APP_ID> <CALL_ID>
  */
 
 const { sendIncomingVoIPCallNotification } = require('@condo/domains/notification/helpers')
@@ -14,13 +14,13 @@ const { connectKeystone } = require('../lib/keystone.helpers')
 async function main () {
     const keystone = await connectKeystone()
     const userId = process.argv[2]
-    const miniAppId = process.argv[3]
+    const B2CAppId = process.argv[3]
     const callId = process.argv[4]
     const extraAttrs = process.argv.slice(5)
 
     if (!isEmpty(extraAttrs)) console.info('Extra attrs will be omitted:', extraAttrs)
 
-    await sendIncomingVoIPCallNotification({ userId, miniAppId, callId })
+    await sendIncomingVoIPCallNotification({ userId, B2CAppId, callId })
 
     keystone.disconnect()
 }
