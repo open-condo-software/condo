f38fe994c7cdc8f2ef1963ff1100db75b63fa65e
SavelevMatthew <savelevmatthew@gmail.com>
2023-02-07 12:54:33 +0500

    feat(dev-portal): DOMA-5232 Base content layout added

--- apps/dev-portal/pages/docs/[...path].tsx
+++ apps/dev-portal/pages/docs/[...path].tsx
@@ -1,4 +1,4 @@
-import { Layout, Menu } from 'antd'
+import { Layout, Menu, Row, Col } from 'antd'
 import { DEFAULT_LOCALE } from 'domains/common/constants/locales'
 import { useMenuItems } from 'domains/docs/hooks/useMenuItems'
 import { getNavTree, getAllRoutes, getFlatArticles } from 'domains/docs/utils/routing'
@@ -6,25 +6,39 @@ import get from 'lodash/get'
 import { useRouter } from 'next/router'
 import React, { CSSProperties, useMemo } from 'react'
 
+import { Typography } from '@open-condo/ui'
+import { colors } from '@open-condo/ui/colors'
+
+import styles from './path.module.css'
+
+import type { RowProps } from 'antd'
 import type { NavItem, ArticleInfo } from 'domains/docs/utils/routing'
 import type { GetStaticPaths, GetStaticProps } from 'next'
 
 const DOCS_ROOT_FOLDER = 'docs'
 const DOCS_ROOT_ENDPOINT = '/docs'
 const SIDER_WIDTH = 336
+const TITLE_GUTTER: RowProps['gutter'] = [40, 40]
+
+const SIDER_STYLES: CSSProperties = {
+    minHeight: 'calc(100vh - 72px)',
+    padding: '28px 20px',
+}
+
+const CONTENT_STYLES: CSSProperties = {
+    minHeight: 'calc(100vh - 72px)',
+    padding: '40px 48px 0 20px',
+    background: colors.white,
+}
 
 type DocPageProps = {
     navigation: Array<NavItem>
+    pageTitle: string
     nextPage: ArticleInfo | null
     prevPage: ArticleInfo | null
 }
 
-const STATIC_SIDER_STYLES: CSSProperties = {
-    minHeight: 'calc(100vh - 72px)',
-    padding: 20,
-}
-
-const DocPage: React.FC<DocPageProps> = ({ navigation }) => {
+const DocPage: React.FC<DocPageProps> = ({ navigation, pageTitle }) => {
     const router = useRouter()
     const currentRoute = router.asPath.split('?')[0]
     const menuItems = useMenuItems(navigation, DOCS_ROOT_ENDPOINT)
@@ -42,8 +56,8 @@ const DocPage: React.FC<DocPageProps> = ({ navigation }) => {
     }, [currentRoute])
 
     return (
-        <Layout hasSider={true}>
-            <Layout.Sider width={SIDER_WIDTH} style={{ ...STATIC_SIDER_STYLES }} theme='light'>
+        <Layout hasSider>
+            <Layout.Sider width={SIDER_WIDTH} style={SIDER_STYLES} theme='light'>
                 <Menu
                     mode='inline'
                     items={menuItems}
@@ -52,10 +66,20 @@ const DocPage: React.FC<DocPageProps> = ({ navigation }) => {
                     selectedKeys={[currentRoute]}
                 />
             </Layout.Sider>
-            <Layout.Content>
-                <pre>
-                    {JSON.stringify(navigation, null, 2)}
-                </pre>
+            <Layout.Content style={CONTENT_STYLES}>
+                <div className={styles.pageContainer}>
+                    <div className={styles.articleColumn}>
+                        <Row gutter={TITLE_GUTTER}>
+                            <Col span={24}>
+                                <Typography.Title>{pageTitle}</Typography.Title>
+                            </Col>
+                            <Col span={24}>
+                                <Typography.Text>123123</Typography.Text>
+                            </Col>
+                        </Row>
+                    </div>
+                    <div className={styles.tableOfContentsColumn}></div>
+                </div>
             </Layout.Content>
         </Layout>
     )
@@ -86,6 +110,7 @@ export const getStaticProps: GetStaticProps<DocPageProps, GetStaticPathParams> =
     const route = get(params, 'path', []).join('/')
     const pageIndex = articles.findIndex((item) => item.route === route)
 
+    const pageTitle = articles[pageIndex].label
     const prevPage = pageIndex > 0 ? articles[pageIndex - 1] : null
     const nextPage = (pageIndex < articles.length - 1 && pageIndex != -1) ? articles[pageIndex + 1] : null
 
@@ -94,6 +119,7 @@ export const getStaticProps: GetStaticProps<DocPageProps, GetStaticPathParams> =
             navigation: navTree,
             prevPage,
             nextPage,
+            pageTitle,
         },
     }
 }
