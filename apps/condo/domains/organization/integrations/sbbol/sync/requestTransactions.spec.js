203f68510c9a589f4d84fdab7131a798194d8e90
VKislov <wkislov0@gmail.com>
2023-02-07 11:23:45 +0500

    test(condo): DOMA-4800 tests write

--- apps/condo/domains/organization/integrations/sbbol/sync/requestTransactions.spec.js
+++ apps/condo/domains/organization/integrations/sbbol/sync/requestTransactions.spec.js
@@ -6,16 +6,13 @@ const dayjs = require('dayjs')
 const get = require('lodash/get')
 const { v4: uuid, validate: uuidValidate } = require('uuid')
 
-
 const { setFakeClientMode, makeLoggedInAdminClient } = require('@open-condo/keystone/test.utils')
-
-
-
 
 const { BANK_INTEGRATION_IDS } = require('@condo/domains/banking/constants')
 const { BankIntegration, createTestBankIntegrationContext, BankAccount } = require('@condo/domains/banking/utils/testSchema')
 const { createValidRuRoutingNumber, createValidRuNumber } = require('@condo/domains/banking/utils/testSchema/bankAccount')
 const { RUSSIA_COUNTRY } = require('@condo/domains/common/constants/countries')
+const { initSbbolFintechApi } = require('@condo/domains/organization/integrations/sbbol/SbbolFintechApi')
 const { requestTransactions } = require('@condo/domains/organization/integrations/sbbol/sync/requestTransactions')
 const { createTestOrganization, Organization, generateTin } = require('@condo/domains/organization/utils/testSchema')
 const { makeLoggedInClient, User } = require('@condo/domains/user/utils/testSchema')
@@ -26,20 +23,28 @@ const { dvSenderFields } = require('../constants')
 
 const { keystone } = index
 
+jest.mock('@condo/domains/organization/integrations/sbbol/SbbolFintechApi',  () => {
+    class SbbolFintechApi {
+        requestCount = 0
 
-jest.mock('@condo/domains/organization/integrations/sbbol/SbbolFintechApi', async () => {
-    const originalModule = jest.requireActual('@condo/domains/organization/integrations/sbbol/SbbolFintechApi')
-    const requestMock = {
-        getStatementTransactions: jest.fn(async () => {
-            return await Promise.resolve({ data: MockSbbolResponses.getStatementTransactions(), statusCode: 200 })
-        }),
+        async getStatementTransactions (accountNumber, statementDate, page = 1) {
+            if (this.requestCount === 0) {
+                setTimeout(() => {
+                    this.requestCount++
+                }, 3000)
+                return Promise.resolve({ error: { cause: 'STATEMENT_RESPONSE_PROCESSING' }, statusCode: 202 })
+            } else {
+                this.requestCount = 0
+                return Promise.resolve({ data: MockSbbolResponses.getStatementTransactions(), statusCode: 200 })
+            }
+        }
     }
+
     return {
-        ...originalModule,
-        getStatementTransactions: async () => ({
-            ...requestMock,
-            child: async () => requestMock,
+        initSbbolFintechApi: jest.fn().mockImplementation( () => {
+            return new SbbolFintechApi()
         }),
+        SbbolFintechApi,
     }
 })
 
@@ -48,6 +53,8 @@ describe('syncBankAccount from SBBOL', () => {
 
     let adminClient, commonClient, adminContext, context, commonOrganization, bankIntegration, commonBankIntegrationContext, commonBankIntegrationContextAttrs
     beforeAll(async () => {
+        jest.setTimeout(300000)
+
         adminClient = await makeLoggedInAdminClient()
         commonClient = await makeLoggedInClient()
         adminContext = await keystone.createContext({ skipAccessControl: true })
@@ -57,16 +64,16 @@ describe('syncBankAccount from SBBOL', () => {
         }
         const [createdOrganization] = await createTestOrganization(adminClient)
         commonOrganization = await Organization.update(adminClient, createdOrganization.id, { tin: generateTin(RUSSIA_COUNTRY).toString(), ...dvSenderFields })
-        // bankIntegration = await BankIntegration.getOne(adminClient, { id: BANK_INTEGRATION_IDS.SBBOL })
-        // const [obj, attrs] = await createTestBankIntegrationContext(adminClient, bankIntegration, commonOrganization)
-        // commonBankIntegrationContext = obj
-        // commonBankIntegrationContextAttrs = attrs
+        bankIntegration = await BankIntegration.getOne(adminClient, { id: BANK_INTEGRATION_IDS.SBBOL })
+        const [obj, attrs] = await createTestBankIntegrationContext(adminClient, bankIntegration, commonOrganization)
+
+        commonBankIntegrationContext = obj
+        commonBankIntegrationContextAttrs = attrs
     })
 
     describe('requestTransactions', async () => {
         it('Request transactions from SBBOL ', async () => {
-            jest.setTimeout(300000)
-            const transactions = await requestTransactions(dayjs().format('YYYY-MM-DD'), adminClient.user.id)
+            const transactions = await requestTransactions(dayjs().format('YYYY-MM-DD'), commonClient.user.id, commonOrganization)
 
 
             expect(transactions).toBeTruthy()
