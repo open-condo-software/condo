cfda53a13e4aae6a2f9844b52e8da04fa65cd83b
AleX83Xpert <AleX83Xpert@mail.ru>
2023-02-09 17:15:03 +0500

    Revert "feat(condo): DOMA-5208 Improved payment tests"
    This reverts commit 33e214f7de541402fc199aaeba4064b4e90a8bfd.

--- apps/condo/domains/acquiring/schema/Payment.test.js
+++ apps/condo/domains/acquiring/schema/Payment.test.js
@@ -57,19 +57,14 @@ const {
 } = require('@condo/domains/organization/utils/testSchema')
 const { makeClientWithSupportUser, makeClientWithNewRegisteredAndLoggedInUser, makeClientWithServiceUser } = require('@condo/domains/user/utils/testSchema')
 
-let admin, client
+
+
+
+
+
+const { updateTestAcquiringIntegration } = require('../utils/testSchema')
 
 describe('Payment', () => {
-    beforeAll(async () => {
-        // Create&save cached clients to use them at the end of each test
-        ({ admin, client } = await makePayer())
-    })
-
-    afterEach(async () => {
-        await admin.runDeferredActionsAndClean()
-        await client.runDeferredActionsAndClean()
-    })
-
     describe('CRUD tests', () => {
         describe('Create', () => {
             test('admin can', async () => {
@@ -132,7 +127,7 @@ describe('Payment', () => {
                     })
                     test('linked', async () => {
                         const { admin, payments: firstPayments, acquiringIntegration: firstAcquiringIntegration, client: firstClient, organization: firstOrganization } = await makePayerAndPayments()
-                        const { payments: secondPayments, acquiringIntegration: secondAcquiringIntegration, client: secondClient, organization: secondOrganization } = await makePayerAndPayments(1, false)
+                        const { payments: secondPayments, acquiringIntegration: secondAcquiringIntegration, client: secondClient, organization: secondOrganization } = await makePayerAndPayments()
                         const [firstMultiPayment] = await createTestMultiPayment(admin, firstPayments, firstClient.user, firstAcquiringIntegration)
                         const [secondMultiPayment] = await createTestMultiPayment(admin, secondPayments, secondClient.user, secondAcquiringIntegration)
 
