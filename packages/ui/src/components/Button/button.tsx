c735bebc792065906cf65e76e9f68c17593a99cc
AleX83Xpert <AleX83Xpert@mail.ru>
2023-02-10 16:02:55 +0500

    feat(ui): DOMA-5306 remove redundant parameter & add style to main class

--- packages/ui/src/components/Button/button.tsx
+++ packages/ui/src/components/Button/button.tsx
@@ -13,20 +13,18 @@ type CondoButtonProps = {
     type: 'primary' | 'secondary',
     children?: string
     stateless?: boolean
-    textAlign?: 'left' | 'center' | 'right'
 }
 
 export type ButtonProps = Omit<DefaultButtonProps, 'shape' | 'size' | 'style' | 'ghost' | 'type' | 'prefix' | 'prefixCls'>
 & CondoButtonProps
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>((props, ref) => {
-    const { type, className, icon, children, onClick, stateless, textAlign = 'center', ...rest } = props
+    const { type, className, icon, children, onClick, stateless, ...rest } = props
     const classes = classNames(
         {
             [`${BUTTON_CLASS_PREFIX}-${type}`]: type,
             [`${BUTTON_CLASS_PREFIX}-stateless`]: stateless,
         },
-        `${BUTTON_CLASS_PREFIX}-${textAlign}-text`,
         className,
     )
 
