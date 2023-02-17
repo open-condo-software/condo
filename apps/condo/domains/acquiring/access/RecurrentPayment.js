3fd000eee361f8fe11f6b45438910642a2465d0e
ekabardinsky <ekabardinsky@gmail.com>
2023-02-17 11:56:57 +0500

    fix(condo): DOMA-3961 reorganize RecurrentPayment.js rules

--- apps/condo/domains/acquiring/access/RecurrentPayment.js
+++ apps/condo/domains/acquiring/access/RecurrentPayment.js
@@ -22,10 +22,18 @@ async function canReadRecurrentPayments ({ authentication: { item: user } }) {
     return false
 }
 
+async function canManageRecurrentPayments ({ authentication: { item: user }, originalInput, operation, itemId }) {
+    if (!user) return throwAuthenticationError()
+    if (user.deletedAt) return false
+    if (user.isSupport || user.isAdmin) return true
+
+    return false
+}
 /*
   Rules are logical functions that used for list access, and may return a boolean (meaning
   all or no items are available) or a set of filters that limit the available items.
 */
 module.exports = {
     canReadRecurrentPayments,
+    canManageRecurrentPayments,
 }
