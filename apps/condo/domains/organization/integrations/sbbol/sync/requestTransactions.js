e66e0a28ca3c15ec613e0581915622517a476ae7
VKislov <wkislov0@gmail.com>
2023-02-22 14:55:59 +0500

    fix(condo): DOMA-4800 save contractor only for credit transaction. Store syncTaskStatus in BankAccount meta field

--- apps/condo/domains/organization/integrations/sbbol/sync/requestTransactions.js
+++ apps/condo/domains/organization/integrations/sbbol/sync/requestTransactions.js
@@ -18,7 +18,11 @@ const { ERROR_PASSED_DATE_IN_THE_FUTURE } = require('../constants')
 
 const logger = getLogger('sbbol/SbbolSyncTransactions')
 const isTransactionsReceived = (response) => (get(response, 'error.cause', '') !== 'STATEMENT_RESPONSE_PROCESSING')
-
+// ---------------------------------
+const FAILED_STATUS = 'failed'
+const IN_PROGRESS_STATUS = 'inProgress'
+const COMPLETED_STATUS = 'completed'
+// ^^ the code needed in the prototype. In MVP, synchronization status will be stored in BankSyncTask
 /**
  * Connects new BankTransaction records for BankAccount according to transaction data from SBBOL.
  *  @param {String} userId
@@ -35,6 +39,17 @@ async function _requestTransactions ({ userId, bankAccounts, context, statementD
     const transactions = []
 
     for (const bankAccount of bankAccounts) {
+        // ---------------------------------
+        await BankAccount.update(context, bankAccount.id, {
+            meta: {
+                sbbol:{
+                    syncTransactionsTaskStatus: IN_PROGRESS_STATUS,
+                },
+            },
+            ...dvSenderFields,
+        })
+        // ^^ the code needed in the prototype. In MVP, synchronization status will be stored in BankSyncTask
+
         let page = 1,
             doNextRequest = true,
             timeout = 1000,
@@ -65,6 +80,16 @@ async function _requestTransactions ({ userId, bankAccounts, context, statementD
             if (receivedTransactions) {
                 receivedTransactions.map( transaction => transactions.push(transaction))
             } else {
+                // ---------------------------------
+                await BankAccount.update(context, bankAccount.id, {
+                    meta: {
+                        sbbol:{
+                            syncTransactionsTaskStatus: FAILED_STATUS,
+                        },
+                    },
+                    ...dvSenderFields,
+                })
+                // ^^ the code needed in the prototype. In MVP, synchronization status will be stored in BankSyncTask
                 logger.error(`Unsuccessful response to transaction request by state: ${{ 
                     bankAccount: bankAccount.number, 
                     statementDate,
@@ -106,24 +131,28 @@ async function _requestTransactions ({ userId, bankAccounts, context, statementD
                     ...whereTransactionConditions,
                 })
 
-                let [bankContractorAccount] = await BankContractorAccount.getAll(context, {
-                    organization: { id: organizationId },
-                    tin: transaction.rurTransfer.payeeInn,
-                    number: transaction.rurTransfer.payeeAccount,
-                }, { first: 1 })
-
-                if (!bankContractorAccount) {
-                    bankContractorAccount = await BankContractorAccount.create(context, {
-                        organization: { connect: { id: organizationId } },
-                        name: transaction.rurTransfer.payeeName,
+                let bankContractorAccount
+                // in mvp we will keep the contractor account for debit payments as well. Dividing them into individuals and legal entities
+                if (transaction.direction === 'CREDIT') {
+                    [bankContractorAccount] = await BankContractorAccount.getAll(context, {
+                        organization: { id: organizationId },
                         tin: transaction.rurTransfer.payeeInn,
-                        country: RUSSIA_COUNTRY,
-                        routingNumber: transaction.rurTransfer.payeeBankBic,
                         number: transaction.rurTransfer.payeeAccount,
-                        currencyCode: transaction.amount.currencyName,
-                        ...dvSenderFields,
-                    })
-                    logger.info({ msg: `BankContractorAccount instance created with id: ${bankContractorAccount.id}` })
+                    }, { first: 1 })
+
+                    if (!bankContractorAccount) {
+                        bankContractorAccount = await BankContractorAccount.create(context, {
+                            organization: { connect: { id: organizationId } },
+                            name: transaction.rurTransfer.payeeName,
+                            tin: transaction.rurTransfer.payeeInn,
+                            country: RUSSIA_COUNTRY,
+                            routingNumber: transaction.rurTransfer.payeeBankBic,
+                            number: transaction.rurTransfer.payeeAccount,
+                            currencyCode: transaction.amount.currencyName,
+                            ...dvSenderFields,
+                        })
+                        logger.info({ msg: `BankContractorAccount instance created with id: ${bankContractorAccount.id}` })
+                    }
                 }
 
                 if (!foundTransaction) {
@@ -132,7 +161,7 @@ async function _requestTransactions ({ userId, bankAccounts, context, statementD
                         organization: { connect: { id: organizationId } },
                         account: { connect: { id: bankAccount.id } },
                         integrationContext: { connect: { id: bankIntegrationContextId } },
-                        contractorAccount: { connect: { id: bankContractorAccount.id } },
+                        contractorAccount: bankContractorAccount ? { connect: { id: bankContractorAccount.id } } : undefined,
                         meta: { sbbol: transaction },
                         ...whereTransactionConditions,
                         ...dvSenderFields,
@@ -141,9 +170,29 @@ async function _requestTransactions ({ userId, bankAccounts, context, statementD
                 }
 
             } else {
+                // ---------------------------------
+                await BankAccount.update(context, bankAccount.id, {
+                    meta: {
+                        sbbol:{
+                            syncTransactionsTaskStatus: FAILED_STATUS,
+                        },
+                    },
+                    ...dvSenderFields,
+                })
+                // ^^ the code needed in the prototype. In MVP, synchronization status will be stored in BankSyncTask
                 logger.warn({ msg: 'Not supported currency of BankTransaction from SBBOL. It will not be saved.', transaction })
             }
         }
+        // ---------------------------------
+        await BankAccount.update(context, bankAccount.id, {
+            meta: {
+                sbbol:{
+                    syncTransactionsTaskStatus: COMPLETED_STATUS,
+                },
+            },
+            ...dvSenderFields,
+        })
+        // ^^ the code needed in the prototype. In MVP, synchronization status will be stored in BankSyncTask
     }
     return transactions
 }
