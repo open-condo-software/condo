6d207b920127e133f8a68748450f17a155922d45
Alllex202 <alllex.202@gmail.com>
2023-02-22 17:29:15 +0500

    fix(ui): DOMA-5408 fixed RadioGroup component

--- packages/ui/src/stories/Radio/RadioGroup.stories.tsx
+++ packages/ui/src/stories/Radio/RadioGroup.stories.tsx
@@ -1,33 +1,12 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react'
 import React from 'react'
 
-import { ChevronDown } from '@open-condo/icons'
-import { RadioGroup as Component } from '@open-condo/ui/src'
-
-const icons = {
-    ChevronDown: <ChevronDown size='small' />,
-}
+import { RadioGroup as Component, Radio, Space } from '@open-condo/ui/src'
 
 export default {
     title: 'Components/Radio',
     component: Component,
-    args: {
-        icon: <ChevronDown size='small' />,
-        groups: [
-            {
-                name: 'Group 1',
-                options: [{ label: 'Group option 1', value: 'value 1' }, { label: 'Group option 2', value: 'value 2' }],
-            },
-            {
-                name: 'Group 2',
-                options: [{ label: 'Group option 3', value: 'value 3' }, { label: 'Group option 4', value: 'value 4' }],
-            },
-            {
-                name: 'Group 3',
-                options: [{ label: 'Group option 5', value: 'value 5' }, { label: 'Group option 6', value: 'value 6' }],
-            },
-        ],
-    },
+    args: {},
     argTypes: {
         onChange: {
             table: {
@@ -39,20 +18,33 @@ export default {
                 type: null,
             },
         },
-        icon:{
-            options: Object.keys(icons),
-            mapping: icons,
-            control: {
-                type: 'select',
-                labels: {
-                    ChevronDown: 'Down arrow',
-                },
-            },
-        },
     },
 } as ComponentMeta<typeof Component>
 
 const Template: ComponentStory<typeof Component> = (props) => <Component {...props} />
 
 export const RadioGroup = Template.bind({})
-RadioGroup.args = {}
+RadioGroup.args = {
+    groups: [
+        {
+            name: 'Group 1',
+            options: [{ label: 'Group option 1', value: 'value 1' }, { label: 'Group option 2', value: 'value 2' }],
+        },
+        {
+            name: 'Group 2',
+            options: [{ label: 'Group option 3', value: 'value 3' }, { label: 'Group option 4', value: 'value 4' }],
+        },
+        {
+            name: 'Group 3',
+            options: [{ label: 'Group option 5', value: 'value 5' }, { label: 'Group option 6', value: 'value 6' }],
+        },
+    ],
+}
+
+export const CustomRadioGroup = Template.bind({})
+CustomRadioGroup.args = {
+    children: <Space direction='vertical' size={12}>
+        <Radio label='Group option 1' value='value 1' />
+        <Radio label='Group option 2' value='value 2' />
+    </Space>,
+}
