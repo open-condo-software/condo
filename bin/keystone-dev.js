ecc3527df3fd57fdcacb716a6415d98cb788a136
Pahaz <pahaz.white@gmail.com>
2023-02-23 15:06:31 +0300

    feat: DOMA-5394 `yarn dev` add HTTPS support by default

--- bin/keystone-dev.js
+++ bin/keystone-dev.js
@@ -4,37 +4,53 @@
  * You can now use command like so: `PORT=3002 yarn dev`
  * Or put PORT inside your .env file
  */
+const fs = require('fs')
+const https = require('https')
 const path = require('path')
 
-const { getEntryFileFullPath, executeDefaultServer } = require('@keystonejs/keystone/bin/utils')
-const ciInfo = require('ci-info')
-const ora = require('ora')
+const chalk = require('chalk')
 
 const conf = require('@open-condo/config')
+const { prepareKeystoneExpressApp } = require('@open-condo/keystone/test.utils')
 
-const spinner = ora({
-    text: 'Initialising Keystone CLI',
-    // Don't show any loading output on CI
-    isSilent: !!ciInfo.isCi,
-}).start()
+const PORT = conf['PORT']
+const SPORT = conf['SPORT']
+const SERVER_URL = conf['SERVER_URL']
+const KEY_FILE = path.join(__filename, '..', '.ssl', 'localhost.key')
+const CERT_FILE = path.join(__filename, '..', '.ssl', 'localhost.pem')
+const HTTPS_OPTIONS = {}
 
-const info = {
-    exeName: __filename,
-    _cwd: process.cwd(),
-}
-
-const args = {
-    ['--port']: conf['PORT'],
-    ['--entry']: 'index.js',
-    ['--app-url']: conf['SERVER_URL'],
+if (fs.existsSync(KEY_FILE) && fs.existsSync(CERT_FILE)) {
+    HTTPS_OPTIONS.key = fs.readFileSync(KEY_FILE)
+    HTTPS_OPTIONS.cert = fs.readFileSync(CERT_FILE)
 }
 
 async function main () {
-    spinner.text = 'Validating project entry file'
-    const entryFile = await getEntryFileFullPath(args, info)
-    spinner.succeed(`Validated project entry file ./${path.relative(info._cwd, entryFile)}`)
-    spinner.start(' ')
-    await executeDefaultServer(args, entryFile, undefined, spinner)
+    const index = path.resolve('./index.js')
+    const { keystone, app } = await prepareKeystoneExpressApp(index)
+    let httpServer, httpsServer
+
+    httpServer = await new Promise((resolve, reject) => {
+        const server = app.listen(PORT, (error) => {
+            if (error) return reject(error)
+            return resolve(server)
+        })
+    })
+
+    if (HTTPS_OPTIONS.key && HTTPS_OPTIONS.cert)  {
+        httpsServer = await new Promise((resolve, reject) => {
+            const server = https.createServer(HTTPS_OPTIONS, app).listen(SPORT, (error) => {
+                if (error) return reject(error)
+                return resolve(server)
+            })
+        })
+    }
+
+    if (httpServer) console.log(chalk.green.bold(`HTTP 🚀 server started on port ${PORT}`))
+    if (httpsServer) console.log(chalk.blue.bold(`HTTPS 🚀 server started on port ${SPORT}`))
+
+    console.log(chalk.red.bold(`🔗🔗🔗 SERVER_URL=${SERVER_URL}`))
+    return { keystone, app, httpServer, httpsServer }
 }
 
 main().catch((e) => {
