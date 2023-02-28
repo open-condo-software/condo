fa43a6eb9b5a8cba864fcd01b93b68ffe090e288
AleX83Xpert <AleX83Xpert@mail.ru>
2023-02-28 10:00:11 +0500

    refactor(address-service): DOMA-5433 fix provider detector tests

--- apps/address-service/domains/common/utils/services/providerDetectors.spec.js
+++ apps/address-service/domains/common/utils/services/providerDetectors.spec.js
@@ -5,24 +5,64 @@ const {
 } = require('@address-service/domains/common/utils/services/providerDetectors')
 const {
     DadataSearchProvider,
+    GoogleSearchProvider,
 } = require('@address-service/domains/common/utils/services/search/providers')
 const {
     DadataSuggestionProvider,
     GoogleSuggestionProvider,
 } = require('@address-service/domains/common/utils/services/suggest/providers')
 
-describe('Search provider detector', () => {
+describe('Provider detector', () => {
+
+    const originalEnv = process.env
+
+    const providersEligibleConfigs = {
+        [DADATA_PROVIDER]: { DADATA_SUGGESTIONS: '{"url":"where is dadata?", "token":"tooooken"}' },
+        [GOOGLE_PROVIDER]: { GOOGLE_API_KEY: 'just an api key' },
+    }
+
+    beforeEach(() => {
+        jest.resetModules() // clear the cache
+        process.env = { ...originalEnv }
+    })
+
+    afterAll(() => {
+        process.env = originalEnv
+    })
+
+    describe('throws an error if there is no config variable for provider', () => {
+        const cases = [
+            // [<provider name>, <env variable name>]
+            [DADATA_PROVIDER, 'DADATA_SUGGESTIONS'],
+            [GOOGLE_PROVIDER, 'GOOGLE_API_KEY'],
+        ]
+
+        test.each(cases)('in the case of %p provider the %p variable must exist', (providerName, variableName) => {
+            process.env.PROVIDER = providerName
+            expect(getSearchProvider).toThrow(`There is no '${variableName}' in .env.`)
+        })
+    })
 
     describe('detects correct search provider', () => {
         const cases = [
+            // [<provider name>, <provider's class>, <env variable name for the provider>, <env variable value for the provider>]
             [DADATA_PROVIDER, DadataSearchProvider],
-            ['ru', DadataSearchProvider],
-            [undefined, DadataSearchProvider],
-            ['some string', DadataSearchProvider],
+            [GOOGLE_PROVIDER, GoogleSearchProvider],
+            [undefined], // No provider set
+            ['some string'], // An un-existing provider set
         ]
 
-        test.each(cases)('for %p - is %p', (geo, expected) => {
-            expect(getSearchProvider(geo)).toBeInstanceOf(expected)
+        test.each(cases)('for %p', (providerName, expected = undefined) => {
+            process.env = {
+                ...process.env,
+                PROVIDER: providerName,
+                ...(providersEligibleConfigs[providerName] || {}),
+            }
+            if (expected) {
+                expect(getSearchProvider()).toBeInstanceOf(expected)
+            } else {
+                expect(getSearchProvider()).toBeUndefined()
+            }
         })
     })
 
@@ -30,13 +70,21 @@ describe('Search provider detector', () => {
         const cases = [
             [DADATA_PROVIDER, DadataSuggestionProvider],
             [GOOGLE_PROVIDER, GoogleSuggestionProvider],
-            ['ru', DadataSuggestionProvider],
-            [undefined, DadataSuggestionProvider],
-            ['abra-shvabra-cadabra', DadataSuggestionProvider],
+            [undefined], // No provider set
+            ['abra-shvabra-cadabra'], // An un-existing provider set
         ]
 
-        test.each(cases)('for %p - is %p', (geo, expected) => {
-            expect(getSuggestionsProvider(geo)).toBeInstanceOf(expected)
+        test.each(cases)('for %p', (providerName, expected = undefined) => {
+            process.env = {
+                ...process.env,
+                PROVIDER: providerName,
+                ...(providersEligibleConfigs[providerName] || {}),
+            }
+            if (expected) {
+                expect(getSuggestionsProvider()).toBeInstanceOf(expected)
+            } else {
+                expect(getSuggestionsProvider()).toBeUndefined()
+            }
         })
     })
 })
