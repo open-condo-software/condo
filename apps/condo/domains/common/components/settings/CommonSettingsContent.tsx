5f70f8bcd2caab5f7fbbf8bff0ba72112e60aca9
AleX83Xpert <AleX83Xpert@mail.ru>
2023-03-02 10:28:51 +0500

    feat(condo): DOMA-5438 Output current language value

--- apps/condo/domains/common/components/settings/CommonSettingsContent.tsx
+++ apps/condo/domains/common/components/settings/CommonSettingsContent.tsx
@@ -1,14 +1,51 @@
 import { Table } from 'antd'
-import { useCallback, useState } from 'react'
+import { ColumnsType } from 'antd/es/table'
+import React, { useCallback, useState } from 'react'
 
 import { LocaleContext, useIntl } from '@open-condo/next/intl'
 import { Modal } from '@open-condo/ui'
 
-const LanguageSettingPopupContent = ({ locale, setLocale }): JSX.Element => {
-    const intl = useIntl()
-    // const RuTitle = intl.formatMessage({ id: '' })
-    // const EnTitle = intl.formatMessage({ id: '' })
+interface ISettingsItem {
+    title: string
+    value: JSX.Element
+    modalContent: JSX.Element
+}
 
+interface ISettingsModal {
+    title: string
+    content: JSX.Element
+}
+
+interface ILanguageNameProps {
+    locale: string
+    useFlag?: boolean
+}
+
+const LanguageName: React.FC<ILanguageNameProps> = ({ locale, useFlag = true }) => {
+    const intl = useIntl()
+    const RuTitle = intl.formatMessage({ id: 'language.russian' })
+    const EnTitle = intl.formatMessage({ id: 'language.english-us' })
+
+    // todo(AleX83Xpert) maybe move languages to some global place
+    const possible: { [locale: string]: { title: string, flagEmoji: string } } = {
+        ru: { title: RuTitle, flagEmoji: '🇷🇺' },
+        en: { title: EnTitle, flagEmoji: '🇺🇸' },
+    }
+
+    return (
+        <>
+            {useFlag && `${possible[locale].flagEmoji}\u00a0`}
+            {possible[locale].title}
+        </>
+    )
+}
+
+interface ILanguageSettingPopupContentProps {
+    locale: string
+    setLocale: { (locale: string): void }
+}
+
+const LanguageSettingPopupContent: React.FC<ILanguageSettingPopupContentProps> = ({ locale, setLocale }) => {
     return (
         <div>
             <button onClick={() => setLocale('ru')}>{locale === 'ru' ? 'RU' : 'ru'}</button>
@@ -17,18 +54,18 @@ const LanguageSettingPopupContent = ({ locale, setLocale }): JSX.Element => {
     )
 }
 
-export const CommonSettingsContent = (): JSX.Element => {
+export const CommonSettingsContent: React.FC = () => {
     const intl = useIntl()
     const SettingNameTitle = intl.formatMessage({ id: 'pages.condo.settings.common.settingName' })
     const SettingValueTitle = intl.formatMessage({ id: 'pages.condo.settings.common.settingValue' })
     const InterfaceLanguageTitle = intl.formatMessage({ id: 'pages.condo.settings.common.interfaceLanguage' })
 
-    const [modalContent, setModalContent] = useState<JSX.Element>()
+    const [modalContent, setModalContent] = useState<ISettingsModal>()
 
-    const handleRowClick = useCallback((row) => {
+    const handleRowClick = useCallback((row: ISettingsItem) => {
         return {
             onClick: () => {
-                setModalContent(row.modalContent)
+                setModalContent({ content: row.modalContent, title: row.title })
             },
         }
     }, [])
@@ -36,28 +73,36 @@ export const CommonSettingsContent = (): JSX.Element => {
     return (
         <LocaleContext.Consumer>
             {({ locale, setLocale }) => {
-                const dataSource = [
+                const dataSource: ISettingsItem[] = [
                     {
                         title: InterfaceLanguageTitle,
-                        value: <div>{locale}</div>,
+                        value: <LanguageName locale={locale}/>,
                         modalContent: <LanguageSettingPopupContent locale={locale} setLocale={setLocale}/>,
                     },
                 ]
-                const columns = [
+                const columns: ColumnsType<ISettingsItem> = [
                     { title: SettingNameTitle, dataIndex: 'title', width: 250 },
                     { title: SettingValueTitle, dataIndex: 'value' },
                 ]
 
                 return (
                     <>
-                        <Table
+                        <Table<ISettingsItem>
                             dataSource={dataSource}
                             columns={columns}
                             onRow={handleRowClick}
+                            pagination={false}
+                            bordered={true}
                         />
-                        <Modal title='123' open={!!modalContent} onCancel={() => setModalContent(undefined)}>
-                            {modalContent}
-                        </Modal>
+                        {!!modalContent && (
+                            <Modal
+                                title={modalContent.title}
+                                open={!!modalContent}
+                                onCancel={() => setModalContent(undefined)}
+                            >
+                                {modalContent.content}
+                            </Modal>
+                        )}
                     </>
                 )
             }}
