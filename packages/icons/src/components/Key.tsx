b0470faa7d57b9c2bb48b707cf8381a944747862
Alllex202 <alllex.202@gmail.com>
2023-03-06 15:35:49 +0500

    feat(icons): DOMA-5498 added new icons

---
+++ packages/icons/src/components/Key.tsx
@@ -0,0 +1,29 @@
+/** This file is auto-generated, do not edit it manually **/
+import React from 'react'
+
+import { IconWrapper, IconProps } from '../wrappers'
+
+export const Key: React.FC<IconProps> = ({ svgProps: props, ...restProps }) => {
+    return (
+        <IconWrapper
+            icon={
+                <svg
+                    width='inherit'
+                    height='inherit'
+                    fill='none'
+                    xmlns='http://www.w3.org/2000/svg'
+                    viewBox='0 0 24 24'
+                    {...props}
+                >
+                    <path
+                        fillRule='evenodd'
+                        clipRule='evenodd'
+                        d='M21.707 1.293a1 1 0 0 1 0 1.414L20.414 4l2.293 2.293a1 1 0 0 1 0 1.414l-3.5 3.5a1 1 0 0 1-1.414 0L15.5 8.914l-2.746 2.747A6.5 6.5 0 0 1 2.9 20.09l-.007-.008a6.5 6.5 0 0 1 8.43-9.821l8.97-8.97a1 1 0 0 1 1.414 0ZM10.722 12.356a4.501 4.501 0 1 0 0 0ZM16.914 7.5 18.5 9.086 20.586 7 19 5.414 16.914 7.5Z'
+                        fill='currentColor'
+                    />
+                </svg>
+            }
+            {...restProps}
+        />
+    )
+}
