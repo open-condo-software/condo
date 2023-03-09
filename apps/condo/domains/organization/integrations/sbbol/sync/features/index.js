72f371550a484233fe7a9c40a0a9eded00534a79
SavelevMatthew <savelevmatthew@gmail.com>
2023-03-09 11:27:04 +0500

    feat(condo): DOMA-5482 Expand logic to support other cases

--- apps/condo/domains/organization/integrations/sbbol/sync/features/index.js
+++ apps/condo/domains/organization/integrations/sbbol/sync/features/index.js
@@ -2,11 +2,15 @@ const concat = require('lodash/concat')
 const get = require('lodash/get')
 const uniq = require('lodash/uniq')
 
+const { getLogger } = require('@open-condo/keystone/logging')
+
 const { SERVICE_PROVIDER_PROFILE_FEATURE, ALL_FEATURES } = require('@condo/domains/organization/constants/features')
 const { dvSenderFields } = require('@condo/domains/organization/integrations/sbbol/constants')
 const { Organization } = require('@condo/domains/organization/utils/serverSchema')
 
 const { syncServiceProviderProfileState } = require('./serviceProviderProfile')
+
+const logger = getLogger('sbbol/syncFeatures')
 
 
 /**
@@ -18,24 +22,34 @@ const { syncServiceProviderProfileState } = require('./serviceProviderProfile')
  */
 async function syncFeatures ({ context, organization, features }) {
     const { context: adminContext } = context
+    const orgId = organization.id
     // Get current feature-list
     const existingFeatures = get(organization, 'features', [])
-    // Merge with new features
-    const mergedFeatures = uniq(concat(existingFeatures, features))
-    // Filter outdated features
-    const actualFeatures = mergedFeatures.filter(feat => ALL_FEATURES.includes(feat))
+    logger.info({ msg: `Staring feature-sync process for organization: ${orgId}`, orgId, existingFeatures })
+    // Remove out-dated features from existing feature list
+    const actualExistingFeatures = existingFeatures.filter(feat => ALL_FEATURES.includes(feat))
+    // Remove out-dated features from incoming feature list
+    const actualFeaturesToConnect = features.filter(feat => ALL_FEATURES.includes(feat))
+    const connectedFeatures = []
+
+    // Feature-specific logic
+    if (actualFeaturesToConnect.includes(SERVICE_PROVIDER_PROFILE_FEATURE)) {
+        const success = await syncServiceProviderProfileState({ context, organization })
+        if (success) {
+            logger.info({ msg: `${SERVICE_PROVIDER_PROFILE_FEATURE} feature is successfully connected` })
+            connectedFeatures.push(SERVICE_PROVIDER_PROFILE_FEATURE)
+        }
+    }
+
+    const actualFeatures = uniq(concat(actualExistingFeatures, connectedFeatures))
 
     // NOTE: syncOrganization uses registerNewOrganization under the hood.
-    // We cannot open 'features' prop directly in this API, so instead we're syncing all models here in single place
+    // We cannot open 'features' prop directly in this API method, so instead we're syncing all models here in single place
+    // It's also moved after syncOrganization, since feature cannot be connected in 100% cases
     await Organization.update(adminContext, organization.id, {
         ...dvSenderFields,
         features: actualFeatures,
     })
-
-    // Feature-specific logic
-    if (features.includes(SERVICE_PROVIDER_PROFILE_FEATURE)) {
-        await syncServiceProviderProfileState({ context, organization })
-    }
 }
 
 module.exports = {
