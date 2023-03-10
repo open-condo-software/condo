41f0578708c7c62efc8647e39c76cc3070f5bdb3
ekabardinsky <ekabardinsky@gmail.com>
2023-03-10 14:54:59 +0500

    feat(condo): DOMA-3962 listening for new billing receipts and create RecurrentPayment in case if autoPayReceipts is true

--- apps/condo/domains/acquiring/tasks/recurrent-payment-notification-task.js
+++ apps/condo/domains/acquiring/tasks/recurrent-payment-notification-task.js
@@ -2,8 +2,6 @@ const { createCronTask } = require('@open-condo/keystone/tasks')
 
 const { process } = require('@condo/domains/acquiring/tasks/recurrent-payment-notification/recurrent-payment-notification')
 
-// TODO set proper schedule
-// TODO how its gonna work with horizontal app scaling?
 module.exports = createCronTask('recurrentPaymentNotification', '0 * * * *', async () => {
     await process()
 })
---
+++ apps/condo/domains/acquiring/tasks/recurrent-payment-seeking-for-new-receipt/recurrent-payment-seeking-for-new-receipt.spec.js
@@ -0,0 +1,410 @@
+/**
+ * @jest-environment node
+ */
+const index = require('@app/condo/index')
+const dayjs = require('dayjs')
+const faker = require('faker')
+
+const {
+    setFakeClientMode,
+    makeLoggedInAdminClient,
+} = require('@open-condo/keystone/test.utils')
+
+const {
+    RECURRENT_PAYMENT_INIT_STATUS,
+} = require('@condo/domains/acquiring/constants/recurrentPayment')
+const {
+    makePayerWithMultipleConsumers,
+    createTestRecurrentPaymentContext,
+    RecurrentPayment,
+} = require('@condo/domains/acquiring/utils/testSchema')
+const { DATE_FORMAT, DATE_FORMAT_Z } = require('@condo/domains/common/utils/date')
+const {
+    RECURRENT_PAYMENT_TOMORROW_PAYMENT_MESSAGE_TYPE,
+} = require('@condo/domains/notification/constants/constants')
+const {
+    Message,
+} = require('@condo/domains/notification/utils/serverSchema')
+
+const {
+    processContext,
+} = require('./recurrent-payment-seeking-for-new-receipt')
+
+const { keystone } = index
+
+describe('recurrent-payment-seeking-for-new-receipt', () => {
+    let adminContext, admin, getContextRequest
+    setFakeClientMode(index)
+
+    beforeAll(async () => {
+        adminContext = await keystone.createContext({ skipAccessControl: true })
+
+        admin = await makeLoggedInAdminClient()
+
+        getContextRequest = (batch) => ({
+            enabled: false,
+            limit: '10000',
+            autoPayReceipts: true,
+            paymentDay: null,
+            settings: { cardId: faker.datatype.uuid() },
+            serviceConsumer: { connect: { id: batch.serviceConsumer.id } },
+            billingCategory: { connect: { id: batch.billingReceipts[0].category.id } },
+        })
+    })
+
+    it('should create RecurrentPayment - lastDt is start of the month, period - this month', async () => {
+        const { batches } = await makePayerWithMultipleConsumers(1, 2)
+        const [batch] = batches
+        const {
+            billingReceipts,
+        } = batch
+        const [{ period }] = billingReceipts
+        const lastDt = dayjs(period).startOf('month')
+        const lastDtString = lastDt.format(DATE_FORMAT)
+        const periods = [lastDt.subtract(1, 'months').format(DATE_FORMAT), lastDtString]
+        const tomorrowMidnight = dayjs().add(1, 'days').startOf('day')
+
+        const [recurrentPaymentContext] = await createTestRecurrentPaymentContext(admin, {
+            ...getContextRequest(batch),
+            enabled: true,
+        })
+
+        // create recurrent payments
+        await processContext(adminContext, recurrentPaymentContext, periods, lastDtString)
+
+        const recurrentPayments = await RecurrentPayment.getAll(admin, {
+            recurrentPaymentContext: { id: recurrentPaymentContext.id },
+        })
+
+        expect(recurrentPayments).toHaveLength(1)
+
+        const [recurrentPayment] = recurrentPayments
+        expect(recurrentPayment).toHaveProperty('status')
+        expect(recurrentPayment).toHaveProperty('tryCount')
+        expect(recurrentPayment).toHaveProperty('state')
+        expect(recurrentPayment).toHaveProperty('payAfter')
+        expect(recurrentPayment).toHaveProperty('billingReceipts')
+        expect(recurrentPayment).toHaveProperty('recurrentPaymentContext')
+
+        expect(recurrentPayment.status).toEqual(RECURRENT_PAYMENT_INIT_STATUS)
+        expect(recurrentPayment.payAfter).toEqual(tomorrowMidnight.toISOString())
+        expect(recurrentPayment.tryCount).toEqual(0)
+        expect(recurrentPayment.state).toBeDefined()
+        expect(recurrentPayment.billingReceipts).toHaveLength(2)
+
+        const ids = recurrentPayment.billingReceipts.map(receipt => receipt.id)
+        expect(ids).toContain(billingReceipts[0].id)
+        expect(ids).toContain(billingReceipts[1].id)
+
+        const [notification] = await Message.getAll(adminContext, {
+            type: RECURRENT_PAYMENT_TOMORROW_PAYMENT_MESSAGE_TYPE,
+            user: { id: batch.resident.user.id },
+        }, {
+            sortBy: 'createdAt_DESC',
+        })
+        expect(notification).toBeDefined()
+        expect(notification).toHaveProperty('user')
+        expect(notification.user).toHaveProperty('id')
+        expect(notification.user.id).toEqual(batch.resident.user.id)
+        expect(notification).toHaveProperty('meta')
+        expect(notification.meta).toHaveProperty('recurrentPaymentContext')
+        expect(notification.meta.recurrentPaymentContext).toHaveProperty('id')
+        expect(notification.meta.recurrentPaymentContext.id).toEqual(recurrentPaymentContext.id)
+    })
+
+    it('should create RecurrentPayment - lastDt is start of the month, period - prev month', async () => {
+        const { batches } = await makePayerWithMultipleConsumers(1, 2)
+        const [batch] = batches
+        const {
+            billingReceipts,
+        } = batch
+        const [{ period }] = billingReceipts
+        const lastDt = dayjs(period).startOf('month')
+        const lastDtString = lastDt.format(DATE_FORMAT)
+        const periods = [lastDt.add(1, 'months').format(DATE_FORMAT), lastDtString]
+        const tomorrowMidnight = dayjs().add(1, 'days').startOf('day')
+
+        const [recurrentPaymentContext] = await createTestRecurrentPaymentContext(admin, {
+            ...getContextRequest(batch),
+            enabled: true,
+        })
+
+        // create recurrent payments
+        await processContext(adminContext, recurrentPaymentContext, periods, lastDtString)
+
+        const recurrentPayments = await RecurrentPayment.getAll(admin, {
+            recurrentPaymentContext: { id: recurrentPaymentContext.id },
+        })
+
+        expect(recurrentPayments).toHaveLength(1)
+
+        const [recurrentPayment] = recurrentPayments
+        expect(recurrentPayment).toHaveProperty('status')
+        expect(recurrentPayment).toHaveProperty('tryCount')
+        expect(recurrentPayment).toHaveProperty('state')
+        expect(recurrentPayment).toHaveProperty('payAfter')
+        expect(recurrentPayment).toHaveProperty('billingReceipts')
+        expect(recurrentPayment).toHaveProperty('recurrentPaymentContext')
+
+        expect(recurrentPayment.status).toEqual(RECURRENT_PAYMENT_INIT_STATUS)
+        expect(recurrentPayment.payAfter).toEqual(tomorrowMidnight.toISOString())
+        expect(recurrentPayment.tryCount).toEqual(0)
+        expect(recurrentPayment.state).toBeDefined()
+        expect(recurrentPayment.billingReceipts).toHaveLength(2)
+
+        const ids = recurrentPayment.billingReceipts.map(receipt => receipt.id)
+        expect(ids).toContain(billingReceipts[0].id)
+        expect(ids).toContain(billingReceipts[1].id)
+
+        const [notification] = await Message.getAll(adminContext, {
+            type: RECURRENT_PAYMENT_TOMORROW_PAYMENT_MESSAGE_TYPE,
+            user: { id: batch.resident.user.id },
+        }, {
+            sortBy: 'createdAt_DESC',
+        })
+        expect(notification).toBeDefined()
+        expect(notification).toHaveProperty('user')
+        expect(notification.user).toHaveProperty('id')
+        expect(notification.user.id).toEqual(batch.resident.user.id)
+        expect(notification).toHaveProperty('meta')
+        expect(notification.meta).toHaveProperty('recurrentPaymentContext')
+        expect(notification.meta.recurrentPaymentContext).toHaveProperty('id')
+        expect(notification.meta.recurrentPaymentContext.id).toEqual(recurrentPaymentContext.id)
+    })
+
+    it('should create RecurrentPayment - lastDt less than createdAt', async () => {
+        const { batches } = await makePayerWithMultipleConsumers(1, 2)
+        const [batch] = batches
+        const {
+            billingReceipts,
+        } = batch
+        const [{ period, createdAt }] = billingReceipts
+        const lastDt = dayjs(createdAt).startOf('day')
+        const periodDt = dayjs(period).startOf('month')
+        const lastDtString = lastDt.format(DATE_FORMAT)
+        const periods = [periodDt.add(1, 'months').format(DATE_FORMAT), periodDt.format(DATE_FORMAT)]
+        const tomorrowMidnight = dayjs().add(1, 'days').startOf('day')
+
+        const [recurrentPaymentContext] = await createTestRecurrentPaymentContext(admin, {
+            ...getContextRequest(batch),
+            enabled: true,
+        })
+
+        // create recurrent payments
+        await processContext(adminContext, recurrentPaymentContext, periods, lastDtString)
+
+        const recurrentPayments = await RecurrentPayment.getAll(admin, {
+            recurrentPaymentContext: { id: recurrentPaymentContext.id },
+        })
+
+        expect(recurrentPayments).toHaveLength(1)
+
+        const [recurrentPayment] = recurrentPayments
+        expect(recurrentPayment).toHaveProperty('status')
+        expect(recurrentPayment).toHaveProperty('tryCount')
+        expect(recurrentPayment).toHaveProperty('state')
+        expect(recurrentPayment).toHaveProperty('payAfter')
+        expect(recurrentPayment).toHaveProperty('billingReceipts')
+        expect(recurrentPayment).toHaveProperty('recurrentPaymentContext')
+
+        expect(recurrentPayment.status).toEqual(RECURRENT_PAYMENT_INIT_STATUS)
+        expect(recurrentPayment.payAfter).toEqual(tomorrowMidnight.toISOString())
+        expect(recurrentPayment.tryCount).toEqual(0)
+        expect(recurrentPayment.state).toBeDefined()
+        expect(recurrentPayment.billingReceipts).toHaveLength(2)
+
+        const ids = recurrentPayment.billingReceipts.map(receipt => receipt.id)
+        expect(ids).toContain(billingReceipts[0].id)
+        expect(ids).toContain(billingReceipts[1].id)
+
+        const [notification] = await Message.getAll(adminContext, {
+            type: RECURRENT_PAYMENT_TOMORROW_PAYMENT_MESSAGE_TYPE,
+            user: { id: batch.resident.user.id },
+        }, {
+            sortBy: 'createdAt_DESC',
+        })
+        expect(notification).toBeDefined()
+        expect(notification).toHaveProperty('user')
+        expect(notification.user).toHaveProperty('id')
+        expect(notification.user.id).toEqual(batch.resident.user.id)
+        expect(notification).toHaveProperty('meta')
+        expect(notification.meta).toHaveProperty('recurrentPaymentContext')
+        expect(notification.meta.recurrentPaymentContext).toHaveProperty('id')
+        expect(notification.meta.recurrentPaymentContext.id).toEqual(recurrentPaymentContext.id)
+    })
+
+    it('should create RecurrentPayment - lastDt date time zoned format', async () => {
+        const { batches } = await makePayerWithMultipleConsumers(1, 2)
+        const [batch] = batches
+        const {
+            billingReceipts,
+        } = batch
+        const [{ period, createdAt }] = billingReceipts
+        const lastDt = dayjs(createdAt).startOf('day')
+        const periodDt = dayjs(period).startOf('month')
+        const lastDtString = lastDt.format(DATE_FORMAT_Z)
+        const periods = [periodDt.add(1, 'months').format(DATE_FORMAT), periodDt.format(DATE_FORMAT)]
+        const tomorrowMidnight = dayjs().add(1, 'days').startOf('day')
+
+        const [recurrentPaymentContext] = await createTestRecurrentPaymentContext(admin, {
+            ...getContextRequest(batch),
+            enabled: true,
+        })
+
+        // create recurrent payments
+        await processContext(adminContext, recurrentPaymentContext, periods, lastDtString)
+
+        const recurrentPayments = await RecurrentPayment.getAll(admin, {
+            recurrentPaymentContext: { id: recurrentPaymentContext.id },
+        })
+
+        expect(recurrentPayments).toHaveLength(1)
+
+        const [recurrentPayment] = recurrentPayments
+        expect(recurrentPayment).toHaveProperty('status')
+        expect(recurrentPayment).toHaveProperty('tryCount')
+        expect(recurrentPayment).toHaveProperty('state')
+        expect(recurrentPayment).toHaveProperty('payAfter')
+        expect(recurrentPayment).toHaveProperty('billingReceipts')
+        expect(recurrentPayment).toHaveProperty('recurrentPaymentContext')
+
+        expect(recurrentPayment.status).toEqual(RECURRENT_PAYMENT_INIT_STATUS)
+        expect(recurrentPayment.payAfter).toEqual(tomorrowMidnight.toISOString())
+        expect(recurrentPayment.tryCount).toEqual(0)
+        expect(recurrentPayment.state).toBeDefined()
+        expect(recurrentPayment.billingReceipts).toHaveLength(2)
+
+        const ids = recurrentPayment.billingReceipts.map(receipt => receipt.id)
+        expect(ids).toContain(billingReceipts[0].id)
+        expect(ids).toContain(billingReceipts[1].id)
+
+        const [notification] = await Message.getAll(adminContext, {
+            type: RECURRENT_PAYMENT_TOMORROW_PAYMENT_MESSAGE_TYPE,
+            user: { id: batch.resident.user.id },
+        }, {
+            sortBy: 'createdAt_DESC',
+        })
+        expect(notification).toBeDefined()
+        expect(notification).toHaveProperty('user')
+        expect(notification.user).toHaveProperty('id')
+        expect(notification.user.id).toEqual(batch.resident.user.id)
+        expect(notification).toHaveProperty('meta')
+        expect(notification.meta).toHaveProperty('recurrentPaymentContext')
+        expect(notification.meta.recurrentPaymentContext).toHaveProperty('id')
+        expect(notification.meta.recurrentPaymentContext.id).toEqual(recurrentPaymentContext.id)
+    })
+
+    it('should create RecurrentPayment - lastDt iso format', async () => {
+        const { batches } = await makePayerWithMultipleConsumers(1, 2)
+        const [batch] = batches
+        const {
+            billingReceipts,
+        } = batch
+        const [{ period, createdAt }] = billingReceipts
+        const lastDt = dayjs(createdAt).startOf('day')
+        const periodDt = dayjs(period).startOf('month')
+        const lastDtString = lastDt.toISOString()
+        const periods = [periodDt.add(1, 'months').format(DATE_FORMAT), periodDt.format(DATE_FORMAT)]
+        const tomorrowMidnight = dayjs().add(1, 'days').startOf('day')
+
+        const [recurrentPaymentContext] = await createTestRecurrentPaymentContext(admin, {
+            ...getContextRequest(batch),
+            enabled: true,
+        })
+
+        // create recurrent payments
+        await processContext(adminContext, recurrentPaymentContext, periods, lastDtString)
+
+        const recurrentPayments = await RecurrentPayment.getAll(admin, {
+            recurrentPaymentContext: { id: recurrentPaymentContext.id },
+        })
+
+        expect(recurrentPayments).toHaveLength(1)
+
+        const [recurrentPayment] = recurrentPayments
+        expect(recurrentPayment).toHaveProperty('status')
+        expect(recurrentPayment).toHaveProperty('tryCount')
+        expect(recurrentPayment).toHaveProperty('state')
+        expect(recurrentPayment).toHaveProperty('payAfter')
+        expect(recurrentPayment).toHaveProperty('billingReceipts')
+        expect(recurrentPayment).toHaveProperty('recurrentPaymentContext')
+
+        expect(recurrentPayment.status).toEqual(RECURRENT_PAYMENT_INIT_STATUS)
+        expect(recurrentPayment.payAfter).toEqual(tomorrowMidnight.toISOString())
+        expect(recurrentPayment.tryCount).toEqual(0)
+        expect(recurrentPayment.state).toBeDefined()
+        expect(recurrentPayment.billingReceipts).toHaveLength(2)
+
+        const ids = recurrentPayment.billingReceipts.map(receipt => receipt.id)
+        expect(ids).toContain(billingReceipts[0].id)
+        expect(ids).toContain(billingReceipts[1].id)
+
+        const [notification] = await Message.getAll(adminContext, {
+            type: RECURRENT_PAYMENT_TOMORROW_PAYMENT_MESSAGE_TYPE,
+            user: { id: batch.resident.user.id },
+        }, {
+            sortBy: 'createdAt_DESC',
+        })
+        expect(notification).toBeDefined()
+        expect(notification).toHaveProperty('user')
+        expect(notification.user).toHaveProperty('id')
+        expect(notification.user.id).toEqual(batch.resident.user.id)
+        expect(notification).toHaveProperty('meta')
+        expect(notification.meta).toHaveProperty('recurrentPaymentContext')
+        expect(notification.meta.recurrentPaymentContext).toHaveProperty('id')
+        expect(notification.meta.recurrentPaymentContext.id).toEqual(recurrentPaymentContext.id)
+    })
+
+    it('should not create RecurrentPayment - no receipts for period', async () => {
+        const { batches } = await makePayerWithMultipleConsumers(1, 2)
+        const [batch] = batches
+        const {
+            billingReceipts,
+        } = batch
+        const [{ period, createdAt }] = billingReceipts
+        const lastDt = dayjs(createdAt).startOf('day')
+        const periodDt = dayjs(period).startOf('month')
+        const lastDtString = lastDt.format(DATE_FORMAT)
+        const periods = [periodDt.add(2, 'months').format(DATE_FORMAT), periodDt.add(1, 'months').format(DATE_FORMAT)]
+
+        const [recurrentPaymentContext] = await createTestRecurrentPaymentContext(admin, {
+            ...getContextRequest(batch),
+            enabled: true,
+        })
+
+        // create recurrent payments
+        await processContext(adminContext, recurrentPaymentContext, periods, lastDtString)
+
+        const recurrentPayments = await RecurrentPayment.getAll(admin, {
+            recurrentPaymentContext: { id: recurrentPaymentContext.id },
+        })
+
+        expect(recurrentPayments).toHaveLength(0)
+    })
+
+    it('should not create RecurrentPayment - last dt is bigger than created at', async () => {
+        const { batches } = await makePayerWithMultipleConsumers(1, 2)
+        const [batch] = batches
+        const {
+            billingReceipts,
+        } = batch
+        const [{ period }] = billingReceipts
+        const periodDt = dayjs(period).startOf('month')
+        const lastDtString = dayjs().add(1, 'day').format(DATE_FORMAT)
+        const periods = [periodDt.format(DATE_FORMAT), periodDt.add(1, 'months').format(DATE_FORMAT)]
+
+        const [recurrentPaymentContext] = await createTestRecurrentPaymentContext(admin, {
+            ...getContextRequest(batch),
+            enabled: true,
+        })
+
+        // create recurrent payments
+        await processContext(adminContext, recurrentPaymentContext, periods, lastDtString)
+
+        const recurrentPayments = await RecurrentPayment.getAll(admin, {
+            recurrentPaymentContext: { id: recurrentPaymentContext.id },
+        })
+
+        expect(recurrentPayments).toHaveLength(0)
+    })
+})
