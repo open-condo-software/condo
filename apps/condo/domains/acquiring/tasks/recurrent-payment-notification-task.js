7afa2e852232fb90670d0ddd00068e48be0bdc6a
ekabardinsky <ekabardinsky@gmail.com>
2023-03-14 12:59:15 +0500

    feat(condo): DOMA-3962 move createCronTask to implementation files

--- apps/condo/domains/acquiring/tasks/recurrent-payment-notification-task.js
+++
@@ -1,7 +0,0 @@
-const { createCronTask } = require('@open-condo/keystone/tasks')
-
-const { process } = require('@condo/domains/acquiring/tasks/recurrent-payment-notification/recurrent-payment-notification')
-
-module.exports = createCronTask('recurrentPaymentNotification', '0 * * * *', async () => {
-    await process()
-})
--- apps/condo/domains/acquiring/tasks/recurrent-payment-processing-task.js
+++
@@ -1,7 +0,0 @@
-const { createCronTask } = require('@open-condo/keystone/tasks')
-
-const { process } = require('@condo/domains/acquiring/tasks/recurrent-payments/recurrent-payment-processing')
-
-module.exports = createCronTask('recurrentPaymentProcessing', '0 * * * *', async () => {
-    await process()
-})
