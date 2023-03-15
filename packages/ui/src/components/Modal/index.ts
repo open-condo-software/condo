a41311828612e285fbe98248fb2e2f3fff9ca2ed
SavelevMatthew <savelevmatthew@gmail.com>
2023-03-15 13:24:07 +0500

    feat(ui): DOMA-5542 Added useModal hook

--- packages/ui/src/components/Modal/index.ts
+++ packages/ui/src/components/Modal/index.ts
@@ -1,5 +1,15 @@
-import { Modal } from './modal'
+import { Modal as ModalComponent } from './modal'
+import { useModal } from './useModal'
 import './style.less'
+
+type CompoundedModal = typeof ModalComponent & {
+    useModal: typeof useModal
+}
+
+const Modal: CompoundedModal = ModalComponent as CompoundedModal
+Modal.useModal = useModal
+
+
 
 export type { ModalProps } from './modal'
 export { Modal }
