98ab41ea72f6ecc53584395f23333331003b969e
Pavel <nomerdvadcatpyat@gmail.com>
2023-04-05 20:28:02 +0500

    feat(icons): DOMA-5745 added Print icon

---
+++ packages/icons/src/components/Print.tsx
@@ -0,0 +1,29 @@
+/** This file is auto-generated, do not edit it manually **/
+import React from 'react'
+
+import { IconWrapper, IconProps } from '../wrappers'
+
+export const Print: React.FC<IconProps> = ({ svgProps: props, ...restProps }) => {
+    return (
+        <IconWrapper
+            icon={
+                <svg
+                    width='inherit'
+                    height='inherit'
+                    fill='none'
+                    xmlns='http://www.w3.org/2000/svg'
+                    viewBox='0 0 24 24'
+                    {...props}
+                >
+                    <path
+                        fillRule='evenodd'
+                        clipRule='evenodd'
+                        d='M5 2a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v6h1a3 3 0 0 1 3 3v5a3 3 0 0 1-3 3h-1v3a1 1 0 0 1-1 1H6a1 1 0 0 1-1-1v-3H4a3 3 0 0 1-3-3v-5a3 3 0 0 1 3-3h1V2Zm-1 8a1 1 0 0 0-1 1v5a1 1 0 0 0 1 1h1v-3a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v3h1a1 1 0 0 0 1-1v-5a1 1 0 0 0-1-1H4Zm13-2H7V3h10v5Zm0 7H7v6h10v-6Z'
+                        fill='currentColor'
+                    />
+                </svg>
+            }
+            {...restProps}
+        />
+    )
+}
