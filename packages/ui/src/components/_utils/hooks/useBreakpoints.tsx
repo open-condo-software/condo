4ede0761f1e9e9bbd7576eec2f53c02bdd903b5a
Pavel <nomerdvadcatpyat@gmail.com>
2023-04-07 14:19:32 +0500

    fix(ui): DOMA-5838 set default breakpoint values to true

--- packages/ui/src/components/_utils/hooks/useBreakpoints.tsx
+++ packages/ui/src/components/_utils/hooks/useBreakpoints.tsx
@@ -5,8 +5,18 @@ import ResponsiveObserve from '../responsiveObserve'
 
 import { useForceUpdate } from './index'
 
+// NOTE: In ssr we do not know the width of the window, so we set all breakpoints to true by default
+const DEFAULT_BREAKPOINTS_VALUE: ScreenMap = {
+    MOBILE_SMALL: true,
+    MOBILE_LARGE: true,
+    TABLET_SMALL: true,
+    TABLET_LARGE: true,
+    DESKTOP_SMALL: true,
+    DESKTOP_LARGE: true,
+}
+
 export function useBreakpoints (refreshOnChange = true): ScreenMap {
-    const screensRef = useRef<ScreenMap>({})
+    const screensRef = useRef<ScreenMap>(DEFAULT_BREAKPOINTS_VALUE)
     const forceUpdate = useForceUpdate()
 
     useEffect(() => {
