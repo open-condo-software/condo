04fda92313d200d6c85e15257f78f34873aaf7d5
Pavel <nomerdvadcatpyat@gmail.com>
2023-04-10 14:33:18 +0500

    Revert "hotfix(condo): Disable reading of some relations (#3145)"
    This reverts commit e04f50b4f353dcfe19f5ffd21e7814ae5318b03d.

--- apps/condo/domains/organization/schema/Organization.js
+++ apps/condo/domains/organization/schema/Organization.js
@@ -82,13 +82,13 @@ const Organization = new GQLListSchema('Organization', {
             type: Relationship,
             ref: 'OrganizationEmployee.organization',
             many: true,
-            access: { read: false, create: false, update: false },
+            access: { create: false, update: false },
         },
         relatedOrganizations: {
             type: Relationship,
             ref: 'OrganizationLink.to',
             many: true,
-            access: { read: false, create: false, update: false },
+            access: { create: false, update: false },
         },
         statusTransitions: {
             schemaDoc: 'Graph of possible transitions for statuses. If there is no transition in this graph, ' +
