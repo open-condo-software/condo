e1f6b665695061599dd5b8abefb75ef21bdfb50f
Alllex202 <alllex.202@gmail.com>
2023-04-13 16:55:59 +0500

    hotfix(condo): fixed PageFieldRow

--- apps/condo/domains/common/components/PageFieldRow.tsx
+++ apps/condo/domains/common/components/PageFieldRow.tsx
@@ -25,10 +25,10 @@ const PageFieldRow: React.FC<IPageFieldRowProps> = (props) => {
     return (
         <Col span={24}>
             <Row gutter={[16, 0]}>
-                <Col lg={labelSpan} xs={24}>
+                <Col md={labelSpan} xs={24}>
                     <Typography.Paragraph style={{ fontSize: fontSizes.content }} ellipsis={ellipsis} title={title}>{title}</Typography.Paragraph>
                 </Col>
-                <Col lg={24 - labelSpan - 1} xs={24} offset={!breakpoints.TABLET_LARGE ? 0 : 1}>
+                <Col md={24 - labelSpan - 1} xs={24} offset={!breakpoints.TABLET_LARGE ? 0 : 1}>
                     <Typography.Text type={highlight ? 'success' : null} style={textStyle}>
                         {children}
                     </Typography.Text>
