a36c81342671fee93d71ecf6a325d5f5905a5b00
Daria Abushenko <2009add@gmail.com>
2023-04-20 13:36:19 +0500

    feat(condo): DOMA-5955 added active state for card

--- packages/ui/src/stories/Card.stories.tsx
+++ packages/ui/src/stories/Card.stories.tsx
@@ -10,6 +10,7 @@ export default {
         width: 400,
         bodyPadding: 24,
         hoverable: true,
+        active: false,
         children: 'A decision tree is a decision support tool that uses a tree-like model of decisions and their possible consequences, including chance event outcomes, resource costs, and utility. It is one way to display an algorithm that only contains conditional control statements.  Decision trees are commonly used in operations research, specifically in decision analysis, to help identify a strategy most likely to reach a goal, but are also a popular tool in machine learning.',
     },
 } as ComponentMeta<typeof Component>
