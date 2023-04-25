398d3f73121db3cd0c278ace5f5edb379542b0df
Alllex202 <alllex.202@gmail.com>
2023-04-25 20:24:08 +0500

    feat(ui): DOMA-5597 reworked component Select
    BREAKING-CHANGE: Select component no longer have "children" prop.
    Option and OptGroup components no longer have export from "@open-condo/ui".
    Now you should use "options" prop in Select component
    
    fix(ui): DOMA-5597 fixed Select styles. Fixed analytics

--- packages/ui/src/stories/Radio/RadioGroup.stories.tsx
+++ packages/ui/src/stories/Radio/RadioGroup.stories.tsx
@@ -3,6 +3,8 @@ import React from 'react'
 
 import { RadioGroup as Component, Radio, Space } from '@open-condo/ui/src'
 
+const { ItemGroup } = Component
+
 export default {
     title: 'Components/Radio',
     component: Component,
@@ -25,7 +27,16 @@ const Template: ComponentStory<typeof Component> = (props) => <Component {...pro
 
 export const RadioGroup = Template.bind({})
 RadioGroup.args = {
-    groups: [
+    children: <Space direction='vertical' size={12}>
+        <Radio label='Group option 1' value='value 1' />
+        <Radio label='Group option 2' value='value 2' />
+    </Space>,
+}
+
+
+export const ManyRadioGroups = Template.bind({})
+ManyRadioGroups.args = {
+    children: <>{[
         {
             name: 'Group 1',
             options: [{ label: 'Group option 1', value: 'value 1' }, { label: 'Group option 2', value: 'value 2' }],
@@ -38,13 +49,7 @@ RadioGroup.args = {
             name: 'Group 3',
             options: [{ label: 'Group option 5', value: 'value 5' }, { label: 'Group option 6', value: 'value 6' }],
         },
-    ],
-}
-
-export const CustomRadioGroup = Template.bind({})
-CustomRadioGroup.args = {
-    children: <Space direction='vertical' size={12}>
-        <Radio label='Group option 1' value='value 1' />
-        <Radio label='Group option 2' value='value 2' />
-    </Space>,
+    ].map(group => (
+        <ItemGroup name={group.name} options={group.options} key={group.name} />
+    ))}</>,
 }
