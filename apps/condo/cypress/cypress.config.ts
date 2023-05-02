7882f3e8c884f1166e92d789a8440de510cdfdaf
toplenboren <toplenboren@gmail.com>
2023-05-02 18:12:51 +0500

    chore(condo) DOMA-5849 do not record videos or shoot scrennshots if cypress is in load testing mode
    Signed-off-by: toplenboren <toplenboren@gmail.com>

--- apps/condo/cypress/cypress.config.ts
+++ apps/condo/cypress/cypress.config.ts
@@ -2,16 +2,21 @@ import { defineConfig } from 'cypress'
 
 import conf from '@open-condo/config'
 
-export default defineConfig({
+
+const LOAD_TESTING = conf['CYPRESS_IS_LOAD_TESTING']
+
+const config = {
     e2e: {
         baseUrl: conf['SERVER_URL'],
         projectId: 'dc36jj',
+        video: true,
         videoCompression: 15,
         waitForAnimations: true,
         defaultCommandTimeout: 10000,
+        screenshotOnRunFailure: true,
         retries: 3,
         setupNodeEvents (on, config) {
-            // eslint-disable-next-line import/order
+        // eslint-disable-next-line import/order
             require('./plugins/index.js')(on, config)
             require('./plugins/metrics.js')(on, config)
         },
@@ -24,4 +29,15 @@ export default defineConfig({
         pageLoadTimeout: 10000,
         numTestsKeptInMemory: 0,
     },
-})
+}
+
+if (LOAD_TESTING) {
+
+    console.log('Cypress is loaded in LOAD TESTING mode. No videos will be recorded, and no screenshots will be saved')
+
+    config.e2e.video = false
+    config.e2e.screenshotOnRunFailure = false
+    config.e2e.retries = 0
+}
+
+export default defineConfig(config)
