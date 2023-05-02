726b470c5f48dc09a46269fda536e525e1ca3fa6
toplenboren <toplenboren@gmail.com>
2023-05-02 17:49:20 +0500

    chore(condo) DOMA-5849 exec => spawn
    Signed-off-by: toplenboren <toplenboren@gmail.com>

--- apps/condo/bin/runCypressLoadTests.js
+++ apps/condo/bin/runCypressLoadTests.js
@@ -1,61 +1,61 @@
+const { spawn, exec } = require('child_process')
 const path = require('path')
 const util = require('util')
 
 const faker = require('faker')
 const { Client } = require('pg')
 
-const spawn = util.promisify(require('child_process').spawn)
-const exec = util.promisify(require('child_process').exec)
 
-const RUN_TESTS_COMMAND = 'yarn workspace @app/condo cypress run -b chrome -C ./cypress/cypress.config.ts --spec "./cypress/e2e/pages/auth.cy.js"'
+//const spawn = util.promisify(require('child_process').spawn)
+//const exec = util.promisify(require('child_process').exec)
+
+const RUN_TESTS_COMMAND = 'yarn'// -b chrome -C ./cypress/cypress.config.ts --spec "./cypress/e2e/pages/auth.cy.js"'
+
+const RUN_TESTS_ARGS = ['workspace', '@app/condo', 'cypress', 'run', '-b', 'chrome', 'C', './cypress/cypress.config.ts']
+
+// const RUN_TESTS_COMMAND = 'ls'
+// const RUN_TESTS_ARGS = ['-la']
+
+console.log( process.env.PATH )
 
 const runCypressInParallel = async (instances) => {
     const cypressInstances = []
+
     for (let i = 0; i < instances; i++) {
-        cypressInstances.push(exec(
-            RUN_TESTS_COMMAND,
-            (error, stdout, stderr) => {
-                if (error) {
-                    console.log(`error: ${error.message}`)
-                    return
-                }
-                if (stderr) {
-                    console.log(`stderr: ${stderr}`)
-                    return
-                }
-                console.log(`stdout: ${stdout}`)
-            })
-        )
+
+        const cypressInstance = spawn(RUN_TESTS_COMMAND, RUN_TESTS_ARGS)
+
+        cypressInstance.stdout.on('data', (data) => {
+            console.log(`STDOUT Instance: ${i}: ${data.toString()}`)
+        })
+
+        cypressInstance.stderr.on('data', (data) => {
+            console.log(`STDERR Instance: ${i}: ${data}`)
+        })
+
+        cypressInstance.on('close', (code) => {
+            console.log(`child process close all stdio with code ${code}`)
+        })
+
+        cypressInstance.on('error', (err) => {
+            console.log(`child process emitted an error: ${err}`)
+        })
+
+        cypressInstances.push(cypressInstance)
     }
+
     await Promise.all(cypressInstances)
-    await setTimeout(() => {console.log('Slept for 1 second')}, 1000)
+
+    console.log('All done!')
 }
 
-const setupPostgresStats = async (url) => {
-    const client = new Client(url)
-    await client.connect()
-    return setInterval(async () => {
-        const result = await client.query('SELECT COUNT(*) FROM pg_stat_activity')
-        console.log(`Query rate: ${result.rows[0].count} queries per second`)
-    }, 1000)
-}
 
 const runCypressLoadTests = async () => {
     const [,,instances, condoUrl] = process.argv
 
-    const { DATABASE_URL } = process.env
-
-    let postgresStats = await setupPostgresStats(DATABASE_URL)
     await runCypressInParallel(parseInt(instances), condoUrl)
 
-    postgresStats = null
+    await setTimeout(() => {console.log('Slept for 5 seconds')}, 5000)
 }
 
 runCypressLoadTests()
-    .then(() => {
-        console.log('\r\n')
-        console.log('All done')
-        process.exit(0)
-    }).catch((err) => {
-        console.error('Failed to done', err)
-    })
