1e8afb0ced05e9578df76c2ab7bf57d48c6aa087
toplenboren <toplenboren@gmail.com>
2023-05-03 15:06:19 +0500

    chore(condo) DOMA-5849 use support client instead of admin one
    Signed-off-by: toplenboren <toplenboren@gmail.com>

--- apps/condo/cypress/plugins/index.js
+++ apps/condo/cypress/plugins/index.js
@@ -3,8 +3,6 @@
  */
 const isEmpty = require('lodash/isEmpty')
 
-const { makeLoggedInAdminClient } = require('@open-condo/keystone/test.utils')
-
 const { OrganizationEmployee } = require('@condo/domains/organization/utils/testSchema')
 const { makeClientWithRegisteredOrganization } = require('@condo/domains/organization/utils/testSchema/Organization')
 const { buildingMapJson } = require('@condo/domains/property/constants/property')
@@ -29,24 +27,22 @@ module.exports = async (on, config) => {
         throw new Error('Please provide cypress with support credentials for correct user creation')
     }
 
-    // const supportClient = await makeLoggedInClient({ email: supportEmail, password: supportPassword })
+    const supportClient = await makeLoggedInClient({ email: supportEmail, password: supportPassword })
 
-    const admin = await makeLoggedInAdminClient()
+    const admin = null
 
     on('task', {
-        // async 'keystone:createUser' () {
-        //     return await createTestUser(supportClient)=
-        // },
         async 'keystone:createUser' () {
-            return await createTestUser(admin)
+            return await createTestUser(supportClient)
         },
         async 'keystone:createForgotPasswordAction' (user) {
-            return await createTestForgotPasswordAction(admin, user)
+            return await createTestForgotPasswordAction(supportClient, user)
         },
         async 'keystone:getConfirmPhoneAction' (phone) {
-            return await ConfirmPhoneAction.getAll(admin, { phone })
+            return await ConfirmPhoneAction.getAll(supportClient, { phone })
         },
-        async 'keystone:createUserWithProperty' (forceCreate = false) {
+
+        async 'keystone:createUserWithProperty' (forceCreate = true) {
             if (forceCreate || isEmpty(userObject)) {
                 const result = await makeClientWithProperty()
                 const client = await makeLoggedInClient(result.userAttrs)
@@ -69,8 +65,8 @@ module.exports = async (on, config) => {
             }
 
             return userObject
-
         },
+
         async 'keystone:createSupportWithProperty' () {
             if (isEmpty(supportObject)) {
                 const client = await makeClientWithRegisteredOrganization()
