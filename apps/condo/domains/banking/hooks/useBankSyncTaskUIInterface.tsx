0e53917a00fd50651cebadde74b01d30216efb0d
Sitnikov Ivan <sit7602@gmail.com>
2023-05-04 11:58:43 +0500

    fix(condo): DOMA-6068 add split bankaccount report card title by categories & handle external import at banksynctaskuiinterface

--- apps/condo/domains/banking/hooks/useBankSyncTaskUIInterface.tsx
+++ apps/condo/domains/banking/hooks/useBankSyncTaskUIInterface.tsx
@@ -6,6 +6,7 @@ import { useCallback } from 'react'
 import { useIntl } from '@open-condo/next/intl'
 import { Button } from '@open-condo/ui'
 
+import { SBBOL } from '@condo/domains/banking/constants'
 import { BankSyncTask } from '@condo/domains/banking/utils/clientSchema'
 import { ITask, TASK_REMOVE_STRATEGY } from '@condo/domains/common/components/tasks'
 import { TasksCondoStorage } from '@condo/domains/common/components/tasks/storage/TasksCondoStorage'
@@ -13,25 +14,29 @@ import { TASK_COMPLETED_STATUS } from '@condo/domains/common/constants/tasks'
 
 import type { BankSyncTask as BankSyncTaskType } from '@app/condo/schema'
 
+const BANK_ACCOUNT_REPORT_PAGE_PATHNAME = '/property/[id]/report'
+
 export const useBankSyncTaskUIInterface = () => {
     const intl = useIntl()
-    const BankSyncTaskProgressTitle = intl.formatMessage({ id: 'tasks.BankSyncTask.progress.title' })
+    const BankSyncTaskProgressTitle = intl.formatMessage({ id: 'tasks.BankSyncTask.file.progress.title' })
+    const BankSyncTaskExternalSystemProgressTitle = intl.formatMessage({ id: 'tasks.BankSyncTask.externalSystem.progress.title' })
     const BankSyncTaskProgressDescriptionPreparing = intl.formatMessage({ id: 'tasks.BankSyncTask.progress.description.preparing' })
     const BankSyncTaskProgressDescriptionProcessing = intl.formatMessage({ id: 'tasks.BankSyncTask.progress.description.processing' })
-    const BankSyncTaskProgressDescriptionCompleted = intl.formatMessage({ id: 'tasks.BankSyncTask.progress.description.completed' })
+    const BankSyncTaskProgressDescriptionCompleted = intl.formatMessage({ id: 'tasks.BankSyncTask.file.progress.description.completed' })
+    const BankSyncTaskExternalSystemProgressDescriptionCompleted = intl.formatMessage({ id: 'tasks.BankSyncTask.externalSystem.progress.description.completed' })
     const UpdateTitle = intl.formatMessage({ id: 'Update' })
 
-    const { reload, push } = useRouter()
+    const { reload, push, pathname } = useRouter()
 
     const getCompleteButtonClickHandler = useCallback((taskRecord) => () => {
         const propertyId = get(taskRecord, 'property.id')
 
-        if (propertyId) {
+        if (propertyId && BANK_ACCOUNT_REPORT_PAGE_PATHNAME === pathname) {
             push(`/property/${propertyId}/report/`)
         } else {
             reload()
         }
-    }, [reload, push])
+    }, [reload, push, pathname])
 
     const TaskUIInterface: ITask = {
         storage: new TasksCondoStorage({
@@ -39,12 +44,20 @@ export const useBankSyncTaskUIInterface = () => {
         }),
         removeStrategy: [TASK_REMOVE_STRATEGY.PANEL],
         translations: {
-            title: () => BankSyncTaskProgressTitle,
+            title: (taskRecord) => {
+                return get(taskRecord, 'options.type') === SBBOL
+                    ? BankSyncTaskExternalSystemProgressTitle
+                    : BankSyncTaskProgressTitle
+            },
             description: (taskRecord) => {
+                const completedMessage = get(taskRecord, 'options.type') === SBBOL
+                    ? BankSyncTaskExternalSystemProgressDescriptionCompleted
+                    : BankSyncTaskProgressDescriptionCompleted
+
                 // @ts-ignore
                 const { status, processedCount, totalCount } = taskRecord // this record is of type BankSyncTask
                 return status === TASK_COMPLETED_STATUS
-                    ? BankSyncTaskProgressDescriptionCompleted
+                    ? completedMessage
                     : !totalCount || !processedCount
                         ? BankSyncTaskProgressDescriptionPreparing
                         : BankSyncTaskProgressDescriptionProcessing
@@ -56,11 +69,17 @@ export const useBankSyncTaskUIInterface = () => {
             return Math.floor(task.processedCount / task.totalCount) * 100
         },
         onComplete: (taskRecord) => {
-            notification.success({
-                message: BankSyncTaskProgressDescriptionCompleted,
-                btn: <Button onClick={getCompleteButtonClickHandler(taskRecord)} type='primary'>{UpdateTitle}</Button>,
-                duration: 0,
-            })
+            const propertyId = get(taskRecord, 'property.id')
+            if (pathname === BANK_ACCOUNT_REPORT_PAGE_PATHNAME || propertyId) {
+                const message = get(taskRecord, 'options.type') === SBBOL
+                    ? BankSyncTaskExternalSystemProgressDescriptionCompleted
+                    : BankSyncTaskProgressDescriptionCompleted
+                notification.success({
+                    message,
+                    btn: <Button onClick={getCompleteButtonClickHandler(taskRecord)} type='primary'>{UpdateTitle}</Button>,
+                    duration: 0,
+                })
+            }
         },
         onCancel: () => null,
     }
