bb5d1cc8331149ed76b1f0787d00138c8ed787c9
Anton Aleshin <Anton_Aleschin@mail.ru>
2023-05-04 16:29:38 +0300

    hotfix(condo): removed old SBBOL Cron tasks

--- apps/condo/domains/organization/tasks/index.js
+++ apps/condo/domains/organization/tasks/index.js
@@ -1,6 +1,12 @@
+const { removeCronTask } = require('@open-condo/keystone/tasks')
+
 const refreshSbbolClientSecret = require('./refreshSbbolClientSecret')
 const { syncSbbolBankAccountsCron: syncSbbolBankAccounts } = require('./syncSbbolBankAccounts')
 const { syncSbbolTransactionsCron: syncSbbolTransactions } = require('./syncSbbolTransactions')
+
+removeCronTask('syncSbbolSubscriptions', '20 9 * * *')
+removeCronTask('syncSbbolSubscriptionPaymentRequestsState', '10 9 * * *')
+removeCronTask('syncSbbolPaymentRequestsForSubscriptions', '0 9 * * *')
 
 module.exports = {
     syncSbbolTransactions,
