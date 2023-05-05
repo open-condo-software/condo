3caba5f29f5693fc5c81e0e5e90eee3938a67559
Sitnikov Ivan <sit7602@gmail.com>
2023-05-05 10:03:55 +0500

    refactor(ui): DOMA-5893 rename modal prop

--- packages/ui/src/components/Modal/modal.tsx
+++ packages/ui/src/components/Modal/modal.tsx
@@ -13,7 +13,7 @@ type CondoModalProps = {
     title?: string
     open: boolean
     width?: CondoModalWidthType
-    fixedContentWidth?: boolean
+    scrollX?: boolean
 }
 
 export type ModalProps = Pick<DefaultModalProps,
@@ -36,14 +36,14 @@ const CONDO_MODAL_WIDTH: Readonly<Record<CondoModalWidthType, number | string>> 
 }
 
 const Modal: React.FC<ModalProps> = (props) => {
-    const { children, className, title, width = 'small', fixedContentWidth = false, footer, ...rest } = props
+    const { children, className, title, width = 'small', scrollX = true, footer, ...rest } = props
     const contentChildrenRef = useRef<HTMLDivElement>(null)
     const [isScrolling, setIsScrolling] = useState<boolean>(false)
 
     const classes = classNames(
         {
             [`${MODAL_CLASS_PREFIX}-scrolling`]: isScrolling,
-            [`${MODAL_CLASS_PREFIX}-fixed-width`]: fixedContentWidth,
+            [`${MODAL_CLASS_PREFIX}-fixed-width`]: !scrollX,
         },
         className,
     )
