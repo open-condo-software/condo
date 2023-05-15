6cab2144c23848eea554d956fdd9a3d9e7fea759
toplenboren <toplenboren@gmail.com>
2023-05-15 16:22:34 +0500

    feat(condo) HOTFIX: run yearly
    Signed-off-by: toplenboren <toplenboren@gmail.com>

--- apps/condo/domains/common/tasks/index.js
+++ apps/condo/domains/common/tasks/index.js
@@ -3,9 +3,9 @@ const { createCronTask } = require('@open-condo/keystone/tasks')
 
 const logger = getLogger('common-sample-task')
 
-// Will never be called. (At 00:00 on day-of-month 31 in February)
+// Runs yearly
 module.exportts = {
-    sampleTask: createCronTask('commonSampleCronTask', '0 0 31 2 *', () => {
+    sampleTask: createCronTask('commonSampleCronTask', '0 0 1 1 *', () => {
         logger.info('common-sample-cron-task successful call')
     }),
 }
