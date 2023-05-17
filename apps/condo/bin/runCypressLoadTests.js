5be69c552a3d39a550d5702ec7b52377b7760fe4
toplenboren <toplenboren@gmail.com>
2023-05-17 18:38:22 +0500

    chore(condo) DOMA-5849: continious test running
    Signed-off-by: toplenboren <toplenboren@gmail.com>

--- apps/condo/bin/runCypressLoadTests.js
+++ apps/condo/bin/runCypressLoadTests.js
@@ -1,12 +1,14 @@
 const { spawn } = require('child_process')
 
 const conf = require('@open-condo/config')
+const { getLogger } = require('@open-condo/keystone/logging')
 
 const CYPRESS_BROWSER = conf['CYPRESS_BROWSER'] || 'chrome'
 
+const logger = console
+
 const RUN_TESTS_COMMAND = 'yarn'
 const RUN_TESTS_ARGS = ['workspace', '@app/condo', 'cypress', 'run', '-b', CYPRESS_BROWSER, 'C', './cypress/cypress.config.ts']
-
 
 const trimMessage = (message) => {
     return message
@@ -21,14 +23,14 @@ const processCypressStdoutMessage = (message, instance) => {
     const msg = trimMessage(message.toString())
 
     if (msg.includes('Running:')) {
-        console.info(`STDOUT Cypress instance: ${instance} ${msg}`)
+        logger.info(`STDOUT Cypress instance: ${instance} ${msg}`)
     }
 }
 
 const processCypressStderrMessage = (message, instance) => {
     const msg = trimMessage(message.toString())
 
-    console.warn(`STDERR Cypress instance: ${instance} ${msg}`)
+    logger.warn(`STDERR Cypress instance: ${instance} ${msg}`)
 }
 
 
@@ -48,24 +50,68 @@ const runCypressInParallel = (instances) => {
         })
 
         cypressInstance.on('close', (code) => {
-            console.info(`Cypress instance ${i} close all stdio with code ${code}`)
+            logger.info(`Cypress instance ${i} close all stdio with code ${code}`)
         })
 
         cypressInstance.on('error', (err) => {
-            console.error(`Cypress instance ${i} emitted an error: ${err}`)
+            logger.error(`Cypress instance ${i} emitted an error: ${err}`)
         })
 
         cypressInstances.push(cypressInstance)
     }
-
-    return Promise.all(cypressInstances)
 }
 
 
-const runCypressLoadTests = async () => {
-    const [,,instances] = process.argv
+const runCypressContinuously = async (instances) => {
 
-    await runCypressInParallel(parseInt(instances))
+    let idx = 0
+    let cypressInstances = {}
+
+    // eslint-disable-next-line no-constant-condition
+    while (true) {
+
+        await new Promise(r => setTimeout(r, 1000))
+
+        if (Object.keys(cypressInstances).length >= instances) { continue }
+
+        const instanceId = idx
+        idx++
+        const cypressInstance = spawn(RUN_TESTS_COMMAND, RUN_TESTS_ARGS)
+        cypressInstance.stdout.on('data', (data) => {
+            processCypressStdoutMessage(data, instanceId)
+        })
+        cypressInstance.stderr.on('data', (data) => {
+            processCypressStderrMessage(data, instanceId)
+        })
+        cypressInstance.on('close', (code) => {
+            logger.info(
+                `IEVENT Cypress instance ${instanceId} close all stdio with code ${code}`)
+
+            delete cypressInstances[instanceId]
+            logger.info(`IEVENT Cypress instance ${instanceId} deleted`)
+        })
+        cypressInstance.on('error', (err) => {
+            logger.error(
+                `IEVENT Cypress instance ${instanceId} emitted an error: ${err}`)
+
+            delete cypressInstances[instanceId]
+            logger.info(`IEVENT Cypress instance ${instanceId} deleted`)
+        })
+        logger.info(`IEVENT Cypress instance ${instanceId} spawned`)
+        cypressInstances[instanceId] = cypressInstance
+    }
+}
+
+
+
+const runCypressLoadTests = async () => {
+    const [,,instances, continious] = process.argv
+
+    if (continious) {
+        await runCypressContinuously(parseInt(instances))
+    } else {
+        await runCypressInParallel(parseInt(instances))
+    }
 }
 
 runCypressLoadTests().then(async r => {
