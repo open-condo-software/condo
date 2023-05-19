d7daede674d8087c9740c5bbfe40d68539137ee7
AleX83Xpert <AleX83Xpert@mail.ru>
2023-05-19 14:18:23 +0500

    feat(condo): DOMA-5919 Empty NewsItemScope is not needed anymore for sending news item to all organization

--- apps/condo/domains/news/schema/NewsItem.test.js
+++ apps/condo/domains/news/schema/NewsItem.test.js
@@ -341,7 +341,6 @@ describe('NewsItems', () => {
 
                 // News item for all organization
                 const [newsItem3, newsItem3Attrs] = await createTestNewsItem(adminClient, o10n)
-                const [newsItemScope3] = await createTestNewsItemScope(adminClient, newsItem3)
 
                 await publishTestNewsItem(adminClient, newsItem1.id)
                 const newsItems1 = await NewsItem.getAll(residentClient1, {})
@@ -413,8 +412,8 @@ describe('NewsItems', () => {
                     unitName: unitName2,
                 })
 
+                // Without scope == all organization
                 const [newsItem3, newsItem3Attrs] = await createTestNewsItem(adminClient, o10n)
-                const [newsItemScope3] = await createTestNewsItemScope(adminClient, newsItem3)
 
                 const [newsItem4, newsItem4Attrs] = await createTestNewsItem(adminClient, o10n)
                 const [newsItemScope4] = await createTestNewsItemScope(adminClient, newsItem4, {
--- apps/condo/domains/news/utils/accessSchema/index.js
+++ apps/condo/domains/news/utils/accessSchema/index.js
@@ -2,52 +2,37 @@ const { flatten, get } = require('lodash')
 
 /**
  * @param {Resident[]} residents
- * @returns {{deletedAt: null, OR: Object[]}}}
+ * @returns {{OR: Object[]}}}
  */
 function queryFindNewsItemsScopesByResidents (residents) {
     return {
         OR: flatten(residents.map((resident) => {
-            const organizationId = get(resident, 'organization')
             const propertyId = get(resident, 'property')
             const unitType = get(resident, 'unitType')
             const unitName = get(resident, 'unitName')
 
             return [
                 {
-                    AND: [
+                    OR: [
                         {
-                            newsItem: { organization: { id: organizationId } },
+                            AND: [
+                                { property: { id: propertyId } },
+                                { unitType: null },
+                                { unitName: null },
+                            ],
                         },
                         {
-                            OR: [
-                                {
-                                    AND: [
-                                        { property_is_null: true },
-                                        { unitType: null },
-                                        { unitName: null },
-                                    ],
-                                },
-                                {
-                                    AND: [
-                                        { property: { id: propertyId } },
-                                        { unitType: null },
-                                        { unitName: null },
-                                    ],
-                                },
-                                {
-                                    AND: [
-                                        { property: { id: propertyId } },
-                                        { unitType },
-                                        { unitName: null },
-                                    ],
-                                },
-                                {
-                                    AND: [
-                                        { property: { id: propertyId } },
-                                        { unitType },
-                                        { unitName: unitName },
-                                    ],
-                                },
+                            AND: [
+                                { property: { id: propertyId } },
+                                { unitType },
+                                { unitName: null },
+                            ],
+                        },
+                        {
+                            AND: [
+                                { property: { id: propertyId } },
+                                { unitType },
+                                { unitName: unitName },
                             ],
                         },
                     ],
