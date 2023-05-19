5fdeb74fe32b69f5a0f954c1a7b625b702b1f002
Matthew <50069872+SavelevMatthew@users.noreply.github.com>
2023-05-19 18:39:13 +0500

    feat(icons): DOMA-5862 new icons imported (#3349)
    * feat(icons): DOMA-5862 new icons imported
    
    * feat(icons): DOMA-5862 updated key icon

--- packages/icons/src/components/History.tsx
+++ packages/icons/src/components/History.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 import { IconWrapper, IconProps } from '../wrappers'
 
-export const CalendarDays: React.FC<IconProps> = ({ svgProps: props, ...restProps }) => {
+export const History: React.FC<IconProps> = ({ svgProps: props, ...restProps }) => {
     return (
         <IconWrapper
             icon={
@@ -18,7 +18,7 @@ export const CalendarDays: React.FC<IconProps> = ({ svgProps: props, ...restProp
                     <path
                         fillRule='evenodd'
                         clipRule='evenodd'
-                        d='M8 1a1 1 0 0 1 1 1v1h6V2a1 1 0 1 1 2 0v1h2a3 3 0 0 1 3 3v14a3 3 0 0 1-3 3H5a3 3 0 0 1-3-3V6a3 3 0 0 1 3-3h2V2a1 1 0 0 1 1-1ZM7 5H5a1 1 0 0 0-1 1v3h16V6a1 1 0 0 0-1-1h-2v1a1 1 0 1 1-2 0V5H9v1a1 1 0 0 1-2 0V5Zm13 6H4v9a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1v-9ZM7 14a1 1 0 0 1 1-1h.01a1 1 0 1 1 0 2H8a1 1 0 0 1-1-1Zm4 0a1 1 0 0 1 1-1h.01a1 1 0 1 1 0 2H12a1 1 0 0 1-1-1Zm4 0a1 1 0 0 1 1-1h.01a1 1 0 1 1 0 2H16a1 1 0 0 1-1-1Zm-8 4a1 1 0 0 1 1-1h.01a1 1 0 1 1 0 2H8a1 1 0 0 1-1-1Zm4 0a1 1 0 0 1 1-1h.01a1 1 0 1 1 0 2H12a1 1 0 0 1-1-1Zm4 0a1 1 0 0 1 1-1h.01a1 1 0 1 1 0 2H16a1 1 0 0 1-1-1Z'
+                        d='M3 2a1 1 0 0 1 1 1v2.752l1.334-1.2a10.003 10.003 0 0 1 16.213 4.495 9.988 9.988 0 0 1-4.423 11.539 10.005 10.005 0 0 1-12.262-1.577 9.991 9.991 0 0 1-2.806-5.906 1 1 0 0 1 1.988-.22 7.991 7.991 0 0 0 6.93 7.052 8.004 8.004 0 0 0 8.492-5.07 7.988 7.988 0 0 0-2.928-9.44 8.004 8.004 0 0 0-9.87.618l-.002.001-1.059.953H8a1 1 0 0 1 0 2H3a1 1 0 0 1-1-1V3a1 1 0 0 1 1-1Zm9 3.997a1 1 0 0 1 1 1v4.379l3.447 1.722a1 1 0 0 1-.894 1.79l-4-2a1 1 0 0 1-.553-.894V6.997a1 1 0 0 1 1-1Z'
                         fill='currentColor'
                     />
                 </svg>
---
+++ packages/icons/src/components/Newspaper.tsx
@@ -0,0 +1,35 @@
+/** This file is auto-generated, do not edit it manually **/
+import React from 'react'
+
+import { IconWrapper, IconProps } from '../wrappers'
+
+export const Newspaper: React.FC<IconProps> = ({ svgProps: props, ...restProps }) => {
+    return (
+        <IconWrapper
+            icon={
+                <svg
+                    width='inherit'
+                    height='inherit'
+                    fill='none'
+                    xmlns='http://www.w3.org/2000/svg'
+                    viewBox='0 0 24 24'
+                    {...props}
+                >
+                    <path
+                        fillRule='evenodd'
+                        clipRule='evenodd'
+                        d='M5.879 1.879A3 3 0 0 1 8 1h12a3 3 0 0 1 3 3v16a3 3 0 0 1-3 3H4a3 3 0 0 1-3-3v-9c0-1.652 1.348-3 3-3h1V4a3 3 0 0 1 .879-2.121ZM5 10H4c-.548 0-1 .452-1 1v9a1 1 0 1 0 2 0V10Zm1.828 11A3 3 0 0 0 7 20V4a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v16a1 1 0 0 1-1 1H6.828Z'
+                        fill='currentColor'
+                    />
+                    <path
+                        fillRule='evenodd'
+                        clipRule='evenodd'
+                        d='M9 6a1 1 0 0 1 1-1h8a1 1 0 0 1 1 1v4a1 1 0 0 1-1 1h-8a1 1 0 0 1-1-1V6Zm2 1v2h6V7h-6Zm-2 7a1 1 0 0 1 1-1h8a1 1 0 1 1 0 2h-8a1 1 0 0 1-1-1Zm0 4a1 1 0 0 1 1-1h5a1 1 0 1 1 0 2h-5a1 1 0 0 1-1-1Z'
+                        fill='currentColor'
+                    />
+                </svg>
+            }
+            {...restProps}
+        />
+    )
+}
