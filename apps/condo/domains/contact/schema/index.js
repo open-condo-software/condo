fa2d8f967477fce01f90973273c52805c5edaa1f
Andrey Karmanov <andrey.karmanov.2011@yandex.ru>
2023-05-30 20:12:28 +0500

    Feature/condo/DOMA-6235/add contact from resident for org mutation (#3401)
    * feat(condo): DOMA-6235 Add Service
    
    * feat(condo): DOMA-6235 Add Service access
    
    * feat(condo): DOMA-6235 Add Service tests
    
    * feat(condo): DOMA-6235 Add Service test utils
    
    * feat(condo): DOMA-6235 Add Service schema
    
    * feat(condo): DOMA-6235 Fix linter errors
    
    * feat(condo): DOMA-6235 Add fix to return Contact with all fields
    
    * feat(condo): DOMA-6235 Update tests to not use getById
    
    * feat(condo): DOMA-6235 Update return types
    
    * feat(condo): DOMA-6235 Add contact by organization exist check
    
    * feat(condo): DOMA-6235 Add schemaDoc
    
    * feat(condo): DOMA-6235 Update loading entities by chunks
    
    * feat(condo): DOMA-6235 Update getting residents by properties ids

--- apps/condo/domains/contact/schema/index.js
+++ apps/condo/domains/contact/schema/index.js
@@ -3,6 +3,7 @@
  * In most cases you should not change it by hands. And please don't remove `AUTOGENERATE MARKER`s
  */
 
+const { _internalSyncContactsWithResidentsForOrganizationService } = require('./_internalSyncContactsWithResidentsForOrganizationService')
 const { Contact } = require('./Contact')
 const { ContactExportTask } = require('./ContactExportTask')
 const { ContactRole } = require('./ContactRole')
@@ -12,5 +13,6 @@ module.exports = {
     Contact,
     ContactRole,
     ContactExportTask,
+    _internalSyncContactsWithResidentsForOrganizationService,
 /* AUTOGENERATE MARKER <EXPORTS> */
 }
