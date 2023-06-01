4574b227c72d6906dd38c7c42e77cdfb1dcb5864
Sitnikov Ivan <sit7602@gmail.com>
2023-06-01 15:17:12 +0500

    fix(condo): DOMA-6268 security issue with external report organization access

--- apps/condo/domains/analytics/access/GetExternalReportIframeUrlService.js
+++ apps/condo/domains/analytics/access/GetExternalReportIframeUrlService.js
@@ -6,12 +6,16 @@ const isEmpty = require('lodash/isEmpty')
 const { throwAuthenticationError } = require('@open-condo/keystone/apolloErrorFormatter')
 const { find } = require('@open-condo/keystone/schema')
 
+const { checkUserBelongsToOrganization } = require('@condo/domains/organization/utils/accessSchema')
 const { queryOrganizationEmployeeFor, queryOrganizationEmployeeFromRelatedOrganizationFor } = require('@condo/domains/organization/utils/accessSchema')
 
-async function canGetExternalReportIframeUrl ({ authentication: { item: user }, args: { data: { id } } }) {
+async function canGetExternalReportIframeUrl ({ authentication: { item: user }, args: { data: { id, organizationId } } }) {
     if (!user) return throwAuthenticationError()
     if (user.deletedAt) return false
     if (user.isAdmin || user.isSupport) return {}
+
+    const userBelongsToOrganization = await checkUserBelongsToOrganization(user.id, organizationId)
+    if (!userBelongsToOrganization) return false
 
     const externalReport = await find('ExternalReport', {
         id,
