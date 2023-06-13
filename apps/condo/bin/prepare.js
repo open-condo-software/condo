03bb0a74208ca10cb3abe21ceab5965df1052009
Mikhail Gorbunov <toplenboren@gmail.com>
2023-06-13 21:55:32 +0500

    chore(condo) double stringify

--- apps/condo/bin/prepare.js
+++ apps/condo/bin/prepare.js
@@ -10,7 +10,7 @@ async function prepareAppEnvCypressConfig (appName) {
     if (appName !== 'condo') { throw new Error(`Can't configure Cypress support user for ${appName}. Cypress support user configuration is only available for condo app`) }
     const CYPRESS_SERVER_SUPPORT_EMAIL = 'service.condo.cypress.tests@example.com'
     const CYPRESS_SERVER_SUPPORT_PASSWORD = getRandomString()
-    const CYPRESS_SERVER_SUPPORT_OPTS = { password: CYPRESS_SERVER_SUPPORT_PASSWORD, isSupport: true, isAdmin: false, name: 'CypressSupportUser' }
+    const CYPRESS_SERVER_SUPPORT_OPTS = JSON.stringify({ password: CYPRESS_SERVER_SUPPORT_PASSWORD, isSupport: true, isAdmin: false, name: 'CypressSupportUser' })
     await safeExec(`yarn workspace @app/${appName} node ./bin/create-user.js ${CYPRESS_SERVER_SUPPORT_EMAIL} ${JSON.stringify(CYPRESS_SERVER_SUPPORT_OPTS)}`)
     await updateAppEnvFile(appName, 'CYPRESS_SERVER_SUPPORT_EMAIL', CYPRESS_SERVER_SUPPORT_EMAIL)
     await updateAppEnvFile(appName, 'CYPRESS_SERVER_SUPPORT_PASSWORD', CYPRESS_SERVER_SUPPORT_PASSWORD)
