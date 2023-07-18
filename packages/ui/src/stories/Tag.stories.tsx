565814b60dbf15f069586f949542325d4899919f
Alllex202 <alllex.202@gmail.com>
2023-07-18 13:18:00 +0500

    feat(ui): DOMA-6671 added icon for Tag

--- packages/ui/src/stories/Tag.stories.tsx
+++ packages/ui/src/stories/Tag.stories.tsx
@@ -1,8 +1,14 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react'
 import React from 'react'
 
+import * as condoIcons from '@open-condo/icons'
 import { Tag as Component } from '@open-condo/ui/src'
 import { colors } from '@open-condo/ui/src/colors'
+
+
+const icons = Object.assign({}, ...Object.entries(condoIcons).map(([key, Icon]) => ({
+    [`${key}-small`]: <Icon size='small'/>,
+})))
 
 export default {
     title: 'Components/Tag',
@@ -11,6 +17,19 @@ export default {
         children: 'Label',
         textColor: colors.gray['7'],
         bgColor: colors.gray['1'],
+    },
+    argTypes: {
+        icon: {
+            options: Object.keys(icons),
+            mapping: icons,
+            control: {
+                type: 'select',
+            },
+        },
+        iconPosition: {
+            control: { type: 'select' },
+            options: ['start', 'end'],
+        },
     },
 } as ComponentMeta<typeof Component>
 
