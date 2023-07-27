5d6f59c520815c4c0dfab9b50e9028d646613e82
Alllex202 <alllex.202@gmail.com>
2023-07-27 11:04:32 +0500

    fix(condo): DOMA-6670 fixed imports

--- packages/ui/src/components/Dropdown/index.ts
+++ packages/ui/src/components/Dropdown/index.ts
@@ -3,8 +3,8 @@ import './style.less'
 import { Dropdown as InternalDropdown } from './dropdown'
 import { DropdownButton } from './dropdownButton'
 
-export type { DropdownProps, ItemType } from './dropdown'
-export type { DropdownButtonProps } from './dropdownButton'
+export type { DropdownProps } from './dropdown'
+export type { DropdownButtonProps, ItemType } from './dropdownButton'
 
 
 type CombinedDropdownType = typeof InternalDropdown & {
