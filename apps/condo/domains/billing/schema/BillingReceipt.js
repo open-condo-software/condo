4cc2bbbe849a0eb66825bfcb867d5308003c4302
AleX83Xpert <AleX83Xpert@mail.ru>
2023-08-03 11:03:57 +0500

    Revert "feat(condo): DOMA-6556 prevent editing of BillingReceipt"
    This reverts commit 0caccc6d204f0f0be8b71f8f534a8610e702a5d7.

--- apps/condo/domains/billing/schema/BillingReceipt.js
+++ apps/condo/domains/billing/schema/BillingReceipt.js
@@ -6,7 +6,6 @@ const { Text, Relationship, Virtual } = require('@keystonejs/fields')
 const { Big } = require('big.js')
 const { get } = require('lodash')
 
-const { userIsAdmin } = require('@open-condo/keystone/access')
 const { historical, versioned, uuided, tracked, softDeleted, dvAndSender } = require('@open-condo/keystone/plugins')
 const { GQLListSchema, getById } = require('@open-condo/keystone/schema')
 
@@ -179,8 +178,8 @@ const BillingReceipt = new GQLListSchema('BillingReceipt', {
     plugins: [uuided(), versioned(), tracked(), softDeleted(), dvAndSender(), historical()],
     access: {
         read: access.canReadBillingReceipts,
-        create: userIsAdmin,
-        update: userIsAdmin,
+        create: access.canManageBillingReceipts,
+        update: access.canManageBillingReceipts,
         delete: false,
         auth: true,
     },
