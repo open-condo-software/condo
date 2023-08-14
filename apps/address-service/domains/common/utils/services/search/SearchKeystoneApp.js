5ef4d68ff4269a149855a765637bca7d79815b0c
AleX83Xpert <AleX83Xpert@mail.ru>
2023-08-14 17:12:05 +0500

    feat(condo): DOMA-6521 Use new address parser utility within SearchKeystoneApp

--- apps/address-service/domains/common/utils/services/search/SearchKeystoneApp.js
+++ apps/address-service/domains/common/utils/services/search/SearchKeystoneApp.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const { isArray, get, set, isEmpty } = require('lodash')
 
+const { AddressFromStringParser } = require('@open-condo/clients/address-service-client/utils')
 const { getLogger } = require('@open-condo/keystone/logging')
 
 const { OVERRIDING_ROOT } = require('@address-service/domains/address/constants')
-const { AddressFromStringParser } = require('@address-service/domains/common/utils/parseAddressesFromString')
 const { getSearchProvider } = require('@address-service/domains/common/utils/services/providerDetectors')
 
 const { createReturnObject } = require('./searchServiceUtils')
