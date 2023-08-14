47c0f8901fa1f23a7b8fa2715515df9822f7ab2b
ekabardinsky <ekabardinsky@gmail.com>
2023-08-14 15:27:15 +0500

    feat(condo): DOMA-6871 integrate with standalone semgrep SAST (#3721)
    * feat(condo): DOMA-6871 integrate with standalone semgrep SAST
    
    * feat(condo): DOMA-6871 integrate with semgrep sarif file
    
    * feat(condo): DOMA-6871 move SAST analysis to project root (in order to analyse ont a particular app)
    
    * feat(condo): DOMA-6871 move SAST analysis to project root (in order to analyse ont a particular app)
    
    * feat(condo): DOMA-6871 update semgrep ignore

---
+++ bin/run-semgrep.sh
@@ -0,0 +1,2 @@
+export SEMGREP_RULES="p/default p/expressjs p/react p/nextjs p/sql-injection p/jwt p/cwe-top-25 p/owasp-top-ten p/r2c-security-audit p/command-injection p/insecure-transport p/secrets p/xss p/gitleaks p/security-code-scan"
+semgrep ci --sarif --output=semgrep.sarif
