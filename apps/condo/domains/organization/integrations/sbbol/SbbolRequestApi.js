a2076026eb597be913a21a2ee40225a0a89e0c43
ekabardinsky <ekabardinsky@gmail.com>
2023-08-15 18:13:47 +0500

    fix(condo): DOMA-6871 fix code for semgrep default ruleset vulnerabilities

--- apps/condo/domains/organization/integrations/sbbol/SbbolRequestApi.js
+++ apps/condo/domains/organization/integrations/sbbol/SbbolRequestApi.js
@@ -41,7 +41,6 @@ class SbbolRequestApi {
         this.options = {
             hostname,
             port,
-            // nosemgrep: problem-based-packs.insecure-transport.js-node.bypass-tls-verification.bypass-tls-verification
             rejectUnauthorized: false,
             requestCert: true,
             timeout: REQUEST_TIMEOUT,
