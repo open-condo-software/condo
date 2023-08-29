40487bfbb618ecd14e8f165c181e2d62e29bc8f9
Pavel <nomerdvadcatpyat@gmail.com>
2023-08-29 16:29:44 +0500

    feat(condo): DOMA-7022 base expandable permissions table

--- apps/condo/domains/organization/components/EmployeeRolesSettingsContent.tsx
+++ apps/condo/domains/organization/components/EmployeeRolesSettingsContent.tsx
@@ -1,67 +1,139 @@
-import styled from '@emotion/styled'
+import { MinusCircleTwoTone } from '@ant-design/icons'
+import { SortOrganizationEmployeeRolesBy } from '@app/condo/schema'
 import { Col, Row, Typography } from 'antd'
+import { Table as AntdTable } from 'antd'
 import { Gutter } from 'antd/es/grid/row'
+import compact from 'lodash/compact'
 import get from 'lodash/get'
+import omit from 'lodash/omit'
+import uniq from 'lodash/uniq'
 import { useRouter } from 'next/router'
-import { TableComponents } from 'rc-table/lib/interface'
-import React, { useMemo } from 'react'
+import React, { useMemo, useState } from 'react'
 
+import { ChevronDown, ChevronUp } from '@open-condo/icons'
 import { useIntl } from '@open-condo/next/intl'
 import { useOrganization } from '@open-condo/next/organization'
+import { Checkbox } from '@open-condo/ui'
 
-import { DEFAULT_PAGE_SIZE, Table, TableRecord } from '@condo/domains/common/components/Table/Index'
-import { Tooltip } from '@condo/domains/common/components/Tooltip'
-import { getPageIndexFromOffset, parseQuery } from '@condo/domains/common/utils/tables.utils'
+import { Table } from '@condo/domains/common/components/Table/Index'
+import { parseQuery } from '@condo/domains/common/utils/tables.utils'
 import { useEmployeeRolesTableColumns } from '@condo/domains/organization/hooks/useEmployeeRolesTableColumns'
 import { OrganizationEmployeeRole } from '@condo/domains/organization/utils/clientSchema'
 
-const StyledTable = styled(Table)`
-  .ant-table-cell-ellipsis {
-    white-space: inherit;
-  }
-  
-  .ant-table-cell-row-hover {
-    background-color: inherit !important;
-    cursor: not-allowed;
-  }
-`
+import { Loader } from '../../common/components/Loader'
+import { B2BAppPermission, B2BAppRole } from '../../miniapp/utils/clientSchema'
 
 const MEDIUM_VERTICAL_GUTTER: [Gutter, Gutter] = [0, 40]
+
+const Check = ({ initialValue }) => {
+    const [checked, setChecked] = useState<boolean>(initialValue)
+    const handleChange = e => setChecked(e.target.checked)
+
+    return (
+        <div style={{ width: '100px' }}>
+            <Checkbox checked={checked} onChange={handleChange} />
+        </div>
+    )
+}
+
+const OUTER_TABLE_ID = 'outer-table'
+function getPopupContainer (): HTMLElement {
+    if (typeof document !== 'undefined') {
+        return document.getElementById(OUTER_TABLE_ID)
+    }
+}
+
 
 export const EmployeeRolesSettingsContent = () => {
     const intl = useIntl()
     const TitleMessage = intl.formatMessage({ id: 'EmployeeRoles' })
-    const EditProhibitedMessage = intl.formatMessage({ id: 'EditProhibited' })
-
-    const router = useRouter()
-    const { offset } = parseQuery(router.query)
 
     const userOrganization = useOrganization()
     const userOrganizationId = useMemo(() => get(userOrganization, ['organization', 'id']), [userOrganization])
 
-    const currentPageIndex = getPageIndexFromOffset(offset, DEFAULT_PAGE_SIZE)
-
     const {
         loading: isRolesLoading,
-        count: totalRoles,
+        count: rolesCount,
         objs: roles,
     } = OrganizationEmployeeRole.useObjects({
         where: { organization: { id: userOrganizationId } },
-        first: DEFAULT_PAGE_SIZE,
-        skip: (currentPageIndex - 1) * DEFAULT_PAGE_SIZE,
+        sortBy: [SortOrganizationEmployeeRolesBy.NameAsc],
     })
 
-    const tableColumns = useEmployeeRolesTableColumns([])
+    const {
+        loading: isB2BAppRolesLoading,
+        count: b2BAppRolesCount,
+        objs: b2BAppRoles,
+    } = B2BAppRole.useObjects({
+        where: { role: { id_in: roles.map(role => role.id) } },
+    })
 
-    const tableComponents: TableComponents<TableRecord> = useMemo(() => ({
-        body: {
-            row: (props) => (
-                <Tooltip showArrow title={EditProhibitedMessage}>
-                    <tr {...props} />
-                </Tooltip>
-            ),
+    const connectedB2BAppIds = uniq(compact(b2BAppRoles.map(b2BAppRole => get(b2BAppRole, 'app.id'))))
+
+    const {
+        loading: isB2BAppPermissionsLoading,
+        objs: b2BAppPermissions,
+    } = B2BAppPermission.useObjects({
+        where: { app: { id_in: connectedB2BAppIds } },
+    })
+
+    const totalRows = connectedB2BAppIds.length
+    const loading = isRolesLoading || isB2BAppRolesLoading || isB2BAppPermissionsLoading
+
+    const rowGroups = [
+        {
+            id: '3',
+            key: '3',
+            groupName: 'Сервисы',
+            permissions: [
+                {
+                    name: 'Подключение сервисов',
+                    '1': true,
+                    '2': false,
+                    '11': false,
+                    '22': false,
+                    '111': false,
+                    '222': false,
+                },
+            ],
         },
-    }), [EditProhibitedMessage])
+        {
+            id: '4',
+            key: '4',
+            groupName: 'Пропуска',
+            permissions: [
+                {
+                    id: '5',
+                    key: '5',
+                    name: 'Просмотр пропусков',
+                    '1': true,
+                    '2': true,
+                    '11': false,
+                    '22': false,
+                    '111': false,
+                    '222': false,
+                },
+                {
+                    id: '6',
+                    key: '6',
+                    name: 'Управление пропусками',
+                    '1': true,
+                    '2': false,
+                    '11': false,
+                    '22': false,
+                    '111': false,
+                    '222': false,
+                },
+            ],
+        },
+    ]
+
+    const tableColumns = useEmployeeRolesTableColumns(roles)
+    const dataForTable = [...rowGroups]
+
+    if (loading) return <Loader />
+
+    console.log(b2BAppRoles, b2BAppPermissions)
 
     return (
         <Row gutter={MEDIUM_VERTICAL_GUTTER}>
@@ -69,13 +141,97 @@ export const EmployeeRolesSettingsContent = () => {
                 <Typography.Title level={3}>{TitleMessage}</Typography.Title>
             </Col>
             <Col span={24}>
-                <StyledTable
-                    totalRows={totalRoles}
-                    loading={isRolesLoading}
-                    dataSource={roles}
+                <Table
+                    id={OUTER_TABLE_ID}
+                    sticky
+                    pagination={false}
+                    totalRows={totalRows}
+                    loading={loading}
+                    dataSource={dataForTable}
                     columns={tableColumns}
                     data-cy='employeeRoles__table'
-                    components={tableComponents}
+                    rowClassName={(record, index) => {
+                        const classNames = ['condo-table-expandable-row']
+
+                        if (record.expanded) {
+                            classNames.push('condo-table-expandable-row-expanded')
+                        }
+                        if (index === dataForTable.length - 1) {
+                            classNames.push('condo-table-expandable-row-last-row')
+                        }
+
+                        return classNames.join(' ')
+                    }}
+                    expandable={{
+                        indentSize: 0,
+                        expandRowByClick: true,
+                        columnWidth: '60px',
+                        expandedRowClassName: (record, index, indent) => {
+                            return 'condo-table-expandable-row-inner-row'
+                        },
+                        onExpand: (expanded, record) => {
+                            record.expanded = expanded
+                        },
+                        expandIcon: ({ expanded, onExpand, record }) =>
+                            expanded ? (
+                                <ChevronUp size='medium' onClick={e => onExpand(record, e)} />
+                            ) : (
+                                <ChevronDown size='medium' onClick={e => onExpand(record, e)} />
+                            ),
+                        expandedRowRender: (record) => {
+                            const dataSource = record.permissions
+
+                            const columns = [
+                                {
+                                    dataIndex: 'name',
+                                    width: '20%',
+                                },
+                                {
+                                    dataIndex: '1',
+                                    render: (value) => <Check initialValue={value} />,
+                                    width: '100px',
+                                },
+                                {
+                                    dataIndex: '2',
+                                    render: (value) => <Check initialValue={value} />,
+                                    width: '100px',
+                                },
+                                {
+                                    dataIndex: '11',
+                                    render: (value) => <Check initialValue={value} />,
+                                    width: '100px',
+                                },
+                                {
+                                    dataIndex: '22',
+                                    render: (value) => <Check initialValue={value} />,
+                                    width: '100px',
+                                },
+                                {
+                                    dataIndex: '111',
+                                    render: (value) => <Check initialValue={value} />,
+                                    width: '100px',
+                                },
+                                {
+                                    dataIndex: '222',
+                                    render: (value) => <Check initialValue={value} />,
+                                    width: '100px',
+                                },
+                                {
+                                    width: '60px',
+                                },
+                            ]
+
+                            return <Table
+                                tableLayout='auto'
+                                rowClassName='inner-table-row'
+                                showHeader={false}
+                                pagination={false}
+                                dataSource={dataSource}
+                                columns={columns}
+                                getPopupContainer={getPopupContainer}
+                            />
+                        },
+                    }}
                 />
             </Col>
         </Row>
--- apps/condo/domains/organization/hooks/useEmployeeRolesTableColumns.tsx
+++ apps/condo/domains/organization/hooks/useEmployeeRolesTableColumns.tsx
@@ -1,3 +1,5 @@
+import { OrganizationEmployeeRole } from '@app/condo/schema'
+import { Table } from 'antd'
 import get from 'lodash/get'
 import { useRouter } from 'next/router'
 import { useCallback, useMemo } from 'react'
@@ -17,59 +19,29 @@ import {
     PROPERTY_TICKET_VISIBILITY,
 } from '../constants/common'
 
-export function useEmployeeRolesTableColumns<T> (filterMetas: Array<FiltersMeta<T>>): Array<Record<string, unknown>> {
+
+interface IRolePermissionsData {
+    roleID: string
+    roleName: string
+
+}
+
+export function useEmployeeRolesTableColumns (roles): Array<Record<string, unknown>> {
     const intl = useIntl()
-    const NameMessage = intl.formatMessage({ id: 'field.Role' })
-    const AvailableTicketsMessage = intl.formatMessage({ id: 'EmployeeRoles.availableTickets' })
-    const AllTicketsMessage = intl.formatMessage({ id: 'EmployeeRoles.availableTickets.allTickets' })
-    const TicketsBySelectedPropertiesMessage = intl.formatMessage({ id: 'EmployeeRoles.availableTickets.bySelectedProperties' })
-    const TicketsBySelectedPropertiesAndSpecializationsMessage = intl.formatMessage({ id: 'EmployeeRoles.availableTickets.bySelectedPropertiesAndSpecializations' })
-    const AssignedTicketsMessage = intl.formatMessage({ id: 'EmployeeRoles.availableTickets.assignedTickets' })
 
-    const router = useRouter()
-    const { filters } = parseQuery(router.query)
-
-    const renderAvailableTicketsByRoleMessage = useCallback((_, role) => {
-        const ticketVisibilityType = get(role, 'ticketVisibilityType')
-
-        switch (ticketVisibilityType) {
-            case ORGANIZATION_TICKET_VISIBILITY: {
-                return AllTicketsMessage
-            }
-            case PROPERTY_TICKET_VISIBILITY: {
-                return `${TicketsBySelectedPropertiesMessage} + ${AssignedTicketsMessage}`
-            }
-            case PROPERTY_AND_SPECIALIZATION_VISIBILITY: {
-                return `${TicketsBySelectedPropertiesAndSpecializationsMessage} + ${AssignedTicketsMessage}`
-            }
-            case ASSIGNED_TICKET_VISIBILITY: {
-                return AssignedTicketsMessage
-            }
-
-            default: {
-                return '—'
-            }
-        }
-    }, [AllTicketsMessage, AssignedTicketsMessage, TicketsBySelectedPropertiesAndSpecializationsMessage, TicketsBySelectedPropertiesMessage])
-
-    return useMemo(() => {
-        return [
-            {
-                title: NameMessage,
-                filteredValue: getFilteredValue<IFilters>(filters, 'name'),
-                dataIndex: 'name',
-                key: 'name',
-                sorter: true,
-                filterDropdown: getFilterDropdownByKey(filterMetas, 'name'),
-                filterIcon: getFilterIcon,
-                ellipsis: true,
-                width: '35%',
+    return [
+        {
+            dataIndex: 'groupName',
+            width: '20%',
+            key: 'groupName',
+        },
+        ...roles.map(role => ({
+            title: role.name,
+            key: role.id,
+            render: (text, record, index) => {
+                return <div style={{ width: '100px' }} />
             },
-            {
-                title: AvailableTicketsMessage,
-                key: 'description',
-                render: renderAvailableTicketsByRoleMessage,
-            },
-        ]
-    }, [NameMessage, filters, filterMetas, AvailableTicketsMessage, renderAvailableTicketsByRoleMessage])
+        })),
+        Table.EXPAND_COLUMN,
+    ]
 }
