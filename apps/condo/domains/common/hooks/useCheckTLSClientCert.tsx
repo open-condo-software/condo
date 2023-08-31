08bf5cc85f0228670f53649f68182d4e7ecca921
Anton Aleshin <Anton_Aleschin@mail.ru>
2023-08-31 15:06:36 +0300

    fix(condo): DOMA-6513 changed handling of not successful response from verificationUrl

--- apps/condo/domains/common/hooks/useCheckTLSClientCert.tsx
+++ apps/condo/domains/common/hooks/useCheckTLSClientCert.tsx
@@ -27,10 +27,13 @@ export const useCheckTLSClientCert = ({ onSuccess, onFail }: UseCheckSSLClientCe
             // Due to "no-cors" mode, there is no information about response status
             // See https://developer.mozilla.org/en-US/docs/Web/API/Request/mode
             // > JavaScript may not access any properties of the resulting Response. This ensures that ServiceWorkers do not affect the semantics of the Web and prevents security and privacy issues arising from leaking data across domains
-            if (response) {
+            if (response.ok) {
                 await onSuccess()
             } else {
-                console.error('Not successful response from server to verify TLS certificate, probably. Certificate may be correct and somethings is wrong on verification server', response)
+                // Due to lack of appropriate API in Sber and due to inability to get response status, we are making an assumption about certificate issue on the client when response is not successful.
+                // But, not successful response from server may not mean an issue with certificate on the client.
+                // When the assumption is wrong, there is a LoginWithSBBOLButton on `/tls` page with disabled checking of TLS certificate. So, a user can reach authorization in SberBusiness.
+                await onFail()
             }
         } catch (e) {
             // There is no possibility to catch `net::ERR_CERT_AUTHORITY_INVALID` error, because it occurs on transport level
