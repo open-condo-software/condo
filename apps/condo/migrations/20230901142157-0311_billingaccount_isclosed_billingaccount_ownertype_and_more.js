568244d5c1340c1e48e1f801e974132732bbf2cd
AleX83Xpert <AleX83Xpert@mail.ru>
2023-09-01 14:59:13 +0500

    feat(condo): DOMA-7063 sonar config + migration timeout

--- apps/condo/migrations/20230901142157-0311_billingaccount_isclosed_billingaccount_ownertype_and_more.js
+++ apps/condo/migrations/20230901142157-0311_billingaccount_isclosed_billingaccount_ownertype_and_more.js
@@ -4,6 +4,12 @@
 exports.up = async (knex) => {
     await knex.raw(`
     BEGIN;
+    
+--
+-- [CUSTOM] Set Statement Timeout to some large amount - 25 min (25 * 60 => 1500 sec)
+--
+SET statement_timeout = '1500s';
+
 --
 -- Add field isClosed to billingaccount
 --
@@ -22,6 +28,12 @@ ALTER TABLE "BillingAccountHistoryRecord" ADD COLUMN "isClosed" boolean NULL;
 -- Add field ownerType to billingaccounthistoryrecord
 --
 ALTER TABLE "BillingAccountHistoryRecord" ADD COLUMN "ownerType" text NULL;
+
+--
+-- [CUSTOM] Revert Statement Timeout to default amount - 10 secs
+--
+SET statement_timeout = '10s';
+
 COMMIT;
 
     `)
