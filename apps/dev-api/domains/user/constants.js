72ac78c38545e3766c7093856c41ce323a385ae1
SavelevMatthew <savelevmatthew@gmail.com>
2023-09-06 14:49:29 +0500

    feat(dev-api): DOMA-7048 Added basic user model

---
+++ apps/dev-api/domains/user/constants.js
@@ -0,0 +1,5 @@
+const MIN_PASSWORD_LENGTH = 12
+
+module.exports = {
+    MIN_PASSWORD_LENGTH,
+}
--- apps/dev-api/index.js
+++ apps/dev-api/index.js
@@ -1,7 +1,7 @@
 const { prepareKeystone } = require('@open-condo/keystone/KSv5v6/v5/prepareKeystone')
 
 const schemas = () => [
-//
+    require('@dev-api/domains/user/schema'),
 ]
 
 module.exports = prepareKeystone({
