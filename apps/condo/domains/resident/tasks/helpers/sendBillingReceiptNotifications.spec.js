728effa6527aefba155da269b2bf6ad275b0aaaa
AleX83Xpert <AleX83Xpert@mail.ru>
2023-09-07 18:15:36 +0500

    feat(condo): DOMA-6817 refactoring of existing tests
    `feature-flags` header was removed

--- apps/condo/domains/resident/tasks/helpers/sendBillingReceiptNotifications.spec.js
+++ apps/condo/domains/resident/tasks/helpers/sendBillingReceiptNotifications.spec.js
@@ -4,7 +4,7 @@
 
 const index = require('@app/condo/index')
 
-const { setFakeClientMode } = require('@open-condo/keystone/test.utils')
+const { setFakeClientMode, setAllFeatureFlags } = require('@open-condo/keystone/test.utils')
 
 const { sendBillingReceiptNotifications } = require('./sendBillingReceiptNotifications')
 
@@ -14,11 +14,13 @@ describe('sendBillingReceiptNotifications', () => {
 
     describe('feature flag', () => {
         it('checks for proper result on enabled', async () => {
-            expect(await sendBillingReceiptNotifications({ req: { headers: { 'feature-flags': 'true' } } })).toBeUndefined()
+            setAllFeatureFlags(true)
+            expect(await sendBillingReceiptNotifications()).toBeUndefined()
         })
 
         it('checks for proper result on disabled', async () => {
-            expect(await sendBillingReceiptNotifications({ req: { headers: { 'feature-flags': 'false' } } })).toEqual('disabled')
+            setAllFeatureFlags(false)
+            expect(await sendBillingReceiptNotifications()).toEqual('disabled')
         })
     })
 })
