f1956c9ef79ef17b98af86e51631d99453d3c9a4
Alexander Naydenov <AleX83Xpert@mail.ru>
2023-09-08 10:49:54 +0500

    feat(condo): DOMA-6963 add bin-script for discover service consumers mutation (#3829)
    * feat(condo): DOMA-6963 add bin-script for discover service consumers mutation
    
    * feat(condo): DOMA-6963 fix bin files description
    
    * feat(condo): DOMA-6963 add ability to process async functions for `loadListByChunks()`
    
    * feat(condo): DOMA-6963 install the `commander@11.0.0` package
    
    * feat(condo): DOMA-6963 fix option's description
    
    * feat(condo): DOMA-6963 put cross-env back

---
+++ apps/condo/bin/billing/discoverServiceConsumers.js
@@ -0,0 +1,73 @@
+/**
+ * Creates ServiceConsumer models for given organization
+ *
+ * Usage:
+ *      yarn workspace @app/condo node bin/billing/discoverServiceConsumers -o <organizationId>
+ */
+
+const path = require('path')
+
+const commander = require('commander')
+const { map } = require('lodash')
+
+const { prepareKeystoneExpressApp } = require('@open-condo/keystone/test.utils')
+
+const { BillingAccount } = require('@condo/domains/billing/utils/serverSchema')
+const { UUID_REGEXP } = require('@condo/domains/common/constants/regexps')
+const { loadListByChunks } = require('@condo/domains/common/utils/serverSchema')
+const { discoverServiceConsumers } = require('@condo/domains/resident/utils/serverSchema')
+
+const DV_SENDER = { dv: 1, sender: { dv: 1, fingerprint: 'discoverServiceConsumersScript' } }
+
+const program = new commander.Command()
+program.requiredOption('-o, --organization <organizationId>', 'The organization\'s uuid', (value, previous) => {
+    if (!UUID_REGEXP.test(value)) {
+        throw new commander.InvalidArgumentError('Not a UUID.')
+    }
+
+    return value
+})
+
+async function main () {
+    program.parse()
+    /**
+     * @type {{organization: string}}
+     */
+    const options = program.opts()
+
+    const { keystone: context } = await prepareKeystoneExpressApp(
+        path.resolve('./index.js'),
+        { excludeApps: ['NextApp', 'AdminUIApp'] },
+    )
+
+    await loadListByChunks({
+        context,
+        list: BillingAccount,
+        where: {
+            context: { organization: { id: options.organization } },
+            deletedAt: null,
+        },
+        chunkSize: 50,
+        chunkProcessor: async (/** @type {BillingAccount[]} */ chunk) => {
+            try {
+                const result = await discoverServiceConsumers(context, {
+                    ...DV_SENDER,
+                    billingAccountsIds: map(chunk, 'id'),
+                })
+                console.log(`chunk[${chunk.length}] result: ${JSON.stringify(result)}`)
+            } catch (err) {
+                console.error(`chunk[${chunk.length}] error: ${err.message}`)
+            }
+
+            return []
+        },
+    })
+}
+
+main().then(() => {
+    console.info('✅ All done!')
+    process.exit()
+}).catch((e) => {
+    console.error(e)
+    process.exit(1)
+})
