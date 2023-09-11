819b967f54201672c5be12f84dc7e0310d082c5f
Sitnikov Ivan <sit7602@gmail.com>
2023-09-11 10:45:24 +0500

    chore(condo): DOMA-7004 rid of Bigjs at property dataloader

--- apps/condo/domains/analytics/utils/services/dataLoaders/property.js
+++ apps/condo/domains/analytics/utils/services/dataLoaders/property.js
@@ -1,5 +1,3 @@
-const Big = require('big.js')
-
 const { AbstractDataLoader } = require('@condo/domains/analytics/utils/services/dataLoaders/AbstractDataLoader')
 const { GqlWithKnexLoadList } = require('@condo/domains/common/utils/serverSchema')
 
@@ -17,7 +15,7 @@ class PropertyDataLoader extends AbstractDataLoader {
         const propertyIds = await propertyUnitDataLoader.load()
         const result = await propertyUnitDataLoader.loadAggregate('SUM("unitsCount")', propertyIds.map(({ id }) => id))
 
-        return { sum: new Big(result.sum || 0).toFixed(0) }
+        return { sum: result.sum || 0 }
     }
 }
 
