4c188217f881263422046a5c02af8697f347a6c2
AleX83Xpert <AleX83Xpert@mail.ru>
2023-09-13 10:16:48 +0500

    chore(condo): DOMA-7111 fix linter error

--- apps/condo/domains/acquiring/gql.js
+++ apps/condo/domains/acquiring/gql.js
@@ -7,6 +7,7 @@
 const { gql } = require('graphql-tag')
 
 const { generateGqlQueries } = require('@open-condo/codegen/generate.gql')
+
 const { ADDRESS_META_SUBFIELDS_QUERY_LIST } = require('@condo/domains/property/schema/fields/AddressMetaField')
 
 const COMMON_FIELDS = 'id dv sender { dv fingerprint } v deletedAt newId createdBy { id name } updatedBy { id name } createdAt updatedAt'
