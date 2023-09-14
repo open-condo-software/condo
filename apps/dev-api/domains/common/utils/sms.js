637939d3858fe54609134c534c76148da3f67a53
SavelevMatthew <savelevmatthew@gmail.com>
2023-09-14 15:55:14 +0500

    feat(dev-api): DOMA-7048 added condo SMS providers

--- apps/dev-api/domains/common/utils/sms.js
+++ apps/dev-api/domains/common/utils/sms.js
@@ -1,8 +1,22 @@
 const conf = require('@open-condo/config')
 
+const { developmentClient, productionClient } = require('@dev-api/domains/common/utils/serverClients')
+
 class FakeSMSAdapter {
     async sendMessage (phone, message) {
         console.log(JSON.stringify({ adapter: 'Fake SMS Adapter', phone, message }))
+    }
+}
+
+class CondoSMSAdapter {
+    #client
+
+    constructor (serverClient) {
+        this.#client = serverClient
+    }
+
+    async sendMessage (phone, message) {
+        await this.#client.sendMessage({ phone }, message)
     }
 }
 
@@ -10,8 +24,15 @@ class SMSAdapter {
     #internalAdapter = null
 
     constructor (type = 'fake') {
-        if (type === 'fake') {
-            this.#internalAdapter = new FakeSMSAdapter()
+        switch (type) {
+            case 'condo-dev':
+                this.#internalAdapter = new CondoSMSAdapter(developmentClient)
+                break
+            case 'condo-prod':
+                this.#internalAdapter = new CondoSMSAdapter(productionClient)
+                break
+            default:
+                this.#internalAdapter = new FakeSMSAdapter()
         }
     }
 
