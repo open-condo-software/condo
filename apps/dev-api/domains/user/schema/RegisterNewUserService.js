1b85b8fbe5187b4fde48e78a318805fc6961b6d5
SavelevMatthew <savelevmatthew@gmail.com>
2023-09-18 12:47:27 +0500

    feat(dev-api): DOMA-7048 added basic registration tests

--- apps/dev-api/domains/user/schema/RegisterNewUserService.js
+++ apps/dev-api/domains/user/schema/RegisterNewUserService.js
@@ -4,9 +4,27 @@
 
 const dayjs = require('dayjs')
 
+const { GQLError, GQLErrorCode: { BAD_USER_INPUT } } = require('@open-condo/keystone/errors')
 const { GQLCustomSchema, getById } = require('@open-condo/keystone/schema')
 
+const {
+    USER_ALREADY_EXISTS,
+    ACTION_NOT_FOUND,
+} = require('@dev-api/domains/user/constants/errors')
 const { User, ConfirmPhoneAction } = require('@dev-api/domains/user/utils/serverSchema')
+
+const ERRORS = {
+    USER_ALREADY_EXISTS: {
+        code: BAD_USER_INPUT,
+        type: USER_ALREADY_EXISTS,
+        message: 'User with specified phone number is already registered',
+    },
+    ACTION_NOT_FOUND: {
+        code: BAD_USER_INPUT,
+        type: ACTION_NOT_FOUND,
+        message: 'ConfirmPhoneAction with the specified ID is not verified, expired, or does not exist',
+    },
+}
 
 const RegisterNewUserService = new GQLCustomSchema('RegisterNewUserService', {
     types: [
@@ -15,7 +33,6 @@ const RegisterNewUserService = new GQLCustomSchema('RegisterNewUserService', {
             type: 'input RegisterNewUserInput { dv: Int!, sender: SenderFieldInput!, confirmPhoneActionId: String!, name: String!, password: String!}',
         },
     ],
-    
     mutations: [
         {
             access: true,
@@ -25,13 +42,14 @@ const RegisterNewUserService = new GQLCustomSchema('RegisterNewUserService', {
 
                 const currentTime = dayjs().toISOString()
                 const confirmAction = await ConfirmPhoneAction.getOne(context, {
+                    deletedAt: null,
                     id: confirmPhoneActionId,
                     isVerified: true,
                     expiresAt_gte: currentTime,
                 })
 
                 if (!confirmAction) {
-                    throw new Error('NOT FOUND')
+                    throw new GQLError(ERRORS.ACTION_NOT_FOUND, context)
                 }
 
 
@@ -41,6 +59,10 @@ const RegisterNewUserService = new GQLCustomSchema('RegisterNewUserService', {
                     sender,
                     phone: confirmAction.phone,
                     password,
+                }, {
+                    errorMapping: {
+                        'duplicate key value violates unique constraint "user_unique_phone"': ERRORS.USER_ALREADY_EXISTS,
+                    },
                 })
 
                 return await getById('User', createdUser.id)
---
+++ apps/dev-api/domains/user/utils/password.js
@@ -0,0 +1,37 @@
+const { MIN_PASSWORD_LENGTH, MIN_PASSWORD_DIFFERENT_CHARACTERS } = require('@dev-api/domains/user/constants')
+
+const UNICODE_CHARS_REGEX = /[\w\W]/gu
+
+function getStringLength (str){
+    const chars = str.match(UNICODE_CHARS_REGEX)
+    return Array.isArray(chars) ? chars.length : 0
+}
+
+function hasDifferentCharacters (str, amount = 0) {
+    const chars = str.match(UNICODE_CHARS_REGEX)
+    return Array.isArray(chars) && (new Set(chars)).size >= amount
+}
+
+function hasPhoneInside (str, phone, prefixLength = 7) {
+    let croppedPhone = phone.length > prefixLength ? phone.substring(phone.length - prefixLength) : phone
+    return str.includes(croppedPhone)
+}
+
+function isValidPassword (password, extraData = {}) {
+    const length = getStringLength(password)
+    if (length < MIN_PASSWORD_LENGTH) {
+        return false
+    }
+
+    if (!hasDifferentCharacters(password, MIN_PASSWORD_DIFFERENT_CHARACTERS)) {
+        return false
+    }
+
+    return !(extraData.hasOwnProperty('phone') && hasPhoneInside(password, extraData.phone))
+
+
+}
+
+module.exports = {
+    isValidPassword,
+}
