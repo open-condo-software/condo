652c230d20d2904fe34142c2e05facd12cda0864
Alllex202 <alllex.202@gmail.com>
2023-09-18 17:24:41 +0500

    refactor(condo): DOMA-7056 refactored after review

--- apps/condo/domains/user/utils/directAccess/config.js
+++ apps/condo/domains/user/utils/directAccess/config.js
@@ -23,9 +23,12 @@ const DIRECT_ACCESS_AVAILABLE_SCHEMAS = {
         'B2CAppAccessRight',
         'B2CAppBuild',
         'B2CAppProperty',
-        'Ticket',
+
         // Organization domain
         { schemaName: 'Organization', readonly: true },
+
+        // Ticket domain
+        'Ticket',
     ],
     services: [
         'registerNewServiceUser',
