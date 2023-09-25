d82ec3e46528eff4eaa17ac273a2b2fc4312e6e3
SavelevMatthew <savelevmatthew@gmail.com>
2023-09-25 11:21:53 +0500

    ci(condo): DOMA-7048 check API empty response with "||"

--- .github/workflows/waitForLocalhostApiReady.sh
+++ .github/workflows/waitForLocalhostApiReady.sh
@@ -6,7 +6,7 @@ API_SLEEP_INTERVAL=3
 API_WAIT_TIMEOUT=120
 API_RESULT=$(curl -s 'http://localhost:3000/admin/api' --data-raw '{"operationName":null,"variables":{},"query":"{appVersion}"}' -H 'content-type: application/json' | tr -d ' \n')
 # API_RESULT is empty if server is down!
-while [[ "${API_RESULT}" != *"appVersion"* && "${API_RESULT}" != "" && ((${API_WAIT_TIMEOUT} > 0)) ]]; do
+while [[ ("${API_RESULT}" != *"appVersion"* || "${API_RESULT}" == "") && ((${API_WAIT_TIMEOUT} > 0)) ]]; do
   sleep ${API_SLEEP_INTERVAL}
   API_RESULT=$(curl -s 'http://localhost:3000/admin/api' --data-raw '{"operationName":null,"variables":{},"query":"{appVersion}"}' -H 'content-type: application/json' | tr -d ' \n')
   ((API_WAIT_TIMEOUT-=${API_SLEEP_INTERVAL}))
