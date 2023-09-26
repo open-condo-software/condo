322b827ee7fb07cc6f6098e587ee378ab5e6b6d7
Matthew <50069872+SavelevMatthew@users.noreply.github.com>
2023-09-26 17:15:00 +0500

    feat(ui): DOMA-7255 added basic input (#3891)
    * feat(ui): DOMA-7255 added basic input
    
    * fix(ui): DOMA-7255 fix disable and hover states
    
    * feat(ui): DOMA-7255 added input allowClear prop
    
    * feat(ui): DOMA-7255 added password input
    
    * feat(ui): DOMA-7255 added phone input
    
    * build(ui): DOMA-7255 fixed types of phone input onChange

---
+++ packages/ui/src/stories/Input/Input.stories.tsx
@@ -0,0 +1,18 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+import React from 'react'
+
+import { Input as Component } from '@open-condo/ui/src'
+
+export default {
+    title: 'Components/Input',
+    component: Component,
+    args: {
+        placeholder: 'Placeholder',
+        disabled: false,
+        allowClear: false,
+    },
+} as ComponentMeta<typeof Component>
+
+const Template: ComponentStory<typeof Component> = (props) => <Component {...props}/>
+
+export const Input = Template.bind({})
