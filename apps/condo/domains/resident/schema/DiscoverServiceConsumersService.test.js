84d8eb2967158a79807f06bf6109ef245dcf26f0
AleX83Xpert <AleX83Xpert@mail.ru>
2023-10-02 10:26:34 +0500

    fix(condo): DOMA-7311 fix two tests

--- apps/condo/domains/resident/schema/DiscoverServiceConsumersService.test.js
+++ apps/condo/domains/resident/schema/DiscoverServiceConsumersService.test.js
@@ -114,20 +114,19 @@ describe('DiscoverServiceConsumersService', () => {
             const [billingProperty] = await createTestBillingProperty(admin, billingIntegrationContext, { address: user.property.address })
             const [resident] = await createTestResident(admin, user.user, user.property, { address: billingProperty.address })
 
-            await createTestBillingAccount(admin, billingIntegrationContext, billingProperty, {
+            const [billingAccount] = await createTestBillingAccount(admin, billingIntegrationContext, billingProperty, {
                 unitName: resident.unitName,
                 unitType: resident.unitType,
             })
 
-            // wait for the task to finish
-            await waitFor(async () => {
-                const createdServiceConsumers = await ServiceConsumer.getAll(admin, {
-                    resident: { id: resident.id },
-                    deletedAt: null,
-                })
+            await discoverServiceConsumersByTestClient(admin, { billingAccountsIds: [billingAccount.id] })
 
-                expect(createdServiceConsumers).toHaveLength(0)
-            }, { delay: 500 })
+            const createdServiceConsumers = await ServiceConsumer.getAll(admin, {
+                resident: { id: resident.id },
+                deletedAt: null,
+            })
+
+            expect(createdServiceConsumers).toHaveLength(0)
         })
 
         test('must not discover service consumer if billingAccount.isClosed=true', async () => {
@@ -821,7 +820,7 @@ describe('DiscoverServiceConsumersService', () => {
             })
         })
 
-        test('Create only one consumer if there are duplicates of billing account', async () => {
+        test('Create only one consumer if there are duplicates (by number) of billing account', async () => {
             const user1 = await makeClientWithProperty()
             const residentClient1 = await makeClientWithResidentUser()
 
@@ -832,6 +831,7 @@ describe('DiscoverServiceConsumersService', () => {
 
             const unitType1 = FLAT_UNIT_TYPE
             const unitName1 = faker.lorem.word()
+            const unitName1a = faker.lorem.word()
             const number = faker.random.alphaNumeric(8)
             let billingAccount1
 
@@ -864,19 +864,17 @@ describe('DiscoverServiceConsumersService', () => {
                 billingAccount1 = billingAccounts[0]
             }, { delay: 500 })
 
-            // Add duplicated billing account
+            // Add duplicated (by number) billing account
             const [billingAccount1a] = await createTestBillingAccount(admin, billingIntegrationContext1, billingProperty1, {
                 unitType: unitType1,
-                unitName: unitName1,
+                unitName: unitName1a,
                 number,
             })
 
-            // Make sure we have duplicates
+            // Make sure we have duplicates by number
             await waitFor(async () => {
                 const billingAccounts = await BillingAccount.getAll(admin, {
                     context: { organization: { id: user1.organization.id } },
-                    unitType: unitType1,
-                    unitName: unitName1,
                     number,
                     deletedAt: null,
                 })
