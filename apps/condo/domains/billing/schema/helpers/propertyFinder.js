09b2748da37c81463abd02945140e4830b8697aa
AleX83Xpert <AleX83Xpert@mail.ru>
2023-10-11 08:37:24 +0500

    feat(condo): DOMA-7060 improve properties searching
    The searching was improved by detecting the size of the ordered intersection of two arrays of tokens

--- apps/condo/domains/billing/schema/helpers/propertyFinder.js
+++ apps/condo/domains/billing/schema/helpers/propertyFinder.js
@@ -1,10 +1,8 @@
-const { intersection } = require('lodash')
-
 const { loadListByChunks } = require('@condo/domains/common/utils/serverSchema')
 const { Property } = require('@condo/domains/property/utils/serverSchema')
 
 const SYMBOLS_TO_REMOVE_REGEXP = /[!@#$%^&*)(+=_:"'`[\]]/g
-const SPLITTERS_REGEXP = /[,;. -]/
+const SPLITTERS_REGEXP = /[,;. -/]/
 const IS_DIGITS_ONLY_REGEXP = /^\d+$/
 
 /**
@@ -18,6 +16,25 @@ function tokenifyAddress (addressStr) {
         .split(SPLITTERS_REGEXP)
         .filter(Boolean)
         .filter((x) => x.length > 1 || IS_DIGITS_ONLY_REGEXP.test(x))
+}
+
+/**
+ * @param {string[]} arr1
+ * @param {string[]} arr2
+ * @returns {string[]}
+ */
+function orderedIntersection (arr1, arr2) {
+    const result = []
+    const _arr2 = Array.from(arr2) // to keep arr2 untouchable
+
+    for (const str1 of arr1) {
+        if (_arr2.includes(str1)) {
+            result.push(str1)
+            _arr2.splice(0, _arr2.indexOf(str1) + 1)
+        }
+    }
+
+    return result
 }
 
 /**
@@ -41,7 +58,7 @@ async function findPropertyByOrganizationAndAddress (context, organizationId, ad
         chunkProcessor: (/** @type {Property[]} */ chunk) => {
             for (const property of chunk) {
                 const tokens = tokenifyAddress(property.address)
-                const score = 100 / targetTokens.length * intersection(targetTokens, tokens).length
+                const score = 100 / targetTokens.length * orderedIntersection(targetTokens, tokens).length
                 if (score >= maxScore) {
                     if (score === maxScore) {
                         theMostProbablyProperties.push(property)
@@ -59,4 +76,4 @@ async function findPropertyByOrganizationAndAddress (context, organizationId, ad
     return [theMostProbablyProperties, Number(maxScore.toFixed(2))]
 }
 
-module.exports = { findPropertyByOrganizationAndAddress, tokenifyAddress }
+module.exports = { findPropertyByOrganizationAndAddress, tokenifyAddress, orderedIntersection }
