92d5754889fe4cf5209a757ee59fc7caf11db00f
AleX83Xpert <AleX83Xpert@mail.ru>
2023-10-11 08:38:09 +0500

    feat(condo): DOMA-7060 Some test cases were added

--- apps/condo/domains/billing/schema/helpers/propertyFinder.spec.js
+++ apps/condo/domains/billing/schema/helpers/propertyFinder.spec.js
@@ -5,7 +5,7 @@ const { makeLoggedInAdminClient, setFakeClientMode } = require('@open-condo/keys
 const { createTestOrganization } = require('@condo/domains/organization/utils/testSchema')
 const { createTestProperty } = require('@condo/domains/property/utils/testSchema')
 
-const { findPropertyByOrganizationAndAddress, tokenifyAddress } = require('./propertyFinder')
+const { findPropertyByOrganizationAndAddress, tokenifyAddress, orderedIntersection } = require('./propertyFinder')
 
 const { keystone } = index
 
@@ -13,6 +13,7 @@ setFakeClientMode(index)
 describe('propertyFinder', () => {
     describe('tokenify', () => {
         const cases = [
+            // [<address string>, <array of tokens>]
             ['Краснодарский край, г Новороссийск, К.Маркса 13, Офис 3', ['краснодарский', 'край', 'новороссийск', 'маркса', '13', 'офис', '3']],
             ['Краснодарский "край", город Новороссийск, Карла Маркса 3, Офис 1', ['краснодарский', 'край', 'новороссийск', 'маркса', 'карла', '3', 'офис', '1', 'город']],
             ['г Екатеринбург, ул. Тепло-Мягкого; дом 3а,. кв 100;', ['екатеринбург', 'ул', 'тепло', 'мягкого', 'дом', '3а', 'кв', '100']],
@@ -26,15 +27,32 @@ describe('propertyFinder', () => {
         })
     })
 
+    describe('orderedIntersection', () => {
+        const cases = [
+            // ['string1', 'string2', ['expected', 'interception', 'tokens', 'array']]
+            ['aa bb cc', 'dd aa bb aa bb cc dd', ['aa', 'bb', 'cc']],
+            ['1 2 3 4 5 6 7', '1 2 1 2 3 1 2 3 5 1 2 3 4 5 6 2 3 7', ['1', '2', '3', '4', '5', '6', '7']],
+            ['Батюшкова, д 2 к 4', 'г Москва, поселение Сосенское, деревня Николо-Хованское, ул Батюшкова, д 2 к 4', ['батюшкова', '2', '4']],
+            ['Батюшкова, д 2/4', 'г Москва, поселение Сосенское, деревня Николо-Хованское, ул Батюшкова, д 2 к 4', ['батюшкова', '2', '4']],
+            ['Батюшкова, д 2 к 4, к. 7', 'г Москва, поселение Сосенское, деревня Николо-Хованское, ул Батюшкова, д 2 к 4', ['батюшкова', '2', '4']],
+            ['Батюшкова, д 2/4, к. 7', 'г Москва, поселение Сосенское, деревня Николо-Хованское, ул Батюшкова, д 2 к 4', ['батюшкова', '2', '4']],
+        ]
+
+        test.each(cases)('%p', (str1, str2, expectedIntersection) => {
+            expect(orderedIntersection(tokenifyAddress(str1), tokenifyAddress(str2))).toEqual(expectedIntersection)
+        })
+    })
+
     describe('find', () => {
         let adminContext
+        let admin
 
         beforeAll(async () => {
             adminContext = await keystone.createContext({ skipAccessControl: true })
+            admin = await makeLoggedInAdminClient()
         })
 
         test('find property', async () => {
-            const admin = await makeLoggedInAdminClient()
             const [organization] = await createTestOrganization(admin)
 
             const [property1] = await createTestProperty(admin, organization, { address: 'Краснодарский край, г Новороссийск, К.Маркса 13, офис 3' })
@@ -43,12 +61,31 @@ describe('propertyFinder', () => {
             await createTestProperty(admin, organization, { address: 'Краснодарский край, г Новороссийск, Карла Маркса 14' })
             await createTestProperty(admin, organization, { address: 'Новороссийск, ул К Маркса 14' })
 
-            const addressFromReceipt = 'Краснодарский край, г Новороссийск, К.Маркса 13, Офис 3'
-            const [[finded], score] = await findPropertyByOrganizationAndAddress(adminContext, organization.id, addressFromReceipt)
+            await createTestProperty(admin, organization, { address: 'г Москва, поселение Сосенское, деревня Николо-Хованское, ул Батюшкова, д 4 к 2' })
+            const [property2] = await createTestProperty(admin, organization, { address: 'г Москва, поселение Сосенское, деревня Николо-Хованское, ул Батюшкова, д 2/4' })
+            await createTestProperty(admin, organization, { address: 'г Москва, поселение Сосенское, деревня Николо-Хованское, ул Батюшкова, д 2/2' })
+            await createTestProperty(admin, organization, { address: 'г Москва, поселение Сосенское, деревня Николо-Хованское, ул Батюшкова, д 8 к 3' })
 
-            expect(finded).toBeTruthy()
-            expect(finded.id).toEqual(property1.id)
-            expect(score).toEqual(100)
+            const addressFromReceipt1 = 'Краснодарский край, г Новороссийск, Маркса 13, Офис 3'
+            const [found1, score1] = await findPropertyByOrganizationAndAddress(adminContext, organization.id, addressFromReceipt1)
+
+            const addressFromReceipt2 = 'Батюшкова, д 2 к 4'
+            const [found2, score2] = await findPropertyByOrganizationAndAddress(adminContext, organization.id, addressFromReceipt2)
+
+            const addressFromReceipt3 = 'Николо-Хованское; Батюшкова, д 2/4, к.3'
+            const [found3, score3] = await findPropertyByOrganizationAndAddress(adminContext, organization.id, addressFromReceipt3)
+
+            expect(found1).toHaveLength(1)
+            expect(found1[0].id).toEqual(property1.id)
+            expect(score1).toEqual(100)
+
+            expect(found2).toHaveLength(1)
+            expect(found2[0].id).toEqual(property2.id)
+            expect(score2).toEqual(100)
+
+            expect(found3).toHaveLength(1)
+            expect(found3[0].id).toEqual(property2.id)
+            expect(score3).toEqual(83.33)
         })
     })
 })
