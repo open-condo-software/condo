4213ad6b02d0b411ce2d9aedbed3335efe8912fd
ekabardinsky <ekabardinsky@gmail.com>
2023-10-17 16:06:56 +0500

    feat(condo): DOMA-7106 implement AddressTransform.js address parser

--- apps/condo/domains/billing/schema/helpers/AddressTransform.js
+++ apps/condo/domains/billing/schema/helpers/AddressTransform.js
@@ -1,3 +1,14 @@
+const KEYWORDS = {
+    parking: ['автоместо', 'парковка', 'паркинг', 'машиноместо', 'гараж', 'м/м', 'мм', 'место', 'м/место', 'а/м', 'бокс', 'парк'],
+    apartment: ['аппарт', 'апарт', 'ап', 'апп'],
+    commercial: ['офис', 'оф'],
+    warehouse: ['помещение', 'подвал', 'помещ', 'пом', 'кл', 'кладовка'],
+    flat: ['квартира', 'кв', 'комн', 'кварт'],
+}
+
+const HOUSE_IDENTIFIERS = 'д.|дом'
+const SPLIT_SYMBOL = '%'
+
 class AddressTransform {
 
     regExps = {}
@@ -46,6 +57,122 @@ class AddressTransform {
 
 }
 
+class AddressParser {
+
+    constructor () {
+        const keywordsRegex = this.keywordsToRegexp(Object.values(KEYWORDS).flat())
+        this.splitRegexp = new RegExp(`(.*?[${HOUSE_IDENTIFIERS}].*?)\\s(${keywordsRegex}[.\\s].*?)`, 'i')
+    }
+
+    /**
+     * @param {string[]} keyword
+     * @returns {string}
+     */
+    keywordsToRegexp (keyword = []) {
+        return keyword
+            .map(keyword => keyword.replace('/', '\\/'))
+            .sort((a, b) => b.length - a.length) // sort by length desc
+            .join('|') // to use in regexp
+    }
+
+    /**
+     * Remove trailing and starting spaces, comas, dots
+     * @param {string} input
+     * @returns {string}
+     */
+    trim (input = '') {
+        if (!input) {
+            return input
+        }
+        return input.replace(/(^[.,\s]*)|([.,\s]*$)/g, '')
+    }
+
+    /**
+     * @param {string} rawString
+     * @returns {{address: string, unitType: string, unitName: string}}
+     */
+    parse (rawString = '') {
+        const { housePart: address, unitPart, fromFias = false } = this.splitToUnitAndAddress(rawString)
+        const { unitName, unitType } = this.parseUnit(unitPart, fromFias)
+        return {
+            address,
+            unitType,
+            unitName,
+        }
+    }
+
+    /**
+     * @param {string} unitInput
+     * @param {boolean} fromFias
+     * @returns {{unitType: string, unitName: string}}
+     */
+    parseUnit (unitInput = '', fromFias = false) {
+        let detectedType = null
+        for (const unitType in KEYWORDS) {
+            const unitTypeRegex = new RegExp(this.keywordsToRegexp(KEYWORDS[unitType]) + '[.]*', 'ig')
+            if (!detectedType && unitTypeRegex.test(unitInput)) {
+                detectedType = unitType
+            }
+            if (!fromFias) {
+                unitInput = this.trim(unitInput.replace(unitTypeRegex, '').replace(/\s+/g, ' '))
+            }
+        }
+        if (fromFias) {
+            unitInput = this.trim(unitInput.replace(/\s+/g, ' '))
+        }
+        //console.log('>', fromFias, unitInput)
+        return {
+            unitName: unitInput,
+            unitType: detectedType || 'flat',
+        }
+    }
+
+    /**
+     * @param {string} input
+     * @returns {{housePart: string, unitPart: string}}
+     */
+    splitByKeyword (input = '') {
+        const [housePart, unitPart = ''] = input
+            .replace(new RegExp(`[${SPLIT_SYMBOL}]`, 'g'), '')
+            .replace(this.splitRegexp, `$1${SPLIT_SYMBOL}$2`)
+            .split(SPLIT_SYMBOL)
+        return {
+            housePart: this.trim(housePart),
+            unitPart: this.trim(unitPart),
+        }
+    }
+
+    /**
+     * @param {string} input
+     * @returns {{housePart: string, unitPart: string}}
+     */
+    splitByComma (input = '') {
+        const addressParts = input.split(',').map(part => part.trim())
+        const unit = addressParts.pop()
+        return {
+            housePart: addressParts.join(', '),
+            unitPart: unit,
+        }
+    }
+
+    /**
+     * @param {string} input
+     * @returns {{housePart: string, unitPart: string}}
+     */
+    splitToUnitAndAddress (input) {
+        if (input.indexOf('fiasId:') === 0) {
+            const [housePart, ...unitParts] = input.split(',')
+            return { housePart, unitPart: unitParts.join(','), fromFias: true }
+        }
+        let { housePart, unitPart } = this.splitByKeyword(input)
+        if (!unitPart) {
+            return this.splitByComma(input)
+        }
+        return { housePart, unitPart }
+    }
+}
+
 module.exports = {
     AddressTransform,
+    AddressParser,
 }
