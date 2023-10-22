d84a1cc338f3735ad6d56cec4ba01bb4c105e661
VKislov <wkislov0@gmail.com>
2023-10-22 18:43:37 +0500

    chore(condo): DOMA-7502 fix imports

--- apps/condo/domains/marketplace/utils/clientSchema/MarketItemPrice.ts
+++ apps/condo/domains/marketplace/utils/clientSchema/MarketItemPrice.ts
@@ -8,7 +8,9 @@ import {
     MarketItemPriceUpdateInput,
     QueryAllMarketItemPricesArgs,
 } from '@app/condo/schema'
+
 import { generateReactHooks } from '@open-condo/codegen/generate.hooks'
+
 import { MarketItemPrice as MarketItemPriceGQL } from '@condo/domains/marketplace/gql'
 
 // TODO(codegen): write utils like convertToFormState and formValuesProcessor if needed, otherwise delete this TODO
