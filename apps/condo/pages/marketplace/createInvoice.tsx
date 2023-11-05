ca8d153e3396ba151badb26b076a9495e1591c28
Daria Abushenko <2009add@gmail.com>
2023-11-05 10:33:12 +0300

    feat(condo): DOMA-7577 wip create invoice form

---
+++ apps/condo/pages/marketplace/createInvoice.tsx
@@ -0,0 +1,44 @@
+import { Typography, Row, Col } from 'antd'
+import { Gutter } from 'antd/es/grid/row'
+import Head from 'next/head'
+import React from 'react'
+
+import { useIntl } from '@open-condo/next/intl'
+
+import { CreateInvoiceForm } from '../../domains/marketplace/components/createInvoiceForm'
+import { PageContent, PageWrapper } from '@condo/domains/common/components/containers/BaseLayout'
+
+
+interface ICreateMeterPage extends React.FC {
+    headerAction?: JSX.Element
+    requiredAccess?: React.FC
+}
+
+const CREATE_METER_PAGE_GUTTER: [Gutter, Gutter] = [12, 40]
+
+const CreateMeterPage: ICreateMeterPage = () => {
+    const intl = useIntl()
+    const PageTitle = intl.formatMessage({ id: 'pages.condo.marketplace.createBill.title' })
+
+    return (
+        <>
+            <Head>
+                <title>{PageTitle}</title>
+            </Head>
+            <PageWrapper>
+                <PageContent>
+                    <Row gutter={CREATE_METER_PAGE_GUTTER}>
+                        <Col span={24}>
+                            <Typography.Title level={1}>{PageTitle}</Typography.Title>
+                        </Col>
+                        <Col span={24}>
+                            <CreateInvoiceForm />
+                        </Col>
+                    </Row>
+                </PageContent>
+            </PageWrapper>
+        </>
+    )
+}
+
+export default CreateMeterPage
