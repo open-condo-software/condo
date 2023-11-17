dc4feb020d5147cc6bf4236cf52f3d414094ea60
Mikhail Gorbunov <toplenboren@gmail.com>
2023-11-17 18:02:11 +0500

    refactor(condo): DOMA-7397 remove real life tests

--- apps/condo/domains/news/tasks/index.js
+++ apps/condo/domains/news/tasks/index.js
@@ -1,5 +1,11 @@
 const notifyResidentsAboutDelayedNewsItem = require('./notifyResidentsAboutDelayedNewsItems')
 const notifyResidentsAboutNewsItem = require('./notifyResidentsAboutNewsItem')
 const publishSharedNewsItem = require('./publishSharedNewsItem')
+const publishSharedNewsItemsByNewsItem = require('./publishSharedNewsItemsByNewsItem')
 
-module.exports = { notifyResidentsAboutNewsItem, notifyResidentsAboutDelayedNewsItem, publishSharedNewsItem }
+module.exports = {
+    notifyResidentsAboutNewsItem,
+    notifyResidentsAboutDelayedNewsItem,
+    publishSharedNewsItem,
+    publishSharedNewsItemsByNewsItem,
+}
