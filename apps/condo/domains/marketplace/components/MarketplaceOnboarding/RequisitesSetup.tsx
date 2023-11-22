5cf6b2e6f42b54c7f1faf46856fa421feea782d5
Alexander Naydenov <AleX83Xpert@mail.ru>
2023-11-22 08:41:07 +0500

    fix(condo): DOMA-7719 ability to enter data manually (#4107)
    * fix(condo): DOMA-7719 fix random test
    
    * fix(condo): DOMA-7719 ability to enter data manually
    
    * feat(condo): DOMA-7719 remove logs

--- apps/condo/domains/marketplace/components/MarketplaceOnboarding/RequisitesSetup.tsx
+++ apps/condo/domains/marketplace/components/MarketplaceOnboarding/RequisitesSetup.tsx
@@ -1,16 +1,18 @@
-import { Col, Form, Input, Row, RowProps } from 'antd'
+import { BankAccount } from '@app/condo/schema'
+import { AutoComplete, Col, Form, Input, Row, RowProps } from 'antd'
 import get from 'lodash/get'
 import { useRouter } from 'next/router'
-import React, { useCallback, useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useMemo, useState } from 'react'
 
 import { useIntl } from '@open-condo/next/intl'
 import { useOrganization } from '@open-condo/next/organization'
 import { Alert, Button, Radio, RadioGroup, Select, SelectProps, Space } from '@open-condo/ui'
 
 import { AcquiringIntegration } from '@condo/domains/acquiring/utils/clientSchema'
-import { BankAccount } from '@condo/domains/banking/utils/clientSchema'
+import { BankAccount as BankAccountApi } from '@condo/domains/banking/utils/clientSchema'
 import { RUSSIA_COUNTRY } from '@condo/domains/common/constants/countries'
 import { useMutationErrorHandler } from '@condo/domains/common/hooks/useMutationErrorHandler'
+import { useValidations } from '@condo/domains/common/hooks/useValidations'
 import { useBankAccountValidation } from '@condo/domains/common/utils/clientSchema/bankAccountValidationUtils'
 import {
     ERROR_BANK_NOT_FOUND,
@@ -26,24 +28,21 @@ const FORM_VALIDATE_TRIGGER = ['onBlur', 'onSubmit']
 const VERTICAL_GUTTER: RowProps['gutter'] = [0, 40]
 const LABEL_COL = { lg: 10 }
 
-const TAX_PERCENT_OPTIONS: SelectProps['options'] = VAT_OPTIONS.map((option) => ({
+const TAX_PERCENT_OPTIONS: SelectProps['options'] = VAT_OPTIONS.map((option: number) => ({
     label: `${option} %`,
-    key: option,
+    key: `vat_percent_${option}`,
     value: option,
 }))
 
-const getOptions = (items, fieldName) => (items.map((item) => {
+const getOptions = (items: BankAccount[], fieldName: string): SelectProps['options'] => (items.map((item) => {
     const field = get(item, fieldName, null)
-    return {
-        label: field,
-        key: field,
-        value: field,
-    }
+    return { label: field, value: field }
 }))
 
 export const RequisitesSetup: React.FC = () => {
     const intl = useIntl()
     const { numberValidator, routingNumberValidator } = useBankAccountValidation({ country: RUSSIA_COUNTRY })
+    const { requiredValidator } = useValidations()
 
     const AccountLabel = intl.formatMessage({ id: 'pages.condo.marketplace.settings.requisites.field.account' })
     const TINLabel = intl.formatMessage({ id: 'pages.condo.marketplace.settings.requisites.field.tin' })
@@ -59,7 +58,6 @@ export const RequisitesSetup: React.FC = () => {
     const [error, setError] = useState<string | null>(null)
     const [loading, setIsLoading] = useState<boolean>(false)
 
-    const [selectedTaxType, setSelectedTaxType] = useState<typeof TAX_REGIME_GENEGAL | typeof TAX_REGIME_SIMPLE>(TAX_REGIME_GENEGAL)
     const { organization } = useOrganization()
     const orgId = get(organization, 'id', null)
     const [form] = Form.useForm()
@@ -75,7 +73,7 @@ export const RequisitesSetup: React.FC = () => {
         },
     })
 
-    const { objs: bankAccounts, loading: bankAccountsLoading, error: bankAccountsError } = BankAccount.useObjects({
+    const { objs: bankAccounts } = BankAccountApi.useObjects({
         where: {
             organization: { id: orgId },
         },
@@ -88,6 +86,15 @@ export const RequisitesSetup: React.FC = () => {
         },
     })
 
+    const [submittable, setSubmittable] = React.useState(false)
+    const values = Form.useWatch([], form)
+    useEffect(() => {
+        form.validateFields({ validateOnly: true }).then(
+            () => setSubmittable(true),
+            () => setSubmittable(false),
+        )
+    }, [form, values])
+
     const acquiringId = get(acquiring, ['0', 'id'], null)
 
     const createAction = InvoiceContext.useCreate({
@@ -95,26 +102,21 @@ export const RequisitesSetup: React.FC = () => {
         settings: { dv: 1 },
     })
 
-    const handleTypeChange = useCallback((form) => (e) => {
-        setSelectedTaxType(e.target.value)
-    }, [])
-
     const [noTaxOption]: SelectProps['options'] = [{ label: NoTax, key: NoTax, value: null }]
 
-    const bankAccountOptions: SelectProps['options'] = getOptions(bankAccounts, 'number')
-    const bicOptions: SelectProps['options'] = getOptions(bankAccounts, 'routingNumber')
+    const account = Form.useWatch('account', form)
+    const bic = Form.useWatch('bic', form)
+    const selectedTaxType = Form.useWatch<typeof TAX_REGIME_GENEGAL | typeof TAX_REGIME_SIMPLE>('taxType', form)
+
+    const bankAccountOptions = useMemo<SelectProps['options']>(() => getOptions(bic ? bankAccounts.filter(({ routingNumber }) => routingNumber === bic) : bankAccounts, 'number'), [bankAccounts, bic])
+    const bicOptions = useMemo<SelectProps['options']>(() => getOptions(account ? bankAccounts.filter(({ number }) => number === account) : bankAccounts, 'routingNumber'), [bankAccounts, account])
+    const taxPercentOptions = useMemo<SelectProps['options']>(() => selectedTaxType === TAX_REGIME_GENEGAL ? [noTaxOption, ...TAX_PERCENT_OPTIONS] : [noTaxOption, ...TAX_PERCENT_OPTIONS.slice(1)], [noTaxOption, selectedTaxType])
+
     const invoiceContextId = get(invoiceContext, 'id', null)
 
     useEffect(() => {
         form.resetFields(['taxPercent'])
     }, [form, selectedTaxType])
-
-    useEffect(() => {
-        if (!bankAccountsLoading && !bankAccountsError && bankAccounts) {
-            form.setFieldValue('bic', get(bankAccounts[0], 'routingNumber', null))
-            form.setFieldValue('account', get(bankAccounts[0], 'number', null))
-        }
-    }, [bankAccounts, bankAccountsError, bankAccountsLoading, form])
 
     // NOTE: If invoice context is already connected with status inProgress = skip to final step
     useEffect(() => {
@@ -160,24 +162,13 @@ export const RequisitesSetup: React.FC = () => {
             {error && (
                 <Col sm={13} span={24}>
                     <Row>
-                        <Alert
-                            showIcon
-                            type='error'
-                            message={RecipientErrorTitle}
-                            description={error}
-                        />
+                        <Alert showIcon type='error' message={RecipientErrorTitle} description={error}/>
                     </Row>
                 </Col>
             )}
             <Col sm={13} span={24}>
                 <Form
-                    initialValues={{
-                        tin: '',
-                        bic: '',
-                        account: '',
-                        taxType: selectedTaxType,
-                        taxPercent: selectedTaxType === TAX_REGIME_GENEGAL ? VAT_OPTIONS[VAT_OPTIONS.length - 1] : null,
-                    }}
+                    initialValues={{ tin: get(organization, 'tin'), bic: '', account: '' }}
                     form={form}
                     onFinish={handleFormSubmit}
                     layout='horizontal'
@@ -194,11 +185,7 @@ export const RequisitesSetup: React.FC = () => {
                                 labelAlign='left'
                                 rules={numberValidator}
                             >
-                                {!bankAccountsLoading && !bankAccountsError && bankAccounts.length > 1 ? (
-                                    <Select
-                                        options={bankAccountOptions}
-                                    />
-                                ) : <Input/>}
+                                <AutoComplete allowClear filterOption options={bankAccountOptions}/>
                             </Form.Item>
                         </Col>
                         <Col span={24}>
@@ -208,10 +195,9 @@ export const RequisitesSetup: React.FC = () => {
                                 required
                                 labelCol={LABEL_COL}
                                 labelAlign='left'
+                                rules={[requiredValidator]}
                             >
-                                <Col lg={12}>
-                                    <Input disabled value={get(organization, 'tin')}/>
-                                </Col>
+                                <Input disabled/>
                             </Form.Item>
                         </Col>
                         <Col span={24}>
@@ -223,14 +209,7 @@ export const RequisitesSetup: React.FC = () => {
                                 labelAlign='left'
                                 rules={routingNumberValidator}
                             >
-                                <Col lg={12}>
-                                    {!bankAccountsLoading && !bankAccountsError && bankAccounts.length > 1 ? (
-                                        <Select
-                                            placeholder={bicOptions[0].label}
-                                            options={bicOptions}
-                                        />
-                                    ) : <Input/>}
-                                </Col>
+                                <AutoComplete allowClear filterOption options={bicOptions}/>
                             </Form.Item>
                         </Col>
                         <Col span={24}>
@@ -240,15 +219,12 @@ export const RequisitesSetup: React.FC = () => {
                                 required
                                 labelCol={LABEL_COL}
                                 labelAlign='left'
+                                rules={[requiredValidator]}
                             >
-                                <RadioGroup onChange={handleTypeChange(form)}>
+                                <RadioGroup>
                                     <Space size={8} wrap direction='vertical'>
-                                        <Radio value={TAX_REGIME_GENEGAL}>
-                                            {TaxTypeCommonLabel}
-                                        </Radio>
-                                        <Radio value={TAX_REGIME_SIMPLE}>
-                                            {TaxTypeSimpleLabel}
-                                        </Radio>
+                                        <Radio value={TAX_REGIME_GENEGAL}>{TaxTypeCommonLabel}</Radio>
+                                        <Radio value={TAX_REGIME_SIMPLE}>{TaxTypeSimpleLabel}</Radio>
                                     </Space>
                                 </RadioGroup>
                             </Form.Item>
@@ -260,17 +236,16 @@ export const RequisitesSetup: React.FC = () => {
                                 required
                                 labelCol={LABEL_COL}
                                 labelAlign='left'
+                                rules={[requiredValidator]}
                             >
-                                <Select
-                                    placeholder={selectedTaxType === TAX_REGIME_GENEGAL ? TAX_PERCENT_OPTIONS[TAX_PERCENT_OPTIONS.length - 1].label : noTaxOption.label}
-                                    options={selectedTaxType === TAX_REGIME_GENEGAL ? [noTaxOption, ...TAX_PERCENT_OPTIONS] : [noTaxOption, ...TAX_PERCENT_OPTIONS.slice(1)]}
-                                />
+                                <Select disabled={!selectedTaxType} options={taxPercentOptions}/>
                             </Form.Item>
                         </Col>
                         <Col span={24}>
                             <Row justify='start'>
                                 <Space size={16}>
-                                    <Button type='primary' key='submit' htmlType='submit' loading={loading}>
+                                    <Button type='primary' key='submit' htmlType='submit' loading={loading}
+                                        disabled={!submittable}>
                                         {NextButtonLabel}
                                     </Button>
                                 </Space>
