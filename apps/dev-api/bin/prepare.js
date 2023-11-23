b296c7d408c57ef393e14b33a0360de1f2207e6e
SavelevMatthew <savelevmatthew@gmail.com>
2023-11-23 11:31:36 +0500

    feat(dev-api): DOMA-7698 prepare.js to create bots

--- apps/dev-api/bin/prepare.js
+++ apps/dev-api/bin/prepare.js
@@ -1,11 +1,60 @@
 const path = require('path')
 
-const { prepareAppEnvLocalAdminUsers } = require('@open-condo/cli')
+const { faker } = require('@faker-js/faker')
+
+const { prepareAppEnvLocalAdminUsers, getAppEnvValue, updateAppEnvFile, safeExec } = require('@open-condo/cli')
 
 const APP_NAME = path.basename(path.resolve(__dirname, '..'))
+const BOT_RIGHTS_SET = {
+    name: '[DEV-API] Service bot permissions',
+    canReadB2BApps: true,
+    canReadB2BAppAccessRights: true,
+    canReadB2BAppAccessRightSets: true,
+    canReadB2BAppPermissions: true,
+    canReadB2BAppNewsSharingConfigs: true,
+    canReadB2CApps: true,
+    canReadB2CAppAccessRights: true,
+    canReadB2CAppBuilds: true,
+    canReadB2CAppProperties: true,
+
+    canManageB2BApps: true,
+    canManageB2BAppAccessRights: true,
+    canManageB2BAppAccessRightSets: true,
+    canManageB2BAppPermissions: true,
+    canManageB2BAppNewsSharingConfigs: true,
+    canManageB2CApps: true,
+    canManageB2CAppAccessRights: true,
+    canManageB2CAppBuilds: true,
+    canManageB2CAppProperties: true,
+
+    canExecuteRegisterNewServiceUser: true,
+    canExecuteSendMessage: true,
+}
 
 async function main () {
     await prepareAppEnvLocalAdminUsers(APP_NAME, 'phone')
+
+    const condoUrl = await getAppEnvValue(APP_NAME, 'CONDO_DOMAIN')
+    const devBotEnvValue = await getAppEnvValue(APP_NAME, 'CONDO_DEV_BOT_CONFIG')
+    const devBotConfig = devBotEnvValue ? JSON.parse(devBotEnvValue) : {
+        email: 'dev-bot@dev.api',
+        password: faker.internet.password(16),
+    }
+    devBotConfig.apiUrl = condoUrl
+    await updateAppEnvFile(APP_NAME, 'CONDO_DEV_BOT_CONFIG', JSON.stringify(devBotConfig))
+
+    const prodBotEnvValue = await getAppEnvValue(APP_NAME, 'CONDO_PROD_BOT_CONFIG')
+    const prodBotConfig = prodBotEnvValue ? JSON.parse(prodBotEnvValue) : {
+        email: 'prod-bot@dev.api',
+        password: faker.internet.password(16),
+    }
+    prodBotConfig.apiUrl = condoUrl
+    await updateAppEnvFile(APP_NAME, 'CONDO_PROD_BOT_CONFIG', JSON.stringify(prodBotConfig))
+
+    const devBotOptions = { type: 'service', password: devBotConfig.password, name: '[DEV-API] Dev bot' }
+    const prodBotOptions = { type: 'service', password: prodBotConfig.password, name: '[DEV-API] Prod bot' }
+    await safeExec(`yarn workspace @app/condo node bin/create-user.js ${devBotConfig.email} '${JSON.stringify(devBotOptions)}' '${JSON.stringify(BOT_RIGHTS_SET)}'`)
+    await safeExec(`yarn workspace @app/condo node bin/create-user.js ${prodBotConfig.email} '${JSON.stringify(prodBotOptions)}' '${JSON.stringify(BOT_RIGHTS_SET)}'`)
 }
 
 main().then(() => {
