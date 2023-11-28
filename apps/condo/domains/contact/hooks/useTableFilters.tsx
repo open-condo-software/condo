b38e9c0403f2fa9f1b060afd6145bafe85431a86
Alllex202 <56914444+Alllex202@users.noreply.github.com>
2023-11-28 11:59:43 +0500

    feat(condo): DOMA-7680 added filter by units (type + name) (#4125)
    * feat(condo): DOMA-7680 added filter by units (type + name)
    
    * fix(condo): DOMA-7681 fixed translates
    
    * fix(condo): DOMA-7680 fixed default filter value

--- apps/condo/domains/contact/hooks/useTableFilters.tsx
+++ apps/condo/domains/contact/hooks/useTableFilters.tsx
@@ -5,13 +5,12 @@ import { useIntl } from '@open-condo/next/intl'
 
 import { ComponentType, FiltersMeta } from '@condo/domains/common/utils/filters.utils'
 import { getFilter, getStringContainsFilter } from '@condo/domains/common/utils/tables.utils'
-
+import { getUnitFilter } from '@condo/domains/property/utils/tables.utils'
 
 const filterName = getStringContainsFilter('name')
 const filterPhone = getStringContainsFilter('phone')
 const filterEmail = getStringContainsFilter('email')
 const filterAddress = getStringContainsFilter(['property', 'address'])
-const filterUnit = getFilter('unitName', 'array', 'string', 'in')
 const filterRole = getFilter(['role', 'id'], 'array', 'string', 'in')
 
 type UseContactsTableFilters = () => Array<FiltersMeta<ContactWhereInput>>
@@ -23,6 +22,8 @@ export const useContactsTableFilters: UseContactsTableFilters = () => {
     const PhoneMessage = intl.formatMessage({ id: 'Phone' })
     const EmailMessage = intl.formatMessage({ id: 'field.EMail' })
     const EnterUnitNameLabel = intl.formatMessage({ id: 'pages.condo.ticket.filters.EnterUnitName' })
+
+    const filterUnit = useMemo(() => getUnitFilter(intl.messages), [intl])
 
     return useMemo(() => {
         return [
@@ -91,5 +92,5 @@ export const useContactsTableFilters: UseContactsTableFilters = () => {
                 filters: [filterRole],
             },
         ]
-    }, [AddressMessage, EmailMessage, EnterUnitNameLabel, PhoneMessage, UserNameMessage])
+    }, [AddressMessage, EmailMessage, EnterUnitNameLabel, PhoneMessage, UserNameMessage, filterUnit])
 }
