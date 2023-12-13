2dc9b861467d37febd2db96b61d6b9aeb3aeb6b6
Alllex202 <alllex.202@gmail.com>
2023-12-13 17:15:59 +0500

    feat(condo): DOMA-7983 added userParams to YMetrika

--- apps/condo/domains/common/components/containers/YandexMetrika.tsx
+++ apps/condo/domains/common/components/containers/YandexMetrika.tsx
@@ -1,13 +1,36 @@
+import get from 'lodash/get'
+import isNil from 'lodash/isNil'
+import omitBy from 'lodash/omitBy'
 import getConfig from 'next/config'
 import { useRouter } from 'next/router'
 import { useEffect } from 'react'
 import ym, { YMInitializer } from 'react-yandex-metrika'
 
+import { useAuth } from '@open-condo/next/auth'
+import { useOrganization } from '@open-condo/next/organization'
+
+
 const YandexMetrika = () => {
     const { publicRuntimeConfig } = getConfig()
     const { yandexMetrikaID } = publicRuntimeConfig
 
+    const { user } = useAuth()
+    const { organization, link } = useOrganization()
+    const userId = get(user, 'id')
+    const organizationId = get(organization, 'id')
+    const role = get(link, 'role')
+
     const router = useRouter()
+
+    useEffect(() => {
+        const userParams = omitBy({
+            UserID: userId,
+            organizationId,
+            roleName: get(role, 'name'),
+            roleNameNonLocalized: get(role, 'nameNonLocalized'),
+        }, isNil)
+        ym('userParams', userParams)
+    }, [organizationId, role, userId])
 
     const routeChangeComplete = () => {
         ym('hit', window.location.pathname)
