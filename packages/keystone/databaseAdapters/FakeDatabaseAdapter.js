77e3fa392a2838868e8f75cd7482439de56944fc
Pahaz White <pahaz.white@gmail.com>
2024-01-18 15:04:20 +0500

    refactor(keystone): DOMA-7367 add FakeListAdapter

--- packages/keystone/databaseAdapters/FakeDatabaseAdapter.js
+++ packages/keystone/databaseAdapters/FakeDatabaseAdapter.js
@@ -1,13 +1,13 @@
-const { BaseKeystoneAdapter } = require('@keystonejs/keystone')
+const { BaseKeystoneAdapter, BaseListAdapter } = require('@keystonejs/keystone')
 
 class FakeDatabaseAdapter extends BaseKeystoneAdapter {
     constructor (opts = {}) {
         super(...arguments)
-        this.name = 'fake'
+        this.name = 'knex'
     }
 
     newListAdapter (key, adapterConfig) {
-        throw new Error('FakeDatabaseAdapter: newListAdapter() call')
+        return new FakeListAdapter(key, this, adapterConfig)
     }
 
     getListAdapterByKey (key) {
@@ -32,6 +32,44 @@ class FakeDatabaseAdapter extends BaseKeystoneAdapter {
 
     async checkDatabaseVersion () {
         throw new Error('FakeDatabaseAdapter: checkDatabaseVersion() call')
+    }
+}
+
+class FakeListAdapter extends BaseListAdapter {
+    async create (data) {
+        throw new Error('FakeListAdapter: create() call')
+    }
+
+    async delete (id) {
+        throw new Error('FakeListAdapter: delete() call')
+    }
+
+    async update (id, data) {
+        throw new Error('FakeListAdapter: update() call')
+    }
+
+    async findAll () {
+        throw new Error('FakeListAdapter: findAll() call')
+    }
+
+    async findById (id) {
+        throw new Error('FakeListAdapter: findById() call')
+    }
+
+    async find (condition) {
+        throw new Error('FakeListAdapter: find() call')
+    }
+
+    async findOne (condition) {
+        throw new Error('FakeListAdapter: findOne() call')
+    }
+
+    async itemsQuery (args, { meta = false, from = {} } = {}) {
+        throw new Error('FakeListAdapter: itemsQuery() call')
+    }
+
+    itemsQueryMeta (args) {
+        throw new Error('FakeListAdapter: itemsQueryMeta() call')
     }
 }
 
