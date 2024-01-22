ea22c5c9b510db8da8aa0edbbfcf68fdfe06cff0
Alllex202 <alllex.202@gmail.com>
2024-01-23 00:09:55 +0500

    feat(condo): DOMA-8271 connected addOrganizationFieldPlugin for NewsItemScope

--- apps/condo/domains/news/schema/NewsItemScope.js
+++ apps/condo/domains/news/schema/NewsItemScope.js
@@ -16,7 +16,9 @@ const {
     NEWS_ITEM_SCOPE_TYPE_PROPERTY_UNIT_TYPE,
     NEWS_ITEM_SCOPE_TYPE_PROPERTY_UNIT_TYPE_UNIT_NAME,
 } = require('@condo/domains/news/constants/scopesTypes')
+const { addOrganizationFieldPlugin } = require('@condo/domains/organization/schema/plugins/addOrganizationFieldPlugin')
 const { UNIT_TYPES } = require('@condo/domains/property/constants/common')
+
 
 const ERRORS = {
     EDIT_DENIED_PUBLISHED: {
@@ -128,7 +130,10 @@ const NewsItemScope = new GQLListSchema('NewsItemScope', {
             }
         },
     },
-    plugins: [uuided(), versioned(), tracked(), softDeleted(), dvAndSender(), historical()],
+    plugins: [
+        uuided(), versioned(), tracked(), softDeleted(), dvAndSender(), historical(),
+        addOrganizationFieldPlugin({ fromField: 'newsItem', isRequired: true }),
+    ],
     access: {
         read: access.canReadNewsItemScopes,
         create: access.canManageNewsItemScopes,
