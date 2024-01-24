862d6f92d1955176c85f010746efa7221bc43e8b
SavelevMatthew <savelevmatthew@gmail.com>
2024-01-24 14:19:22 +0500

    feat(ui): INFRA-194 Added visibilityToggle prop to password input

--- packages/ui/src/components/Input/password.tsx
+++ packages/ui/src/components/Input/password.tsx
@@ -6,9 +6,10 @@ import React from 'react'
 import { INPUT_CLASS_PREFIX } from './input'
 
 import type { BaseInputProps } from './input'
+import type { PasswordProps } from 'antd/lib/input'
 import type { InputRef } from 'antd/lib/input'
 
-export type PasswordInputProps = BaseInputProps
+export type PasswordInputProps = BaseInputProps & Pick<PasswordProps, 'visibilityToggle'>
 
 const Password: React.ForwardRefExoticComponent<PasswordInputProps & React.RefAttributes<InputRef>> = React.forwardRef((props, ref) => {
     return <DefaultInput.Password {...props} ref={ref} inputPrefixCls={INPUT_CLASS_PREFIX}/>
