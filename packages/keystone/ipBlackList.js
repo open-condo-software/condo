529bd0a7576c8c6b6604db17caef5da6f3a55f3e
ekabardinsky <ekabardinsky@gmail.com>
2024-02-01 12:32:20 +0500

    feat(condo): NO-JIRA ip black list support

--- packages/keystone/ipBlackList.js
+++ packages/keystone/ipBlackList.js
@@ -4,14 +4,38 @@ const { isEmpty } = require('lodash')
 const conf = require('@open-condo/config')
 const { getIp } = require('@open-condo/keystone/ip.utils')
 
+function buildIpDictionary (ips) {
+    const root = {}
+    ips.forEach(ip => {
+        const octets = ip.split('.')
+        let currentLevel = root
+        octets.forEach(octet => {
+            if (!currentLevel[octet]) {
+                currentLevel[octet] = {}
+            }
+            currentLevel = currentLevel[octet]
+        })
+    })
+    return root
+}
+
+function checkIpInDictionary (dictionary, ip) {
+    const octets = ip.split('.')
+    let currentLevel = dictionary
+    for (let i = 0; i < octets.length; i++) {
+        const octet = octets[i]
+        if (!currentLevel[octet]) {
+            return false
+        }
+        currentLevel = currentLevel[octet]
+    }
+    return true
+}
+
 const ipBlackList = (conf.IP_BLACK_LIST || '').split(',')
     .map(ip => ip.trim())
     .filter(ip => !isEmpty(ip))
-    .reduce((map, ip) => {
-        map[ip] = true
-        return map
-    }, {})
-
+const ipDictionary = buildIpDictionary(ipBlackList)
 
 class IpBlackListMiddleware {
     async prepareMiddleware ({ keystone }) {
@@ -19,7 +43,7 @@ class IpBlackListMiddleware {
         const app = express()
         app.use((req, res, next) => {
             const ip = getIp(req)
-            if (ipBlackList[ip]) {
+            if (checkIpInDictionary(ipDictionary, ip)) {
                 res.status(410).send('Too Many Requests')
             } else {
                 next()
