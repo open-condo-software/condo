d6cd0a6faf58fab5314aec402e910cf70be04ee6
abshnko <2009add@gmail.com>
2024-02-08 18:41:28 +0300

    feat(condo): DOMA-8371 add validations for routingNumber and classificationCode

--- apps/condo/domains/banking/utils/validate/routingNumber.utils.spec.js
+++ apps/condo/domains/banking/utils/validate/routingNumber.utils.spec.js
@@ -9,7 +9,7 @@ describe('validateRoutingNumber', () => {
     describe('Ru', () => {
 
         const COUNTRY_CODE_RU = 'ru'
-        const VALID_RU_ROUTING_NUMBER = ['045809749', '042612466', '043194972']
+        const VALID_RU_ROUTING_NUMBER = ['045809749', '042612466', '043194972', '011806101']
         const WRONG_LENGTH_RU_ROUTING_NUMBER = '0484528544'
         const WRONG_FORMAT_RU_ROUTING_NUMBER = '04845B854'
         const WRONG_CODE_COUNTRY_RU_ROUTING_NUMBER = '588453854'
