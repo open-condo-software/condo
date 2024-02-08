c34d8d0f909f4da3fd96e8692963990c3243c452
Sitnikov Ivan <sit7602@gmail.com>
2024-02-08 16:13:11 +0500

    test(condo): INFRA-210 rid of awaiting of aliases at cypress tests bc there is no reason no more

--- apps/condo/cypress/objects/Ticket.ts
+++ apps/condo/cypress/objects/Ticket.ts
@@ -25,7 +25,6 @@ class TicketCreate {
 
     clickAndInputAddress (address: string): this {
         cy.get('[data-cy=ticket__property-address-search-input] input').should('be.visible')
-        cy.wait('@getAllProperties')
 
         cy.get('[data-cy=ticket__property-address-search-input] input')
             .click({ force: true })
@@ -37,7 +36,7 @@ class TicketCreate {
     chooseAddressForTicket (): this {
         cy.get('[data-cy=ticket__property-address-search-option').should('be.visible')
         cy.get('[data-cy=ticket__property-address-search-input] input').click().type('{downArrow}').type('{enter}')
-        cy.wait('@getAllProperties')
+
         return this
     }
 
@@ -79,7 +78,6 @@ class TicketCreate {
     clickOnSubmitButton (): this {
         cy.get('[data-cy=ticket__submit-button]')
             .click()
-        cy.wait('@createTicket')
 
         cy.location('pathname', { timeout: 10000 }).should('contain', TICKET_VIEW_URL)
         return this
@@ -100,23 +98,18 @@ class TicketView {
     visit (): this {
         cy.visit(TICKET_VIEW_URL)
         cy.location('pathname').should('equal', TICKET_VIEW_URL)
-        cy.wait('@getAllOrganizationEmployees')
-        cy.wait('@getAllTicketStatuses')
-        cy.wait('@getAllUserTicketCommentReadTimes')
-        cy.wait('@getAllPropertyScopeOrganizationEmployees')
-        cy.wait('@getAllTickets')
+
         return this
     }
 
     clickIsWarrantyCheckbox (): this {
         cy.get('[data-cy=ticket__filter-isWarranty]').should('not.be.disabled').click()
         cy.location('search').should('contain', 'isWarranty')
-        cy.wait('@getAllTickets')
+
         cy.get('[data-cy=ticket__table] tbody tr').should('have.length.greaterThan', 0)
 
         cy.get('[data-cy=ticket__filter-isWarranty]').click()
         cy.location('search').should('not.contain', 'isWarranty')
-        cy.wait('@getAllTickets')
         cy.get('[data-cy=ticket__table] tbody tr').should('have.length.greaterThan', 3)
 
         return this
@@ -125,12 +118,11 @@ class TicketView {
     clickIsPayableCheckbox (): this {
         cy.get('[data-cy=ticket__filter-isPayable]').click()
         cy.location('search').should('contain', 'isPayable')
-        cy.wait('@getAllTickets')
+
         cy.get('[data-cy=ticket__table] tbody tr').should('have.length.greaterThan', 0)
 
         cy.get('[data-cy=ticket__filter-isPayable]').click()
         cy.location('search').should('not.contain', 'isPayable')
-        cy.wait('@getAllTickets')
         cy.get('[data-cy=ticket__table] tbody tr').should('have.length.greaterThan', 3)
 
         return this
@@ -139,12 +131,11 @@ class TicketView {
     clickIsEmergencyCheckbox (): this {
         cy.get('[data-cy=ticket__filter-isEmergency]').click()
         cy.location('search').should('contain', 'isEmergency')
-        cy.wait('@getAllTickets')
+
         cy.get('[data-cy=ticket__table] tbody tr').should('have.length.greaterThan', 0)
 
         cy.get('[data-cy=ticket__filter-isEmergency]').click()
         cy.location('search').should('not.contain', 'isEmergency')
-        cy.wait('@getAllTickets')
         cy.get('[data-cy=ticket__table] tbody tr').should('have.length.greaterThan', 3)
 
         return this
@@ -152,29 +143,25 @@ class TicketView {
 
     clickOnGlobalFiltersButton (): this {
         cy.get('[data-cy=ticket__filters-button]').click()
-        cy.wait('@getAllTicketFilterTemplates')
 
         return this
     }
 
     typeAddressSearchInput (propertyAddress: string): this {
-        cy.wait(['@selectProperty', '@getAllTicketClassifiers', '@selectOrganizationEmployee', '@getAllOrganizationEmployees'])
         cy.get('input#property')
             .click()
             .type(propertyAddress.slice(0, 5))
-        cy.wait('@selectProperty')
 
         cy.get('[data-cy=search-input--option]').should('be.visible')
         cy.get('[data-cy=search-input--option]')
             .click()
         cy.get('[data-cy=common__filters-button-submit]').click()
 
-        cy.wait('@getAllTickets')
         cy.location('search').should('contain', 'property')
         cy.get('[data-cy=ticket__table] tbody tr').should('have.length.greaterThan', 3)
 
         cy.get('[data-cy=common__filters-button-reset]').filter(':visible').click()
-        cy.wait('@getAllTickets')
+
         cy.location('search').should('not.contain', 'property')
         cy.location('search').should('be.empty')
         cy.get('[data-cy=ticket__table] tbody tr').should('have.length.greaterThan', 3)
@@ -196,34 +183,11 @@ class TicketEdit {
  */
     visit (ticket): this {
         cy.visit(`${TICKET_VIEW_URL}/${ticket.id}`)
-        cy.wait([
-            '@getAllTickets',
-            '@getAllTicketFiles',
-            '@getAllOrganizationEmployees',
-            '@getAllTicketStatuses',
-            '@getAllTicketComments',
-            '@getAllTicketChanges',
-            '@getAllTicketPropertyHints',
-            '@getAllUserTicketCommentReadTimes',
-            '@getAllOrganizationEmployeeSpecializations',
-            '@getAllPropertyScopeProperties',
-            '@getAllPropertyScopes',
-            '@getAllPropertyScopeOrganizationEmployees',
-            '@getAllTicketPropertyHintProperties',
-            '@getAllIncidentProperties',
-            '@getAllIncidents',
-        ])
 
         return this
     }
 
     changeTicketStatus (): this {
-        cy.wait('@getAllTickets')
-        cy.wait('@getAllTicketClassifiers')
-        cy.wait('@getAllTicketFiles')
-        cy.wait('@getAllTicketStatuses')
-        cy.wait('@getAllOrganizationEmployees')
-
         cy.get('[data-cy=ticket__status-select]')
             .click()
         cy.get('[data-cy=ticket__status-select]').should('have.class', 'ant-select-open')
@@ -235,12 +199,6 @@ class TicketEdit {
 
     clickUpdateTicketLink (): this {
         cy.get('[data-cy=ticket__update-link]').click()
-
-        cy.wait([
-            '@getAllTickets',
-            '@getAllTicketClassifiers',
-            '@getAllProperties',
-        ])
 
         cy.location('pathname').should('contain', '/update')
 
@@ -301,7 +259,6 @@ Elements:
     visitTicketsPage (): this {
         cy.visit(TICKET_VIEW_URL)
         cy.location('pathname').should('equal', TICKET_VIEW_URL)
-        cy.wait(['@getAllTickets'])
 
         return this
     }
@@ -309,18 +266,6 @@ Elements:
     importTicketTable (filePath: string): this {
         cy.get('[data-cy=data-importer--upload]', { timeout: 30000 })
             .selectFile(filePath, { force: true })
-
-        return this
-    }
-
-    waitCreatingTicket (): this {
-        cy.wait(['@selectContact', '@createTicket'], { timeout: 60000 })
-
-        return this
-    }
-
-    waitFetchingContact (): this {
-        cy.wait('@selectContact', { timeout: 60000 })
 
         return this
     }
