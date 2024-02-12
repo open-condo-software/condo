377dafcc0db83e3113145fd00db4a975008641ab
VKislov <wkislov0@gmail.com>
2024-02-12 14:28:48 +0500

    refactor(condo): DOMA-8344 refactor throttling for invoice pushes

--- apps/condo/domains/notification/constants/constants.js
+++ apps/condo/domains/notification/constants/constants.js
@@ -816,6 +816,7 @@ const MESSAGE_DELIVERY_OPTIONS = {
     [MARKETPLACE_INVOICE_WITH_TICKET_PUBLISHED_MESSAGE_TYPE]: {
         allowedTransports: [PUSH_TRANSPORT],
         defaultTransports: [PUSH_TRANSPORT],
+        throttlePeriodForUser: 60,
     },
     [MARKETPLACE_INVOICE_CASH_PUBLISHED_MESSAGE_TYPE]: {
         allowedTransports: [PUSH_TRANSPORT],
@@ -824,6 +825,7 @@ const MESSAGE_DELIVERY_OPTIONS = {
     [MARKETPLACE_INVOICE_CASH_WITH_TICKET_PUBLISHED_MESSAGE_TYPE]: {
         allowedTransports: [PUSH_TRANSPORT],
         defaultTransports: [PUSH_TRANSPORT],
+        throttlePeriodForUser: 60,
     },
 
 }
