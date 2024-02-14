279958028b6adc60c5129e61e4d95766f41648b2
AleX83Xpert <AleX83Xpert@mail.ru>
2024-02-14 17:14:22 +0500

    chore(address-service): DOMA-8447 remove regexp while searching address sources

---
+++ packages/keystone/fields/RegexplessText/index.js
@@ -0,0 +1,25 @@
+const { Text } = require('@keystonejs/fields')
+
+class RegexplessKnexFieldAdapter extends Text.adapters.knex {
+
+    equalityConditionsInsensitive (dbPath) {
+        return {
+            [`${this.path}_i`]: value => b => b.whereRaw(`lower(${dbPath}) = lower(?)`, [value]),
+            [`${this.path}_not_i`]: value => b =>
+                value === null
+                    ? b.whereNotNull(dbPath)
+                    : b.whereRaw(`lower(${dbPath}) != lower(?)`, [value]).orWhereNull(dbPath),
+        }
+    }
+}
+
+module.exports = {
+    type: 'RegexplessText',
+    implementation: Text.implementation,
+    views: Text.views,
+    adapters: {
+        mongoose: Text.adapters.mongoose,
+        knex: RegexplessKnexFieldAdapter,
+        prisma: Text.adapters.prisma,
+    },
+}
--- packages/keystone/fields/index.js
+++ packages/keystone/fields/index.js
@@ -5,6 +5,7 @@ const FileWithUTF8Name = require('./FileWithUTF8Name')
 const Json = require('./Json')
 const LocalizedText = require('./LocalizedText')
 const Options = require('./Options')
+const RegexplessText = require('./RegexplessText')
 const SignedDecimal = require('./SignedDecimal')
 const Stars = require('./Stars')
 
@@ -18,4 +19,5 @@ module.exports = {
     DateInterval,
     AddressPartWithType,
     FileWithUTF8Name,
+    RegexplessText,
 }
