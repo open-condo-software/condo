26b50dfdfe24c36f48a00443d092d9f45ea8dbee
Sitnikov Ivan <sit7602@gmail.com>
2024-03-01 12:42:02 +0500

    feat(condo): INFRA-210 extract stitchSchema middleware to packages

---
+++ packages/keystone/stitchSchema.js
@@ -0,0 +1,219 @@
+const { GraphQLFileLoader } = require('@graphql-tools/graphql-file-loader')
+const { GraphQLUpload } = require('@graphql-tools/links')
+const { loadSchema } = require('@graphql-tools/load')
+const { stitchSchemas } = require('@graphql-tools/stitch')
+const { RenameTypes, RenameRootFields } = require('@graphql-tools/wrap')
+const { ApolloServer } = require('apollo-server-express')
+const bodyParser = require('body-parser')
+const { fetch } = require('cross-fetch')
+const express = require('express')
+const FormData = require('form-data')
+const { print } = require('graphql')
+const graphqlUploadExpress = require('graphql-upload/graphqlUploadExpress.js')
+const get = require('lodash/get')
+
+const conf = require('@open-condo/config')
+
+
+const CONDO_ACCESS_TOKEN_KEY = 'condoAccessToken'
+const APP_TOKEN_KEY = 'appToken'
+const ACCEPT_LANGUAGE = 'accept-language'
+// NOTE: DEFAULT VALUES OF GraphQLApp
+const MAX_FILE_SIZE = 200 * 1024 * 1024
+const MAX_FILES = 5
+
+
+function streamToBuffer (stream) {
+    const chunks = []
+    return new Promise((resolve, reject) => {
+        stream.on('data', (chunk) => chunks.push(Buffer.from(chunk)))
+        stream.on('error', (error) => reject(error))
+        stream.on('end', () => resolve(Buffer.concat(chunks)))
+    })
+}
+
+function isPromise (promise) {
+    return promise && Object.prototype.toString.call(promise) === '[object Promise]'
+}
+
+async function packVariablesRecursively (rawVariables, prefix = ['variables'], maps = []) {
+    const variables = {}
+    for (const key of Object.keys(rawVariables)) {
+        const pathParts = [...prefix, key]
+        const path = pathParts.join('.')
+        if (typeof rawVariables[key] !== 'object') {
+            variables[key] = rawVariables[key]
+        } else if (rawVariables[key] === null) {
+            variables[key] = null
+        } else if (isPromise(rawVariables[key])) {
+            const file = await rawVariables[key]
+            maps.push({
+                key: path,
+                file,
+            })
+            variables[key] = null
+        } else if (Array.isArray(rawVariables[key])) {
+            variables[key] = []
+            for (let i = 0; i < rawVariables[key].length; i++) {
+                const { variables: packedVariables } = await packVariablesRecursively(rawVariables[key][i], [...pathParts, `${i}`], maps)
+                variables[key].push(packedVariables)
+            }
+        } else {
+            const { variables: packedVariables } = await packVariablesRecursively(rawVariables[key], pathParts, maps)
+            variables[key] = packedVariables
+        }
+    }
+
+    return { variables, maps }
+}
+
+function makeRemoteExecutor (api_url, token_field) {
+    return async ({ document, variables, context }) => {
+        const query = typeof document === 'string' ? document : print(document)
+
+        const headers = {}
+        if (context && context[ACCEPT_LANGUAGE]) {
+            headers[ACCEPT_LANGUAGE] = context[ACCEPT_LANGUAGE]
+        }
+        if (context && context[token_field]) {
+            headers.Authorization = `Bearer ${context[token_field]}`
+        }
+
+        const { maps, variables: packedVariables } = await packVariablesRecursively(variables)
+
+        // NO FILES IN REQUEST -> SEND JSON REQUEST
+        if (maps.length === 0) {
+            headers['Content-Type'] = 'application/json'
+            const fetchResult = await fetch(api_url, {
+                method: 'POST',
+                headers: headers,
+                body: JSON.stringify({ query, variables }),
+            })
+
+            const status = get(fetchResult, 'status')
+            const statusText = get(fetchResult, 'statusText')
+
+            try {
+                return { ...await fetchResult.json(), status, statusText }
+            } catch (error) {
+                return { status, statusText }
+            }
+        } else {
+            // OTHERWISE, SEND MULTI-PART REQUEST
+            const form = new FormData()
+            form.append('operations', JSON.stringify({ query: query, variables: packedVariables }))
+
+            const map = {}
+            for (let i = 0; i < maps.length; i++) {
+                map[`${i}`] = [maps[i].key]
+            }
+            form.append('map', JSON.stringify(map))
+
+            for (let i = 0; i < maps.length; i++) {
+                const file = maps[i].file
+                const buffer = await streamToBuffer(file.createReadStream())
+                form.append(`${i}`, buffer, {
+                    filename: file.filename,
+                    mimetype: file.mimetype,
+                    encoding: file.encoding,
+                })
+            }
+
+            const fetchResult = await fetch(api_url, {
+                method: 'POST',
+                headers: headers,
+                body: form,
+            })
+
+            const status = get(fetchResult, 'status')
+            const statusText = get(fetchResult, 'statusText')
+
+            try {
+                return { ...await fetchResult.json(), status, statusText }
+            } catch (error) {
+                return { status, statusText }
+            }
+        }
+    }
+}
+
+async function makeGatewaySchema (appTokenKey, condoAccessTokenKey) {
+    const appAPIUrl = `${conf.PASS_DOMAIN}/admin/api`
+    const condoAPIUrl = `${conf.CONDO_DOMAIN}/admin/api`
+    const appExecutor = makeRemoteExecutor(appAPIUrl, appTokenKey)
+    const condoExecutor = makeRemoteExecutor(condoAPIUrl, condoAccessTokenKey)
+
+    // NOTE: Resolves schema from index.js, so '.' instead of '..'
+    const appSchema = await loadSchema('./schema.graphql', {
+        loaders: [new GraphQLFileLoader()],
+    })
+    const condoSchema = await loadSchema('./condoSchema.graphql', {
+        loaders: [new GraphQLFileLoader()],
+    })
+
+    return stitchSchemas({
+        subschemas: [
+            {
+                schema: appSchema,
+                executor: appExecutor,
+            },
+            {
+                schema: condoSchema,
+                executor: condoExecutor,
+                transforms: [
+                    new RenameTypes((name) => `Condo${name}`),
+                    new RenameRootFields((op, name) => `condo${name.charAt(0).toUpperCase()}${name.slice(1)}`),
+                ],
+            },
+        ],
+        resolvers: {
+            Upload: GraphQLUpload,
+        },
+    })
+}
+
+class StitchSchemaMiddleware {
+    /**
+     *
+     * @param apiUrl {String} graphql main endpoint. Default value: /graphql
+     * @param condoAccessTokenKey {String} session key for access token. Default value: condoAccessToken
+     * @param appTokenKey {String} session key for app token. Default value: appToken
+     */
+    constructor ({ apiUrl = '/graphql', condoAccessTokenKey = CONDO_ACCESS_TOKEN_KEY, appTokenKey = APP_TOKEN_KEY }) {
+        this.apiUrl = apiUrl
+        this.condoAccessTokenKey = condoAccessTokenKey
+        this.appTokenKey = appTokenKey
+    }
+
+    async prepareMiddleware () {
+        // creates middleware - no routes exposed
+        // nosemgrep: javascript.express.security.audit.express-check-csurf-middleware-usage.express-check-csurf-middleware-usage
+        const app = express()
+        const schema = await makeGatewaySchema()
+
+        const server = new ApolloServer({
+            schema,
+            context: (req) => ({
+                [this.condoAccessTokenKey]: get(req, ['session', this.condoAccessTokenKey]),
+                [this.appTokenKey]: get(req, ['session', this.appTokenKey]),
+                [ACCEPT_LANGUAGE]: get(req,  ['headers', ACCEPT_LANGUAGE]),
+            }),
+            introspection: false,
+            playground: false,
+            debug: false,
+            uploads: false,
+        })
+
+        app.use(
+            this.apiUrl,
+            bodyParser.json(),
+            graphqlUploadExpress({ maxFiles: MAX_FILES, maxFileSize: MAX_FILE_SIZE }),
+        )
+
+        app.use(server.getMiddleware({ path: this.apiUrl, cors: false }))
+
+        return app
+    }
+}
+
+module.exports = { StitchSchemaMiddleware, makeRemoteExecutor }
