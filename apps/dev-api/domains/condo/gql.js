38bbe235cd5aa4631ad032556cd8181a3e29905b
SavelevMatthew <savelevmatthew@gmail.com>
2024-03-18 17:23:24 +0500

    feat(dev-api): INFRA-281 added publish logic

--- apps/dev-api/domains/condo/gql.js
+++ apps/dev-api/domains/condo/gql.js
@@ -4,10 +4,12 @@ const CondoB2CAppGql = generateGqlQueries('B2CApp', '{ id }')
 const CondoB2CAppBuildGql = generateGqlQueries('B2CAppBuild', '{ id }')
 const CondoB2CAppPropertyGql = generateGqlQueries('B2CAppProperty', '{ id address deletedAt app { importId importRemoteSystem } }')
 const CondoOIDCClientGql = generateGqlQueries('OidcClient', '{ id clientId payload name isEnabled }')
+const CondoB2CAppAccessRightGql = generateGqlQueries('B2CAppAccessRight', '{ id app { id } user { id } }')
 
 module.exports = {
     CondoB2CAppGql,
     CondoB2CAppBuildGql,
     CondoB2CAppPropertyGql,
     CondoOIDCClientGql,
+    CondoB2CAppAccessRightGql,
 }
