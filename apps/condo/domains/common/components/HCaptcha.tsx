ed8a5483e05d5a42629dc048bcf3c2ccfd2aa75c
Alllex202 <alllex.202@gmail.com>
2024-03-19 13:03:02 +0500

    feat(condo): DOMA-8659 error debug

--- apps/condo/domains/common/components/HCaptcha.tsx
+++ apps/condo/domains/common/components/HCaptcha.tsx
@@ -37,13 +37,14 @@ const HCaptchaProvider: FC = ({ children }) => {
         <HCaptchaContext.Provider value={{
             executeCaptcha,
         }}>
-            {/*<HCaptcha*/}
-            {/*    sitekey={hCaptcha && hCaptcha.SITE_KEY}*/}
-            {/*    languageOverride={intl.locale}*/}
-            {/*    size='invisible'*/}
-            {/*    loadAsync={true}*/}
-            {/*    ref={ref}*/}
-            {/*/>*/}
+            <HCaptcha
+                sitekey={hCaptcha && hCaptcha.SITE_KEY}
+                languageOverride={intl.locale}
+                size='invisible'
+                loadAsync={true}
+                reCaptchaCompat={false}
+                ref={ref}
+            />
             {children}
         </HCaptchaContext.Provider>
     )
