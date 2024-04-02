174d10354e85ed0044bac419348a99a58b5b9928
ekabardinsky <ekabardinsky@gmail.com>
2024-04-02 16:03:50 +0500

    feat(condo): DOMA-8687 do not modify isApproved in RegisterBillingReceiptsService.js

--- apps/condo/bin/fix-database-migration-state.js
+++ apps/condo/bin/fix-database-migration-state.js
@@ -181,7 +181,7 @@ ALTER TABLE "ServiceConsumerHistoryRecord" DROP COLUMN "billingAccount" CASCADE;
 COMMIT;
 
 --
--- 20240401134334-0378_remove_billingrecipient_isapproved_and_more Remove field isApproved from billingrecipient
+-- 20240402155921-0379_remove_billingrecipient_isapproved_and_more Remove field isApproved from billingrecipient
 --
 ALTER TABLE "BillingRecipient" DROP COLUMN "isApproved" CASCADE;
 COMMIT;
