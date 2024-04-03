e2773a19416811467b4bf83b939fe224191d76d7
Alllex202 <alllex.202@gmail.com>
2024-04-03 16:27:20 +0500

    feat(condo): DOMA-8523 updated saving previous query params for pages

--- apps/condo/domains/common/hooks/usePreviousFilters.ts
+++ apps/condo/domains/common/hooks/usePreviousFilters.ts
@@ -1,90 +1,149 @@
-import { isObject } from 'lodash'
 import get from 'lodash/get'
+import isArray from 'lodash/isArray'
 import isEmpty from 'lodash/isEmpty'
+import isObject from 'lodash/isObject'
+import isString from 'lodash/isString'
+import pick from 'lodash/pick'
+import toPairs from 'lodash/toPairs'
 import { useRouter } from 'next/router'
-import { useEffect, useMemo } from 'react'
+import { useCallback, useMemo } from 'react'
+
+import { useDeepCompareEffect } from '@open-condo/codegen/utils/useDeepCompareEffect'
 
 import { updateQuery } from '@condo/domains/common/utils/helpers'
 
 
 class PreviousQueryParams {
-    private static key = 'preview-query-params'
+    private static STORAGE_KEY_PREFIX = 'previous-query-params'
 
-    static set (path: string, queryParams: Record<string, unknown>, tab?: string) {
-        if (!path) return
+    static set (employeeId: string, path: string, queryParams: Record<string, unknown>, delimiter?: Record<string, unknown>) {
+        if (!isString(employeeId) || !isString(path)) return
 
-        let fullPath = path
-        if (tab) fullPath += `?tab=${tab}`
+        const pageKey = this.getPageKey(path, delimiter)
 
-        const allQueryParamsFromLocalStorage = this.getAll()
-        const newQueryParams = { ...allQueryParamsFromLocalStorage, [fullPath]: queryParams }
-        const newQueryParamsAsJson = JSON.stringify(newQueryParams)
-        localStorage.setItem(this.key, newQueryParamsAsJson)
+        const stateFromLocalStorage = this.getAll()
+        const allQueryParamsByEmployeeId = get(stateFromLocalStorage, employeeId)
+        const newQueryParamsByEmployeeId = { ...allQueryParamsByEmployeeId, [pageKey]: queryParams }
+        const newStateAsJson = JSON.stringify({ ...stateFromLocalStorage, [employeeId]: newQueryParamsByEmployeeId })
+
+        localStorage.setItem(this.getStorageKey(), newStateAsJson)
     }
 
-    static get (path: string, tab?: string) {
-        if (!path) return
+    static get (employeeId: string, path: string, delimiter?: Record<string, unknown>) {
+        if (!isString(employeeId) || !isString(path)) return
 
-        let fullPath = path
-        if (tab) fullPath += `?tab=${tab}`
+        const pageKey = this.getPageKey(path, delimiter)
 
-        const queryParamsAsJson = localStorage.getItem(this.key)
-        if (!queryParamsAsJson) return
+        const stateAsJson = localStorage.getItem(this.getStorageKey())
+        if (!stateAsJson) return
 
         try {
-            const queryParams = JSON.parse(queryParamsAsJson)
-            if (!isObject(queryParams)) return
+            const stateFromLocalStorage = JSON.parse(stateAsJson)
+            if (!isObject(stateFromLocalStorage)) return
 
-            const queryParamsByPath = get(queryParams, fullPath)
-            if (!isObject(queryParamsByPath)) return
+            const allQueryParamsByOrganizationId = get(stateFromLocalStorage, employeeId)
+            if (!isObject(allQueryParamsByOrganizationId)) return
 
-            return queryParamsByPath
+            const queryParamsByPageKey = get(allQueryParamsByOrganizationId, pageKey)
+            if (!isObject(queryParamsByPageKey)) return
+
+            return queryParamsByPageKey
         } catch (error) {
-            console.error('Cannot parse previous query params by path', { error, queryParamsAsJson, fullPath, path, tab })
+            console.error('Cannot parse previous query params by path', { error, stateAsJson, pageKey, path, delimiter, employeeId })
             return
         }
     }
 
     private static getAll () {
-        const queryParamsAsJson = localStorage.getItem(this.key)
-        if (!queryParamsAsJson) return
+        const stateAsJson = localStorage.getItem(this.getStorageKey())
+        if (!stateAsJson) return
 
         try {
-            const queryParams = JSON.parse(queryParamsAsJson)
-            if (!isObject(queryParams)) return
+            const stateFromLocalStorage = JSON.parse(stateAsJson)
+            if (!isObject(stateFromLocalStorage)) return
 
-            return queryParams
+            return stateFromLocalStorage
         } catch (error) {
-            console.error('Cannot parse previous query params', { error, queryParamsAsJson })
+            console.error('Cannot parse previous query params', { error, stateAsJson })
             return
         }
     }
+
+    private static getStorageKey () {
+        return `${this.STORAGE_KEY_PREFIX}-by-employee-id`
+    }
+
+    private static getPageKey (path: string, delimiter?: Record<string, unknown>) {
+        let pageKey = path
+        if (isObject(delimiter) && !isEmpty(delimiter)) {
+            const delimiterQueryParams = toPairs(delimiter)
+                .sort(([keyA], [keyB]) => keyA.localeCompare(keyB))
+                .map((paramKeyAndValue) => paramKeyAndValue.join('='))
+                .join('&')
+            if (delimiterQueryParams) pageKey += `?${delimiterQueryParams}`
+        }
+        return pageKey
+    }
 }
 
-export const usePreviousQueryParams = (pages: Array<string>) => {
-    const router = useRouter()
-    const filters = useMemo(() => get(router, 'query.filters'), [router])
-    const sort = useMemo(() => get(router, 'query.sort'), [router])
+const getParams = (query, paramsForSave) => {
+    return pick(query, paramsForSave)
+}
 
+type usePreviousQueryParamsType = (props: { trackedParamNames: Array<string>, delimitersParamNames?: Array<string>, employeeId?: string }) => void
+
+export const usePreviousQueryParams: usePreviousQueryParamsType = ({
+    delimitersParamNames,
+    employeeId,
+    trackedParamNames,
+}) => {
+    const router = useRouter()
     const path = router.pathname
     const query = router.query
     const tab = get(router, 'query.tab')
 
-    useEffect(() => {
-        if (!pages.includes(path)) return
-        if (!isEmpty(sort) || !isEmpty(filters)) return
+    const withDelimitersParams = useMemo(() => isArray(delimitersParamNames) && !isEmpty(delimitersParamNames), [])
 
-        const fromLocalStorage = PreviousQueryParams.get(path, tab)
-        if (!fromLocalStorage) return
+    const trackedParams = useMemo(() => getParams(query, trackedParamNames), [query])
+    const delimitersParams = useMemo(() => withDelimitersParams ? getParams(query, delimitersParamNames) : null, [query])
 
-        const newParameters = { ...query, ...fromLocalStorage }
-        updateQuery(router, { newParameters }, { routerAction: 'replace', resetOldParameters: false })
-    }, [path, tab])
+    const applyQueryParamsFromLocalStorage = useCallback(async () => {
+        const haveParamsFromQuery = trackedParamNames.some(paramName => !isEmpty(get(trackedParams, paramName)))
+        if (haveParamsFromQuery) return
 
-    useEffect(() => {
-        if (!pages.includes(path)) return
+        const paramsFromLocalStorage = PreviousQueryParams.get(employeeId, path, withDelimitersParams ? delimitersParams : null)
+        if (!paramsFromLocalStorage) return
 
-        const queryParamsToSave = { sort, filters: filters }
-        PreviousQueryParams.set(path, queryParamsToSave, tab)
-    }, [sort, filters, path, tab])
+        const newParameters = { ...query, ...paramsFromLocalStorage }
+        await updateQuery(router, { newParameters }, { routerAction: 'replace', resetOldParameters: false })
+    }, [trackedParams, path, query, router, tab, employeeId, delimitersParams])
+    
+    const saveQueryParamsToLocalStorage = useCallback(() => {
+        const queryParamsToSave = getParams(query, trackedParamNames)
+        PreviousQueryParams.set(employeeId, path, queryParamsToSave, withDelimitersParams ? delimitersParams : null)
+    }, [path, query, tab, employeeId, delimitersParams])
+
+    // Case with tabs
+    useDeepCompareEffect(() => {
+        if (!employeeId) return
+        if (withDelimitersParams) return
+        applyQueryParamsFromLocalStorage()
+    }, [employeeId])
+    useDeepCompareEffect(() => {
+        if (!employeeId) return
+        if (withDelimitersParams) return
+        saveQueryParamsToLocalStorage()
+    }, [employeeId, trackedParams])
+
+    // Case without tabs
+    useDeepCompareEffect(() => {
+        if (!employeeId) return
+        if (!withDelimitersParams) return
+        applyQueryParamsFromLocalStorage()
+    }, [employeeId, delimitersParams])
+    useDeepCompareEffect(() => {
+        if (!employeeId) return
+        if (!withDelimitersParams) return
+        saveQueryParamsToLocalStorage()
+    }, [employeeId, trackedParams, delimitersParams])
 }
