281a9f0efa31c7d77678eeacea6b78106ea2bed5
Alllex202 <alllex.202@gmail.com>
2024-04-05 22:19:58 +0500

    fix(condo): DOMA-8523 fixed filter

--- apps/condo/domains/ticket/utils/tables.utils.ts
+++ apps/condo/domains/ticket/utils/tables.utils.ts
@@ -6,7 +6,14 @@ import uniq from 'lodash/uniq'
 import { ADDRESS_SEARCH_STOP_WORDS } from '@condo/domains/common/constants'
 import { OMIT_SEARCH_CHARACTERS_REGEXP } from '@condo/domains/common/constants/regexps'
 import { DataIndexType, FilterType } from '@condo/domains/common/utils/tables.utils'
-import { NEW_OR_REOPENED_STATUS_TYPE, PROCESSING_STATUS_TYPE, COMPLETED_STATUS_TYPE, CLOSED_STATUS_TYPE } from '@condo/domains/ticket/constants'
+import {
+    NEW_OR_REOPENED_STATUS_TYPE,
+    PROCESSING_STATUS_TYPE,
+    COMPLETED_STATUS_TYPE,
+    CLOSED_STATUS_TYPE,
+    CANCELED_STATUS_TYPE,
+    DEFERRED_STATUS_TYPE,
+} from '@condo/domains/ticket/constants'
 import { INCIDENT_STATUS_ACTUAL, INCIDENT_STATUS_NOT_ACTUAL } from '@condo/domains/ticket/constants/incident'
 
 
@@ -51,8 +58,8 @@ export const getIsCompletedAfterDeadlineFilter = (): FilterType => {
 
         return {
             OR: [
-                { AND: { status: { type_in: [COMPLETED_STATUS_TYPE, CLOSED_STATUS_TYPE] }, isCompletedAfterDeadline: true } },
-                { AND: { status: { type_in: [NEW_OR_REOPENED_STATUS_TYPE, PROCESSING_STATUS_TYPE] }, deadline_lt: (new Date()).toISOString() } },
+                { AND: { status: { type_in: [COMPLETED_STATUS_TYPE, CLOSED_STATUS_TYPE, CANCELED_STATUS_TYPE] }, isCompletedAfterDeadline: true } },
+                { AND: { status: { type_in: [NEW_OR_REOPENED_STATUS_TYPE, PROCESSING_STATUS_TYPE, DEFERRED_STATUS_TYPE] }, deadline_lt: (new Date()).toISOString() } },
             ],
         }
     }
