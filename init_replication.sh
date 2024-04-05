b89c32f69154d955bd4dc3e5af108a97512da296
danil <emurashin@gmail.com>
2024-04-05 10:39:45 +0500

    fix(condo): INFRA-200 logical replication changed to asynchronous streaming replication

--- init_replication.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker exec -i master psql -U postgres -d main -c "CREATE PUBLICATION my_publication FOR ALL TABLES;"
-docker exec -i replica1 psql -U postgres -d main -c "CREATE SUBSCRIPTION my_subscription CONNECTION 'host=master dbname=main user=postgres password=postgres' PUBLICATION my_publication;"
