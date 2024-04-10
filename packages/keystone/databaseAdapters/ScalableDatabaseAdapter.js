637ba23a0529e734f5d1540c0ff1b58d6dd6d2bb
Pahaz White <pahaz.white@gmail.com>
2024-04-10 15:28:14 +0300

    chore(condo): DOMA-7367 wip

--- packages/keystone/databaseAdapters/ScalableDatabaseAdapter.js
+++ packages/keystone/databaseAdapters/ScalableDatabaseAdapter.js
@@ -5,6 +5,7 @@ const { mapValues } = require('lodash/object')
 
 const { FakeDatabaseAdapter } = require('./FakeDatabaseAdapter')
 const { parseDatabaseUrl, parseDatabaseMapping, matchDatabase } = require('./utils')
+const { wrapToCheckOnlyPublicApi } = require('./wrapToCheckOnlyPublicApi')
 
 function initDatabaseAdapters (databases) {
     return mapValues(databases, initDatabaseAdapter)
@@ -21,6 +22,13 @@ function initDatabaseAdapter (databaseUrl) {
     }
 }
 
+function createListParentAdapter (rootAdapter, commandAdapter, queryAdapter) {
+    throw new Error('! not implemented !')
+    return wrapToCheckOnlyPublicApi({
+
+    }, [])
+}
+
 function initListAdapter (rootAdapter, parentAdapter, key, adapterConfig) {
     // NOTE(pahaz): We can not use `parentAdapter.newListAdapter(key, adapterConfig)`
     // because we need to override `listAdapter.getListAdapterByKey` and `listAdapter.parentAdapter`
@@ -30,7 +38,49 @@ function initListAdapter (rootAdapter, parentAdapter, key, adapterConfig) {
     const getListAdapterByKey = rootAdapter.getListAdapterByKey.bind(rootAdapter)
     listAdapter.parentAdapter.getListAdapterByKey = getListAdapterByKey
     listAdapter.getListAdapterByKey = getListAdapterByKey
+    listAdapter.adapterId = 'WR_ROOT'
     return listAdapter
+}
+
+function initMixAdapter (rootAdapter, commandAdapter, queryAdapter, key, adapterConfig) {
+    const commandListAdapter = commandAdapter.newListAdapter(key, adapterConfig)
+    const queryListAdapter = queryAdapter.newListAdapter(key, adapterConfig)
+    const getListAdapterByKey = rootAdapter.getListAdapterByKey.bind(rootAdapter)
+    commandListAdapter.parentAdapter.getListAdapterByKey = getListAdapterByKey
+    commandListAdapter.getListAdapterByKey = getListAdapterByKey
+    queryListAdapter.parentAdapter.getListAdapterByKey = getListAdapterByKey
+    queryListAdapter.getListAdapterByKey = getListAdapterByKey
+    // NOTE: we need only connected field to adapter list for both adapters! to avoid any collision
+    commandListAdapter.fieldAdapters = queryListAdapter.fieldAdapters
+    commandListAdapter.fieldAdaptersByPath = queryListAdapter.fieldAdaptersByPath
+    commandListAdapter.adapterId = 'MIX_W_PART'
+    queryListAdapter.adapterId = 'MIX_R_PART'
+    return wrapToCheckOnlyPublicApi({
+        key,
+        adapterId: 'MIX_WR_ROOT',
+        config: adapterConfig,
+        parentAdapter: queryListAdapter.parentAdapter,
+        tableName: queryListAdapter.tableName,
+        fieldAdaptersByPath: queryListAdapter.fieldAdaptersByPath,
+        fieldAdapters: queryListAdapter.fieldAdapters,
+        getListAdapterByKey: getListAdapterByKey,
+        _query: () => queryListAdapter._query(),
+        newFieldAdapter: (fieldAdapterClass, name, path, field, getListByKey, config) => commandListAdapter.newFieldAdapter(fieldAdapterClass, name, path, field, getListByKey, config),
+        findAll: () => queryListAdapter.findAll(),
+        findById: (id) => queryListAdapter.findById(id),
+        find: (condition) => queryListAdapter.find(condition),
+        findOne: (condition) => queryListAdapter.findOne(condition),
+        itemsQuery: (args, metaArgs) => queryListAdapter.itemsQuery(args, metaArgs),
+        itemsQueryMeta: (args) => queryListAdapter.itemsQueryMeta(args),
+        create: (data) => commandListAdapter.create(data),
+        update: (id, data) => commandListAdapter.update(id, data),
+        delete: (id) => commandListAdapter.delete(id),
+    }, [
+        'key', 'parentAdapter', 'config', 'newFieldAdapter', 'findAll', 'findById', 'find', 'findOne', 'itemsQuery', 'itemsQueryMeta',
+        'create', 'update', 'delete',
+        'tableName', 'fieldAdapters', 'fieldAdaptersByPath', 'getListAdapterByKey',
+        '_query',  // at QueryBuilder._addWheres (../../node_modules/@keystonejs/adapter-knex/lib/adapter-knex.js:830:40)
+    ])
 }
 
 class ScalableDatabaseAdapter extends BaseKeystoneAdapter {
@@ -74,13 +124,14 @@ class ScalableDatabaseAdapter extends BaseKeystoneAdapter {
         const queryAdapter = this.__databaseAdapters[query]
         if (!queryAdapter) throw new Error(`ScalableDatabaseAdapter.newListAdapter(..): no parentAdapter ${query}`)
 
-        const listAdapter = initListAdapter(this, commandAdapter, key, adapterConfig)
-
-        if (query !== command) {
-            const listQueryAdapter = initListAdapter(this, queryAdapter, key, adapterConfig)
+        let listAdapter
+        if (query === command) {
+            listAdapter = initListAdapter(this, commandAdapter, key, adapterConfig)
+        } else {
+            listAdapter = initMixAdapter(this, commandAdapter, queryAdapter, key, adapterConfig)
             // TODO(pahaz): need to implement it!
             // overrideMethodsFromAnotherObject(listAdapter, listQueryAdapter, [])
-            throw new Error('we do not support it yet! We need to wrap all query methods for that!')
+            // throw new Error('we do not support it yet! We need to wrap all query methods for that!')
         }
 
         this.__listMappingAdapters[key] = listAdapter
