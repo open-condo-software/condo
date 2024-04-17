0178bf5d601f59aaa0945427e891d0fa3b4230a1
Alllex202 <56914444+Alllex202@users.noreply.github.com>
2024-04-17 14:26:35 +0500

    feat(condo): DOMA-8846 added link to employee bot in onboarding (#4627)
    * feat(condo): DOMA-8846 added link to bot for onboarding
    
    * feat(condo): DOMA-8846 fixed translations
    
    * feat(condo): DOMA-8846 added links to employee telegram bot in onboarding to app card
    
    * feat(condo): DOMA-8846 updated link to employee bot
    
    * Revert "feat(condo): DOMA-8846 updated link to employee bot"
    
    This reverts commit 83e4de080d3d35b0a9843ba738931d6a39f43935.

--- apps/condo/domains/onboarding/hooks/TourPage/useTourPageData.tsx
+++ apps/condo/domains/onboarding/hooks/TourPage/useTourPageData.tsx
@@ -1,18 +1,28 @@
 import { TourStepStatusType } from '@app/condo/schema'
+import getConfig from 'next/config'
 import { useRouter } from 'next/router'
 import { useCallback, useMemo } from 'react'
 
 import { useIntl } from '@open-condo/next/intl'
+import { Typography } from '@open-condo/ui'
 
 import { useTourContext } from '@condo/domains/onboarding/contexts/TourContext'
 import { GUIDE_LINK } from '@condo/domains/onboarding/utils/clientSchema/constants'
 
+
+const {
+    publicRuntimeConfig: {
+        telegramEmployeeBotName,
+    },
+} = getConfig()
 
 export const useTourPageData = ({ isAllSecondStepsCompleted, isInnerStepsCompleted }) => {
     const intl = useIntl()
     const CompletedTourDescription = intl.formatMessage({ id: 'tour.pageData.default.completed.description' })
     const OpenGuideMessage = intl.formatMessage({ id: 'tour.openGuide' })
     const ChooseOtherTaskMessage = intl.formatMessage({ id: 'tour.chooseOtherTask' })
+    const AndMessage = intl.formatMessage({ id: 'And' }).toLowerCase()
+    const ChantBotMessage = intl.formatMessage({ id: 'ChatBot' }).toLowerCase()
 
     const router = useRouter()
 
@@ -31,8 +41,21 @@ export const useTourPageData = ({ isAllSecondStepsCompleted, isInnerStepsComplet
         return isInnerStepsCompleted ? TourStepStatusType.Completed : TourStepStatusType.Todo
     }, [isAllSecondStepsCompleted, isDefaultStep, isInnerStepsCompleted])
 
+    const withLinkToEmployeeBot = activeStepType === 'ticket' && innerStepsStatus === 'todo'
+    const andLinkToEmployeeBot = useMemo(() => telegramEmployeeBotName ? (
+        <> {AndMessage} <Typography.Link
+            href={`https://t.me/${telegramEmployeeBotName}`}
+            target='_blank'
+            id='employee-telegram-bot-from-onboarding-by-ticket-step'
+        >
+            {ChantBotMessage}
+        </Typography.Link></>
+    ) : null, [AndMessage, ChantBotMessage])
+
+    const valuesToMessage = withLinkToEmployeeBot ? { andLinkToEmployeeBot } : null
+
     const title = intl.formatMessage({ id: `tour.pageData.${activeStepType}.${innerStepsStatus}.title` })
-    const subtitle = intl.formatMessage({ id: `tour.pageData.${activeStepType}.${innerStepsStatus}.subtitle` })
+    const subtitle = intl.formatMessage({ id: `tour.pageData.${activeStepType}.${innerStepsStatus}.subtitle` }, valuesToMessage)
     const buttonLabel = isDefaultStep ? OpenGuideMessage : ChooseOtherTaskMessage
     const onButtonClick = isDefaultStep ? handleOpenGuide : handleBackClick
     const description = isDefaultStep && CompletedTourDescription
