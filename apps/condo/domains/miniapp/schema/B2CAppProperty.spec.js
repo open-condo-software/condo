8753f63ecc06167d782911930c06b0d017fff64e
AleX83Xpert <AleX83Xpert@mail.ru>
2024-04-20 07:17:00 +0500

    Revert "feat(condo): DOMA-8507 try to mock modules for spec tests globally"
    This reverts commit 75faef2ef30b60fe5cae0cdad5afbd0dcdd8519e.

--- apps/condo/domains/miniapp/schema/B2CAppProperty.spec.js
+++ apps/condo/domains/miniapp/schema/B2CAppProperty.spec.js
@@ -6,8 +6,6 @@ const index = require('@app/condo/index')
 const { faker } = require('@faker-js/faker')
 const { set } = require('lodash')
 
-const { AddressServiceClient } = require('@open-condo/clients/address-service-client/AddressServiceClient')
-const { MockedAddressServiceClient } = require('@open-condo/clients/address-service-client/MockedAddressServiceClient')
 const { GQLErrorCode: { BAD_USER_INPUT } } = require('@open-condo/keystone/errors')
 const {
     makeLoggedInAdminClient,
@@ -29,13 +27,22 @@ const {
     makeClientWithServiceUser,
 } = require('@condo/domains/user/utils/testSchema')
 
+jest.mock('@open-condo/clients/address-service-client/AddressServiceClient')
+//
+// Need to call jest.mock() before
+//
+// eslint-disable-next-line import/order
+const { AddressServiceClient } = require('@open-condo/clients/address-service-client/AddressServiceClient')
+// eslint-disable-next-line import/order
+const { MockedAddressServiceClient } = require('@open-condo/clients/address-service-client/MockedAddressServiceClient')
+
 describe('B2CAppProperty spec', () => {
     let admin
     let app
     let permittedUser
     let anotherPermittedUser
 
-    setFakeClientMode(index, { excludeApps: ['OIDCMiddleware', 'NextApp', 'AdminUIApp'] })
+    setFakeClientMode(index, { excludeApps: ['OIDCMiddleware'] })
 
     beforeAll(async () => {
         admin = await makeLoggedInAdminClient()
--- apps/condo/jest.setupSpec.js
+++ apps/condo/jest.setupSpec.js
@@ -2,11 +2,6 @@ const falsey = require('falsey')
 
 const EXTRA_LOGGING = falsey(process.env.DISABLE_LOGGING)
 
-// The mocked module implementation must be placed into __mocks__/MockedClass near the module itself
-console.log('🥸Mock some modules')
-jest.mock('@open-condo/clients/address-service-client/AddressServiceClient')
-jest.mock('@open-condo/clients/finance-info-client')
-
 if (EXTRA_LOGGING) {
     // Patch tests to include their own name
     jasmine.getEnv().addReporter({ // eslint-disable-line
