03d855f7096234cec8279f95238189ddf655f4fb
Artyom Vancyan <artyom.vancyan2000@gmail.com>
2024-05-02 18:23:31 +0400

    feat(ui): #3906 replaced phone-input component

--- packages/ui/src/components/Input/phone.tsx
+++ packages/ui/src/components/Input/phone.tsx
@@ -1,42 +1,45 @@
-import classNames from 'classnames'
-import React, { useCallback } from 'react'
-import ReactPhoneInput from 'react-phone-input-2'
+import ConfigProvider, { defaultPrefixCls } from 'antd/es/config-provider'
+import PhoneInput from 'antd-phone-input'
+import { PhoneInputProps as DefaultPhoneInputProps } from 'antd-phone-input/types'
+import React, { cloneElement, useCallback } from 'react'
+import { getFormattedNumber, getMetadata } from 'react-phone-hooks'
 
-import { INPUT_CLASS_PREFIX } from './input'
+import { SELECT_CLASS_PREFIX } from '../Select/select'
 
-import type { PhoneInputProps as DefaultPhoneInputProps } from 'react-phone-input-2'
+export type PhoneInputProps = Omit<DefaultPhoneInputProps, 'onChange'> & {
+    onChange?: (value: string, data: { countryCode: any }, event: React.ChangeEvent<HTMLInputElement>, formattedValue: string) => void
+}
 
 const DEFAULT_COUNTRY = 'ru'
 const DEFAULT_PLACEHOLDER = '7 (999) 123-4567'
 
-export type PhoneInputProps = Omit<React.InputHTMLAttributes<HTMLInputElement>, 'style' | 'value' | 'onChange'>
-& Pick<DefaultPhoneInputProps, 'country' | 'placeholder' | 'onChange' | 'value'>
-
 const Phone: React.FC<PhoneInputProps> = (props) => {
     const { country, placeholder, onChange, disabled = false, ...restProps } = props
 
-    const containerClasses = classNames(`${INPUT_CLASS_PREFIX}-phone`, {
-        [`${INPUT_CLASS_PREFIX}-phone-disabled`]: disabled,
-    })
-
-    const internalOnChange = useCallback<Required<PhoneInputProps>['onChange']>((rawValue, data, event, formattedValue) => {
-        const value = rawValue ? `+${rawValue}` : rawValue
+    const internalOnChange = useCallback<Required<DefaultPhoneInputProps>['onChange']>(({ countryCode, areaCode, phoneNumber, isoCode }, event) => {
+        const data = { countryCode: isoCode }
+        const value = `${countryCode}${areaCode || ''}${phoneNumber || ''}`
+        const formattedValue = getFormattedNumber(value, getMetadata(value)?.[3] as string)
         if (onChange) {
             onChange(value, data, event, formattedValue)
         }
     }, [onChange])
 
     return (
-        <ReactPhoneInput
-            {...restProps}
-            onChange={internalOnChange}
-            containerClass={containerClasses}
-            disabled={disabled}
-            inputClass={INPUT_CLASS_PREFIX}
-            country={country || DEFAULT_COUNTRY}
-            copyNumbersOnly={false}
-            placeholder={placeholder || DEFAULT_PLACEHOLDER}
-        />
+        <ConfigProvider prefixCls='condo'>
+            <PhoneInput
+                {...restProps}
+                disabled={disabled}
+                onChange={internalOnChange}
+                country={country as any || DEFAULT_COUNTRY}
+                placeholder={placeholder || DEFAULT_PLACEHOLDER}
+                dropdownRender={(menu) => (
+                    <ConfigProvider prefixCls={defaultPrefixCls}>
+                        {cloneElement(menu as any, { prefixCls: SELECT_CLASS_PREFIX })}
+                    </ConfigProvider>
+                )}
+            />
+        </ConfigProvider>
     )
 }
 
