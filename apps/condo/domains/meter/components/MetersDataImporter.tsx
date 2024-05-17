896d3725f3d48d1bb160e97206974a67d0bb9852
AleX83Xpert <AleX83Xpert@mail.ru>
2024-05-17 12:42:02 +0500

    fix(condo): DOMA-9037 extract normalization of rows lengths to function

--- apps/condo/domains/meter/components/MetersDataImporter.tsx
+++ apps/condo/domains/meter/components/MetersDataImporter.tsx
@@ -13,6 +13,19 @@ import {
     TOnMetersUpload,
 } from './MetersDataImporterTypes'
 
+/**
+ * Mutates argument
+ * @param data
+ */
+function normalizeRowsShape (data: unknown[][]) {
+    const rowLength = Math.max.apply(null, data.map((x) => x.length))
+    for (let i = 0; i < data.length; i++) {
+        for (let j = 0; j < rowLength; j++) {
+            data[i][j] = (isNil(data[i][j]) || data[i][j] === '') ? null : String(data[i][j])
+        }
+    }
+}
+
 const useUploadConfig = (onUpload: TOnMetersUpload) => {
     const intl = useIntl()
 
@@ -50,13 +63,7 @@ const useUploadConfig = (onUpload: TOnMetersUpload) => {
                     const sheetData = XLSX.utils.sheet_to_json<string[]>(wb.Sheets[wsName], { header: 1 })
                         .filter((x) => x.length) // filter out empty rows
 
-                    // Normalize row length
-                    const rowLength = Math.max.apply(null, sheetData.map((x) => x.length))
-                    for (let i = 0; i < sheetData.length; i++) {
-                        for (let j = 0; j < rowLength; j++) {
-                            sheetData[i][j] = (isNil(sheetData[i][j]) || sheetData[i][j] === '') ? null : String(sheetData[i][j])
-                        }
-                    }
+                    normalizeRowsShape(sheetData)
 
                     onUpload(ImportDataType.doma, sheetData)
                 }
