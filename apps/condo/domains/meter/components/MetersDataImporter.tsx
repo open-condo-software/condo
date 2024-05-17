8a40afa91a44841802feb54b5286993daa26ea40
Alexander Naydenov <AleX83Xpert@mail.ru>
2024-05-17 13:24:08 +0500

    fix(condo): DOMA-9037 normalize rows shape (#4741)
    * fix(condo): DOMA-9037 normalize row length
    
    To be sure that cell with error will be added in the end of each row.
    
    * fix(condo): DOMA-9037 extract normalization of rows lengths to function

--- apps/condo/domains/meter/components/MetersDataImporter.tsx
+++ apps/condo/domains/meter/components/MetersDataImporter.tsx
@@ -13,6 +13,19 @@ import {
     TOnMetersUpload,
 } from './MetersDataImporterTypes'
 
+/**
+ * Mutates argument
+ * @param data
+ */
+function normalizeRowsShape (data: unknown[][]) {
+    const rowLength = Math.max.apply(null, data.map((x) => x.length))
+    for (let i = 0; i < data.length; i++) {
+        for (let j = 0; j < rowLength; j++) {
+            data[i][j] = (isNil(data[i][j]) || data[i][j] === '') ? null : String(data[i][j])
+        }
+    }
+}
+
 const useUploadConfig = (onUpload: TOnMetersUpload) => {
     const intl = useIntl()
 
@@ -49,7 +62,8 @@ const useUploadConfig = (onUpload: TOnMetersUpload) => {
                     const wsName = wb.SheetNames[0]
                     const sheetData = XLSX.utils.sheet_to_json<string[]>(wb.Sheets[wsName], { header: 1 })
                         .filter((x) => x.length) // filter out empty rows
-                        .map((row) => row.map((cell) => isNil(cell) ? '' : String(cell))) // Stringify all values
+
+                    normalizeRowsShape(sheetData)
 
                     onUpload(ImportDataType.doma, sheetData)
                 }
