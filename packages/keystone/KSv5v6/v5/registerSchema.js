365de009c498f55396e687e6237e17c907d011d2
Alllex202 <alllex.202@gmail.com>
2024-05-31 20:28:54 +0500

    fix(keystone): DOMA-8928 Added missing field types to "convertStringToTypes"

--- packages/keystone/KSv5v6/v5/registerSchema.js
+++ packages/keystone/KSv5v6/v5/registerSchema.js
@@ -1,10 +1,33 @@
-const { Relationship, Integer, Float, Slug, Virtual, Url, Uuid, Checkbox, DateTimeUtc, CalendarDay, Decimal, Password } = require('@keystonejs/fields')
+const {
+    CalendarDay,
+    Checkbox,
+    DateTimeUtc,
+    Decimal,
+    Float,
+    Integer,
+    Password,
+    Relationship,
+    Slug,
+    Url,
+    Uuid,
+    Virtual,
+} = require('@keystonejs/fields')
 const { AuthedRelationship } = require('@keystonejs/fields-authed-relationship')
+const { Markdown } = require('@keystonejs/fields-markdown')
+const { Wysiwyg } = require('@keystonejs/fields-wysiwyg-tinymce')
 const { get, isObject, isString } = require('lodash')
 
-const { Text } = require('@open-condo/keystone/fields')
-
-const { Json, SignedDecimal, AutoIncrementInteger, LocalizedText, DateInterval, FileWithUTF8Name, Select } = require('../../fields')
+const {
+    AddressPartWithType,
+    AutoIncrementInteger,
+    DateInterval,
+    FileWithUTF8Name,
+    Json,
+    LocalizedText,
+    Select,
+    SignedDecimal,
+    Text,
+} = require('../../fields')
 const { HiddenRelationship } = require('../../plugins/utils/HiddenRelationship')
 const { GQL_SCHEMA_TYPES, GQL_CUSTOM_SCHEMA_TYPE, GQL_LIST_SCHEMA_TYPE } = require('../../schema')
 
@@ -33,28 +56,34 @@ function applyKeystoneV5AdminFixes (schema) {
 /** @deprecated it's part of internal API use prepareKeystone instead */
 function convertStringToTypes (schema) {
     const mapping = {
+        // Default types
+        AuthedRelationship,
         CalendarDay,
         Checkbox,
         DateTimeUtc,
         Decimal,
-        File: FileWithUTF8Name,
         Float,
         Integer,
+        Markdown,
         Password,
         Relationship,
-        Select,
         Slug,
-        Text,
         Url,
         Uuid,
         Virtual,
-        Json,
-        SignedDecimal,
+        Wysiwyg,
+
+        // Custom types
+        AddressPartWithType,
         AutoIncrementInteger,
-        LocalizedText,
-        AuthedRelationship,
-        HiddenRelationship,
         DateInterval,
+        File: FileWithUTF8Name,
+        Json,
+        HiddenRelationship,
+        LocalizedText,
+        Select,
+        SignedDecimal,
+        Text,
     }
 
     if (!schema.fields) throw new Error('convertStringToTypes(): wrong schema type! no fields!')
