abc6531f2667f9c877af874017e3eb16c3dcb029
Vasiliy Kislov <64303474+VKislov@users.noreply.github.com>
2024-06-17 12:52:25 +0500

    fix(condo): DOMA-9087 use isPayable field of BillingReceipt for filtrate only actual receipt in notifyResidentsOnPayday cron task (#4763)

--- apps/condo/domains/resident/tasks/notifyResidentsOnPayday.js
+++ apps/condo/domains/resident/tasks/notifyResidentsOnPayday.js
@@ -7,7 +7,7 @@ const { getLogger } = require('@open-condo/keystone/logging')
 const { getSchemaCtx } = require('@open-condo/keystone/schema')
 
 const { CONTEXT_FINISHED_STATUS } = require('@condo/domains/acquiring/constants/context')
-const { BillingReceipt, getPaymentsSum } = require('@condo/domains/billing/utils/serverSchema')
+const { BillingReceiptAdmin, BillingReceipt, getPaymentsSum } = require('@condo/domains/billing/utils/serverSchema')
 const { COUNTRIES } = require('@condo/domains/common/constants/countries')
 const { RU_LOCALE } = require('@condo/domains/common/constants/locale')
 const { loadListByChunks } = require('@condo/domains/common/utils/serverSchema')
@@ -107,7 +107,7 @@ async function notifyResidentsOnPayday () {
                 const receipts = await loadListByChunks({
                     context,
                     chunkSize:20,
-                    list: BillingReceipt,
+                    list: BillingReceiptAdmin,
                     where: {
                         account: {
                             number: accountNumber,
@@ -119,6 +119,11 @@ async function notifyResidentsOnPayday () {
                         toPay_gt: '0',
                         deletedAt: null,
                     },
+                    /**
+                     * @param {BillingReceipt[]} chunk
+                     * @returns {BillingReceipt[]}
+                     */
+                    chunkProcessor: async chunk => chunk.filter(receipt => receipt.isPayable),
                 })
                 state.processedReceipts += receipts.length
 
