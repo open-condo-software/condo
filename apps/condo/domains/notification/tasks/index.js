bf91367ed081c13cbe91b0446c7d55204ebfcffb
Pavel <52532264+nomerdvadcatpyat@users.noreply.github.com>
2024-06-21 12:35:18 +0500

    feat(condo): DOMA-9151 send hashed resident phones to marketing (#4810)
    * feat(condo): DOMA-9151 added _internalSendResidentPhonesService
    
    * feat(condo): DOMA-9151 added script
    
    * feat(condo): DOMA-9151 added task
    
    * feat(condo): DOMA-9151 remove redundant code
    
    * feat(condo): DOMA-9151 change math random to crypto randomInt
    
    * feat(condo): DOMA-9151 added tests
    
    * fix(condo): DOMA-9151 update tests and add hash check
    
    * fix(condo): DOMA-9151 move rmfile to finally block and update cron time
    
    * feat(condo): DOMA-9151 move task to notification domain
    
    * feat(condo): DOMA-9151 made a internal endpoint
    
    * feat(condo): DOMA-9151 added tests and generate migration
    
    * feat(condo): DOMA-9151 fix lint issue
    
    * feat(condo): DOMA-9151 set marketing_email env

--- apps/condo/domains/notification/tasks/index.js
+++ apps/condo/domains/notification/tasks/index.js
@@ -1,5 +1,7 @@
 const { deliverMessage } = require('./deliverMessage')
+const { sendHashedResidentPhonesTask } = require('./sendHashedResidentPhones')
 
 module.exports = {
     deliverMessage,
+    sendHashedResidentPhonesTask,
 }
