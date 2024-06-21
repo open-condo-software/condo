431c75ac39f1395a4e49ffb20a0c760dccac1b08
Mikhail Gorbunov <toplenboren@gmail.com>
2024-06-21 16:54:50 +0500

    refactor(condo): DOMA-7888: fixes on review

--- apps/condo/domains/news/tasks/index.js
+++ apps/condo/domains/news/tasks/index.js
@@ -2,12 +2,10 @@ const notifyResidentsAboutDelayedNewsItem = require('./notifyResidentsAboutDelay
 const { notifyResidentsAboutNewsItemTask } = require('./notifyResidentsAboutNewsItem')
 const publishDelayedSharedNewsItems = require('./publishDelayedSharedNewsItems')
 const publishNewsItemSharing = require('./publishNewsItemSharing')
-const publishSharedNewsItemsByNewsItem = require('./publishSharedNewsItemsByNewsItem')
 
 module.exports = {
     notifyResidentsAboutNewsItemTask,
     notifyResidentsAboutDelayedNewsItem,
     publishDelayedSharedNewsItems,
     publishNewsItemSharing,
-    publishSharedNewsItemsByNewsItem,
 }
