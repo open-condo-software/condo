3e76d1634c43d3f2533e1f6547daf113c2d6202a
Pavel <nomerdvadcatpyat@gmail.com>
2024-06-21 16:17:26 +0500

    feat(condo): INFRA-316 fix tests in app01

--- packages/codegen/templates/app01/domains/condo/gql/fields.js
+++
@@ -1,5 +0,0 @@
-const COMMON_FIELDS = 'id dv sender { dv fingerprint } v deletedAt newId createdBy { id name } updatedBy { id name } createdAt updatedAt'
-
-module.exports = {
-    COMMON_FIELDS,
-}
