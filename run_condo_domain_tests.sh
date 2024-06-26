b087f88a225e3597e4104712b429ca2c0799e4a5
SavelevMatthew <savelevmatthew@gmail.com>
2024-06-26 15:40:39 +0500

    fix(condo): INFRA-357 fix tests

--- run_condo_domain_tests.sh
+++ run_condo_domain_tests.sh
@@ -58,11 +58,11 @@ yarn workspace @app/condo dev 2>&1 > /app/test_logs/condo.dev.log &
 # waitForLocalhostApiReady.sh
 API_SLEEP_INTERVAL=3
 API_WAIT_TIMEOUT=120
-API_RESULT=$(curl -s 'http://localhost:3000/admin/api' --data-raw '{"operationName":null,"variables":{},"query":"{appVersion}"}' -H 'content-type: application/json' | tr -d ' \n')
+API_RESULT=$(curl -s 'http://localhost:4004/admin/api' --data-raw '{"operationName":null,"variables":{},"query":"{appVersion}"}' -H 'content-type: application/json' | tr -d ' \n')
 # API_RESULT is empty if server is down!
 while [[ ("${API_RESULT}" != *"appVersion"* || "${API_RESULT}" == "") && ((${API_WAIT_TIMEOUT} > 0)) ]]; do
   sleep ${API_SLEEP_INTERVAL}
-  API_RESULT=$(curl -s 'http://localhost:3000/admin/api' --data-raw '{"operationName":null,"variables":{},"query":"{appVersion}"}' -H 'content-type: application/json' | tr -d ' \n')
+  API_RESULT=$(curl -s 'http://localhost:4004/admin/api' --data-raw '{"operationName":null,"variables":{},"query":"{appVersion}"}' -H 'content-type: application/json' | tr -d ' \n')
   ((API_WAIT_TIMEOUT-=${API_SLEEP_INTERVAL}))
 done
 
