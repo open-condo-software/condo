1eddfc548b67b41b20a3f9e4d633629fedbb52c2
Pavel <52532264+nomerdvadcatpyat@users.noreply.github.com>
2024-06-26 11:53:00 +0500

    fix(condo): DOMA-9343 fix refresh sbbol client secret (#4864)
    * fix(condo): DOMA-9343 pass useExtendedConfig to SbbolOauth2Api
    
    * fix(condo): DOMA-9343 used one user access token for change client secrets

--- apps/condo/domains/organization/tasks/refreshSbbolClientSecret.js
+++ apps/condo/domains/organization/tasks/refreshSbbolClientSecret.js
@@ -1,9 +1,18 @@
 const dayjs = require('dayjs')
 const passwordGenerator = require('generate-password')
+const get = require('lodash/get')
 
+const conf = require('@open-condo/config')
+const { getSchemaCtx } = require('@open-condo/keystone/schema')
 const { createCronTask } = require('@open-condo/keystone/tasks')
 
 const { getSbbolSecretStorage, changeClientSecret } = require('@condo/domains/organization/integrations/sbbol/utils')
+const { getAccessTokenForUser } = require('@condo/domains/organization/integrations/sbbol/utils/getAccessTokenForUser')
+const { User } = require('@condo/domains/user/utils/serverSchema')
+
+
+const SBBOL_AUTH_CONFIG = conf.SBBOL_AUTH_CONFIG ? JSON.parse(conf.SBBOL_AUTH_CONFIG) : {}
+const SBBOL_AUTH_CONFIG_EXTENDED = conf.SBBOL_AUTH_CONFIG_EXTENDED ? JSON.parse(conf.SBBOL_AUTH_CONFIG_EXTENDED) : {}
 
 /**
  * Changing of a client secret is performed daily for security reasons
@@ -13,6 +22,22 @@ const refreshSbbolClientSecret = createCronTask('refreshSbbolClientSecret', '0 1
     const sbbolSecretStorage = getSbbolSecretStorage()
     const sbbolSecretStorageExtended = getSbbolSecretStorage(true)
 
+    const { keystone: context } = getSchemaCtx('User')
+
+    const serviceUserId = get(SBBOL_AUTH_CONFIG, 'serviceUserId')
+    const serviceUserIdFromExtendedConfig = get(SBBOL_AUTH_CONFIG_EXTENDED, 'serviceUserId')
+
+    if (!serviceUserId) throw new Error('changeClientSecret: no SBBOL_AUTH_CONFIG.serviceUserId')
+    if (!serviceUserIdFromExtendedConfig) throw new Error('changeClientSecret: no SBBOL_AUTH_CONFIG_EXTENDED.serviceUserId')
+    if (serviceUserId !== serviceUserIdFromExtendedConfig) throw new Error('changeClientSecret: users from SBBOL_AUTH_CONFIG and SBBOL_AUTH_CONFIG_EXTENDED must be same')
+
+    const user = await User.getOne(context, { id: serviceUserId })
+    if (!user) {
+        throw new Error(`Not found service User with id=${serviceUserId} to change Client Secret for SBBOL integration`)
+    }
+
+    const { accessToken } = await getAccessTokenForUser(user.id)
+
     const { newClientSecret, clientSecretExpiration } = await changeClientSecret({
         clientId: sbbolSecretStorage.clientId,
         currentClientSecret: await sbbolSecretStorage.getClientSecret(),
@@ -20,6 +45,7 @@ const refreshSbbolClientSecret = createCronTask('refreshSbbolClientSecret', '0 1
             length: 8,
             numbers: true,
         }),
+        accessToken,
         useExtendedConfig: false,
     })
 
@@ -35,6 +61,7 @@ const refreshSbbolClientSecret = createCronTask('refreshSbbolClientSecret', '0 1
             length: 8,
             numbers: true,
         }),
+        accessToken,
         useExtendedConfig: true,
     })
 })
