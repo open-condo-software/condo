7f87fd46f64ae07f64f14a702d8c73af3e9d3d57
Alllex202 <56914444+Alllex202@users.noreply.github.com>
2024-07-01 12:15:48 +0500

    hotfix(condo): DOMA-9348 fixed run of task (#4901)

--- apps/condo/domains/common/components/tasks/TaskLauncher.ts
+++ apps/condo/domains/common/components/tasks/TaskLauncher.ts
@@ -1,3 +1,4 @@
+import get from 'lodash/get'
 import { useCallback, useContext, useState } from 'react'
 
 import { TasksContext } from './TasksContextProvider'
@@ -6,7 +7,7 @@ import { ITask, TaskRecord } from './index'
 
 type UseTaskLauncherOutputType = {
     loading: boolean
-    handleRunTask: (launchAttrs?: Record<string, any>) => void
+    handleRunTask: (params?: { taskAttrs?: Record<string, any> } & Record<string, any>) => void
 }
 type UseTaskLauncherType = (taskUIInterface: ITask, attrs: Record<string, any>) => UseTaskLauncherOutputType
 
@@ -38,8 +39,13 @@ export const useTaskLauncher: UseTaskLauncherType = (taskUIInterface, attrs) => 
         })
     })
 
-    const handleRunTask = useCallback((launchAttrs) => {
-        launchTask({ ...attrs, ...launchAttrs })
+    const handleRunTask = useCallback((params) => {
+        // NOTE: The "taskAttrs" property is used here because
+        // if "handleRunTask" is called in "onClick" (like "onClick={handleRunTask}"),
+        // then a lot of unnecessary properties will be passed, which are passed by "onClick".
+        // All these properties will be passed to the request - this should be avoided
+        const taskAttrs = get(params, 'taskAttrs')
+        launchTask({ ...attrs, ...taskAttrs })
     }, [launchTask, attrs])
 
     return { loading, handleRunTask }
