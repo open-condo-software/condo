18d7112ee701bc5179749b4fb63624af2bbf37cb
Mikhail Gorbunov <toplenboren@gmail.com>
2024-07-15 02:40:56 +0500

    feat(condo): DOMA-9613: Proof of work, add recipeints count to shared news items using custom query

--- apps/condo/domains/news/components/types.ts
+++ apps/condo/domains/news/components/types.ts
@@ -1,5 +1,5 @@
 import { NewsItemScope } from '@app/condo/schema'
 
-export type TNewsItemScopeNoInstance = Pick<NewsItemScope, 'property' | 'unitType' | 'unitName'>
+export type NewsItemScopeNoInstanceType = Pick<NewsItemScope, 'property' | 'unitType' | 'unitName'>
 
 export type TUnit = { unitType: string, unitName: string }
