52c6f5955d93bd3b3aaee196aaf0516a11aad7eb
Alllex202 <56914444+Alllex202@users.noreply.github.com>
2024-08-01 17:49:11 +0500

    refactor(condo): DOMA-8306 refactored mutation "getNewsItemsRecipientsCounters" (#5038)
    * refactor(condo): DOMA-8306 refactored mutation "getNewsItemsRecipientsCounters"
    
    * test(condo): DOMA-8306 updated timeout for one test only

--- apps/condo/domains/news/constants/common.js
+++ apps/condo/domains/news/constants/common.js
@@ -4,7 +4,7 @@ const conf = require('@open-condo/config')
 
 const SENDING_DELAY_SEC = Number(get(conf, 'NEWS_ITEMS_SENDING_DELAY_SEC', 15))
 const NEWS_SENDING_TTL_IN_SEC = Number(get(conf, 'NEWS_ITEM_SENDING_TTL_SEC', 60 * 30))
-const LOAD_RESIDENTS_CHUNK_SIZE = 20
+const LOAD_RESIDENTS_CHUNK_SIZE = 50
 
 const MESSAGE_TITLE_MAX_LEN = 50
 const MESSAGE_BODY_MAX_LEN = 150
