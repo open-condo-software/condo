3e37257e4228feac471c88a11ea513798d345914
Sitnikov Ivan <sit7602@gmail.com>
2024-08-02 14:56:35 +0500

    fix(condo): INFRA-296 temp rollback http logger(#5053)

--- packages/keystone/logging/getKeystonePinoOptions.js
+++ packages/keystone/logging/getKeystonePinoOptions.js
@@ -2,12 +2,15 @@ const { getLogger } = require('./getLogger')
 const { getReqLoggerContext } = require('./getReqLoggerContext')
 
 const logger = getLogger('http')
+const IGNORE_PATH = new Set(['/api/features', '/api/version', '/favicon.ico'])
 
 function getKeystonePinoOptions () {
     // NOTE(pahaz): https://github.com/pinojs/pino-http#pinohttpopts-stream
     return {
         logger,
-        autoLogging: false,
+        autoLogging: {
+            ignore: (req) => IGNORE_PATH.has(req.url),
+        },
         customProps: (req, res) => {
             return getReqLoggerContext(req)
         },
