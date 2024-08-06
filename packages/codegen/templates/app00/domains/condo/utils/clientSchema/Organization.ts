22574cfd7884a993806eeb01d28ad015e5faf59b
Pavel <52532264+nomerdvadcatpyat@users.noreply.github.com>
2024-08-06 16:16:04 +0500

    refactor(codegen): INFRA-316 update app00 template and add backend miniapp template (#4708)
    * refactor(codegen): INFRA-219 base update app template
    
    * refactor(codegen): INFRA-316 added backend app template
    
    * refactor(codegen): INFRA-316 added nosonar comments
    
    * fix(codegen): INFRA-316 update app types
    
    * fix(codegen): INFRA-316 move generateCondoSchema to codegen package
    
    * fix(codegen): INFRA-316 some fixes
    
    * fix(codegen): INFRA-316 update components in template
    
    * fix(codegen): INFRA-316 remove some redundant code, move generate.condo.server.utils to codegen
    
    * fix(codegen): INFRA-316 update yarn.lock
    
    * feat(condo): INFRA-316 added todo to move oidc logic to separate component
    
    * feat(condo): INFRA-316 fix tests in template
    
    * feat(condo): INFRA-316 fix tests in app01
    
    * fix(codegen): INFRA-316 some fixes
    
    * fix(codegen): INFRA-316 added info about prepare in README
    
    * feat(codegen): INFRA-316 update oidc middleware in backend app
    
    * feat(codegen): INFRA-316 use values for session headers from envs
    
    * feat(codegen): INFRA-316 change start route of backend miniapp

---
+++ packages/codegen/templates/app00/domains/condo/utils/clientSchema/Organization.ts
@@ -0,0 +1,24 @@
+import { Organization, QueryAllOrganizationsArgs } from '@app/{{name}}/condoSchema'
+
+import { generateReactHooks } from '@open-condo/codegen/generate.hooks'
+
+import { clientGql } from '@{{name}}/domains/condo/gql'
+
+
+type EmptyObjectTypePlaceHolder = {
+
+}
+
+const {
+    useObject,
+    useObjects,
+    useAllObjects,
+    useCount,
+} = generateReactHooks<Organization, EmptyObjectTypePlaceHolder, EmptyObjectTypePlaceHolder, QueryAllOrganizationsArgs>(clientGql.Organization)
+
+export {
+    useAllObjects,
+    useObjects,
+    useObject,
+    useCount,
+}
---
+++ packages/codegen/templates/app01/middlewares/oidc.js
@@ -0,0 +1,71 @@
+const express = require('express')
+const { get, isObject, isString } = require('lodash')
+const { generators } = require('openid-client')
+
+const { OIDCHelper } = require('@{{name}}/domains/condo/utils/oidcHelper')
+const { createOrUpdateUser } = require('@{{name}}/domains/user/utils/serverSchema/createOrUpdateUser')
+
+// TODO(DOMA-9342): move oidc logic to separate package
+class CondoOIDCMiddleware {
+    prepareMiddleware ({ keystone }) {
+        // this route can not be used for csrf attack (use oidc-client library to handle auth flows properly)
+        // nosemgrep: javascript.express.security.audit.express-check-csurf-middleware-usage.express-check-csurf-middleware-usage
+        const app = express()
+        const oidcSessionKey = 'oidc'
+        const redirectKey = 'next'
+        const helper = new OIDCHelper()
+
+        app.get('/oidc/auth', async (req, res, next) => {
+            const checks = { nonce: generators.nonce(), state: generators.state() }
+            req.session[oidcSessionKey] = checks
+
+            const nextUrl = get(req, 'query.next')
+            if (isString(nextUrl)) {
+                req.session[redirectKey] = nextUrl
+            }
+
+            await req.session.save()
+            try {
+                const redirectUrl = helper.getAuthorizationUrlWithParams(checks)
+                return res.redirect(redirectUrl)
+            } catch (err) {
+                await req.session.destroy()
+                return next(err)
+            }
+        })
+
+        app.get('/oidc/callback', async (req, res, next) => {
+            try {
+                const checks = req.session[oidcSessionKey]
+                const nextUrl = req.session[redirectKey] || '/'
+
+                if (!isObject(checks) || !checks) {
+                    await req.session.destroy()
+                    return res.status(400).send('ERROR: Invalid nonce and state')
+                }
+
+                const { userInfo } = await helper.completeAuth(req, checks)
+                const user = await createOrUpdateUser(keystone, userInfo)
+                await keystone._sessionManager.startAuthedSession(req, {
+                    item: { id: user.id },
+                    list: keystone.lists['User'],
+                })
+
+                delete req.session[oidcSessionKey]
+                await req.session.save()
+
+                return res.redirect(nextUrl)
+            } catch (err) {
+                return next(err)
+            }
+        })
+
+        app.set('trust proxy', true)
+
+        return app
+    }
+}
+
+module.exports = {
+    CondoOIDCMiddleware,
+}
