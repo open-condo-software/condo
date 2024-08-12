7211e38fa43864054121194f3f7198a953b056dc
AleX83Xpert <AleX83Xpert@mail.ru>
2024-08-12 09:02:08 +0500

    chore(condo): DOMA-9803 improve memory monitoring

--- packages/keystone/memMon/plugin.js
+++ packages/keystone/memMon/plugin.js
@@ -1,6 +1,8 @@
 const cuid = require('cuid')
 const { get } = require('lodash')
 
+const { normalizeQuery, normalizeVariables } = require('@open-condo/keystone/logging/normalize')
+
 const { logger } = require('./utils')
 
 /** @implements {import('apollo-server-plugin-base').ApolloServerPlugin} */
@@ -10,20 +12,20 @@ class ApolloMemMonPlugin {
         return {
             didResolveOperation: async (requestContext) => {
                 const operationId = get(requestContext, 'operationId') || cuid()
-                const mem = process.memoryUsage()
-                const operationName = get(requestContext, ['request', 'operationName'])
-                const query = get(requestContext, ['request', 'query'])
-                const variables = get(requestContext, ['request', 'variables'])
+                const { heapTotal, heapUsed } = process.memoryUsage()
+                const operationName = get(requestContext,  'operationName')
+                const query = normalizeQuery(get(requestContext, 'request.query'))
+                const variables = normalizeVariables(get(requestContext, 'request.variables'))
 
                 requestContext.operationId = operationId
 
-                logger.info({ msg: 'beforeQuery', operationId, operationName, query, variables, data: { mem } })
+                logger.info({ msg: 'beforeQuery', operationId, operationName, gql: { query, variables }, memoryUsage: { heapTotal, heapUsed } })
             },
             willSendResponse: async (requestContext) => {
-                const mem = process.memoryUsage()
-                const operationName = get(requestContext, ['request', 'operationName'])
+                const { heapTotal, heapUsed } = process.memoryUsage()
+                const operationName = get(requestContext,  'operationName')
 
-                logger.info({ msg: 'afterQuery', operationId: requestContext.operationId, operationName, data: { mem } })
+                logger.info({ msg: 'afterQuery', operationId: requestContext.operationId, operationName, memoryUsage: { heapTotal, heapUsed } })
             },
         }
     }
