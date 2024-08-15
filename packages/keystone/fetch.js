14559236bb30a6a93fc582e82ca59b9fbfefb696
Mikhail Gorbunov <toplenboren@gmail.com>
2024-08-15 18:07:28 +0500

    fix(condo): HOTFIX add users ability to skip tracing headers (#5113)

--- packages/keystone/fetch.js
+++ packages/keystone/fetch.js
@@ -89,7 +89,7 @@ const fetchWithRetriesAndLogger = async (url, options = {}) => {
         maxRetries = 0,
         abortRequestTimeout = 60 * 1000,
         timeoutBetweenRequests = 0,
-        setTracingHeaders = false,
+        skipTracingHeaders = false,
         ...fetchOptions
     } = options
     let retries = 0
@@ -101,7 +101,7 @@ const fetchWithRetriesAndLogger = async (url, options = {}) => {
             const controller = new AbortController()
             const signal = controller.signal
             const response = await Promise.race([
-                fetchWithLogger(url, { ... fetchOptions, signal }, { setTracingHeaders }),
+                fetchWithLogger(url, { ... fetchOptions, signal }, { skipTracingHeaders }),
                 new Promise((_, reject) =>
                     setTimeout(() => {
                         controller.abort()
