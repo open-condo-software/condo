e3e2dee7d2dc2ef52e2af635a8c7bcf5e0ce31a7
AleX83Xpert <AleX83Xpert@mail.ru>
2024-08-15 13:02:59 +0500

    refactor(address-service): DOMA-9952 add @babel/plugin-proposal-private-methods

--- apps/address-service/babel.config.js
+++ apps/address-service/babel.config.js
@@ -2,6 +2,9 @@ module.exports = function (api) {
     api.cache(true)
     return {
         'presets': ['next/babel'],
-        'plugins': ['@emotion'],
+        'plugins': [
+            '@emotion',
+            '@babel/plugin-proposal-private-methods',
+        ],
     }
 }
