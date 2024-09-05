e0c3af4f7204de660ada80c55df13f0a962115d2
Владимир <vladimir@vfadeev.local>
2024-09-05 19:18:42 +0500

    feat(condo): DOMA-9935 schema

--- apps/condo/schema.ts
+++ apps/condo/schema.ts
@@ -20357,6 +20357,18 @@ export enum CacheControlScope {
   Private = 'PRIVATE'
 }
 
+export type CalculateFeeForReceiptInput = {
+  receipt: BillingReceiptWhereUniqueInput;
+  amount: Scalars['String'];
+};
+
+export type CalculateFeeForReceiptOutput = {
+  __typename?: 'CalculateFeeForReceiptOutput';
+  amountWithoutExplicitFee: Scalars['String'];
+  explicitFee: Scalars['String'];
+  explicitServiceCharge: Scalars['String'];
+};
+
 /**  Conversation record between operator and client  */
 export type CallRecord = {
   __typename?: 'CallRecord';
@@ -66199,6 +66211,7 @@ export type Query = {
   exportPaymentsToExcel?: Maybe<ExportPaymentsToExcelOutput>;
   generatePaymentLink?: Maybe<GeneratePaymentLinkOutput>;
   _allPaymentsSum?: Maybe<PaymentsSumOutput>;
+  calculateFeeForReceipt?: Maybe<CalculateFeeForReceiptOutput>;
   ticketReportWidgetData?: Maybe<TicketReportWidgetOutput>;
   ticketAnalyticsReport?: Maybe<TicketAnalyticsReportOutput>;
   exportTicketAnalyticsToExcel?: Maybe<ExportTicketAnalyticsToExcelOutput>;
@@ -72878,6 +72891,11 @@ export type QueryGeneratePaymentLinkArgs = {
 
 export type Query_AllPaymentsSumArgs = {
   where: PaymentWhereInput;
+};
+
+
+export type QueryCalculateFeeForReceiptArgs = {
+  data: CalculateFeeForReceiptInput;
 };
 
 
