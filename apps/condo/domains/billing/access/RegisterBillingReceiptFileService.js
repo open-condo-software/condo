0c1b17dc690fb42986257228764037eb99941a5a
ekabardinsky <ekabardinsky@gmail.com>
2024-09-06 14:48:57 +0500

    feat(condo): INFRA-468 get back canRegisterBillingReceiptFile access logic

--- apps/condo/domains/billing/access/RegisterBillingReceiptFileService.js
+++ apps/condo/domains/billing/access/RegisterBillingReceiptFileService.js
@@ -1,5 +1,7 @@
 const { throwAuthenticationError } = require('@open-condo/keystone/apolloErrorFormatter')
+const { getById } = require('@open-condo/keystone/schema')
 
+const { checkBillingIntegrationsAccessRights } = require('@condo/domains/billing/utils/accessSchema')
 const { SERVICE } = require('@condo/domains/user/constants/common')
 
 async function canRegisterBillingReceiptFile ({ authentication: { item: user }, args: { data: { context: { id: contextId } } } } ) {
@@ -7,7 +9,15 @@ async function canRegisterBillingReceiptFile ({ authentication: { item: user }, 
     if (user.deletedAt) return false
     if (user.isAdmin) return true
 
-    return user.type === SERVICE
+    if (user.type === SERVICE) {
+        const context = await getById('BillingIntegrationOrganizationContext', contextId)
+        if (!context) {
+            return false
+        }
+        return await checkBillingIntegrationsAccessRights(user.id, [context.integration])
+    }
+
+    return false
 }
 
 /*
