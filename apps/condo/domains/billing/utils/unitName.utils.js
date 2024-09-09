880ab67cddffa09295def0851763917af484ed1c
YEgorLu <elukianchikov@doma.ai>
2024-09-09 10:24:39 +0500

    style(condo): DOMA-10067 moved from function expression to function declaration

--- apps/condo/domains/billing/utils/unitName.utils.js
+++ apps/condo/domains/billing/utils/unitName.utils.js
@@ -8,13 +8,22 @@ const SYMBOLS_TO_REMOVE_REGEXP = /[^\wа-яА-ЯёЁ]+/g
 const EMOJI_REGEXP = /[\u{1F300}-\u{1F6FF}\u{1F900}-\u{1F9FF}\u{1F680}-\u{1F6C5}\u{2600}-\u{26FF}\u{2700}-\u{27BF}\u{1F1E0}-\u{1F1FF}\u{1F004}\u{1F0CF}\u{1F18E}\u{1F191}-\u{1F19A}\u{1F201}-\u{1F251}\u{1F191}-\u{1F19A}]+/gu
 const DELIMITER_IN_RESULT = '-'
 
-const addSpaceBetweenWordsAndNumbers = (str) => str
-    .replace(NUMBER_REGEXP, ' $1 ')
+const UNIT_NAME_NORMALIZATION_STEPS = [
+    addSpaceBetweenWordsAndNumbers,
+    replaceUnwantedSymbolsWithDelimiter,
+    joinToOneLowercaseWordWithDelimiter,
+]
 
-const replaceUnwantedSymbolsWithDelimiter = (unitName) => unitName
-    .replace(SYMBOLS_TO_REPLACE_WITH_SPACE_REGEXP, DELIMITER_IN_RESULT)
-    .replace(EMOJI_REGEXP, DELIMITER_IN_RESULT)
-    .replace(SYMBOLS_TO_REMOVE_REGEXP, DELIMITER_IN_RESULT)
+function addSpaceBetweenWordsAndNumbers (str) {
+    return str.replace(NUMBER_REGEXP, ' $1 ')
+}
+
+function replaceUnwantedSymbolsWithDelimiter (unitName) {
+    return unitName
+        .replace(SYMBOLS_TO_REPLACE_WITH_SPACE_REGEXP, DELIMITER_IN_RESULT)
+        .replace(EMOJI_REGEXP, DELIMITER_IN_RESULT)
+        .replace(SYMBOLS_TO_REMOVE_REGEXP, DELIMITER_IN_RESULT)
+}
 
 /**
  * replaces spaces with "-", does lowercase and replaces symbols with english analogues
@@ -22,20 +31,16 @@ const replaceUnwantedSymbolsWithDelimiter = (unitName) => unitName
  * @param locale
  * @returns {string}
  */
-const joinToOneLowercaseWordWithDelimiter = (unitName, locale = conf.DEFAULT_LOCALE) => slugify(unitName, {
-    replacement: DELIMITER_IN_RESULT,
-    locale,
-    strict: true,
-    lower: true,
-})
+function joinToOneLowercaseWordWithDelimiter (unitName, locale = conf.DEFAULT_LOCALE) {
+    return slugify(unitName, {
+        replacement: DELIMITER_IN_RESULT,
+        locale,
+        strict: true,
+        lower: true,
+    })
+}
 
-const UNIT_NAME_NORMALIZATION_STEPS = [
-    addSpaceBetweenWordsAndNumbers,
-    replaceUnwantedSymbolsWithDelimiter,
-    joinToOneLowercaseWordWithDelimiter,
-]
-
-const normalizeUnitName = (unitName, locale = conf.DEFAULT_LOCALE) => {
+function normalizeUnitName (unitName, locale = conf.DEFAULT_LOCALE) {
     locale = locale.toLowerCase()
     return UNIT_NAME_NORMALIZATION_STEPS.reduce((unitName, step) =>
         step(unitName, locale),
