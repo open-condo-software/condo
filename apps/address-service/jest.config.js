81de57d4e4b54c077fa6c820da27a3c8c3e8744a
AleX83Xpert <AleX83Xpert@mail.ru>
2024-09-10 16:48:00 +0500

    refactor(condo): INFRA-475 fix address service spec config

--- apps/address-service/jest.config.js
+++ apps/address-service/jest.config.js
@@ -2,22 +2,38 @@ module.exports = {
     projects: [
         {
             testRunner: 'jasmine2',
+            displayName: 'main',
+            testEnvironment: 'jsdom',
+            testEnvironmentOptions: {
+                url: 'http://localhost:3000/',
+            },
+            testPathIgnorePatterns: ['/node_modules/', '/.next/', '/dist/', '/.kmigrator/', '/schema/'],
+            transform: {
+                '\\.[jt]sx?$': 'babel-jest',
+            },
+            setupFilesAfterEnv: [`${__dirname}/jest.setupSpec.js`],
+            // NOTE: need to pass uuid export syntax through babel
+            transformIgnorePatterns: ['/node_modules/(?!(uuid)/)'],
+        },
+        {
+            testRunner: 'jasmine2',
             displayName: 'schema',
             testEnvironment: 'node',
-            testMatch: [`${__dirname}/schema/**/*.test.js`, `${__dirname}/domains/**/schema/*.test.js`],
+            testMatch: [
+                `${__dirname}/schema/**/*.test.js`,
+                `${__dirname}/domains/**/schema/*.test.js`,
+            ],
             setupFilesAfterEnv: [`${__dirname}/jest.setupTest.js`],
             // NOTE: need to pass uuid export syntax through babel
             transformIgnorePatterns: ['/node_modules/(?!(uuid)/)'],
         },
         {
             testRunner: 'jasmine2',
-            displayName: 'main',
-            testEnvironment: 'jsdom',
-            testURL: 'http://localhost:3000/',
-            testPathIgnorePatterns: ['/node_modules/', '/.next/', '/dist/', '/.kmigrator/', '/schema/'],
-            transform: {
-                '\\.[jt]sx?$': 'babel-jest',
-            },
+            displayName: 'common spec',
+            testEnvironment: 'node',
+            testMatch: [
+                `${__dirname}/domains/common/utils/**/*.spec.js`,
+            ],
             setupFilesAfterEnv: [`${__dirname}/jest.setupSpec.js`],
             // NOTE: need to pass uuid export syntax through babel
             transformIgnorePatterns: ['/node_modules/(?!(uuid)/)'],
