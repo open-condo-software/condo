c6cfceb66a03e95d81947f1e2e7f8b08cd2bf742
Dmitry Kovyazin <dkoviazin@gmail.com>
2024-09-12 00:43:28 +0500

    fix(condo): revert AllResidentBillingReceiptsService

--- apps/condo/domains/billing/schema/AllResidentBillingReceiptsService.js
+++ apps/condo/domains/billing/schema/AllResidentBillingReceiptsService.js
@@ -3,33 +3,25 @@
  */
 
 const Big = require('big.js')
-const { pick, get, isNil, min } = require('lodash')
+const { pick, get, isNil } = require('lodash')
 
 const { generateQuerySortBy } = require('@open-condo/codegen/generate.gql')
 const { generateQueryWhereInput } = require('@open-condo/codegen/generate.gql')
 const FileAdapter = require('@open-condo/keystone/fileAdapter/fileAdapter')
 const { GQLCustomSchema, find } = require('@open-condo/keystone/schema')
 
-const { PAYMENT_DONE_STATUS, PAYMENT_WITHDRAWN_STATUS } = require('@condo/domains/acquiring/constants/payment')
-const { AcquiringIntegrationContext } = require('@condo/domains/acquiring/utils/serverSchema')
-const {
-    getAcquiringIntegrationContextFormulaByInstance,
-    FeeDistribution,
-} = require('@condo/domains/acquiring/utils/serverSchema/feeDistribution')
+const { getAcquiringIntegrationContextFormula, FeeDistribution } = require('@condo/domains/acquiring/utils/serverSchema/feeDistribution')
 const access = require('@condo/domains/billing/access/AllResidentBillingReceipts')
 const { BILLING_RECEIPT_FILE_FOLDER_NAME } = require('@condo/domains/billing/constants/constants')
+const { BillingReceiptAdmin, getPaymentsSum } = require('@condo/domains/billing/utils/serverSchema')
+const { normalizeUnitName } = require('@condo/domains/billing/utils/unitName.utils')
+const { Contact } = require('@condo/domains/contact/utils/serverSchema')
+
 const {
     BILLING_RECEIPT_RECIPIENT_FIELD_NAME,
     BILLING_RECEIPT_TO_PAY_DETAILS_FIELD_NAME,
     BILLING_RECEIPT_SERVICES_FIELD,
-} = require('@condo/domains/billing/constants/constants')
-const {
-    ResidentBillingReceiptAdmin,
-    getPaymentsSumByPayments,
-} = require('@condo/domains/billing/utils/serverSchema')
-const { normalizeUnitName } = require('@condo/domains/billing/utils/unitName.utils')
-const { Contact } = require('@condo/domains/contact/utils/serverSchema')
-
+} = require('../constants/constants')
 
 const Adapter = new FileAdapter(BILLING_RECEIPT_FILE_FOLDER_NAME)
 
@@ -47,7 +39,7 @@ const getFile = (receipt, contacts) => {
     }
     const accountUnitName = get(receipt, ['account', 'unitName'])
     const accountUnitType = get(receipt, ['account', 'unitType'])
-    const propertyAddress = get(receipt, ['account', 'property', 'address'])
+    const propertyAddress = get(receipt, ['property', 'address'])
 
     // let's search for a contact
     // if any exists = user allowed to see sensitive data
@@ -145,7 +137,7 @@ const AllResidentBillingReceiptsService = new GQLCustomSchema('AllResidentBillin
                     'OR': receiptsQuery,
                 }
 
-                const receiptsForConsumer = await ResidentBillingReceiptAdmin.getAll(
+                const receiptsForConsumer = await BillingReceiptAdmin.getAll(
                     context,
                     joinedReceiptsQuery,
                     {
@@ -162,33 +154,8 @@ const AllResidentBillingReceiptsService = new GQLCustomSchema('AllResidentBillin
                     deletedAt: null,
                 })
 
-                // cache billing receipt for calculation of is payable field
-                const minPeriod = min(receiptsForConsumer.map(item => item.period))
-                const nextPeriodBillingReceiptsCache = await find('BillingReceipt', {
-                    account: { id_in: [...new Set(receiptsForConsumer.map(item => item.account.id))], deletedAt: null },
-                    OR: [
-                        { receiver: { AND: [{ id_in: [...new Set(receiptsForConsumer.map(item => item.account.id))] }, { deletedAt: null } ] } },
-                        { category: { AND: [{ id_in: [...new Set(receiptsForConsumer.map(item => item.category.id))] }, { deletedAt: null } ] } },
-                    ],
-                    period_gt: minPeriod,
-                    deletedAt: null,
-                })
-
-                // cache acquiring integration contexts
-                const aicIds = serviceConsumersWithBillingAccount.map(consumer => consumer.acquiringIntegrationContext)
-                    .filter(aic => !isNil(aic))
-                const acquiringContexts = await AcquiringIntegrationContext.getAll(context, {
-                    id_in: aicIds,
-                })
-
                 receiptsForConsumer.forEach(receipt => {
                     const file = getFile(receipt, contacts)
-                    const isPayable = nextPeriodBillingReceiptsCache.filter(item => {
-                        return receipt.account.id === item.account && (
-                            receipt.category.id === item.category ||
-                            receipt.receiver.id === item.receiver
-                        ) && receipt.period < item.period
-                    }).length === 0
                     processedReceipts.push({
                         id: receipt.id,
                         dv: receipt.dv,
@@ -206,16 +173,8 @@ const AllResidentBillingReceiptsService = new GQLCustomSchema('AllResidentBillin
                             get(receipt, ['context', 'organization', 'id']) === organization ),
                         currencyCode: get(receipt, ['context', 'integration', 'currencyCode'], null),
                         file,
-                        isPayable,
+                        isPayable: receipt.isPayable,
                     })
-                })
-
-                // cache already paid payments
-                const paymentsCache = await  find('Payment', {
-                    organization: { id_in: [... new Set(processedReceipts.map(item => get(item, ['serviceConsumer', 'organization'])))] },
-                    accountNumber_in: [... new Set(processedReceipts.map(item => get(item, ['serviceConsumer', 'accountNumber'])))],
-                    period: minPeriod,
-                    status_in: [PAYMENT_DONE_STATUS, PAYMENT_WITHDRAWN_STATUS],
                 })
 
                 //
@@ -226,20 +185,19 @@ const AllResidentBillingReceiptsService = new GQLCustomSchema('AllResidentBillin
                     const organizationId = get(receipt.serviceConsumer, ['organization'])
                     const accountNumber = get(receipt.serviceConsumer, ['accountNumber'])
                     const billingCategory = get(receipt, ['category']) || {}
-                    const alreadyPaidPayments = paymentsCache.filter(payment => {
-                        return payment.organization === organizationId
-                            && payment.accountNumber === accountNumber
-                            && payment.period === get(receipt, 'period', null)
-                            && payment.recipientBic === get(receipt, ['recipient', 'bic'], null)
-                            && payment.recipientBankAccount === get(receipt, ['recipient', 'bankAccount'], null)
-                    })
-                    const paid = await getPaymentsSumByPayments(alreadyPaidPayments)
+                    const paid = await getPaymentsSum(
+                        context,
+                        organizationId,
+                        accountNumber,
+                        get(receipt, 'period', null),
+                        get(receipt, ['recipient', 'bic'], null),
+                        get(receipt, ['recipient', 'bankAccount'], null)
+                    )
                     const acquiringContextId = get(receipt, ['serviceConsumer', 'acquiringIntegrationContext'], null)
                     const toPay = get(receipt, ['toPay'], 0)
                     let fee = '0'
                     if (acquiringContextId) {
-                        const acquiringContext = acquiringContexts.find(item => item.id === acquiringContextId) || {}
-                        const formula = getAcquiringIntegrationContextFormulaByInstance(acquiringContext)
+                        const formula = await getAcquiringIntegrationContextFormula(context, acquiringContextId)
                         const feeCalculator = new FeeDistribution(formula, billingCategory.id)
                         const { explicitFee } = feeCalculator.calculate(Big(toPay).minus(Big(paid)).toFixed(2))
                         fee = String(explicitFee)
