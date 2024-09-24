3cc7c3ad5709460861d2e6507dee5d8862d1b751
abshnko <2009add@gmail.com>
2024-09-24 09:04:58 +0300

    refactor(condo): INFRA-496 fix importid constraint to not include deleted receipts

---
+++ apps/condo/migrations/20240920093948-0426_manual_billingreceipt_importid_constraint.js
@@ -0,0 +1,48 @@
+// KMIGRATOR:0409_manual_insert_meter_resource
+
+exports.up = async (knex) => {
+    await knex.raw(`
+    BEGIN;
+    --
+    -- [CUSTOM] Set Statement Timeout to some large amount - 25 min (25 * 60 => 1500 sec)
+    --
+    SET statement_timeout = '1500s';
+    --
+    -- Delete old (context, importId) constraint
+    --
+    ALTER TABLE "BillingReceipt" DROP CONSTRAINT "BillingReceipt_importId_9da6acbf_uniq";
+    --
+    -- Create new (context, importId) constraint where deletedAt is null
+    --
+    CREATE UNIQUE INDEX IF NOT EXISTS "BillingReceipt_context_importId" ON "BillingReceipt" ("context", "importId") WHERE "deletedAt" IS NULL;
+    --
+    -- [CUSTOM] Revert Statement Timeout to default amount - 10 secs
+    --
+    SET statement_timeout = '10s';
+    COMMIT;
+    `)
+}
+
+exports.down = async (knex) => {
+    await knex.raw(`
+    BEGIN;
+    --
+    -- [CUSTOM] Set Statement Timeout to some large amount - 25 min (25 * 60 => 1500 sec)
+    --
+    SET statement_timeout = '1500s';
+    --
+    -- Delete old (context, importId) constraint
+    --
+    ALTER TABLE "BillingReceipt" ADD CONSTRAINT "BillingReceipt_importId_9da6acbf_uniq" UNIQUE ("context", "importId");
+    --
+    -- Create new (context, importId) constraint where deletedAt is null
+    --
+    DROP INDEX IF EXISTS "BillingReceipt_context_importId";
+    --
+    -- [CUSTOM] Revert Statement Timeout to default amount - 10 secs
+    --
+    SET statement_timeout = '10s';
+    COMMIT;
+
+    `)
+}
