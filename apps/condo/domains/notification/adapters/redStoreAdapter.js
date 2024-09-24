71f8b629b3b004ab25bf0f65cd3da9730ead035d
VKislov <wkislov0@gmail.com>
2024-09-24 12:08:56 +0500

    Revert "fix(condo): DOMA-10033 add logging notificationRaw in redstore prepareBatchData"
    This reverts commit 25ae1193

--- apps/condo/domains/notification/adapters/redStoreAdapter.js
+++ apps/condo/domains/notification/adapters/redStoreAdapter.js
@@ -78,7 +78,7 @@ class RedStoreAdapter {
         const notification = RedStoreAdapter.validateAndPrepareNotification(notificationRaw)
         const notifications = [] // User can have many Remote Clients. Message is created for the user, so from 1 message there can be many notifications
         const pushContext = {}
-        console.log('notificationRaw', notificationRaw)
+
         tokens.forEach((pushToken) => {
             const pushType = pushTypes[pushToken] || PUSH_TYPE_DEFAULT
             const preparedData = prepareData(data, pushToken)
