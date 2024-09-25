7a4ae33caf157421af19306312ba734e4464e3ad
Pavel <nomerdvadcatpyat@gmail.com>
2024-09-25 16:51:10 +0500

    feat(condo): INFRA-535 added k6 test

--- apps/condo/k6/src/getAllOrganizationEmployees.test.ts
+++ apps/condo/k6/src/getAllOrganizationEmployees.test.ts
@@ -1,11 +1,11 @@
 import { check } from 'k6'
 
 import {
-    setupCondoAuth,
     createOrganization,
     createProperty,
+    getOrganizationEmployeeId,
     sendAuthorizedRequest,
-    getOrganizationEmployeeId, createTicket, createMeter,
+    setupCondoAuth,
 } from './utils'
 
 const DURATION = '60s'
@@ -13,8 +13,8 @@ const DURATION = '60s'
 export const options = {
     tags: { testid: 'tour', serverUrl: __ENV.BASE_URL },
     scenarios: {
-        syncTourSteps: {
-            exec: 'syncTourSteps',
+        getAllOrganizationEmployees: {
+            exec: 'getAllOrganizationEmployees',
             executor: 'constant-arrival-rate',
             duration: DURATION,
             rate: 5,
@@ -37,28 +37,28 @@ export function setup () {
     getOrganizationEmployeeId({ token, organizationId })
     const createdProperty = createProperty({ token, organizationId })
 
-    const data = {
+    return {
         token,
         organizationId,
         propertyId: createdProperty.json('data.obj.id'),
     }
-
-    for (let i = 0; i < 200; i++) {
-        createTicket(data)
-        createMeter(data)
-    }
-
-    return data
 }
 
-export function syncTourSteps (data) {
+const COMMON_FIELDS = 'id dv sender { dv fingerprint } v deletedAt newId createdBy { id name } updatedBy { id name } createdAt updatedAt'
+const ORGANIZATION_FIELDS = `{ country name type description avatar { publicUrl } meta tin features statusTransitions defaultEmployeeRoleStatusTransitions importId importRemoteSystem phone phoneNumberPrefix isApproved ${COMMON_FIELDS} }`
+const ORGANIZATION_EMPLOYEE_ROLE_FIELDS = `{ isEditable isDefault organization { id } name nameNonLocalized description descriptionNonLocalized statusTransitions canReadAnalytics canManageOrganization canManageCallRecords canDownloadCallRecords canReadEmployees canManageEmployees canInviteNewOrganizationEmployees canManageRoles canManageTicketPropertyHints canManageIntegrations canImportBillingReceipts canReadBillingReceipts canReadPayments canManageProperties canReadProperties canReadDocuments canManageDocuments canReadTickets canManageTickets canReadContacts canManageContacts canManageContactRoles canManageTicketComments canManagePropertyScopes canShareTickets canBeAssignedAsResponsible canBeAssignedAsExecutor canManageMeters canManageMeterReadings ticketVisibilityType canManageBankAccounts canManageBankContractorAccounts canManageBankIntegrationAccountContexts canManageBankIntegrationOrganizationContexts canManageBankTransactions canManageBankAccountReports canManageBankAccountReportTasks canManageBankAccountReports canReadIncidents canManageIncidents canReadNewsItems canManageNewsItems canManageNewsItemTemplates canManageMobileFeatureConfigs canManageB2BApps canReadMeters canReadSettings canReadExternalReports canReadServices canReadCallRecords canReadInvoices canManageInvoices canReadMarketItems canManageMarketItems canManageMarketItemPrices canReadMarketItemPrices canReadMarketPriceScopes canManageMarketPriceScopes canReadMarketplace canManageMarketplace canReadPaymentsWithInvoices canReadTour canManageTour canReadMarketSetting canManageMarketSetting canManageTicketAutoAssignments ${COMMON_FIELDS} }`
+const ORGANIZATION_EMPLOYEE_FIELDS = `{ organization ${ORGANIZATION_FIELDS} user { id name } name email phone role ${ORGANIZATION_EMPLOYEE_ROLE_FIELDS} hasAllSpecializations isRejected isAccepted isBlocked id dv sender { dv fingerprint } v createdBy { id name } updatedBy { id name } position createdAt deletedAt updatedAt }`
+
+export function getAllOrganizationEmployees (data) {
     const payload = {
-        operationName: 'syncTourSteps',
-        query: 'mutation syncTourSteps($data: SyncTourStepsInput!) {result: syncTourSteps(data: $data) {ok}}',
+        operationName: 'getAllOrganizationEmployees',
+        query: `
+          query getAllOrganizationEmployees($where: OrganizationEmployeeWhereInput) {
+            objs: allOrganizationEmployees(where: $where) ${ORGANIZATION_EMPLOYEE_FIELDS}
+          }
+        `,
         variables: {
-            data: {
-                dv: 1,
-                sender: { dv: 1, fingerprint: 'k6-load-test' },
+            where: {
                 organization: { id: data.organizationId },
             },
         },
@@ -66,7 +66,6 @@ export function syncTourSteps (data) {
     const response = sendAuthorizedRequest(data, payload)
 
     check(response, {
-        'syncTourSteps status is 200': (res) => res.status === 200,
-        'syncTourSteps': (res) => res.json('result.ok') === 'true',
+        'getAllOrganizationEmployees status is 200': (res) => res.status === 200,
     })
 }
