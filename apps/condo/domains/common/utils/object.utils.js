e2bb7d67455b63cddb6c229e85cb4cc2cd69cb24
YEgorLu <elukianchikov@doma.ai>
2024-09-26 15:37:57 +0500

    fix(condo): try fix quality gate issue

--- apps/condo/domains/common/utils/object.utils.js
+++ apps/condo/domains/common/utils/object.utils.js
@@ -8,7 +8,8 @@ function canonicalize (value) {
         return `[${value.map(canonicalize).join(',')}]`
     } else if (value !== null && value !== undefined && typeof value === 'object') {
         const keys = Object.keys(value).sort()
-        return `{${keys.map(key => `"${key}":${canonicalize(value[key])}`).join(',')}}`
+        const serializedKeyValues = keys.map(key => `"${key}":${canonicalize(value[key])}`).join(',')
+        return `{${serializedKeyValues}}`
     } else {
         return JSON.stringify(value)
     }
