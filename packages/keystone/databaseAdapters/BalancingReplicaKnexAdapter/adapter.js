8be3531267ffa63f688a6dcccfd6104c07de8313
SavelevMatthew <savelevmatthew@gmail.com>
2024-10-03 01:53:48 +0500

    feat(keystone): INFRA-446 DATABASE_POOLS parser added

--- packages/keystone/databaseAdapters/BalancingReplicaKnexAdapter/adapter.js
+++ packages/keystone/databaseAdapters/BalancingReplicaKnexAdapter/adapter.js
@@ -2,11 +2,14 @@ const { KnexAdapter } = require('@keystonejs/adapter-knex')
 
 const conf = require('@open-condo/config')
 
+const { getNamedDBs } = require('./utils/env')
+
+
 class BalancingReplicaKnexAdapter extends KnexAdapter {
     constructor ({ databaseUrl, ...baseKnexConfig }) {
         super(baseKnexConfig)
 
-        this.databaseUrl = databaseUrl || conf['DATABASE_URL']
-        this
+        this._dbConnections = getNamedDBs(databaseUrl || conf['DATABASE_URL'])
+        this._replicaPoolsConfig = 1 // TODO
     }
 }
