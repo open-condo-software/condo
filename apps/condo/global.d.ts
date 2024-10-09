12bb2749cd1b3dca09b862a96cf296b0a3e8ed68
Alllex202 <alllex.202@gmail.com>
2024-10-09 23:30:42 +0500

    feat(condo): INFRA-574 added types

--- apps/condo/global.d.ts
+++ apps/condo/global.d.ts
@@ -1,3 +1,4 @@
+import { GetActiveOrganizationEmployeeQuery, AuthenticatedUserQuery } from '@app/condo/gql'
 import en from '@app/condo/lang/en/en.json'
 import ru from '@app/condo/lang/ru/ru.json'
 
@@ -6,11 +7,30 @@ import ru from '@app/condo/lang/ru/ru.json'
 const translations = [en, ru] as const
 type MessagesKeysType = keyof typeof translations[number]
 
-// NOTE: Override global interface allows us to use autocomplete in intl
+type LinkExtendsType = GetActiveOrganizationEmployeeQuery['employees'][number]
+type OrganizationExtendsType = GetActiveOrganizationEmployeeQuery['employees'][number]['organization']
+type UserExtendsType = AuthenticatedUserQuery['authenticatedUser']
+
 declare global {
+    // NOTE: Override global interface allows us to use autocomplete in intl
     namespace FormatjsIntl {
         interface Message {
             ids: MessagesKeysType
         }
+    }
+
+    // NOTE: Override global interface allows us to use autocomplete in useOrganization/useAuth from '@open-condo/next'
+    namespace OpenCondoNext {
+        // eslint-disable-next-line @typescript-eslint/no-empty-interface
+        interface GetActiveOrganizationEmployeeQueryType extends GetActiveOrganizationEmployeeQuery {}
+
+        // eslint-disable-next-line @typescript-eslint/no-empty-interface
+        interface LinkType extends LinkExtendsType {}
+
+        // eslint-disable-next-line @typescript-eslint/no-empty-interface
+        interface OrganizationType extends OrganizationExtendsType {}
+
+        // eslint-disable-next-line @typescript-eslint/no-empty-interface
+        interface UserType extends UserExtendsType {}
     }
 }
