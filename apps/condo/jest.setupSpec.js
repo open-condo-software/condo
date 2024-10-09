f00109d4f7cdb6ec1adc140d7e7f34b06ae85ec0
pahaz <pahaz.white@gmail.com>
2024-10-09 14:53:39 +0300

    chore(condo): DOMA-10232 use the same timeout for Specs as for Tests

--- apps/condo/jest.setupSpec.js
+++ apps/condo/jest.setupSpec.js
@@ -3,6 +3,8 @@ const { get } = require('lodash')
 
 const EXTRA_LOGGING = falsey(process.env.DISABLE_LOGGING)
 
+jest.setTimeout(60000)
+
 // Patch tests to include their own name
 jasmine.getEnv().addReporter({ // eslint-disable-line
     specStarted: result => jasmine.currentTest = result, // eslint-disable-line
@@ -11,5 +13,5 @@ jasmine.getEnv().addReporter({ // eslint-disable-line
 
 if (EXTRA_LOGGING) {
     beforeEach(() => console.log(`[TEST BEGIN][${get(jasmine, ['currentTest', 'fullName'], jasmine['testPath'].split('/').pop().split('.')[0])}]`)) // eslint-disable-line
-    afterEach(() => console.log(`[TEST END][${get(jasmine, ['currentTest', 'fullName'], jasmine['testPath'].split('/').pop().split('.')[0])}]`)) // eslint-disable-line
+    afterEach(() => console.log(`[TEST END][${get(jasmine, ['currentTest', 'fullName'], jasmine['testPath'].split('/').pop().split('.')[0])}]`, get(jasmine, ['currentTest', 'failedExpectations'], []).map(x => x.stack))) // eslint-disable-line
 }
