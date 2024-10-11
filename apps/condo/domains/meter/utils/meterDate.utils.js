98a1c7e73011856a556635d5460395064ecf149a
YEgorLu <elukianchikov@doma.ai>
2024-10-11 23:58:27 +0500

    fix(condo): DOMA-9422 fix invalid datestrings with time-zone offsets parsing

--- apps/condo/domains/meter/utils/meterDate.utils.js
+++ apps/condo/domains/meter/utils/meterDate.utils.js
@@ -11,7 +11,7 @@ dayjs.extend(utc)
 const SYMBOLS_TO_CUT_FROM_DATES_REGEXP = /[^+\-TZ_:.,( )/\d]/gi // -_:.,()/ recognizable by dayjs https://day.js.org/docs/en/parse/string-format
 const DATE_WITH_OFFSET_REGEXP = /([T\s]\d{2}:\d{2}:\d{2}(\.\d{1,3})?)[+-](\d{2}):?(\d{2})$/ // +5000 | +50:00
 const UTC_STRING_FORMAT = 'YYYY-MM-DDTHH:mm:ss.SSS[Z]'
-const DEFAULT_PARSING_OPTIONS = { formats: DATE_PARSING_FORMATS, utc: true, offsets: true }
+const DEFAULT_PARSING_OPTIONS = { formats: DATE_PARSING_FORMATS, offsets: true }
 
 /**
  * Clear bad user input
@@ -38,7 +38,7 @@ function clearDateStr (dateStr) {
  * @param {string} dateStr
  * @param options - change default formats, disable utc or offset formats
  * @param {Array<string>?} options.formats - date parsing formats
- * @param {boolean?} options.offsets - process date with offsets or not
+ * @param {boolean?} options.offsets - enable check for dates like 2024-01-20T00:00:00 with +00:00 or +0000
  * @return {boolean}
  */
 function isDateStrValid (dateStr, options = DEFAULT_PARSING_OPTIONS) {
@@ -46,16 +46,27 @@ function isDateStrValid (dateStr, options = DEFAULT_PARSING_OPTIONS) {
         return false
     }
 
-    options = { DEFAULT_PARSING_OPTIONS, ...options }
+    options = { ...DEFAULT_PARSING_OPTIONS, ...options }
 
     if (options.offsets && DATE_WITH_OFFSET_REGEXP.test(dateStr)) {
-        // remove +HH:mm | +HHmm part, it does not matter
+        // we need to pass dateStr to dayjs in strict mode to check valid values in MM, DD, HH, mm...
+        // but dayjs can not parse this format strictly
+        // so lets manually drop offsets, because they are irrelevant, add utc ending and check validity
+
         const maybeSemicolon = dateStr[dateStr.length - 3]
         let offsetStartIndex = dateStr.length - 5
         if (maybeSemicolon === ':') {
             offsetStartIndex = dateStr.length - 6
         }
         dateStr = dateStr.substring(0, offsetStartIndex)
+
+        const dateStrHasMilliseconds = dateStr[dateStr.length - 4] === '.'
+        if (dateStrHasMilliseconds) {
+            dateStr += 'Z'
+        } else {
+            dateStr += '.000Z'
+        }
+        return dayjs(dateStr, UTC_STRING_FORMAT, true).isValid()
     }
 
     return dayjs(dateStr, options.formats, true).isValid()
