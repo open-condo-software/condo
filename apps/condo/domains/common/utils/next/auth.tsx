00935be6a47d0b12f17c3f0d3944fb755c61807a
Alllex202 <alllex.202@gmail.com>
2024-10-14 21:52:11 +0500

    fix(condo): INFRA-574 fixed types

--- apps/condo/domains/common/utils/next/auth.tsx
+++ apps/condo/domains/common/utils/next/auth.tsx
@@ -1,18 +1,29 @@
 import { ApolloClient, NormalizedCacheObject } from '@apollo/client'
-import { AuthenticatedUserQuery, AuthenticatedUserDocument } from '@app/condo/gql'
-import { GetServerSideProps } from 'next'
+import { AuthenticatedUserQuery, AuthenticatedUserDocument, AuthenticatedUserQueryResult } from '@app/condo/gql'
+import { GetServerSideProps, GetServerSidePropsContext } from 'next'
 
 
-export async function prefetchAuth (client: ApolloClient<NormalizedCacheObject>) {
+type PrefetchAuthType = (client: ApolloClient<NormalizedCacheObject>) => Promise<AuthenticatedUserQueryResult['data']['authenticatedUser']>
+
+export const prefetchAuth: PrefetchAuthType = async (client) => {
     const response = await client.query<AuthenticatedUserQuery>({
         query: AuthenticatedUserDocument,
     })
 
-    return response.data.authenticatedUser
+    return response?.data?.authenticatedUser || null
 }
 
-export async function prefetchAuthOrRedirect (client: ApolloClient<NormalizedCacheObject>, context: Parameters<GetServerSideProps>[0]) {
-    const result: { user: Awaited<ReturnType<typeof prefetchAuth>> | null, redirect: null | Awaited<ReturnType<GetServerSideProps>> } = { user: null, redirect: null }
+
+type Redirect = { user: null, redirect: Awaited<ReturnType<GetServerSideProps>> }
+type AuthData = { user: Awaited<ReturnType<typeof prefetchAuth>>, redirect: null }
+type RedirectOrAuthDataType = Redirect | AuthData
+type PrefetchAuthOrRedirectType = (
+    client: ApolloClient<NormalizedCacheObject>,
+    context: GetServerSidePropsContext
+) => Promise<RedirectOrAuthDataType>
+
+export const prefetchAuthOrRedirect: PrefetchAuthOrRedirectType = async (client, context) =>{
+    const result: RedirectOrAuthDataType = { user: null, redirect: null }
     const user = await prefetchAuth(client)
 
     if (user) {
