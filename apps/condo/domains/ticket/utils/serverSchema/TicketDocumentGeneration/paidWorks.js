78079e4ebd1ea802c3f7bde63dcf4289f6cde4c1
Viktor Tolmachev <tolmach0221@yandex.ru>
2024-10-14 15:02:50 +0500

    feat(condo): DOMA-10397 four commit

--- apps/condo/domains/ticket/utils/serverSchema/TicketDocumentGeneration/paidWorks.js
+++ apps/condo/domains/ticket/utils/serverSchema/TicketDocumentGeneration/paidWorks.js
@@ -1,6 +1,7 @@
 const dayjs = require('dayjs')
 const { get } = require('lodash')
 
+const { FinanceInfoClient } = require('@open-condo/clients/finance-info-client/FinanceInfoClient')
 const { getById, getByCondition } = require('@open-condo/keystone/schema')
 
 const { buildExportFile, DOCX_FILE_META } = require('@condo/domains/common/utils/createExportFile')
@@ -9,8 +10,8 @@ const { normalizeTimeZone } = require('@condo/domains/common/utils/timezone')
 const { DEFAULT_INVOICE_CURRENCY_CODE, INVOICE_STATUS_CANCELED } = require('@condo/domains/marketplace/constants')
 const { Invoice } = require('@condo/domains/marketplace/utils/serverSchema')
 const { DEFAULT_ORGANIZATION_TIMEZONE } = require('@condo/domains/organization/constants/common')
-const { getAddressDetails } = require('@condo/domains/property/utils/serverSchema/helpers')
 const { TICKET_DOCUMENT_GENERATION_TASK_FORMAT } = require('@condo/domains/ticket/constants/ticketDocument')
+const { CONTEXT_FINISHED_STATUS } = require('@condorb/domains/condorb/constants')
 
 
 const DATE_FORMAT = 'DD.MM.YYYY'
@@ -47,14 +48,26 @@ const renderMoney = (amount, currencyCode, locale) => {
 
 
 const generateTicketDocumentOfPaidWorks = async ({ task, baseAttrs, context, locale, ticket, organization }) => {
+    const dadata = new FinanceInfoClient()
+    const { psrn } = await dadata.getOrganization(organization.tin)
     const { format, timeZone: timeZoneFromUser } = task
 
     const timeZone = normalizeTimeZone(timeZoneFromUser) || DEFAULT_ORGANIZATION_TIMEZONE
     const printDate = dayjs().tz(timeZone).locale(locale)
 
-    const property = await getById('Property', ticket.property)
-    const { renderData, streetPart, settlement } = getAddressDetails(get(property, 'addressMeta', ticket.propertyAddressMeta))
-
+    const billingOrganizationContext = organization.id
+        ? await getByCondition('BillingIntegrationOrganizationContext', {
+            organization: { id: organization.id },
+            deletedAt: null,
+            status: CONTEXT_FINISHED_STATUS,
+        })
+        : null
+    const property = billingOrganizationContext.id
+        ? await getByCondition('BillingProperty', {
+            context: { id: billingOrganizationContext.id },
+            deletedAt: null,
+        })
+        : null
     const contact = ticket.contact
         ? await getById('Contact', ticket.contact)
         : null
@@ -73,10 +86,9 @@ const generateTicketDocumentOfPaidWorks = async ({ task, baseAttrs, context, loc
     }, {
         sortBy: ['createdAt_ASC'],
     })
-    console.log('This is invoices 1', invoices)
 
     const currencyCode = get(invoices, '0.currencyCode') || DEFAULT_INVOICE_CURRENCY_CODE
-    let totalSum = 0, totalVAT = 0
+    let totalSum = 0, totalVAT = 0, totalSumInWords = null, totalVATInWords = null
 
     const listOfWorks = invoices.reduce((acc, invoice) => {
         const rows = Array.isArray(invoice.rows) ? invoice.rows : []
@@ -96,11 +108,11 @@ const generateTicketDocumentOfPaidWorks = async ({ task, baseAttrs, context, loc
 
             return {
                 name: row.name || '',
-                index: index + 1 || '', 
+                index: index + 1 || '',
                 count: String(row.count) || '',
                 price: !Number.isNaN(price) ? renderPrice : '',
                 sum: !Number.isNaN(price) ? renderSum : '',
-                vat:  !Number.isNaN(price) ? renderVat : '',
+                vat: !Number.isNaN(price) ? renderVat : '',
                 total: !Number.isNaN(price) ? renderTotalPrice : '',
             }
         }))
@@ -116,14 +128,14 @@ const generateTicketDocumentOfPaidWorks = async ({ task, baseAttrs, context, loc
             numberTicket: ticket.number || '',
         },
         property: {
-            fullAddress: [renderData, streetPart].filter(Boolean).join(' '),
-            address: [renderData, settlement].filter(Boolean).join(' '),
+            address: get(property, 'normalizedAddress'),
         },
         client: {
             name: get(contact, 'name') || '',
         },
         company: {
             name: get(organization, 'name') || '',
+            psrn: psrn || '',
         },
         executor: {
             name: get(employee, 'name') || '',
@@ -142,6 +154,8 @@ const generateTicketDocumentOfPaidWorks = async ({ task, baseAttrs, context, loc
             totalSum: totalSum,
             totalVAT: totalVAT,
             totalSumWithVAT: totalSumWithVAT,
+            totalSumInWords: totalSumInWords,
+            totalVATInWords: totalVATInWords,
         },
     }
 
