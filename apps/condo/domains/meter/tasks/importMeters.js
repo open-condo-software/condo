fe63567549131d0079ff3084cb0bd5f6a3f1a291
Pavel <nomerdvadcatpyat@gmail.com>
2024-10-17 20:12:15 +0300

    refactor(condo): INFRA-576 fix fields in importMeters

--- apps/condo/domains/meter/tasks/importMeters.js
+++ apps/condo/domains/meter/tasks/importMeters.js
@@ -78,13 +78,13 @@ async function failWithErrorFile (context, taskId, content, format) {
  */
 async function importMeters (taskId) {
     if (!taskId) throw new Error('taskId is undefined')
-    const { keystone: context } = await getSchemaCtx('MeterReadingsImportTask')
+    const { keystone: context } = getSchemaCtx('MeterReadingsImportTask')
 
     // get task definition
     const { file, user, organization, locale } = await MeterReadingsImportTask.getOne(
         context,
         { id: taskId },
-        'file { id originalFilename publicUrl mimetype } user { id } organization { id } locale'
+        'file { id originalFilename filename publicUrl mimetype } user { id } organization { id } locale'
     )
 
     // since we would like to catch all errors and immediately tell to user about them
