c5e65374a20c51647d678d38214251786e9c77c3
Mikhail Gorbunov <toplenboren@gmail.com>
2024-11-11 19:48:49 +0500

    chore(condo): fix ui affix jumping

--- packages/ui/src/components/ActionBar/actionBar.tsx
+++ packages/ui/src/components/ActionBar/actionBar.tsx
@@ -6,6 +6,7 @@ import { Typography } from '../Typography'
 
 const ACTION_BAR_CLASS_PREFIX = 'condo-affix'
 const AFFIX_CONTENT_WRAPPER_CLASS = `${ACTION_BAR_CLASS_PREFIX}-content-wrapper`
+const ACTION_BAR_PLACEHOLDER_CLASS = `${ACTION_BAR_CLASS_PREFIX}-placeholder`
 
 const SPACE_SIZE: SpaceProps['size'] = [16, 16]
 
@@ -19,11 +20,14 @@ export const ActionBar: React.FC<ActionBarProps> = (props) => {
     const { actions, message, wrap = true } = props
 
     return (
-        <Affix offsetBottom={0} prefixCls={ACTION_BAR_CLASS_PREFIX}>
-            <Space wrap={wrap} size={SPACE_SIZE} className={AFFIX_CONTENT_WRAPPER_CLASS}>
-                {message && <Typography.Text strong>{message}</Typography.Text>}
-                {actions}
-            </Space>
-        </Affix>
+        <>
+            <Affix offsetBottom={24} prefixCls={ACTION_BAR_CLASS_PREFIX}>
+                <Space wrap={wrap} size={SPACE_SIZE} className={AFFIX_CONTENT_WRAPPER_CLASS}>
+                    {message && <Typography.Text strong>{message}</Typography.Text>}
+                    {actions}
+                </Space>
+            </Affix>
+            <div className={ACTION_BAR_PLACEHOLDER_CLASS} />
+        </>
     )
 }
