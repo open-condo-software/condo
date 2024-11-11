ed74e51ae0b80bec861e8404497d57c821828b37
Pavel White <pahaz.white@gmail.com>
2024-11-11 20:45:08 +0300

    fix(condo): close #5471 no more useless warnings (#5472)

--- apps/condo/domains/common/components/icons/ClientRenderedIcon.tsx
+++ apps/condo/domains/common/components/icons/ClientRenderedIcon.tsx
@@ -7,6 +7,7 @@ interface IClientRenderedIconProps {
     iconProps?: IconProps
 }
 
+/** @deprecated you don't need to use it! we will remove it in a future */
 export const ClientRenderedIcon: React.FC<IClientRenderedIconProps> = ({ icon: Icon, iconProps = {} }) => {
     if (typeof window === 'undefined') return null
     return <Icon {...iconProps}/>
