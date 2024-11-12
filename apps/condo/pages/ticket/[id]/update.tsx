9c26c93c25bb19c44d8a7972b13dedcb8c583518
Pavel <nomerdvadcatpyat@gmail.com>
2024-11-12 21:42:38 +0500

    refactor(condo): INFRA-643 added prefetch on contact and ticket pages

--- apps/condo/pages/ticket/[id]/update.tsx
+++ apps/condo/pages/ticket/[id]/update.tsx
@@ -1,6 +1,5 @@
 import { Typography, Row, Col } from 'antd'
 import Head from 'next/head'
-import { useRouter } from 'next/router'
 import React from 'react'
 
 import { useIntl } from '@open-condo/next/intl'
@@ -9,12 +8,12 @@ import { PageContent, PageWrapper } from '@condo/domains/common/components/conta
 import { PageComponentType } from '@condo/domains/common/types'
 import { TicketReadAndManagePermissionRequired } from '@condo/domains/ticket/components/PageAccess'
 import { TicketForm } from '@condo/domains/ticket/components/TicketForm'
+import { prefetchTicket } from '@condo/domains/ticket/utils/next/Ticket'
 
 
-const TicketUpdatePage: PageComponentType = () => {
+const TicketUpdatePage: PageComponentType<{ id: string }> = ({ id }) => {
     const intl = useIntl()
     const PageTitleMsg = intl.formatMessage({ id:'pages.condo.ticket.index.EditTicketModalTitle' })
-    const { query } = useRouter()
 
     return (
         <>
@@ -27,7 +26,7 @@ const TicketUpdatePage: PageComponentType = () => {
                         <Col span={24}>
                             <Typography.Title level={1} style={{ margin: 0 }}>{PageTitleMsg}</Typography.Title>
                         </Col>
-                        <TicketForm id={query.id as string}/>
+                        <TicketForm id={id}/>
                     </Row>
                 </PageContent>
             </PageWrapper>
@@ -36,5 +35,18 @@ const TicketUpdatePage: PageComponentType = () => {
 }
 
 TicketUpdatePage.requiredAccess = TicketReadAndManagePermissionRequired
+
+TicketUpdatePage.getPrefetchedData = async ({ context, apolloClient }) => {
+    const { query } = context
+    const { id: ticketId } = query as { id: string }
+
+    await prefetchTicket({ client: apolloClient, ticketId })
+
+    return {
+        props: {
+            id: ticketId,
+        },
+    }
+}
 
 export default TicketUpdatePage
