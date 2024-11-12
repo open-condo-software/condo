656b70ef739e097f97e26bf7093847a2766bd03b
Dmitry Koviazin <dkovyazin@doma.ai>
2024-11-12 16:44:39 +0500

    feat(global): INFRA-791 add script for redis key migration & custom redis adapter

--- packages/keystone/redis.js
+++ packages/keystone/redis.js
@@ -1,11 +1,50 @@
 const IORedis = require('ioredis')
+const { get } = require('lodash')
 
 const conf = require('@open-condo/config')
 
 const { getLogger } = require('./logging')
 
+const READONLY_COMMANDS = ['bitcount', 'bitfield_ro', 'bitpos', 'dbsize', 'dump', 'eval_ro', 'evalsha_ro', 'exists', 'expiretime', 'fcall_ro', 'geodist', 'geohash', 'geopos', 'georadius_ro', 'georadiusbymember_ro', 'geosearch', 'get', 'getbit', 'getrange', 'hexists', 'hget', 'hgetall', 'hkeys', 'hlen', 'hmget', 'hrandfield', 'hscan', 'hstrlen', 'hvals', 'keys', 'lcs', 'lindex', 'llen', 'lolwut', 'lpos', 'lrange', 'mget', 'pexpiretime', 'pfcount', 'pttl', 'randomkey', 'scan', 'scard', 'sdiff', 'sinter', 'sintercard', 'sismember', 'smembers', 'smismember', 'sort_ro', 'srandmember', 'sscan', 'strlen', 'substr', 'sunion', 'touch', 'ttl', 'type', 'xlen', 'xpending', 'xrange', 'xread', 'xrevrange', 'zcard', 'zcount', 'zdiff', 'zinter', 'zintercard', 'zlexcount', 'zmscore', 'zrandmember', 'zrange', 'zrangebylex', 'zrangebyscore', 'zrank', 'zrevrange', 'zrevrangebylex', 'zrevrangebyscore', 'zrevrank', 'zscan', 'zscore', 'zunion']
+
+
+const PREFIX = process.cwd().split('/').pop() + ':'
 const REDIS_CLIENTS = {}
+
 const logger = getLogger('redis')
+
+if (get(conf, 'REDIS_FALLBACK_ENABLED', 'false') === 'true') {
+    const originalSendCommand = IORedis.prototype.sendCommand
+    IORedis.prototype.sendCommand = function (...args) {
+        const [command] = args
+
+        if (!READONLY_COMMANDS.includes(command.name)) {
+            return originalSendCommand.apply(this, args)
+        }
+
+        const prefixedKey = command.args[0]
+
+        return new Promise((resolve, reject) => {
+            originalSendCommand.apply(this, args).then((value) => {
+                if (value !== null || !prefixedKey.startsWith(PREFIX)) {
+                    resolve(value)
+                } else {
+                    const nonPrefixedKey = prefixedKey.substring(PREFIX.length)
+
+                    const otherCommand = new IORedis.Command(command.name, [nonPrefixedKey], 'utf-8')
+
+                    originalSendCommand.apply(this, [otherCommand]).then((result) => {
+                        if (result instanceof Buffer) {
+                            resolve(result.toString('utf8'))
+                        }
+                        resolve(result)
+
+                    }).catch(reject)
+                }
+            }).catch(reject)
+        })
+    }
+}
 
 /**
  * If you really need to use Redis then you should use this function!
@@ -29,7 +68,7 @@ function getRedisClient (name = 'default', purpose = 'regular', opts = {}) {
         if (!redisUrl) throw new Error(`No REDIS_URL env! You need to set ${redisEnvName} / REDIS_URL env`)
         // BUILD STEP! OR SOME CASE WITH REDIS_URL=undefined
         if (redisUrl === 'undefined') return undefined
-        const client = new IORedis(redisUrl, { connectionName: clientKey, ...opts })
+        const client = new IORedis(redisUrl, { connectionName: clientKey, keyPrefix: PREFIX, ...opts })
         client.on('connect', () => logger.info({ msg: 'connect', clientKey }))
         client.on('close', () => logger.info({ msg: 'close', clientKey }))
         client.on('reconnecting', (waitTime) => logger.info({ msg: 'reconnecting', clientKey, waitTime }))
