5d264b75f32c5efce5f447ccb26122a3e4a461fe
Alllex202 <alllex.202@gmail.com>
2024-11-14 14:48:41 +0500

    refactor(condo): INFRA-574 refactored cookie utils (update submodule)
    refactor(condo): INFRA-574 refactored cookie utils

--- apps/condo/domains/common/utils/next/organization.tsx
+++ apps/condo/domains/common/utils/next/organization.tsx
@@ -6,45 +6,11 @@ import {
     GetActiveOrganizationEmployeeQueryResult,
 } from '@app/condo/gql'
 import { getCookie, setCookie, deleteCookie } from 'cookies-next'
-import cookie from 'js-cookie'
 import get from 'lodash/get'
 import { NextPageContext } from 'next'
 
-import { isSSR } from '@open-condo/miniapp-utils'
-
 
 export const ACTIVE_EMPLOYEE_COOKIE_NAME = 'organizationLinkId'
-
-function getCookieEmployeeId (context: NextPageContext): string | null {
-    let state: string | null
-    if (isSSR()) {
-        state = getCookie(ACTIVE_EMPLOYEE_COOKIE_NAME, { req: context.req, res: context.res })
-    } else {
-        try {
-            state = cookie.get(ACTIVE_EMPLOYEE_COOKIE_NAME)
-        } catch (e) {
-            console.error('Failed to get employee id from cookie', e)
-            state = null
-        }
-    }
-    return state || null
-}
-
-function setCookieEmployeeId (context: NextPageContext, activeEmployeeId: string) {
-    if (isSSR()) {
-        setCookie(ACTIVE_EMPLOYEE_COOKIE_NAME, activeEmployeeId, { req: context.req, res: context.res })
-    } else {
-        cookie.set(ACTIVE_EMPLOYEE_COOKIE_NAME, activeEmployeeId, { expires: 365 })
-    }
-}
-
-function removeCookieEmployeeId (context: NextPageContext) {
-    if (isSSR()) {
-        deleteCookie(ACTIVE_EMPLOYEE_COOKIE_NAME, { req: context.req, res: context.res })
-    } else {
-        cookie.remove(ACTIVE_EMPLOYEE_COOKIE_NAME)
-    }
-}
 
 type PrefetchOrganizationEmployeeArgs = {
     apolloClient: ApolloClient<NormalizedCacheObject>
@@ -59,7 +25,7 @@ type PrefetchOrganizationEmployeeReturnType = Promise<{
 export async function prefetchOrganizationEmployee (args: PrefetchOrganizationEmployeeArgs): PrefetchOrganizationEmployeeReturnType {
     const { apolloClient, context, userId } = args
 
-    const activeEmployeeId = getCookieEmployeeId(context)
+    const activeEmployeeId = getCookie(ACTIVE_EMPLOYEE_COOKIE_NAME, { req: context?.req, res: context?.res }) || null
 
     if (activeEmployeeId) {
         const response = await apolloClient.query<GetActiveOrganizationEmployeeQuery, GetActiveOrganizationEmployeeQueryVariables>({
@@ -103,9 +69,9 @@ export async function prefetchOrganizationEmployee (args: PrefetchOrganizationEm
             data: response.data,
         })
 
-        setCookieEmployeeId(context, activeEmployee.id)
+        setCookie(ACTIVE_EMPLOYEE_COOKIE_NAME, activeEmployee.id, { req: context?.req, res: context?.res })
     } else {
-        removeCookieEmployeeId(context)
+        deleteCookie(ACTIVE_EMPLOYEE_COOKIE_NAME, { req: context?.req, res: context?.res })
     }
 
     return { activeEmployee }
