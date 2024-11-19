e339ffbf7c8f51135992ebe9399b92359fbab64a
Dmitry Koviazin <dkovyazin@doma.ai>
2024-11-19 09:31:32 +0500

    feat(spp): DOMA-10475 added spp global hints banners (#5505)
    * feat(spp): DOMA-10475 added spp global hints banners
    
    * feat(spp): DOMA-10475 review fixes

--- apps/condo/pages/service-provider-profile.tsx
+++ apps/condo/pages/service-provider-profile.tsx
@@ -4,8 +4,10 @@ import React from 'react'
 import { useFeatureFlags } from '@open-condo/featureflags/FeatureFlagsContext'
 import { useIntl } from '@open-condo/next/intl'
 
+import { PageWrapper } from '@condo/domains/common/components/containers/BaseLayout'
 import LoadingOrErrorPage from '@condo/domains/common/components/containers/LoadingOrErrorPage'
 import { SERVICE_PROVIDER_PROFILE } from '@condo/domains/common/constants/featureflags'
+import { useGlobalHints } from '@condo/domains/common/hooks/useGlobalHints'
 import { BillingAppPage } from '@condo/domains/miniapp/components/AppIndex'
 import { OrganizationRequired } from '@condo/domains/organization/components/OrganizationRequired'
 
@@ -23,9 +25,15 @@ const ServiceProviderProfilePage: PageType = () => {
     const NoPermissionsMessage = intl.formatMessage({ id: 'global.noPageViewPermission' })
     const { useFlag } = useFeatureFlags()
     const isSPPOrg = useFlag(SERVICE_PROVIDER_PROFILE)
+    const { GlobalHints } = useGlobalHints()
 
     if (sppConfig && 'BillingIntegrationId' in sppConfig && isSPPOrg) {
-        return <BillingAppPage id={sppConfig.BillingIntegrationId}/>
+        return (
+            <PageWrapper>
+                {GlobalHints}
+                <BillingAppPage id={sppConfig.BillingIntegrationId}/>
+            </PageWrapper>
+        )
     }
 
     return <LoadingOrErrorPage title={PageTitle} error={NoPermissionsMessage}/>
