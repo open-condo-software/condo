011b4775bfa2efb1b8af2d381d1cb183d88f86f7
SavelevMatthew <savelevmatthew@gmail.com>
2024-11-19 10:59:13 +0500

    refactor(keystone): INFRA-650 move graphqlCtx to separate file

--- packages/keystone/KSv5v6/utils/resolvers.js
+++ packages/keystone/KSv5v6/utils/resolvers.js
@@ -1,11 +1,6 @@
 const get = require('lodash/get')
 
-const { _internalGetAsyncLocalStorage } = require('@open-condo/keystone/executionContext')
-
-// NOTE(SavelevMatthew): separate context is required because I don't want to mix its logic executionContext one:
-// 1. it has different entry points (it is hard to manage the merge process + dependency on executionContext's shape appears)
-// 2. We may want to add call-stack later, merging states will become hard
-const graphqlCtx = _internalGetAsyncLocalStorage('graphqlCtx')
+const { graphqlCtx } = require('./graphqlCtx')
 
 /**
  * Extracts alias map for a operation
