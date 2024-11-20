ad85c6cca4415ae3b9a2c59e991999c2656e9bc8
Mikhail Gorbunov <toplenboren@gmail.com>
2024-11-21 01:51:05 +0500

    chore(ui): fix ActionBar `jumping` when scrolling (#5462)
    * chore(condo): fix ui affix jumping
    
    * chore(condo): fix ui affix component jump
    
    * chore(condo): make it beautiful on small screens and small tablets
    
    * chore(ui): make affix look better on smartphones
    
    * fix(ui): remove 40px mandatory padding
    
    * fix(ui): fix linter errors
    
    * fix(ui): fix linter errors

--- packages/ui/src/components/ActionBar/actionBar.tsx
+++ packages/ui/src/components/ActionBar/actionBar.tsx
@@ -1,11 +1,13 @@
 import { Affix } from 'antd'
 import React, { ReactElement } from 'react'
 
+import { useBreakpoints } from '../../hooks'
 import { Space, SpaceProps } from '../Space'
 import { Typography } from '../Typography'
 
 const ACTION_BAR_CLASS_PREFIX = 'condo-affix'
 const AFFIX_CONTENT_WRAPPER_CLASS = `${ACTION_BAR_CLASS_PREFIX}-content-wrapper`
+const ACTION_BAR_PLACEHOLDER_CLASS = `${ACTION_BAR_CLASS_PREFIX}-placeholder`
 
 const SPACE_SIZE: SpaceProps['size'] = [16, 16]
 
@@ -18,12 +20,17 @@ export type ActionBarProps = {
 export const ActionBar: React.FC<ActionBarProps> = (props) => {
     const { actions, message, wrap = true } = props
 
+    const { TABLET_SMALL } = useBreakpoints()
+
     return (
-        <Affix offsetBottom={0} prefixCls={ACTION_BAR_CLASS_PREFIX}>
-            <Space wrap={wrap} size={SPACE_SIZE} className={AFFIX_CONTENT_WRAPPER_CLASS}>
-                {message && <Typography.Text strong>{message}</Typography.Text>}
-                {actions}
-            </Space>
-        </Affix>
+        <>
+            <Affix offsetBottom={TABLET_SMALL ? 23 : 0} prefixCls={ACTION_BAR_CLASS_PREFIX}>
+                <Space wrap={wrap} size={SPACE_SIZE} className={AFFIX_CONTENT_WRAPPER_CLASS}>
+                    {message && <Typography.Text strong>{message}</Typography.Text>}
+                    {actions}
+                </Space>
+            </Affix>
+            <div className={ACTION_BAR_PLACEHOLDER_CLASS} />
+        </>
     )
 }
