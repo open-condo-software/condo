92a43799bc1f7a3cf43d02c84fbd79a1a7bf7792
pahaz <pahaz.white@gmail.com>
2024-11-22 21:43:18 +0300

    chore(condo): DOMA-10737 refactor show-conf-value.js to use common pattern

--- bin/show-conf-value.js
+++ bin/show-conf-value.js
@@ -1,3 +1,18 @@
+const { program } = require('commander')
+
 const conf = require('@open-condo/config')
-const name = process.argv[2]
-console.log(conf[name] || '')
+
+program
+    .argument('<name>', 'Configuration key to retrieve')
+    .description('Retrieve a configuration value by key from @open-condo/config')
+
+async function main () {
+    const { args } = program.parse()
+    const name = args[0]
+    console.log(conf[name] || '')
+}
+
+main().catch(e => {
+    console.error('Error:', e.message)
+    process.exit(1)
+})
