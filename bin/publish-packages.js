72c513b37f118b723fd2b8198f5f3c944417289f
SavelevMatthew <savelevmatthew@gmail.com>
2024-12-06 08:54:19 +0500

    ci(global): debug execution graph

--- bin/publish-packages.js
+++ bin/publish-packages.js
@@ -8,7 +8,10 @@ const publish = require('mono-pub')
 const builder = {
     name: '@open-condo/turbo-builder',
     async prepare (_, ctx) {
-        await execa('yarn', ['build:packages'], { cwd: ctx.cwd })
+        // TODO: remove debug messages
+        await execa('yarn', ['build:packages', '--graph'], { cwd: ctx.cwd, stdio: 'inherit' })
+        console.log('='.repeat(80))
+        await execa('yarn', ['build:packages'], { cwd: ctx.cwd, stdio: 'inherit' })
     },
 }
 
