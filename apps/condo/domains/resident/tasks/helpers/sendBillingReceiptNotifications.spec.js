eacdd59ae738c0f4cae4b9793ca20e333c25c3bc
vovaaxeapolla <vovaaxeapolla@gmail.com>
2024-12-17 16:28:02 +0500

    fix(condo): DOMA-9373 fix linter

--- apps/condo/domains/resident/tasks/helpers/sendBillingReceiptNotifications.spec.js
+++ apps/condo/domains/resident/tasks/helpers/sendBillingReceiptNotifications.spec.js
@@ -3,14 +3,14 @@
  */
 
 const index = require('@app/condo/index')
+const dayjs = require('dayjs')
 
 const { getRedisClient } = require('@open-condo/keystone/redis')
 const { setFakeClientMode, setAllFeatureFlags } = require('@open-condo/keystone/test.utils')
 
 const { LAST_SEND_BILLING_RECEIPT_NOTIFICATION_DATE } = require('@condo/domains/resident/constants')
 
-const { sendBillingReceiptNotifications, NO_REDIS_KEY, DISABLED, SKIP_NOTIFICATION, DONE} = require('./sendBillingReceiptNotifications')
-const dayjs = require("dayjs");
+const { sendBillingReceiptNotifications, NO_REDIS_KEY, DISABLED, SKIP_NOTIFICATION, DONE } = require('./sendBillingReceiptNotifications')
 
 describe('sendBillingReceiptNotifications', () => {
     setFakeClientMode(index)
