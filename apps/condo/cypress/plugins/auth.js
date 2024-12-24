71ff694abfaecafe9f98482cbdff640fb3391759
Sitnikov Ivan <sit7602@gmail.com>
2024-12-24 20:40:58 +0500

    hotfix(condo): remove cypress tests

--- apps/condo/cypress/plugins/auth.js
+++
@@ -1,5 +0,0 @@
-export const authUserWithCookies = (userData) => {
-    cy.setCookie('locale', 'en')
-    cy.setCookie('keystone.sid', userData.cookie.replace('keystone.sid=', ''))
-    cy.setCookie('organizationLinkId', userData.organizationLinkId)
-}
