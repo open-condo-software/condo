cd53a1596375e7bd9a920674d11c3b53c95c391e
Pavel <nomerdvadcatpyat@gmail.com>
2024-12-28 12:40:05 +0500

    feat(condo): DOMA-10771 change bage to antd component, close dropdown when modal opens

---
+++ apps/condo/domains/common/components/containers/BaseLayout/components/UserMessagesList/MessagesCounter/index.tsx
@@ -0,0 +1,36 @@
+import { Badge } from 'antd'
+import React, { useEffect, useState } from 'react'
+
+import { Bell } from '@open-condo/icons'
+import { colors } from '@open-condo/ui/dist/colors'
+
+import './MessagesCounter.css'
+
+
+export const MessagesCounter = ({ count }) => {
+    const [currentCount, setCurrentCount] = useState(0)
+    const [isAnimating, setIsAnimating] = useState(false)
+
+    useEffect(() => {
+        if (count !== currentCount) {
+            setIsAnimating(true)
+
+            setTimeout(() => {
+                setCurrentCount(count)
+                setIsAnimating(false)
+            }, 60)
+        }
+    }, [count, currentCount])
+
+    return (
+        <Badge
+            count={currentCount > 9 ? '∞' : currentCount}
+            // overflowCount={9}
+            className={`messages-counter${isAnimating ? ' animating' : ''}`}
+            color={colors.pink[5]}
+            showZero
+        >
+            <Bell className='messages-counter-icon' />
+        </Badge>
+    )
+}
