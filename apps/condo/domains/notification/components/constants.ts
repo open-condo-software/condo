b749eab3b1216a06d5681fbb215b660007445405
Pavel <nomerdvadcatpyat@gmail.com>
2024-12-28 15:40:28 +0500

    feat(condo): DOMA-10771 added localStorage logic

---
+++ apps/condo/domains/notification/components/constants.ts
@@ -0,0 +1,33 @@
+import { MessageType } from '@app/condo/schema'
+
+/**
+ * Message types available for filtering in the UserMessagesList component
+ */
+export const USER_MESSAGE_TYPES_FILTER_ON_CLIENT = [
+    MessageType.TicketCreated,
+    MessageType.TicketCommentCreated,
+    MessageType.PassTicketCreated,
+]
+
+/**
+ * Message types which user excluded from USER_MESSAGE_TYPES_FILTER_ON_CLIENT.
+ * If no organizationId or MESSAGE_TYPE in this object, then this type of notification is shown to the user.
+ *
+ * Structure:
+ * {
+ *     [organizationId]: ["MESSAGE_TYPE_1", "MESSAGE_TYPE_2"]
+ * }
+ */
+export const EXCLUDED_USER_MESSAGE_TYPES_LOCAL_STORAGE_KEY = 'excludedUserMessageTypes'
+
+/**
+ * Date and time when user read messages in specific organization.
+ * Refreshes when user opens UserMessagesList dropdown in specific organization.
+ * Relates to this value unread and read messages are displayed
+ *
+ * Structure:
+ * {
+ *     [organizationId]: datetime_iso_string
+ * }
+ */
+export const READ_USER_MESSAGES_AT_LOCAL_STORAGE_KEY = 'readUserMessagesAt'
