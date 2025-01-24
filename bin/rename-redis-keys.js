a7f2f50290023b68233210318f42a95df84465a5
Sitnikov Ivan <sit7602@gmail.com>
2025-01-24 18:56:45 +0500

    fix(condo): INFRA-791 move redis rename script execution to the migration side

--- bin/rename-redis-keys.js
+++ bin/rename-redis-keys.js
@@ -9,44 +9,49 @@ if (conf['REDIS_URL'] === undefined) {
 const client = new Redis(conf['REDIS_URL'])
 const REDIS_KEY_PREFIX = process.cwd().split('/').pop() + ':'
 
-const renameRedisKeys = async () => {
+const renameRedisKeys = async ({ direction, stepSize = 1000 }) => {
     const size = await client.dbsize()
     console.log('database size -> ', size)
     console.log('key prefix -> ', REDIS_KEY_PREFIX)
-    const step = 1000
     let cursor = '0'
-    const iters = Math.ceil(size / step)
+    const iters = Math.ceil(size / stepSize)
 
     for (const i of Array.from({ length: iters })) {
         if (i % 100 === 0) {
             console.log(`I=${i}/${iters}`)
         }
 
-        const [newCursor, keys] = await client.scan(cursor, 'MATCH', '*', 'COUNT', step)
+        const [newCursor, keys] = await client.scan(cursor, 'MATCH', '*', 'COUNT', stepSize)
         console.log('newCursor -> ', newCursor)
         console.log('keys count -> ', keys.length)
         cursor = newCursor
 
         if (keys.length) {
             const multi = client.multi()
-            keys.filter(key => !key.startsWith(REDIS_KEY_PREFIX)).forEach((key) => {
-                multi.rename(key, REDIS_KEY_PREFIX + key)
+
+            const filterQuery = direction === 'forward'
+                ? (key) => !key.startsWith(REDIS_KEY_PREFIX)
+                : (key) => key.startsWith(REDIS_KEY_PREFIX)
+            keys.filter(filterQuery).forEach((key) => {
+                if (direction === 'forward') {
+                    multi.rename(key, REDIS_KEY_PREFIX + key)
+                } else {
+                    multi.rename(key, key.substring(REDIS_KEY_PREFIX.length))
+                }
             })
 
             const result = await multi.exec()
 
-            console.log(`rename for batch ${i} result -> `, result.length)
+            console.log('rename for batch result -> ', result.length)
         }
 
         if (newCursor === 0 || newCursor === '0') {
-            console.log('CURSOR=0')
+            console.log('CURSOR=0 -> stopping execution')
             break
         }
     }
-
 }
 
-renameRedisKeys().then(() => process.exit()).catch(err => {
-    console.error(err)
-    process.exit(1)
-})
+module.exports = {
+    renameRedisKeys,
+}
