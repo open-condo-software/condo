3034d948f5c11d37d65ee14237a748cd07a1d0c2
Sitnikov Ivan <sit7602@gmail.com>
2025-01-28 20:16:21 +0500

    refactor(global): INFRA-791 extract keyPrefix from package name

--- bin/rename-redis-keys.js
+++ bin/rename-redis-keys.js
@@ -1,13 +1,15 @@
 const Redis = require('ioredis')
 
 const conf = require('@open-condo/config')
+const { getRedisPrefix } = require('@open-condo/keystone/redis')
+
 
 if (conf['REDIS_URL'] === undefined) {
     throw new Error('Redis URL missing. You should specify a REDIS_URL env variable for establishing connection')
 }
 
 const client = new Redis(conf['REDIS_URL'])
-const REDIS_KEY_PREFIX = process.cwd().split('/').pop() + ':'
+let REDIS_KEY_PREFIX = getRedisPrefix()
 
 const renameRedisKeys = async ({ direction, stepSize = 1000 }) => {
     const size = await client.dbsize()
@@ -17,13 +19,7 @@ const renameRedisKeys = async ({ direction, stepSize = 1000 }) => {
     const iters = Math.ceil(size / stepSize)
 
     for (const i of Array.from({ length: iters })) {
-        if (i % 100 === 0) {
-            console.log(`I=${i}/${iters}`)
-        }
-
         const [newCursor, keys] = await client.scan(cursor, 'MATCH', '*', 'COUNT', stepSize)
-        console.log('newCursor -> ', newCursor)
-        console.log('keys count -> ', keys.length)
         cursor = newCursor
 
         if (keys.length) {
@@ -34,19 +30,19 @@ const renameRedisKeys = async ({ direction, stepSize = 1000 }) => {
                 : (key) => key.startsWith(REDIS_KEY_PREFIX)
             keys.filter(filterQuery).forEach((key) => {
                 if (direction === 'forward') {
-                    multi.rename(key, REDIS_KEY_PREFIX + key)
+                    multi.renamenx(key, REDIS_KEY_PREFIX + key)
                 } else {
-                    multi.rename(key, key.substring(REDIS_KEY_PREFIX.length))
+                    multi.renamenx(key, key.substring(REDIS_KEY_PREFIX.length))
                 }
             })
 
             const result = await multi.exec()
 
-            console.log('rename for batch result -> ', result.length)
+            console.log('renamed batch length -> ', result.length)
         }
 
         if (newCursor === 0 || newCursor === '0') {
-            console.log('CURSOR=0 -> stopping execution')
+            console.log('No more keys found -> stopping execution')
             break
         }
     }
