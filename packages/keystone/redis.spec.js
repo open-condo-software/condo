bb79ac7a722fdd7b400f0f851a6d817c20d4889e
Sitnikov Ivan <sit7602@gmail.com>
2025-01-29 12:54:14 +0500

    refactor(keystone): INFRA-791 add ability to set redis prefix via env variable

--- packages/keystone/redis.spec.js
+++ packages/keystone/redis.spec.js
@@ -2,16 +2,21 @@ const Redis = require('ioredis')
 
 const conf = require('@open-condo/config')
 
+const { getRedisPrefix } = require('./redis')
 
 describe('Redis adapter', () => {
     const OLD_ENV = process.env
     let redisClient
     let nonPrefixedClient
+    let moduleName
 
     beforeAll(() => {
+
         jest.resetModules()
         process.env.REDIS_URL = conf['REDIS_URL'] || 'redis://127.0.0.1:6379'
         process.env.REDIS_FALLBACK_ENABLED = conf['REDIS_FALLBACK_ENABLED'] || 'true'
+
+        moduleName = require(process.cwd() + '/package.json').name.split('/').pop() + ':'
 
         const { getRedisClient } = require('./redis')
         redisClient = getRedisClient('test')
@@ -20,17 +25,37 @@ describe('Redis adapter', () => {
 
     afterAll(async () => {
         await nonPrefixedClient.del('test')
-        await nonPrefixedClient.del('keystone:test1')
-        await nonPrefixedClient.del('keystone:incrTest')
-        await nonPrefixedClient.del('keystone:testList')
+        await nonPrefixedClient.del(`${moduleName}test1`)
+        await nonPrefixedClient.del(`${moduleName}incrTest`)
+        await nonPrefixedClient.del(`${moduleName}testList`)
         await nonPrefixedClient.disconnect()
         await redisClient.disconnect()
 
         process.env = { ...OLD_ENV }
     })
 
+    test('prefix should be the name of root package json', () => {
+        expect(getRedisPrefix()).toEqual(moduleName)
+    })
+
+    test('prefix might be overrided via REDIS_PREFIX env variable', () => {
+        jest.resetModules()
+        process.env.REDIS_URL = conf['REDIS_URL'] || 'redis://127.0.0.1:6379'
+        process.env.REDIS_FALLBACK_ENABLED = conf['REDIS_FALLBACK_ENABLED'] || 'true'
+        process.env.REDIS_PREFIX = 'someNewPrefix:'
+
+        const { getRedisPrefix } = require('./redis')
+        expect(getRedisPrefix()).toEqual('someNewPrefix:')
+
+        jest.resetModules()
+        process.env.REDIS_URL = conf['REDIS_URL'] || 'redis://127.0.0.1:6379'
+        process.env.REDIS_FALLBACK_ENABLED = conf['REDIS_FALLBACK_ENABLED'] || 'true'
+        process.env.REDIS_PREFIX = ''
+    })
+
+
     test('redis keyPrefix should be module specific', async () => {
-        expect(redisClient.options.keyPrefix).toMatch('keystone:')
+        expect(redisClient.options.keyPrefix).toMatch(moduleName)
     })
 
     test('redis handles non prefixed key with fallback enabled', async () => {
@@ -50,7 +75,7 @@ describe('Redis adapter', () => {
         const result1 = await nonPrefixedClient.get('test1')
         expect(result1).toBeNull()
 
-        const result2 = await nonPrefixedClient.get('keystone:test1')
+        const result2 = await nonPrefixedClient.get(`${moduleName}test1`)
         expect(result2).toMatch(result)
     })
 
@@ -69,8 +94,8 @@ describe('Redis adapter', () => {
 
     test('redis should work with oidc adapter', async () => {
         await redisClient.rpush('testList', 1)
-        const key = await nonPrefixedClient.keys('keystone:testList')
-        expect(key).toEqual(expect.arrayContaining(['keystone:testList']))
+        const key = await nonPrefixedClient.keys(`${moduleName}testList`)
+        expect(key).toEqual(expect.arrayContaining([`${moduleName}testList`]))
         const range = await redisClient.lrange('testList', 0, -1)
         expect(range).toEqual(expect.arrayContaining(['1']))
         const multi = redisClient.multi()
