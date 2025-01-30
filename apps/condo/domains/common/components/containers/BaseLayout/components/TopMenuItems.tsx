1884e9a4222e50625321e42f26f4fa7ed1e96c00
Pavel <nomerdvadcatpyat@gmail.com>
2025-01-30 20:52:34 +0500

    feat(condo): DOMA-10687 added usePollUserMessages hook

--- apps/condo/domains/common/components/containers/BaseLayout/components/TopMenuItems.tsx
+++ apps/condo/domains/common/components/containers/BaseLayout/components/TopMenuItems.tsx
@@ -4,12 +4,11 @@ import { useAuth } from '@open-condo/next/auth'
 import { useOrganization } from '@open-condo/next/organization'
 import { Space } from '@open-condo/ui'
 
+import { UserMessagesList } from '@condo/domains/notification/components/UserMessagesList'
 import { InlineOrganizationSelect } from '@condo/domains/organization/components/OrganizationSelect'
 import { SBBOLIndicator } from '@condo/domains/organization/components/SBBOLIndicator'
 import { ServiceSubscriptionIndicator } from '@condo/domains/subscription/components/ServiceSubscriptionIndicator'
 import { UserMenu } from '@condo/domains/user/components/UserMenu'
-
-import { UserMessagesList } from '../../../../../notification/components/UserMessagesList'
 
 
 export interface ITopMenuItemsProps {
