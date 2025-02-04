52b4d5b94179c2815e9a03bf83029d3219199c33
Alexander Naydenov <AleX83Xpert@mail.ru>
2025-02-04 14:01:41 +0500

    chore(condo): DOMA-11041 pass category to registerMultiPaymentForVirt… (#5740)
    * chore(condo): DOMA-11041 pass category to registerMultiPaymentForVirtualReceipt
    
    Also, throw an error if no receipt found after qr-code scanned
    
    * chore(condo): DOMA-11041 add category checking to tests
    
    * chore(condo): DOMA-11041 fix some test names
    
    * chore(condo): DOMA-11041 search last receipt with `tin` and `bic`

--- apps/condo/domains/billing/utils/receiptQRCodeUtils.spec.js
+++ apps/condo/domains/billing/utils/receiptQRCodeUtils.spec.js
@@ -16,6 +16,7 @@ const {
     createValidRuRoutingNumber,
     createValidRuNumber,
 } = require('@condo/domains/banking/utils/testSchema/bankAccount')
+const { HOUSING_CATEGORY_ID } = require('@condo/domains/billing/constants/constants')
 const { getCountrySpecificQRCodeParser } = require('@condo/domains/billing/utils/countrySpecificQRCodeParsers')
 const { RUSSIA_COUNTRY } = require('@condo/domains/common/constants/countries')
 const { createTestOrganization } = require('@condo/domains/organization/utils/testSchema')
@@ -169,6 +170,7 @@ describe('receiptQRCodeUtils', () => {
             ;[billingProperty] = await createTestBillingProperty(adminClient, billingIntegrationContext)
             ;[billingAccount] = await createTestBillingAccount(adminClient, billingIntegrationContext, billingProperty, { number: qrCodeObj.PersAcc })
             ;[billingRecipient] = await createTestBillingRecipient(adminClient, billingIntegrationContext, {
+                tin: qrCodeObj.PayeeINN,
                 bankAccount: qrCodeObj.PersonalAcc,
                 bic: qrCodeObj.BIC,
             })
@@ -200,7 +202,9 @@ describe('receiptQRCodeUtils', () => {
                     period: '2024-06-01',
                     receiver: { connect: { id: billingRecipient.id } },
                     recipient: createTestRecipient({
+                        tin: billingRecipient.tin,
                         bic: billingRecipient.bic,
+                        bankAccount: billingRecipient.bankAccount,
                     }),
                     toPay: Big(qrCodeObj.Sum).div(100),
                 })
@@ -209,6 +213,7 @@ describe('receiptQRCodeUtils', () => {
                     id: billingReceipt.id,
                     period: billingReceipt.period,
                     toPay: billingReceipt.toPay,
+                    category: { id: HOUSING_CATEGORY_ID },
                 }
             })
 
