d417684d33352140d60254e529913d6995eda5c3
Pavel <nomerdvadcatpyat@gmail.com>
2025-02-05 13:52:20 +0500

    feat(condo): DOMA-10687 added localStorage manager

--- apps/condo/domains/notification/hooks/usePollUserMessages.ts
+++ apps/condo/domains/notification/hooks/usePollUserMessages.ts
@@ -6,7 +6,7 @@ import { useDeepCompareEffect } from '@open-condo/codegen/utils/useDeepCompareEf
 
 import { useBroadcastChannel } from '@condo/domains/common/hooks/useBroadcastChannel'
 import { useExecuteWithLock } from '@condo/domains/common/hooks/useExecuteWithLock'
-import { USER_MESSAGE_LIST_POLL_INTERVAL_IN_MS, UserMessageType } from '@condo/domains/notification/components/constants'
+import { USER_MESSAGE_LIST_POLL_INTERVAL_IN_MS, UserMessageType } from '@condo/domains/notification/utils/client/constants'
 
 
 type UserPollUserMessagesArgsType = {
@@ -16,17 +16,15 @@ type UserPollUserMessagesArgsType = {
 type UserPollUserMessagesReturnType = {
     userMessages: UserMessageType[]
 }
-type UserPollUserMessagesType = (args: UserPollUserMessagesArgsType) => UserPollUserMessagesReturnType
+type UsePollUserMessagesType = (args: UserPollUserMessagesArgsType) => UserPollUserMessagesReturnType
 type UserMessagesBroadcastMessageType = { error?: ApolloError, messages: UserMessageType[] }
 
 const USER_MESSAGES_POLL_LOCK_NAME = 'user-messages-list'
 const USER_MESSAGES_POLL_CHANNEL_NAME = 'user-messages-list'
 
-export const usePollUserMessages: UserPollUserMessagesType = ({ queryMessagesVariables, skipQueryMessagesCondition }) => {
+export const usePollUserMessages: UsePollUserMessagesType = ({ queryMessagesVariables, skipQueryMessagesCondition }) => {
     const [isPollingTab, setIsPollingTab] = useState<boolean>(false)
     const [userMessages, setUserMessages] = useState<UserMessageType[]>([])
-
-    console.log('isPollingTab', isPollingTab)
 
     useExecuteWithLock(USER_MESSAGES_POLL_LOCK_NAME, () => {
         setIsPollingTab(true)
