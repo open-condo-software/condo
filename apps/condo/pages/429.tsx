e3c03ba50529f1ee6850da7c90dbcfe04100bb74
Alllex202 <alllex.202@gmail.com>
2025-02-07 15:25:57 +0500

    feat(condo): DOMA-0000 added error 429

---
+++ apps/condo/pages/429.tsx
@@ -0,0 +1,48 @@
+import { Col, Row } from 'antd'
+
+import { useIntl } from '@open-condo/next/intl'
+import { Typography } from '@open-condo/ui'
+
+import { PageComponentType } from '@condo/domains/common/types'
+import { PosterLayout } from '@condo/domains/user/components/containers/PosterLayout'
+
+import { ErrorLayoutFooter, ErrorLayoutHeader } from './500'
+
+
+const Src429 = { poster: '/404Poster.webp', placeholder: '/404PosterPlaceholder.jpg' }
+
+const Custom429: PageComponentType = () => {
+    const intl = useIntl()
+    const PageTitle = intl.formatMessage( { id: 'pages.condo.error.TooManyRequestsTitle' })
+    const DescriptionMessage = intl.formatMessage({ id: 'pages.condo.error.TooManyRequestsDescription' })
+
+    return (
+        <Row justify='space-between'>
+            <Col span={24}>
+                <Row gutter={[0, 24]} justify='center'>
+                    <Col span={24}>
+                        <Typography.Title>{PageTitle}</Typography.Title>
+                    </Col>
+                    <Col span={24}>
+                        <Typography.Paragraph>
+                            {DescriptionMessage}
+                        </Typography.Paragraph>
+                    </Col>
+                </Row>
+            </Col>
+        </Row>
+    )
+}
+
+const Error429Layout = (props): React.ReactElement => <PosterLayout
+    {...props}
+    Header={<ErrorLayoutHeader />}
+    Footer={<ErrorLayoutFooter />}
+    layoutBgImage={Src429}
+/>
+
+Custom429.container = Error429Layout
+Custom429.isError = true
+Custom429.skipUserPrefetch = true
+
+export default Custom429
