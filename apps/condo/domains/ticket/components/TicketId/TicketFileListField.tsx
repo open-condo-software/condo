14874997c9e071284f3744487aa98254034fcc30
Viktor Tolmachev <tolmach0221@yandex.ru>
2025-02-11 20:42:10 +0500

    perf(condo): DOMA-11056 some changes

--- apps/condo/domains/ticket/components/TicketId/TicketFileListField.tsx
+++ apps/condo/domains/ticket/components/TicketId/TicketFileListField.tsx
@@ -1,6 +1,7 @@
+import { useGetTicketFilesQuery } from '@app/condo/gql'
 import { SortTicketFilesBy, Ticket } from '@app/condo/schema'
 import { isEmpty } from 'lodash'
-import React from 'react'
+import React, { useMemo } from 'react'
 
 import { useIntl } from '@open-condo/next/intl'
 
@@ -18,13 +19,28 @@ export const TicketFileListField: React.FC<TicketFileListFieldProps> = ({ ticket
     const intl = useIntl()
     const FilesFieldLabel = intl.formatMessage({ id: 'pages.condo.ticket.field.Files' })
 
-    const { objs: files } = TicketFile.useObjects({
-        where: { ticket: { id: ticket ? ticket.id : null } },
-        sortBy: [SortTicketFilesBy.CreatedAtAsc],
-    }, {
+    // const { objs: files } = TicketFile.useObjects({
+    //     where: { ticket: { id: ticket ? ticket.id : null } },
+    //     sortBy: [SortTicketFilesBy.CreatedAtAsc],
+    // }, {
+    //     fetchPolicy: 'network-only',
+    // })
+    // Почему только network-only? В кэше не могут это храниться?
+    const {
+        data,
+    } = useGetTicketFilesQuery({
+        variables: {
+            where: { ticket: { id: ticket?.id || null } },
+            sortBy: [SortTicketFilesBy.CreatedAtAsc],
+            first: 100,
+        },
+        skip: !ticket?.id,
         fetchPolicy: 'network-only',
     })
-
+    console.log('data', data)
+    const files = useMemo(() => data?.ticketFiles.filter(Boolean) || [], [data?.ticketFiles])
+    console.log('files', files)
+    console.log('isEmpty', isEmpty(files))
     return (
         !isEmpty(files) && (
             <PageFieldRow title={FilesFieldLabel} ellipsis>
