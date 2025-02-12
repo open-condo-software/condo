1642424705be84c71c67e7cd6e38e571ac33e90a
Viktor Tolmachev <tolmach0221@yandex.ru>
2025-02-12 12:56:18 +0500

    perf(condo): DOMA-11056 change TicketStatus

--- apps/condo/domains/ticket/hooks/useStatusTransitions.ts
+++ apps/condo/domains/ticket/hooks/useStatusTransitions.ts
@@ -1,14 +1,27 @@
+import { useGetTicketStatusesQuery } from '@app/condo/gql'
 import { Organization, OrganizationEmployee } from '@app/condo/schema'
-import get from 'lodash/get'
 
-import { TicketStatus } from '../utils/clientSchema'
+// import { TicketStatus } from '../utils/clientSchema'
+import { useCachePersistor } from '@open-condo/apollo'
+
 import { getPossibleStatuses } from '../utils/status'
 
 export const useStatusTransitions = (ticketStatusId: string, organization: Organization, employee: OrganizationEmployee) => {
-    const { objs: statusList, loading } = TicketStatus.useObjects({})
+    // const { objs: statusList, loading } = TicketStatus.useObjects({})
+    // console.log('statusList', statusList)
 
-    const organizationStatusTransition = get(organization, 'statusTransitions')
-    const employeeRoleStatusTransitions = get(employee, ['role', 'statusTransitions'])
+    const { persistor } = useCachePersistor()
+
+    const {
+        data,
+        loading,
+    } = useGetTicketStatusesQuery({
+        skip: !persistor,
+    })
+    // console.log('data', data)
+    const statusList = data?.statuses.filter(Boolean) || []
+    const organizationStatusTransition = organization?.statusTransitions || null
+    const employeeRoleStatusTransitions = employee?.role?.statusTransitions || null
 
     return {
         loading: loading,
