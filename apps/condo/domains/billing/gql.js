eab630e01ce5f7e7c6e7d97e23ee8449fd1ea757
AleX83Xpert <AleX83Xpert@mail.ru>
2025-02-13 12:07:35 +0500

    feat(condo): DOMA-10978 fix build by fix including of constants

--- apps/condo/domains/billing/gql.js
+++ apps/condo/domains/billing/gql.js
@@ -8,7 +8,7 @@ const { gql } = require('graphql-tag')
 
 const { generateGqlQueries } = require('@open-condo/codegen/generate.gql')
 
-const { RECIPIENT_FIELDS_DEFINITION } = require('@condo/domains/acquiring/schema/fields/Recipient')
+const { RECIPIENT_FIELDS_DEFINITION } = require('@condo/domains/acquiring/constants/gql')
 const { ADDRESS_META_SUBFIELDS_QUERY_LIST } = require('@condo/domains/property/schema/fields/AddressMetaField')
 
 const COMMON_FIELDS = 'id dv sender { dv fingerprint } v deletedAt newId createdBy { id name } updatedBy { id name } createdAt updatedAt'
