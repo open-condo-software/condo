8a58ec1e7cb6668905a2469014ab60c1c406327d
Pavel <nomerdvadcatpyat@gmail.com>
2025-02-17 18:36:36 +0500

    feat(condo): DOMA-10771 added feature flag

--- apps/condo/domains/common/components/containers/BaseLayout/components/TopMenuItems.tsx
+++ apps/condo/domains/common/components/containers/BaseLayout/components/TopMenuItems.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
+import { useFeatureFlags } from '@open-condo/featureflags/FeatureFlagsContext'
 import { useAuth } from '@open-condo/next/auth'
 import { useOrganization } from '@open-condo/next/organization'
 import { Space } from '@open-condo/ui'
 
+import { PLATFORM_NOTIFICATIONS } from '@condo/domains/common/constants/featureflags'
 import { UserMessagesList } from '@condo/domains/notification/components/UserMessagesList'
 import { InlineOrganizationSelect } from '@condo/domains/organization/components/OrganizationSelect'
 import { SBBOLIndicator } from '@condo/domains/organization/components/SBBOLIndicator'
@@ -19,13 +21,16 @@ export const TopMenuItems: React.FC<ITopMenuItemsProps> = (props) => {
     const auth = useAuth()
     const { organization } = useOrganization()
 
+    const { useFlag } = useFeatureFlags()
+    const isPlatformNotificationsFeatureEnabled = useFlag(PLATFORM_NOTIFICATIONS)
+
     if (auth.isLoading) return null
 
     return (
         <>
             {props.headerAction ? props.headerAction : null}
             <Space direction='horizontal' size={40} className='top-menu-items'>
-                <UserMessagesList />
+                {isPlatformNotificationsFeatureEnabled && <UserMessagesList />}
                 <Space size={12}>
                     <SBBOLIndicator organization={organization} />
                     <ServiceSubscriptionIndicator />
