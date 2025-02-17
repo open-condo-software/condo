d669c8508089e42790472b27e25a98ba3ecce548
Pavel <nomerdvadcatpyat@gmail.com>
2025-02-17 17:46:50 +0500

    feat(condo): DOMA-10771 added analytics events

--- apps/condo/domains/notification/components/UserMessagesList/MessageCard/index.tsx
+++ apps/condo/domains/notification/components/UserMessagesList/MessageCard/index.tsx
@@ -1,9 +1,10 @@
 import dayjs from 'dayjs'
-import React, { useMemo } from 'react'
+import React, { useCallback, useMemo } from 'react'
 
 import { useIntl } from '@open-condo/next/intl'
 import { Card, Typography } from '@open-condo/ui'
 
+import { useTracking } from '@condo/domains/common/components/TrackingContext'
 import { MessageTypeAllowedToFilterType, UserMessageType } from '@condo/domains/notification/utils/client/constants'
 
 import './MessageCard.css'
@@ -23,12 +24,23 @@ const MESSAGE_ICON: Record<MessageTypeAllowedToFilterType, string> = {
 const DATE_FORMAT = 'DD.MM.YYYY, HH:mm'
 
 export const MessageCard: React.FC<MessageCardProps> = ({ message, viewed }) => {
+    const messageType = useMemo(() => message?.type, [message?.type])
+
     const intl = useIntl()
-    const MessageTitle = intl.formatMessage({ id: `notification.UserMessagesList.message.${message.type}.label` })
+    const MessageTitle = intl.formatMessage({ id: `notification.UserMessagesList.message.${messageType}.label` })
+
+    const { logEvent } = useTracking()
 
     const messageContent = useMemo(() => message?.defaultContent?.content, [message?.defaultContent?.content])
     const titleLink = useMemo(() => message?.meta?.data?.url, [message?.meta])
     const createdAt = useMemo(() => dayjs(message?.createdAt).format(DATE_FORMAT), [message?.createdAt])
+
+    const handleLinkClick = useCallback(() => {
+        logEvent({
+            eventName: 'UserMessageCardClickTitle',
+            eventProperties: { type: messageType, id: message?.id },
+        })
+    }, [logEvent, message?.id, messageType])
 
     return (
         <Card
@@ -37,7 +49,9 @@ export const MessageCard: React.FC<MessageCardProps> = ({ message, viewed }) => 
             className={`message-card${viewed ? ' message-card-viewed' : ''}`}
         >
             <div className='message-card-title'>
-                <Typography.Link href={titleLink}>{MessageTitle}</Typography.Link>
+                <Typography.Link onClick={handleLinkClick} href={titleLink}>
+                    {MessageTitle}
+                </Typography.Link>
                 {MESSAGE_ICON[message.type]}
             </div>
             <Typography.Paragraph type='secondary' size='medium'>
