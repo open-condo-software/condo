c1669dd1d33d118a44e2ffcd0e93b0d46382b884
Pavel <nomerdvadcatpyat@gmail.com>
2025-02-18 17:59:06 +0500

    fix(condo): DOMA-10771 set selected message types state when toggling switch

--- apps/condo/domains/common/utils/localStorageManager.ts
+++ apps/condo/domains/common/utils/localStorageManager.ts
@@ -1,15 +1,15 @@
-export class LocalStorageManager <T> {
-    getItem (key: string): T | null {
+export class LocalStorageManager <ItemType> {
+    getItem (key: string): ItemType | null {
         try {
             const item = localStorage.getItem(key)
-            return item ? JSON.parse(item) as T : null
+            return item ? JSON.parse(item) as ItemType : null
         } catch (error) {
             console.error(`Error reading key "${key}" from LocalStorage:`, error)
             return null
         }
     }
 
-    setItem (key: string, value: T): void {
+    setItem (key: string, value: ItemType): void {
         try {
             localStorage.setItem(key, JSON.stringify(value))
         } catch (error) {
