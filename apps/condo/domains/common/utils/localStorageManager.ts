55333471ca07f3f97f0adc33d6943851b147463f
Pavel <nomerdvadcatpyat@gmail.com>
2025-02-21 12:58:57 +0500

    chore(condo): DOMA-10771 fix limt issue

--- apps/condo/domains/common/utils/localStorageManager.ts
+++ apps/condo/domains/common/utils/localStorageManager.ts
@@ -4,7 +4,7 @@ export class LocalStorageManager <ItemType> {
             const item = localStorage.getItem(key)
             return item ? JSON.parse(item) as ItemType : null
         } catch (error) {
-            console.error(`Error reading key "${key}" from LocalStorage:`, error)
+            console.error(`Error reading key "${key}" from LocalStorage:` + error)
             return null
         }
     }
@@ -13,7 +13,7 @@ export class LocalStorageManager <ItemType> {
         try {
             localStorage.setItem(key, JSON.stringify(value))
         } catch (error) {
-            console.error(`Error writing key "${key}" to LocalStorage:`, error)
+            console.error(`Error writing key "${key}" to LocalStorage:` + error)
         }
     }
 }
