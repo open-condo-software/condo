e68dc715f509959283d6ba2f570d4e82e0f9ad60
Mikhail Gorbunov <toplenboren@gmail.com>
2025-02-24 21:49:51 +0500

    feat(condo): add tooltip on building map screent

--- apps/condo/pages/property/[id]/index.tsx
+++ apps/condo/pages/property/[id]/index.tsx
@@ -195,7 +195,7 @@ export const PropertyPageContent = ({ property, role = null, organizationId = nu
                     </Space>
                 </Col>
                 <Col xl={12} md={24} sm={24} xs={24}>
-                    <PropertyReportCard property={property} organizationId={organizationId} role={role} />
+                    <PropertyReportCard property={property} organizationId={organizationId} role={role}/>
                 </Col>
             </Row>
             <Row gutter={[0, 24]} style={PROPERTY_PAGE_CONTENT_ROW_STYLE}>
