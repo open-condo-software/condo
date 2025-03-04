8d95d5ce053aa223264f5101e54cf9f5f6ef0dd5
abshnko <2009add@gmail.com>
2025-03-04 12:11:56 +0300

    refactor(condo): DOMA-11144 renamed model MeterUserData to MeterUserSetting

--- apps/condo/domains/meter/schema/index.js
+++ apps/condo/domains/meter/schema/index.js
@@ -15,7 +15,7 @@ const { MeterReadingSource } = require('./MeterReadingSource')
 const { MeterReportingPeriod } = require('./MeterReportingPeriod')
 const { MeterResource } = require('./MeterResource')
 const { MeterResourceOwner } = require('./MeterResourceOwner')
-const { MeterUserData } = require('./MeterUserData')
+const { MeterUserSetting } = require('./MeterUserSetting')
 const { PropertyMeter } = require('./PropertyMeter')
 const { PropertyMeterReading } = require('./PropertyMeterReading')
 const { RegisterMetersReadingsService } = require('./RegisterMetersReadingsService')
@@ -38,7 +38,7 @@ module.exports = {
     RegisterMetersReadingsService,
     MeterReadingsImportTask,
     MeterReadingExportTask,
-    MeterUserData,
     RegisterPropertyMetersReadingsService,
+    MeterUserSetting,
 /* AUTOGENERATE MARKER <EXPORTS> */
 }
