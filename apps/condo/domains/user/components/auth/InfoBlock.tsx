13eb06ee9b90745c3dea196c9b12b99eb0f51891
Alllex202 <alllex.202@gmail.com>
2025-03-04 19:24:32 +0500

    feat(condo): DOMA-9722 added display of QR code. Refactored styles

---
+++ apps/condo/domains/user/components/auth/InfoBlock.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import './InfoBlock.css'
+
+
+export const InfoBlock: React.FC = ({ children }) => {
+    return <div className='info-block' children={children} />
+}
--- apps/condo/pages/auth/resident.tsx
+++ apps/condo/pages/auth/resident.tsx
@@ -1,3 +1,4 @@
+import { QRCodeCanvas } from '@rc-component/qrcode'
 import { Col, Row } from 'antd'
 import { setCookie } from 'cookies-next'
 import Head from 'next/head'
@@ -10,7 +11,9 @@ import { Typography } from '@open-condo/ui'
 
 import { COOKIE_MAX_AGE_IN_SEC } from '@condo/domains/common/constants/cookies'
 import { PageComponentType } from '@condo/domains/common/types'
+import { InfoBlock } from '@condo/domains/user/components/auth/InfoBlock'
 import AuthLayout from '@condo/domains/user/components/containers/AuthLayout'
+import { ResponsiveCol } from '@condo/domains/user/components/containers/ResponsiveCol'
 import { WelcomeHeaderTitle } from '@condo/domains/user/components/UserWelcomeTitle'
 import { AUTH_FLOW_USER_TYPE_COOKIE_NAME } from '@condo/domains/user/constants/auth'
 
@@ -28,43 +31,49 @@ const ResidentAuthPage: PageComponentType = () => {
         <>
             <Head><title>{SignInTitleMsg}</title></Head>
 
-            {/* TODO(DOMA-9722): use css styles */}
-            <Row justify='center' style={{ maxWidth: 422 }} gutter={[0, 48]}>
-                <Col span={24}>
-                    <Row gutter={[0, 24]}>
+            <Row justify='center'>
+                <ResponsiveCol desktopWidth='422px' span={24}>
+                    <Row justify='center' gutter={[0, 48]}>
                         <Col span={24}>
-                            <Typography.Title level={2}>
-                                Мобильное приложение жителя
-                            </Typography.Title>
-                        </Col>
-                        <Col span={24}>
-                            <Typography.Text>
-                                Оплачивайте квитанции, сдавайте показания счетчиков и&nbsp;отправляйте заявки в управляющую организацию
-                            </Typography.Text>
-                        </Col>
-                    </Row>
-                </Col>
-                <Col span={24}>
-                    <Row gutter={[0, 48]}>
-                        <Col span={24}>
-                            <Row justify='center'>
-                                <Col>
-                                    Qr code
+                            <Row gutter={[0, 24]}>
+                                <Col span={24}>
+                                    <Typography.Title level={2}>
+                                        Мобильное приложение жителя
+                                    </Typography.Title>
                                 </Col>
-                            </Row>
-                        </Col>
-                        <Col span={24}>
-                            <Row justify='center'>
-                                {/* TODO(DOMA-9722): use css styles */}
-                                <Col style={{ padding: 16, background: '#F2F4F6', borderRadius: 4, textAlign: 'center' }}>
-                                    <Typography.Text size='medium'>
-                                        Наведите камеру вашего телефона на QR-код или скачайте приложение по <Link href='#'>ссылке</Link>
+                                <Col span={24}>
+                                    <Typography.Text>
+                                        Оплачивайте квитанции, сдавайте показания счетчиков и&nbsp;отправляйте заявки в управляющую организацию
                                     </Typography.Text>
                                 </Col>
                             </Row>
                         </Col>
+
+                        <Col span={24}>
+                            <Row gutter={[0, 48]}>
+                                <Col span={24}>
+                                    <Row justify='center'>
+                                        <Col>
+                                            {/* TODO(DOMA-9722): remove hardcode */}
+                                            <QRCodeCanvas
+                                                value='https://redirect.appmetrica.yandex.com/serve/1181330335116320672'
+                                                size={168}
+                                                level='M'
+                                            />
+                                        </Col>
+                                    </Row>
+                                </Col>
+                                <Col span={24}>
+                                    <InfoBlock>
+                                        <Typography.Text size='medium'>
+                                            Наведите камеру вашего телефона на QR-код или скачайте приложение по <Link href='#'>ссылке</Link>
+                                        </Typography.Text>
+                                    </InfoBlock>
+                                </Col>
+                            </Row>
+                        </Col>
                     </Row>
-                </Col>
+                </ResponsiveCol>
             </Row>
         </>
     )
