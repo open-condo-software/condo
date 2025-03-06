6dc80c0f505031c404e657b061935f656eee9007
Alllex202 <alllex.202@gmail.com>
2025-03-06 14:07:49 +0500

    feat(condo): DOMA-9722 added redirects if no resident app. Fixed styles

--- apps/condo/pages/auth/resident.tsx
+++ apps/condo/pages/auth/resident.tsx
@@ -1,6 +1,7 @@
 import { QRCodeCanvas } from '@rc-component/qrcode'
 import { Col, Row } from 'antd'
 import { setCookie } from 'cookies-next'
+import getConfig from 'next/config'
 import Head from 'next/head'
 import React from 'react'
 
@@ -8,6 +9,7 @@ import { useEffectOnce } from '@open-condo/miniapp-utils'
 import { useIntl } from '@open-condo/next/intl'
 import { Typography } from '@open-condo/ui'
 
+import LoadingOrErrorPage from '@condo/domains/common/components/containers/LoadingOrErrorPage'
 import { COOKIE_MAX_AGE_IN_SEC } from '@condo/domains/common/constants/cookies'
 import { PageComponentType } from '@condo/domains/common/types'
 import { InfoBlock } from '@condo/domains/user/components/auth/InfoBlock'
@@ -17,23 +19,33 @@ import { WelcomeHeaderTitle } from '@condo/domains/user/components/UserWelcomeTi
 import { AUTH_FLOW_USER_TYPE_COOKIE_NAME } from '@condo/domains/user/constants/auth'
 
 
+const {
+    publicRuntimeConfig: {
+        residentAppInfo,
+    },
+} = getConfig()
+
 const ResidentAuthPage: PageComponentType = () => {
     const intl = useIntl()
+    const ServerErrorMessage = intl.formatMessage({ id: 'ServerError' })
     const ResidentAuthTitle = intl.formatMessage({ id: 'pages.auth.resident.title' })
     const ResidentAuthDescription = intl.formatMessage({ id: 'pages.auth.resident.description' })
     const LinkDativeMessage = intl.formatMessage({ id: 'pages.auth.resident.qrCode.info.link.dative' })
     const QRCodeInfoMessage = intl.formatMessage({ id: 'pages.auth.resident.qrCode.info' }, {
         link: (
-            <Typography.Link href='https://help.doma.ai/article/171-doma' target='_blank'>
+            <Typography.Link href={residentAppInfo?.mobile?.help || '#'} target='_blank'>
                 {LinkDativeMessage}
             </Typography.Link>
         ),
     })
-    // TODO(DOMA-9722): REMOVE HARDCODE
 
     useEffectOnce(() => {
         setCookie(AUTH_FLOW_USER_TYPE_COOKIE_NAME, 'resident', { maxAge: COOKIE_MAX_AGE_IN_SEC })
     })
+
+    if (!residentAppInfo?.mobile?.help || !residentAppInfo?.mobile?.download) {
+        return <LoadingOrErrorPage title={ResidentAuthTitle} loading={false} error={ServerErrorMessage}/>
+    }
 
     return (
         <>
@@ -62,9 +74,8 @@ const ResidentAuthPage: PageComponentType = () => {
                                 <Col span={24}>
                                     <Row justify='center'>
                                         <Col>
-                                            {/* TODO(DOMA-9722): remove hardcode */}
                                             <QRCodeCanvas
-                                                value='https://redirect.appmetrica.yandex.com/serve/1181330335116320672'
+                                                value={residentAppInfo.mobile.download}
                                                 size={168}
                                                 level='M'
                                             />
@@ -90,5 +101,20 @@ const ResidentAuthPage: PageComponentType = () => {
 ResidentAuthPage.container = AuthLayout
 ResidentAuthPage.headerAction = <WelcomeHeaderTitle userType='resident' />
 ResidentAuthPage.skipUserPrefetch = true
+
+ResidentAuthPage.getPrefetchedData = async () => {
+    if (!residentAppInfo?.mobile?.help || !residentAppInfo?.mobile?.download) {
+        return {
+            redirect: {
+                destination: '/auth',
+                permanent: false,
+            },
+        }
+    }
+
+    return {
+        props: {},
+    }
+}
 
 export default ResidentAuthPage
