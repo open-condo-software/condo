c40a0ff1ab16f733329807ad937483fc62276d75
SavelevMatthew <savelevmatthew@gmail.com>
2025-03-07 11:44:48 +0500

    feat(miniapp-utils): INFRA-915 added posthog utils for proxy setup

--- packages/miniapp-utils/src/helpers/posthog.ts
+++ packages/miniapp-utils/src/helpers/posthog.ts
@@ -7,6 +7,27 @@ const POSTHOG_CLOUD_HOST_BASE = 'i.posthog.com'
 const POSTHOG_CLOUD_HOST_MATCHER = new RegExp(`^(\\w+)\\.${POSTHOG_CLOUD_HOST_BASE.replaceAll('.', '\\.')}$`)
 
 /**
+ * Gets posthog host and pathname based on domain and requested path. See example below for detailed explanation
+ * @example
+ * getPosthogPath('https://eu.i.posthog.com', ['static', 'something']) // { host: 'eu-assets.i.posthog.com', pathname: 'something' }
+ * getPosthogPath('https://eu.i.posthog.com', ['other', 'path']) // { host: 'eu.i.posthog.com', pathname: 'other/path' }
+ * getPosthogPath('https://ph.self-hosted.com', ['static', 'something']) // { host: 'ph.self-hosted.com', pathname: 'static/something' }
+ * getPosthogPath('https://ph.self-hosted.com', ['other', 'path']) // { host: 'ph.self-hosted.com', pathname: 'other/path' }
+ */
+export function getPosthogEndpoint (posthogDomain: string, requestedPath: Array<string>): { host: string, pathname: string } {
+    const posthogURL = new URL(posthogDomain)
+    const cloudMatch = posthogURL.host.match(POSTHOG_CLOUD_HOST_MATCHER)
+    if (cloudMatch && cloudMatch.length > 1) {
+        const region = cloudMatch[1]
+        if (requestedPath.length && requestedPath[0] === 'static') {
+            return { host: `${region}-assets.${POSTHOG_CLOUD_HOST_BASE}`, pathname: requestedPath.slice(1).join('/') }
+        }
+    }
+
+    return { host: posthogURL.host, pathname: requestedPath.join('/') }
+}
+
+/**
  * Generates Next.js rewrites based on PostHog domain,
  * so that PostHog can run in cloud and self-hosted versions without any ad blocker restrictions
  * @example
@@ -31,10 +52,6 @@ export function generateRewrites (postHogDomain: string, routeEndpoint: string):
             {
                 source: `${routeEndpoint}/:path*`,
                 destination: `https://${region}.${POSTHOG_CLOUD_HOST_BASE}/:path*`,
-            },
-            {
-                source: `${routeEndpoint}/decide`,
-                destination: `https://${region}.${POSTHOG_CLOUD_HOST_BASE}/decide`,
             },
         ]
     }
