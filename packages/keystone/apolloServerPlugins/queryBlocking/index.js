a42d776f1a4e5885b22d1f247dbe604f3e8dd340
Matthew <50069872+SavelevMatthew@users.noreply.github.com>
2025-03-14 15:31:29 +0500

    feat(keystone): INFRA-931 add ApolloQueryBlockingPlugin (#5914)
    * refactor(keystone): INFRA-931 reorganize plugins
    
    * feat(keystone): INFRA-931 add ApolloQueryBlockingPlugin
    
    * docs(keystone): INFRA-931 added docs to ApolloQueryBlockingPlugin
    
    * refactor(keystone): INFRA-931 inline error type

---
+++ packages/keystone/apolloServerPlugins/queryBlocking/index.js
@@ -0,0 +1,5 @@
+const { ApolloQueryBlockingPlugin } = require('./plugin')
+
+module.exports = {
+    ApolloQueryBlockingPlugin,
+}
--- packages/keystone/apolloServerPlugins/utils/requests.js
+++ packages/keystone/apolloServerPlugins/utils/requests.js
@@ -1,5 +1,3 @@
-const { PLUGIN_KEY_PREFIX } = require('./constants')
-
 function extractArgValue (valueNode, variables) {
     switch (valueNode.kind) {
         case 'Variable':
@@ -75,39 +73,6 @@ function extractQueriesAndMutationsFromRequest (requestContext) {
     return { queries, mutations }
 }
 
-function buildQuotaKey (identityPrefix, identity) {
-    return [PLUGIN_KEY_PREFIX, identityPrefix, identity].join(':')
-}
-
-function extractQuotaKeyFromRequest (requestContext) {
-    const isAuthed = Boolean(requestContext.context.authedItem)
-    const identifier = isAuthed ? requestContext.context.authedItem.id : requestContext.context.req.ip
-    const identityPrefix = isAuthed ? 'user' : 'ip'
-    const key = buildQuotaKey(identityPrefix, identifier)
-
-    return { isAuthed, key, identifier }
-}
-
-function addComplexity (existingComplexity, newComplexity) {
-    if (!existingComplexity) {
-        return newComplexity
-    }
-
-    return {
-        ...existingComplexity,
-        details: {
-            queries: [...existingComplexity.details.queries, ...newComplexity.details.queries],
-            mutations: [...existingComplexity.details.mutations, ...newComplexity.details.mutations],
-        },
-        queries: existingComplexity.queries + newComplexity.queries,
-        mutations: existingComplexity.mutations + newComplexity.mutations,
-        total: existingComplexity.total + newComplexity.total,
-    }
-}
-
 module.exports = {
     extractQueriesAndMutationsFromRequest,
-    extractQuotaKeyFromRequest,
-    addComplexity,
-    buildQuotaKey,
 }
