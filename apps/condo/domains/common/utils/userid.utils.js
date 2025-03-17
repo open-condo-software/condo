c3c34f3037c6abb725c8569fe0e84eade900de39
Matthew <50069872+SavelevMatthew@users.noreply.github.com>
2025-03-17 13:21:29 +0500

    fix(condo): DOMA-11266 fix updateTourStep payload (#5924)
    * fix(condo): DOMA-11266 fix updateTourStep payload
    
    * fix(condo): DOMA-11266 fix button tracking
    
    * fix(condo): DOMA-11264 update ticket ignore fields
    
    * fix(condo): DOMA-11265 fix meter address search

--- apps/condo/domains/common/utils/userid.utils.js
+++ apps/condo/domains/common/utils/userid.utils.js
@@ -2,15 +2,17 @@ import cookie from 'js-cookie'
 
 import { makeId } from './makeid.utils'
 
+/** @deprecated use one from @open-condo/miniapp-utils */
 function getCurrentUserId () {
-    let current = cookie.get('userId')
+    let current = cookie.get('fingerprint')
     if (!current) {
         current = makeId(12)
     }
-    cookie.set('userId', current, { expires: 365 })
+    cookie.set('fingerprint', current, { expires: 365 })
     return current
 }
 
+/** @deprecated use one from @open-condo/miniapp-utils */
 function getClientSideSenderInfo () {
     return {
         dv: 1,
