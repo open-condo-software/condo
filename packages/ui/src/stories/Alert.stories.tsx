b8fb25284446ab38de00eccd59e27cb021164bb1
Pavel <nomerdvadcatpyat@gmail.com>
2025-03-19 17:36:06 +0500

    feat(condo): DOMA-9723 temp banner alert ui

--- packages/ui/src/stories/Alert.stories.tsx
+++ packages/ui/src/stories/Alert.stories.tsx
@@ -1,4 +1,6 @@
-import { Alert } from '@open-condo/ui/src'
+import React from 'react'
+
+import { Alert, Button, Space } from '@open-condo/ui/src'
 
 import type { Meta, StoryObj } from '@storybook/react'
 
@@ -34,5 +36,21 @@ export const WarningAlert: StoryObj<typeof Alert> = {
 export const ErrorAlert: StoryObj<typeof Alert> = {
     args: {
         type: 'error',
+    },
+}
+
+export const Banner: StoryObj<typeof Alert> = {
+    args: {
+        banner: true,
+        action: (
+            <Space size={16}>
+                <Button type='primary'>
+                    Label
+                </Button>
+                <Button type='secondary'>
+                    Label
+                </Button>
+            </Space>
+        ),
     },
 }
