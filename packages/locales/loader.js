f08fe2a59dea44e808477fc06b26de55f71c5da7
Mikhail Gorbunov <toplenboren@gmail.com>
2025-04-06 22:28:07 +0500

    fix(condo): #5990 Add custom locale support to loader.js

--- packages/locales/loader.js
+++ packages/locales/loader.js
@@ -1,4 +1,3 @@
-// TODO (DOMA-3868) Move this package to app/condo, remove this package and redeclare functions used in other packages locally
 const fs = require('fs')
 const path = require('path')
 const process = require('process')
@@ -16,14 +15,36 @@ const loadTranslations = () => {
     const availableLocales = fs.readdirSync(translationsDir, { withFileTypes: true })
 
     translations = Object.assign({}, ...availableLocales.map(dirent => {
-        if (dirent.isDirectory()) {
-            const locale = dirent.name
-            return { [locale]: require(path.join(translationsDir, locale, `${locale}.json`)) }
-        } else if (dirent.isFile() && dirent.name.endsWith('.json')) {
-            const locale = dirent.name.split('.')[0]
-            return { [locale]: require(path.join(translationsDir, dirent.name)) }
-        } else {
-            throw new Error('Unsupported locale config. Must be [lang].json or [lang]/[lang].json file')
+        try {
+            if (dirent.isDirectory()) {
+                const locale = dirent.name
+
+                const translationsPath = path.join(translationsDir, locale, `${locale}.json`)
+                const translationsContent = JSON.parse(fs.readFileSync(translationsPath, 'utf8'))
+
+                const customTranslationsPath = path.join(translationsDir, locale, `${locale}.custom.json`)
+                const customTranslationsContent = fs.existsSync(customTranslationsPath) ? JSON.parse(fs.readFileSync(customTranslationsPath, 'utf8')) : {}
+
+                console.log(customTranslationsPath)
+                console.log(customTranslationsContent)
+
+                return { [locale]: { ...translationsContent, ...customTranslationsContent } }
+            } else if (dirent.isFile() && dirent.name.endsWith('.json')) {
+                const locale = dirent.name.split('.')[0]
+
+                const isCustomTranslation = dirent.name.endsWith('custom.json')
+                if (isCustomTranslation) {
+                    throw new Error('Custom translations should be inside of folder, like this: [lang]/[lang].custom.json')
+                }
+
+                const filePath = path.join(translationsDir, dirent.name)
+                const content = JSON.parse(fs.readFileSync(filePath, 'utf8'))
+
+                return { [locale]: content }
+            }
+            throw new Error('Found unsupported locale config. Translations must look like [lang].json/[lang].custom.json or [lang]/[lang].json/[lang]/[lang].custom.json')
+        } catch (err) {
+            throw new Error(`Failed to load translations for ${dirent.name}: ${err.message}`)
         }
     }))
 }
@@ -34,48 +55,25 @@ const maybeLoadTranslations = () => {
 
 const getTranslations = (lang = conf.DEFAULT_LOCALE) => {
     maybeLoadTranslations()
-
     return translations[lang] || translations[conf.DEFAULT_LOCALE]
 }
 
 const getAvailableLocales = () => {
     maybeLoadTranslations()
-
     return Object.keys(translations)
 }
 
 const getLocalized = (lang, key) => {
     const translations = getTranslations(lang)
-
     return get(translations, key, key)
 }
 
-
-/**
- * @param {string} code - the translation code written in en.json, ru.json, ...
- * @param {{locale: string?, meta: Object?}?} options
- * @param {string?} options.locale - the language code
- * @param {Object?} options.meta - variables passing to the translation string
- * @returns {string} translated string
- * @example
- * // en.json:
- * {
- *   ...,
- *   "greeting": "Hello, {name}!",
- *   ...,
- * }
- *
- * i18n('greeting', { meta: { name: 'World' } })
- * // => "Hello, World!"
- */
 const i18n = (code, options = { locale: conf.DEFAULT_LOCALE, meta: {} }) => {
     const { locale, meta } = options
-
     maybeLoadTranslations()
-
-    return template(get(translations, [locale, code], code), { interpolate: VARIABLE_REGEXP })(meta)
+    const translation = get(translations, [locale, code].join('.'), code)
+    return template(translation, { interpolate: VARIABLE_REGEXP })(meta)
 }
-
 
 module.exports = {
     getTranslations,
