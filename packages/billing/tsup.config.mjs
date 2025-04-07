7a51ea6024f6b7e6540e59fdb4c8ada25863188d
AleX83Xpert <AleX83Xpert@mail.ru>
2025-04-07 14:08:31 +0500

    feat(condo): DOMA-11309 ts entries were synced with module exports

--- packages/billing/tsup.config.mjs
+++ packages/billing/tsup.config.mjs
@@ -1,7 +1,30 @@
+import fs from 'fs'
+import { createRequire } from 'node:module'
+import path from 'path'
+
 import { defineConfig } from 'tsup'
 
+const require = createRequire(import.meta.url)
+const pkg = require('./package.json')
+const AVAILABLE_EXTENSIONS = ['.ts']
+
+function getInputFileFromExports (exportsPath) {
+    const isDirectory = fs.existsSync(exportsPath) && fs.lstatSync(exportsPath).isDirectory()
+    // NOTE: inputs are controlled by package.json exports field
+    // nosemgrep: javascript.lang.security.audit.path-traversal.path-join-resolve-traversal.path-join-resolve-traversal
+    const pathToFile = isDirectory ? path.join(exportsPath, 'index') : exportsPath
+
+    const ext = AVAILABLE_EXTENSIONS.find(ext => fs.existsSync(`${pathToFile}${ext}`))
+    if (!ext) throw new Error('Unknown extension')
+
+    // NOTE: tsup expect linux-styled path with "/"
+    // https://github.com/egoist/tsup/pull/1300
+    return `${pathToFile}${ext}`.split(path.win32.sep).join(path.posix.sep)
+}
+
 export default defineConfig({
-    entry: ['src/**/*.ts', '!./src/**/*.d.ts', '!./src/**/*.spec.ts'],
+    // nosemgrep: javascript.lang.security.audit.path-traversal.path-join-resolve-traversal.path-join-resolve-traversal
+    entry: Object.keys(pkg.exports).map(entry => getInputFileFromExports(path.join('.', 'src', entry))),
     outDir: './dist/',
     clean: true,
     dts: true,
