083af315cdb65a67110a221460e18d82388afff7
SavelevMatthew <savelevmatthew@gmail.com>
2025-04-17 13:53:10 +0500

    ci(global): INFRA-962 update test script

--- tst.js
+++ tst.js
@@ -1,23 +1,76 @@
 const fs = require('fs')
 
 const COMMENT_LINE_REGEX = /^\s*#/
+const NON_EMPTY_LINE_REGEX = /\S+/
 
-async function main () {
-    const filename = 'eslint.seatbelt.tsv'
-    const text = fs.readFileSync(filename, 'utf8')
+function decodeLine (line, index) {
+    // eslint-disable-next-line no-useless-catch
+    try {
+        const lineParts = line.split('\t')
+        if (lineParts.length !== 3) {
+            throw new Error(
+                `Expected 3 tab-separated JSON strings, instead have ${lineParts.length}`,
+            )
+        }
+        let filename
+        // eslint-disable-next-line no-useless-catch
+        try {
+            filename = JSON.parse(lineParts[0])
+        } catch (e) {
+            // appendErrorContext(e, 'at tab-separated column 1 (filename)')
+            throw e
+        }
 
-    console.log([text])
+        let ruleId
+        // eslint-disable-next-line no-useless-catch
+        try {
+            ruleId = JSON.parse(lineParts[1])
+        } catch (e) {
+            // appendErrorContext(e, 'at tab-separated column 2 (RuleId)')
+            throw e
+        }
 
-    const split = text.split(/(?<=\n)/)
-    const lines = split
-        .filter(
-            (line) =>
-                line !== '' && line !== '\n' && !COMMENT_LINE_REGEX.test(line),
-        )
+        let maxErrors
+        // eslint-disable-next-line no-useless-catch
+        try {
+            maxErrors = JSON.parse(lineParts[2])
+        } catch (e) {
+            // appendErrorContext(e, 'at tab-separated column 3 (maxErrors)')
+            throw e
+        }
 
-    console.log(lines)
+        return {
+            encoded: line,
+            filename,
+            ruleId,
+            maxErrors,
+        }
+    } catch (e) {
+        // appendErrorContext(e, `at line ${index + 1}: \`${line.trim()}\``)
+        throw e
+    }
 }
 
-main().then(() => console.log('ALL DONE'))
+async function main () {
+    const text = fs.readFileSync('eslint.seatbelt.tsv', 'utf8')
 
-console.log(JSON.parse('1\r\n'))
+    console.log('T', [text])
+
+    const split = text.split(/(?<=\n)/)
+    console.log('SPLIT', split)
+
+    const filtered = split
+        .filter(
+            (line) =>
+                NON_EMPTY_LINE_REGEX.test(line) && !COMMENT_LINE_REGEX.test(line),
+        )
+
+    console.log('filtered', filtered)
+
+    const lines = filtered.map(decodeLine)
+
+    console.log('lines', lines)
+}
+
+main()
+
