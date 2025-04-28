d48131c6939390afe961f395f436517e9ead2ebd
Dmitry Kovyazin <dkoviazin@gmail.com>
2025-04-28 18:15:13 +0500

    feat(condo): DOMA-9124 wip

--- apps/condo/domains/organization/integrations/sbbol/sync/syncTokens.js
+++ apps/condo/domains/organization/integrations/sbbol/sync/syncTokens.js
@@ -3,15 +3,17 @@ const { getSbbolSecretStorage } = require('../utils')
 /**
  *
  * @param {TokenSet} tokenInfoFromOAuth
- * @param {String} userId
+ * @param {Object} userInfo
+ * @param {string} userInfo.userId
+ * @param {object} [userInfo.organizationId]
  * @param {boolean} useExtendedConfig
  * @return {Promise<void>}
  */
-const syncTokens = async (tokenInfoFromOAuth, userId, useExtendedConfig = false) => {
+const syncTokens = async (tokenInfoFromOAuth, userInfo, useExtendedConfig = false) => {
     const { access_token, expires_at: expiresAt, refresh_token } = tokenInfoFromOAuth
     const sbbolSecretStorage = getSbbolSecretStorage(useExtendedConfig)
-    await sbbolSecretStorage.setAccessToken(access_token, userId, { expiresAt })
-    await sbbolSecretStorage.setRefreshToken(refresh_token, userId)
+    await sbbolSecretStorage.setAccessToken(access_token, userInfo, { expiresAt })
+    await sbbolSecretStorage.setRefreshToken(refresh_token, userInfo)
 }
 
 module.exports = {
