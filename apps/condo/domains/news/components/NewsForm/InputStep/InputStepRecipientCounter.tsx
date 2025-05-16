d695d357bce3b8343aa0f81cfa366352b8198fc3
Alexander-Turkin <aturkin@doma.ai>
2025-05-16 15:10:54 +0500

    fix(condo): DOMA-11614 add custom counter for telegram news

--- apps/condo/domains/news/components/NewsForm/InputStep/InputStepRecipientCounter.tsx
+++ apps/condo/domains/news/components/NewsForm/InputStep/InputStepRecipientCounter.tsx
@@ -1,17 +1,22 @@
+import { GetNewsSharingRecipientsQuery } from '@app/condo/gql'
 import { B2BAppNewsSharingConfig } from '@app/condo/schema'
 import { Col } from 'antd'
 import React, { useMemo } from 'react'
 
+import { Card, Space } from '@open-condo/ui'
+
 import { useLayoutContext } from '@condo/domains/common/components/LayoutContext'
 import { HiddenBlock } from '@condo/domains/news/components/NewsForm/BaseNewsForm'
-import { MemoizedNewsSharingRecipientCounter, MemoizedRecipientCounter } from '@condo/domains/news/components/RecipientCounter'
+import { Counter, MemoizedNewsSharingRecipientCounter, MemoizedRecipientCounter } from '@condo/domains/news/components/RecipientCounter'
 import { NewsItemScopeNoInstanceType } from '@condo/domains/news/components/types'
+
 
 interface InputStepRecipientCounterProps {
     newsSharingConfig: B2BAppNewsSharingConfig
     isSharingStep: boolean
     sharingAppId: string
     newsItemScopesNoInstance: Array<NewsItemScopeNoInstanceType>
+    newsSharingScope: GetNewsSharingRecipientsQuery['recipients']
 }
 
 export const InputStepRecipientCounter: React.FC<InputStepRecipientCounterProps> = ({
@@ -19,6 +24,7 @@ export const InputStepRecipientCounter: React.FC<InputStepRecipientCounterProps>
     isSharingStep,
     sharingAppId,
     newsItemScopesNoInstance,
+    newsSharingScope,
 }) => {
     const { breakpoints } = useLayoutContext()
 
@@ -26,14 +32,27 @@ export const InputStepRecipientCounter: React.FC<InputStepRecipientCounterProps>
     const formFieldsColSpan = isMediumWindow ? 24 : 14
     const formInfoColSpan = 24 - formFieldsColSpan
 
+    const receiversCount = newsSharingScope?.reduce((acc, el) => {
+        const count = Number(el?.receiversCount) || 0
+        return acc + count
+    }, 0) || 0
+
     const newsSharingRecipientCounter = useMemo(() => <>{isSharingStep && (
-        // TODO: (DOMA-11416) create RecipientCounter for no custom CountersUrl
         newsSharingConfig?.getRecipientsCountersUrl ? (
             <MemoizedNewsSharingRecipientCounter
                 contextId={sharingAppId}
                 newsItemScopes={newsItemScopesNoInstance}
-            /> ) : null
-    )}</>, [newsSharingConfig, sharingAppId, newsItemScopesNoInstance])
+            /> ) :
+            <HiddenBlock hide={!newsSharingScope?.length} className='custom-counter' >
+                <Card title={<Card.CardHeader headingTitle='Статистика' />}>
+                    <Space direction='horizontal' size={24}  className='custom-counter-content'>
+                        <Counter label='Каналов и чатов' value={newsSharingScope?.length}/>
+
+                        <Counter label='Всего подписчкиов' value={receiversCount}/>
+                    </Space>
+                </Card>
+            </HiddenBlock>
+    )}</>, [isSharingStep, newsSharingConfig?.getRecipientsCountersUrl, sharingAppId, newsItemScopesNoInstance, newsSharingScope?.length, receiversCount])
 
     return (
         <Col span={formInfoColSpan} className='recipient-counter'>
