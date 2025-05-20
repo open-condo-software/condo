d3378be65b72a3e33ed0ad6ce9bede9ea7db1874
Viktor Tolmachev <119614372+tolmachev21@users.noreply.github.com>
2025-05-20 18:44:26 +0500

    feat(keystone): DOMA-11468 request limiting plugin (#6110)
    * fix(condo): DOMA-11468 add new plugin
    
    * feat(condo): DOMA-11486 rename env variables
    
    * feat(keystone): DOMA-11468 finish request limiting plugin
    
    * feat(keystone): sync yarn.lock
    
    ---------
    
    Co-authored-by: SavelevMatthew <savelevmatthew@gmail.com>
    (cherry picked from commit 983e6b6c1f3771476cd5f1fa16e9be392475df2d)

--- packages/keystone/apolloServerPlugins/index.js
+++ packages/keystone/apolloServerPlugins/index.js
@@ -1,9 +1,11 @@
 const { ApolloQueryBlockingPlugin } = require('./queryBlocking')
 const { ApolloRateLimitingPlugin } = require('./rateLimiting')
+const { ApolloRequestLimitingPlugin } = require('./requestLimiting')
 const { ApolloSentryPlugin } = require('./sentry')
 
 module.exports = {
     ApolloQueryBlockingPlugin,
     ApolloRateLimitingPlugin,
+    ApolloRequestLimitingPlugin,
     ApolloSentryPlugin,
 }
---
+++ packages/keystone/apolloServerPlugins/requestLimiting/plugin.js
@@ -0,0 +1,57 @@
+const { GQLError, GQLErrorCode: { PAYLOAD_TOO_LARGE } } = require('@open-condo/keystone/errors')
+
+const { validatePluginOptions } = require('./config.utils')
+const {
+    DEFAULT_REQUEST_LENGTH_LIMIT,
+    DEFAULT_VARIABLES_SIZE_LIMIT_IN_BYTES,
+    ROOT_QUERY_LENGTH_EXCEEDED_ERROR_TYPE,
+    VARIABLES_SIZE_EXCEEDED_ERROR_TYPE,
+} = require('./constants')
+const { computeObjectSizeInBytes } = require('./size.utils')
+
+
+class ApolloRequestLimitingPlugin {
+    #queryLengthLimit = DEFAULT_REQUEST_LENGTH_LIMIT
+    #variablesSizeLimit = DEFAULT_VARIABLES_SIZE_LIMIT_IN_BYTES
+
+    constructor (opts = {}) {
+        opts = validatePluginOptions(opts)
+
+        if (typeof opts.queryLengthLimit === 'number') {
+            this.#queryLengthLimit = opts.queryLengthLimit
+        }
+        if (typeof opts.variablesSizeLimit === 'number') {
+            this.#variablesSizeLimit = opts.variablesSizeLimit
+        }
+    }
+
+    requestDidStart () {
+        return {
+            didResolveSource: async (requestContext) => {
+                if (requestContext.source.length > this.#queryLengthLimit) {
+                    throw new GQLError({
+                        code: PAYLOAD_TOO_LARGE,
+                        type: ROOT_QUERY_LENGTH_EXCEEDED_ERROR_TYPE,
+                        message: `GraphQL query length limit exceeded. Query size can not be more than ${this.#queryLengthLimit} characters`,
+                        messageForUser: `api.global.requestLimit.${ROOT_QUERY_LENGTH_EXCEEDED_ERROR_TYPE}`,
+                    }, requestContext.context)
+                }
+
+                const variablesSizeInBytesExcludingFiles = computeObjectSizeInBytes(requestContext.request?.variables || {})
+
+                if (variablesSizeInBytesExcludingFiles > this.#variablesSizeLimit) {
+                    throw new GQLError({
+                        code: PAYLOAD_TOO_LARGE,
+                        type: VARIABLES_SIZE_EXCEEDED_ERROR_TYPE,
+                        message: 'Variables size limit exceeded. Consider using simpler filters or decreasing batch size',
+                        messageForUser: `api.global.requestLimit.${VARIABLES_SIZE_EXCEEDED_ERROR_TYPE}`,
+                    }, requestContext.context)
+                }
+            },
+        }
+    }
+}
+
+module.exports = {
+    ApolloRequestLimitingPlugin,
+}
