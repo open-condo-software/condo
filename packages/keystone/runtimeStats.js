da35f3f0e63079e3b237b36f4cf86f364e5088ec
AleX83Xpert <AleX83Xpert@mail.ru>
2025-05-22 11:30:59 +0500

    feat(condo): DOMA-11665 After code review 1

--- packages/keystone/runtimeStats.js
+++ packages/keystone/runtimeStats.js
@@ -1,11 +1,14 @@
+const crypto = require('crypto')
+
 const express = require('express')
-const camelCase = require('lodash/camelCase')
 
 const conf = require('@open-condo/config')
 const { getLogger } = require('@open-condo/keystone/logging')
 const metrics = require('@open-condo/keystone/metrics')
 
 const { DEFAULT_HEALTHCHECK_URL } = require('./healthCheck')
+
+const SEND_METRICS_INTERVAL_IN_MS = 1000
 
 const X_TARGET_OPTIONS_VAR_NAME = 'X_TARGET_OPTIONS'
 const ACCESS_TOKEN_VAR_NAME = 'RUNTIME_STATS_ACCESS_TOKEN'
@@ -21,6 +24,53 @@ class RuntimeStatsMiddleware {
         statsUrl = '/api/runtime-stats',
     } = {}) {
         this.statsUrl = statsUrl
+        this.requestTargetOptions = (conf[X_TARGET_OPTIONS_VAR_NAME] || '').split(',').filter(Boolean)
+    }
+
+    /**
+     * @private
+     * @param req
+     * @returns {'api'|'graphql'|'oidc'|'wellKnown'|'healthCheck'|'other'}
+     */
+    detectRequestType (req) {
+        const url = new URL(`${conf['SERVER_URL']}${req.url}`).pathname
+
+        const method = (req.method || 'get').toLowerCase()
+        const isPost = method === 'post'
+
+        const isAdminApiUrl = url === '/admin/api'
+        const isApiUrl = url.startsWith('/api')
+        const isOidcUrl = url.startsWith('/oidc')
+        const isWellKnownUrl = url.startsWith('/.well-known')
+        const isHealthCheckUrl = url.startsWith(DEFAULT_HEALTHCHECK_URL)
+
+        /** @type {'api' | 'graphql' | 'oidc' | 'wellKnown' | 'healthCheck' | 'other'} */
+        let requestType = 'other'
+
+        if (isPost && isAdminApiUrl) {
+            requestType = 'graphql'
+        } else if (isApiUrl) {
+            requestType = 'api'
+        } else if (isOidcUrl) {
+            requestType = 'oidc'
+        } else if (isWellKnownUrl) {
+            requestType = 'wellKnown'
+        } else if (isHealthCheckUrl) {
+            requestType = 'healthCheck'
+        }
+
+        return requestType
+    }
+
+    /**
+     * @private
+     * @param req
+     * @returns {string}
+     */
+    detectRequestTarget (req) {
+        const xTargetHeader = req.headers['x-target']
+
+        return this.requestTargetOptions.includes(xTargetHeader) ? xTargetHeader : 'other'
     }
 
     async prepareMiddleware ({ keystone }) {
@@ -51,83 +101,64 @@ class RuntimeStatsMiddleware {
 
                 logger.info({
                     msg: 'current values',
-                    runtimeStats: { activeRequestsIds: Array.from(activeRequestsIds.keys()), ...otherStats },
+                    runtimeStats: otherStats,
+                    data: {
+                        activeRequestsIds: Array.from(activeRequestsIds.keys()).slice(0, 200),
+                        activeRequestsCount: activeRequestsIds.size,
+                    },
                 })
 
                 metrics.gauge({ name: 'runtimeStats.requestsCount.total', value: runtimeStats.totalRequestsCount })
 
                 for (const type of Object.keys(runtimeStats.totalRequestsCountByType)) {
                     metrics.gauge({
-                        name: `runtimeStats.requestsCount.type.${camelCase(type)}.total`,
+                        name: 'runtimeStats.requestsCount.byType.total',
                         value: runtimeStats.totalRequestsCountByType[type],
+                        tags: { type },
                     })
                 }
 
                 for (const target of Object.keys(runtimeStats.totalRequestsCountByTarget)) {
                     metrics.gauge({
-                        name: `runtimeStats.requestsCount.target.${camelCase(target)}.total`,
+                        name: 'runtimeStats.requestsCount.byTarget.total',
                         value: runtimeStats.totalRequestsCountByTarget[target],
+                        tags: { target },
                     })
                 }
-            }, 1000)
+            }, SEND_METRICS_INTERVAL_IN_MS)
         }
 
         // nosemgrep: javascript.express.security.audit.express-check-csurf-middleware-usage.express-check-csurf-middleware-usage
         const app = express()
 
+        const detectRequestType = this.detectRequestType.bind(this)
+        const detectRequestTarget = this.detectRequestTarget.bind(this)
+
         app.use(function runtimeStatsMiddleware (req, res, next) {
             try {
-                const url = new URL(`${conf['SERVER_URL']}${req.url}`).pathname
-                const method = (req.method || 'get').toLowerCase()
-
-                const possibleXTargetValues = (conf[X_TARGET_OPTIONS_VAR_NAME] || '').split(',').filter(Boolean)
-                const xTargetHeader = req.headers['x-target']
-
-                const xTarget = possibleXTargetValues.includes(xTargetHeader) ? xTargetHeader : 'other'
-
-                const isPost = method === 'post'
-
-                const isAdminApiUrl = url === '/admin/api'
-                const isApiUrl = url.startsWith('/api')
-                const isOidcUrl = url.startsWith('/oidc')
-                const isWellKnownUrl = url.startsWith('/.well-known')
-                const isHealthCheckUrl = url.startsWith(DEFAULT_HEALTHCHECK_URL)
-
-                /** @type {'api' | 'graphql' | 'oidc' | 'wellKnown' | 'healthCheck' | 'other'} */
-                let requestType = 'other'
-
-                if (isPost && isAdminApiUrl) {
-                    requestType = 'graphql'
-                } else if (isApiUrl) {
-                    requestType = 'api'
-                } else if (isOidcUrl) {
-                    requestType = 'oidc'
-                } else if (isWellKnownUrl) {
-                    requestType = 'wellKnown'
-                } else if (isHealthCheckUrl) {
-                    requestType = 'healthCheck'
-                }
+                const requestTarget = detectRequestTarget(req)
+                const requestType = detectRequestType(req)
 
                 runtimeStats.activeRequestsIds.add(req.id)
 
                 runtimeStats.activeRequestsCountByType[requestType] = (runtimeStats.activeRequestsCountByType[requestType] || 0) + 1
-                runtimeStats.activeRequestsCountByTarget[xTarget] = (runtimeStats.activeRequestsCountByTarget[xTarget] || 0) + 1
+                runtimeStats.activeRequestsCountByTarget[requestTarget] = (runtimeStats.activeRequestsCountByTarget[requestTarget] || 0) + 1
 
                 runtimeStats.totalRequestsCount = (runtimeStats.totalRequestsCount || 0) + 1
                 runtimeStats.totalRequestsCountByType[requestType] = (runtimeStats.totalRequestsCountByType[requestType] || 0) + 1
-                runtimeStats.totalRequestsCountByTarget[xTarget] = (runtimeStats.totalRequestsCountByTarget[xTarget] || 0) + 1
+                runtimeStats.totalRequestsCountByTarget[requestTarget] = (runtimeStats.totalRequestsCountByTarget[requestTarget] || 0) + 1
 
                 res.on('close', () => {
                     if (requestType) {
                         runtimeStats.activeRequestsIds.delete(req.id)
                         runtimeStats.activeRequestsCountByType[requestType] = Math.max(0, runtimeStats.activeRequestsCountByType[requestType] - 1)
-                        runtimeStats.activeRequestsCountByTarget[xTarget] = Math.max(0, runtimeStats.activeRequestsCountByTarget[xTarget] - 1)
+                        runtimeStats.activeRequestsCountByTarget[requestTarget] = Math.max(0, runtimeStats.activeRequestsCountByTarget[requestTarget] - 1)
                     }
                 })
-            } catch (error) {
+            } catch (err) {
                 logger.error({
                     msg: 'runtimeStatsMiddleware error',
-                    error,
+                    err,
                     data: { url: req.url, method: req.method, runtimeStats },
                 })
             }
@@ -140,7 +171,12 @@ class RuntimeStatsMiddleware {
                 const token = req.query['token']
                 const reqIds = req.query['reqIds']
                 const accessToken = conf[ACCESS_TOKEN_VAR_NAME]
-                if (!!accessToken && accessToken === token) {
+
+                const isTokenOk = !!token && !!accessToken
+                    && token.length === accessToken.length
+                    && crypto.timingSafeEqual(Buffer.from(token), Buffer.from(accessToken))
+
+                if (isTokenOk) {
                     res.json({
                         activeRequestsCount: runtimeStats.activeRequestsIds.size,
                         activeRequestsCountByType: runtimeStats.activeRequestsCountByType,
