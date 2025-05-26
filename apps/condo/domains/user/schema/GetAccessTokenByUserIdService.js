c58ac671c9280abdbeb405b3fc0565088b625aa4
Dmitry Kovyazin <dkoviazin@gmail.com>
2025-05-26 16:23:10 +0500

    feat(condo): DOMA-9124 added organizationId to keys

--- apps/condo/domains/user/schema/GetAccessTokenByUserIdService.js
+++ apps/condo/domains/user/schema/GetAccessTokenByUserIdService.js
@@ -1,7 +1,7 @@
 const { validate: uuidValidate } = require('uuid')
 
 const { GQLError, GQLErrorCode: { BAD_USER_INPUT, INTERNAL_ERROR } } = require('@open-condo/keystone/errors')
-const { GQLCustomSchema } = require('@open-condo/keystone/schema')
+const { GQLCustomSchema, find } = require('@open-condo/keystone/schema')
 
 const { getAccessTokenForUser } = require('@condo/domains/organization/integrations/sbbol/utils')
 const access = require('@condo/domains/user/access/GetAccessTokenByUserIdService')
@@ -16,22 +16,25 @@ const ERRORS = {
         query: 'GetAccessTokenByUserIdService',
         code: INTERNAL_ERROR,
         type: 'REFRESH_TOKEN_EXPIRED',
-        message: 'SBBOL user refreshToken expired',
-        messageForUser: 'api.user.getAccessTokenByUserId.REFRESH_TOKEN_EXPIRED',
+        message: 'User refreshToken expired',
     },
     INVALID_USER_ID: {
         query: 'GetAccessTokenByUserIdService',
         code: BAD_USER_INPUT,
         type: 'INVALID_USER_ID',
         message: 'Received userId must be uuid',
-        messageForUser: 'api.user.getAccessTokenByUserId.INVALID_USER_ID',
+    },
+    INVALID_ORGANIZATION_ID: {
+        query: 'GetAccessTokenByUserIdService',
+        code: BAD_USER_INPUT,
+        type: 'INVALID_ORGANIZATION_ID',
+        message: 'Received organizationId must be uuid',
     },
     ERROR_GETTING_ACCESS_TOKEN: {
         query: 'GetAccessTokenByUserIdService',
         code: INTERNAL_ERROR,
         type: 'ERROR_GETTING_ACCESS_TOKEN',
         message: 'Unresolved error in getAccessTokenForUser',
-        messageForUser: 'api.user.getAccessTokenByUserId.ERROR_GETTING_ACCESS_TOKEN',
     },
 }
 
@@ -39,7 +42,7 @@ const GetAccessTokenByUserIdService = new GQLCustomSchema('GetAccessTokenByUserI
     types: [
         {
             access: true,
-            type: 'input GetAccessTokenByUserIdInput { userId: ID!, type: String! }',
+            type: 'input GetAccessTokenByUserIdInput { userId: ID!, organizationId: ID!, type: ExternalTokenAccessRightTypeType! }',
         },
         {
             access: true,
@@ -58,18 +61,17 @@ const GetAccessTokenByUserIdService = new GQLCustomSchema('GetAccessTokenByUserI
             },
             schema: 'getAccessTokenByUserId (data: GetAccessTokenByUserIdInput!): GetAccessTokenByUserIdOutput',
             resolver: async (parent, args, context, info, extra = {}) => {
-                const { data } = args
-                if (!uuidValidate(data.userId)) throw new GQLError(ERRORS.INVALID_USER_ID, context)
-
-                let accessToken, ttl
-
-                try {
-                    ({ accessToken, ttl } = await getAccessTokenForUser(data.userId))
-                } catch (e) {
-                    if (e.message.includes('refreshToken')) {
-                        throw new GQLError(ERRORS.REFRESH_TOKEN_EXPIRED, context)
-                    }
-                    throw new GQLError(ERRORS.ERROR_GETTING_ACCESS_TOKEN, context)
+                const { data: { userId, organizationId } } = args
+                if (!uuidValidate(userId)) {
+                    throw new GQLError(ERRORS.INVALID_USER_ID, context)
+                }
+                if (!uuidValidate(organizationId)) {
+                    throw new GQLError(ERRORS.INVALID_ORGANIZATION_ID, context)
+                }
+                const { error, accessToken, ttl } =  await getAccessTokenForUser(userId, organizationId)
+                if (error) {
+                    const errorKey = ERRORS.hasOwnProperty(error) ? error : 'ERROR_GETTING_ACCESS_TOKEN'
+                    throw new GQLError(ERRORS[errorKey], context)
                 }
 
                 return { accessToken, ttl }
