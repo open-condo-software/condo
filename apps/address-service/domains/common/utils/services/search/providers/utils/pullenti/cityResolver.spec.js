1ce740cebffa854a01dfef1b2eae3573ab8f6c9c
AleX83Xpert <AleX83Xpert@mail.ru>
2025-06-05 12:10:30 +0500

    feat(address-service): 🛂 DOMA-11602 new tests for pullenti resolvers

---
+++ apps/address-service/domains/common/utils/services/search/providers/utils/pullenti/cityResolver.spec.js
@@ -0,0 +1,54 @@
+const { resolveCity } = require('./cityResolver')
+
+describe('cityResolver', () => {
+    describe('resolveCity', () => {
+        it('should return empty object for null input', () => {
+            expect(resolveCity(null)).toEqual({})
+            expect(resolveCity(undefined)).toEqual({})
+        })
+
+        it('should resolve city with all fields', () => {
+            const cityLevel = {
+                gar: [{
+                    guid: 'test-guid',
+                    area: {
+                        type: 'город',
+                        name: 'Москва',
+                    },
+                    param: [
+                        { '@_name': 'kladrcode', '#text': '7700000000000' },
+                    ],
+                }],
+            }
+
+            expect(resolveCity(cityLevel)).toEqual({
+                city: 'Москва',
+                city_type: 'г',
+                city_type_full: 'город',
+                city_with_type: 'г Москва',
+                city_fias_id: 'test-guid',
+                city_kladr_id: '7700000000000',
+            })
+        })
+
+        it('should handle missing GAR parameters', () => {
+            const cityLevel = {
+                gar: [{
+                    area: {
+                        type: 'город',
+                        name: 'Казань',
+                    },
+                }],
+            }
+
+            expect(resolveCity(cityLevel)).toEqual({
+                city: 'Казань',
+                city_type: 'г',
+                city_type_full: 'город',
+                city_with_type: 'г Казань',
+                city_fias_id: null,
+                city_kladr_id: null,
+            })
+        })
+    })
+})
