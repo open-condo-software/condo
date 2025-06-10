a331b562ed239b8a1df7c976904482576c43bf59
Alexander-Turkin <aturkin@doma.ai>
2025-06-10 19:21:56 +0500

    fix(ui): DOMA-11759 fix styles and stories

--- packages/ui/src/stories/Input/TextArea.stories.tsx
+++ packages/ui/src/stories/Input/TextArea.stories.tsx
@@ -1,22 +1,64 @@
 import React from 'react'
 
-import { Copy } from '@open-condo/icons'
+import * as condoIcons from '@open-condo/icons'
 import { Input as Component } from '@open-condo/ui/src'
 import { Button } from '@open-condo/ui/src'
 
 import type { Meta, StoryObj } from '@storybook/react'
 
+const singleIcons = Object.assign({}, ...Object.entries(condoIcons).map(([key, Icon]) => ({
+    [`${key}-small`]: [<Icon size='small' color='grey' key={key} />],
+    [`${key}-medium`]: [<Icon size='medium' color='grey' key={key} />],
+    ['none']: [],
+})))
+
+const iconCombinations = {
+    'copy-search': [
+        <condoIcons.Copy size='small' color='grey' key='copy' />,
+        <condoIcons.Search size='small' color='grey' key='search' />,
+    ],
+    'edit-copy': [
+        <condoIcons.Edit size='small' color='grey' key='edit' />,
+        <condoIcons.Copy size='small' color='grey' key='copy' />,
+    ],
+    'lock-eye': [
+        <condoIcons.Lock size='small' color='grey' key='lock' />,
+        <condoIcons.Eye size='small' color='grey' key='eye' />,
+        <condoIcons.FileUp size='small' color='grey' key='eye-invisible' />,
+    ],
+    'full-set': [
+        <condoIcons.Copy size='small' color='grey' key='copy' />,
+        <condoIcons.Search size='small' color='grey' key='search' />,
+        <condoIcons.Edit size='small' color='grey' key='edit' />,
+        <condoIcons.Trash size='small' color='grey' key='trash' />,
+    ],
+}
+
+const allPanelUtilsOptions = {
+    ...singleIcons,
+    ...iconCombinations,
+}
+
 export default {
-    title: 'Components/Textarea',
+    title: 'Components/Input',
     component: Component.TextArea,
     args: {
         placeholder: 'Placeholder',
-        bottomPanelUtils: [<Copy key={1} size='small' color='grey' />],
-        submitButton: <Button.Icon><Copy key={1} size='small' /></Button.Icon>,
+        bottomPanelUtils: allPanelUtilsOptions['copy-search'],
+        submitButton: <Button.Icon><condoIcons.ChevronUp size='small' /></Button.Icon>,
         autoSize: { minRows: 1, maxRows: 4 },
         disabled: false,
         rows: 1,
         showCount: true,
+    },
+    argTypes: {
+        bottomPanelUtils: {
+            options: Object.keys(allPanelUtilsOptions),
+            mapping: allPanelUtilsOptions,
+            control: {
+                type: 'select',
+            },
+        },
     },
 } as Meta<typeof Component.TextArea>
 
