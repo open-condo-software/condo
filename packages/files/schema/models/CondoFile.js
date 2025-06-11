f947c0e89d0569891fa6452508fb57d8af1c1e3c
Sitnikov Ivan <sit7602@gmail.com>
2025-06-11 13:35:13 +0500

    fix(global): INFRA-1009 update docs for CondoFile model

--- packages/files/schema/models/CondoFile.js
+++ packages/files/schema/models/CondoFile.js
@@ -2,21 +2,20 @@ const FileAdapter = require('@open-condo/keystone/fileAdapter/fileAdapter')
 const { historical, versioned, uuided, tracked, softDeleted, dvAndSender } = require('@open-condo/keystone/plugins')
 const { GQLListSchema } = require('@open-condo/keystone/schema')
 
-
 const Adapter = new FileAdapter('condofiles', false)
 
 const CondoFile = new GQLListSchema('CondoFile', {
-    schemaDoc: 'TODO',
+    schemaDoc: 'File uploaded to platform and meta with ownership data',
     fields: {
         file: {
             type: 'CustomFile',
             adapter: Adapter,
             isRequired: true,
-            schemaDoc: 'TODO',
+            schemaDoc: 'File uploaded to platform',
         },
         meta: {
             type: 'Json',
-            schemaDoc: 'TODO',
+            schemaDoc: 'Untyped metadata for this object. May be used by custom integrations.',
             isRequired: false,
         },
     },
