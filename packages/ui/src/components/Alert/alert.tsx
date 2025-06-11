b7e76977a09f30c71aa8e7031086e7e8583ba467
abshnko <2009add@gmail.com>
2025-06-11 15:21:51 +0300

    feat(ui): DOMA-11779 added expandable description prop to Alert component

--- packages/ui/src/components/Alert/alert.tsx
+++ packages/ui/src/components/Alert/alert.tsx
@@ -1,19 +1,82 @@
 import { Alert as DefaultAlert, AlertProps as DefaultAlertProps } from 'antd'
-import React from 'react'
+import classNames from 'classnames'
+import React, { useState } from 'react'
+
+import { ChevronDown, ChevronUp } from '@open-condo/icons'
+
+import { Typography } from '../Typography'
 
 type CondoAlertProps = {
     type: 'success' | 'info' | 'warning' | 'error'
+    expandable?: boolean
+    maxLines?: number
+    showMoreText?: string
+    showLessText?: string
 }
 
 const ALERT_CLASS_PREFIX = 'condo-alert'
 
 export type AlertProps = Pick<DefaultAlertProps, 'message' | 'showIcon' | 'description' | 'banner' | 'action'> & CondoAlertProps
 
-const Alert: React.FC<AlertProps> = (props) => {
+const ALERT_DESCRIPTION_MAX_HEIGHT = '300px'
+const ALERT_DEFAULT_SHOW_MORE_TEXT = 'Show more'
+const ALERT_DEFAULT_SHOW_LESS_TEXT = 'Show less'
+
+const Alert: React.FC<AlertProps> = ({
+    description,
+    maxLines,
+    showMoreText,
+    showLessText,
+    ...rest
+}) => {
+    const [isExpanded, setIsExpanded] = useState(false)
+    const toggleDescription = () => setIsExpanded(!isExpanded)
+
+    const renderDescription = () => {
+        if (!description) return null
+
+        return (
+            <>
+                <div style={{
+                    maxHeight: maxLines && !isExpanded ? 'none' : ALERT_DESCRIPTION_MAX_HEIGHT,
+                    overflowY: 'auto',
+                }}>
+                    <Typography.Paragraph ellipsis={maxLines && !isExpanded ? { rows: maxLines } : false} >
+                        {description}
+                    </Typography.Paragraph>
+                </div>
+                {renderToggle()}
+            </>
+        )
+    }
+
+    const renderToggle = () => {
+        if (!maxLines || !description) return null
+
+        const toggleText = isExpanded ? showLessText || ALERT_DEFAULT_SHOW_LESS_TEXT : showMoreText || ALERT_DEFAULT_SHOW_MORE_TEXT
+
+        const icon = isExpanded ? 
+            <ChevronUp className={`${ALERT_CLASS_PREFIX}-toggle-icon`} size='small' /> : 
+            <ChevronDown className={`${ALERT_CLASS_PREFIX}-toggle-icon`} size='small' />
+
+        return (
+            <div className={`${ALERT_CLASS_PREFIX}-toggle-container`}>
+                <Typography.Link onClick={toggleDescription} >
+                    {toggleText}
+                </Typography.Link>
+                {icon}
+            </div>
+        )
+    }
+    
     return (
         <DefaultAlert
-            {...props}
+            {...rest}
             prefixCls={ALERT_CLASS_PREFIX}
+            description={renderDescription()}
+            className={classNames({
+                [`${ALERT_CLASS_PREFIX}-expandable`]: !!maxLines,
+            })}
         />
     )
 }
--- packages/ui/src/stories/Alert.stories.tsx
+++ packages/ui/src/stories/Alert.stories.tsx
@@ -39,6 +39,27 @@ export const ErrorAlert: StoryObj<typeof Alert> = {
     },
 }
 
+export const AlertWithExpandableDescriptionAndAction: StoryObj<typeof Alert> = {
+    args: {
+        type: 'warning',
+        maxLines: 3,
+        showLessText: 'Show less text',
+        showMoreText: 'Show more text',
+        description: 'Super long description example Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam tristique a nunc at blandit. Aliquam viverra sem a odio semper, sed ultricies purus commodo. Proin sit amet metus vehicula, volutpat mauris pharetra, malesuada lectus. Etiam ultrices leo est, at hendrerit nisi blandit ac. Aenean eleifend, urna vitae elementum dignissim, est erat volutpat nisi, vitae blandit dolor massa eget felis. Sed molestie dui quis turpis pulvinar, non viverra tellus suscipit. Sed a posuere turpis. Ut purus tellus, vehicula a lobortis et, tristique eu nunc.',
+        action: (
+            <Space size={16} wrap width='100%'>
+                <Button type='secondary'>
+                    Cancel
+                </Button>
+                <Button type='primary'>
+                    Save
+                </Button>
+            </Space>
+        ),
+    },
+}
+
+
 export const Banner: StoryObj<typeof Alert> = {
     args: {
         type: 'info',
