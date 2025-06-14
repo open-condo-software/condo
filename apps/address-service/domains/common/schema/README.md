a15ea3f61473e321c3a1e9924cd2b7b81c5ef4b9
SavelevMatthew <savelevmatthew@gmail.com>
2025-06-14 20:22:08 +0500

    feat(global)!: TEMPORARY remove all apps except condo

--- apps/address-service/domains/common/schema/README.md
+++
@@ -1,5 +0,0 @@
-Each **model** have **own file** with the same name as the model. 
-
-All model files are located inside the domain directory.
-
-You can read more detail about domain logic in the [domains/README](../../README.md) file.
--- apps/dev-portal-web/domains/common/constants/buildVars.ts
+++
@@ -1,9 +0,0 @@
-
-// NOTE: These values are used in getStaticProps, so they should be available at build time.
-// You can use different values in yarn start and yarn build, which can cause some bugs in CI envs
-// That's the reason they're moved away from publicRuntimeConfig, and only accessible from process.env
-// https://github.com/vercel/next.js/discussions/11493#discussioncomment-14606
-export const DOCS_ROOT_PATH = process.env.DOCS_ROOT_PATH || 'docs'
-export const DOCS_REPO = process.env.DOCS_REPO || 'https://github.com/open-condo-software/condo'
-export const DOCS_REPO_DOCS_ROOT = process.env.DOCS_REPO_DOCS_ROOT || 'apps/dev-portal-web/docs'
-export const DOCS_EDIT_BRANCH = process.env.DOCS_EDIT_BRANCH || 'main'
