632c4eb5641fa495e3917f1b92987b0d8c9bd216
Sitnikov Ivan <sit7602@gmail.com>
2025-06-17 16:31:29 +0500

    refactor(global): INFRA-1009 specify new file format

--- packages/files/schema/models/CondoFile.js
+++ packages/files/schema/models/CondoFile.js
@@ -1,22 +1,13 @@
-const FileAdapter = require('@open-condo/keystone/fileAdapter/fileAdapter')
 const { historical, versioned, uuided, tracked, softDeleted, dvAndSender } = require('@open-condo/keystone/plugins')
 const { GQLListSchema } = require('@open-condo/keystone/schema')
-
-const Adapter = new FileAdapter('condofiles', false)
 
 const CondoFile = new GQLListSchema('CondoFile', {
     schemaDoc: 'File uploaded to platform and meta with ownership data',
     fields: {
         file: {
-            type: 'CustomFile',
-            adapter: Adapter,
+            type: 'Json',
             isRequired: true,
             schemaDoc: 'File uploaded to platform',
-        },
-        meta: {
-            type: 'Json',
-            schemaDoc: 'Untyped metadata for this object. May be used by custom integrations.',
-            isRequired: false,
         },
     },
     plugins: [uuided(), versioned(), tracked(), softDeleted(), dvAndSender(), historical()],
