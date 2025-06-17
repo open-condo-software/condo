482a7a27f516c525da532cff69301b81b2c17ceb
Sitnikov Ivan <sit7602@gmail.com>
2025-06-17 11:48:07 +0500

    fix(global): INFRA-1009 extended meta field validation

--- packages/keystone/fields/CustomFile/index.js
+++ packages/keystone/fields/CustomFile/index.js
@@ -3,7 +3,7 @@ const { File } = require('@keystonejs/fields')
 const { CustomFile } = require('./Implementation')
 
 module.exports = {
-    type: 'CustomFile',
+    type: 'File',
     implementation: CustomFile,
     views: File.views,
     adapters: File.adapters,
