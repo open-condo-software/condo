31ba0ade506c88f0cc1a56bf1443736438c22a24
YEgorLu <elukianchikov@doma.ai>
2025-06-18 11:25:15 +0500

    chore(resident-app): DOMA-11150 fix lint / knip / semgrep issues

--- knip.js
+++ knip.js
@@ -128,7 +128,7 @@ async function config () {
             ignoreDependencies: ['@graphql-codegen/typescript'],
         },
         'apps/resident-app': {
-            entry: ['domains/common/utils/sw.ts'],
+            entry: ['domains/common/utils/sw.ts', 'domains/telegram-bot/utils/bot.js', 'domains/telegram-bot/middlewares/i18n.js'],
         },
         'packages/icons': {
             ignoreDependencies: ['@svgr/plugin-svgo', '@svgr/plugin-prettier', '@svgr/plugin-jsx'],
