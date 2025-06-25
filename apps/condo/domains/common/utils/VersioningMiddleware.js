6acc6dc84cc7988139cc3636bee9fd9d4dd542fa
Alllex202 <alllex.202@gmail.com>
2025-06-25 18:03:56 +0500

    fix(condo): DOMA-11838 replaced app.use(url, ...cb)

--- apps/condo/domains/common/utils/VersioningMiddleware.js
+++ apps/condo/domains/common/utils/VersioningMiddleware.js
@@ -9,7 +9,7 @@ class VersioningMiddleware {
         // this route can not be used for csrf attack (because no cookies and tokens are used in a public route)
         // nosemgrep: javascript.express.security.audit.express-check-csurf-middleware-usage.express-check-csurf-middleware-usage
         const app = express()
-        app.use('/api/version', (req, res) => {
+        app.get('/api/version', (req, res) => {
             res.status(200).json({
                 build: getCurrentVersion(),
             })
