f0df70057c8a7c5e474ab26e52c269373a8c3ac9
Alexander Naydenov <AleX83Xpert@mail.ru>
2025-07-09 19:34:01 +0500

    fix(condo): DOMA-11909 manage body height style dynamically in AppFrameWrapper (#6359)
    * fix(condo): DOMA-11909 manage body height style dynamically in AppFrameWrapper
    
    * fix(condo): DOMA-11909 refactor style management in AppFrameWrapper for consistency
    
    * fix(condo): DOMA-11909 improve type annotations for style element in AppFrameWrapper

--- apps/condo/domains/miniapp/components/AppFrameWrapper.tsx
+++ apps/condo/domains/miniapp/components/AppFrameWrapper.tsx
@@ -1,13 +1,9 @@
-import { css, Global } from '@emotion/react'
 import React, { useEffect } from 'react'
 
 import bridge from '@open-condo/bridge'
 
-const BODY_RESIZE_STYLES = css`
-  body {
-    height: auto;
-  }
-`
+const TAG_ID = 'condo-app-frame-wrapper-styles'
+const COUNTER_DATA_ATTR_NAME = 'data-same-style-requests-counter'
 
 export const AppFrameWrapper: React.FC = ({ children }) => {
     useEffect(() => {
@@ -21,9 +17,37 @@ export const AppFrameWrapper: React.FC = ({ children }) => {
         return () => observer.unobserve(document.body)
     }, [])
 
+    useEffect(() => {
+        let styleElement: HTMLStyleElement | null = document.querySelector(`style#${TAG_ID}`)
+
+        if (styleElement) {
+            const count = parseInt(styleElement.getAttribute(COUNTER_DATA_ATTR_NAME) || '0', 10)
+            styleElement.setAttribute(COUNTER_DATA_ATTR_NAME, String(count + 1))
+        } else {
+            styleElement = document.createElement('style')
+            styleElement.id = TAG_ID
+            styleElement.textContent = 'body{height:auto;}'
+            styleElement.setAttribute(COUNTER_DATA_ATTR_NAME, '1')
+            document.head.appendChild(styleElement)
+        }
+
+        return () => {
+            const styleEl: HTMLStyleElement | null = document.querySelector(`style#${TAG_ID}`)
+            if (styleEl) {
+                const count = parseInt(styleEl.getAttribute(COUNTER_DATA_ATTR_NAME) || '1', 10)
+                if (count <= 1) {
+                    if (styleEl.parentNode) {
+                        styleEl.parentNode.removeChild(styleEl)
+                    }
+                } else {
+                    styleEl.setAttribute(COUNTER_DATA_ATTR_NAME, String(count - 1))
+                }
+            }
+        }
+    }, [])
+
     return (
         <>
-            <Global styles={BODY_RESIZE_STYLES}/>
             {children}
         </>
     )
