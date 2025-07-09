04aab0f44a9adfe814e90e0886734d1af74e9a0e
AleX83Xpert <AleX83Xpert@mail.ru>
2025-07-09 14:48:49 +0500

    fix(condo): DOMA-11909 manage body height style dynamically in AppFrameWrapper

--- apps/condo/domains/miniapp/components/AppFrameWrapper.tsx
+++ apps/condo/domains/miniapp/components/AppFrameWrapper.tsx
@@ -1,13 +1,6 @@
-import { css, Global } from '@emotion/react'
 import React, { useEffect } from 'react'
 
 import bridge from '@open-condo/bridge'
-
-const BODY_RESIZE_STYLES = css`
-  body {
-    height: auto;
-  }
-`
 
 export const AppFrameWrapper: React.FC = ({ children }) => {
     useEffect(() => {
@@ -21,9 +14,40 @@ export const AppFrameWrapper: React.FC = ({ children }) => {
         return () => observer.unobserve(document.body)
     }, [])
 
+    useEffect(() => {
+        const tagId = 'fix-body-height-style-tag'
+        const counterAttrName = 'same-style-requests-counter'
+
+        let styleEl = document.querySelector(`#${tagId}`) as HTMLStyleElement | null
+
+        if (styleEl) {
+            const count = parseInt(styleEl.getAttribute(counterAttrName) || '0', 10)
+            styleEl.setAttribute(counterAttrName, String(count + 1))
+        } else {
+            styleEl = document.createElement('style')
+            styleEl.id = tagId
+            styleEl.textContent = 'body{height:auto;}'
+            styleEl.setAttribute(counterAttrName, '1')
+            document.head.appendChild(styleEl)
+        }
+
+        return () => {
+            const styleEl = document.querySelector(`#${tagId}`) as HTMLStyleElement | null
+            if (styleEl) {
+                const count = parseInt(styleEl.getAttribute(counterAttrName) || '1', 10)
+                if (count <= 1) {
+                    if (styleEl.parentNode) {
+                        styleEl.parentNode.removeChild(styleEl)
+                    }
+                } else {
+                    styleEl.setAttribute(counterAttrName, String(count - 1))
+                }
+            }
+        }
+    }, [])
+
     return (
         <>
-            <Global styles={BODY_RESIZE_STYLES}/>
             {children}
         </>
     )
