447097b026a8f75183099ec08fdeec60d4774b4d
Sitnikov Ivan <sit7602@gmail.com>
2025-07-10 12:02:03 +0500

    test(condo): INFRA-1093 add more integration tests for externalAuth

--- apps/condo/domains/user/schema/externalAuth.spec.js
+++ apps/condo/domains/user/schema/externalAuth.spec.js
@@ -4,6 +4,10 @@ jest.mock('@open-condo/config', () => {
         if (name === 'PASSPORT_GITHUB') {
             return '{"clientId": "123", "clientSecret": "321", "callbackUrl": "/api/github/auth/callback", "name": "github", "isEmailTrusted": true, "isPhoneTrusted": false}'
         }
+
+        if (name === 'USER_EXTERNAL_IDENTITY_TYPES') {
+            return '["github"]'
+        }
         return conf[name]
     }
     return new Proxy(conf, { get, set: jest.fn() })
@@ -18,14 +22,47 @@ const { fetch } = require('@open-condo/keystone/fetch')
 const { makeLoggedInAdminClient } = require('@open-condo/keystone/test.utils')
 const { setFakeClientMode } = require('@open-condo/keystone/test.utils')
 
+const { UserExternalIdentity, UserAdmin } = require('@condo/domains/user/utils/testSchema')
+
+
+const mockGithubUrls = (userId, userEmail) => {
+    nock('https://github.com')
+        .post('/login/oauth/access_token')
+        .reply(200, {
+            access_token: 'some_access_token',
+            scope:'user:email',
+            'token_type': 'bearer',
+        })
+
+    const githubUserResponse = {
+        'login': userId,
+        'id': userId,
+        'name': userId,
+        'email': userEmail,
+    }
+
+    nock('https://api.github.com')
+        .get('/user')
+        .reply(200, githubUserResponse)
+
+    nock('https://api.github.com')
+        .get('/user/emails')
+        .reply(200,
+            [
+                {
+                    'email': userEmail,
+                    'verified': true,
+                    'primary': true,
+                    'visibility': 'public',
+                },
+            ])
+}
 
 describe('external authentication', () => {
     setFakeClientMode(index)
     let admin
     let serverUrl
     let agent
-
-    const OLD_ENV = JSON.parse(JSON.stringify(process.env))
 
     beforeAll(async () => {
         admin = await makeLoggedInAdminClient()
@@ -35,7 +72,6 @@ describe('external authentication', () => {
     })
     afterAll(() => {
         nock.cleanAll()
-        process.env = { ...OLD_ENV }
     })
 
     describe('Validation', () => {
@@ -61,92 +97,137 @@ describe('external authentication', () => {
 
     describe('GitHub authentication flow', () => {
         test('should create user, userExternalIdentity and return oidc token', async () => {
-            nock('https://github.com')
-                .get('/login/oauth/authorize')
-                .query(true)
-                .reply(302, undefined,  { location: `${serverUrl}/api/github/auth/callback?code=some_gh_code` })
-
-            nock('https://github.com')
-                .post('/login/oauth/access_token')
-                .reply(200, {
-                    access_token: 'some_access_token',
-                    scope:'user:email',
-                    'token_type': 'bearer',
-                })
-
             const fakedUserId = faker.datatype.uuid()
-            const fakedUserEmail = faker.datatype.uuid() + faker.internet.email()
+            const fakedUserEmail = faker.datatype.uuid() + '@gmail.com'
+            mockGithubUrls(fakedUserId, fakedUserEmail)
 
-            const githubUserResponse = {
-                'login': fakedUserId,
-                'id': fakedUserId,
-                'node_id': 'MDQ6VXNlcjE=',
-                'avatar_url': 'https://github.com/images/error/octocat_happy.gif',
-                'gravatar_id': '',
-                'url': 'https://api.github.com/users/octocat',
-                'html_url': 'https://github.com/octocat',
-                'followers_url': 'https://api.github.com/users/octocat/followers',
-                'following_url': 'https://api.github.com/users/octocat/following{/other_user}',
-                'gists_url': 'https://api.github.com/users/octocat/gists{/gist_id}',
-                'starred_url': 'https://api.github.com/users/octocat/starred{/owner}{/repo}',
-                'subscriptions_url': 'https://api.github.com/users/octocat/subscriptions',
-                'organizations_url': 'https://api.github.com/users/octocat/orgs',
-                'repos_url': 'https://api.github.com/users/octocat/repos',
-                'events_url': 'https://api.github.com/users/octocat/events{/privacy}',
-                'received_events_url': 'https://api.github.com/users/octocat/received_events',
-                'type': 'User',
-                'site_admin': false,
-                'name': fakedUserId,
-                'company': 'GitHub',
-                'blog': 'https://github.com/blog',
-                'location': 'San Francisco',
-                'email': fakedUserEmail,
-                'hireable': false,
-                'bio': 'There once was...',
-                'twitter_username': 'monatheoctocat',
-                'public_repos': 2,
-                'public_gists': 1,
-                'followers': 20,
-                'following': 0,
-                'created_at': '2008-01-14T04:33:35Z',
-                'updated_at': '2008-01-14T04:33:35Z',
-                'private_gists': 81,
-                'total_private_repos': 100,
-                'owned_private_repos': 100,
-                'disk_usage': 10000,
-                'collaborators': 8,
-                'two_factor_authentication': true,
-                'plan': {
-                    'name': 'Medium',
-                    'space': 400,
-                    'private_repos': 20,
-                    'collaborators': 0,
-                },
-            }
-
-            nock('https://api.github.com')
-                .get('/user')
-                .reply(200, githubUserResponse)
-
-            nock('https://api.github.com')
-                .get('/user/emails')
-                .reply(200,
-                    [
-                        {
-                            'email': fakedUserEmail,
-                            'verified': true,
-                            'primary': true,
-                            'visibility': 'public',
-                        },
-                    ])
             const response = await agent.get('/api/github/auth?userType=staff')
             expect(response.statusCode).toEqual(302)
 
             const tokenResponse = await agent.get('/api/github/auth/callback?code=some_gh_code')
+
+            expect(tokenResponse.statusCode).toEqual(200)
             expect(tokenResponse.body).toHaveProperty('accessToken')
             expect(tokenResponse.body).toHaveProperty('scope', 'openid')
             expect(tokenResponse.body).toHaveProperty('tokenType', 'Bearer')
-            expect(tokenResponse.statusCode).toEqual(200)
+
+            const userMeta = {
+                email: fakedUserEmail,
+                phone: null,
+                provider: 'github',
+            }
+
+            const userExternalIdentity = await UserExternalIdentity.getOne(admin, { identityId: fakedUserId })
+            expect(userExternalIdentity).toHaveProperty('identityType', 'github')
+            expect(userExternalIdentity).toHaveProperty('user')
+            expect(userExternalIdentity).toHaveProperty('userType', 'staff')
+            expect(userExternalIdentity.meta).toMatchObject(userMeta)
+
+            const user = await UserAdmin.getOne(admin, { id: userExternalIdentity.user.id })
+
+            expect(user).toHaveProperty('email', fakedUserEmail.toLowerCase())
+            expect(user).toHaveProperty('type', 'staff')
+            expect(user).toHaveProperty('isEmailVerified', true)
+            expect(user).toHaveProperty('isPhoneVerified', false)
+            expect(user).toHaveProperty('isAdmin', false)
+            expect(user).toHaveProperty('isSupport', false)
+            expect(user.meta).toMatchObject(userMeta)
+        })
+
+        test('should be able to authorize already existing user', async () => {
+            let userExternalIdentity, user
+            const fakedUserId = faker.datatype.uuid()
+            const fakedUserEmail = faker.datatype.uuid() + '@gmail.com'
+
+            userExternalIdentity = await UserExternalIdentity.getOne(admin, { identityId: fakedUserId })
+            expect(userExternalIdentity).toBeUndefined()
+            mockGithubUrls(fakedUserId, fakedUserEmail)
+
+            // Register new user and external identity
+            const loginResponse = await agent.get('/api/github/auth?userType=staff')
+            expect(loginResponse.statusCode).toEqual(302)
+            const response = await agent.get('/api/github/auth/callback?code=some_gh_code')
+            expect(response.statusCode).toEqual(200)
+            expect(response.body).toHaveProperty('accessToken')
+
+            userExternalIdentity = await UserExternalIdentity.getOne(admin, { identityId: fakedUserId })
+            expect(userExternalIdentity).toHaveProperty('identityType', 'github')
+            expect(userExternalIdentity).toHaveProperty('user')
+            expect(userExternalIdentity).toHaveProperty('userType', 'staff')
+
+            user = await UserAdmin.getOne(admin, { id: userExternalIdentity.user.id })
+            expect(user).toHaveProperty('email', fakedUserEmail.toLowerCase())
+
+            // Call authorization flow second time to make sure it's not creating another user
+            mockGithubUrls(fakedUserId, fakedUserEmail)
+            await agent.get('/api/github/auth?userType=staff')
+            const reauthResponse = await agent.get('/api/github/auth/callback?code=some_gh_code')
+            expect(reauthResponse.statusCode).toEqual(200)
+            expect(reauthResponse.body).toHaveProperty('accessToken')
+            expect(reauthResponse.body.accessToken).not.toEqual(response.body.accessToken)
+
+            const existingUserExternalIdentity = await UserExternalIdentity.getOne(admin, { identityId: fakedUserId })
+            expect(existingUserExternalIdentity.id).toEqual(userExternalIdentity.id)
+        })
+
+        test('should be able to register resident and staff users for one email as well', async () => {
+            const fakedUserId = faker.datatype.uuid()
+            const fakedUserEmail = faker.datatype.uuid() + '@gmail.com'
+            mockGithubUrls(fakedUserId, fakedUserEmail)
+
+            // Register staff user via first request
+            const staffResponse = await agent.get('/api/github/auth?userType=staff')
+            expect(staffResponse.statusCode).toEqual(302)
+
+            const staffTokenResponse = await agent.get('/api/github/auth/callback?code=some_gh_code')
+
+            expect(staffTokenResponse.statusCode).toEqual(200)
+            expect(staffTokenResponse.body).toHaveProperty('accessToken')
+
+            const staffUserExternalIdentity = await UserExternalIdentity.getOne(admin, { identityId: fakedUserId, userType: 'staff' })
+            expect(staffUserExternalIdentity).toHaveProperty('identityType', 'github')
+            expect(staffUserExternalIdentity).toHaveProperty('user')
+            expect(staffUserExternalIdentity).toHaveProperty('userType', 'staff')
+
+            const staffUser = await UserAdmin.getOne(admin, { id: staffUserExternalIdentity.user.id })
+
+            expect(staffUser).toHaveProperty('email', fakedUserEmail.toLowerCase())
+            expect(staffUser).toHaveProperty('type', 'staff')
+            expect(staffUser).toHaveProperty('isEmailVerified', true)
+            expect(staffUser).toHaveProperty('isPhoneVerified', false)
+            expect(staffUser).toHaveProperty('isAdmin', false)
+            expect(staffUser).toHaveProperty('isSupport', false)
+
+            // Register resident user with same credentials (email)
+            mockGithubUrls(fakedUserId, fakedUserEmail)
+            const residentResponse = await agent.get('/api/github/auth?userType=resident')
+            expect(residentResponse.statusCode).toEqual(302)
+
+            const residentTokenResponse = await agent.get('/api/github/auth/callback?code=some_gh_code')
+
+            expect(residentTokenResponse.statusCode).toEqual(200)
+            expect(residentTokenResponse.body).toHaveProperty('accessToken')
+
+            const residentUserExternalIdentity = await UserExternalIdentity.getOne(admin, { identityId: fakedUserId, userType: 'resident' })
+            expect(residentUserExternalIdentity).toHaveProperty('identityType', 'github')
+            expect(residentUserExternalIdentity).toHaveProperty('user')
+            expect(residentUserExternalIdentity).toHaveProperty('userType', 'resident')
+
+            const residentUser = await UserAdmin.getOne(admin, { id: residentUserExternalIdentity.user.id })
+
+            expect(residentUser).toHaveProperty('email', fakedUserEmail.toLowerCase())
+            expect(residentUser).toHaveProperty('type', 'resident')
+            expect(residentUser).toHaveProperty('isEmailVerified', true)
+            expect(residentUser).toHaveProperty('isPhoneVerified', false)
+            expect(residentUser).toHaveProperty('isAdmin', false)
+            expect(residentUser).toHaveProperty('isSupport', false)
+
+            expect(residentUser.id).not.toEqual(staffUser.id)
+            expect(residentUserExternalIdentity.id).not.toEqual(staffUserExternalIdentity.id)
+            expect(residentUserExternalIdentity.user.id).not.toEqual(staffUserExternalIdentity.user.id)
+            expect(residentUser.id).toEqual(residentUserExternalIdentity.user.id)
+            expect(staffUser.id).toEqual(staffUserExternalIdentity.user.id)
+            expect(residentTokenResponse.body.accessToken).not.toEqual(staffTokenResponse.body.accessToken)
         })
     })
 })
