354bbebe341b301e4949d4baabe4ac48d377ca39
Alexander-Turkin <aturkin@doma.ai>
2025-07-10 16:20:56 +0500

    feat(icons): add arrowUp and sparkles icons (#6368)
    (cherry picked from commit 7798246a39eeda5ca592b07cbeeadf44c28fc399)

---
+++ packages/icons/src/components/Sparkles.tsx
@@ -0,0 +1,47 @@
+/** This file is auto-generated, do not edit it manually **/
+import React from 'react'
+
+import { IconWrapper, IconProps } from '../wrappers'
+
+export const Sparkles: React.FC<IconProps> = ({ svgProps: props, ...restProps }) => {
+    return (
+        <IconWrapper
+            icon={
+                <svg
+                    width='inherit'
+                    height='inherit'
+                    fill='none'
+                    xmlns='http://www.w3.org/2000/svg'
+                    viewBox='0 0 24 24'
+                    {...props}
+                >
+                    <path
+                        fillRule='evenodd'
+                        clipRule='evenodd'
+                        d='M11.094 1.305a1.5 1.5 0 0 1 2.351.79l.005.02 1.581 6.135a1 1 0 0 0 .719.719l6.135 1.58.016.005a1.5 1.5 0 0 1 0 2.892l-.016.004-6.135 1.581a1 1 0 0 0-.719.719l-1.582 6.135-.005.02a1.5 1.5 0 0 1-2.889 0l-.005-.02-1.581-6.135a1.001 1.001 0 0 0-.719-.719L2.115 13.45l-.023-.006a1.5 1.5 0 0 1 0-2.886l.023-.006L8.25 8.968a1 1 0 0 0 .719-.718l1.582-6.135.005-.02a1.5 1.5 0 0 1 .538-.79ZM12 4.503l1.095 4.247a3.001 3.001 0 0 0 2.155 2.155L19.498 12l-4.248 1.095a3.001 3.001 0 0 0-2.155 2.155L12 19.497l-1.095-4.246a3.001 3.001 0 0 0-2.155-2.156L4.504 12l4.246-1.096a3.001 3.001 0 0 0 2.155-2.154L12 4.503Z'
+                        fill='currentColor'
+                    />
+                    <path
+                        fillRule='evenodd'
+                        clipRule='evenodd'
+                        d='M20 2a1 1 0 0 1 1 1v4a1 1 0 1 1-2 0V3a1 1 0 0 1 1-1Z'
+                        fill='currentColor'
+                    />
+                    <path
+                        fillRule='evenodd'
+                        clipRule='evenodd'
+                        d='M17 5a1 1 0 0 1 1-1h4a1 1 0 1 1 0 2h-4a1 1 0 0 1-1-1ZM4 16a1 1 0 0 1 1 1v2a1 1 0 1 1-2 0v-2a1 1 0 0 1 1-1Z'
+                        fill='currentColor'
+                    />
+                    <path
+                        fillRule='evenodd'
+                        clipRule='evenodd'
+                        d='M2 18a1 1 0 0 1 1-1h2a1 1 0 1 1 0 2H3a1 1 0 0 1-1-1Z'
+                        fill='currentColor'
+                    />
+                </svg>
+            }
+            {...restProps}
+        />
+    )
+}
