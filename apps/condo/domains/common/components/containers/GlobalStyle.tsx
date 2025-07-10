a7a1930796477a14232d7866bb707ae26d770c28
Daria Abushenko <2009add@gmail.com>
2025-07-10 17:42:32 +0500

    fix(condo): DOMA-11922 fixed double borders in tables (#6369)
    * fix(condo): DOMA-11922 added !important to condo table border: none to fix double borders
    
    * fix(condo): DOMA-11922 added !important to condo table border-right: none to fix double borders
    
    (cherry picked from commit 308c83112635fe9e399330c6376235a30c149b6a)

--- apps/condo/domains/common/components/containers/GlobalStyle.tsx
+++ apps/condo/domains/common/components/containers/GlobalStyle.tsx
@@ -230,7 +230,7 @@ export default function GlobalStyle () {
               .ant-table.ant-table-bordered > .ant-table-container > .ant-table-header > table > tfoot > tr > td,
               .ant-table.ant-table-bordered > .ant-table-container > .ant-table-body > table > tfoot > tr > td {
                 &:not(:last-of-type) {
-                  border-right: none;
+                  border-right: none !important;
                 }
               }
               
@@ -270,11 +270,11 @@ export default function GlobalStyle () {
               .ant-table.ant-table-bordered > .ant-table-container > .ant-table-body > table > tbody > tr > td,
               .ant-table.ant-table-bordered > .ant-table-container > .ant-table-content > table > tbody > tr > td,
               .ant-table.ant-table-bordered > .ant-table-container > .ant-table-content > table > thead > tr > th {
-                border-right: none;
+                border-right: none !important;
               }
               
               .ant-table.ant-table-bordered > .ant-table-container > .ant-table-content > table, .ant-table.ant-table-bordered > .ant-table-container > .ant-table-header > table {
-                border: none;
+                border: none !important;
               }
               
               .ant-table.ant-table-bordered div::-webkit-scrollbar-track {
