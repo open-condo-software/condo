246ffac9780e1067dcbb00739dcc8ea257f61485
Alexander-Turkin <aturkin@doma.ai>
2025-07-10 15:21:34 +0500

    feat(icons): add arrowUp and sparkles icons

---
+++ packages/icons/src/components/ArrowUp.tsx
@@ -0,0 +1,29 @@
+/** This file is auto-generated, do not edit it manually **/
+import React from 'react'
+
+import { IconWrapper, IconProps } from '../wrappers'
+
+export const ArrowUp: React.FC<IconProps> = ({ svgProps: props, ...restProps }) => {
+    return (
+        <IconWrapper
+            icon={
+                <svg
+                    width='inherit'
+                    height='inherit'
+                    fill='none'
+                    xmlns='http://www.w3.org/2000/svg'
+                    viewBox='0 0 24 24'
+                    {...props}
+                >
+                    <path
+                        fillRule='evenodd'
+                        clipRule='evenodd'
+                        d='M4.293 12.707a1 1 0 0 1 0-1.414l7-7a1 1 0 0 1 1.414 0l7 7a1 1 0 0 1-1.414 1.414L13 7.414V19a1 1 0 1 1-2 0V7.414l-5.293 5.293a1 1 0 0 1-1.414 0Z'
+                        fill='currentColor'
+                    />
+                </svg>
+            }
+            {...restProps}
+        />
+    )
+}
