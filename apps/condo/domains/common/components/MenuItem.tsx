f57d073db1c97605bb3c078e3a5137909389b5f4
Mikhail Gorbunov <toplenboren@gmail.com>
2025-07-14 21:25:39 +0500

    chore(condo): replace emotion for ResidentActions and MenuItem (#6384)
    * chore(condo): replace emotion for ResidentActions and MenuItem
    
    * chore(condo): fixes on review
    
    * chore(condo): fixes on review

--- apps/condo/domains/common/components/MenuItem.tsx
+++ apps/condo/domains/common/components/MenuItem.tsx
@@ -1,22 +1,18 @@
-import styled from '@emotion/styled'
 import classnames from 'classnames'
 import { useRouter } from 'next/router'
-import React, { useCallback, useMemo, useState } from 'react'
+import React, { useCallback, useState } from 'react'
 
 import { useDeepCompareEffect } from '@open-condo/codegen/utils/useDeepCompareEffect'
-import type { IconProps } from '@open-condo/icons'
 import { useIntl } from '@open-condo/next/intl'
 import { Space, Typography } from '@open-condo/ui'
-import { colors } from '@open-condo/ui/colors'
 
 import { Tooltip } from '@condo/domains/common/components/Tooltip'
-import { transitions } from '@condo/domains/common/constants/style'
 import { analytics } from '@condo/domains/common/utils/analytics'
 import { renderLink } from '@condo/domains/common/utils/Renders'
 import { getEscaped } from '@condo/domains/common/utils/string.utils'
 import { INoOrganizationToolTipWrapper } from '@condo/domains/onboarding/hooks/useNoOrganizationToolTip'
 
-import { useLayoutContext } from './LayoutContext'
+import styles from './MenuItem.module.css'
 
 
 interface IMenuItemWrapperProps {
@@ -25,43 +21,6 @@ interface IMenuItemWrapperProps {
     labelFontSize?: string
     className?: string
 }
-
-const MenuItemWrapper = styled.div<IMenuItemWrapperProps>`
-  cursor: pointer;
-  padding: ${props => props.padding ? props.padding : '12px 0'};
-  display: flex;
-  border-radius: 8px;
-  flex-direction: row;
-  align-items: center;
-  justify-content: ${({ isCollapsed }) => isCollapsed ? 'center' : 'flex-start'};
-  vertical-align: center;
-  color: ${colors.gray['7']};
-  
-  &:hover,
-  &.active {
-    color: ${colors.black};
-  }
-
-  .condo-typography, 
-  .icon {
-    transition: ${transitions.allDefault};
-  }
-  
-  .condo-typography {
-    width: max-content;
-  }
-
-  // NOTE: Fix width to reduce flick effect on collapse / expand
-  &.side:not(.width-full) {
-    .condo-typography {
-      width: 155px;
-    }
-  }
-
-  &.disabled {
-    opacity: 0.4;
-  }
-`
 
 interface IMenuItemProps {
     id: string
@@ -88,11 +47,6 @@ const addToolTipForCollapsedMenu = (content: JSX.Element, Message: string) => (
     </Tooltip>
 )
 
-const MenuItemIconProps: IconProps = {
-    size: 'medium',
-    className: 'icon',
-}
-
 export const MenuItem: React.FC<IMenuItemProps> = (props) => {
     const {
         id,
@@ -108,7 +62,6 @@ export const MenuItem: React.FC<IMenuItemProps> = (props) => {
         excludePaths = [],
         labelRaw,
     } = props
-    const { breakpoints } = useLayoutContext()
     const router = useRouter()
     const asPath = router.asPath
     const intl = useIntl()
@@ -139,30 +92,39 @@ export const MenuItem: React.FC<IMenuItemProps> = (props) => {
         ? label
         : intl.formatMessage({ id: label as FormatjsIntl.Message['ids'] })
 
-    const menuItemClassNames = classnames(wrapperClassName, {
-        'side': breakpoints.TABLET_LARGE,
-        'active': isActive,
-        'disabled': disabled,
-    })
-
-    const linkContent = isCollapsed
-        ? (
-            <Icon {...MenuItemIconProps} />
-        ) : (
-            <Space size={12} align='center' direction='horizontal' className='menu-item'>
-                <Icon {...MenuItemIconProps} />
-                <Typography.Title ellipsis={{ rows: 2 }} level={5}>
-                    {Message}
-                </Typography.Title>
-            </Space>
-        )
+    const linkContent = (
+        <Space size={12} align='center' direction='horizontal' className={styles.menuItem}>
+            <Icon size='medium' />
+            { !isCollapsed && <Typography.Title ellipsis={{ rows: 2 }} level={5}>
+                {Message}
+            </Typography.Title> }
+        </Space>
+    )
 
     const menuItemIdProp = id ? { id: id } : {}
 
+    const menuItemClassName = classnames(
+        styles.menuItemWrapper,
+        {
+            [styles.justifyCenter]: isCollapsed,
+            [styles.active]: isActive,
+            [styles.disabled]: disabled,
+        },
+        wrapperClassName,
+        'side'
+    )
+
     const menuItem = (
-        <MenuItemWrapper onClick={handleClick} className={menuItemClassNames} isCollapsed={isCollapsed} {...menuItemIdProp} {...restWrapperProps}>
-            {(isCollapsed && !disabled) ? addToolTipForCollapsedMenu(linkContent, Message) : linkContent}
-        </MenuItemWrapper>
+        <div
+            onClick={handleClick}
+            className={menuItemClassName}
+            {...menuItemIdProp}
+            {...restWrapperProps}
+        >
+            {(isCollapsed && !disabled)
+                ? addToolTipForCollapsedMenu(linkContent, Message)
+                : linkContent}
+        </div>
     )
 
     const nextjsLink = !path || disabled ? menuItem : renderLink(menuItem, path, false)
--- apps/condo/domains/common/components/ResidentActions/ResidentActions.tsx
+++ apps/condo/domains/common/components/ResidentActions/ResidentActions.tsx
@@ -1,12 +1,10 @@
-import styled from '@emotion/styled'
-import { Divider, Dropdown, DropDownProps, Menu } from 'antd'
-import React, { CSSProperties, useCallback, useMemo, useState } from 'react'
+import React, { useCallback, useMemo, useState } from 'react'
 
 import { Readings, NewAppeal, Smartphone, Plus } from '@open-condo/icons'
 import type { IconProps } from '@open-condo/icons'
 import { useIntl } from '@open-condo/next/intl'
 import { useOrganization } from '@open-condo/next/organization'
-import { Button } from '@open-condo/ui'
+import { Button, Dropdown, DropdownProps } from '@open-condo/ui'
 
 import { useLayoutContext } from '@condo/domains/common/components/LayoutContext'
 import { MenuItem } from '@condo/domains/common/components/MenuItem'
@@ -15,76 +13,11 @@ import { useSearchByPhoneModal } from '@condo/domains/common/hooks/useSearchByPh
 import { searchByPhone } from '@condo/domains/contact/utils/clientCard'
 import { useTicketVisibility } from '@condo/domains/ticket/contexts/TicketVisibilityContext'
 
-
-export const StyledMenu = styled(Menu)`
-  box-sizing: border-box;
-  border-radius: 8px;
-`
+import styles from './ResidentActions.module.css'
 
 export const ResidentAppealDropDownMenuItemWrapperProps = {
     labelFontSize: fontSizes.label,
-    padding: '16px 24px 16px 16px',
     className: 'width-full',
-}
-
-const DIVIDER_STYLES: CSSProperties = { margin: 0 }
-
-const ResidentAppealDropdownOverlay = ({ setIsSearchByPhoneModalVisible, setDropdownVisible }) => {
-    const { employee } = useOrganization()
-    const canReadContacts = employee?.role?.canReadContacts || false
-    const canReadTickets = employee?.role?.canReadTickets || false
-    const canManageTickets = employee?.role?.canManageTickets || false
-    const canManageMeterReadings = employee?.role?.canManageMeterReadings || false
-    const showSearchButton = canReadContacts && canReadTickets
-
-    const handleButtonClick = useCallback(() => {
-        setDropdownVisible(false)
-        setIsSearchByPhoneModalVisible(true)
-    }, [setIsSearchByPhoneModalVisible, setDropdownVisible])
-
-    return (
-        <StyledMenu>
-            {
-                showSearchButton && (
-                    <>
-                        <MenuItem
-                            id='menu-item-search-by-phone'
-                            onClick={handleButtonClick}
-                            menuItemWrapperProps={ResidentAppealDropDownMenuItemWrapperProps}
-                            icon={Smartphone}
-                            label='SearchByPhoneNumber'
-                        />
-                        {(canManageTickets || canManageMeterReadings) && <Divider style={DIVIDER_STYLES}/>}
-                    </>
-                )
-            }
-            {
-                canManageTickets && (
-                    <>
-                        <MenuItem
-                            id='menu-item-create-appeal'
-                            menuItemWrapperProps={ResidentAppealDropDownMenuItemWrapperProps}
-                            path='/ticket/create'
-                            icon={NewAppeal}
-                            label='CreateTicket'
-                        />
-                        {canManageMeterReadings && <Divider style={DIVIDER_STYLES}/>}
-                    </>
-                )
-            }
-            {
-                canManageMeterReadings && (
-                    <MenuItem
-                        id='menu-item-create-meter-reading'
-                        menuItemWrapperProps={ResidentAppealDropDownMenuItemWrapperProps}
-                        path='/meter/create?tab=meter-reading'
-                        icon={Readings}
-                        label='CreateMeterReading'
-                    />
-                )
-            }
-        </StyledMenu>
-    )
 }
 
 const DROPDOWN_POPUP_CONTAINER_ID = 'residentActionsPopupContainer'
@@ -120,14 +53,7 @@ export const ResidentActions: React.FC<IResidentActionsProps> = (props) => {
 
     const [dropdownVisible, setDropdownVisible] = useState<boolean>()
 
-    const Overlay = useMemo(() => (
-        <ResidentAppealDropdownOverlay
-            setIsSearchByPhoneModalVisible={setIsSearchByPhoneModalVisible}
-            setDropdownVisible={setDropdownVisible}
-        />
-    ), [setIsSearchByPhoneModalVisible, setDropdownVisible])
-
-    const trigger: DropDownProps['trigger'] = useMemo(() => isMobile ? ['click'] : ['hover'], [isMobile])
+    const trigger: DropdownProps['trigger'] = useMemo(() => isMobile ? ['click'] : ['hover'], [isMobile])
     const iconSize: IconProps['size'] = minified ? 'medium' : 'small'
 
     const canReadTickets = employee?.role?.canReadTickets || false
@@ -135,24 +61,62 @@ export const ResidentActions: React.FC<IResidentActionsProps> = (props) => {
     const canManageMeterReadings = employee?.role?.canManageMeterReadings || false
     const canReadContacts = employee?.role?.canReadContacts || false
 
+    const openSearchByPhoneModal = useCallback(() => {
+        setDropdownVisible(false)
+        setIsSearchByPhoneModalVisible(true)
+    }, [setIsSearchByPhoneModalVisible, setDropdownVisible])
+
     if (!canReadTickets && !canReadContacts && !canManageTickets && !canManageMeterReadings) {
         return null
     }
 
+    const showSearchButton = canReadContacts && canReadTickets
+
+    const items = [
+        showSearchButton && {
+            key: 'menu-item-search-by-phone',
+            label: <MenuItem
+                id='menu-item-search-by-phone'
+                onClick={openSearchByPhoneModal}
+                menuItemWrapperProps={ResidentAppealDropDownMenuItemWrapperProps}
+                icon={Smartphone}
+                label='SearchByPhoneNumber'
+            />,
+        },
+        canManageTickets && {
+            key: 'menu-item-create-appeal',
+            label: <MenuItem
+                id='menu-item-create-appeal'
+                menuItemWrapperProps={ResidentAppealDropDownMenuItemWrapperProps}
+                path='/ticket/create'
+                icon={NewAppeal}
+                label='CreateTicket'
+            />,
+        },
+        canManageMeterReadings && {
+            key: 'menu-item-create-meter-reading',
+            label: <MenuItem
+                id='menu-item-create-meter-reading'
+                menuItemWrapperProps={ResidentAppealDropDownMenuItemWrapperProps}
+                path='/meter/create?tab=meter-reading'
+                icon={Readings}
+                label='CreateMeterReading'
+            />,
+        },
+    ]
+
     return (
         <div id={DROPDOWN_POPUP_CONTAINER_ID}>
             <Dropdown
-                overlay={Overlay}
-                placement='bottomRight'
+                overlayClassName={styles.menuWrapper}
+                menu={{ items }}
                 getPopupContainer={getPopupContainer}
                 trigger={trigger}
-                visible={dropdownVisible}
-                overlayClassName='appeals-dropdown-menu'
-                onVisibleChange={setDropdownVisible}
+                open={dropdownVisible}
+                onOpenChange={setDropdownVisible}
             >
-                {/* NOTE: you need to use `div` wrapper because of warning: Function components cannot be given refs. Attempts to access this ref will fail. Did you mean to use React.forwardRef() */}
                 <div>
-                    <Button type='secondary' block icon={<Plus size={iconSize}/>} className='appeals-button'>
+                    <Button type='accent' className={styles.accentButton} block icon={<Plus size={iconSize}/>}>
                         {!minified && ResidentAppealMessage}
                     </Button>
                 </div>
