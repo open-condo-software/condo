0ae2233d53d12c22b2a9c4aa931c7ba9ca4fc598
Alllex202 <alllex.202@gmail.com>
2025-07-17 19:17:58 +0500

    feat(condo): DOMA-11966 setup review stand for passapp

--- apps/condo/bin/prepare-review-deploy.js
+++ apps/condo/bin/prepare-review-deploy.js
@@ -5,6 +5,7 @@ const { escapeRegExp } = require('lodash')
 const conf = require('@open-condo/config')
 const { prepareKeystoneExpressApp } = require('@open-condo/keystone/prepareKeystoneApp')
 
+const { B2BApp } = require('@condo/domains/miniapp/utils/serverSchema')
 const { OidcClient } = require('@condo/domains/user/utils/serverSchema')
 
 
@@ -27,17 +28,30 @@ function getReviewPrefix ({ reviewBaseDomain }) {
     return match?.[1] || ''
 }
 
-let REVIEW_PREFIX = ''
-function updateUrls (obj, { devBaseDomain, reviewBaseDomain }) {
+/**
+ * Tries to change the url from the dev stand to the review stand.
+ * If this is not the url of the dev stand, it will return the original string
+ *
+ * @param {string} value
+ * @param {string} devBaseDomain
+ * @param {string} reviewBaseDomain
+ * @return {string}
+ */
+function tryChangeDevUrlToReviewUrl (value, { devBaseDomain, reviewBaseDomain }) {
     // controlled devBaseDomain
     // nosemgrep: javascript.lang.security.audit.detect-non-literal-regexp.detect-non-literal-regexp
     const devAppsUrlRegex = new RegExp(`https://([a-zA-Z0-9-]+)\\.${escapeRegExp(devBaseDomain)}(\\/[^\\s]*)?`, 'g')
 
+    return value.replace(devAppsUrlRegex, (match, subdomain, path = '') => {
+        return `https://${REVIEW_PREFIX}${subdomain}.${reviewBaseDomain}${path}`
+    })
+}
+
+let REVIEW_PREFIX = ''
+function updateOidcConfigUrls (obj, { devBaseDomain, reviewBaseDomain }) {
     function traverse (current) {
         if (typeof current === 'string') {
-            return current.replace(devAppsUrlRegex, (match, subdomain, path = '') => {
-                return `https://${REVIEW_PREFIX}${subdomain}.${reviewBaseDomain}${path}`
-            })
+            return tryChangeDevUrlToReviewUrl(current, { devBaseDomain, reviewBaseDomain })
         } else if (Array.isArray(current)) {
             return current.map(item => traverse(item))
         } else if (typeof current === 'object' && current !== null) {
@@ -70,7 +84,7 @@ async function migrateOidcClientUrlsToReview ({ devBaseDomain, reviewBaseDomain 
     const updatedOidcClients = oidcClients.map((client) => {
         return {
             ...client,
-            payload: updateUrls(client.payload, { devBaseDomain, reviewBaseDomain }),
+            payload: updateOidcConfigUrls(client.payload, { devBaseDomain, reviewBaseDomain }),
         }
     })
 
@@ -87,6 +101,36 @@ async function migrateOidcClientUrlsToReview ({ devBaseDomain, reviewBaseDomain 
     }
 }
 
+/**
+ * Changes all urls in B2BApp.appUrl from devBaseDomain to reviewBaseDomain with prefix
+ * @return {Promise<void>}
+ */
+async function migrateB2BAppUrlsToReview ({ devBaseDomain, reviewBaseDomain }) {
+    const context = await getContext()
+
+    const b2bApps = await B2BApp.getAll(context, {
+        deletedAt: null,
+    }, 'id appUrl dv sender { dv fingerprint }')
+
+    const updatedB2BApps = b2bApps.map((b2bApp) => {
+        return {
+            ...b2bApp,
+            appUrl: tryChangeDevUrlToReviewUrl(b2bApp.appUrl, { devBaseDomain, reviewBaseDomain }),
+        }
+    })
+
+    for (const b2bApp of updatedB2BApps) {
+        try {
+            await B2BApp.update(context, b2bApp.id, {
+                appUrl: b2bApp.appUrl,
+                dv: b2bApp.dv,
+                sender: b2bApp.sender,
+            })
+        } catch (error) {
+            console.error(error)
+        }
+    }
+}
 
 async function main (args) {
     const [devBaseDomain, reviewBaseDomain] = args
@@ -95,6 +139,8 @@ async function main (args) {
     REVIEW_PREFIX = getReviewPrefix({ reviewBaseDomain })
 
     await migrateOidcClientUrlsToReview({ devBaseDomain, reviewBaseDomain })
+
+    await migrateB2BAppUrlsToReview({ devBaseDomain, reviewBaseDomain })
 }
 
 main(process.argv.slice(2))
