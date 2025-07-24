4a55417a63c899d79aba138402a8fe4c1e6461c9
Pavel <52532264+nomerdvadcatpyat@users.noreply.github.com>
2025-07-24 13:44:27 +0500

    fix(condo): DOMA-11985 fix platform notifications sound (#6427)
    * fix(condo): DOMA-11985 fix platform notifications sound
    
    Compare last message created at instead new messages count
    
    * fix(condo): DOMA-11985 fix read messages with several tabs
    
    Used broadcast channel for update readUserMessagesAt in other opened tabs

--- apps/condo/domains/notification/contexts/UserMessagesListContext.tsx
+++ apps/condo/domains/notification/contexts/UserMessagesListContext.tsx
@@ -13,6 +13,7 @@ import React, {
 import { useAuth } from '@open-condo/next/auth'
 import { useOrganization } from '@open-condo/next/organization'
 
+import { useBroadcastChannel } from '@condo/domains/common/hooks/useBroadcastChannel'
 import { analytics } from '@condo/domains/common/utils/analytics'
 import { useAllowedToFilterMessageTypes } from '@condo/domains/notification/hooks/useAllowedToFilterMessageTypes'
 import { useUserMessages } from '@condo/domains/notification/hooks/useUserMessages'
@@ -58,6 +59,8 @@ const UserMessageListContext = createContext<UserMessagesListContextType>({
 })
 
 export const useUserMessagesList = () => useContext(UserMessageListContext)
+
+const READ_USER_MESSAGES_AT_BROADCAST_CHANNEL = 'read-user-messages-at'
 
 type UserMessagesListContextProviderProps = {
     children: ReactNode
@@ -113,14 +116,22 @@ export const UserMessagesListContextProvider: React.FC<UserMessagesListContextPr
         setIsNotificationSoundEnabled(isSoundEnabled)
     }, [organizationId, userId, userMessagesSettingsStorage])
 
+    const { sendMessageToBroadcastChannel: sendReadUserMessagesAtToBroadcast } = useBroadcastChannel<string>(
+        READ_USER_MESSAGES_AT_BROADCAST_CHANNEL,
+        (newestMessageCreatedAt) => {
+            setReadUserMessagesAt(newestMessageCreatedAt)
+        }
+    )
+
     const updateReadUserMessagesAt = useCallback(() => {
         const newestMessageCreatedAt = userMessages?.[0]?.createdAt
 
         if (new Date(newestMessageCreatedAt) > new Date(readUserMessagesAt)) {
             setReadUserMessagesAt(newestMessageCreatedAt)
             userMessagesSettingsStorage.setReadUserMessagesAt(newestMessageCreatedAt)
+            sendReadUserMessagesAtToBroadcast(newestMessageCreatedAt)
         }
-    }, [readUserMessagesAt, userMessages, userMessagesSettingsStorage])
+    }, [readUserMessagesAt, sendReadUserMessagesAtToBroadcast, userMessages, userMessagesSettingsStorage])
 
     const handleDropdownOpenChange = useCallback((isOpen: boolean) => {
         setIsDropdownOpen(isOpen)
