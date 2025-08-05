0b812af8bad906332dea70912ddd32e7e943a4c4
Alllex202 <alllex.202@gmail.com>
2025-08-05 18:17:13 +0500

    feat(condo): DOMA-11997 added ChangeUserPasswordService

--- apps/condo/domains/user/utils/serverSchema/requestLimitHelpers.js
+++ apps/condo/domains/user/utils/serverSchema/requestLimitHelpers.js
@@ -17,7 +17,7 @@ const { RedisGuard } = require('@condo/domains/user/utils/serverSchema/guards')
 
 
 const REDIS_GUARD = new RedisGuard()
-const MAX_REQUESTS_BY_API_PER_DAY = Number(conf['MAX_REQUESTS_FOR_IP_PER_DAY']) || MAX_REQUESTS_FOR_IP_PER_DAY
+const MAX_REQUESTS_BY_IP_PER_DAY = Number(conf['MAX_REQUESTS_FOR_IP_PER_DAY']) || MAX_REQUESTS_FOR_IP_PER_DAY
 const MAX_REQUESTS_BY_PHONE_PER_DAY = Number(conf['MAX_REQUESTS_FOR_PHONE_PER_DAY']) || MAX_REQUESTS_FOR_PHONE_PER_DAY
 const MAX_REQUESTS_BY_EMAIL_PER_DAY = Number(conf['MAX_REQUESTS_FOR_EMAIL_PER_DAY']) || MAX_REQUESTS_FOR_EMAIL_PER_DAY
 const MAX_REQUESTS_BY_USER_PER_DAY = Number(conf['MAX_REQUESTS_FOR_USER_PER_DAY']) || MAX_REQUESTS_FOR_USER_PER_DAY
@@ -29,11 +29,11 @@ const PHONE_WHITE_LIST = conf.PHONE_WHITE_LIST ? JSON.parse(conf.PHONE_WHITE_LIS
 const EMAIL_WHITE_LIST = conf.EMAIL_WHITE_LIST ? JSON.parse(conf.EMAIL_WHITE_LIST) : []
 const USER_ID_WHITE_LIST = conf.USER_ID_WHITE_LIST ? JSON.parse(conf.USER_ID_WHITE_LIST) : []
 
-const checkDailyRequestLimitCountersByIp = async (context, prefix, rawIp) => {
+const checkDailyRequestLimitCountersByIp = async (context, prefix, rawIp, maxRequests = MAX_REQUESTS_BY_IP_PER_DAY) => {
     const ip = rawIp.split(':').pop()
     const key = [prefix, 'ip', ip].filter(Boolean).join(':')
     const byIpCounter = await REDIS_GUARD.incrementDayCounter(key)
-    if (byIpCounter > MAX_REQUESTS_BY_API_PER_DAY && !IP_WHITE_LIST.includes(ip)) {
+    if (byIpCounter > maxRequests && !IP_WHITE_LIST.includes(ip)) {
         throw new GQLError(GQL_ERRORS.DAILY_REQUEST_LIMIT_FOR_IP_REACHED, context)
     }
 }
