dd28252ada407097a004c5fb89cfbc44c8877b5d
Mikhail Gorbunov <toplenboren@gmail.com>
2025-08-18 20:40:28 +0500

    chore(condo): skip ai tests (#6544)

--- apps/condo/domains/ai/schema/ExecutionAIFlowTask.test.js
+++ apps/condo/domains/ai/schema/ExecutionAIFlowTask.test.js
@@ -36,7 +36,7 @@ const {
 } = require('@condo/domains/user/utils/testSchema')
 
 
-describe('ExecutionAIFlowTask', () => {
+describe.skip('ExecutionAIFlowTask', () => {
     let adminClient, supportClient, userClient, userClient2, anonymousClient
 
     // In envs the full url is specified, so we must specify the port
