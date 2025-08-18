29ea7651f09b0e89239ca251de867b1fb824a234
Sitnikov Ivan <sit7602@gmail.com>
2025-08-18 14:03:27 +0500

    chore(global): INFRA-1009 temp upload files from ui through new api

--- apps/condo/domains/ticket/components/BaseTicketForm/index.tsx
+++ apps/condo/domains/ticket/components/BaseTicketForm/index.tsx
@@ -632,7 +632,7 @@ export const TicketSourceSelect: React.FC = () => {
     const intl = useIntl()
     const TicketSourceLabel = intl.formatMessage({ id: 'pages.condo.ticket.field.Source.label' })
     const LoadingMessage = intl.formatMessage({ id: 'Loading' })
-    
+
     const {
         data: sourcesData,
         loading,
@@ -775,6 +775,8 @@ export const BaseTicketForm: React.FC<ITicketFormProps> = (props) => {
         Model: TicketFile,
         relationField: 'ticket',
         initialFileList: files,
+        appId: 'condo',
+        modelNames: ['TicketFile'],
     })
 
     const { ContactsEditorComponent } = useContactsEditorHook({
