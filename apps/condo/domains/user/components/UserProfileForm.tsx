8deb4d6ef7487bf6ced03e4c31b9324aadd7244f
Alllex202 <alllex.202@gmail.com>
2025-09-05 14:15:56 +0500

    feat(condo): DOMA-12201 new flow for email change

--- apps/condo/domains/user/components/UserProfileForm.tsx
+++ apps/condo/domains/user/components/UserProfileForm.tsx
@@ -1,4 +1,4 @@
-import { useUpdateUserMutation } from '@app/condo/gql'
+import { useUpdateUserMutation, useChangeUserEmailMutation } from '@app/condo/gql'
 import { Col, Form, Row } from 'antd'
 import getConfig from 'next/config'
 import Link from 'next/link'
@@ -12,11 +12,13 @@ import { ActionBar, Button, Typography, Input } from '@open-condo/ui'
 
 import { FormWithAction } from '@condo/domains/common/components/containers/FormList'
 import Prompt from '@condo/domains/common/components/Prompt'
+import { useMutationErrorHandler } from '@condo/domains/common/hooks/useMutationErrorHandler'
 import { useValidations } from '@condo/domains/common/hooks/useValidations'
+import { normalizeEmail } from '@condo/domains/common/utils/mail'
 import { EMAIL_ALREADY_REGISTERED_ERROR } from '@condo/domains/user/constants/errors'
 import { PHONE_TYPE } from '@condo/domains/user/constants/identifiers'
 
-import { UserAvatar } from './UserAvatar'
+import { useSudoToken } from './SudoTokenProvider'
 
 
 const INPUT_LAYOUT_PROPS = {
@@ -57,19 +59,103 @@ export const UserProfileForm: React.FC = () => {
     const CancelLabel = intl.formatMessage({ id: 'Cancel' })
 
     const { user } = useAuth()
-    const [updateUserAction] = useUpdateUserMutation({
-        onCompleted: () => router.push('/user'), 
-    })
-    const formAction = (formValues) => updateUserAction({
-        variables: {
-            id: user.id,
-            data: {
-                ...formValues,
-                dv: 1,
-                sender: getClientSideSenderInfo(),
-            },
+
+    const { getSudoTokenWithModal, clearSudoToken } = useSudoToken()
+
+    const [form] = Form.useForm()
+    const errorHandler = useMutationErrorHandler({
+        form,
+        typeToFieldMapping: {
+            WRONG_EMAIL_VALUE: 'email',
         },
     })
+    
+    const [updateUserMutation] = useUpdateUserMutation({
+        onError: errorHandler,
+    })
+    const [changeUserEmailMutation] = useChangeUserEmailMutation({
+        onError: (error) => {
+            const isExpiredToken = error?.graphQLErrors?.some((gqlError) => gqlError.extensions?.type === 'TOKEN_NOT_FOUND')
+            const isExistsEmail = error?.graphQLErrors?.some(gqlError => (gqlError.extensions?.message as string)?.includes(EMAIL_ALREADY_REGISTERED_ERROR))
+            if (isExistsEmail) {
+                form.setFields([{
+                    name: 'email',
+                    errors: [EmailIsAlreadyRegisteredMsg],
+                }])
+                throw error
+            } else if (isExpiredToken) {
+                // get new sudo token
+            } else {
+                errorHandler(error)
+            }
+        },
+    })
+
+    const formAction = useCallback(async (formValues) => {
+        const userPayload = {
+            name: formValues?.name?.trim() || '',
+        }
+        if (userPayload?.name && userPayload?.name !== user.name) {
+            const res = await updateUserMutation({
+                variables: {
+                    id: user.id,
+                    data: {
+                        dv: 1,
+                        sender: getClientSideSenderInfo(),
+                        ...userPayload,
+                    },
+                },
+            })
+
+            if (res.errors) {
+                throw new Error('cannot change user data')
+            }
+        }
+
+        if (isEmailEditable) {
+            const newEmail = normalizeEmail(formValues?.email) || null
+            const oldEmail = user?.email || null
+
+            if (newEmail !== oldEmail) {
+                const changeUserEmailWithSudoToken = async () => {
+                    const sudoToken = await getSudoTokenWithModal({
+                        phone: user.phone,
+                    })
+
+                    return await changeUserEmailMutation({
+                        variables: {
+                            data: {
+                                dv: 1,
+                                sender: getClientSideSenderInfo(),
+                                newEmail,
+                                token: sudoToken,
+                            },
+                        },
+                    })
+                }
+
+                const res = await changeUserEmailWithSudoToken()
+
+                if (res.errors || res?.data?.result?.status !== 'ok') {
+                    // @ts-ignore
+                    const isExpiredSudoToken = res?.errors?.graphQLErrors?.some((gqlError) => gqlError?.extensions?.type === 'TOKEN_NOT_FOUND')
+                    // @ts-ignore
+                    const isEmailChangingFailed = res?.errors?.graphQLErrors?.some((gqlError) => gqlError?.extensions?.type === 'OPERATION_FAILED')
+                    if (isExpiredSudoToken || isEmailChangingFailed) {
+                        clearSudoToken()
+                        const res2 = await changeUserEmailWithSudoToken()
+                        if (res2.errors) {
+                            throw new Error('cannot change email')
+                        }
+                    } else {
+                        throw new Error('cannot change email')
+                    }
+                }
+            }
+        }
+
+        await router.push('/user')
+    }, [changeUserEmailMutation, clearSudoToken, getSudoTokenWithModal, router, updateUserMutation, user?.email, user?.id, user?.name, user?.phone])
 
     const onCancel = useCallback(() => {
         router.push('/user')
@@ -85,108 +171,99 @@ export const UserProfileForm: React.FC = () => {
 
     const initialValues = useMemo(() => ({
         name: user?.name,
-        // TODO(DOMA-12201): Restore user email editing
-        // email: user?.email,
-        avatar: user?.avatar,
+        email: user?.email,
     }), [user])
-    const ErrorToFormFieldMsgMapping = {
-        [EMAIL_ALREADY_REGISTERED_ERROR]: {
-            name: 'email',
-            errors: [EmailIsAlreadyRegisteredMsg],
-        },
-    }
 
     return (
-        <FormWithAction
-            colon={false}
-            action={formAction}
-            initialValues={initialValues}
-            layout='horizontal'
-            ErrorToFormFieldMsgMapping={ErrorToFormFieldMsgMapping}
-            validateTrigger={['onBlur', 'onSubmit']}
-        >
-            {({ handleSave, isLoading, form }) => {
-                return (
-                    <>
-                        <Prompt
-                            title={PromptTitle}
-                            form={form}
-                            handleSave={handleSave}
-                        >
-                            <Typography.Paragraph>
-                                {PromptHelpMessage}
-                            </Typography.Paragraph>
-                        </Prompt>
-                        <Row gutter={[0, 40]}>
-                            <Col span={24}>
-                                <Typography.Title level={1}>
-                                    {ProfileUpdateTitle}
-                                </Typography.Title>
-                            </Col>
-                            <Col span={24}>
-                                <Form.Item
-                                    {...INPUT_LAYOUT_PROPS}
-                                    labelAlign='left'
-                                    name='name'
-                                    label={FullNameLabel}
-                                    rules={validations.name}
-                                >
-                                    <Input/>
-                                </Form.Item>
-                            </Col>
-                            {/* TODO(DOMA-12201): Restore user email editing */}
-                            {/*{*/}
-                            {/*    isEmailEditable && (*/}
-                            {/*        <Col span={24}>*/}
-                            {/*            <Form.Item*/}
-                            {/*                {...INPUT_LAYOUT_PROPS}*/}
-                            {/*                labelAlign='left'*/}
-                            {/*                name='email'*/}
-                            {/*                label={EmailLabel}*/}
-                            {/*                rules={validations.email}*/}
-                            {/*            >*/}
-                            {/*                <Input placeholder={ExampleEmailMessage}/>*/}
-                            {/*            </Form.Item>*/}
-                            {/*        </Col>*/}
-                            {/*    )*/}
-                            {/*}*/}
+        <>
+            <FormWithAction
+                formInstance={form}
+                colon={false}
+                action={formAction}
+                initialValues={initialValues}
+                layout='horizontal'
+                validateTrigger={['onBlur', 'onSubmit']}
+            >
+                {({ handleSave, isLoading, form }) => {
+                    return (
+                        <>
+                            <Prompt
+                                title={PromptTitle}
+                                form={form}
+                                handleSave={handleSave}
+                            >
+                                <Typography.Paragraph>
+                                    {PromptHelpMessage}
+                                </Typography.Paragraph>
+                            </Prompt>
+                            <Row gutter={[0, 40]}>
+                                <Col span={24}>
+                                    <Typography.Title level={1}>
+                                        {ProfileUpdateTitle}
+                                    </Typography.Title>
+                                </Col>
+                                <Col span={24}>
+                                    <Form.Item
+                                        {...INPUT_LAYOUT_PROPS}
+                                        labelAlign='left'
+                                        name='name'
+                                        label={FullNameLabel}
+                                        rules={validations.name}
+                                    >
+                                        <Input/>
+                                    </Form.Item>
+                                </Col>
+                                {
+                                    isEmailEditable && (
+                                        <Col span={24}>
+                                            <Form.Item
+                                                {...INPUT_LAYOUT_PROPS}
+                                                labelAlign='left'
+                                                name='email'
+                                                label={EmailLabel}
+                                                rules={validations.email}
+                                            >
+                                                <Input placeholder={ExampleEmailMessage}/>
+                                            </Form.Item>
+                                        </Col>
+                                    )
+                                }
+                                <Col span={24}>
+                                    <Form.Item {...INPUT_LAYOUT_PROPS} labelAlign='left' label={PasswordLabel}>
+                                        <Link href={RESET_PASSWORD_URL}>
+                                            <Typography.Link href={RESET_PASSWORD_URL}>
+                                                {ChangePasswordLabel}
+                                            </Typography.Link>
+                                        </Link>
+                                    </Form.Item>
+                                </Col>
 
-                            <Col span={24}>
-                                <Form.Item {...INPUT_LAYOUT_PROPS} labelAlign='left' label={PasswordLabel}>
-                                    <Link href={RESET_PASSWORD_URL}>
-                                        <Typography.Link href={RESET_PASSWORD_URL}>
-                                            {ChangePasswordLabel}
-                                        </Typography.Link>
-                                    </Link>
-                                </Form.Item>
-                            </Col>
-
-                            <Col span={24}>
-                                <ActionBar
-                                    actions={[
-                                        <Button
-                                            key='submit'
-                                            onClick={handleSave}
-                                            type='primary'
-                                            loading={isLoading}
-                                        >
-                                            {ApplyChangesMessage}
-                                        </Button>,
-                                        <Button
-                                            key='cancel'
-                                            type='secondary'
-                                            onClick={onCancel}
-                                        >
-                                            {CancelLabel}
-                                        </Button>,
-                                    ]}
-                                />
-                            </Col>
-                        </Row>
-                    </>
-                )
-            }}
-        </FormWithAction>
+                                <Col span={24}>
+                                    <ActionBar
+                                        actions={[
+                                            <Button
+                                                key='submit'
+                                                onClick={handleSave}
+                                                type='primary'
+                                                loading={isLoading}
+                                            >
+                                                {ApplyChangesMessage}
+                                            </Button>,
+                                            <Button
+                                                key='cancel'
+                                                type='secondary'
+                                                onClick={onCancel}
+                                            >
+                                                {CancelLabel}
+                                            </Button>,
+                                        ]}
+                                    />
+                                </Col>
+                            </Row>
+                        </>
+                    )
+                }}
+            </FormWithAction>
+        </>
     )
 }
-
