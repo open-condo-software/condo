5b60ba159f6dc49c042fcfde48ef6a8229fd1266
Matthew <50069872+SavelevMatthew@users.noreply.github.com>
2025-09-22 00:09:48 +0500

    feat(dev-portal-web): INFRA-1190 add environment info section to B2C app page (#6660)

---
+++ apps/dev-portal-web/domains/miniapp/components/B2CApp/edit/info/EnvironmentInfoSubsection.tsx
@@ -0,0 +1,91 @@
+import { Form } from 'antd'
+import getConfig from 'next/config'
+import React, { useCallback, useState, useEffect, useMemo } from 'react'
+import { useIntl } from 'react-intl'
+
+import { Select, Input } from '@open-condo/ui'
+import type { SelectProps } from '@open-condo/ui'
+
+import { CopyableInput } from '@/domains/common/components/CopyableInput'
+import {
+    DEV_ENVIRONMENT,
+    PROD_ENVIRONMENT,
+} from '@dev-portal-api/domains/miniapp/constants/publishing'
+
+import { AppEnvironment, useGetB2CAppQuery } from '@/gql'
+
+const { publicRuntimeConfig: { environmentsUris } } = getConfig()
+
+const DEFAULT_STAND = DEV_ENVIRONMENT
+
+export const EnvironmentInfoSubsection: React.FC<{ id: string }> = ({ id }) => {
+    const intl = useIntl()
+    const DevStandLabel = intl.formatMessage({ id: 'apps.environments.development.label' })
+    const ProdStandLabel = intl.formatMessage({ id: 'apps.environments.production.label' })
+    const SelectStandLabel = intl.formatMessage({ id: 'apps.b2c.sections.info.environmentInfo.form.items.stand.label' })
+    const AppIdLabel = intl.formatMessage({ id: 'apps.b2c.sections.info.environmentInfo.form.items.appId.label' })
+    const AppIdPlaceholder = intl.formatMessage({ id: 'apps.b2c.sections.info.environmentInfo.form.items.appId.placeholder' })
+    const EnvironmentUriLabel = intl.formatMessage({ id: 'apps.b2c.sections.info.environmentInfo.form.items.environmentUri.label' })
+
+    const [form] = Form.useForm()
+    const [environment, setEnvironment] = useState<AppEnvironment>(DEFAULT_STAND as AppEnvironment)
+
+    const handleEnvironmentChange = useCallback<Required<SelectProps>['onChange']>((value) => {
+        setEnvironment(value as AppEnvironment)
+    }, [])
+
+    const { data } = useGetB2CAppQuery({ variables: { id } })
+
+    const appId = useMemo(() => {
+        return data?.app?.[`${environment}ExportId`]
+    }, [data?.app, environment])
+
+    const environmentUri: string | undefined = useMemo(() => {
+        return environmentsUris[environment]
+    }, [environment])
+    
+    useEffect(() => {
+        form.setFieldValue('appId', appId)
+    }, [appId, form])
+
+    useEffect(() => {
+        form.setFieldValue('environmentUri', environmentUri)
+    }, [environmentUri, form])
+
+
+    return (
+        <Form
+            name='environment-app-info'
+            layout='vertical'
+            initialValues={{ environment: DEFAULT_STAND, appId }}
+            form={form}
+        >
+            <Form.Item name='environment' label={SelectStandLabel}>
+                <Select
+                    options={[
+                        { label: DevStandLabel, value: DEV_ENVIRONMENT, key: DEV_ENVIRONMENT },
+                        { label: ProdStandLabel, value: PROD_ENVIRONMENT, key: PROD_ENVIRONMENT },
+                    ]}
+                    onChange={handleEnvironmentChange}
+                />
+            </Form.Item>
+            {environmentUri && (
+                <Form.Item name='environmentUri' label={EnvironmentUriLabel}>
+                    <CopyableInput value={environmentUri}/>
+                </Form.Item>
+            )}
+            <Form.Item
+                name='appId'
+                label={AppIdLabel}
+                valuePropName='data-value'
+            >
+                {
+                    appId
+                        ? <CopyableInput value={appId}/>
+                        : <Input readOnly disabled placeholder={AppIdPlaceholder} value={AppIdPlaceholder}/>
+                }
+            </Form.Item>
+            {/*  TODO: Add information about current build / entrypoint here later  */}
+        </Form>
+    )
+}
--- apps/dev-portal-web/domains/miniapp/components/B2CApp/edit/info/InfoSection.tsx
+++ apps/dev-portal-web/domains/miniapp/components/B2CApp/edit/info/InfoSection.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useIntl } from 'react-intl'
 
 import { Section, SubSection } from '@/domains/miniapp/components/AppSettings'
+import { EnvironmentInfoSubsection } from '@/domains/miniapp/components/B2CApp/edit/info/EnvironmentInfoSubsection'
 
 import { CommonInfoSubsection } from './CommonInfoSubsection'
 import { IconsSubsection } from './IconsSubsection'
@@ -9,12 +10,16 @@ import { IconsSubsection } from './IconsSubsection'
 export const InfoSection: React.FC<{ id: string }> = ({ id }) => {
     const intl = useIntl()
     const CommonInfoSubtitle = intl.formatMessage({ id: 'apps.b2c.sections.info.commonInfo.subtitle' })
+    const EnvironmentInfoSubtitle = intl.formatMessage({ id: 'apps.b2c.sections.info.environmentInfo.subtitle' })
     const AppIconsSubtitle = intl.formatMessage({ id: 'apps.b2c.sections.info.icons.subtitle' })
 
     return (
         <Section>
             <SubSection title={CommonInfoSubtitle}>
                 <CommonInfoSubsection id={id}/>
+            </SubSection>
+            <SubSection title={EnvironmentInfoSubtitle}>
+                <EnvironmentInfoSubsection id={id}/>
             </SubSection>
             <SubSection title={AppIconsSubtitle}>
                 <IconsSubsection id={id}/>
