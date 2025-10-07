830bd5c76c9ac17c8e37e718ffb099d049dcca14
SavelevMatthew <savelevmatthew@gmail.com>
2025-10-07 13:59:17 +0500

    feat(miniapp-utils): INFRA-1242 add omit to collection utils

--- packages/miniapp-utils/src/helpers/collections.ts
+++ packages/miniapp-utils/src/helpers/collections.ts
@@ -8,3 +8,21 @@
 export function nonNull<TVal> (val: TVal): val is NonNullable<TVal> {
     return val !== null && val !== undefined
 }
+
+/**
+ * Removes keys from object
+ * @example
+ * const obj = { a: 1, b: 2, c: 3 }
+ * const newObj = omit(obj, ['a', 'b']) // { c: 3 }
+ * const newObj2 = omit(obj, 'c') // { a: 1, b: 2 }
+ */
+export function omit<TObj extends Record<string, unknown>, TKey extends keyof TObj> (obj: TObj, keys: TKey | Array<TKey>): Omit<TObj, TKey> {
+    const newObj: Omit<TObj, TKey> = { ...obj }
+    const keysToRemove = Array.isArray(keys) ? keys : [keys]
+    keysToRemove.forEach((key) => {
+        if (Reflect.has(newObj, key)) {
+            Reflect.deleteProperty(newObj, key)
+        }
+    })
+    return newObj
+}
