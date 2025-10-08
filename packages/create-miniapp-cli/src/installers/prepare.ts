e555935bd656e82c204b98f8641bb983ddb19b0e
abshnko <2009add@gmail.com>
2025-10-08 17:11:59 +0300

    feat(packages): fixed index.js for server template and corrected prepare

--- packages/create-miniapp-cli/src/installers/prepare.ts
+++ packages/create-miniapp-cli/src/installers/prepare.ts
@@ -1,3 +1,6 @@
+import path from 'path'
+import { fileURLToPath } from 'url'
+
 import { type PackageManager } from '@cli/utils/getUserPkgManager.js'
 import { logger } from '@cli/utils/logger'
 import { execa } from 'execa'
@@ -18,6 +21,9 @@ interface ScriptConfig {
     ignoreFailure?: boolean
 }
 
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const CONDO_ROOT_PATH = path.resolve(__dirname, '../../../')
 
 const runScript = async ({
     command,
@@ -35,6 +41,7 @@ const runScript = async ({
     ignoreFailure?: boolean
 }) => {
     try {
+        logger.info(`Running ${command} ${args} ...`)
         await execa(command, args, { cwd, stdio: 'inherit' })
         logger.success(successMessage)
         return true
@@ -55,13 +62,23 @@ const handleFailureWithMigrator = async (
 
 export const prepareApp = async ({ pkgManager, appName, projectDir }: PrepareAppProps) => {
     const SCRIPTS = {
+        BUILD_PACKAGES: {
+            name: 'build-packages',
+            command: pkgManager,
+            args: ['build:packages'],
+            successMessage: `${pkgManager} build:packages script finished successfully!`,
+            errorMessage: `${pkgManager} build:packages script failed:`,
+            ignoreFailure: true,
+            cwd: CONDO_ROOT_PATH,
+        },
         GLOBAL_PREPARE: {
             name: 'global-prepare',
             command: 'node',
-            args: ['../../bin/prepare.js', '--https', '-f', appName],
+            args: ['./bin/prepare.js', '--https', '-f', appName],
             successMessage: 'Successfully finished global prepare.js script!',
             errorMessage: 'Global prepare.js script failed, but it is okay on the first run:',
             ignoreFailure: true,
+            cwd: CONDO_ROOT_PATH,
         },
         BUILD_DEPS: {
             name: 'build-deps',
@@ -116,13 +133,12 @@ export const prepareApp = async ({ pkgManager, appName, projectDir }: PrepareApp
     } satisfies Record<string, ScriptConfig>
 
     const scriptOrder = [
-        SCRIPTS.GLOBAL_PREPARE,
-        SCRIPTS.BUILD_DEPS,
+        SCRIPTS.BUILD_PACKAGES,
+        SCRIPTS.GLOBAL_PREPARE, // base env vars
         SCRIPTS.MAKE_MIGRATIONS,
-        SCRIPTS.LOCAL_PREPARE,
+        SCRIPTS.GLOBAL_PREPARE, // migrate and run local prepare
+        SCRIPTS.BUILD_DEPS,
         SCRIPTS.MAKE_TYPES,
-        SCRIPTS.LOCAL_PREPARE,
-        SCRIPTS.BUILD,
     ]
 
     const retryableScripts = [
