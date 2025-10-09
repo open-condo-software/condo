3cf7cf252732388786d9f0fb413b87ef210b085d
Matthew <50069872+SavelevMatthew@users.noreply.github.com>
2025-10-09 18:55:03 +0500

    feat(condo): INFRA-1247 automatically redirect OIDC requests from resident-app (#6757)
    * feat(condo): automatically redirect OIDC requests from resident-app
    
    * feat(resident-app): INFRA-1247 add bin/prepare
    
    * chore: INFRA-1247 sync modules to main

--- apps/condo/domains/user/oidc/OIDCMiddleware.js
+++ apps/condo/domains/user/oidc/OIDCMiddleware.js
@@ -3,10 +3,25 @@ const express = require('express')
 const conf = require('@open-condo/config')
 const { getLogger } = require('@open-condo/keystone/logging')
 
+const { RESIDENT } = require('@condo/domains/user/constants/common')
+
 const createConfiguration = require('./configuration')
 const { OIDCBearerTokenKeystonePatch } = require('./OIDCBearerTokenKeystonePatch')
-
 const logger = getLogger()
+
+const RESIDENT_APP_DOMAIN = conf['RESIDENT_APP_DOMAIN']
+const RESIDENT_APP_PROXY_IPS = JSON.parse(conf['RESIDENT_APP_PROXY_IPS'] || '[]')
+const RESIDENT_APP_PROXY_HEADERS = JSON.parse(conf['RESIDENT_APP_PROXY_HEADERS'] || '{"via": "Next"}')
+
+function _isResidentAppProxy (req) {
+    for (const [headerName, headerValue] of Object.entries(RESIDENT_APP_PROXY_HEADERS)) {
+        if (req.headers[headerName] !== headerValue) {
+            return false
+        }
+    }
+
+    return RESIDENT_APP_PROXY_IPS.includes(req?.originalIp)
+}
 
 class OIDCMiddleware {
     prepareMiddleware ({ keystone }) {
@@ -148,7 +163,56 @@ class OIDCMiddleware {
                 })
             }
         })
-        app.use('/oidc', provider.callback())
+        app.use('/oidc', (req, res, next) => {
+            // NOTE: we need to redirect OIDC request from resident app to itself. Whole pipeline looks like this:
+            // 1. Web-miniapp inside resident app calls postMessage to start auth process by URL
+            // 2. Parent app (resident-app) stores auth cookie and does fetch request to miniapp.example.com/api/oidc/auth
+            // 3. Miniapp knows only about condo, so it redirects to condo.example.com/oidc/auth where auth cookie is not stored
+            // So our goal is to catch this situation, performing redirect to resident-app, so auth cookie is handled correctly
+
+            const url = req.url
+            // First of all, we need to catch only auth route (initial request)
+            if (!url || !url.startsWith('/auth')) return next()
+
+            // When request goes "resident-app" -> "miniapp" -> "condo/oidc/auth", cross-site redirect occurs
+            // In such cases user-agents must set origin to "null" to hide redirecting endpoint, but expose referer
+            // as initial page (in our case its "resident-app"
+            const refererHeader = req.headers['referer']
+            const originHeader = req.headers['origin']
+
+            if (!refererHeader || originHeader !== 'null') {
+                return next()
+            }
+
+            let refererOrigin = ''
+            try {
+                refererOrigin = new URL(refererHeader).origin
+            } catch {
+                // eslint hack for no empty block :)
+            }
+
+            if (!RESIDENT_APP_DOMAIN || refererOrigin !== RESIDENT_APP_DOMAIN) {
+                return next()
+            }
+
+            // If request is already redirected to resident-app and now proxied, we should not redirect again (infinity redirect loop prevention)
+            // When resident-app proxying the request, it sets "via" header
+            // We can detect it by IP and "via" header.
+            // (ip only is not enough for local development, where everything is localhost (::1))
+            if (_isResidentAppProxy(req)) {
+                return next()
+            }
+
+            // At this point we have request from web resident-app, so we should redirect request to it, where correct auth cookie is stored
+            const userType = req?.user?.type
+            if (userType !== RESIDENT) {
+                // /api/auth/oidc/auth?client_id=...
+                return res.redirect(`${RESIDENT_APP_DOMAIN}/api/auth${req.originalUrl}`)
+            }
+
+            return next()
+        }, provider.callback())
+        
         return app
     }
 }
