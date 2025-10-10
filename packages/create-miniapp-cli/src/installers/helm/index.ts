8c87d0483f59bd782979f4f6b863b9eeca75b459
abshnko <2009add@gmail.com>
2025-10-10 10:50:03 +0300

    feat(packages): added updating values

--- packages/create-miniapp-cli/src/installers/helm/index.ts
+++ packages/create-miniapp-cli/src/installers/helm/index.ts
@@ -3,31 +3,41 @@ import { updateSecretValues } from '@cli/installers/helm/secretValues'
 import { updateServicesUrls } from '@cli/installers/helm/servicesUrls'
 import { writeHelmTemplates } from '@cli/installers/helm/templates'
 import { getNextPrefix } from '@cli/installers/helm/utils'
+import { MaxOldSpace, ResourceSettings, updateValues } from '@cli/installers/helm/values'
 import { logger } from '@cli/utils/logger'
 
 interface SetupHelmProps {
     appName: string
-    wantReview: boolean
+    hasReview: boolean
+    appResources: ResourceSettings
+    maxOldSpace: MaxOldSpace
+    workerResources?: ResourceSettings
+    hasWorker: boolean
 }
 
-export async function setupHelm ({ appName, wantReview }: SetupHelmProps) {
+export async function setupHelm ({ appName, hasReview, appResources, workerResources, hasWorker, maxOldSpace }: SetupHelmProps) {
     const nextPrefix = await getNextPrefix()
     logger.info(`Next prefix block: ${nextPrefix}`)
 
-    const created = await writeHelmTemplates(appName, nextPrefix, wantReview)
+    const created = await writeHelmTemplates(appName, nextPrefix, hasReview)
     const servicesFile = await updateServicesUrls(appName)
-    
+
     created.push(servicesFile)
 
-    if (wantReview) {
+    if (hasReview) {
         const reviewFile = await updateReviewSecrets(appName)
         if (reviewFile) created.push(reviewFile)
     }
 
-    const secretValuesFile = await updateSecretValues(appName, wantReview)
+    const secretValuesFile = await updateSecretValues(appName, hasReview)
     if (secretValuesFile) {
         created.push(secretValuesFile)
     }
+
+    // const valuesFile = await updateValues({ appName, appResources, workerResources, hasReview, hasWorker, maxOldSpace })
+    // if (valuesFile) {
+    //     created.push(valuesFile)
+    // }
 
     logger.success('Helm setup completed! Created/modified files:\n' + created.join('\n'))
 }
