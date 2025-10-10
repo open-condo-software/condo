3ab992c7779b555c5dc8db8a0489e056f43b249e
abshnko <2009add@gmail.com>
2025-10-10 10:50:03 +0300

    feat(packages): added updating values

--- packages/create-miniapp-cli/src/consts.ts
+++ packages/create-miniapp-cli/src/consts.ts
@@ -16,6 +16,25 @@ export const APP_TYPES = {
 export type AppType = keyof typeof APP_TYPES
 export type AppTypeValue = (typeof APP_TYPES)[keyof typeof APP_TYPES]
 
+export const DEFAULT_APP_RESOURCES = {
+    cpu: {
+        default: '250m',
+        development: '250m',
+        production :'500m',
+    },
+    memory: {
+        default: '512Mi',
+        development: '1024Mi',
+        production: '1024Mi',
+    }, 
+}
+
+export const DEFAULT_MAX_OLD_SPACE = {
+    default: 256,
+    development: 512,
+    production: 768,
+}
+
 export const TITLE_TEXT = `
  ▗▄▄▖▗▄▄▖ ▗▄▄▄▖ ▗▄▖▗▄▄▄▖▗▄▄▄▖     ▗▄▄▖ ▗▄▖ ▗▖  ▗▖▗▄▄▄   ▗▄▖     ▗▖  ▗▖▗▄▄▄▖▗▖  ▗▖▗▄▄▄▖ ▗▄▖ ▗▄▄▖ ▗▄▄▖ 
 ▐▌   ▐▌ ▐▌▐▌   ▐▌ ▐▌ █  ▐▌       ▐▌   ▐▌ ▐▌▐▛▚▖▐▌▐▌  █ ▐▌ ▐▌    ▐▛▚▞▜▌  █  ▐▛▚▖▐▌  █  ▐▌ ▐▌▐▌ ▐▌▐▌ ▐▌
