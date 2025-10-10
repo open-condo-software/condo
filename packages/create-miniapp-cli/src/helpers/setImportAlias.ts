a1e4dcb3c545ca81a685118b827ddc74ff6c9dde
abshnko <2009add@gmail.com>
2025-10-10 10:50:03 +0300

    feat(packages): added updating values

--- packages/create-miniapp-cli/src/helpers/setImportAlias.ts
+++ packages/create-miniapp-cli/src/helpers/setImportAlias.ts
@@ -1,7 +1,7 @@
 import fs from 'fs'
 import path from 'path'
 
-function replaceTextInFiles (
+export function replaceTextInFiles (
     directoryPath: string,
     search: string,
     replacement: string,
