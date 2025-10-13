b1ab8e4d1ededfc3f5ea65e8b54f7cafccfcfb59
AleX83Xpert <AleX83Xpert@mail.ru>
2025-10-13 15:14:13 +0500

    feat(address-service): use FIAS ID for address key generation in FIAS-compatible providers

--- apps/address-service/domains/common/utils/addressKeyUtils.js
+++ apps/address-service/domains/common/utils/addressKeyUtils.js
@@ -1,15 +1,33 @@
 const get = require('lodash/get')
 
+const { FIAS_PROVIDERS } = require('../constants/providers')
+
 const JOINER = '~'
 const SPACE_REPLACER = '_'
 const SPECIAL_SYMBOLS_TO_REMOVE_REGEX = /[!@#$%^&*)(+=.,_:;"'`[\]{}№|<>~]/g
 
 /**
- * @param {NormalizedBuilding} normalizedBuilding
+ * @param {import('@address-service/domains/common/utils/services/index.js').NormalizedBuilding} normalizedBuilding
  * @returns {string}
  */
 function generateAddressKey (normalizedBuilding) {
     const data = normalizedBuilding.data
+    const providerName = normalizedBuilding.provider?.name
+
+    // FIAS providers' result always contain unique identifier of the building (FIAS ID)
+    // So, it's useful to use such id as unique key for the address.
+    // Also, such id gives an ability to get the same address from different providers.
+    if (FIAS_PROVIDERS.includes(providerName)) {
+        const key = generateAddressKeyFromFiasId(normalizedBuilding)
+
+        if (key) {
+            return key
+        }
+    }
+
+    // The next logic is used as fallback.
+    // It's not the best way, but it's the only way to get unique key for the address.
+
     /**
      * @type {string[]}
      */
@@ -47,4 +65,18 @@ function generateAddressKey (normalizedBuilding) {
         .toLowerCase()
 }
 
-module.exports = { generateAddressKey, JOINER, SPACE_REPLACER }
+/**
+ * @param {NormalizedBuilding} normalizedBuilding
+ * @returns {string | null}
+ */
+function generateAddressKeyFromFiasId (normalizedBuilding) {
+    const houseFiasId = normalizedBuilding.data.house_fias_id
+
+    if (!houseFiasId) {
+        return null
+    }
+
+    return `fias:${houseFiasId}`
+}
+
+module.exports = { generateAddressKey, generateAddressKeyFromFiasId, JOINER, SPACE_REPLACER }
