3a0fb6a7feba6cfdd78eb3c56f5bb682f307a1fa
Matthew <50069872+SavelevMatthew@users.noreply.github.com>
2025-11-24 13:48:34 +0500

    feat(condo): INFRA-1342 trigger applications update on OIDC config change (#6948)

--- apps/condo/domains/miniapp/tasks/index.js
+++ apps/condo/domains/miniapp/tasks/index.js
@@ -1,7 +1,9 @@
 const { deleteB2BAppRoles } = require('./deleteB2BAppRoles')
 const { updateB2BAppRolesPermissions } = require('./updateB2BAppRolesPermissions')
+const { updateRelatedMiniappsFromOIDCClient } = require('./updateRelatedMiniappsFromOIDCClient')
 
 module.exports = {
     deleteB2BAppRoles,
     updateB2BAppRolesPermissions,
+    updateRelatedMiniappsFromOIDCClient,
 }
