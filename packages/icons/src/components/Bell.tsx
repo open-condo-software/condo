d59942a7e00f208803556dfcc1afb70f085e18c2
Vladimir Fadeev <88741707+vovaaxeapolla@users.noreply.github.com>
2025-12-15 15:14:10 +0500

    fix(resident-app): DOMA-12732, DOMA-12636 review fixes (#7035)
    * fix(resident-app): DOMA-12732, DOMA-12636 review fixes
    
    * fix(resident-app): DOMA-12732, DOMA-12636 review fixes

--- packages/icons/src/components/Bell.tsx
+++ packages/icons/src/components/Bell.tsx
@@ -17,7 +17,7 @@ export const Bell: React.FC<IconProps> = ({ svgProps: props, ...restProps }) => 
                 >
                     <path
                         d='M6 8a6 6 0 1 1 12 0c0 7 3 9 3 9H3s3-2 3-9ZM10.3 21a1.94 1.94 0 0 0 3.4 0'
-                        stroke='#000'
+                        stroke='currentColor'
                         strokeWidth={2}
                         strokeLinecap='round'
                         strokeLinejoin='round'
