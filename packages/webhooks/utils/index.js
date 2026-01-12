ed43a1c8993efa3e67869fdb4b5da8263aa3c137
AleX83Xpert <AleX83Xpert@mail.ru>
2026-01-12 15:23:16 +0500

    refactor(webhooks): DOMA-11848 remove generateSignature, calculateNextRetryAt, and trySendWebhookPayload from utils/index.js exports to reduce public API surface

--- packages/webhooks/utils/index.js
+++ packages/webhooks/utils/index.js
@@ -1,13 +1,5 @@
 const { queueWebhookPayload } = require('./queueWebhookPayload')
-const {
-    generateSignature,
-    calculateNextRetryAt,
-    trySendWebhookPayload,
-} = require('./webhookPayload')
 
 module.exports = {
-    generateSignature,
-    calculateNextRetryAt,
-    trySendWebhookPayload,
     queueWebhookPayload,
 }
