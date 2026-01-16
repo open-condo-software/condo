b4d8363d02611a91071479bed7ff22cd9fdc22e4
Daria Abushenko <2009add@gmail.com>
2026-01-16 12:20:30 +0300

    refactor(condo): DOMA-12509 remove jspdf dependency (#6840) (#7106)
    * refactor(condo): DOMA-12509 remove jspdf dependency (#6840)
    
    * refactor(condo): update createWrappedPdf to pdfmake
    
    * refactor(condo): temp use pdfmake
    
    * refactor(condo): DOMA-12509 update pdf util
    
    * refactor(condo): DOMA-12509 remove redundant code
    
    * refactor(condo): DOMA-12509 remove old pages
    
    * refactor(condo): DOMA-12509 remove jspdf dependency
    
    * Revert "refactor(condo): DOMA-12509 remove redundant code"
    
    This reverts commit 72ac894ee4a6b26e762ab16509ff5fc94e481733.
    
    * refactor(condo): DOMA-12509 added redirect and pixel/pdf dimensions
    
    * Revert "Revert "refactor(condo): DOMA-12509 remove redundant code""
    
    This reverts commit 246a0231b8093678f71e96b9ea9279a3061af462.
    
    * refactor(condo): DOMA-12509 update redirect
    
    * fix(eps): DOMA-12509 fit long order numbers in check on mobile devices
    
    * chore(eps): DOMA-12509 updated submodule
    
    ---------
    
    Co-authored-by: Pavel <52532264+nomerdvadcatpyat@users.noreply.github.com>

--- apps/condo/domains/common/utils/pdf.ts
+++ apps/condo/domains/common/utils/pdf.ts
@@ -1,79 +1,54 @@
 import html2canvas from 'html2canvas'
-import Jspdf from 'jspdf'
-const PDF_FORMAT_SETTINGS = {
-    // pdfWidth - width of final image (in mm)
-    // pdfHeight - height of final image (in mm)
-    // elementOffset - offset of image (in mm)
-    // firstLineOffset - margin right for first line (in css pixels)
-    // lineSpace - margin right for rest of lines (in css pixels)
-    'a4': { pdfWidth: 210, pdfHeight: 297, elementOffset: 10, firstLineOffset: 23, lineSpace: 80 },
-    'a5': { pdfWidth: 148, pdfHeight: 210, elementOffset: 10, firstLineOffset: 23, lineSpace: 80 },
-}
-
-function getPdfHeightFromElement (element: HTMLElement, expectedWidth: number) {
-    const { clientWidth, clientHeight } = element
-    const originalRatio = clientHeight / clientWidth
-    return expectedWidth * originalRatio
-}
+import pdfMake from 'pdfmake/build/pdfmake'
 
 interface ICreatePdfOptions {
     element: HTMLElement
     fileName: string
-    format: string
 }
 
-interface ICreatePdfWithPageBreaks {
-    (options: Omit<ICreatePdfOptions, 'format'>): Promise<void | Jspdf>
+interface ICreatePdf {
+    (options: ICreatePdfOptions): Promise<void>
 }
 
-export const createPdfWithPageBreaks: ICreatePdfWithPageBreaks = (options) => {
+
+export const createWrappedPdf: ICreatePdf = async (options) => {
+    if (typeof window === 'undefined') {
+        console.warn('createWrappedPdf can only be called on client side')
+        return
+    }
+
     const { element, fileName } = options
-    const { elementOffset } = PDF_FORMAT_SETTINGS.a4
+    
+    try {
+        const pixelWidth = element.clientWidth
+        const pixelHeight = element.clientHeight
+        const pdfWidth = pixelWidth * 0.75
+        const pdfHeight = pixelHeight * 0.75
 
-    return html2canvas(element).then(canvas => {
-        const imgData = canvas.toDataURL('image/jpeg', 1.0)
-
-        // Default page size is a4
-        const pdf = new Jspdf('landscape')
-        const pdfWidth = pdf.internal.pageSize.getWidth()
-        const pdfHeight = pdf.internal.pageSize.getHeight()
-        const imgHeight = getPdfHeightFromElement(element, pdfWidth)
-
-        let heightLeft = imgHeight + elementOffset * 2
-        let position = 0
-        let page = 1
-        pdf.addImage(imgData, 'JPG', elementOffset, elementOffset + position, pdfWidth, imgHeight)
-        heightLeft -= pdfHeight
-
-        while (heightLeft >= 0) {
-            position = heightLeft - imgHeight + elementOffset * page
-            pdf.addPage([pdfWidth, pdfHeight], 'landscape')
-            pdf.addImage(imgData, 'JPG', elementOffset, position, pdfWidth, imgHeight)
-            heightLeft -= (pdfHeight - elementOffset * page)
-            page++
+        const canvas = await html2canvas(element, {
+            windowWidth: pixelWidth,
+            windowHeight: pixelHeight,
+        })
+        const imageData = canvas.toDataURL('image/png')
+        
+        
+        const docDefinition = {
+            pageSize: {
+                width: pdfWidth,
+                height: pdfHeight,
+            },
+            pageMargins: [0, 0, 0, 0],
+            content: [
+                {
+                    image: imageData,
+                    width: pdfWidth,
+                    height: pdfHeight,
+                },
+            ],
         }
-
-        return pdf.save(fileName, { returnPromise: true })
-    })
-}
-
-export const createWrappedPdf: ICreatePdfWithPageBreaks = (options) => {
-    const { element, fileName } = options
-    const pdfWidth = element.clientWidth
-    const pdfHeight = element.clientHeight
-    return html2canvas(element, {
-        windowWidth: pdfWidth,
-        windowHeight: pdfHeight,
-    }).then(canvas => {
-        const doc = new Jspdf('p', 'px', [pdfWidth, pdfHeight])
-        const imageOptions = {
-            imageData: canvas,
-            x: 0,
-            y: 0,
-            width: pdfWidth,
-            height: pdfHeight,
-        }
-        doc.addImage(imageOptions)
-        return doc.save(fileName, { returnPromise: true })
-    })
+        const pdfDocGenerator = pdfMake.createPdf(docDefinition)
+        pdfDocGenerator.download(fileName)
+    } catch (error) {
+        console.error('Error generating PDF:', error)
+    }
 }
