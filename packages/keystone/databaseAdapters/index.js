3890fcd775cb98718383c4e05d4f2d0f2b4a4a14
AleX83Xpert <AleX83Xpert@mail.ru>
2026-01-21 12:57:31 +0500

    fix(keystone): override KnexAdapter _connect method to fix error handling and resource cleanup
    - moved KnexAdapter from @open-keystone/adapter-knex to local implementation
    - replaced catch with try-catch block for better error handling
    - added knex.destroy() call on connection failure to prevent resource leaks
    - simplified error propagation by directly throwing the caught error

--- packages/keystone/databaseAdapters/index.js
+++ packages/keystone/databaseAdapters/index.js
@@ -1,7 +1,9 @@
 const { BalancingReplicaKnexAdapter } = require('./BalancingReplicaKnexAdapter')
 const { FakeDatabaseAdapter } = require('./FakeDatabaseAdapter')
+const { KnexAdapter } = require('./KnexAdapter')
 
 module.exports = {
     FakeDatabaseAdapter,
     BalancingReplicaKnexAdapter,
+    KnexAdapter,
 }
