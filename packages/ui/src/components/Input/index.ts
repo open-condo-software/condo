982c9d95ed771cdfec17e12606be70dae7cc021e
Viktor Tolmachev <tolmach0221@yandex.ru>
2026-02-12 16:43:28 +0500

    feat(ui): DOMA-12914 consolidate RichTextArea and MarkdownEditor components
    - Removed MarkdownEditor and RichTextAreaV2 components, consolidating their functionality into a single RichTextArea component.
    - Updated exports in index.ts and Input component to reflect the changes.
    - Deleted associated styles and storybook entries for the removed components.
    - Enhanced RichTextArea with improved styling and toolbar options.

--- packages/ui/src/components/Input/index.ts
+++ packages/ui/src/components/Input/index.ts
@@ -1,32 +1,27 @@
 import { Input as InputComponent } from './input'
 import { Password } from './password'
 import { Phone } from './phone'
+import { RichTextArea } from './richTextAria'
 import { TextArea } from './textArea'
 import './style.less'
-import './markdownEditor.less'
-import './richTextArea.less'
 
 export type { BaseInputProps, InputProps } from './input'
-export type { MarkdownEditorProps, ToolbarLabels } from './markdownEditor'
 export type { PasswordInputProps } from './password'
-export type { RichTextAreaProps, RichTextAreaToolbarLabels } from './reachTextAria'
-export type { RichTextAreaV2Props, RichTextAreaLinkModalLabels } from './richTextAria'
+export type { RichTextAreaProps, RichTextAreaToolbarLabels, RichTextAreaLinkModalLabels } from './richTextAria'
 export type { PhoneInputProps } from './phone'
-
-export { MarkdownEditor, replaceHeaders } from './markdownEditor'
-export { RichTextArea } from './reachTextAria'
-export { RichTextAreaV2 } from './richTextAria'
 
 export type InputType = typeof InputComponent & {
     Password: typeof Password
     Phone: typeof Phone
     TextArea: typeof TextArea
+    RichTextArea: typeof RichTextArea
 }
 
 const Input = InputComponent as InputType
 Input.Password = Password
 Input.Phone = Phone
 Input.TextArea = TextArea
+Input.RichTextArea = RichTextArea
 
 export {
     Input,
