9dac30c0325f25e35aecf070dec0ca269d9589f1
AleX83Xpert <AleX83Xpert@mail.ru>
2026-02-13 11:37:56 +0500

    docs(address-service): DOMA-12746 update migration guide and remove deprecated addressKeyUtils
    Update MIGRATION-heuristics.md to clarify merge-duplicate-addresses script runs as remote GraphQL client with setup instructions. Remove deprecated generateAddressKey() and generateAddressKeyFromFiasId() functions from addressKeyUtils.js along with associated tests and FIAS_PROVIDERS constant, as address key generation has been moved to provider classes. Update ActualizeAddressesService tests to call provider

--- apps/address-service/domains/common/constants/providers.js
+++ apps/address-service/domains/common/constants/providers.js
@@ -5,12 +5,6 @@ const PULLENTI_PROVIDER = 'pullenti'
 
 const INJECTIONS_PROVIDER = 'injections'
 
-/**
- * Providers that support FIAS (Federal Information Address System) IDs
- * These providers return house_fias_id in their response data (normalized data)
- */
-const FIAS_PROVIDERS = [DADATA_PROVIDER, PULLENTI_PROVIDER]
-
 const PROVIDERS = [DADATA_PROVIDER, GOOGLE_PROVIDER, YANDEX_PROVIDER, PULLENTI_PROVIDER, INJECTIONS_PROVIDER]
 
 module.exports = {
@@ -21,6 +15,5 @@ module.exports = {
 
     INJECTIONS_PROVIDER,
 
-    FIAS_PROVIDERS,
     PROVIDERS,
 }
