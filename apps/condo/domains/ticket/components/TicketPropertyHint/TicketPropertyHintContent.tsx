ac307b9a630de36324a154ebcb14ffbfd54ea7b7
Viktor Tolmachev <119614372+tolmachev21@users.noreply.github.com>
2026-03-05 21:36:18 +0500

    hotfix(condo): DOMA-13023 reduce space in hint (#7316)
    * hotfix(condo): DOMA-12023 reduce space in hint
    
    * hotfix(condo): DOMA-13023 update subproject commit and adjust CSS styles for ticket property hints
    
    (cherry picked from commit d9318d90524ae52e54da9b2f7e8d29c894daafef)

--- apps/condo/domains/ticket/components/TicketPropertyHint/TicketPropertyHintContent.tsx
+++ apps/condo/domains/ticket/components/TicketPropertyHint/TicketPropertyHintContent.tsx
@@ -1,6 +1,6 @@
 import { Col, Row, RowProps } from 'antd'
 import classNames from 'classnames'
-import React, { CSSProperties } from 'react'
+import React from 'react'
 
 import { useIntl } from '@open-condo/next/intl'
 import { Markdown, Typography } from '@open-condo/ui'
@@ -9,7 +9,6 @@ import styles from './TicketPropertyHintContent.module.css'
 
 type TicketPropertyHintContentProps = {
     content: string
-    style?: CSSProperties
     className?: string
     linkToHint?: string
 }
@@ -30,27 +29,24 @@ export const TicketPropertyHintContent = (props: TicketPropertyHintContentProps)
     }, [ref])
 
     return (
-        <>
-            <Row gutter={CONTENT_GUTTER}>
-                <Col span={24}>
-                    <div
-                        ref={ref}
-                        className={classNames(styles.contentWrapper, props.className)}
-                        style={props.style}
-                    >
-                        <Markdown type='inline'>{props.content}</Markdown>
-                    </div>
-                </Col>
-                {
-                    isContentOverflow && props.linkToHint && (
-                        <Col span={24}>
-                            <Typography.Link href={props.linkToHint} size='large' target='_blank' rel='noreferrer'>
-                                {ExtraTitleMessage}
-                            </Typography.Link>
-                        </Col>
-                    )
-                }
-            </Row>
-        </>
+        <Row gutter={CONTENT_GUTTER}>
+            <Col span={24}>
+                <div
+                    ref={ref}
+                    className={classNames(styles.contentWrapper, props.className)}
+                >
+                    <Markdown type='inline'>{props.content}</Markdown>
+                </div>
+            </Col>
+            {
+                isContentOverflow && props.linkToHint && (
+                    <Col span={24}>
+                        <Typography.Link href={props.linkToHint} size='large' target='_blank' rel='noreferrer'>
+                            {ExtraTitleMessage}
+                        </Typography.Link>
+                    </Col>
+                )
+            }
+        </Row>
     )
 }
