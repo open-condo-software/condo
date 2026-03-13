af23193def66916a4df6c0bf4aa9993df604485f
Mikhail Gorbunov <toplenboren@gmail.com>
2026-03-13 17:03:32 +0500

    fix(condo): DOMA-13057 fixes on review

--- apps/condo/domains/common/utils/stripMarkdown.ts
+++ apps/condo/domains/common/utils/stripMarkdown.ts
@@ -4,8 +4,8 @@ export const stripMarkdown = (input?: string | null): string => {
     }
 
     return input
-        .replaceAll(/!\[[^\]]*?\]\([^)]*?\)/g, '')
-        .replaceAll(/\[([^\]]+?)\]\(([^)]+?)\)/g, '$1')
+        .replaceAll(/!\[[^\]]{0,1000}\]\([^)]{0,1000}\)/g, '')
+        .replaceAll(/\[([^\]]{1,1000})\]\(([^)]{1,1000})\)/g, '$1')
         .replaceAll(/\*\*|__|[_*`~]/g, '')
         .replaceAll(/^\s{0,3}>\s?/gm, '')
         .replaceAll(/^#{1,6}\s*/gm, '')
