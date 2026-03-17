b8032da18a902e670f4908bff365295fe2eb4a58
Viktor Tolmachev <tolmach0221@yandex.ru>
2026-03-17 13:23:17 +0500

    fix(condo): DOMA-13071 update cache in useOrganizationInvites

--- apps/condo/domains/organization/hooks/useOrganizationInvites.tsx
+++ apps/condo/domains/organization/hooks/useOrganizationInvites.tsx
@@ -39,6 +39,7 @@ export const useOrganizationInvites = (organizationTypes: Array<OrganizationType
             userId,
             organizationType: organizationTypes,
         },
+        fetchPolicy: 'network-only',
         skip: authLoading || !isAuthenticated || !organizationTypes || organizationTypes.length < 1 || !persistor,
     })
     const userInvites = useMemo(() => userInvitationsData?.invitations?.filter(Boolean) || [], [userInvitationsData?.invitations])
