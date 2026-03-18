2c1ab029b7890881f6c80f734acfc96c73ddb0fe
vovaaxeapolla <vovaaxeapolla@gmail.com>
2026-03-18 12:15:40 +0500

    feat(condo): DOMA-13059 register external payments service

---
+++ apps/condo/domains/acquiring/constants/registerExternalPayments.js
@@ -0,0 +1,68 @@
+const { GQLErrorCode: { BAD_USER_INPUT } } = require('@open-condo/keystone/errors')
+
+const {
+    DV_VERSION_MISMATCH,
+    WRONG_FORMAT,
+    NOT_FOUND,
+} = require('@condo/domains/common/constants/errors')
+
+const PAYMENTS_LIMIT = 100
+
+const ERRORS = {
+    DV_VERSION_MISMATCH: {
+        mutation: 'registerExternalPayments',
+        variable: ['data', 'dv'],
+        code: BAD_USER_INPUT,
+        type: DV_VERSION_MISMATCH,
+        message: 'Wrong value for data version number',
+    },
+    WRONG_SENDER_FORMAT: {
+        mutation: 'registerExternalPayments',
+        variable: ['data', 'sender'],
+        code: BAD_USER_INPUT,
+        type: WRONG_FORMAT,
+        message: 'Invalid format of "sender" field value. {details}',
+        correctExample: '{ "dv": 1, "fingerprint": "uniq-device-or-container-id" }',
+        messageInterpolation: { details: 'Please, check the example for details' },
+    },
+    PAYMENTS_LIMIT_EXCEEDED: {
+        mutation: 'registerExternalPayments',
+        variable: ['data', 'payments'],
+        code: BAD_USER_INPUT,
+        type: WRONG_FORMAT,
+        message: `Too many payments in one request. The maximum allowed is ${PAYMENTS_LIMIT}`,
+    },
+    ACQUIRING_CONTEXT_NOT_FOUND: {
+        mutation: 'registerExternalPayments',
+        variable: ['data', 'acquiringIntegrationContext'],
+        code: BAD_USER_INPUT,
+        type: NOT_FOUND,
+        message: 'AcquiringIntegrationContext not found or has been deleted',
+    },
+    ACQUIRING_INTEGRATION_NOT_FOUND: {
+        mutation: 'registerExternalPayments',
+        variable: ['data', 'acquiringIntegrationContext'],
+        code: BAD_USER_INPUT,
+        type: NOT_FOUND,
+        message: 'AcquiringIntegration not found or has been deleted',
+    },
+    DUPLICATED_PAYMENTS: {
+        mutation: 'registerExternalPayments',
+        variable: ['data', 'payments'],
+        code: BAD_USER_INPUT,
+        type: 'DUPLICATED_VALUES',
+        message: 'Some payments have transactionIds that already exist in the system: {ids}',
+    },
+    NEGATIVE_PAYMENT_AMOUNT: {
+        mutation: 'registerExternalPayments',
+        variable: ['data', 'payments'],
+        code: BAD_USER_INPUT,
+        type: 'INVALID_VALUE',
+        message: 'Payment amount cannot be negative or zero. Found invalid amount: {amount} for transaction: {transactionId}',
+    },
+}
+
+module.exports = {
+    ERRORS,
+    PAYMENTS_LIMIT,
+}
