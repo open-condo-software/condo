049786b45f7e0133fbd1546214791cc0830857c6
SavelevMatthew <savelevmatthew@gmail.com>
2026-05-04 08:31:51 +0500

    feat(bridge): INFRA-1622 add historical methods

---
+++ packages/bridge/src/types/methods/historical/PopHistoryState.ts
@@ -0,0 +1,7 @@
+export type PopHistoryStateParams = {
+    amount?: number
+}
+
+export type PopHistoryStateData = {
+    success: boolean
+}
---
+++ packages/bridge/src/types/methods/historical/PushHistoryState.ts
@@ -0,0 +1,7 @@
+import type { HistoryState } from '../../incoming-events/HistoryPopState'
+
+export type PushHistoryStateParams = HistoryState
+
+export type PushHistoryStateData = {
+    success: boolean
+}
