11878d3620370d0f7489171d5fa58da044ef46c9
Pavel <nomerdvadcatpyat@gmail.com>
2026-05-14 13:00:58 +0500

    feat(condo): DOMA-13105 remove png mascot

--- apps/condo/domains/subscription/components/SubscriptionBlockedContent.tsx
+++ apps/condo/domains/subscription/components/SubscriptionBlockedContent.tsx
@@ -22,7 +22,7 @@ export const SubscriptionBlockedContent: React.FC<SubscriptionBlockedContentProp
     <div className={styles.container}>
         <div className={styles.contentWrapper}>
             <Space direction='vertical' align='center' size={24}>
-                <Image src='/mascot/upgradePlan.png' alt='' width={240} height={240} className={styles.mascot} />
+                <Image src='/mascot/upgradePlan.webp' alt='' width={240} height={240} className={styles.mascot} />
                 <Space size={16} direction='vertical'>
                     <Typography.Title level={3}>{title}</Typography.Title>
                     <Typography.Paragraph type='secondary'>{description}</Typography.Paragraph>
