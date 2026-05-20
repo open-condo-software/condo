0ef6a909f13cf1eab872294843e45cae159dd0a3
Dmitry Kovyazin <dkoviazin@gmail.com>
2026-05-20 11:24:42 +0500

    feat(condo): DOMA-13196 upgrade billing receipts table

--- apps/condo/domains/billing/components/BillingPageContent/ReceiptsTable.tsx
+++ apps/condo/domains/billing/components/BillingPageContent/ReceiptsTable.tsx
@@ -1,33 +1,42 @@
-import { SortBillingReceiptsBy, BillingReceipt as BillingReceiptType, TourStepTypeType } from '@app/condo/schema'
-import { Row, Col, Typography, Space, type RowProps } from 'antd'
-import dayjs from 'dayjs'
+import { BillingReceipt as BillingReceiptType, BillingReceiptWhereInput, SortBillingReceiptsBy, TourStepTypeType, UserTypeType } from '@app/condo/schema'
+import { Col, Row, Space, Typography, type RowProps } from 'antd'
+import dayjs, { Dayjs } from 'dayjs'
 import get from 'lodash/get'
+import isEqual from 'lodash/isEqual'
 import getConfig from 'next/config'
 import { useRouter } from 'next/router'
-import React, { useCallback, useMemo, useState, CSSProperties, useEffect } from 'react'
+import React, { CSSProperties, useCallback, useEffect, useMemo, useRef, useState } from 'react'
 
 import bridge from '@open-condo/bridge'
-import { useDeepCompareEffect } from '@open-condo/codegen/utils/useDeepCompareEffect'
+import { useApolloClient } from '@open-condo/next/apollo'
+import { useAuth } from '@open-condo/next/auth'
 import { useIntl } from '@open-condo/next/intl'
+import {
+    ActionBar,
+    ActionBarProps,
+    Button,
+    FullTableState,
+    GetTableData,
+    Input,
+    RowSelectionState,
+    Table,
+    TableRef,
+} from '@open-condo/ui'
 
+import { BillingReceiptForOrganization as BillingReceiptForOrganizationGQL } from '@condo/domains/billing/gql'
 import { ServicesModal } from '@condo/domains/billing/components/BillingPageContent/ServicesModal'
 import { useReceiptTableColumns } from '@condo/domains/billing/hooks/useReceiptTableColumns'
 import { useReceiptTableFilters } from '@condo/domains/billing/hooks/useReceiptTableFilters'
 import { BillingReceiptForOrganization } from '@condo/domains/billing/utils/clientSchema'
-import Input from '@condo/domains/common/components/antd/Input'
+import { DeleteButtonWithConfirmModal } from '@condo/domains/common/components/DeleteButtonWithConfirmModal'
 import { BasicEmptyListView } from '@condo/domains/common/components/EmptyListView'
 import DatePicker from '@condo/domains/common/components/Pickers/DatePicker'
-import { Table, DEFAULT_PAGE_SIZE } from '@condo/domains/common/components/Table/Index'
+import { DEFAULT_PAGE_SIZE } from '@condo/domains/common/components/Table/Index'
 import { TableFiltersContainer } from '@condo/domains/common/components/TableFiltersContainer'
 import { useQueryMappers } from '@condo/domains/common/hooks/useQueryMappers'
-import { useSearch } from '@condo/domains/common/hooks/useSearch'
-import { getFiltersQueryData } from '@condo/domains/common/utils/filters.utils'
-import { getFiltersFromQuery } from '@condo/domains/common/utils/helpers'
-import { updateQuery } from '@condo/domains/common/utils/helpers'
-import {
-    getPageIndexFromOffset,
-    parseQuery,
-} from '@condo/domains/common/utils/tables.utils'
+import { useTableSearch } from '@condo/domains/common/hooks/useSearch'
+import { useTableTranslations } from '@condo/domains/common/hooks/useTableTranslations'
+import { defaultParseUrlQuery } from '@condo/domains/common/utils/tableUrls'
 import { useTourContext } from '@condo/domains/onboarding/contexts/TourContext'
 
 import { useBillingAndAcquiringContexts } from './ContextProvider'
@@ -38,86 +47,245 @@ const SORTABLE_PROPERTIES = ['toPay']
 const INPUT_STYLE: CSSProperties = { width: '18em' }
 const ITEMS_GUTTER: RowProps['gutter'] = [0, 24]
 const FILTERS_GUTTER: RowProps['gutter'] = [16, 20]
+const ASC = 'ASC'
+const DESC = 'DESC'
 
 export const ReceiptsTable: React.FC = () => {
     const intl = useIntl()
     const SearchPlaceholder = intl.formatMessage({ id: 'filters.FullSearch' })
     const LoadingErrorMessage = intl.formatMessage({ id: 'errors.LoadingError' })
+    const CancelSelectionMessage = intl.formatMessage({ id: 'global.cancelSelection' })
+    const DeleteMessage = intl.formatMessage({ id: 'Delete' })
+    const DontDeleteMessage = intl.formatMessage({ id: 'DontDelete' })
+    const ImpossibleToRestoreMessage = intl.formatMessage({ id: 'global.ImpossibleToRestore' })
 
+    const { user } = useAuth()
     const { billingContexts } = useBillingAndAcquiringContexts()
     const billingContext = billingContexts.length > 0 ? billingContexts[0] : null
     const currencyCode = get(billingContext, ['integration', 'currencyCode'], defaultCurrencyCode)
     const reportPeriod = get(billingContexts.find(({ lastReport }) => !!lastReport), ['lastReport', 'period'], null)
-    const contextIds = billingContexts.map(({ id }) => id)
+    const contextIdsKey = useMemo(() => billingContexts.map(({ id }) => id).sort().join(','), [billingContexts])
+    const contextIds = useMemo(() => contextIdsKey ? contextIdsKey.split(',') : [], [contextIdsKey])
     const hasToPayDetails = get(billingContext, ['integration', 'dataFormat', 'hasToPayDetails'], false)
     const hasServices = get(billingContext, ['integration', 'dataFormat', 'hasServices'], false)
     const hasServicesDetails = get(billingContext, ['integration', 'dataFormat', 'hasServicesDetails'], false)
+    const canManageReceipts = Boolean(user?.isAdmin) || user?.type === UserTypeType.Service
 
     const router = useRouter()
-    const { filters, sorters, offset } = parseQuery(router.query)
-    const currentPageIndex = getPageIndexFromOffset(offset, DEFAULT_PAGE_SIZE)
-    const filtersFromQuery: Record<string, string | unknown> = useMemo(() => getFiltersFromQuery(router.query), [router.query])
+    const tableRef = useRef<TableRef | null>(null)
+    const [search, handleSearchChange, setSearch] = useTableSearch(tableRef)
+    const [selectedRowsCount, setSelectedRowsCount] = useState<number>(0)
+    const [loadingError, setLoadingError] = useState<boolean>(false)
+    const [period, setPeriod] = useState<Dayjs | null>(() => reportPeriod ? dayjs(reportPeriod, 'YYYY-MM-DD') : null)
 
-    const [search, handleSearchChange] = useSearch()
-    const filterMetas = useReceiptTableFilters(reportPeriod, search)
-    const { filtersToWhere, sortersToSortBy } = useQueryMappers(filterMetas, SORTABLE_PROPERTIES)
+    const filterMetas = useReceiptTableFilters(reportPeriod, search, contextIds)
+    const { filtersToWhere, sortersToSortBy } = useQueryMappers<BillingReceiptWhereInput>(filterMetas, SORTABLE_PROPERTIES)
+    const mainTableColumns = useReceiptTableColumns(filterMetas, hasToPayDetails, currencyCode)
+    const columnLabels = useTableTranslations()
+    const initialTableState = useMemo(() => defaultParseUrlQuery(router.query, DEFAULT_PAGE_SIZE), [router.query])
+    const updateUrlQuery = useCallback((params: FullTableState) => {
+        const { startRow, filterState, sortState, rowSelectionState, globalFilter } = params
 
-    const onPeriodChange = useMemo(() => async (periodString) => {
-        setPeriod(periodString)
-        const newParameters = getFiltersQueryData({ ...filtersFromQuery, period: periodString ? dayjs(periodString).format( 'YYYY-MM-01') : null })
-        await updateQuery(router, { newParameters }, { shallow: true, resetOldParameters: false })
-    }, [router, filtersFromQuery])
+        const nextFilters = { ...filterState }
+        if (globalFilter && globalFilter.trim() !== '') {
+            nextFilters.search = globalFilter
+        } else {
+            delete nextFilters.search
+        }
 
-    const {
-        loading,
-        count: total,
-        objs: receipts,
-        error,
-        refetch,
-    } = BillingReceiptForOrganization.useObjects({
-        where: { ...filtersToWhere(filters), context: { id_in: contextIds } },
-        sortBy: sortersToSortBy(sorters) as SortBillingReceiptsBy[],
-        first: DEFAULT_PAGE_SIZE,
-        skip: (currentPageIndex - 1) * DEFAULT_PAGE_SIZE,
-    })
+        const sort = sortState?.length
+            ? `${sortState[0].id}_${sortState[0].desc ? DESC : ASC}`
+            : null
+
+        const nextQuery: Record<string, string | string[]> = { ...router.query } as Record<string, string | string[]>
+        const nextOffset = startRow > 0 ? String(startRow) : undefined
+        const nextFiltersValue = Object.keys(nextFilters).length > 0 ? JSON.stringify(nextFilters) : undefined
+        const nextSortValue = sort ?? undefined
+        const nextSelectedRows = rowSelectionState?.length > 0 ? JSON.stringify(rowSelectionState) : undefined
+
+        if (nextOffset) nextQuery.offset = nextOffset
+        else delete nextQuery.offset
+
+        if (nextFiltersValue) nextQuery.filters = nextFiltersValue
+        else delete nextQuery.filters
+
+        if (nextSortValue) nextQuery.sort = nextSortValue
+        else delete nextQuery.sort
+
+        if (nextSelectedRows) nextQuery.selectedRows = nextSelectedRows
+        else delete nextQuery.selectedRows
+
+        if (isEqual(router.query, nextQuery)) return
+
+        void router.replace({
+            pathname: router.pathname,
+            query: nextQuery,
+        }, undefined, { shallow: true })
+    }, [router])
 
     const { updateStepIfNotCompleted } = useTourContext()
-    useDeepCompareEffect(() => {
-        if (receipts.length > 0) {
-            updateStepIfNotCompleted(TourStepTypeType.UploadReceipts)
-        }
-    }, [receipts])
-
-    const mainTableColumns = useReceiptTableColumns(filterMetas, hasToPayDetails, currencyCode)
+    const apolloClient = useApolloClient()
+    const updateReceipt = BillingReceiptForOrganization.useUpdate({})
 
     const [modalIsVisible, setModalIsVisible] = useState(false)
     const [detailedReceipt, setDetailedReceipt] = useState<BillingReceiptType>(null)
-    const [period, setPeriod] = useState(dayjs(reportPeriod, 'YYYY-MM-DD'))
-    const showServiceModal = (receipt: BillingReceiptType) => {
+
+    const showServiceModal = useCallback((receipt: BillingReceiptType) => {
         setModalIsVisible(true)
         setDetailedReceipt(receipt || null)
-        return
-    }
-    const hideServiceModal = () => {
+    }, [])
+
+    const hideServiceModal = useCallback(() => {
         setModalIsVisible(false)
-    }
-    
-    useDeepCompareEffect(()=>{
-        const filtersPeriod = get(filters, 'period')
-        if (filtersPeriod) {
-            setPeriod(dayjs(filtersPeriod as string))
+    }, [])
+
+    const onPeriodChange = useCallback((value: Dayjs | null, dateString: string) => {
+        setPeriod(value)
+
+        const currentFilterState = tableRef.current?.api?.getFilterState() || {}
+        const nextFilterState = { ...currentFilterState }
+        if (value && dateString) {
+            nextFilterState.period = value.startOf('month').format('YYYY-MM-01')
+        } else {
+            nextFilterState.period = reportPeriod || undefined
         }
-    }, [filters])
+        tableRef.current?.api?.setFilterState(nextFilterState)
+    }, [reportPeriod])
+
+    const onRowClick = useCallback((record: BillingReceiptType) => {
+        if (hasServices) {
+            showServiceModal(record)
+        }
+    }, [hasServices, showServiceModal])
+
+    const dataSource: GetTableData<BillingReceiptType> = useCallback(async ({
+        filterState,
+        sortState,
+        startRow,
+        endRow,
+        globalFilter,
+    }) => {
+        const sortBy = sortersToSortBy(sortState) as SortBillingReceiptsBy[]
+        const where = {
+            ...filtersToWhere({
+                ...filterState,
+                search: globalFilter,
+            }),
+            context: { id_in: contextIds },
+        }
+
+        try {
+            const { data } = await apolloClient.query({
+                query: BillingReceiptForOrganizationGQL.GET_ALL_OBJS_WITH_COUNT_QUERY,
+                variables: {
+                    where,
+                    sortBy,
+                    first: endRow - startRow,
+                    skip: startRow,
+                },
+                fetchPolicy: 'network-only',
+            })
+
+            const rowData: BillingReceiptType[] = data?.objs?.filter(Boolean) ?? []
+            const rowCount = data?.meta?.count ?? 0
+            setLoadingError(false)
+
+            if (rowData.length > 0) {
+                updateStepIfNotCompleted(TourStepTypeType.UploadReceipts)
+            }
+
+            return {
+                rowData,
+                rowCount,
+            }
+        } catch (error) {
+            setLoadingError(true)
+            return {
+                rowData: [],
+                rowCount: 0,
+            }
+        }
+    }, [apolloClient, contextIds, filtersToWhere, sortersToSortBy, updateStepIfNotCompleted])
+
+    const softDeleteSelectedReceipts = useCallback(async () => {
+        const selectedRows = tableRef.current?.api?.getRowSelection() || []
+        if (!selectedRows.length) return
+
+        const deletedAt = new Date().toISOString()
+
+        for (const id of selectedRows) {
+            await updateReceipt({ deletedAt }, { id })
+        }
+
+        tableRef.current?.api?.resetRowSelection()
+        setSelectedRowsCount(0)
+        tableRef.current?.api?.setPagination({ startRow: 0, endRow: DEFAULT_PAGE_SIZE })
+        await tableRef.current?.api?.refetchData()
+    }, [updateReceipt])
+
+    const selectedReceiptsActionBarButtons: ActionBarProps['actions'] = useMemo(() => [
+        <DeleteButtonWithConfirmModal
+            key='deleteSelectedReceipts'
+            title={DeleteMessage}
+            message={ImpossibleToRestoreMessage}
+            okButtonLabel={DeleteMessage}
+            action={softDeleteSelectedReceipts}
+            buttonContent={DeleteMessage}
+            cancelMessage={DontDeleteMessage}
+            showCancelButton
+            cancelButtonType='primary'
+        />,
+        <Button
+            key='cancelReceiptSelection'
+            type='secondary'
+            onClick={() => {
+                tableRef.current?.api?.resetRowSelection()
+                setSelectedRowsCount(0)
+            }}
+        >
+            {CancelSelectionMessage}
+        </Button>,
+    ], [
+        CancelSelectionMessage,
+        DeleteMessage,
+        DontDeleteMessage,
+        ImpossibleToRestoreMessage,
+        softDeleteSelectedReceipts,
+    ])
+
+    const rowSelectionOptions = useMemo(() => ({
+        enableRowSelection: canManageReceipts,
+        onRowSelectionChange: (rowSelectionState: RowSelectionState) => {
+            setSelectedRowsCount(rowSelectionState.length)
+        },
+    }), [canManageReceipts])
+
+    const getRowId = useCallback((row: BillingReceiptType) => row.id, [])
+    const onTableReady = useCallback((nextTableRef: TableRef) => {
+        const tableSearch = nextTableRef.api.getGlobalFilter()
+        setSearch(String(tableSearch || ''))
+        setSelectedRowsCount(nextTableRef.api.getRowSelection().length)
+
+        const tablePeriod = get(nextTableRef.api.getFilterState(), 'period')
+        const nextPeriod = tablePeriod ? dayjs(String(tablePeriod), 'YYYY-MM-DD') : (reportPeriod ? dayjs(reportPeriod, 'YYYY-MM-DD') : null)
+        setPeriod(nextPeriod)
+    }, [reportPeriod, setSearch])
 
     useEffect(() => {
         const handleRedirect = async (event) => {
-            if (get(event, 'type') === 'condo-bridge') refetch()
+            if (get(event, 'type') === 'condo-bridge') {
+                await tableRef.current?.api?.refetchData()
+            }
         }
         bridge.subscribe(handleRedirect)
         return () => {
             bridge.unsubscribe(handleRedirect)
         }
-    }, [refetch])
+    }, [])
+
+    const SelectedItemsMessage = useMemo(() => {
+        return intl.formatMessage({ id: 'ItemsSelectedCount' }, { count: selectedRowsCount })
+    }, [intl, selectedRowsCount])
 
     const periodMetaSelect = useMemo(() => {
         return (
@@ -131,19 +299,9 @@ export const ReceiptsTable: React.FC = () => {
                 />
             </Space>
         )
-    }, [period, onPeriodChange])
+    }, [onPeriodChange, period])
 
-    const onRow = useCallback((record: BillingReceiptType) => {
-        return {
-            onClick: () => {
-                if (hasServices) {
-                    showServiceModal(record)
-                }
-            },
-        }
-    }, [hasServices])
-
-    if (error) {
+    if (loadingError) {
         return (
             <BasicEmptyListView>
                 <Typography.Title level={4}>
@@ -170,17 +328,31 @@ export const ReceiptsTable: React.FC = () => {
                             <Col xs={24} md={8}>{periodMetaSelect}</Col>
                         </Row>
                     </TableFiltersContainer>
-
                 </Col>
                 <Col span={24}>
-                    <Table
-                        loading={loading}
-                        totalRows={total}
-                        dataSource={receipts}
+                    <Table<BillingReceiptType>
+                        id='billing-receipts-table'
+                        dataSource={dataSource}
                         columns={mainTableColumns}
-                        onRow={onRow}
+                        pageSize={DEFAULT_PAGE_SIZE}
+                        onTableStateChange={updateUrlQuery}
+                        initialTableState={initialTableState}
+                        columnLabels={columnLabels}
+                        rowSelectionOptions={rowSelectionOptions}
+                        getRowId={getRowId}
+                        onTableReady={onTableReady}
+                        onRowClick={onRowClick}
+                        ref={tableRef}
                     />
                 </Col>
+                {canManageReceipts && selectedRowsCount > 0 && (
+                    <Col span={24}>
+                        <ActionBar
+                            message={SelectedItemsMessage}
+                            actions={selectedReceiptsActionBarButtons}
+                        />
+                    </Col>
+                )}
             </Row>
             <ServicesModal
                 receipt={detailedReceipt}
