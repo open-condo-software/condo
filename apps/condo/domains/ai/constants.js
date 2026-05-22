91ef2ca57c60de17a352a15e0164a5cbbafcaab3
Mikhail Gorbunov <toplenboren@gmail.com>
2026-05-22 13:36:06 +0500

    chore(ai): add todo to make scopes more secure

--- apps/condo/domains/ai/constants.js
+++ apps/condo/domains/ai/constants.js
@@ -268,12 +268,8 @@ const FLOW_META_SCHEMAS = {
  */
 const FLOW_TOKEN_SCOPES = {
     [FLOW_TYPES.CHAT_WITH_CONDO]: [
+        // Todo @toplenboren (DOMA-13291) make scopes logic more secure
         { gqlOperationType: 'query' }, // Allow all queries
-        { gqlOperationNames: ['allTickets', '...'] },
-        { gqlSchemas: ['User', '...'] },
-        // b2bAccessRight
-        // canReadTickets ...
-        // orgranization-scoped
     ],
 }
 
