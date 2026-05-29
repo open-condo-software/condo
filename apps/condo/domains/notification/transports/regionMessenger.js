b6662e444b9ac2312f7559176aa80f99d57cd737
Alexander-Turkin <aturkin@doma.ai>
2026-05-29 15:05:17 +0500

    feat(condo): DOMA-13299 add template and fix api

--- apps/condo/domains/notification/transports/regionMessenger.js
+++ apps/condo/domains/notification/transports/regionMessenger.js
@@ -3,6 +3,7 @@ const { get } = require('lodash')
 const conf = require('@open-condo/config')
 const { fetch } = require('@open-condo/keystone/fetch')
 const { getByCondition } = require('@open-condo/keystone/schema')
+const { i18n } = require('@open-condo/locales/loader')
 
 const { REGION_MESSENGER_TRANSPORT } = require('@condo/domains/notification/constants/constants')
 const { NO_REGION_MESSENGER_CHAT_FOR_USER, NO_USER_ID_TO_SEND_REGION_MESSENGER_NOTIFICATION } = require('@condo/domains/notification/constants/errors')
@@ -23,6 +24,7 @@ async function prepareMessageToSend (message) {
 
     if (!regionMessengerUserChat) throw new Error(NO_REGION_MESSENGER_CHAT_FOR_USER)
 
+
     const { text, html, inlineKeyboard } = await renderTemplate(REGION_MESSENGER_TRANSPORT, message)
 
     return { userId, text, html, regionMessengerChatId, inlineKeyboard }
@@ -36,23 +38,17 @@ async function send ({ regionMessengerChatId, text, html, inlineKeyboard } = {})
     const messageText = html || text
     const messageData = { text: messageText }
 
-    // Max API uses 'link' instead of 'inline_keyboard'
-    if (inlineKeyboard) {
-        messageData.link = inlineKeyboard
-    }
 
-    const proxyAgent = getProxyAgent()
 
-    const response = await fetch(`https://api.max.ru/bot${conf.REGION_MESSENGER_EMPLOYEE_BOT_TOKEN}/sendMessage`, {
+    const response = await fetch(`https://platform-api.max.ru/messages?chat_id=${regionMessengerChatId}`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
+            'Authorization': conf.REGION_MESSENGER_BOT_TOKEN,
         },
         body: JSON.stringify({
-            chatId: regionMessengerChatId,
             ...messageData,
         }),
-        agent: proxyAgent,
     })
 
     if (!response.ok) throw new Error('Request to region messenger failed')
