817d8d9cfeb4693bd1ce44eb59b1aa8771c1cf67
abshnko <2009add@gmail.com>
2026-06-03 11:08:32 +0300

    feat(create-miniapp-cli): do not use gradient for title

--- packages/create-miniapp-cli/src/utils/renderTitle.ts
+++ packages/create-miniapp-cli/src/utils/renderTitle.ts
@@ -1,25 +1,14 @@
-import gradient from 'gradient-string'
+import chalk from 'chalk'
 
 import { getUserPkgManager } from './getUserPkgManager.js'
 
 import { TITLE_TEXT } from '../consts.js'
 
-const themeColors = {
-    blue: '#3f8cff',
-    cyan: '#89ddff',
-    green: '#8ad17a',
-    magenta: '#fae4fc',
-    red: '#d0679d',
-    yellow: '#fffac2',
-}
-
 export const renderTitle = () => {
-    const gradientTitle = gradient(Object.values(themeColors))
-
     // resolves weird behavior where the ascii is offset
     const pkgManager = getUserPkgManager()
     if (pkgManager === 'yarn' || pkgManager === 'pnpm') {
         console.log('')
     }
-    console.log(gradientTitle.multiline(TITLE_TEXT))
+    console.log(chalk.hex('#8ad17a')(TITLE_TEXT))
 }
