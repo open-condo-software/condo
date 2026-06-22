b180fb9ce420a6d79fe44dc30d6225a77ce7da72
SavelevMatthew <savelevmatthew@gmail.com>
2026-06-22 10:25:45 +0500

    refactor(dev-portal-api): INFRA-1650 inject scripts in CSP-secure way

--- apps/dev-portal-api/domains/miniapp/utils/serverSchema/builds.js
+++ apps/dev-portal-api/domains/miniapp/utils/serverSchema/builds.js
@@ -1,38 +1,84 @@
 const crypto = require('crypto')
 
 const got = require('got')
+const { z } = require('zod')
+
 
 const conf = require('@open-condo/config')
+const { fetch } = require('@open-condo/keystone/fetch')
 
 const { modifyZipStream } = require('@dev-portal-api/domains/common/utils/streams')
 const { injectScriptTags } = require('@dev-portal-api/domains/miniapp/utils/serverSchema/html')
 
+const PACKAGE_JSON_SCHEMA = z.object({
+    name: z.string(),
+    version: z.string(),
+})
+
 const DISABLE_BUILD_RESTREAMING = conf['DISABLE_BUILD_RESTREAMING'] === 'true'
 const B2C_APP_BUILD_INJECTED_SCRIPTS = JSON.parse(conf['B2C_APP_BUILD_INJECTED_SCRIPTS'] || '[]')
 
-/**
- * Generates a hash for the build if build restreaming is enabled,
- * since single dev-portal-api build can produce multiple builds for condo because of injected scripts / B2CApp parameters
- * @returns {string|null}
- */
-function generateB2CAppBuildHash () {
-    if (DISABLE_BUILD_RESTREAMING) return null
+async function _fetchScriptContent ({ packageSrc, scriptSrc }) {
+    const response = await fetch(packageSrc)
+    if (response.status !== 200) throw new Error(`Failed to fetch script package description: ${response.status}`)
+    const body = await response.json()
+    const { name, version } = PACKAGE_JSON_SCHEMA.parse(body)
+    const scriptUrl = scriptSrc.replace('{{version}}', version)
+    const scriptResponse = await fetch(scriptUrl)
+    if (scriptResponse.status !== 200) throw new Error(`Failed to fetch script content: ${scriptResponse.status}`)
+    const scriptText = await scriptResponse.text()
 
-    return crypto.createHash('sha256').update(JSON.stringify({
-        scripts: B2C_APP_BUILD_INJECTED_SCRIPTS,
-        // TODO: take device permissions into account once we support generation of native config
-        permissions: [],
-    })).digest('hex')
+    return {
+        name,
+        version,
+        script: scriptText,
+    }
 }
 
-function getB2CAppBuildStream (publicUrl) {
+/**
+ * Prepares metadata for the build if build restreaming is enabled,
+ * since single dev-portal-api build can produce multiple builds for condo because of injected scripts / B2CApp parameters
+ * @returns {Promise<{hash: string | null, scripts: {name: string, version: string, script: string, injectPath: string}[], permissions: string[]}>}
+ */
+async function prepareB2CAppBuildMetadata () {
+    if (DISABLE_BUILD_RESTREAMING) return {
+        hash: null,
+        scripts: [],
+        permissions: [],
+    }
+
+    const scripts = await Promise.all(B2C_APP_BUILD_INJECTED_SCRIPTS.map(async ({ packageSrc, scriptSrc, injectPath }) => {
+        const { name, version, script } = await _fetchScriptContent({ packageSrc, scriptSrc })
+        return { name, version, script, injectPath }
+    }))
+
+    const signObject = {
+        dv: 1,
+        scripts: scripts.map(({ name, version, injectPath }) => `${name}:${version}:${injectPath}`),
+        permissions: [],
+    }
+
+    const hash = crypto.createHash('sha256').update(JSON.stringify(signObject)).digest('hex')
+
+    return {
+        hash,
+        scripts,
+        permissions: [],
+    }
+}
+
+/**
+ * Returns a stream of the build to upload to condo stand
+ * Might be modified to inject scripts via build restreaming
+ */
+function getB2CAppBuildStream (publicUrl, metadata) {
     const originalStream = got.stream(publicUrl)
 
     if (DISABLE_BUILD_RESTREAMING) return originalStream
 
     const htmlFileMatcher = /^.+\.html$/
-    const scriptTags = B2C_APP_BUILD_INJECTED_SCRIPTS
-        .map((src) => `<script src="${src}"></script>`)
+    const scriptTags = metadata.scripts
+        .map(({ injectPath }) => `<script src="${injectPath}"></script>`)
         .join('')
 
     return modifyZipStream(originalStream, {
@@ -45,10 +91,16 @@ function getB2CAppBuildStream (publicUrl) {
                 },
             },
         ],
+        afterEntries: ({ append }) => {
+            for (const { script, injectPath } of metadata.scripts) {
+                const scriptPath = `www/${injectPath}`
+                append({ path: scriptPath, content: script })
+            }
+        },
     })
 }
 
 module.exports = {
-    generateB2CAppBuildHash,
+    prepareB2CAppBuildMetadata,
     getB2CAppBuildStream,
 }
--- apps/dev-portal-api/domains/miniapp/utils/serverSchema/html.js
+++ apps/dev-portal-api/domains/miniapp/utils/serverSchema/html.js
@@ -1,9 +1,33 @@
-function injectScriptTags (content, scriptTags) {
+const { parse } = require('node-html-parser')
+
+function _prependScriptsRegexp (content, scriptTags) {
     const headMatch = /<head/i.exec(content)
     if (!headMatch) return content
     const headEnd = content.indexOf('>', headMatch.index)
     if (headEnd === -1) return content
     return content.slice(0, headEnd + 1) + scriptTags + content.slice(headEnd + 1)
+}
+
+function injectScriptTags (content, scriptTags) {
+    try {
+        const root = parse(content)
+
+        const cspTag = root.querySelector('meta[http-equiv="Content-Security-Policy"]')
+        if (cspTag) {
+            cspTag.insertAdjacentHTML('afterend', scriptTags)
+            return root.toString()
+        }
+
+        const head = root.querySelector('head')
+        if (head) {
+            head.insertAdjacentHTML('afterbegin', scriptTags)
+            return root.toString()
+        }
+
+        return content
+    } catch (e) {
+        return _prependScriptsRegexp(content, scriptTags)
+    }
 }
 
 module.exports = {
