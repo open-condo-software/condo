9734bf43065202d14cae6049087088c15ad26714
Pavel <nomerdvadcatpyat@gmail.com>
2026-06-23 17:14:27 +0500

    fix(files): DOMA-13373 rename files.test.js to files.spec.js

--- apps/condo/domains/common/schema/file.spec.js
+++ apps/condo/domains/common/schema/file.spec.js
@@ -5,15 +5,17 @@ const index = require('@app/condo/index')
 const conf = require('@open-condo/config')
 const { FileMiddlewareTests } = require('@open-condo/files/fileMiddleware.test')
 const { FileMiddlewareUtilsTests } = require('@open-condo/files/utils.test')
+const { setFakeClientMode } = require('@open-condo/keystone/test.utils')
 
 const { createTestOrganization } = require('@condo/domains/organization/utils/testSchema')
 const { makeClientWithNewRegisteredAndLoggedInUser, User } = require('@condo/domains/user/utils/testSchema')
 
 const TEST_FILE = path.resolve(conf['PROJECT_ROOT'], 'apps/condo/domains/common/test-assets/dino.png')
 
+setFakeClientMode(index, { excludeApps: ['NextApp', 'AdminUIApp'] })
 
 describe('external file middleware tests', () => {
-    FileMiddlewareTests(TEST_FILE, User, makeClientWithNewRegisteredAndLoggedInUser, createTestOrganization, index)
+    FileMiddlewareTests(TEST_FILE, User, makeClientWithNewRegisteredAndLoggedInUser, createTestOrganization)
 })
 
 describe('external file middleware utils', () => {
