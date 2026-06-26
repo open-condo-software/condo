be27587cba79f6067d3cd54e21c39da4c9c08764
Alexander-Turkin <aturkin@doma.ai>
2026-06-26 14:21:01 +0500

    feat(condo): DOMA-13296 review fix

--- apps/condo/domains/notification/tasks/deliverMessage.js
+++ apps/condo/domains/notification/tasks/deliverMessage.js
@@ -23,7 +23,6 @@ const {
     MESSAGE_DISABLED_BY_USER_STATUS,
     MESSAGE_DELIVERY_STRATEGY_AT_LEAST_ONE_TRANSPORT,
     MESSAGE_THROTTLED_STATUS,
-    WEBHOOK_TRANSPORT,
     TELEGRAM_TRANSPORT,
 } = require('@condo/domains/notification/constants/constants')
 const { ONE_MESSAGE_PER_THROTTLING_PERIOD_FOR_USER } = require('@condo/domains/notification/constants/errors')
@@ -31,7 +30,6 @@ const emailAdapter = require('@condo/domains/notification/transports/email')
 const pushAdapter = require('@condo/domains/notification/transports/push')
 const smsAdapter = require('@condo/domains/notification/transports/sms')
 const telegramAdapter = require('@condo/domains/notification/transports/telegram')
-const webhookAdapter = require('@condo/domains/notification/transports/webhook')
 const {
     Message,
     checkMessageTypeInBlackList,
@@ -50,7 +48,6 @@ const TRANSPORT_ADAPTERS = {
     [EMAIL_TRANSPORT]: emailAdapter,
     [PUSH_TRANSPORT]: pushAdapter,
     [TELEGRAM_TRANSPORT]: telegramAdapter,
-    [WEBHOOK_TRANSPORT]: webhookAdapter,
 }
 const MESSAGE_TASK_RETRY_STATUSES = [
     MESSAGE_PROCESSING_STATUS,
