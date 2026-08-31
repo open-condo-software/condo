f8a2a41afa04b630845bd2858cf11fcd8368bdbd
Matthew <50069872+SavelevMatthew@users.noreply.github.com>
2026-08-31 18:22:49 +0500

    fix: INFRA-1794 fix isHidden behaviour on publish and translations wording for auto-publish (#7967)
    * fix(dev-portal-api): INFRA-1794 fix isHidden behaviour
    
    * fix(dev-portal-api): INFRA-1794 fix wording

--- apps/dev-portal-web/domains/miniapp/hooks/useMutationCompletedHandler.tsx
+++ apps/dev-portal-web/domains/miniapp/hooks/useMutationCompletedHandler.tsx
@@ -7,9 +7,7 @@ type OnCompletedType = () => void
 export function useMutationCompletedHandler (): OnCompletedType {
     const intl = useIntl()
     const SuccessMessageTitle = intl.formatMessage({ id: 'pages.apps.any.id.notifications.successSave.title' })
-    const SuccessMessageDescription = intl.formatMessage({ id: 'pages.apps.any.id.notifications.successSave.description' })
-
     return useCallback(() => {
-        notification.success({ message: SuccessMessageTitle, description: SuccessMessageDescription })
-    }, [SuccessMessageTitle, SuccessMessageDescription])
+        notification.success({ message: SuccessMessageTitle })
+    }, [SuccessMessageTitle])
 }
