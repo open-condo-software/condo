1bb3accbc0d8ccc0f849d6542f800a0ea36350ad
Pavel <nomerdvadcatpyat@gmail.com>
2026-09-09 16:52:20 +0500

    refactor(condo): DOMA-13530 fixes after self review

--- apps/condo/schema.ts
+++ apps/condo/schema.ts
@@ -118046,7 +118046,6 @@ export type UnitType = {
 export type UpdateSubscriptionContextPaymentMethodInput = {
   bindingId?: InputMaybe<Scalars['String']['input']>;
   dv: Scalars['Int']['input'];
-  invoice?: InputMaybe<InvoiceWhereUniqueInput>;
   sender: SenderFieldInput;
   subscriptionContext: SubscriptionContextWhereUniqueInput;
 };
