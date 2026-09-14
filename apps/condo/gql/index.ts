5c425af63740f21b77ec856050e87b00959aac8d
Mikhail Gorbunov <toplenboren@gmail.com>
2026-09-14 18:45:09 +0500

    fix(condo): remove locale filtering from AISkill (#8012)

--- apps/condo/gql/index.ts
+++ apps/condo/gql/index.ts
@@ -1436,7 +1436,7 @@ export type GetBillingReceiptsByPropertyCountQueryResult = Apollo.QueryResult<Ty
 export const GetOrganizationBillingRecipientsDocument = gql`
     query getOrganizationBillingRecipients($organization: OrganizationWhereInput!) {
   recipients: allBillingRecipients(
-    where: {context: {organization: $organization, status: "Finished", deletedAt: null}}
+    where: {context: {organization: $organization, status: "Finished"}}
     first: 100
     sortBy: [createdAt_ASC]
   ) {
